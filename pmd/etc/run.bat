e94ae4851df9f0f3cc9021531d9120180e102998
Tom Copeland <tomcopeland@users.sourceforge.net>
2002-06-27 18:13:15 +0000

    oops, fixing a typo
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@97 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/etc/run.bat
+++ pmd/etc/run.bat
@@ -1,5 +1,6 @@
 @echo off
-set CLASSPATH=../lib/pmd-0.1.jar
+set CLASSPATH=../lib/pmd-0.2.jar
 set FILE=%1%
 set FORMAT=%2%
-java net.sourceforge.pmd.PMD %FILE% %FORMAT%
+set RULESET=%3%
+java net.sourceforge.pmd.PMD %FILE% %FORMAT% %RULESET%
