352ff6a8f1ee8f2926db9b8362bc74cc36d77aba
David Dixon-Peugh <dpeugh@users.sourceforge.net>
2002-06-28 13:57:03 +0000

    Probably wrong package. . . .
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@106 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/src/net/sourceforge/pmd/ast/AccessFlags.java
@@ -0,0 +1,25 @@
+package net.sourceforge.pmd.ast;
+
+public interface AccessFlags {
+    // Stolen Shamelessly from BCEL
+    /** Access flags for classes, fields and methods.
+     */
+    public final static short ACC_PUBLIC       = 0x0001;
+    public final static short ACC_PRIVATE      = 0x0002;
+    public final static short ACC_PROTECTED    = 0x0004;
+    public final static short ACC_STATIC       = 0x0008;
+    
+    public final static short ACC_FINAL        = 0x0010;
+    public final static short ACC_SYNCHRONIZED = 0x0020;
+    public final static short ACC_VOLATILE     = 0x0040;
+    public final static short ACC_TRANSIENT    = 0x0080;
+    
+    public final static short ACC_NATIVE       = 0x0100;
+    public final static short ACC_INTERFACE    = 0x0200;
+    public final static short ACC_ABSTRACT     = 0x0400;
+    public final static short ACC_STRICT       = 0x0800;
+    
+    // Applies to classes compiled by new compilers only
+    public final static short ACC_SUPER        = 0x0020;
+    
+}
