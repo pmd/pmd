176ff26a274feae15e9419e62b18978f3c2d7d0f
David Dixon-Peugh <dpeugh@users.sourceforge.net>
2002-07-01 16:05:20 +0000

    Testing RuleSet.
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@147 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/regress/test/net/sourceforge/pmd/MockRule.java
+++ pmd/regress/test/net/sourceforge/pmd/MockRule.java
@@ -6,14 +6,20 @@
 package test.net.sourceforge.pmd;
 
 import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.Report;
 import net.sourceforge.pmd.RuleContext;
+import net.sourceforge.pmd.RuleViolation;
 
+import java.util.Set;
 import java.util.List;
+import java.util.HashSet;
+import java.util.Iterator;
 
 public class MockRule implements Rule {
 
     private String name;
     private String desc;
+    private Set violations = new HashSet();
 
     public MockRule() {}
 
@@ -22,9 +28,19 @@ public class MockRule implements Rule {
         this.desc = desc;
     }
 
+    public void addViolation( RuleViolation violation ) {
+	violations.add( violation );
+    }
+
     public String getName() {return name;}
     public String getDescription() {return desc;}
 
     public void apply(List astCompilationUnits, RuleContext ctx) {
+	Report report = ctx.getReport();
+
+	Iterator vs = violations.iterator();
+	while (vs.hasNext()) {
+	    report.addRuleViolation( (RuleViolation) vs.next() );
+	}
     }
 }
---
+++ pmd/regress/test/net/sourceforge/pmd/RuleSetTest.java
@@ -0,0 +1,149 @@
+package test.net.sourceforge.pmd;
+
+import net.sourceforge.pmd.*;
+import net.sourceforge.pmd.ast.*;
+
+import java.io.StringReader;
+
+import java.util.Set;
+import java.util.List;
+import java.util.Random;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import junit.framework.*;
+
+public class RuleSetTest extends TestCase
+{
+    private String javaCode =
+	"public class Test { }";
+
+    public RuleSetTest( String name ) {
+	super( name );
+    }
+
+    public void testConstructor() {
+	RuleSet IUT = new RuleSet();
+    }
+
+    public void testRuleList() {
+	RuleSet IUT = new RuleSet();
+
+	assertEquals( "Size of RuleSet isn't zero.",
+		      0, IUT.size() );
+
+	MockRule rule = new MockRule();
+	IUT.addRule( rule );
+
+	assertEquals( "Size of RuleSet isn't one.",
+		      1, IUT.size() );
+
+	Set rules = IUT.getRules();
+	
+	Iterator i = rules.iterator();
+	assertTrue( "Empty Set",
+		    i.hasNext());
+	assertEquals( "Returned set of wrong size.",
+		      1, rules.size() );
+
+	assertEquals( "Rule isn't in ruleset.",
+		      rule, i.next() );
+
+    }
+
+    public void testApply0Rules() 
+	throws Throwable
+    {
+	RuleSet IUT = new RuleSet();
+	verifyRuleSet( IUT, 0, new HashSet() );
+    }
+
+    public void testApply1Rule()
+	throws Throwable
+    {
+	RuleSet IUT = new RuleSet();
+
+	MockRule rule = new MockRule();
+	RuleViolation violation = new RuleViolation( rule, 1 );
+	rule.addViolation( violation );
+
+	IUT.addRule( rule );
+
+	verifyRuleSet( IUT, 1, Collections.singleton( violation ) );
+    }
+
+    public void testApplyNRule() 
+	throws Throwable
+    {
+	RuleSet IUT = new RuleSet();
+
+	Random rand = new Random();
+	int numRules = rand.nextInt( 10 ) + 1;
+	Set ruleViolations = new HashSet();
+
+	for (int i = 0; i < numRules; i++) {
+	    MockRule rule = new MockRule();
+	    RuleViolation violation = new RuleViolation( rule, i );
+
+	    ruleViolations.add( violation );
+	    rule.addViolation( violation );
+
+	    IUT.addRule( rule );
+	}
+
+	verifyRuleSet( IUT, numRules, ruleViolations );
+    }
+
+    protected void verifyRuleSet( RuleSet IUT, 
+				  int size,
+				  Set values )
+	throws Throwable
+    {
+
+	RuleContext context = new RuleContext();
+	Set reportedValues = new HashSet();
+	Report report = new Report("xml");
+
+	context.setReport( report );
+	IUT.apply( makeCompilationUnits(),
+		   context );
+
+	assertEquals("Invalid number of Violations Reported",
+		     size, report.countViolationsInCurrentFile() );
+
+	Iterator violations =
+	    report.violationsInCurrentFile();
+	while (violations.hasNext()) {
+	    RuleViolation violation =
+		(RuleViolation) violations.next();
+
+	    reportedValues.add( violation );
+	    assertTrue( "Unexpected Violation Returned: " + 
+			violation,
+			values.contains( violation ) );
+	}
+
+	Iterator expected = values.iterator();
+	while (expected.hasNext()) {
+	    RuleViolation violation =
+		(RuleViolation) expected.next();
+	    assertTrue( "Expected Violation not Returned: " +
+			violation,
+			reportedValues.contains( violation ));
+	}    
+    }
+
+    
+    protected List makeCompilationUnits()
+	throws Throwable
+    {
+	List RC = new ArrayList();
+
+	JavaParser parser = new JavaParser( new StringReader( javaCode ));
+	RC.add( parser.CompilationUnit() );
+
+	return RC;
+    }
+}
