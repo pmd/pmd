e293ceb113d416c7446a76e6d7c784d2646c7558
Tom Copeland <tomcopeland@users.sourceforge.net>
2002-07-06 02:22:20 +0000

    tweak
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@221 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/net/sourceforge/pmd/rules/OverrideBothEqualsAndHashcodeRule.java
+++ pmd/src/net/sourceforge/pmd/rules/OverrideBothEqualsAndHashcodeRule.java
@@ -13,37 +13,48 @@ import net.sourceforge.pmd.ast.*;
 
 public class OverrideBothEqualsAndHashcodeRule extends AbstractRule implements Rule {
 
-    private boolean overridesEquals;
-    private boolean overridesHashcode;
+    private static class ViolationCriteria {
+        private boolean overridesEquals;
+        private boolean overridesHashcode;
+        public void overridesEquals() {
+            overridesEquals = true;
+        }
+        public void overridesHashcode() {
+            overridesHashcode = true;
+        }
+        public boolean isMet() {
+            return (overridesEquals && !overridesHashcode) || (!overridesEquals && overridesHashcode);
+        }
+    }
+
+    private ViolationCriteria criteria;
 
     /**
      * TODO this should work OK with inner classes... I think
      */
     public Object visit(ASTClassBody node, Object data) {
-        this.overridesEquals = false;
-        this.overridesHashcode = false;
+        criteria = new ViolationCriteria();
 
         super.visit(node, data);
 
-        if ((overridesEquals && !overridesHashcode) || (!overridesEquals && overridesHashcode)) {
+        if (criteria.isMet()) {
             RuleContext ctx = (RuleContext)data;
             ctx.getReport().addRuleViolation(createRuleViolation(ctx, 1));
         }
 
-        this.overridesEquals = false;
-        this.overridesHashcode = false;
+        criteria = new ViolationCriteria();
         return data;
     }
 
     public Object visit(ASTMethodDeclarator node, Object data) {
         AccessNode parent = (AccessNode)node.jjtGetParent();
-        // hashcode has no param
-        // TODO ensure it returns an int
+        // hashcode has no params
+        // TODO hashcode returns an int
         if (node.getImage().equals("hashcode") && parent.isPublic() && !parent.isStatic() && node.jjtGetChild(0).jjtGetNumChildren() == 0) {
-            overridesHashcode = true;
+            criteria.overridesEquals();
         } else if (node.getImage().equals("equals") && parent.isPublic()  && !parent.isStatic()) {
-            // TODO equals has 1 param
-            overridesEquals = true;
+            // TODO equals has 1 param of type Object
+            criteria.overridesHashcode();
         }
         return super.visit(node, data);
     }
