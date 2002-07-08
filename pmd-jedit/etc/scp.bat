188797425ea2dd1b27d09c45283dc4e469550ef0
Tom Copeland <tomcopeland@users.sourceforge.net>
2002-07-08 15:43:20 +0000

    Initial revision
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@230 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd-jedit/etc/scp.bat
@@ -0,0 +1,4 @@
+@echo off
+set CLASSPATH=build/
+set CLASSPATH=%CLASSPATH%;lib\pmd-0.3.jar
+set CLASSPATH=%CLASSPATH%;lib\jedit.jar
---
+++ pmd-jedit/src/net/sourceforge/pmd/jedit/PMDJEditPlugin.java
@@ -0,0 +1,92 @@
+/*
+ * User: tom
+ * Date: Jul 3, 2002
+ * Time: 2:33:24 PM
+ */
+package net.sourceforge.pmd.jedit;
+
+import org.gjt.sp.jedit.*;
+import org.gjt.sp.jedit.gui.OptionsDialog;
+
+import javax.swing.*;
+import java.util.Vector;
+import java.util.Iterator;
+import java.io.FileNotFoundException;
+import java.io.FileInputStream;
+import java.io.StringBufferInputStream;
+
+import net.sourceforge.pmd.*;
+import net.sourceforge.pmd.reports.ReportFactory;
+
+public class PMDJEditPlugin extends EBPlugin {
+
+
+/*
+    private static class PMDJEditOptionPane extends AbstractOptionPane implements OptionPane {
+        public PMDJEditOptionPane() {
+            super("pmd.general");
+        }
+        public String getName() {
+            return "PMD Option Pane";
+        }
+    }
+*/
+
+    public static final String NAME = "PMD";
+    public static final String MENU = "pmd-menu";
+    public static final String PROPERTY_PREFIX = "plugin.net.sourceforge.pmd.jedit.";
+//    public static final String OPTION_PREFIX = "options.pmd.";
+
+    private static PMDJEditPlugin instance = new PMDJEditPlugin();
+
+    public void createMenuItems(Vector menuItems) {
+        menuItems.addElement(GUIUtilities.loadMenu(MENU));
+    }
+
+/*
+    public void createOptionPanes(OptionsDialog dialog) {
+        dialog.addOptionPane(new PMDJEditOptionPane());
+    }
+*/
+
+    public void instanceCheck(View view) {
+        //view.getStatus().setMessage("HELLO PMD");
+        String text = view.getTextArea().getText();
+
+        PMD pmd = new PMD();
+
+        ReportFactory rf = new ReportFactory();
+        RuleContext ctx = new RuleContext();
+        RuleSetFactory ruleSetFactory = new RuleSetFactory();
+        RuleSet rules = ruleSetFactory.createRuleSet(pmd.getClass().getClassLoader().getResourceAsStream("rulesets/unusedcode.xml"));
+
+        ctx.setReport(rf.createReport("xml"));
+        ctx.setSourceCodeFilename("this");
+        try {
+            // TODO switch to use StringReader once PMD 0.4 gets released
+            pmd.processFile(new StringBufferInputStream(text), rules, ctx);
+
+            String msg = "";
+            for (Iterator i = ctx.getReport().iterator(); i.hasNext();) {
+                RuleViolation rv = (RuleViolation)i.next();
+                msg += rv.getDescription() + " at line " + rv.getLine() + System.getProperty("line.separator");
+            }
+            JOptionPane.showMessageDialog(view, msg, "PMD Results", JOptionPane.INFORMATION_MESSAGE);
+        } catch (FileNotFoundException fnfe) {
+            fnfe.printStackTrace();
+        }
+    }
+
+    public void instanceDisplayPreferencesDialog(View view) {
+        //view.getStatus().setMessage("HELLO PMD DIALOG");
+    }
+
+    public static void check(View view) {
+        instance.instanceCheck(view);
+    }
+
+    public static void displayPreferencesDialog(View view) {
+        instance.instanceDisplayPreferencesDialog(view);
+    }
+
+}
