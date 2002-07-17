ad0c2eca8097d2feb014056add39ab21f6c04478
David Dixon-Peugh <dpeugh@users.sourceforge.net>
2002-07-17 20:52:12 +0000

    Excesive Class Length.
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@419 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/src/net/sourceforge/pmd/rules/design/LongClassRule.java
@@ -0,0 +1,30 @@
+package net.sourceforge.pmd.rules.design;
+
+import net.sourceforge.pmd.AbstractRule;
+import net.sourceforge.pmd.RuleContext;
+
+import net.sourceforge.pmd.ast.*;
+
+/**
+ * This rule detects when a class exceeds a certain
+ * threshold.  i.e. if a class has more than 1000 lines
+ * of code.
+ */
+public class LongClassRule
+    extends AbstractRule
+{
+    public LongClassRule() { }
+
+    public Object visit( ASTClassDeclaration decl, Object data ) {
+	RuleContext ctx = (RuleContext) data;
+
+	if ((decl.getEndLine() - decl.getBeginLine()) > 1000) {
+	    String message = getMessage();
+	    ctx.getReport()
+		.addRuleViolation( createRuleViolation( ctx,
+							decl.getBeginLine(),
+							getMessage() ));
+	}
+	return null;
+    }
+}
