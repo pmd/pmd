246eb251592c72a9125318aad4ef3d5af3da62fa
Tom Copeland <tomcopeland@users.sourceforge.net>
2002-07-31 18:03:03 +0000

    *** empty log message ***
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@575 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/regress/test/net/sourceforge/pmd/cpd/OccurrencesTest.java
+++ pmd/regress/test/net/sourceforge/pmd/cpd/OccurrencesTest.java
@@ -21,23 +21,58 @@ public class OccurrencesTest  extends TestCase {
         assertTrue(!occs.getTiles().hasNext());
         assertTrue(occs.isEmpty());
         assertEquals(0, occs.size());
-    }
 
-    public void testInitialFrequencyCount() {
-        Occurrences occs = new Occurrences();
-        TokenSet ts = new TokenSet("foo");
-        ts.add(new Token('h', 0, "foo"));
-        ts.add(new Token('e', 1, "foo"));
-        ts.add(new Token('l', 2, "foo"));
-        ts.add(new Token('l', 3, "foo"));
-        ts.add(new Token('o', 4, "foo"));
-        occs.addInitial(ts);
+        occs.addInitial(createTokenSetFor("foo"));
         assertEquals(4, occs.size());
         assertTrue(occs.contains(new Token('h', 0, "foo")));
         Iterator i = occs.getOccurrences(new Tile(new Token('h', 0, "foo")));
         assertTrue(i.hasNext());
+        assertTrue(occs.getTiles().hasNext());
+        int count = 0;
+        for (Iterator foo = occs.getTiles(); foo.hasNext();) {
+            foo.next();
+            count++;
+        }
+        assertEquals(4, count);
+    }
+
+    public void testInitialFrequencyCount() {
+        Occurrences occs = new Occurrences();
+        occs.addInitial(createTokenSetFor("foo"));
+        Iterator i = occs.getOccurrences(new Tile(new Token('h', 0, "foo")));
         Occurrence occ = (Occurrence)i.next();
         assertEquals("foo", occ.getTokenSetID());
         assertEquals(0,occ.getIndex());
+    }
+
+    public void testContains() {
+        Occurrences occs = new Occurrences();
+        occs.addInitial(createTokenSetFor("foo"));
+        assertTrue(occs.contains(new Token('h', 0, "foo")));
+    }
+
+    public void testDeleteSolo() {
+        Occurrences occs = new Occurrences();
+        occs.addInitial(createTokenSetFor("foo"));
+        occs.deleteSoloTiles();
+        assertEquals(1, occs.size());
+        assertTrue(!occs.contains(new Token('h', 0, "foo")));
+        assertTrue(occs.contains(new Token('l', 2, "foo")));
+    }
+
+    public void testConsolidate() {
+        Occurrences occs = new Occurrences();
+        TokenSet ts = createTokenSetFor("foo");
+
+    }
+
+    private TokenSet createTokenSetFor(String id) {
+        TokenSet ts = new TokenSet(id);
+        ts.add(new Token('h', 0, id));
+        ts.add(new Token('e', 1, id));
+        ts.add(new Token('l', 2, id));
+        ts.add(new Token('l', 3, id));
+        ts.add(new Token('o', 4, id));
+        return ts;
     }
 }
