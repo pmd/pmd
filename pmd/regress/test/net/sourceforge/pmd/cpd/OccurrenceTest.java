48e40e17a6d5f2309d53637e09a9556e74d35c05
Tom Copeland <tomcopeland@users.sourceforge.net>
2002-07-31 16:26:13 +0000

    *** empty log message ***
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@569 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/cpd/OccurrenceTest.java
@@ -0,0 +1,23 @@
+/*
+ * User: tom
+ * Date: Jul 31, 2002
+ * Time: 12:20:23 PM
+ */
+package test.net.sourceforge.pmd.cpd;
+
+import junit.framework.TestCase;
+import net.sourceforge.pmd.cpd.Occurrence;
+import net.sourceforge.pmd.cpd.Token;
+
+public class OccurrenceTest extends TestCase {
+    public OccurrenceTest(String name) {
+        super(name);
+    }
+
+    public void testBasic() {
+        Occurrence occ = new Occurrence("foo", new Token('h',0,"foo"));
+        assertEquals("foo", occ.getTokenSetID());
+        assertEquals(0, occ.getIndex());
+        assertEquals(occ, new Occurrence("foo", new Token('a', 0, "bar")));
+    }
+}
