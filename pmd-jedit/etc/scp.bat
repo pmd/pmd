efc75ed31dbeeba33ba57308d4f718356893f0fd
Tom Copeland <tomcopeland@users.sourceforge.net>
2002-08-16 19:40:05 +0000

    preps for rls
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@717 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd-jedit/etc/scp.bat
+++ pmd-jedit/etc/scp.bat
@@ -1,6 +1,5 @@
 @echo off
 set CLASSPATH=..\build
-set CLASSPATH=%CLASSPATH%;..\lib\pmd-0.7.jar
 set CLASSPATH=%CLASSPATH%;..\lib\ErrorList.jar
 set CLASSPATH=%CLASSPATH%;..\lib\jedit.jar
 set CVS_RSH=c:\bin\ssh\ssh
--- pmd-jedit/etc/scp.sh
+++ pmd-jedit/etc/scp.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 CLASSPATH=../build/
-CLASSPATH=$CLASSPATH:../lib/pmd-0.7.jar
 CLASSPATH=$CLASSPATH:../lib/jedit.jar
