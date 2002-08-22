abf760435dc251f96a8cc9a07a075d3269c082a2
Tom Copeland <tomcopeland@users.sourceforge.net>
2002-08-22 21:08:21 +0000

    *** empty log message ***
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@756 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd-dcpd/src/net/sourceforge/pmd/dcpd/DCPD.java
+++ pmd-dcpd/src/net/sourceforge/pmd/dcpd/DCPD.java
@@ -11,22 +11,55 @@ import net.jini.core.lookup.ServiceMatches;
 import net.jini.core.lookup.ServiceTemplate;
 import net.jini.core.discovery.LookupLocator;
 import net.jini.core.entry.Entry;
+import net.sourceforge.pmd.cpd.*;
 
 import java.net.MalformedURLException;
 import java.rmi.RemoteException;
 import java.io.IOException;
+import java.io.File;
+import java.io.FileReader;
+import java.util.List;
+import java.util.Iterator;
 
 public class DCPD {
 
     private JavaSpace space;
+    private TokenSets tokenSets = new TokenSets();
 
-    public DCPD(String javaSpaceServerName) {
+    public DCPD(String javaSpaceURL) {
         try {
-            space = getSpace(javaSpaceServerName);
+            space = getSpace(javaSpaceURL);
         } catch (Exception e) {
             e.printStackTrace();
-            throw new RuntimeException("Couldn't connect to the space on " + javaSpaceServerName);
+            throw new RuntimeException("Couldn't connect to the space on " + javaSpaceURL);
         }
+
+        try {
+            add("C:\\j2sdk1.4.0_01\\src\\java\\lang\\", true);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            throw new RuntimeException("Couldn't load the files");
+        }
+
+        Entry wrapper = new TokenSetsWrapper(tokenSets);
+
+    }
+
+    private void add(String dir, boolean recurse) throws IOException {
+        FileFinder finder = new FileFinder();
+        List files = finder.findFilesFrom(dir, new JavaFileOrDirectoryFilter(), recurse);
+        for (Iterator i = files.iterator(); i.hasNext();) {
+            add(files.size(), (File)i.next());
+        }
+    }
+
+    private void add(int fileCount, File file) throws IOException {
+        Tokenizer t = new JavaTokensTokenizer();
+        TokenList ts = new TokenList(file.getAbsolutePath());
+        FileReader fr = new FileReader(file);
+        t.tokenize(ts, fr);
+        fr.close();
+        tokenSets.add(ts);
     }
 
     private JavaSpace getSpace(String javaSpaceServerName) throws ClassNotFoundException, MalformedURLException, IOException, RemoteException {
