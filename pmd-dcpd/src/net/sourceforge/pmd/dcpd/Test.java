210542a13477fa7c85542ffa8e7a086f3a3f866d
Tom Copeland <tomcopeland@users.sourceforge.net>
2002-08-22 20:48:50 +0000

    tweaked to use new file finders and whatnot
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@750 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd-dcpd/src/net/sourceforge/pmd/dcpd/Test.java
+++ pmd-dcpd/src/net/sourceforge/pmd/dcpd/Test.java
@@ -18,20 +18,13 @@ import java.util.Iterator;
 
 public class Test {
 
-
-    public static class JavaFileOrDirectoryFilter implements FilenameFilter {
-      public boolean accept(File dir, String filename) {
-          return filename.endsWith("java") || (new File(dir.getAbsolutePath() + System.getProperty("file.separator") + filename).isDirectory());
-      }
-    }
-
     private List files = new ArrayList();
     private TokenSets tokenSets = new TokenSets();
 
     public Test() {
         try {
             JavaSpace space = getSpace();
-            addRecursively("C:\\j2sdk1.4.0_01\\src\\java\\lang\\ref");
+            add("C:\\j2sdk1.4.0_01\\src\\java\\lang\\ref", true);
             Entry wrapper = convertTSS();
             System.out.println("token count = " + tokenSets.tokenCount());
 
@@ -73,30 +66,9 @@ public class Test {
         add(1, file);
     }
 
-    private void addRecursively(String dir) throws IOException {
-        addDirectory(dir, true);
-    }
-
-    private void addDirectory(String dir, boolean recurse) throws IOException {
-        File root = new File(dir);
-        List list = new ArrayList();
-        scanDirectory(root, list, recurse);
-        add(list);
-    }
-
-    private void scanDirectory(File dir, List list, boolean recurse) {
-     FilenameFilter filter = new JavaFileOrDirectoryFilter();
-     String[] possibles = dir.list(filter);
-     for (int i=0; i<possibles.length; i++) {
-        File tmp = new File(dir + System.getProperty("file.separator") + possibles[i]);
-        if (tmp.isDirectory()) {
-            if (recurse) {
-                scanDirectory(tmp, list, true);
-            }
-        } else {
-           list.add(new File(dir + System.getProperty("file.separator") + possibles[i]));
-        }
-     }
+    private void add(String dir, boolean recurse) throws IOException {
+        FileFinder finder = new FileFinder();
+        add(finder.findFilesFrom(dir, new JavaFileOrDirectoryFilter(), recurse));
     }
 
     public void add(String id, String input) throws IOException {
@@ -111,61 +83,9 @@ public class Test {
         ServiceRegistrar registrar = (new LookupLocator("jini://mordor")).getRegistrar();
         ServiceMatches sm = registrar.lookup(new ServiceTemplate(null, new Class[] {JavaSpace.class}, new Entry[] {}),  1);
         return (JavaSpace)sm.items[0].service;
-/*
-        for (int i=0; i<sm.totalMatches; i++) {
-            if (sm.items[i].service instanceof JavaSpace) {
-                return (JavaSpace)sm.items[i].service;
-            }
-        }
-*/
-        //throw new RuntimeException("Couldn't find a JavaSpace on mordor!");
-/*
-        JavaSpace space = null;
-        try {
-          LookupDiscovery lDisc = new LookupDiscovery(groups);
-          ServiceDiscoveryManager sdm = new ServiceDiscoveryManager(lDisc, null);
-          ServiceItem si = null;
-
-          si = sdm.lookup(new ServiceTemplate(null, serviceTypes, null),
-                null,
-                MAX_WAIT);
-
-          if (si != null) {
-            space = (JavaSpace si.service;
-          }
-
-        } catch (Exception e) {
-          e.printStackTrace();
-        }
-
-        return space;
-*/
     }
 
     public static void main(String[] args) {
         new Test();
     }
 }
-
-
-
-/*
-
-public Test() {
-    try {
-        Message msg = new Message();
-        msg.content = "Hello World Foo";
-        JavaSpace space = getSpace();
-
-        long start = System.currentTimeMillis();
-        for (int i=0; i<10; i++) {
-            space.write(msg, null, Lease.FOREVER);
-            Message result = (Message)space.take(new Message(), null, Long.MAX_VALUE);
-        }
-        long stop = System.currentTimeMillis();
-        System.out.println("that took " + (stop - start) + " milliseconds");
-    } catch (Exception e) {
-        e.printStackTrace();
-    }
-}
-*/
