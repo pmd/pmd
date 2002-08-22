5b44bc94081678caf325133c2ab4a8b03c08e2c7
Tom Copeland <tomcopeland@users.sourceforge.net>
2002-08-22 20:33:43 +0000

    *** empty log message ***
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@747 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd-dcpd/etc/go.bat
+++ pmd-dcpd/etc/go.bat
@@ -1,10 +1,15 @@
-@echo off 
+@echo off
 set CLASSPATH=
-set CLASSPATH=%CLASSPATH%;c:\jini-1_2_1\lib\outrigger.jar
-set CLASSPATH=%CLASSPATH%;c:\jini-1_2_1\lib\reggie.jar
-set CLASSPATH=%CLASSPATH%;c:\data\jini
-set CLASSPATH=%CLASSPATH%;c:\data\jini\pmd-0.9.jar
+set CLASSPATH=%CLASSPATH%;c:\data\pmd\pmd-dcpd\lib\runtimeonly\outrigger-dl.jar
+set CLASSPATH=%CLASSPATH%;c:\data\pmd\pmd-dcpd\lib\runtimeonly\reggie.jar
+set CLASSPATH=%CLASSPATH%;c:\data\pmd\pmd-dcpd\build
+set CLASSPATH=%CLASSPATH%;c:\data\pmd\pmd-dcpd\lib\pmd-0.9.jar
 
-java -Xms128M -Xmx384M -Djava.security.policy=c:\jini-1_2_1\policy\policy.all -Dcom.sun.jini.outrigger.spacename=JavaSpaces -Dcom.sun.jini.lookup.groups=public -cp %CLASSPATH% -Djava.rmi.server.codebase=http://mordor:8081/space-examples-dl.jar Test
+set MAIN=net.sourceforge.pmd.dcpd.Test
+set MEMORY_ARG=-Xms128M -Xmx384M
+set POLICY_ARG=-Djava.security.policy=c:\jini-1_2_1\policy\policy.all
+set SPACENAME_ARG=-Dcom.sun.jini.outrigger.spacename=JavaSpaces
+set GROUP_ARG=-Dcom.sun.jini.lookup.groups=public
+set CODEBASE=-Djava.rmi.server.codebase=http://mordor:8081/space-examples-dl.jar
 
-
+java %MEMORY_ARG% %POLICY_ARG% %SPACENAME_ARG% %GROUP_ARG% -cp %CLASSPATH% %CODEBASE% %MAIN%
