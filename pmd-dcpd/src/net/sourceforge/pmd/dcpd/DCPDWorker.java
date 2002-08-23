141a1a049a6160d9bba46a2f8858a2652ccfca37
Tom Copeland <tomcopeland@users.sourceforge.net>
2002-08-23 21:37:28 +0000

    *** empty log message ***
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@769 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd-dcpd/src/net/sourceforge/pmd/dcpd/DCPDWorker.java
+++ pmd-dcpd/src/net/sourceforge/pmd/dcpd/DCPDWorker.java
@@ -16,31 +16,12 @@ import java.rmi.MarshalledObject;
 
 public class DCPDWorker {
 
-    public class MyListener implements RemoteEventListener {
-
-        public MyListener() {}
-
-        public void notify(RemoteEvent event) throws UnknownEventException, RemoteException {
-            System.out.println("HOWDY!");
-            try {
-                Job job = (Job)space.take(new Job("test"), null, 1000);
-                if (job == null) {
-                    System.out.println("No job found");
-                } else {
-                    System.out.println("job = " + job.name);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
     private JavaSpace space;
 
     public DCPDWorker() {
         try {
             space = Util.findSpace("mordor");
-            space.notify(new Job("test"), null, new MyListener(), Lease.FOREVER, null);
+            space.notify(new Job("test"), null, new JobAddedListener(), Lease.FOREVER, null);
         } catch (Exception e) {
             e.printStackTrace();
         }
---
+++ pmd-dcpd/src/net/sourceforge/pmd/dcpd/JobAddedListener.java
@@ -0,0 +1,38 @@
+/*
+ * User: tom
+ * Date: Aug 23, 2002
+ * Time: 5:19:47 PM
+ */
+package net.sourceforge.pmd.dcpd;
+
+import net.jini.core.event.RemoteEventListener;
+import net.jini.core.event.RemoteEvent;
+import net.jini.core.event.UnknownEventException;
+import net.jini.space.JavaSpace;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class JobAddedListener extends UnicastRemoteObject implements RemoteEventListener {
+    private JavaSpace space;
+
+    public JobAddedListener() throws RemoteException {}
+
+    public JobAddedListener(JavaSpace space)  throws RemoteException  {
+        this.space = space;
+    }
+
+    public void notify(RemoteEvent event) throws UnknownEventException, RemoteException {
+        System.out.println("HOWDY!");
+        try {
+            Job job = (Job)space.take(new Job("test"), null, 1000);
+            if (job == null) {
+                System.out.println("No job found");
+            } else {
+                System.out.println("job = " + job.name);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
