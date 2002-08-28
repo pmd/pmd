60b7c6cf1302e9f349f3dbf1d36413bbd9bb621f
Tom Copeland <tomcopeland@users.sourceforge.net>
2002-08-28 15:28:25 +0000

    fixed null impl
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@807 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/net/sourceforge/pmd/cpd/CPDNullListener.java
+++ pmd/src/net/sourceforge/pmd/cpd/CPDNullListener.java
@@ -8,9 +8,9 @@ package net.sourceforge.pmd.cpd;
 import java.io.File;
 
 public class CPDNullListener implements CPDListener{
-    public boolean update(String msg) {return false;}
-    public boolean addedFile(int fileCount, File file) {return false;}
-    public boolean addingTokens(int tokenSetCount, int doneSoFar, String tokenSrcID) {return false;}
-    public boolean addedNewTile(Tile tile, int tilesSoFar, int totalTiles) {return false;}
+    public boolean update(String msg) {return true;}
+    public boolean addedFile(int fileCount, File file) {return true;}
+    public boolean addingTokens(int tokenSetCount, int doneSoFar, String tokenSrcID) {return true;}
+    public boolean addedNewTile(Tile tile, int tilesSoFar, int totalTiles) {return true;}
 		
 }
