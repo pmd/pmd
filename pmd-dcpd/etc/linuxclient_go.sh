0043b2837eb679933ef034fae91d7fccd2656589
Tom Copeland <tomcopeland@users.sourceforge.net>
2002-09-09 17:15:03 +0000

    tweak
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@874 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd-dcpd/etc/linuxclient_go.sh
+++ pmd-dcpd/etc/linuxclient_go.sh
@@ -1,14 +1,17 @@
-@echo off
-set CLASSPATH=
-set CLASSPATH=/home/tom/dcpdworker/outrigger-dl.jar
-set CLASSPATH=$CLASSPATH:/home/tom/dcpdworker/reggie.jar
-set CLASSPATH=$CLASSPATH:/home/tom/dcpdworker/pmd-0.9.jar
+#!/bin/bash
 
-set MAIN=net.sourceforge.pmd.dcpd.%1
-set MEMORY_ARG=-Xms128M -Xmx384M
-set POLICY_ARG=-Djava.security.policy=/home/tom/dcpdworker/policy.all
-set SPACENAME_ARG=-Dcom.sun.jini.outrigger.spacename=JavaSpaces
-set GROUP_ARG=-Dcom.sun.jini.lookup.groups=public
-set CODEBASE=-Djava.rmi.server.codebase=http://mordor:8081/dcpd.jar
+CLASSPATH=/home/tom/dcpdworker/outrigger-dl.jar
+CLASSPATH=$CLASSPATH:/home/tom/dcpdworker/reggie.jar
+CLASSPATH=$CLASSPATH:/home/tom/dcpdworker/pmd-0.9.jar
+CLASSPATH=$CLASSPATH:/home/tom/dcpdworker/dcpd.jar
 
-java $MEMORY_ARG $POLICY_ARG $SPACENAME_ARG $GROUP_ARG -cp $CLASSPATH $CODEBASE $MAIN $2
+MAIN=net.sourceforge.pmd.dcpd.DCPDWorker
+MEMORY_ARG="-Xms128M -Xmx384M"
+POLICY_ARG=-Djava.security.policy=/home/tom/dcpdworker/policy.all
+SPACENAME_ARG=-Dcom.sun.jini.outrigger.spacename=JavaSpaces
+GROUP_ARG=-Dcom.sun.jini.lookup.groups=public
+CODEBASE=-Djava.rmi.server.codebase=http://mordor:8081/dcpd.jar
+
+java $MEMORY_ARG $POLICY_ARG $SPACENAME_ARG $GROUP_ARG -cp $CLASSPATH $CODEBASE $MAIN
+
+
