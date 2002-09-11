73b99b0a4438db9428bd68062e31e16bc5914626
Tom Copeland <tomcopeland@users.sourceforge.net>
2002-09-11 20:19:28 +0000

    *** empty log message ***
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@919 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd-dcpd/etc/go.bat
+++ pmd-dcpd/etc/go.bat
@@ -7,7 +7,7 @@ set CLASSPATH=%CLASSPATH%;c:\data\pmd\pmd-dcpd\lib\pmd-1.0rc1.jar
 
 set MAIN=net.sourceforge.pmd.dcpd.%1
 set MEMORY_ARG=-Xms128M -Xmx384M
-set POLICY_ARG=-Djava.security.policy=c:\jini-1_2_1\policy\policy.all
+set POLICY_ARG=-Djava.security.policy=policy.all
 set SPACENAME_ARG=-Dcom.sun.jini.outrigger.spacename=JavaSpaces
 set GROUP_ARG=-Dcom.sun.jini.lookup.groups=public
 set CODEBASE=-Djava.rmi.server.codebase=http://mordor:8081/dcpd.jar
