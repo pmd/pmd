80fe65a0846a1cab937a2e82553f30d85ac7fcaa
Tom Copeland <tomcopeland@users.sourceforge.net>
2002-09-23 15:03:53 +0000

    added handy batch file
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@971 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/delete_old_dirs.bat
@@ -0,0 +1,7 @@
+@echo off
+rmdir /q /s src\com
+rmdir /q /s src\org
+rmdir /q /s src\net\sourceforge\pmd\reports
+rmdir /q /s src\net\sourceforge\pmd\swingui\viewer
+rmdir /q /s regress\net
+rmdir /q /s regress\test\com
