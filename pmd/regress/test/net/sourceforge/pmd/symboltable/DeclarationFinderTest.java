e5fa762a6da1410b6b65f43eabf55465e095f09e
Tom Copeland <tomcopeland@users.sourceforge.net>
2002-10-10 12:28:46 +0000

    more tests
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@1099 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/symboltable/DeclarationFinderTest.java
@@ -0,0 +1,36 @@
+/*
+ * User: tom
+ * Date: Oct 10, 2002
+ * Time: 8:20:05 AM
+ */
+package test.net.sourceforge.pmd.symboltable;
+
+import junit.framework.TestCase;
+import net.sourceforge.pmd.symboltable.DeclarationFinder;
+import net.sourceforge.pmd.symboltable.LocalScope;
+import net.sourceforge.pmd.symboltable.NameOccurrence;
+import net.sourceforge.pmd.ast.ASTVariableDeclaratorId;
+import net.sourceforge.pmd.ast.ASTVariableDeclarator;
+import net.sourceforge.pmd.ast.ASTLocalVariableDeclaration;
+
+public class DeclarationFinderTest extends TestCase {
+
+    public void testDeclarationsAreFound() {
+        DeclarationFinder df = new DeclarationFinder();
+
+        ASTVariableDeclaratorId node = new ASTVariableDeclaratorId(1);
+        node.setImage("foo");
+
+        ASTVariableDeclarator parent = new ASTVariableDeclarator(2);
+        node.jjtSetParent(parent);
+
+        ASTLocalVariableDeclaration gparent = new ASTLocalVariableDeclaration(3);
+        parent.jjtSetParent(gparent);
+
+        LocalScope scope = new LocalScope();
+        node.setScope(scope);
+        df.visit(node, null);
+
+        assertTrue(scope.contains(new NameOccurrence(NameDeclarationTest.createNode("foo", 10))));
+    }
+}
