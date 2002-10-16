5fad3cdca2dc09dba32c0452b6a88104ce75ec85
Tom Copeland <tomcopeland@users.sourceforge.net>
2002-10-16 19:38:14 +0000

    a nice refactoring, removed some not so nice code
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@1125 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/net/sourceforge/pmd/rules/UnusedPrivateInstanceVariableRule.java
+++ pmd/src/net/sourceforge/pmd/rules/UnusedPrivateInstanceVariableRule.java
@@ -14,28 +14,17 @@ import net.sourceforge.pmd.symboltable.*;
 
 public class UnusedPrivateInstanceVariableRule extends AbstractRule {
 
-    /**
-     * TODO - simplify this by
-     * 1) visiting UnmodifiedClassDeclarations
-     * 2) getting the scope
-     * 3) getting all unused decls
-     * 4) getting the decl node
-     * 5) if it's private and not serialVersionUID, it's a rule violation
-     */
-    public Object visit(ASTVariableDeclaratorId node, Object data) {
-        if (node.jjtGetParent().jjtGetParent() instanceof ASTFieldDeclaration) {
-            ASTFieldDeclaration field = (ASTFieldDeclaration)node.jjtGetParent().jjtGetParent();
-            if (field.isPrivate()) {
+    public Object visit(ASTUnmodifiedClassDeclaration node, Object data) {
+        for (Iterator i = node.getScope().getUnusedDeclarations();i.hasNext();) {
+            NameDeclaration decl = (NameDeclaration)i.next();
+            SimpleNode declNode = decl.getNode();
+            AccessNode parent = (AccessNode)declNode.jjtGetParent().jjtGetParent();
+            if (parent.isPrivate() && !decl.getImage().equals("serialVersionUID")) {
                 RuleContext ctx = (RuleContext)data;
-                for (Iterator i =  node.getScope().getUnusedDeclarations(); i.hasNext();) {
-                    NameDeclaration decl = (NameDeclaration)i.next();
-                    if (decl.getImage().equals("serialVersionUID") || !decl.getNode().equals(node)) {
-                        continue;
-                    }
-                    ctx.getReport().addRuleViolation(createRuleViolation(ctx, decl.getLine(), MessageFormat.format(getMessage(), new Object[] {decl.getImage()})));
-                }
+                ctx.getReport().addRuleViolation(createRuleViolation(ctx, decl.getLine(), MessageFormat.format(getMessage(), new Object[] {decl.getImage()})));
             }
         }
-        return data;
+        return super.visit(node, data);
     }
+
 }
