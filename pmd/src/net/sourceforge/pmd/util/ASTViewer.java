a251a526fab1c70e0c54b6d5c34b2feb3a3f26c5
Tom Copeland <tomcopeland@users.sourceforge.net>
2002-10-18 19:12:53 +0000

    added a little utility to make it easier to see how an AST looks
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@1131 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/src/net/sourceforge/pmd/util/ASTViewer.java
@@ -0,0 +1,92 @@
+/*
+ * User: tom
+ * Date: Oct 18, 2002
+ * Time: 7:57:38 AM
+ */
+package net.sourceforge.pmd.util;
+
+import net.sourceforge.pmd.ast.JavaParser;
+import net.sourceforge.pmd.ast.ASTCompilationUnit;
+import net.sourceforge.pmd.ast.ParseException;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.StringReader;
+import java.io.PrintStream;
+
+public class ASTViewer {
+
+    private class MyPrintStream extends PrintStream {
+
+        public MyPrintStream() {
+            super(System.out);
+        }
+
+        private StringBuffer buf = new StringBuffer();
+
+        public void println(String s) {
+            super.println(s);
+            buf.append(s);
+            buf.append(System.getProperty("line.separator"));
+        }
+
+        public String getString() {
+            return buf.toString();
+        }
+    }
+
+    private class ShowListener implements ActionListener {
+        public void actionPerformed(ActionEvent ae) {
+            StringReader sr = new StringReader(codeEditorPane.getText());
+            JavaParser parser = new JavaParser(sr);
+            MyPrintStream ps = new MyPrintStream();
+            System.setOut(ps);
+            try {
+                ASTCompilationUnit c = parser.CompilationUnit();
+                c.dump("");
+                astArea.setText(ps.getString());
+            } catch (ParseException pe) {
+                astArea.setText(pe.fillInStackTrace().getMessage());
+            }
+            codeEditorPane.requestFocus();
+        }
+    }
+
+    private JEditorPane codeEditorPane = new JEditorPane();
+    private JTextArea astArea = new JTextArea();
+    private JFrame frame = new JFrame("AST Viewer");
+
+    public ASTViewer() {
+        JPanel codePanel = new JPanel();
+        codeEditorPane.setPreferredSize(new Dimension(500, 700));
+        JScrollPane codeScrollPane = new JScrollPane(codeEditorPane);
+        codePanel.add(codeScrollPane);
+
+        JPanel astPanel = new JPanel();
+        astArea.setRows(40);
+        astArea.setColumns(40);
+        JScrollPane astScrollPane = new JScrollPane(astArea);
+        astPanel.add(astScrollPane);
+
+
+        JButton showButton = new JButton("Show AST");
+        showButton.setMnemonic('s');
+        showButton.addActionListener(new ShowListener());
+
+        frame.getContentPane().setLayout(new BorderLayout());
+        frame.getContentPane().add(codePanel, BorderLayout.WEST);
+        frame.getContentPane().add(astPanel, BorderLayout.EAST);
+        frame.getContentPane().add(showButton, BorderLayout.SOUTH);
+
+        frame.setSize(1000, 800);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.show();
+    }
+
+    public static void main(String[] args) {
+        new ASTViewer();
+    }
+}
