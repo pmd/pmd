a8af2e9a40e15336020aa6ef2973a85715ae3162
Tom Copeland <tomcopeland@users.sourceforge.net>
2002-11-19 13:44:36 +0000

    more suggestions fm jiger
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@1252 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd-jedit/src/net/sourceforge/pmd/jedit/PMDJEditPlugin.java
+++ pmd-jedit/src/net/sourceforge/pmd/jedit/PMDJEditPlugin.java
@@ -58,7 +58,10 @@ public class PMDJEditPlugin extends EditPlugin {
     public void instanceCheckDirectory(View view) {
         if (jEdit.getBooleanProperty(PMDJEditPlugin.OPTION_UI_DIRECTORY_POPUP)) {
             final String dir = JOptionPane.showInputDialog(jEdit.getFirstView(), "Please type in a directory to scan", NAME, JOptionPane.QUESTION_MESSAGE);
-            if (dir == null || !(new File(dir)).exists() || !(new File(dir)).isDirectory() ) {
+            if (dir == null) {
+                return;
+            }
+            if (!(new File(dir)).exists() || !(new File(dir)).isDirectory() ) {
                 JOptionPane.showMessageDialog(jEdit.getFirstView(), dir + " is not a valid directory name", NAME, JOptionPane.ERROR_MESSAGE);
                 return;
             }
@@ -81,7 +84,10 @@ public class PMDJEditPlugin extends EditPlugin {
     public void instanceCheckDirectoryRecursively(View view) {
         if (jEdit.getBooleanProperty(PMDJEditPlugin.OPTION_UI_DIRECTORY_POPUP)) {
             final String dir = JOptionPane.showInputDialog(jEdit.getFirstView(), "Please type in a directory to scan recursively", NAME, JOptionPane.QUESTION_MESSAGE);
-            if (dir == null || !(new File(dir)).exists() || !(new File(dir)).isDirectory() ) {
+            if (dir == null) {
+                return;
+            }
+            if (!(new File(dir)).exists() || !(new File(dir)).isDirectory() ) {
                 JOptionPane.showMessageDialog(jEdit.getFirstView(), dir + " is not a valid directory name", NAME, JOptionPane.ERROR_MESSAGE);
                 return;
             }
