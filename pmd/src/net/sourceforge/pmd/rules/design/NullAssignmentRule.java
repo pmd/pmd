a6c3f8dcc8ed26d1492a5b0f39900f84d76ca52d
David Dixon-Peugh <dpeugh@users.sourceforge.net>
2002-12-13 14:47:15 +0000

    New NullAssignment Rule.  Checks for assignments to null outside of declarations.
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@1286 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/src/net/sourceforge/pmd/rules/design/NullAssignmentRule.java
@@ -0,0 +1,85 @@
+/**
+ * <copyright>
+ *  Copyright 1997-2002 BBNT Solutions, LLC
+ *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ *
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ * </copyright>
+ *
+ * Created on Dec 13, 2002
+ */
+package net.sourceforge.pmd.rules.design;
+
+import net.sourceforge.pmd.AbstractRule;
+import net.sourceforge.pmd.RuleContext;
+import net.sourceforge.pmd.ast.ASTAssignmentOperator;
+import net.sourceforge.pmd.ast.ASTConditionalExpression;
+import net.sourceforge.pmd.ast.ASTNullLiteral;
+import net.sourceforge.pmd.ast.ASTPrimaryExpression;
+import net.sourceforge.pmd.ast.ASTStatementExpression;
+import net.sourceforge.pmd.stat.StatisticalRule;
+
+/**
+ * @author dpeugh
+ *
+ * This checks for excessive Null Assignments.
+ * 
+ * For instance:
+ * 
+ * public void foo() {
+ *   Object x = null; // OK
+ *   // Some stuff
+ *   x = new Object(); // Also OK
+ *   // Some more stuff
+ *   x = null; // BAD
+ * }
+ */
+
+public class NullAssignmentRule extends AbstractRule {
+	private class NullAssignments extends Object {
+		private int count = 0;
+		public void addNullAssignment() { count++; }
+		public int getNullAssignments() { return count; }
+	}
+	
+	public Object visit(ASTStatementExpression expr, Object data ) {
+		if (!(data instanceof NullAssignments)) {
+			if (expr.jjtGetNumChildren() <= 2) {
+				return expr.childrenAccept(this, data);
+			}			
+			
+			if (expr.jjtGetChild(1) instanceof ASTAssignmentOperator) {
+				NullAssignments newData = new NullAssignments();
+				newData = (NullAssignments) expr.childrenAccept(this, newData);
+		
+				if (newData.count > 0) {
+        		   	RuleContext ctx = (RuleContext)data;
+            		ctx.getReport().addRuleViolation(createRuleViolation(ctx, expr.getBeginLine() ));
+				}
+			}
+		
+			return data;	
+		} else {
+			return expr.childrenAccept(this, data);
+		}
+	}
+	
+	public Object visit(ASTNullLiteral nullLiteral, Object data ) {
+		if (data instanceof NullAssignments) {
+			((NullAssignments) data).addNullAssignment();	
+		}		
+		return data;
+	}
+}
