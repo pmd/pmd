e939fac306aa6cd2e21a1370f1013c0080c3aa45
Tom Copeland <tomcopeland@users.sourceforge.net>
2003-03-18 16:17:59 +0000

    Random twiddling
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@1555 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/net/sourceforge/pmd/RuleProperties.java
+++ pmd/src/net/sourceforge/pmd/RuleProperties.java
@@ -18,21 +18,6 @@ public class RuleProperties implements Constants {
     // Constants
     private static final String SEPARATOR = "&PS;";
 
-    /**
-     ******************************************************************************
-     *
-     */
-    public RuleProperties() {
-        super();
-    }
-
-    /**
-     ******************************************************************************
-     *
-     * @param name
-     *
-     * @return
-     */
     public boolean containsKey(String name) {
         return m_properties.containsKey(name);
     }
@@ -48,22 +33,10 @@ public class RuleProperties implements Constants {
         return m_properties.keys();
     }
 
-    /**
-     ******************************************************************************
-     *
-     * @return
-     */
     public int size() {
         return m_properties.size();
     }
 
-    /**
-     ******************************************************************************
-     *
-     * @param name
-     *
-     * @return
-     */
     public String getValue(String name) {
         name = (name == null) ? EMPTY_STRING : name.trim();
 
@@ -80,13 +53,6 @@ public class RuleProperties implements Constants {
         return EMPTY_STRING;
     }
 
-    /**
-     ******************************************************************************
-     *
-     * @param name
-     *
-     * @return
-     */
     public String getValueType(String name) {
         name = (name == null) ? EMPTY_STRING : name.trim();
 
@@ -105,24 +71,10 @@ public class RuleProperties implements Constants {
         return EMPTY_STRING;
     }
 
-    /**
-     *******************************************************************************
-     *
-     * @param name
-     *
-     * @return
-     */
     public boolean getBooleanValue(String name) {
         return Boolean.getBoolean(getValue(name));
     }
 
-    /**
-     *******************************************************************************
-     *
-     * @param name
-     *
-     * @return
-     */
     public double getDoubleValue(String name) {
         try {
             return Double.parseDouble(getValue(name));
@@ -131,13 +83,6 @@ public class RuleProperties implements Constants {
         }
     }
 
-    /**
-     *******************************************************************************
-     *
-     * @param name
-     *
-     * @return
-     */
     public int getIntegerValue(String name) {
         try {
             return Integer.parseInt(getValue(name));
@@ -146,35 +91,16 @@ public class RuleProperties implements Constants {
         }
     }
 
-    /**
-     *******************************************************************************
-     *
-     * @param name
-     *
-     * @return
-     */
     public String getProperty(String name) {
         return getValue(name);
     }
 
-    /**
-     *******************************************************************************
-     *
-     * @param name
-     * @param value
-     */
     public Object setProperty(String name, String value) {
         setValue(name, value);
 
         return null;
     }
 
-    /**
-     *******************************************************************************
-     *
-     * @param name
-     * @param value
-     */
     public void setValue(String name, String value) {
         name = (name == null) ? EMPTY_STRING : name.trim();
 
@@ -190,12 +116,6 @@ public class RuleProperties implements Constants {
         }
     }
 
-    /**
-     *******************************************************************************
-     *
-     * @param name
-     * @param valueType
-     */
     public void setValueType(String name, String valueType) {
         name = (name == null) ? EMPTY_STRING : name.trim();
 
