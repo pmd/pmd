377dbba2b9aaecc35552772b53cdd68863178c31
Tom Copeland <tomcopeland@users.sourceforge.net>
2003-03-18 15:17:21 +0000

    Adding some tests
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@1553 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/jaxen/AttributeTest.java
@@ -0,0 +1,25 @@
+package test.net.sourceforge.pmd.jaxen;
+
+import junit.framework.TestCase;
+import net.sourceforge.pmd.jaxen.Attribute;
+
+public class AttributeTest extends TestCase {
+
+    public void testConstructor() {
+        Attribute a = new Attribute(null, "name", "value");
+        assertEquals("name", a.getName());
+        assertEquals("value", a.getValue());
+        assertNull(a.getParent());
+    }
+
+    public void testAccessors() {
+        Attribute a = new Attribute(null, null, null);
+        a.setName("name");
+        a.setValue("value");
+        a.setParent(null);
+        assertEquals("name", a.getName());
+        assertEquals("value", a.getValue());
+        assertNull(a.getParent());
+
+    }
+}
