c86488658be7c39f824bfd24780e22da95ba9216
Tom Copeland <tomcopeland@users.sourceforge.net>
2003-03-21 20:50:11 +0000

    rls preps
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@1588 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/etc/run.sh
+++ pmd/etc/run.sh
@@ -1,4 +1,4 @@
 FILE=$1
 FORMAT=$2
 RULESETFILES=$3
-java -cp ../lib/pmd-1.03.jar:../lib/jaxen-core-1.0-fcs.jar:../lib/saxpath-1.0-fcs.jar net.sourceforge.pmd.PMD $FILE $FORMAT $RULESETFILES
+java -cp ../lib/pmd-1.04.jar:../lib/jaxen-core-1.0-fcs.jar:../lib/saxpath-1.0-fcs.jar net.sourceforge.pmd.PMD $FILE $FORMAT $RULESETFILES
