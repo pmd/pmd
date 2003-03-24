bc2616e626a201859be746160895a8868bb39986
Tom Copeland <tomcopeland@users.sourceforge.net>
2003-03-24 21:34:43 +0000

    tweak
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@1605 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/upload_docs.sh
+++ pmd/upload_docs.sh
@@ -8,7 +8,6 @@ DOCS_FILE=docs.tar.gz
 cp xdocs/cpdresults.txt target/docs/
 cd target
 rm $DOCS_FILE
-tar zcf $DOCS_FILE docs
+tar zcf $DOCS_FILE docs/
 scp -i ~/.ssh/identity $DOCS_FILE tomcopeland@pmd.sourceforge.net:/home/groups/p/pm/pmd/
-rm $DOCS_FILE
 cd ../
