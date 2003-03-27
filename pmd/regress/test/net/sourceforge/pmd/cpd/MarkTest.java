718ba732e93a2fd8b8d86838ab6946354d787c7a
Tom Copeland <tomcopeland@users.sourceforge.net>
2003-03-27 21:01:11 +0000

    *** empty log message ***
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@1631 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/cpd/MarkTest.java
@@ -0,0 +1,17 @@
+package test.net.sourceforge.pmd.cpd;
+
+import net.sourceforge.pmd.cpd.Mark;
+import net.sourceforge.pmd.cpd.Locator;
+import net.sourceforge.pmd.cpd.CPDNullListener;
+import junit.framework.TestCase;
+
+import java.util.ArrayList;
+
+public class MarkTest extends TestCase {
+
+    public void testSimple() {
+        Locator loc = new Locator("/var/foo.java", 10);
+        Mark mark = new Mark(new ArrayList(), 5, loc, new CPDNullListener());
+        assertEquals(mark.getLocator(), loc);
+    }
+}
