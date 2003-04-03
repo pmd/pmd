e82483e5a8d19256b63d81992094a83a2d007a0b
Tom Copeland <tomcopeland@users.sourceforge.net>
2003-04-03 21:15:17 +0000

    a little more automation on the web site upload
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@1692 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/upload_docs.sh
+++ pmd/upload_docs.sh
@@ -11,3 +11,4 @@ rm $DOCS_FILE
 tar zcf $DOCS_FILE docs/
 scp -i ~/.ssh/identity $DOCS_FILE tomcopeland@pmd.sourceforge.net:/home/groups/p/pm/pmd/
 cd ../
+ssh -l tomcopeland pmd.sourceforge.net "cd /home/groups/p/pm/pmd/ && ./update_docs.sh"
