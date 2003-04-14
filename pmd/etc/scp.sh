ddf13668b3834f75d5fb9e09fa09fb77e1d796d5
Tom Copeland <tomcopeland@users.sourceforge.net>
2003-04-14 20:19:48 +0000

    Slowly moving towards a newer version of Maven
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@1743 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/etc/scp.sh
+++ pmd/etc/scp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MAVEN_HOME=/usr/local/pmd_maven/maven
+MAVEN_HOME=/usr/local/maven
 PATH=$MAVEN_HOME/bin:$PATH
 CLASSPATH=../:../build/:../lib/jaxen-core-1.0-fcs.jar:../lib/saxpath-1.0-fcs.jar:/usr/local/ant/lib/junit.jar:/usr/local/ant/lib/ant.jar
 export CLASSPATH MAVEN_HOME PATH
