4614fdfd6b6a481ef1fcc1eb303dee857925af8b
Tom Copeland <tomcopeland@users.sourceforge.net>
2003-04-16 18:03:49 +0000

    *** empty log message ***
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@1773 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/etc/stringify_test_files.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/ruby
-
-output = "private static final String TEST#{ARGV[1]} = \n"
-File.open(ARGV[0]).each {|line|
- output.concat "\"#{line.chomp}\" + CPD.EOL + \n"
-}
-output = output.slice(0, output.length-14)
-output.concat ";" 
-puts output
