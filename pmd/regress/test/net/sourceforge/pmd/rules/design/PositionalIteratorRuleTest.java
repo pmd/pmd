6314ffc7dc30927bd6658e0d13156d73061d9ba7
Tom Copeland <tomcopeland@users.sourceforge.net>
2003-04-16 20:48:44 +0000

    More test inlining
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@1782 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/regress/test/net/sourceforge/pmd/rules/design/PositionalIteratorRuleTest.java
+++ pmd/regress/test/net/sourceforge/pmd/rules/design/PositionalIteratorRuleTest.java
@@ -1,19 +1,53 @@
 package test.net.sourceforge.pmd.rules.design;
 
 import net.sourceforge.pmd.rules.design.PositionalIteratorRule;
+import net.sourceforge.pmd.cpd.CPD;
 import test.net.sourceforge.pmd.rules.RuleTst;
 
 public class PositionalIteratorRuleTest extends RuleTst {
 
+    private static final String TEST1 =
+    "public class PositionalIterator1 {" + CPD.EOL +
+    " public void foo(Iterator i) {" + CPD.EOL +
+    "  while(i.hasNext()) {" + CPD.EOL +
+    "   Object one = i.next();" + CPD.EOL +
+    "   " + CPD.EOL +
+    "   // 2 calls to next() inside the loop == bad!" + CPD.EOL +
+    "   Object two = i.next(); " + CPD.EOL +
+    "  }" + CPD.EOL +
+    " }" + CPD.EOL +
+    "}";
+
+    private static final String TEST2 =
+    "public class PositionalIterator2 {" + CPD.EOL +
+    " public void foo(Iterator i) {" + CPD.EOL +
+    "  while(i.hasNext()) {" + CPD.EOL +
+    "   Object one = i.next();" + CPD.EOL +
+    "  }" + CPD.EOL +
+    " }" + CPD.EOL +
+    "}";
+
+    private static final String TEST3 =
+    "public class PositionalIterator3 {" + CPD.EOL +
+    " public void foo() {" + CPD.EOL +
+    "  Iterator i = (new List()).iterator();" + CPD.EOL +
+    "  while(i.hasNext()) {" + CPD.EOL +
+    "   Object one = i.next();" + CPD.EOL +
+    "   Iterator j = (new List()).iterator();" + CPD.EOL +
+    "   while (j.hasNext()) {" + CPD.EOL +
+    "    j.next();" + CPD.EOL +
+    "   }" + CPD.EOL +
+    "  }" + CPD.EOL +
+    " }" + CPD.EOL +
+    "}";
+
     public void test1() throws Throwable {
-        runTestFromFile("PositionalIterator1.java", 1, new PositionalIteratorRule());
+        runTestFromString(TEST1, 1, new PositionalIteratorRule());
     }
-
     public void test2() throws Throwable {
-        runTestFromFile("PositionalIterator2.java", 0, new PositionalIteratorRule());
+        runTestFromString(TEST2, 0, new PositionalIteratorRule());
     }
-
     public void test3() throws Throwable {
-        runTestFromFile("PositionalIterator3.java", 0, new PositionalIteratorRule());
+        runTestFromString(TEST3, 0, new PositionalIteratorRule());
     }
 }
--- pmd/test-data/PositionalIterator2.java
+++
@@ -1,7 +0,0 @@
-public class PositionalIterator2 {
- public void foo(Iterator i) {
-  while(i.hasNext()) {
-   Object one = i.next();
-  }
- }
-}
