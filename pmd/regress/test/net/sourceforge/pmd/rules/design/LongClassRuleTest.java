c0ef77890e9388db6da6e8036ad2fb34084b709e
Tom Copeland <tomcopeland@users.sourceforge.net>
2003-04-17 13:40:24 +0000

    More test inlining
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@1793 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/regress/test/net/sourceforge/pmd/rules/design/LongClassRuleTest.java
+++ pmd/regress/test/net/sourceforge/pmd/rules/design/LongClassRuleTest.java
@@ -1,40 +1,57 @@
-/* $Id$ */
-
 package test.net.sourceforge.pmd.rules.design;
 
 import net.sourceforge.pmd.Report;
+import net.sourceforge.pmd.cpd.CPD;
 import net.sourceforge.pmd.rules.design.LongClassRule;
 import test.net.sourceforge.pmd.rules.RuleTst;
 
 public class LongClassRuleTest extends RuleTst {
 
-
     public LongClassRule getIUT() {
         LongClassRule IUT = new LongClassRule();
-        IUT.addProperty("minimum", "1000");
+        IUT.addProperty("minimum", "10");
         return IUT;
     }
 
     public void testShortClass() throws Throwable {
-        Report report = runTestFromFile("LongClass0.java", getIUT());
-        assertEquals(0, report.size());
+        runTestFromString(TEST0, 0, getIUT());
     }
 
     public void testLongClass() throws Throwable {
-        Report report = runTestFromFile("LongClass1.java", getIUT());
-        assertEquals(1, report.size());
+        runTestFromString(TEST1, 1, getIUT());
     }
 
     public void testLongClassWithLongerTest() throws Throwable {
         LongClassRule IUT = getIUT();
         IUT.addProperty("minimum", "2000");
-
-        Report report = runTestFromFile("LongClass1.java", IUT);
-        assertEquals(0, report.size());
+        runTestFromString(TEST1, 0, IUT);
     }
 
-    public void testNotQuiteLongClass() throws Throwable {
-        Report report = runTestFromFile("LongClass2.java", getIUT());
-        assertEquals(0, report.size());
-    }
+    private static final String TEST0 =
+    "public class LongMethod1 {" + CPD.EOL +
+    "    public static void main(String args[]) {" + CPD.EOL +
+    "	System.err.println(\"This is short.\");" + CPD.EOL +
+    "    }" + CPD.EOL +
+    "}";
+
+    private static final String TEST1 =
+    "public class LongClass1" + CPD.EOL +
+    "{" + CPD.EOL +
+    "    public void method0() {" + CPD.EOL +
+    "	System.err.println(\"This is a long class.\");" + CPD.EOL +
+    "	System.err.println(\"This is a long class.\");" + CPD.EOL +
+    "	System.err.println(\"This is a long class.\");" + CPD.EOL +
+    "	System.err.println(\"This is a long class.\");" + CPD.EOL +
+    "	System.err.println(\"This is a long class.\");" + CPD.EOL +
+    "	System.err.println(\"This is a long class.\");" + CPD.EOL +
+    "	System.err.println(\"This is a long class.\");" + CPD.EOL +
+    "	System.err.println(\"This is a long class.\");" + CPD.EOL +
+    "	System.err.println(\"This is a long class.\");" + CPD.EOL +
+    "	System.err.println(\"This is a long class.\");" + CPD.EOL +
+    "	System.err.println(\"This is a long class.\");" + CPD.EOL +
+    "	System.err.println(\"This is a long class.\");" + CPD.EOL +
+    "	System.err.println(\"This is a long class.\");" + CPD.EOL +
+    "    }" + CPD.EOL +
+    "}";
 }
+
