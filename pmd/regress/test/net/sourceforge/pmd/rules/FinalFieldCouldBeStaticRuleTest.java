889cac77b909201f9e7a29ba4dc985fec2a3cba0
Tom Copeland <tomcopeland@users.sourceforge.net>
2003-04-29 15:40:59 +0000

    Added new FinalFieldCouldBeStatic rule
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@1874 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/rules/FinalFieldCouldBeStaticRuleTest.java
@@ -0,0 +1,51 @@
+package test.net.sourceforge.pmd.rules;
+
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.cpd.CPD;
+import net.sourceforge.pmd.rules.XPathRule;
+
+public class FinalFieldCouldBeStaticRuleTest extends SimpleAggregatorTst {
+
+    private Rule rule;
+
+    public void setUp() {
+        rule = new XPathRule();
+        rule.addProperty("xpath", "//FieldDeclaration[@Final='true' and @Static='false']/VariableDeclarator/VariableInitializer/Expression/ConditionalAndExpression/InstanceOfExpression/PrimaryExpression/PrimaryPrefix/Literal");
+    }
+
+    public void testAll() {
+       runTests(new TestDescriptor[] {
+           new TestDescriptor(TEST1, "simple failure case", 1, rule),
+           new TestDescriptor(TEST2, "already static, OK", 0, rule),
+           new TestDescriptor(TEST3, "non-final, OK", 0, rule),
+           new TestDescriptor(TEST4, "non-primitive failure case - only works for String", 1, rule),
+           new TestDescriptor(TEST5, "final field that's a thread, OK", 0, rule)
+       });
+    }
+
+    private static final String TEST1 =
+    "public class Foo {" + CPD.EOL +
+    " public final int BAR = 42;" + CPD.EOL +
+    "}";
+
+    private static final String TEST2 =
+    "public class Foo {" + CPD.EOL +
+    " public static final int BAR = 42;" + CPD.EOL +
+    "}";
+
+    private static final String TEST3 =
+    "public class Foo {" + CPD.EOL +
+    " public int BAR = 42;" + CPD.EOL +
+    "}";
+
+    private static final String TEST4 =
+    "public class Foo {" + CPD.EOL +
+    " public final String BAR = \"42\";" + CPD.EOL +
+    "}";
+
+    private static final String TEST5 =
+    "public class Foo {" + CPD.EOL +
+    " public final Thread BAR = new Thread();" + CPD.EOL +
+    "}";
+
+}
