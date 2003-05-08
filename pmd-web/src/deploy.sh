89b7b567b7c27288cff2f21c61ccf40afa9e1247
Tom Copeland <tomcopeland@users.sourceforge.net>
2003-05-08 14:38:19 +0000

    tweak
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@1891 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd-web/src/deploy.sh
+++ pmd-web/src/deploy.sh
@@ -2,7 +2,7 @@
 
 scp webpmd.pl tomcopeland@pmd.sourceforge.net:/home/groups/p/pm/pmd/cgi-bin/
 scp PMD/Project.pm tomcopeland@pmd.sourceforge.net:/home/groups/p/pm/pmd/cgi-bin/PMD/
-scp pmd.rb tomcopeland@pmd.sourceforge.net:/home/users/t/to/tomcopeland/pmdweb/
+scp pmd.rb tomcopeland@pmd.sourceforge.net:pmdweb/
 scp processor.rb tomcopeland@pmd.sourceforge.net:pmdweb/
 scp jobs.tar.gz tomcopeland@pmd.sourceforge.net:pmdweb/
 
