00f2d7aa315a3612e58483a4293becb97b064240
Tom Copeland <tomcopeland@users.sourceforge.net>
2003-05-08 14:27:13 +0000

    To Linux and beyond!
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@1888 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd-web/src/copytolocal.sh
+++ pmd-web/src/copytolocal.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 scp tomcopeland@pmd.sourceforge.net:/home/groups/p/pm/pmd/cgi-bin/webpmd.pl .
 scp tomcopeland@pmd.sourceforge.net:/home/groups/p/pm/pmd/cgi-bin/PMD/Project.pm ./PMD/
 scp tomcopeland@pmd.sourceforge.net:/home/users/t/to/tomcopeland/pmdweb/pmd.rb .
