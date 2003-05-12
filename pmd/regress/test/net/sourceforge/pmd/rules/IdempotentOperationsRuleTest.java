4911abc0cb3d55f1ea0c5dadcffca2df6bbb24c6
Tom Copeland <tomcopeland@users.sourceforge.net>
2003-05-12 13:20:05 +0000

    Added source files and such for IdempotentOperationsRule
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@1916 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/rules/IdempotentOperationsRuleTest.java
@@ -0,0 +1,31 @@
+package test.net.sourceforge.pmd.rules;
+
+import net.sourceforge.pmd.cpd.CPD;
+import net.sourceforge.pmd.rules.IdempotentOperationsRule;
+
+public class IdempotentOperationsRuleTest extends SimpleAggregatorTst {
+
+    private static final String TEST1 =
+    "public class Foo {" + CPD.EOL +
+    " private void bar() { " + CPD.EOL +
+    "  int x = 2;" + CPD.EOL +
+    "  x = x;" + CPD.EOL +
+    " }" + CPD.EOL +
+    "}";
+
+    private static final String TEST2 =
+    "public class Foo {" + CPD.EOL +
+    " private void bar() { " + CPD.EOL +
+    "  int[] x = {2,3};" + CPD.EOL +
+    "  x[0] = x[1];" + CPD.EOL +
+    " }" + CPD.EOL +
+    "}";
+
+    public void testAll() {
+       runTests(new TestDescriptor[] {
+           new TestDescriptor(TEST1, "assignment of a local to itself", 1, new IdempotentOperationsRule()),
+           // FIXME
+           new TestDescriptor(TEST2, "assignment of one array element to another ", 1, new IdempotentOperationsRule())
+       });
+    }
+}
---
+++ pmd/src/net/sourceforge/pmd/rules/IdempotentOperationsRule.java
@@ -0,0 +1,42 @@
+package net.sourceforge.pmd.rules;
+
+import net.sourceforge.pmd.AbstractRule;
+import net.sourceforge.pmd.RuleContext;
+import net.sourceforge.pmd.ast.Node;
+import net.sourceforge.pmd.ast.ASTStatementExpression;
+import net.sourceforge.pmd.ast.ASTAssignmentOperator;
+import net.sourceforge.pmd.ast.ASTExpression;
+import net.sourceforge.pmd.ast.ASTPrimaryExpression;
+import net.sourceforge.pmd.ast.ASTName;
+import net.sourceforge.pmd.ast.SimpleNode;
+
+public class IdempotentOperationsRule extends AbstractRule {
+
+    public Object visit(ASTStatementExpression node, Object data) {
+        if (node.jjtGetNumChildren() != 3
+                || !(node.jjtGetChild(0) instanceof ASTPrimaryExpression)
+                || !(node.jjtGetChild(1) instanceof ASTAssignmentOperator)
+                || !(node.jjtGetChild(2) instanceof ASTExpression)
+        ) {
+            return super.visit(node, data);
+        }
+
+        SimpleNode lhs = (SimpleNode)node.jjtGetChild(0).jjtGetChild(0).jjtGetChild(0);
+        if (!(lhs instanceof ASTName)) {
+            return super.visit(node, data);
+        }
+
+        SimpleNode rhs = (SimpleNode)node.jjtGetChild(2).jjtGetChild(0).jjtGetChild(0).jjtGetChild(0).jjtGetChild(0).jjtGetChild(0);
+        if (!(rhs instanceof ASTName)) {
+            return super.visit(node, data);
+        }
+
+        if (!lhs.getImage().equals(rhs.getImage())) {
+            return super.visit(node, data);
+        }
+
+        RuleContext ctx = (RuleContext) data;
+        ctx.getReport().addRuleViolation(createRuleViolation(ctx, node.getBeginLine(), "Avoid idempotent operations"));
+        return data;
+    }
+}
