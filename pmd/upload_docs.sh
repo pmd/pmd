233eee41e0e8a448f74a5ea15bdccf7db16e0124
Tom Copeland <tomcopeland@users.sourceforge.net>
2003-05-29 15:10:48 +0000

    tweak
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@1948 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/upload_docs.sh
+++ pmd/upload_docs.sh
@@ -11,6 +11,6 @@ rm $DOCS_FILE
 tar zcf $DOCS_FILE docs/
 scp -i ~/.ssh/identity $DOCS_FILE tomcopeland@pmd.sourceforge.net:/home/groups/p/pm/pmd/
 cd ../
-ssh -l tomcopeland pmd.sourceforge.net "cd /home/groups/p/pm/pmd/ && ./update_docs.sh"
+ssh -l tomcopeland pmd.sourceforge.net "cd /home/groups/p/pm/pmd/ && rm -rf htdocs/xref && rm -rf htdocs/apidocs && ./update_docs.sh"
 
 rm velocity.log maven.log
