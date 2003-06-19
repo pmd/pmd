fcb7e42b98bafb56fb9e8207ba9e51c0b87c4e52
Tom Copeland <tomcopeland@users.sourceforge.net>
2003-06-19 19:15:22 +0000

    rls preps
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@1980 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/upload_docs.sh
+++ pmd/upload_docs.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-#rm -rf target
-#maven pmd-site
+option="${1}"
 
-maven -b xdoc:generate-from-pom xdoc:transform
-DOCS_FILE=docs.tar.gz
-cp xdocs/cpdresults.txt xdocs/cpp_cpdresults.txt target/docs/
-cd target
-rm $DOCS_FILE
-tar zcf $DOCS_FILE docs/
-scp -i ~/.ssh/identity $DOCS_FILE tomcopeland@pmd.sourceforge.net:/home/groups/p/pm/pmd/
-cd ../
-ssh -l tomcopeland pmd.sourceforge.net "cd /home/groups/p/pm/pmd/ && rm -rf htdocs/xref && rm -rf htdocs/apidocs && ./update_docs.sh"
+if [ $option == "gen" ]; then
+	echo "Generating from pom and transforming xdocs"
+	maven -b xdoc:generate-from-pom xdoc:transform
+elif [ $option == "all" ]; then
+	echo "Running maven pmd-site"
+	rm -rf target
+	maven pmd-site
+elif [ $option == "all" ]; then
+	echo "Generating xdocs and uploading"
+	maven -b xdoc:transform
+	DOCS_FILE=docs.tar.gz
+	cp xdocs/cpdresults.txt xdocs/cpp_cpdresults.txt target/docs/
+	cd target
+	rm $DOCS_FILE
+	tar zcf $DOCS_FILE docs/
+	scp -i ~/.ssh/identity $DOCS_FILE tomcopeland@pmd.sourceforge.net:/home/groups/p/pm/pmd/
+	cd ../
+	ssh -l tomcopeland pmd.sourceforge.net "cd /home/groups/p/pm/pmd/ && rm -rf htdocs/xref && rm -rf htdocs/apidocs && ./update_docs.sh"
+	rm velocity.log maven.log
+fi
 
-rm velocity.log maven.log
