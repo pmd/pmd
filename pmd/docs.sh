ab2170ea63c660e7f6edf2cd797811dd75188848
Tom Copeland <tomcopeland@users.sourceforge.net>
2003-07-17 17:20:59 +0000

    More documentation
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@2094 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/docs.sh
+++ pmd/docs.sh
@@ -4,14 +4,14 @@ option="${1}"
 
 if [ -z $option ]; then
 	echo "Generating from pom and transforming xdocs"
-	maven -b xdoc:generate-from-pom xdoc:transform
+	maven -f maven-build.xml -b xdoc:generate-from-pom xdoc:transform
 elif [ $option = "all" ]; then
 	echo "Running maven pmd-site"
 	rm -rf target
-	maven pmd-site
+	maven -f maven-build.xml pmd-site
 elif [ $option = "upload" ]; then
 	echo "Generating xdocs and uploading"
-	maven -b xdoc:transform
+	maven -f maven-build.xml -b xdoc:transform
 	DOCS_FILE=docs.tar.gz
 	cp xdocs/cpdresults.txt xdocs/cpp_cpdresults.txt target/docs/
 	cd target
