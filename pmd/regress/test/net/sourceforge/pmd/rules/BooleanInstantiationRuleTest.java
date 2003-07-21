3a879a7c4f78203113d4bdf5ee34a960584dcc44
Tom Copeland <tomcopeland@users.sourceforge.net>
2003-07-21 12:40:17 +0000

    Added new BooleanInstantiation; thx to Chris Webster for the code
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@2107 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/rules/BooleanInstantiationRuleTest.java
@@ -0,0 +1,42 @@
+package test.net.sourceforge.pmd.rules;
+
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.PMD;
+import net.sourceforge.pmd.rules.BeanMembersShouldSerializeRule;
+import net.sourceforge.pmd.rules.XPathRule;
+
+public class BooleanInstantiationRuleTest extends SimpleAggregatorTst {
+
+    private Rule rule;
+
+    public void setUp() {
+        rule = new XPathRule();
+        rule.addProperty(
+            "xpath",
+            "//AllocationExpression[not (ArrayDimsAndInits) and (Name/@Image='Boolean' or Name/@Image='java.lang.Boolean')]");
+    }
+
+    public void testAll() {
+       runTests(new TestDescriptor[] {
+           new TestDescriptor(TEST1, "simple failure case", 1, rule),
+           new TestDescriptor(TEST2, "new java.lang.Boolean", 1, rule),
+           new TestDescriptor(TEST3, "ok", 0, rule),
+       });
+    }
+
+    private static final String TEST1 =
+    "public class Foo {" + PMD.EOL +
+    " Boolean b = new Boolean(\"true\");" + PMD.EOL +
+    "}";
+
+    private static final String TEST2 =
+    "public class Foo {" + PMD.EOL +
+    " Boolean b = new java.lang.Boolean(\"true\");" + PMD.EOL +
+    "}";
+
+    private static final String TEST3 =
+    "public class Foo {" + PMD.EOL +
+    " Boolean b = Boolean.TRUE;" + PMD.EOL +
+    "}";
+
+}
