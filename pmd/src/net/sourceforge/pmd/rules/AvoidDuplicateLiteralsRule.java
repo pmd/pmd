4c73274944d7164edbebe490a58c445f47d7751d
no_author <no_author@no_author>
2003-09-25 20:20:21 +0000

    Added an 'exceptions' feature to the AvoidDuplicateLiteralsRule
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@2278 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/net/sourceforge/pmd/rules/AvoidDuplicateLiteralsRule.java
+++ pmd/src/net/sourceforge/pmd/rules/AvoidDuplicateLiteralsRule.java
@@ -14,14 +14,38 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.HashSet;
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.File;
+import java.io.LineNumberReader;
+import java.io.BufferedReader;
 
 public class AvoidDuplicateLiteralsRule extends AbstractRule {
 
     private Map literals = new HashMap();
+    private Set exceptions = new HashSet();
 
     public Object visit(ASTCompilationUnit node, Object data) {
         literals.clear();
+
+        if (super.hasProperty("exceptions")) {
+            exceptions = new HashSet();
+            try {
+                LineNumberReader reader = new LineNumberReader(new BufferedReader(new FileReader(new File(getStringProperty("exceptions")))));
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    exceptions.add(line);
+                }
+                reader.close();
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+        }
+
         super.visit(node, data);
+
         int threshold = getIntProperty("threshold");
         for (Iterator i = literals.keySet().iterator(); i.hasNext();) {
             String key = (String) i.next();
@@ -46,7 +70,12 @@ public class AvoidDuplicateLiteralsRule extends AbstractRule {
         }
 
         // just catching strings for now
-        if (node.getImage() == null || node.getImage().indexOf('\"') == -1) {
+        if (node.getImage() == null || node.getImage().indexOf('\"') == -1 || node.getImage().length() < 3) {
+            return data;
+        }
+
+        // skip any exceptions
+        if (exceptions.contains(node.getImage().substring(1, node.getImage().length()-1))) {
             return data;
         }
 
