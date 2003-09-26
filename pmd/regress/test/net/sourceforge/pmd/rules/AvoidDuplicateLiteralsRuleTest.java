bb2e6aa32b621651d555e7f839f71bdcd3444c7b
Tom Copeland <tomcopeland@users.sourceforge.net>
2003-09-26 18:12:21 +0000

    Added some more exceptions functionality to AvoidDuplicateLiteralsRule
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@2281 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/regress/test/net/sourceforge/pmd/rules/AvoidDuplicateLiteralsRuleTest.java
+++ pmd/regress/test/net/sourceforge/pmd/rules/AvoidDuplicateLiteralsRuleTest.java
@@ -3,6 +3,8 @@ package test.net.sourceforge.pmd.rules;
 import net.sourceforge.pmd.PMD;
 import net.sourceforge.pmd.rules.AvoidDuplicateLiteralsRule;
 
+import java.util.Set;
+
 public class AvoidDuplicateLiteralsRuleTest extends RuleTst {
 
     public static final String TEST1 =
@@ -40,10 +42,42 @@ public class AvoidDuplicateLiteralsRuleTest extends RuleTst {
     public void testTwoLiteralStringArgs() throws Throwable {
         runTestFromString(TEST1, 1, rule);
     }
+
     public void testLiteralIntArg() throws Throwable {
         runTestFromString(TEST2, 0, rule);
     }
+
     public void testLiteralFieldDecl() throws Throwable {
         runTestFromString(TEST3, 0, rule);
+    }
+
+    public void testStringParserEmptyString() {
+        AvoidDuplicateLiteralsRule.ExceptionParser p = new AvoidDuplicateLiteralsRule.ExceptionParser(',');
+        Set res = p.parse("");
+        assertTrue(res.isEmpty());
+    }
+    public void testStringParserSimple() {
+        AvoidDuplicateLiteralsRule.ExceptionParser p = new AvoidDuplicateLiteralsRule.ExceptionParser(',');
+        Set res = p.parse("a,b,c");
+        assertEquals(3, res.size());
+        assertTrue(res.contains("a"));
+        assertTrue(res.contains("b"));
+        assertTrue(res.contains("c"));
+    }
+    public void testStringParserEscapedChar() {
+        AvoidDuplicateLiteralsRule.ExceptionParser p = new AvoidDuplicateLiteralsRule.ExceptionParser(',');
+        Set res = p.parse("a,b,\\,");
+        assertEquals(3, res.size());
+        assertTrue(res.contains("a"));
+        assertTrue(res.contains("b"));
+        assertTrue(res.contains(","));
+    }
+    public void testStringParserEscapedEscapedChar() {
+        AvoidDuplicateLiteralsRule.ExceptionParser p = new AvoidDuplicateLiteralsRule.ExceptionParser(',');
+        Set res = p.parse("a,b,\\\\");
+        assertEquals(3, res.size());
+        assertTrue(res.contains("a"));
+        assertTrue(res.contains("b"));
+        assertTrue(res.contains("\\"));
     }
 }
--- pmd/src/net/sourceforge/pmd/rules/AvoidDuplicateLiteralsRule.java
+++ pmd/src/net/sourceforge/pmd/rules/AvoidDuplicateLiteralsRule.java
@@ -16,6 +16,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.HashSet;
+import java.util.StringTokenizer;
 import java.io.IOException;
 import java.io.FileReader;
 import java.io.File;
@@ -24,16 +25,74 @@ import java.io.BufferedReader;
 
 public class AvoidDuplicateLiteralsRule extends AbstractRule {
 
+    public static class ExceptionParser {
+
+        private static final char ESCAPE_CHAR = '\\';
+        private char delimiter;
+
+        public ExceptionParser(char delimiter) {
+            this.delimiter = delimiter;
+        }
+
+        public Set parse(String in) {
+            Set result = new HashSet();
+
+            StringBuffer currentToken = new StringBuffer();
+            boolean inEscapeMode = false;
+
+            for (int i=0; i<in.length(); i++) {
+
+                if (inEscapeMode) {
+                    inEscapeMode = false;
+                    currentToken.append(in.charAt(i));
+                    continue;
+                }
+
+                if (!inEscapeMode && in.charAt(i) == ESCAPE_CHAR) {
+                    inEscapeMode = true;
+                    continue;
+                }
+
+                if (in.charAt(i) == delimiter) {
+                    result.add(currentToken.toString());
+                    currentToken = new StringBuffer();
+                } else {
+                    currentToken.append(in.charAt(i));
+                }
+            }
+
+            if (currentToken.length()>0) {
+                result.add(currentToken.toString());
+                currentToken = new StringBuffer();
+            }
+
+            return result;
+        }
+    }
+
+    private static final char DEFAULT_SEPARATOR = ',';
+    private static final String EXCEPTION_LIST_PROPERTY = "exceptionlist";
+    private static final String SEPARATOR_PROPERTY = "separator";
+    private static final String EXCEPTION_FILE_NAME_PROPERTY = "exceptionfile";
+
     private Map literals = new HashMap();
     private Set exceptions = new HashSet();
 
     public Object visit(ASTCompilationUnit node, Object data) {
         literals.clear();
 
-        if (super.hasProperty("exceptionsFile")) {
+        if (hasProperty(EXCEPTION_LIST_PROPERTY)) {
+            ExceptionParser p;
+            if (hasProperty(SEPARATOR_PROPERTY)) {
+                p = new ExceptionParser(getStringProperty(SEPARATOR_PROPERTY).charAt(0));
+            } else {
+                p = new ExceptionParser(DEFAULT_SEPARATOR);
+            }
+            exceptions = p.parse(getStringProperty(EXCEPTION_LIST_PROPERTY));
+        } else if (hasProperty(EXCEPTION_FILE_NAME_PROPERTY)) {
             exceptions = new HashSet();
             try {
-                LineNumberReader reader = new LineNumberReader(new BufferedReader(new FileReader(new File(getStringProperty("exceptions")))));
+                LineNumberReader reader = new LineNumberReader(new BufferedReader(new FileReader(new File(getStringProperty(EXCEPTION_FILE_NAME_PROPERTY)))));
                 String line = null;
                 while ((line = reader.readLine()) != null) {
                     exceptions.add(line);
