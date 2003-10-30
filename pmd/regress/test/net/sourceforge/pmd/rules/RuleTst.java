6cf5ecdc6c321c9e632a00a701d330ecb6076d11
Tom Copeland <tomcopeland@users.sourceforge.net>
2003-10-30 18:59:28 +0000

    Removed some unused methods
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@2350 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/regress/test/net/sourceforge/pmd/rules/RuleTst.java
+++ pmd/regress/test/net/sourceforge/pmd/rules/RuleTst.java
@@ -34,8 +34,6 @@ import java.io.StringReader;
 
 public class RuleTst extends TestCase {
 
-    private static final String TEST_FILE_DIR = "test-data/";
-
     public void runTestFromString(String code, int expectedResults, Rule rule) throws Throwable {
         assertEquals(expectedResults, processUsingStringReader(code, rule).size());
     }
@@ -48,31 +46,6 @@ public class RuleTst extends TestCase {
         RuleSet rules = new RuleSet();
         rules.addRule(rule);
         p.processFile(new StringReader(code), rules, ctx);
-    }
-
-    public void runTestFromFile(String filename, int expectedResults, Rule rule) throws Throwable {
-        assertEquals(expectedResults, runTestFromFile(filename, rule).size());
-    }
-
-    public Report runTestFromFile(String fileName, Rule rule) throws Throwable {
-        Report report = new Report();
-        runTestFromPartialFileName(fileName, rule, report);
-        return report;
-    }
-
-    public void runTestFromFullFilename(String fullFileName, Rule rule, Report report) throws Throwable {
-        PMD p = new PMD();
-        RuleContext ctx = new RuleContext();
-        ctx.setReport(report);
-        ctx.setSourceCodeFilename(fullFileName);
-        RuleSet rules = new RuleSet();
-        rules.addRule(rule);
-        p.processFile(ResourceLoader.loadResourceAsStream(fullFileName), rules, ctx);
-    }
-
-    //////////////////////
-    private void runTestFromPartialFileName(String fileName, Rule rule, Report report) throws Throwable {
-        runTestFromFullFilename(TEST_FILE_DIR + fileName, rule, report);
     }
 
     private Report processUsingStringReader(String code, Rule rule) throws Throwable {
