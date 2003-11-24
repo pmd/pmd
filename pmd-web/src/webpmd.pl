3c8a8816c104f4d71d517ec3b8789bb5a11cd684
Tom Copeland <tomcopeland@users.sourceforge.net>
2003-11-24 15:37:08 +0000

    *** empty log message ***
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@2415 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd-web/src/webpmd.pl
+++ pmd-web/src/webpmd.pl
@@ -27,7 +27,7 @@ sub default() {
  print "PMD is a Java source code analysis tool - it checks your code for unused fields, empty try/catch/finally/if/while blocks, unused method parameters, and stuff like that.  There's much more info <a href=\"http://pmd.sf.net/\">here</a>.  This table contains the results of running PMD's <a href=\"http://pmd.sourceforge.net/rules/unusedcode.html\">unused code ruleset</a> against a bunch of Sourceforge projects.  The JavaNCSS column contains the lines of code analyzed as reported by the excellent <a href=\"http://www.kclee.com/clemens/java/javancss/\">JavaNCSS</a> utility.  This report is regenerated once a day.  The last run finished at ", $lastruntime; 
  print br();
  print br();
- print "Want to run PMD on your Java Sourceforge project?  Please post <a href=\"http://sourceforge.net/forum/forum.php?forum_id=188192\">here</a> or email <a href=\"mailto:tom\@infoether.com\">tom\@infoether.com</a>";
+ print "Want to run PMD on your Java Sourceforge project?  Please post <a href=\"http://sourceforge.net/forum/forum.php?forum_id=188192\">here</a> or email <a href=\"mailto:tom\@infoether.com\">tom\@infoether.com</a>.  Note that you can link to your project's PMD results <a href=\"http://sourceforge.net/forum/message.php?msg_id=2299020\">as illustrated here</a>.";
  print br();
  print br();
 
