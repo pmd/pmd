97ff65b1991198620095ed3223587191671d75c0
Tom Copeland <tomcopeland@users.sourceforge.net>
2003-12-01 21:17:34 +0000

    Checked in Nicolas Liochon's implementation of ProperCloneImplementationRule
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@2436 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/rules/ProperCloneImplementationRuleTest.java
@@ -0,0 +1,49 @@
+/**
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+*/
+package test.net.sourceforge.pmd.rules;
+
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.PMD;
+import net.sourceforge.pmd.rules.XPathRule;
+
+public class ProperCloneImplementationRuleTest extends SimpleAggregatorTst{
+
+  private Rule rule;
+
+  public void setUp() {
+      rule = new XPathRule();
+      rule.addProperty("xpath", "//ClassDeclaration//MethodDeclarator[@Image = 'clone'][count(FormalParameters/*) = 0][count(../Block//*[	(self::AllocationExpression) and	(./Name/@Image = ancestor::UnmodifiedClassDeclaration[position()=last()]/@Image)  ])> 0]");
+  }
+
+  public void testAll() {
+     runTests(new TestDescriptor[] {
+         new TestDescriptor(TEST1, "ok, calls super.clone", 0, rule),
+         new TestDescriptor(TEST2, "bad, Foo.clone() calls new Foo();", 1, rule),
+         new TestDescriptor(TEST3, "clone([whatever]) is fine", 0, rule),
+     });
+  }
+
+    private static final String TEST1 =
+    "public class Foo {" + PMD.EOL +
+    " void clone() {" + PMD.EOL +
+    "  super.clone();" + PMD.EOL +
+    " }" + PMD.EOL +
+    "}";
+
+    private static final String TEST2 =
+    "public class Foo {" + PMD.EOL +
+    " void clone() {" + PMD.EOL +
+    "  Foo f = new Foo();" + PMD.EOL +
+    " }" + PMD.EOL +
+    "}";
+
+    private static final String TEST3 =
+    "public class Foo {" + PMD.EOL +
+    " void clone(String fiddle) {" + PMD.EOL +
+    "  Foo f = new Foo();" + PMD.EOL +
+    " }" + PMD.EOL +
+    "}";
+}
