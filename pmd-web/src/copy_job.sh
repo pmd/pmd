79ffe0532dc71a7fd33ab66be2c43e88db07ad2c
Tom Copeland <tomcopeland@users.sourceforge.net>
2003-12-16 20:07:45 +0000

    Removing old cruft
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@2481 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd-web/src/copy_job.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-scp $1 tomcopeland@pmd.sf.net:/home/groups/p/pm/pmd/cgi-bin/jobs/
--- pmd-web/src/upload_webpmd.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-scp webpmd.pl tomcopeland@pmd.sf.net:/home/groups/p/pm/pmd/cgi-bin/
-scp PMD/Project.pm tomcopeland@pmd.sf.net:/home/groups/p/pm/pmd/cgi-bin/PMD/
