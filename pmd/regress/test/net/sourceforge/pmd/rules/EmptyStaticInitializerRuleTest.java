afdef6ec56c8eb2e33e6c3f2dc00e38b0e042288
Tom Copeland <tomcopeland@users.sourceforge.net>
2004-01-14 20:30:00 +0000

    Checked in Chad Loder's implementation of EmptyStaticInitializer
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@2515 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/rules/EmptyStaticInitializerRuleTest.java
@@ -0,0 +1,37 @@
+package test.net.sourceforge.pmd.rules;
+
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.PMD;
+import net.sourceforge.pmd.rules.XPathRule;
+
+public class EmptyStaticInitializerRuleTest extends SimpleAggregatorTst {
+
+    private Rule rule;
+
+    public void setUp() {
+        rule = new XPathRule();
+        rule.addProperty("xpath", "//ClassBodyDeclaration/Initializer[@Static='true']/Block[count(*)=0]");
+    }
+
+    public void testAll() {
+       runTests(new TestDescriptor[] {
+           new TestDescriptor(TEST1, "bad", 1, rule),
+           new TestDescriptor(TEST2, "ok", 0, rule),
+       });
+    }
+
+    private static final String TEST1 =
+    "public class Foo {" + PMD.EOL +
+    " static {}" + PMD.EOL +
+    "}";
+
+    private static final String TEST2 =
+    "public class Foo {" + PMD.EOL +
+    " static {" + PMD.EOL +
+    "  x++;" + PMD.EOL +
+    " }" + PMD.EOL +
+    "}";
+
+}
