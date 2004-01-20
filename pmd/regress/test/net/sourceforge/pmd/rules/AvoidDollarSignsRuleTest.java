8f6c1ddc3bfd74b4796875c4aca3a92eacfef320
Tom Copeland <tomcopeland@users.sourceforge.net>
2004-01-20 19:37:50 +0000

    Added Chad's dollar sign avoidance rules
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@2517 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/rules/AvoidDollarSignsRuleTest.java
@@ -0,0 +1,50 @@
+package test.net.sourceforge.pmd.rules;
+
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.rules.XPathRule;
+import net.sourceforge.pmd.PMD;
+
+public class AvoidDollarSignsRuleTest extends SimpleAggregatorTst {
+    private Rule rule;
+
+    public void setUp() {
+        rule = new XPathRule();
+        rule.addProperty("xpath", "//UnmodifiedClassDeclaration[contains(@Image, '$')]|//VariableDeclaratorId[contains(@Image, '$')]|//UnmodifiedInterfaceDeclaration[contains(@Image, '$')]|//MethodDeclarator[contains(@Image, '$')]");
+    }
+
+    public void testAll() {
+       runTests(new TestDescriptor[] {
+           new TestDescriptor(TEST1, "class Fo$o", 1, rule),
+           new TestDescriptor(TEST2, "variable fo$oo", 1, rule),
+           new TestDescriptor(TEST3, "method foo$oo", 1, rule),
+           new TestDescriptor(TEST4, "interface fo$oo", 1, rule),
+           new TestDescriptor(TEST5, "ok", 0, rule),
+       });
+    }
+
+    private static final String TEST1 =
+    "public class F$oo {}";
+
+    private static final String TEST2 =
+    "public class Foo {" + PMD.EOL +
+    " int fo$o;" + PMD.EOL +
+    "}";
+
+    private static final String TEST3 =
+    "public class Foo {" + PMD.EOL +
+    " void fo$o() {}" + PMD.EOL +
+    "}";
+
+    private static final String TEST4 =
+    "public interface Foo$oo {}";
+
+    private static final String TEST5 =
+    "public class Foo {" + PMD.EOL +
+    " void foo() {}" + PMD.EOL +
+    " int buz;" + PMD.EOL +
+    "}" + PMD.EOL +
+    "public interface Baz {} ";
+
+}
