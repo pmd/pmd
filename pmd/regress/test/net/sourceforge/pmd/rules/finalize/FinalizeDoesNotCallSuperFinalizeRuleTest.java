cd92c5b3f4e5b15eeecf9a0014bb541a548f7012
Tom Copeland <tomcopeland@users.sourceforge.net>
2004-01-21 21:36:19 +0000

    Added Chad Loder's implementation of FinalizeDoesNotCallSuperFinalize
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@2521 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/rules/finalize/FinalizeDoesNotCallSuperFinalizeRuleTest.java
@@ -0,0 +1,55 @@
+package test.net.sourceforge.pmd.rules.finalize;
+
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.PMD;
+import net.sourceforge.pmd.rules.XPathRule;
+
+public class FinalizeDoesNotCallSuperFinalizeRuleTest extends SimpleAggregatorTst {
+
+    private Rule rule;
+
+    public void setUp() {
+        rule = new XPathRule();
+        rule.addProperty("xpath", "//MethodDeclaration[MethodDeclarator[@Image='finalize'][not(FormalParameters/*)]]/Block/BlockStatement[last()][not(Statement/StatementExpression/PrimaryExpression/PrimaryPrefix[@Image='finalize'])]");
+    }
+
+    public void testAll() {
+       runTests(new TestDescriptor[] {
+           new TestDescriptor(TEST1, "bad", 1, rule),
+           new TestDescriptor(TEST2, "ok", 0, rule),
+       });
+    }
+
+    private static final String TEST1 =
+    "public class Foo {" + PMD.EOL +
+    " public void finalize() {" + PMD.EOL +
+    "  super.finalize();" + PMD.EOL +
+    "  int x = 2;" + PMD.EOL +
+    " }" + PMD.EOL +
+    "}";
+
+    private static final String TEST2 =
+    "public class Foo {" + PMD.EOL +
+    " public void finalize() {" + PMD.EOL +
+    "  int x = 2;" + PMD.EOL +
+    "  super.finalize();" + PMD.EOL +
+    " }" + PMD.EOL +
+    "}";
+
+/*
+TODO
+This should handle a call to super.finalize() within a finally block, e.g.
+
+protected void finalize()
+{
+   try { //  do something
+    }
+   finally
+   {
+      super.finalize(); // this is OK and should not be flagged
+   }
+}
+*/
+}
