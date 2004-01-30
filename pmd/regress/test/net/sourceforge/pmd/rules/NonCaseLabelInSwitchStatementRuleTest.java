670c908c1ee966f9c6785c37cae12b1d731add5b
Tom Copeland <tomcopeland@users.sourceforge.net>
2004-01-30 19:44:57 +0000

    Chad's implementation of NonCaseLabelInSwitchStatement
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@2524 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/regress/test/net/sourceforge/pmd/rules/NonCaseLabelInSwitchStatementRuleTest.java
+++ pmd/regress/test/net/sourceforge/pmd/rules/NonCaseLabelInSwitchStatementRuleTest.java
@@ -1,50 +1,49 @@
-/**
- * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
-*/
 package test.net.sourceforge.pmd.rules;
 
-import net.sourceforge.pmd.PMD;
-import net.sourceforge.pmd.Rule;
-import net.sourceforge.pmd.rules.XPathRule;
 import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
 import test.net.sourceforge.pmd.testframework.TestDescriptor;
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.PMD;
+import net.sourceforge.pmd.rules.XPathRule;
 
-public class SwitchStmtsShouldHaveDefaultRuleTest extends SimpleAggregatorTst {
-
+public class NonCaseLabelInSwitchStatementRuleTest extends SimpleAggregatorTst {
     private Rule rule;
 
     public void setUp() {
         rule = new XPathRule();
-        rule.addProperty("xpath", "//SwitchStatement[not(SwitchLabel[count(*) = 0])]");
+        rule.addProperty("xpath", "//SwitchStatement//BlockStatement/Statement/LabeledStatement");
     }
 
     public void testAll() {
        runTests(new TestDescriptor[] {
-           new TestDescriptor(TEST1, "simple failure case", 1, rule),
-           new TestDescriptor(TEST2, "simple ok case", 0, rule)
+           new TestDescriptor(TEST1, "label inside switch", 1, rule),
+           new TestDescriptor(TEST2, "ok", 0, rule),
        });
     }
 
     private static final String TEST1 =
     "public class Foo {" + PMD.EOL +
-    " public void bar() {" + PMD.EOL +
-    "  int x = 2;" + PMD.EOL +
+    " public void bar(int x) {" + PMD.EOL +
     "  switch (x) {" + PMD.EOL +
     "   case 2: int y=8;" + PMD.EOL +
+    "    break;" + PMD.EOL +
+    "   somelabel: " + PMD.EOL +
+    "    break;" + PMD.EOL +
+    "   default: " + PMD.EOL +
+    "    int j=8;" + PMD.EOL +
     "  }" + PMD.EOL +
     " }" + PMD.EOL +
     "}";
 
     private static final String TEST2 =
     "public class Foo {" + PMD.EOL +
-    " public void bar() {" + PMD.EOL +
-    "  int x = 2;" + PMD.EOL +
+    " public void bar(int x) {" + PMD.EOL +
     "  switch (x) {" + PMD.EOL +
     "   case 2: int y=8;" + PMD.EOL +
-    "   default: int j=8;" + PMD.EOL +
+    "    break;" + PMD.EOL +
+    "   default: " + PMD.EOL +
+    "    int j=8;" + PMD.EOL +
     "  }" + PMD.EOL +
     " }" + PMD.EOL +
     "}";
-
-
 }
