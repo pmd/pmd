6eb0cfecdd7f95d607c6b639cfbc4ca0c2ef43a9
Tom Copeland <tomcopeland@users.sourceforge.net>
2004-03-25 16:40:57 +0000

    Fully Mavenized now
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@2597 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/docs.sh
+++ pmd/docs.sh
@@ -4,14 +4,14 @@ option="${1}"
 
 if [ -z $option ]; then
 	echo "Generating from pom, regenerating ruleset docs, and transforming"
-	maven -b xdoc:generate-from-pom pmd:ruleset-docs xdoc:transform 
+	maven -qob xdoc:generate-from-pom pmd:ruleset-docs xdoc:transform 
 elif [ $option = "all" ]; then
 	echo "Running maven pmd-site"
 	rm -rf target
-	maven pmd-site
+	maven -qb pmd-site
 elif [ $option = "upload" ]; then
 	echo "Generating xdocs and uploading"
-	maven -b xdoc:generate-from-pom pmd:ruleset-docs xdoc:transform 
+	maven -qob xdoc:generate-from-pom pmd:ruleset-docs xdoc:transform 
 	DOCS_FILE=docs.tar.gz
 	cp xdocs/cpdresults.txt xdocs/cpp_cpdresults.txt target/docs/
 	cd target
@@ -20,6 +20,6 @@ elif [ $option = "upload" ]; then
 	scp -i ~/.ssh/identity $DOCS_FILE tomcopeland@pmd.sourceforge.net:/home/groups/p/pm/pmd/
 	cd ../
 	ssh -l tomcopeland pmd.sourceforge.net "cd /home/groups/p/pm/pmd/ && rm -rf htdocs/xref && rm -rf htdocs/apidocs && ./update_docs.sh"
-	rm velocity.log maven.log
 fi
+rm velocity.log maven.log
 
