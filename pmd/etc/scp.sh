7dcb636c3bc555156aaa80d40ca9d1861575b115
Tom Copeland <tomcopeland@users.sourceforge.net>
2004-04-15 15:52:53 +0000

    Added CVSROOT
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@2626 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/etc/scp.sh
+++ pmd/etc/scp.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 PATH=$MAVEN_HOME/bin:$PATH
+CVSROOT=:ext:tomcopeland@cvs.pmd.sourceforge.net:/cvsroot/pmd
 CLASSPATH=../build/:../lib/jaxen-core-1.0-fcs.jar:../lib/saxpath-1.0-fcs.jar:/usr/local/junit/junit.jar:/usr/local/ant/lib/ant.jar
 #CLASSPATH=../build/:../lib/jaxen-core-1.0-fcs.jar:../lib/saxpath-1.0-fcs.jar:/usr/local/junit/junit.jar:/usr/local/ant/lib/ant.jar:../lib/asm.jar:../lib/asm-util.jar:../lib/asm-tree.jar:../lib/kasm.jar
-export CLASSPATH PATH
+export CLASSPATH PATH CVSROOT
