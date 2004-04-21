b30b6203f015f9600b02e4031d5b471808b250cd
Tom Copeland <tomcopeland@users.sourceforge.net>
2004-04-21 16:26:54 +0000

    Uncommented test
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@2648 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/regress/test/net/sourceforge/pmd/ast/EncodingTest.java
+++ pmd/regress/test/net/sourceforge/pmd/ast/EncodingTest.java
@@ -2,16 +2,21 @@ package test.net.sourceforge.pmd.ast;
 
 import junit.framework.TestCase;
 import net.sourceforge.pmd.PMD;
+import net.sourceforge.pmd.TargetJDK1_4;
+import net.sourceforge.pmd.ast.ASTMethodDeclarator;
+import net.sourceforge.pmd.ast.ASTCompilationUnit;
+import net.sourceforge.pmd.ast.JavaParser;
+
+import java.util.List;
+import java.io.StringReader;
 
 public class EncodingTest extends TestCase {
 
     public void testDecodingOfUTF8() throws Throwable {
-/*
         JavaParser parser = (new TargetJDK1_4()).createParser(new StringReader(TEST_UTF8));
         ASTCompilationUnit acu = parser.CompilationUnit();
         List kids = acu.findChildrenOfType(ASTMethodDeclarator.class);
         assertEquals("é", ((ASTMethodDeclarator)kids.get(0)).getImage());
-*/
     }
 
     private static final String TEST_UTF8 =
