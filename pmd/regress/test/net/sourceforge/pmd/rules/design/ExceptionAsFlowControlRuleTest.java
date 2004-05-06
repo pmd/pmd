70d96d2c57f52fe0b60352ff38fa6af347ebf365
Tom Copeland <tomcopeland@users.sourceforge.net>
2004-05-06 18:45:24 +0000

    Checking in Will Sargent's ExceptionAsFlowControlRule
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@2674 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/rules/design/ExceptionAsFlowControlRuleTest.java
@@ -0,0 +1,39 @@
+package test.net.sourceforge.pmd.rules.design;
+
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
+import net.sourceforge.pmd.rules.design.NullAssignmentRule;
+import net.sourceforge.pmd.rules.design.ExceptionAsFlowControlRule;
+import net.sourceforge.pmd.PMD;
+
+public class ExceptionAsFlowControlRuleTest extends SimpleAggregatorTst {
+
+    public void testAll() {
+       runTests(new TestDescriptor[] {
+           new TestDescriptor(TEST1, "failure case", 1, new ExceptionAsFlowControlRule()),
+           new TestDescriptor(TEST2, "normal throw catch", 0, new ExceptionAsFlowControlRule())
+       });
+    }
+
+    private static final String TEST1 =
+    "public class Foo {" + PMD.EOL +
+    " void bar() {" + PMD.EOL +
+    "  try {" + PMD.EOL +
+    "   try {" + PMD.EOL +
+    "   } catch (Exception e) {" + PMD.EOL +
+    "    throw new WrapperException(e);" + PMD.EOL +
+    "    // this is essentially a GOTO to the WrapperException catch block" + PMD.EOL +
+    "   }" + PMD.EOL +
+    "  } catch (WrapperException e) {" + PMD.EOL +
+    "   // do some more stuff " + PMD.EOL +
+    "  }" + PMD.EOL +
+    " }" + PMD.EOL +
+    "}";
+
+    private static final String TEST2 =
+    "public class Foo {" + PMD.EOL +
+    " void bar() {" + PMD.EOL +
+    "  try {} catch (Exception e) {}" + PMD.EOL +
+    " }" + PMD.EOL +
+    "}";
+}
---
+++ pmd/src/net/sourceforge/pmd/rules/design/ExceptionAsFlowControlRule.java
@@ -0,0 +1,49 @@
+package net.sourceforge.pmd.rules.design;
+
+import net.sourceforge.pmd.ast.ASTName;
+import net.sourceforge.pmd.ast.Node;
+import net.sourceforge.pmd.ast.ASTThrowStatement;
+import net.sourceforge.pmd.ast.ASTType;
+import net.sourceforge.pmd.ast.ASTCatch;
+import net.sourceforge.pmd.ast.ASTTryStatement;
+import net.sourceforge.pmd.RuleContext;
+import net.sourceforge.pmd.AbstractRule;
+
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Catches the use of exception statements as a flow control device.
+ *
+ * @author Will Sargent
+ */
+public class ExceptionAsFlowControlRule extends AbstractRule {
+    public Object visit(ASTThrowStatement node, Object data) {
+        String throwName = getThrowsName(node);
+        for (Node parent = node.jjtGetParent(); parent != null; parent = parent.jjtGetParent()) {
+            if (parent instanceof ASTTryStatement) {
+                List list = ((ASTTryStatement) parent).getCatchBlocks();
+                for (Iterator iter = list.iterator(); iter.hasNext();) {
+                    ASTCatch catchStmt = (ASTCatch) iter.next();
+                    ASTType type = (ASTType) catchStmt.getFormalParameter().findChildrenOfType(ASTType.class).get(0);
+                    ASTName name = (ASTName) type.findChildrenOfType(ASTName.class).get(0);
+                    if (throwName != null && throwName.equals(name.getImage())) {
+                        ((RuleContext) data).getReport().addRuleViolation(createRuleViolation((RuleContext) data, name.getBeginLine()));
+                    }
+                }
+            }
+        }
+        return data;
+    }
+
+    private String getThrowsName(ASTThrowStatement node) {
+		Node childNode = node;
+		while (childNode.jjtGetNumChildren() > 0) {
+			childNode = childNode.jjtGetChild(0);
+		}
+		if (childNode instanceof ASTName) {
+            return ((ASTName) childNode).getImage();
+		}
+		return null;
+    }
+}
