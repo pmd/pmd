15606be898842b5f78ce667b03e0446e2af9a629
Tom Copeland <tomcopeland@users.sourceforge.net>
2004-05-17 16:10:00 +0000

    Implementing Mike's Double.NaN suggestion
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@2683 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/rules/BadComparisonRuleTest.java
@@ -0,0 +1,41 @@
+package test.net.sourceforge.pmd.rules;
+
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.RuleSetNotFoundException;
+import net.sourceforge.pmd.PMD;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
+import test.net.sourceforge.pmd.testframework.RuleTst;
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+
+public class BadComparisonRuleTest extends SimpleAggregatorTst {
+
+    private Rule rule;
+
+    public void setUp() throws RuleSetNotFoundException {
+        rule = findRule("rulesets/newrules.xml", "BadComparisonRule");
+    }
+
+    public void testAll() {
+       runTests(new TestDescriptor[] {
+           new TestDescriptor(TEST1, "comparison to Double.NaN", 1, rule),
+           new TestDescriptor(TEST2, "ok equality comparison", 0, rule),
+           new TestDescriptor(TEST3, "comparison to Float.NaN", 1, rule),
+       });
+    }
+
+    private static final String TEST1 =
+    "public class Foo {" + PMD.EOL +
+    " boolean x = (y == Double.NaN);" + PMD.EOL +
+    "}";
+
+    private static final String TEST2 =
+    "public class Foo {" + PMD.EOL +
+    " boolean x = (y == z);" + PMD.EOL +
+    "}";
+
+    private static final String TEST3 =
+    "public class Foo {" + PMD.EOL +
+    " boolean x = (y == Float.NaN);" + PMD.EOL +
+    "}";
+
+}
