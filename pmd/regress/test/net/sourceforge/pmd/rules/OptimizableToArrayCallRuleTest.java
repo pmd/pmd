755564bf81bccef29020f669c0d65c8d36f67e79
Tom Copeland <tomcopeland@users.sourceforge.net>
2004-05-18 19:23:57 +0000

    Implemented OptimizableToArrayCallRule
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@2687 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/rules/OptimizableToArrayCallRuleTest.java
@@ -0,0 +1,39 @@
+package test.net.sourceforge.pmd.rules;
+
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.RuleSetNotFoundException;
+import net.sourceforge.pmd.PMD;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+
+public class OptimizableToArrayCallRuleTest extends SimpleAggregatorTst {
+
+    private Rule rule;
+
+    public void setUp() throws RuleSetNotFoundException {
+        rule = findRule("rulesets/newrules.xml", "OptimizableToArrayCallRule");
+    }
+
+    public void testAll() {
+       runTests(new TestDescriptor[] {
+           new TestDescriptor(TEST1, "failure case", 1, rule),
+           new TestDescriptor(TEST2, "Array dimensioner uses method call, ok", 0, rule),
+           new TestDescriptor(TEST3, "Array dimensioner uses variable, ok", 0, rule),
+       });
+    }
+
+    private static final String TEST1 =
+    "public class Foo {" + PMD.EOL +
+    " {x.toArray(new Foo[0]);}" + PMD.EOL +
+    "}";
+
+    private static final String TEST2 =
+    "public class Foo {" + PMD.EOL +
+    " {x.toArray(new Foo[x.size()]);}" + PMD.EOL +
+    "}";
+
+    private static final String TEST3 =
+    "public class Foo {" + PMD.EOL +
+    " {x.toArray(new Foo[y]);}" + PMD.EOL +
+    "}";
+}
