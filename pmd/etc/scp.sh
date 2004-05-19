dbbd3dba071641124cb9b903ea626bc9a4e6df82
Tom Copeland <tomcopeland@users.sourceforge.net>
2004-05-19 18:21:53 +0000

    rls preps
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@2689 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/etc/scp.sh
+++ pmd/etc/scp.sh
@@ -2,7 +2,7 @@
 
 ANT_OPTS=-Xmx512m
 PATH=$MAVEN_HOME/bin:$PATH
-CVSROOT=:ext:tomcopeland@cvs.pmd.sourceforge.net:/cvsroot/pmd
+CVSROOT=:ext:tomcopeland@cvs.sourceforge.net:/cvsroot/pmd
 CLASSPATH=../build/:../lib/jaxen-core-1.0-fcs.jar:../lib/saxpath-1.0-fcs.jar:/usr/local/junit/junit.jar:/usr/local/ant/lib/ant.jar
 #CLASSPATH=../build/:../lib/jaxen-core-1.0-fcs.jar:../lib/saxpath-1.0-fcs.jar:/usr/local/junit/junit.jar:/usr/local/ant/lib/ant.jar:../lib/asm.jar:../lib/asm-util.jar:../lib/asm-tree.jar:../lib/kasm.jar
 export CLASSPATH PATH CVSROOT ANT_OPTS
