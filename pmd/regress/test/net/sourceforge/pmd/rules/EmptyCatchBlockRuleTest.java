2e45401a79e85e86de2149b9b6adfba418a3904f
Tom Copeland <tomcopeland@users.sourceforge.net>
2004-05-21 14:42:19 +0000

    Simplified test a bit
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@2695 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/regress/test/net/sourceforge/pmd/rules/EmptyCatchBlockRuleTest.java
+++ pmd/regress/test/net/sourceforge/pmd/rules/EmptyCatchBlockRuleTest.java
@@ -30,34 +30,23 @@ public class EmptyCatchBlockRuleTest extends SimpleAggregatorTst {
     }
 
     public static final String TEST1 =
-    "import java.io.*;" + PMD.EOL +
     "public class Foo {" + PMD.EOL +
-    " public Foo() {" + PMD.EOL +
-    "  try {" + PMD.EOL +
-    "   FileReader fr = new FileReader(\"/dev/null\");" + PMD.EOL +
-    "  } catch (Exception e) {}" + PMD.EOL +
-    "  try {" + PMD.EOL +
-    "   FileReader fr = new FileReader(\"/dev/null\");" + PMD.EOL +
-    "  } catch (Exception e) {" + PMD.EOL +
-    "   e.printStackTrace();" + PMD.EOL +
-    "   // this shouldn't show up on the report" + PMD.EOL +
-    "  }" + PMD.EOL +
+    " void bar() {" + PMD.EOL +
+    "  try {} catch (Exception e) {}" + PMD.EOL +
     " }" + PMD.EOL +
     "}";
 
     private static final String TEST2 =
     "public class Foo {" + PMD.EOL +
-    "    public Foo() {" + PMD.EOL +
-    "       try {" + PMD.EOL +
-    "       } catch (RuntimeException e) {e.getMessage();}" + PMD.EOL +
-    "    }" + PMD.EOL +
+    " void bar() {" + PMD.EOL +
+    "  try {} catch (RuntimeException e) {e.getMessage();}" + PMD.EOL +
+    " }" + PMD.EOL +
     "}";
 
     private static final String TEST3 =
     "public class Foo {" + PMD.EOL +
-    " private void foo() {" + PMD.EOL +
-    "  try {" + PMD.EOL +
-    "  } finally {" + PMD.EOL +
+    " void foo() {" + PMD.EOL +
+    "  try {} finally { " + PMD.EOL +
     "   try {" + PMD.EOL +
     "    int x =2;" + PMD.EOL +
     "   } catch (Exception e) {}" + PMD.EOL +
@@ -67,7 +56,7 @@ public class EmptyCatchBlockRuleTest extends SimpleAggregatorTst {
 
     private static final String TEST4 =
     "public class Foo {" + PMD.EOL +
-    " private void foo() {" + PMD.EOL +
+    " void foo() {" + PMD.EOL +
     "  try {" + PMD.EOL +
     "  } catch (Exception e) {" + PMD.EOL +
     "  } catch (Throwable t) {" + PMD.EOL +
@@ -77,17 +66,17 @@ public class EmptyCatchBlockRuleTest extends SimpleAggregatorTst {
 
     private static final String TEST5 =
     "public class Foo {" + PMD.EOL +
-    " public void foo() {" + PMD.EOL +
+    " void foo() {" + PMD.EOL +
     "  try {" + PMD.EOL +
     "  } catch (Exception e) {" + PMD.EOL +
-    "   blah.blah();" + PMD.EOL +
+    "   ;" + PMD.EOL +
     "  } finally {}" + PMD.EOL +
     " }" + PMD.EOL +
     "}";
 
     private static final String TEST6 =
     "public class Foo {" + PMD.EOL +
-    " public void foo() {" + PMD.EOL +
+    " void foo() {" + PMD.EOL +
     "  try {" + PMD.EOL +
     "  } catch (InterruptedException e) {}" + PMD.EOL +
     " }" + PMD.EOL +
@@ -95,7 +84,7 @@ public class EmptyCatchBlockRuleTest extends SimpleAggregatorTst {
 
     private static final String TEST7 =
     "public class Foo {" + PMD.EOL +
-    " public void foo() {" + PMD.EOL +
+    " void foo() {" + PMD.EOL +
     "  try {" + PMD.EOL +
     "  } catch (CloneNotSupportedException e) {}" + PMD.EOL +
     " }" + PMD.EOL +
