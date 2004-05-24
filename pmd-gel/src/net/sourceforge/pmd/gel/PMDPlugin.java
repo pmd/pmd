729106bf9dd995a7e74773e0c431bbcf34431ff3
Andrey Lumyanski <a_lumyanski@users.sourceforge.net>
2004-05-24 19:52:52 +0000

    no message
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@2708 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd-gel/src/net/sourceforge/pmd/gel/PMDPlugin.java
+++ pmd-gel/src/net/sourceforge/pmd/gel/PMDPlugin.java
@@ -1,95 +1,259 @@
+/*
+ * @(#)PMDPlugin.java $Revision$ ($Date$)
+ * Copyright (c) 2004
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
 package net.sourceforge.pmd.gel;
 
-import java.awt.Color;
-import java.awt.BorderLayout;
-import java.util.List;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FilenameFilter;
+import java.io.Reader;
+import java.io.StringReader;
+
+import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.Vector;
-import java.io.*;
-import javax.swing.*;
-import com.gexperts.gel.*;
-import net.sourceforge.pmd.*;
-import net.sourceforge.pmd.cpd.*;
+import java.util.List;
 
-public class PMDPlugin implements GelAction {
+import javax.swing.ProgressMonitor;
 
-    // GelAction
-    public boolean isActive(Gel p0) {
-           return true;
-    }
+import com.gexperts.gel.Gel;
+import com.gexperts.gel.GelAction;
 
-    public void perform(Gel p0) {
-        try {
-            PMD pmd = new PMD();
-            RuleContext ctx = new RuleContext();
-            RuleSetFactory rsf = new RuleSetFactory();
-            RuleSet ruleSet = new RuleSet();
-            ruleSet.addRuleSet(rsf.createRuleSet("rulesets/unusedcode.xml,rulesets/basic.xml"));
-            ctx.setReport(new Report());
-            if (p0.getProject() == null) {
-                String code = p0.getEditor().getContents();
-                String name = p0.getEditor().getFileName();
-                if (name == null) {
-                   name = "Unnamed.java";
-                }
-                ctx.setSourceCodeFilename(name);
-                Reader reader = new StringReader(code);
-                pmd.processFile(reader, ruleSet, ctx);
-            } else {
-                for (Iterator iter = p0.getProject().getSourcePaths().iterator(); iter.hasNext();) {
-                    String srcDir = (String)iter.next();
-                    FileFinder ff = new FileFinder();
-                    List files = ff.findFilesFrom(srcDir, new JavaLanguage.JavaFileOrDirectoryFilter(), true);
-                    for (Iterator fileIter = files.iterator(); fileIter.hasNext();) {
-                        File fileName = (File)fileIter.next();
-                        ctx.setSourceCodeFilename(fileName.getAbsolutePath());
-                        Reader reader = new FileReader(fileName);
-                        pmd.processFile(reader, ruleSet, ctx);
-                    }
-               }
-           }
-           if (ctx.getReport().isEmpty()) {
-             JOptionPane.showMessageDialog(null, "No problems found", "PMD", JOptionPane.INFORMATION_MESSAGE);
-           } else {
-             createProblemFrame(ctx.getReport());
-           }
-        } catch (Exception e) {
-             JOptionPane.showMessageDialog(null, "ERROR " + e.getClass().getName() + ":" + e.getMessage());
-            e.printStackTrace();
-        }
-    }
+import net.sourceforge.pmd.PMD;
+import net.sourceforge.pmd.Report;
+import net.sourceforge.pmd.ReportListener;
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.RuleContext;
+import net.sourceforge.pmd.RuleSet;
+import net.sourceforge.pmd.RuleSetFactory;
+import net.sourceforge.pmd.RuleViolation;
+import net.sourceforge.pmd.cpd.FileFinder;
+import net.sourceforge.pmd.cpd.JavaLanguage;
+import net.sourceforge.pmd.stat.Metric;
 
-    public String getName() {
-           return "PMD";
-    }
-    // GelAction
 
-    private JFrame createProblemFrame(Report report) {
-            JFrame newFrame = new JFrame();
-            JDialog dialog = new JDialog(newFrame, report.size() +  " problems found");
-            dialog.setContentPane(createProblemListPanel(report));
-            dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-            dialog.pack();
-            dialog.setLocationRelativeTo(newFrame);
-            dialog.setVisible(true);
-            return newFrame;
-    }
+/**
+ * PMD plugin for Gel.
+ *
+ * @author Andrey Lumyanski
+ * @version $Revision$ ($Date$)
+ */
+public class PMDPlugin implements GelAction, Runnable, ReportListener {
+	private Gel gel;
+	private Report report;
+	private ArrayList listOfFiles;
+	private ProgressMonitor progressMonitor;
+	private RuleSet arrayRuleSet[];
 
-    private JPanel createProblemListPanel(Report report) {
-            Vector v = new Vector();
-             for (Iterator i = report.iterator(); i.hasNext();) {
-                 RuleViolation rv = (RuleViolation)i.next();
-                 String msg = rv.getFilename() + " - line " + (rv.getLine()-1) + " - " + rv.getDescription();
-                 v.add(msg);
+	/**
+	 * Creates a <code>PMDPlugin</code> object.
+	 */
+	public PMDPlugin() {
+		super();
+		gel = null;
+		report = null;
+		listOfFiles = null;
+		progressMonitor = null;
+		arrayRuleSet = null;
+	}
 
-              }
-            JList list = new JList(v);
-            list.setForeground(Color.red);
-            list.setBackground(Color.white);
-            list.setVisibleRowCount(20);
+	/**
+	 * Returns <code>true</code> if the plugin is active.
+	 *
+	 * @param gel a <code>Gel</code> instance.
+	 *
+	 * @return <code>true</code> if the plugin is active.
+	 */
+	public boolean isActive(Gel gel) {
+		return gel.getEditor() != null;
+	}
 
-            JPanel container = new JPanel(new BorderLayout());
-            container.add(list, BorderLayout.CENTER);
-            return container;
-    }
+	/**
+	 * Performs action.
+	 *
+	 * @param gel Gel a <code>Gel</code> instance.
+	 */
+	public void perform(Gel gel) {
+		this.gel = gel;
+		report = new Report();
+		report.addListener(this);
+		gel.clearMessages();
+		gel.addMessage("PMD started");
+
+		try {
+			RuleSetFactory rsf = new RuleSetFactory();
+			Iterator it = rsf.getRegisteredRuleSets();
+			ArrayList listOfRuleSet = new ArrayList();
+			while (it.hasNext()) {
+				listOfRuleSet.add((RuleSet)it.next());
+			}
+			arrayRuleSet = new RuleSet[listOfRuleSet.size()];
+			for (int i = 0; i < arrayRuleSet.length; ++i) {
+				arrayRuleSet[i] = (RuleSet) listOfRuleSet.get(i);
+			}
+
+			RuleSetDialog dlgRuleSet = new RuleSetDialog(arrayRuleSet);
+			dlgRuleSet.show();
+			if (dlgRuleSet.isCanceled()) {
+				gel.addMessage("PMD canceled.");
+				return;
+			}
+
+			if (gel.getProject() != null) {
+				listOfFiles = new ArrayList();
+
+				it = gel.getProject().getSourcePaths().iterator();
+				FileFinder ff = new FileFinder();
+				FilenameFilter filter =
+					new JavaLanguage.JavaFileOrDirectoryFilter();
+
+				while (it.hasNext()) {
+					String srcDir = (String) it.next();
+					List files = ff.findFilesFrom(srcDir, filter, true);
+
+					if (files != null) {
+						listOfFiles.addAll(files);
+					}
+				}
+			} else {
+				String name = gel.getEditor().getFileName();
+
+				if (name == null) {
+					name = "Untitled";
+				}
+
+				listOfFiles.add(new File(name));
+			}
+
+			if (listOfFiles != null) {
+				progressMonitor =
+					new ProgressMonitor(null, "PMD", "", 0, listOfFiles.size());
+			}
+		} catch (Exception e) {
+			StringBuffer msg = new StringBuffer();
+			msg.append("PMD error:");
+			msg.append(e.getClass().getName());
+			msg.append(":");
+			msg.append(e.getLocalizedMessage());
+			gel.showMessage(msg.toString());
+		}
+
+		Thread threadPMD = new Thread(this);
+		threadPMD.start();
+	}
+
+	/**
+	 * Returns a plugin's name.
+	 *
+	 * @return a plugin's name.
+	 */
+	public String getName() {
+		return "PMD";
+	}
+
+	/**
+	 * A <code>Runnable</code> interface implementation.
+	 */
+	public void run() {
+		try {
+			PMD pmd = new PMD();
+			RuleContext ctx = new RuleContext();
+			RuleSet ruleSet = new RuleSet();
+			Iterator it;
+			Rule rule;
+			for (int i = 0; i < arrayRuleSet.length; ++i) {
+				if (arrayRuleSet[i].include()) {
+					it = arrayRuleSet[i].getRules().iterator();
+					while (it.hasNext()) {
+						rule = (Rule) it.next();
+						if (rule.include()) {
+							ruleSet.addRule(rule);
+						}
+					}
+				}
+			}
+			if (ruleSet.size() > 0) {
+				ctx.setReport(report);
+
+				if (gel.getProject() == null) {
+					String code = gel.getEditor().getContents();
+					String name = gel.getEditor().getFileName();
+
+					if (name == null) {
+						name = "Untitled";
+					}
+
+					ctx.setSourceCodeFilename(name);
+					progressMonitor.setNote(name);
+					Reader reader = new StringReader(code);
+					pmd.processFile(reader, ruleSet, ctx);
+					progressMonitor.setProgress(1);
+				} else {
+					it = listOfFiles.iterator();
+					int index = 0;
+
+					while (it.hasNext()) {
+						File file = (File) it.next();
+						ctx.setSourceCodeFilename(file.getAbsolutePath());
+						++index;
+						progressMonitor.setNote(file.getAbsolutePath());
+						Reader reader = new FileReader(file);
+						pmd.processFile(reader, ruleSet, ctx);
+						progressMonitor.setProgress(index);
+						if (progressMonitor.isCanceled()) {
+							gel.addMessage("PMD canceled.");
+							return;
+						}
+					}
+				}
+
+				if (report.isEmpty()) {
+					gel.addMessage("No problems found");
+				} else {
+					StringBuffer msg = new StringBuffer();
+					msg.append(report.size());
+					msg.append(" problems found");
+					gel.addMessage(msg.toString());
+				}
+			} else {
+				gel.addMessage("It is not chosen any rule!");
+			}
+		} catch (Exception e) {
+			StringBuffer msg = new StringBuffer();
+			msg.append("PMD error:");
+			msg.append(e.getClass().getName());
+			msg.append(":");
+			msg.append(e.getLocalizedMessage());
+			gel.showMessage(msg.toString());
+		}
+
+		gel.addMessage("PMD finished");
+	}
+
+	/**
+	 * Processes a <em>RuleViolation</em> adding.
+	 *
+	 * @param rv a <em>RuleViolation</em>
+	 */
+	public void ruleViolationAdded(RuleViolation rv) {
+		StringBuffer msg = new StringBuffer();
+		msg.append("[");
+		msg.append(rv.getRule().getName());
+		msg.append("]: ");
+		msg.append(rv.getFilename());
+		msg.append(":");
+		msg.append(rv.getLine());
+		msg.append(": ");
+		msg.append(rv.getDescription());
+		gel.addMessage(msg.toString());
+	}
+
+	/**
+	 * Processes a <em>Metric</em> adding.
+	 *
+	 * @param metric a<em>Metric</em>
+	 */
+	public void metricAdded(Metric metric) {
+	}
 }
