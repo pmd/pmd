240c4de30c23e56c5f369eb4e644572f49a8b301
Tom Copeland <tomcopeland@users.sourceforge.net>
2004-09-02 19:08:52 +0000

    Implemented new rule - SimpleDateFormatNeedsLocale
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@2905 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/rules/SimpleDateFormatNeedsLocaleRuleTest.java
@@ -0,0 +1,37 @@
+package test.net.sourceforge.pmd.rules;
+
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
+import net.sourceforge.pmd.RuleSetNotFoundException;
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.PMD;
+
+public class SimpleDateFormatNeedsLocaleRuleTest extends SimpleAggregatorTst {
+
+    private Rule rule;
+
+    public void setUp() throws RuleSetNotFoundException {
+        rule = findRule("rulesets/newrules.xml", "SimpleDateFormatNeedsLocale");
+    }
+
+    public void testAll() {
+       runTests(new TestDescriptor[] {
+           new TestDescriptor(TEST1, "ok, uses a two arg constructor", 0, rule),
+           new TestDescriptor(TEST2, "bad, using the single-arg contructor", 1, rule),
+           new TestDescriptor(TEST3, "all quiet", 0, rule),
+       });
+    }
+
+    private static final String TEST1 =
+    "class Foo {" + PMD.EOL +
+    "  private SimpleDateFormat sdf = new SimpleDateFormat(\"pattern\", Locale.US);" + PMD.EOL +
+    "}";
+
+    private static final String TEST2 =
+    "class Foo {" + PMD.EOL +
+    "  private SimpleDateFormat sdf = new SimpleDateFormat(\"pattern\");" + PMD.EOL +
+    "}";
+
+    private static final String TEST3 =
+    "class Foo {}";
+}
