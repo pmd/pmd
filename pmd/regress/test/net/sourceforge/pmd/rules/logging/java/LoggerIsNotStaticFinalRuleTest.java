75a25bf09700073cf5a59dbfc4a7eda5385653bd
Tom Copeland <tomcopeland@users.sourceforge.net>
2004-10-15 17:40:18 +0000

    Added new logging rules from Miguel
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@2982 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/rules/logging/java/LoggerIsNotStaticFinalRuleTest.java
@@ -0,0 +1,48 @@
+/**
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+*/
+package test.net.sourceforge.pmd.rules.logging.java;
+
+import net.sourceforge.pmd.PMD;
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.RuleSetNotFoundException;
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
+
+public class LoggerIsNotStaticFinalRuleTest extends SimpleAggregatorTst {
+
+    private Rule rule;
+
+    public void setUp() throws RuleSetNotFoundException {
+        rule = findRule("rulesets/logging-java.xml", "LoggerIsNotStaticFinalRule");
+    }
+
+    public void testAll() {
+       runTests(new TestDescriptor[] {
+           new TestDescriptor(TEST1, "ok", 0, rule),
+           new TestDescriptor(TEST2, "two bad loggers", 2, rule),
+           new TestDescriptor(TEST3, "ok with internal class", 0, rule),
+       });
+    }
+
+    private static final String TEST1 =
+    "public class Foo {" + PMD.EOL +
+    " static final Logger log;" + PMD.EOL +
+    "}";
+
+    private static final String TEST2 =
+    "public class Foo {" + PMD.EOL +
+    " static final Logger log;" + PMD.EOL +
+    " Logger log;" + PMD.EOL +
+    " Logger log2;" + PMD.EOL +
+    "}";
+
+    private static final String TEST3 =
+    "public class Foo {" + PMD.EOL +
+    " static final Logger log;" + PMD.EOL +
+    " class c { " + PMD.EOL +
+    " static final Logger log;" + PMD.EOL +
+    " } " + PMD.EOL +
+    "}";
+
+}
---
+++ pmd/regress/test/net/sourceforge/pmd/rules/logging/java/MoreThanOneLoggerRuleTest.java
@@ -0,0 +1,38 @@
+/**
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+*/
+package test.net.sourceforge.pmd.rules.logging.java;
+
+import net.sourceforge.pmd.PMD;
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.RuleSetNotFoundException;
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
+
+public class MoreThanOneLoggerRuleTest extends SimpleAggregatorTst {
+
+    private Rule rule;
+
+    public void setUp() throws RuleSetNotFoundException {
+        rule = findRule("rulesets/logging-java.xml", "MoreThanOneLoggerRule");
+    }
+
+    public void testAll() {
+       runTests(new TestDescriptor[] {
+           new TestDescriptor(TEST1, "ok", 0, rule),
+           new TestDescriptor(TEST2, "two loggers", 1, rule),
+       });
+    }
+
+    private static final String TEST1 =
+    "public class Foo {" + PMD.EOL +
+    " Logger log;" + PMD.EOL +
+    "}";
+
+    private static final String TEST2 =
+    "public class Foo {" + PMD.EOL +
+    " Logger log;" + PMD.EOL +
+    " Logger log2;" + PMD.EOL +
+    "}";
+
+}
