6e5697f2bf011ac1dbd24f236d246ad2d4181231
Tom Copeland <tomcopeland@users.sourceforge.net>
2004-10-19 14:41:02 +0000

    Rls preps for 2.0
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@2989 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/net/sourceforge/pmd/PMD.java
+++ pmd/src/net/sourceforge/pmd/PMD.java
@@ -31,6 +31,7 @@ import java.util.zip.ZipFile;
 public class PMD {
 
     public static final String EOL = System.getProperty("line.separator", "\n");
+    public static final String VERSION = "1.9";
 
     private TargetJDKVersion targetJDKVersion;
     private String excludeMarker = ExcludeLines.EXCLUDE_MARKER;
