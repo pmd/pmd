2dccbe6d95db71a4ebefcfecd3b2ce2d41ef03ef
Tom Copeland <tomcopeland@users.sourceforge.net>
2004-10-19 18:50:49 +0000

    Fixed bug 1041739 - EmptyStatementNotInLoop no longer reports false positives for nested class declarations in methods.
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@2995 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/regress/test/net/sourceforge/pmd/rules/design/EmptyStatementNotInLoopRuleTest.java
+++ pmd/regress/test/net/sourceforge/pmd/rules/design/EmptyStatementNotInLoopRuleTest.java
@@ -20,6 +20,7 @@ public class EmptyStatementNotInLoopRuleTest extends SimpleAggregatorTst {
            new TestDescriptor(TEST2, "ok, semicolon after while", 0, rule),
            new TestDescriptor(TEST3, "bad, random semicolon", 1, rule),
            new TestDescriptor(TEST4, "bad, double semicolon", 1, rule),
+           new TestDescriptor(TEST5, "ok, nested class", 0, rule),
        });
     }
 
@@ -48,6 +49,13 @@ public class EmptyStatementNotInLoopRuleTest extends SimpleAggregatorTst {
     "public class Foo {" + PMD.EOL +
     " void bar() {" + PMD.EOL +
     "  int x = 2;;" + PMD.EOL +
+    " }" + PMD.EOL +
+    "}";
+
+    private static final String TEST5 =
+    "public class Foo {" + PMD.EOL +
+    " void bar() {" + PMD.EOL +
+    "  class Y {};" + PMD.EOL +
     " }" + PMD.EOL +
     "}";
 }
