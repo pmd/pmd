23e69d576250f417c265d779703b8da08a67aaed
Tom Copeland <tomcopeland@users.sourceforge.net>
2004-10-20 20:17:07 +0000

    Fixed bug 1039963 - CPD no longer fails to parse C++ files with multi-line macros.
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@2997 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/cpd/CPPTokenizerTest.java
@@ -0,0 +1,33 @@
+package test.net.sourceforge.pmd.cpd;
+
+import junit.framework.TestCase;
+import net.sourceforge.pmd.PMD;
+import net.sourceforge.pmd.cpd.CPPTokenizer;
+import net.sourceforge.pmd.cpd.SourceCode;
+import net.sourceforge.pmd.cpd.Tokens;
+
+import java.io.StringReader;
+
+public class CPPTokenizerTest extends TestCase{
+
+    public void testMultiLineMacros() throws Throwable {
+        CPPTokenizer tokenizer = new CPPTokenizer();
+        SourceCode code = new SourceCode("");
+        code.readSource(new StringReader(TEST1));
+        Tokens tokens = new Tokens();
+        tokenizer.tokenize(code, tokens);
+        assertEquals(7, tokens.size());
+    }
+
+    private static final String TEST1 =
+    "#define FOO a +\\" + PMD.EOL +
+    "            b +\\" + PMD.EOL +
+    "            c +\\" + PMD.EOL +
+    "            d +\\" + PMD.EOL +
+    "            e +\\" + PMD.EOL +
+    "            f +\\" + PMD.EOL +
+    "            g"  + PMD.EOL +
+    " void main() {}";
+
+
+}
--- pmd/src/net/sourceforge/pmd/cpd/cppast/CPPParserConstants.java
+++ pmd/src/net/sourceforge/pmd/cpd/cppast/CPPParserConstants.java
@@ -3,261 +3,262 @@ package net.sourceforge.pmd.cpd.cppast;
 
 public interface CPPParserConstants {
 
-    int EOF = 0;
-    int LCURLYBRACE = 14;
-    int RCURLYBRACE = 15;
-    int LSQUAREBRACKET = 16;
-    int RSQUAREBRACKET = 17;
-    int LPARENTHESIS = 18;
-    int RPARENTHESIS = 19;
-    int SCOPE = 20;
-    int COLON = 21;
-    int SEMICOLON = 22;
-    int COMMA = 23;
-    int QUESTIONMARK = 24;
-    int ELLIPSIS = 25;
-    int ASSIGNEQUAL = 26;
-    int TIMESEQUAL = 27;
-    int DIVIDEEQUAL = 28;
-    int MODEQUAL = 29;
-    int PLUSEQUAL = 30;
-    int MINUSEQUAL = 31;
-    int SHIFTLEFTEQUAL = 32;
-    int SHIFTRIGHTEQUAL = 33;
-    int BITWISEANDEQUAL = 34;
-    int BITWISEXOREQUAL = 35;
-    int BITWISEOREQUAL = 36;
-    int OR = 37;
-    int AND = 38;
-    int BITWISEOR = 39;
-    int BITWISEXOR = 40;
-    int AMPERSAND = 41;
-    int EQUAL = 42;
-    int NOTEQUAL = 43;
-    int LESSTHAN = 44;
-    int GREATERTHAN = 45;
-    int LESSTHANOREQUALTO = 46;
-    int GREATERTHANOREQUALTO = 47;
-    int SHIFTLEFT = 48;
-    int SHIFTRIGHT = 49;
-    int PLUS = 50;
-    int MINUS = 51;
-    int STAR = 52;
-    int DIVIDE = 53;
-    int MOD = 54;
-    int PLUSPLUS = 55;
-    int MINUSMINUS = 56;
-    int TILDE = 57;
-    int NOT = 58;
-    int DOT = 59;
-    int POINTERTO = 60;
-    int DOTSTAR = 61;
-    int ARROWSTAR = 62;
-    int AUTO = 63;
-    int BREAK = 64;
-    int CASE = 65;
-    int CATCH = 66;
-    int CHAR = 67;
-    int CONST = 68;
-    int CONTINUE = 69;
-    int _DEFAULT = 70;
-    int DELETE = 71;
-    int DO = 72;
-    int DOUBLE = 73;
-    int ELSE = 74;
-    int ENUM = 75;
-    int EXTERN = 76;
-    int FLOAT = 77;
-    int FOR = 78;
-    int FRIEND = 79;
-    int GOTO = 80;
-    int IF = 81;
-    int INLINE = 82;
-    int INT = 83;
-    int LONG = 84;
-    int NEW = 85;
-    int PRIVATE = 86;
-    int PROTECTED = 87;
-    int PUBLIC = 88;
-    int REDECLARED = 89;
-    int REGISTER = 90;
-    int RETURN = 91;
-    int SHORT = 92;
-    int SIGNED = 93;
-    int SIZEOF = 94;
-    int STATIC = 95;
-    int STRUCT = 96;
-    int CLASS = 97;
-    int SWITCH = 98;
-    int TEMPLATE = 99;
-    int THIS = 100;
-    int TRY = 101;
-    int TYPEDEF = 102;
-    int UNION = 103;
-    int UNSIGNED = 104;
-    int VIRTUAL = 105;
-    int VOID = 106;
-    int VOLATILE = 107;
-    int WHILE = 108;
-    int OPERATOR = 109;
-    int TRUETOK = 110;
-    int FALSETOK = 111;
-    int THROW = 112;
-    int OCTALINT = 113;
-    int OCTALLONG = 114;
-    int UNSIGNED_OCTALINT = 115;
-    int UNSIGNED_OCTALLONG = 116;
-    int DECIMALINT = 117;
-    int DECIMALLONG = 118;
-    int UNSIGNED_DECIMALINT = 119;
-    int UNSIGNED_DECIMALLONG = 120;
-    int HEXADECIMALINT = 121;
-    int HEXADECIMALLONG = 122;
-    int UNSIGNED_HEXADECIMALINT = 123;
-    int UNSIGNED_HEXADECIMALLONG = 124;
-    int FLOATONE = 125;
-    int FLOATTWO = 126;
-    int CHARACTER = 127;
-    int STRING = 128;
-    int ID = 130;
+  int EOF = 0;
+  int LCURLYBRACE = 15;
+  int RCURLYBRACE = 16;
+  int LSQUAREBRACKET = 17;
+  int RSQUAREBRACKET = 18;
+  int LPARENTHESIS = 19;
+  int RPARENTHESIS = 20;
+  int SCOPE = 21;
+  int COLON = 22;
+  int SEMICOLON = 23;
+  int COMMA = 24;
+  int QUESTIONMARK = 25;
+  int ELLIPSIS = 26;
+  int ASSIGNEQUAL = 27;
+  int TIMESEQUAL = 28;
+  int DIVIDEEQUAL = 29;
+  int MODEQUAL = 30;
+  int PLUSEQUAL = 31;
+  int MINUSEQUAL = 32;
+  int SHIFTLEFTEQUAL = 33;
+  int SHIFTRIGHTEQUAL = 34;
+  int BITWISEANDEQUAL = 35;
+  int BITWISEXOREQUAL = 36;
+  int BITWISEOREQUAL = 37;
+  int OR = 38;
+  int AND = 39;
+  int BITWISEOR = 40;
+  int BITWISEXOR = 41;
+  int AMPERSAND = 42;
+  int EQUAL = 43;
+  int NOTEQUAL = 44;
+  int LESSTHAN = 45;
+  int GREATERTHAN = 46;
+  int LESSTHANOREQUALTO = 47;
+  int GREATERTHANOREQUALTO = 48;
+  int SHIFTLEFT = 49;
+  int SHIFTRIGHT = 50;
+  int PLUS = 51;
+  int MINUS = 52;
+  int STAR = 53;
+  int DIVIDE = 54;
+  int MOD = 55;
+  int PLUSPLUS = 56;
+  int MINUSMINUS = 57;
+  int TILDE = 58;
+  int NOT = 59;
+  int DOT = 60;
+  int POINTERTO = 61;
+  int DOTSTAR = 62;
+  int ARROWSTAR = 63;
+  int AUTO = 64;
+  int BREAK = 65;
+  int CASE = 66;
+  int CATCH = 67;
+  int CHAR = 68;
+  int CONST = 69;
+  int CONTINUE = 70;
+  int _DEFAULT = 71;
+  int DELETE = 72;
+  int DO = 73;
+  int DOUBLE = 74;
+  int ELSE = 75;
+  int ENUM = 76;
+  int EXTERN = 77;
+  int FLOAT = 78;
+  int FOR = 79;
+  int FRIEND = 80;
+  int GOTO = 81;
+  int IF = 82;
+  int INLINE = 83;
+  int INT = 84;
+  int LONG = 85;
+  int NEW = 86;
+  int PRIVATE = 87;
+  int PROTECTED = 88;
+  int PUBLIC = 89;
+  int REDECLARED = 90;
+  int REGISTER = 91;
+  int RETURN = 92;
+  int SHORT = 93;
+  int SIGNED = 94;
+  int SIZEOF = 95;
+  int STATIC = 96;
+  int STRUCT = 97;
+  int CLASS = 98;
+  int SWITCH = 99;
+  int TEMPLATE = 100;
+  int THIS = 101;
+  int TRY = 102;
+  int TYPEDEF = 103;
+  int UNION = 104;
+  int UNSIGNED = 105;
+  int VIRTUAL = 106;
+  int VOID = 107;
+  int VOLATILE = 108;
+  int WHILE = 109;
+  int OPERATOR = 110;
+  int TRUETOK = 111;
+  int FALSETOK = 112;
+  int THROW = 113;
+  int OCTALINT = 114;
+  int OCTALLONG = 115;
+  int UNSIGNED_OCTALINT = 116;
+  int UNSIGNED_OCTALLONG = 117;
+  int DECIMALINT = 118;
+  int DECIMALLONG = 119;
+  int UNSIGNED_DECIMALINT = 120;
+  int UNSIGNED_DECIMALLONG = 121;
+  int HEXADECIMALINT = 122;
+  int HEXADECIMALLONG = 123;
+  int UNSIGNED_HEXADECIMALINT = 124;
+  int UNSIGNED_HEXADECIMALLONG = 125;
+  int FLOATONE = 126;
+  int FLOATTWO = 127;
+  int CHARACTER = 128;
+  int STRING = 129;
+  int ID = 131;
 
-    int DEFAULT = 0;
-    int IN_LINE_COMMENT = 1;
-    int IN_COMMENT = 2;
-    int PREPROCESSOR_OUTPUT = 3;
+  int DEFAULT = 0;
+  int IN_LINE_COMMENT = 1;
+  int IN_COMMENT = 2;
+  int PREPROCESSOR_OUTPUT = 3;
 
-    String[] tokenImage = {
-        "<EOF>",
-        "\" \"",
-        "\"\\t\"",
-        "\"\\r\\n\"",
-        "\"\\n\"",
-        "\"//\"",
-        "\"/*\"",
-        "\"#\"",
-        "\"\\n\"",
-        "<token of kind 9>",
-        "\"*/\"",
-        "<token of kind 11>",
-        "\"\\n\"",
-        "<token of kind 13>",
-        "\"{\"",
-        "\"}\"",
-        "\"[\"",
-        "\"]\"",
-        "\"(\"",
-        "\")\"",
-        "\"::\"",
-        "\":\"",
-        "\";\"",
-        "\",\"",
-        "\"?\"",
-        "\"...\"",
-        "\"=\"",
-        "\"*=\"",
-        "\"/=\"",
-        "\"%=\"",
-        "\"+=\"",
-        "\"-=\"",
-        "\"<<=\"",
-        "\">>=\"",
-        "\"&=\"",
-        "\"^=\"",
-        "\"|=\"",
-        "\"||\"",
-        "\"&&\"",
-        "\"|\"",
-        "\"^\"",
-        "\"&\"",
-        "\"==\"",
-        "\"!=\"",
-        "\"<\"",
-        "\">\"",
-        "\"<=\"",
-        "\">=\"",
-        "\"<<\"",
-        "\">>\"",
-        "\"+\"",
-        "\"-\"",
-        "\"*\"",
-        "\"/\"",
-        "\"%\"",
-        "\"++\"",
-        "\"--\"",
-        "\"~\"",
-        "\"!\"",
-        "\".\"",
-        "\"->\"",
-        "\".*\"",
-        "\"->*\"",
-        "\"auto\"",
-        "\"break\"",
-        "\"case\"",
-        "\"catch\"",
-        "\"char\"",
-        "\"const\"",
-        "\"continue\"",
-        "\"default\"",
-        "\"delete\"",
-        "\"do\"",
-        "\"double\"",
-        "\"else\"",
-        "\"enum\"",
-        "\"extern\"",
-        "\"float\"",
-        "\"for\"",
-        "\"friend\"",
-        "\"goto\"",
-        "\"if\"",
-        "\"inline\"",
-        "\"int\"",
-        "\"long\"",
-        "\"new\"",
-        "\"private\"",
-        "\"protected\"",
-        "\"public\"",
-        "\"redeclared\"",
-        "\"register\"",
-        "\"return\"",
-        "\"short\"",
-        "\"signed\"",
-        "\"sizeof\"",
-        "\"static\"",
-        "\"struct\"",
-        "\"class\"",
-        "\"switch\"",
-        "\"template\"",
-        "\"this\"",
-        "\"try\"",
-        "\"typedef\"",
-        "\"union\"",
-        "\"unsigned\"",
-        "\"virtual\"",
-        "\"void\"",
-        "\"volatile\"",
-        "\"while\"",
-        "\"operator\"",
-        "\"true\"",
-        "\"false\"",
-        "\"throw\"",
-        "<OCTALINT>",
-        "<OCTALLONG>",
-        "<UNSIGNED_OCTALINT>",
-        "<UNSIGNED_OCTALLONG>",
-        "<DECIMALINT>",
-        "<DECIMALLONG>",
-        "<UNSIGNED_DECIMALINT>",
-        "<UNSIGNED_DECIMALLONG>",
-        "<HEXADECIMALINT>",
-        "<HEXADECIMALLONG>",
-        "<UNSIGNED_HEXADECIMALINT>",
-        "<UNSIGNED_HEXADECIMALLONG>",
-        "<FLOATONE>",
-        "<FLOATTWO>",
-        "<CHARACTER>",
-        "<STRING>",
-        "\"finally\"",
-        "<ID>",
-    };
+  String[] tokenImage = {
+    "<EOF>",
+    "\" \"",
+    "\"\\t\"",
+    "\"\\r\\n\"",
+    "\"\\n\"",
+    "\"//\"",
+    "\"/*\"",
+    "\"#\"",
+    "\"\\n\"",
+    "<token of kind 9>",
+    "\"*/\"",
+    "<token of kind 11>",
+    "\"\\n\"",
+    "\"\\\\\\n\"",
+    "<token of kind 14>",
+    "\"{\"",
+    "\"}\"",
+    "\"[\"",
+    "\"]\"",
+    "\"(\"",
+    "\")\"",
+    "\"::\"",
+    "\":\"",
+    "\";\"",
+    "\",\"",
+    "\"?\"",
+    "\"...\"",
+    "\"=\"",
+    "\"*=\"",
+    "\"/=\"",
+    "\"%=\"",
+    "\"+=\"",
+    "\"-=\"",
+    "\"<<=\"",
+    "\">>=\"",
+    "\"&=\"",
+    "\"^=\"",
+    "\"|=\"",
+    "\"||\"",
+    "\"&&\"",
+    "\"|\"",
+    "\"^\"",
+    "\"&\"",
+    "\"==\"",
+    "\"!=\"",
+    "\"<\"",
+    "\">\"",
+    "\"<=\"",
+    "\">=\"",
+    "\"<<\"",
+    "\">>\"",
+    "\"+\"",
+    "\"-\"",
+    "\"*\"",
+    "\"/\"",
+    "\"%\"",
+    "\"++\"",
+    "\"--\"",
+    "\"~\"",
+    "\"!\"",
+    "\".\"",
+    "\"->\"",
+    "\".*\"",
+    "\"->*\"",
+    "\"auto\"",
+    "\"break\"",
+    "\"case\"",
+    "\"catch\"",
+    "\"char\"",
+    "\"const\"",
+    "\"continue\"",
+    "\"default\"",
+    "\"delete\"",
+    "\"do\"",
+    "\"double\"",
+    "\"else\"",
+    "\"enum\"",
+    "\"extern\"",
+    "\"float\"",
+    "\"for\"",
+    "\"friend\"",
+    "\"goto\"",
+    "\"if\"",
+    "\"inline\"",
+    "\"int\"",
+    "\"long\"",
+    "\"new\"",
+    "\"private\"",
+    "\"protected\"",
+    "\"public\"",
+    "\"redeclared\"",
+    "\"register\"",
+    "\"return\"",
+    "\"short\"",
+    "\"signed\"",
+    "\"sizeof\"",
+    "\"static\"",
+    "\"struct\"",
+    "\"class\"",
+    "\"switch\"",
+    "\"template\"",
+    "\"this\"",
+    "\"try\"",
+    "\"typedef\"",
+    "\"union\"",
+    "\"unsigned\"",
+    "\"virtual\"",
+    "\"void\"",
+    "\"volatile\"",
+    "\"while\"",
+    "\"operator\"",
+    "\"true\"",
+    "\"false\"",
+    "\"throw\"",
+    "<OCTALINT>",
+    "<OCTALLONG>",
+    "<UNSIGNED_OCTALINT>",
+    "<UNSIGNED_OCTALLONG>",
+    "<DECIMALINT>",
+    "<DECIMALLONG>",
+    "<UNSIGNED_DECIMALINT>",
+    "<UNSIGNED_DECIMALLONG>",
+    "<HEXADECIMALINT>",
+    "<HEXADECIMALLONG>",
+    "<UNSIGNED_HEXADECIMALINT>",
+    "<UNSIGNED_HEXADECIMALLONG>",
+    "<FLOATONE>",
+    "<FLOATTWO>",
+    "<CHARACTER>",
+    "<STRING>",
+    "\"finally\"",
+    "<ID>",
+  };
 
 }
