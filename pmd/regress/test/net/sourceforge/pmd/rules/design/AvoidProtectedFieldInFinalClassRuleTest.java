2afa452cee45f3cdad5202cce8ab4def76288e8a
Tom Copeland <tomcopeland@users.sourceforge.net>
2004-10-26 21:10:07 +0000

    A new rule from Eric Olander - AvoidProtectedFieldInFinalClass
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@3013 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/rules/design/AvoidProtectedFieldInFinalClassRuleTest.java
@@ -0,0 +1,41 @@
+package test.net.sourceforge.pmd.rules.design;
+
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
+import net.sourceforge.pmd.rules.design.ConfusingTernary;
+import net.sourceforge.pmd.PMD;
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.RuleSetNotFoundException;
+
+public class AvoidProtectedFieldInFinalClassRuleTest extends SimpleAggregatorTst {
+
+    private Rule rule;
+
+    public void setUp() throws RuleSetNotFoundException {
+        rule = findRule("rulesets/newrules.xml", "AvoidProtectedFieldInFinalClass");
+    }
+
+    public void testAll() {
+       runTests(new TestDescriptor[] {
+           new TestDescriptor(TEST1, "ok, protected field in non final class", 0, rule),
+           new TestDescriptor(TEST2, "bad, protected field in final class", 1, rule),
+           new TestDescriptor(TEST3, "ok, private field in final class", 0, rule),
+       });
+    }
+
+    private static final String TEST1 =
+    "public class Foo {" + PMD.EOL +
+    " protected int x;" + PMD.EOL +
+    "}";
+
+    private static final String TEST2 =
+    "public final class Foo {" + PMD.EOL +
+    " protected int x;" + PMD.EOL +
+    "}";
+
+    private static final String TEST3 =
+    "public final class Foo {" + PMD.EOL +
+    " private int x;" + PMD.EOL +
+    "}";
+
+}
