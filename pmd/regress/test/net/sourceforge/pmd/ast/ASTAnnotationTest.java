ba3f2a855507a5faf2ff652161ed48b34eafc513
Tom Copeland <tomcopeland@users.sourceforge.net>
2004-11-03 17:03:29 +0000

    Made some stabs at making annotations worked, but backed them out because they were taking too long to get right.  I'll leave the unit test in here for future reference, though
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@3029 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/ast/ASTAnnotationTest.java
@@ -0,0 +1,38 @@
+package test.net.sourceforge.pmd.ast;
+
+import net.sourceforge.pmd.PMD;
+import test.net.sourceforge.pmd.testframework.ParserTst;
+
+public class ASTAnnotationTest extends ParserTst {
+
+    public void testAnnotationFailsWithJDK14() throws Throwable {
+/*
+        try {
+            getNodes(ASTAnnotation.class, TEST1);
+            fail("Should have failed to parse an annotation in JDK 1.4 mode");
+        } catch (ParseException pe) {
+            // cool
+        }
+*/
+    }
+
+    public void testAnnotationSucceedsWithJDK15() throws Throwable {
+/*
+        try {
+            getNodes(new TargetJDK1_5(), ASTAnnotation.class, TEST1);
+        } catch (ParseException pe) {
+            pe.printStackTrace();
+            fail("Should have been able to parse an annotation in JDK 1.5 mode");
+        }
+*/
+    }
+
+    private static final String TEST1 =
+    "public class Foo extends Buz {"  + PMD.EOL +
+    " @Override"  + PMD.EOL +
+    " void bar() {"  + PMD.EOL +
+    "  // overrides a superclass method"  + PMD.EOL +
+    " }"  + PMD.EOL +
+    "}";
+
+}
