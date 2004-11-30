fb0cd0b820720069d64d7ec434a01934e942f0b4
Tom Copeland <tomcopeland@users.sourceforge.net>
2004-11-30 16:28:40 +0000

    Moved BooleanInstantiation from the design ruleset to the basic ruleset.
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@3054 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/regress/test/net/sourceforge/pmd/rules/BooleanInstantiationRuleTest.java
+++ pmd/regress/test/net/sourceforge/pmd/rules/BooleanInstantiationRuleTest.java
@@ -14,7 +14,7 @@ public class BooleanInstantiationRuleTest extends SimpleAggregatorTst {
     private Rule rule;
 
     public void setUp() throws RuleSetNotFoundException {
-        rule = findRule("rulesets/design.xml", "BooleanInstantiation");
+        rule = findRule("rulesets/basic.xml", "BooleanInstantiation");
     }
 
     public void testAll() {
