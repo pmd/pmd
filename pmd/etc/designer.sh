cbd60d74f9c2c66a3ce4f6a8ec71c6194c38d2e8
Tom Copeland <tomcopeland@users.sourceforge.net>
2004-12-13 14:12:43 +0000

    Improved startup script, thx to elharo for the suggestion
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@3067 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/etc/designer.sh
+++ pmd/etc/designer.sh
@@ -1,1 +1,34 @@
-java -cp ../build:../lib/pmd-2.0.jar:../lib/xercesImpl-2.6.2.jar:../lib/xmlParserAPIs-2.6.2.jar:../lib/jaxen-core-1.0-fcs.jar:../lib/saxpath-1.0-fcs.jar net.sourceforge.pmd.util.designer.Designer
+#!/bin/bash
+
+SCRIPT_DIR=`dirname $0`
+CWD="$PWD"
+
+cd "$SCRIPT_DIR/../lib"
+LIB_DIR=`pwd -P`
+
+classpath=$CLASSPATH
+
+build_dir="$SCRIPT_DIR/../build"
+
+if [ -d "$build_dir" ]; then
+    cd "$build_dir"
+    build_dir=`pwd -P`
+    classpath=$classpath:$build_dir
+fi
+
+cd "$CWD"
+
+for jarfile in `ls $LIB_DIR/*.jar`; do
+    classpath=$classpath:$jarfile
+done
+
+
+FILE=$1
+shift
+FORMAT=$1
+shift
+RULESETFILES="$@"
+
+# echo "CLASSPATH: $classpath"
+
+java -cp $classpath net.sourceforge.pmd.util.designer.Designer
