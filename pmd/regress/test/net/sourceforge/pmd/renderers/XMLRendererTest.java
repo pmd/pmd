681abe2d59c57bb63c2c2275112cdac811df19b9
Tom Copeland <tomcopeland@users.sourceforge.net>
2004-12-16 15:33:04 +0000

    Gardening
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@3088 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/regress/test/net/sourceforge/pmd/renderers/XMLRendererTest.java
+++ pmd/regress/test/net/sourceforge/pmd/renderers/XMLRendererTest.java
@@ -10,10 +10,13 @@ import net.sourceforge.pmd.RuleViolation;
 import net.sourceforge.pmd.renderers.XMLRenderer;
 import org.w3c.dom.Element;
 import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 import test.net.sourceforge.pmd.testframework.MockRule;
 
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.StringReader;
+import java.io.IOException;
 
 public class XMLRendererTest extends TestCase {
 
@@ -26,7 +29,7 @@ public class XMLRendererTest extends TestCase {
     }
 
     public void testEmptyReport() throws Throwable {
-        Element root = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new InputSource(new StringReader(new XMLRenderer().render(new Report())))).getDocumentElement();
+        Element root = parseRootElement(new Report());
         assertEquals("pmd", root.getNodeName());
         assertNull(root.getFirstChild().getNextSibling()); // only one child, it's whitespace
     }
@@ -34,7 +37,7 @@ public class XMLRendererTest extends TestCase {
     public void testErrorReport() throws Throwable {
         Report report = new Report();
         report.addError(new Report.ProcessingError("test_msg", "test_filename"));
-        Element root = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new InputSource(new StringReader(new XMLRenderer().render(report)))).getDocumentElement();
+        Element root = parseRootElement(report);
         assertEquals("test_msg", root.getFirstChild().getNextSibling().getAttributes().getNamedItem("msg").getNodeValue());
     }
 
@@ -42,7 +45,7 @@ public class XMLRendererTest extends TestCase {
         Report report = new Report();
         ctx.setSourceCodeFilename("testSingleReport");
         report.addRuleViolation(new RuleViolation(RULE1, 1, "Rule1", ctx));
-        Element root = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new InputSource(new StringReader(new XMLRenderer().render(report)))).getDocumentElement();
+        Element root = parseRootElement(report);
         assertEquals("testSingleReport", root.getFirstChild().getNextSibling().getAttributes().getNamedItem("name").getNodeValue());
         assertEquals("RULE1", root.getFirstChild().getNextSibling().getFirstChild().getNextSibling().getAttributes().getNamedItem("rule").getNodeValue());
         assertEquals("rulesetname", root.getFirstChild().getNextSibling().getFirstChild().getNextSibling().getAttributes().getNamedItem("ruleset").getNodeValue());
@@ -54,7 +57,7 @@ public class XMLRendererTest extends TestCase {
         ctx.setSourceCodeFilename("testDoubleReport");
         report.addRuleViolation(new RuleViolation(RULE1, 1, "Rule1", ctx));
         report.addRuleViolation(new RuleViolation(RULE2, 2, "Rule2", ctx));
-        Element root = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new InputSource(new StringReader(new XMLRenderer().render(report)))).getDocumentElement();
+        Element root = parseRootElement(report);
         assertEquals("RULE1", root.getFirstChild().getNextSibling().getFirstChild().getNextSibling().getAttributes().getNamedItem("rule").getNodeValue());
         assertEquals("RULE2", root.getFirstChild().getNextSibling().getFirstChild().getNextSibling().getNextSibling().getNextSibling().getAttributes().getNamedItem("rule").getNodeValue());
     }
@@ -65,7 +68,7 @@ public class XMLRendererTest extends TestCase {
         report.addRuleViolation(new RuleViolation(RULE1, 1, "Rule1", ctx));
         ctx.setSourceCodeFilename("testTwoFiles_1");
         report.addRuleViolation(new RuleViolation(RULE1, 1, "Rule1", ctx));
-        Element root = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new InputSource(new StringReader(new XMLRenderer().render(report)))).getDocumentElement();
+        Element root = parseRootElement(report);
         assertEquals("testTwoFiles_0", root.getFirstChild().getNextSibling().getAttributes().getNamedItem("name").getNodeValue());
         assertEquals("testTwoFiles_1", root.getFirstChild().getNextSibling().getNextSibling().getNextSibling().getAttributes().getNamedItem("name").getNodeValue());
     }
@@ -81,7 +84,7 @@ public class XMLRendererTest extends TestCase {
         ctx.setSourceCodeFilename("testTwoFiles_0");
         report.addRuleViolation(new RuleViolation(RULE2, 2, "Rule2", ctx));
 
-        Element root = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new InputSource(new StringReader(new XMLRenderer().render(report)))).getDocumentElement();
+        Element root = parseRootElement(report);
         assertEquals("testTwoFiles_0", root.getFirstChild().getNextSibling().getAttributes().getNamedItem("name").getNodeValue());
         assertEquals("testTwoFiles_1", root.getFirstChild().getNextSibling().getNextSibling().getNextSibling().getAttributes().getNamedItem("name").getNodeValue());
         assertEquals("RULE1", root.getFirstChild().getNextSibling().getFirstChild().getNextSibling().getAttributes().getNamedItem("rule").getNodeValue());
@@ -102,7 +105,7 @@ public class XMLRendererTest extends TestCase {
         Report report = new Report();
         ctx.setSourceCodeFilename("testEscaping: Less than: < Greater than: > Ampersand: & Quote: \" 'e' acute: \u00E9");
         report.addRuleViolation(new RuleViolation(RULE1, 1, "[RULE] Less than: < Greater than: > Ampersand: & Quote: \" 'e' acute: \u00E9", ctx));
-        Element root = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new InputSource(new StringReader(new XMLRenderer().render(report)))).getDocumentElement();
+        Element root = parseRootElement(report);
         String out = root.getFirstChild().getNextSibling().toString();
         // TODO - this works when run "manually" but not when run using Maven.  Must
         // be some hideous XML difference.  Argh.
@@ -113,4 +116,8 @@ public class XMLRendererTest extends TestCase {
         assertTrue(out.indexOf("&apos;") != -1);
 */
     }
+    private Element parseRootElement(Report rpt) throws SAXException, IOException, ParserConfigurationException {
+        return DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new InputSource(new StringReader(new XMLRenderer().render(rpt)))).getDocumentElement();
+    }
+
 }
