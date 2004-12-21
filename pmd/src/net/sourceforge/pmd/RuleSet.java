4f5fcc5ee71d3e408b8fc792b1ebe1a3add04c86
Tom Copeland <tomcopeland@users.sourceforge.net>
2004-12-21 19:53:57 +0000

    Deleted a swath of apparently unused code; thanks to Chris Riesbeck for the heads up
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@3097 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/net/sourceforge/pmd/RuleSet.java
+++ pmd/src/net/sourceforge/pmd/RuleSet.java
@@ -9,22 +9,10 @@ import java.util.List;
 import java.util.Set;
 
 public class RuleSet {
+
     private Set rules = new HashSet();
     private String name;
     private String description;
-
-    /**
-     * Indicates whether or not the rule set should be included in PMD's analysis.
-     * True to include the rule set; otherwise, false to exclude the rule set.
-     */
-    private boolean m_include;
-
-    /**
-     * The name of the file the rule set is stored in, e.g., "basic_rules.xml".  The user may
-     * change the rule set name; therefore, the rule set name cannot be used for a file name.
-     * This variable is set when the rule set is read.
-     */
-    private String m_fileName;
 
     public int size() {
         return rules.size();
@@ -100,55 +88,5 @@ public class RuleSet {
 
     public void setDescription(String description) {
         this.description = description;
-    }
-
-    /**
-     * Returns true when the rule set is included in PMD's analysis; otherwise, false when
-     * it is excluded.
-     *
-     * @return True to include during analysis.
-     */
-    public boolean include() {
-        return m_include;
-    }
-
-    /**
-     * Set to true when the rule set is included in PMD's analysis; otherwise, set to false
-     * when it is excluded.
-     *
-     * @param include True to include during analysis.
-     */
-    public void setInclude(boolean include) {
-        m_include = include;
-    }
-
-    /**
-     * Get the name of the file the rule set is to be stored in, e.g., "basic_rules.xml".
-     *
-     * @return The name of the rule set file.
-     */
-    public String getFileName() {
-        if (m_fileName == null) {
-            m_fileName = name.toLowerCase().replace(' ', '_') + ".xml";
-        }
-
-        return m_fileName;
-    }
-
-    /**
-     * Set the name of the file the rule set is to be stored in, e.g., "basic_rules.xml".
-     *
-     * @param fileName The name of the rule set file.
-     */
-    public void setFileName(String fileName) {
-        if (fileName != null) {
-            fileName = fileName.trim();
-
-            if (fileName.length() == 0) {
-                fileName = null;
-            }
-        }
-
-        m_fileName = fileName;
     }
 }
