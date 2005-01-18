f390fd5e71990fac610065962543a6b39d352836
Miguel Griffa <mikkey@users.sourceforge.net>
2005-01-18 15:42:47 +0000

    test case for patch [ 1056855 ] non-final static fields in constructors
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@3160 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/rules/design/AssignementToNonFinalStaticTest.java
@@ -0,0 +1,40 @@
+package test.net.sourceforge.pmd.rules.design;
+
+import net.sourceforge.pmd.PMD;
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.RuleSetNotFoundException;
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
+
+public class AssignementToNonFinalStaticTest extends SimpleAggregatorTst {
+
+    private Rule rule;
+
+    public void setUp() throws RuleSetNotFoundException {
+        rule = findRule("rulesets/design.xml", "AssignmentToNonFinalStatic");
+    }
+
+    public void testAll() {
+       runTests(new TestDescriptor[] {
+               new TestDescriptor(TEST1, "clear rule violation", 1, rule),
+               new TestDescriptor(TEST2, "ok", 0, rule),
+       });
+    }
+
+    private static final String TEST1 =
+    "public class Foo {" + PMD.EOL +
+    " static int x;" + PMD.EOL +
+    "     Foo(int y) {" +
+    "     x = y; " + PMD.EOL +
+    "     }" + PMD.EOL +
+    "}";
+
+    private static final String TEST2 =
+        "public class Foo {" + PMD.EOL +
+        " static final int x;" + PMD.EOL +
+        "     Foo(int y) {" +
+        "     x = y; " + PMD.EOL +
+        "     }" + PMD.EOL +
+        "}";
+
+}
