07e5212fca8b49ce1c30250f1c068a218a925342
Tom Copeland <tomcopeland@users.sourceforge.net>
2005-02-01 22:04:41 +0000

    rls preps
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@3212 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/bin/designer.bat
+++ pmd/bin/designer.bat
@@ -1,1 +1,1 @@
-java -cp ../build;../lib/pmd-2.2.jar;../lib/xercesImpl-2.6.2.jar;../lib/xmlParserAPIs-2.6.2.jar;../lib/jaxen-core-1.0-fcs.jar;../lib/saxpath-1.0-fcs.jar net.sourceforge.pmd.util.designer.Designer
+java -cp ../build;../lib/pmd-2.3.jar;../lib/xercesImpl-2.6.2.jar;../lib/xmlParserAPIs-2.6.2.jar;../lib/jaxen-core-1.0-fcs.jar;../lib/saxpath-1.0-fcs.jar net.sourceforge.pmd.util.designer.Designer
