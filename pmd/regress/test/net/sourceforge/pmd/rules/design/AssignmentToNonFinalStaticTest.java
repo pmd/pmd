ed39586ce5ac157b1849a38ff87d3ac1e73fa6b4
Tom Copeland <tomcopeland@users.sourceforge.net>
2005-02-09 16:21:16 +0000

    Fixed mispeled name
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@3231 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/regress/test/net/sourceforge/pmd/rules/design/AssignmentToNonFinalStaticTest.java
+++ pmd/regress/test/net/sourceforge/pmd/rules/design/AssignmentToNonFinalStaticTest.java
@@ -6,7 +6,7 @@ import net.sourceforge.pmd.RuleSetNotFoundException;
 import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
 import test.net.sourceforge.pmd.testframework.TestDescriptor;
 
-public class AssignementToNonFinalStaticTest extends SimpleAggregatorTst {
+public class AssignmentToNonFinalStaticTest extends SimpleAggregatorTst {
 
     private Rule rule;
 
