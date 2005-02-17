2a1df0bbcfaedc190b0cdc715aba4566ac343433
Miguel Griffa <mikkey@users.sourceforge.net>
2005-02-17 15:45:19 +0000

    implemented [ 1124769 ] Synchronize at the block level rather than the method level
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@3273 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/rules/AvoidSynchronizedAtMethodLevel.java
@@ -0,0 +1,39 @@
+package test.net.sourceforge.pmd.rules;
+
+import net.sourceforge.pmd.PMD;
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.RuleSetNotFoundException;
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
+
+public class AvoidSynchronizedAtMethodLevel extends SimpleAggregatorTst {
+
+    private Rule rule;
+
+    public void setUp() throws RuleSetNotFoundException {
+        rule = findRule("rulesets/newrules.xml", "AvoidSynchronizedAtMethodLevel");
+    }
+
+    public void testAll() {
+        
+       runTests(new TestDescriptor[] {
+               new TestDescriptor(TEST1, "TEST1", 1, rule),
+               new TestDescriptor(TEST2, "TEST2", 0, rule),
+       });
+    }
+
+    private static final String TEST1 =
+        "public class Foo {" + PMD.EOL +
+        " synchronized void foo () {" +
+        " }" + PMD.EOL +
+        "}";
+
+    private static final String TEST2 =
+        "public class Foo {" + PMD.EOL +
+        " void foo () {" +
+        " synchronized(mutex) {" +
+        " }" +
+        "}" + PMD.EOL +
+        "}";
+
+}
