2c9c0aae09d569125f83a29a29acfc1e406086f7
Miguel Griffa <mikkey@users.sourceforge.net>
2005-02-17 15:36:21 +0000

    implemented [ 1124759 ] Avoid calling 'finalize ()' explicitly
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@3272 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/rules/AvoidCallingFinalizeTest.java
@@ -0,0 +1,55 @@
+package test.net.sourceforge.pmd.rules;
+
+import net.sourceforge.pmd.PMD;
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.RuleSetNotFoundException;
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
+
+public class AvoidCallingFinalizeTest extends SimpleAggregatorTst {
+
+    private Rule rule;
+
+    public void setUp() throws RuleSetNotFoundException {
+        rule = findRule("rulesets/newrules.xml", "AvoidCallingFinalize");
+    }
+
+    public void testAll() {
+       runTests(new TestDescriptor[] {
+               new TestDescriptor(TEST1, "TEST1", 1, rule),
+               new TestDescriptor(TEST2, "TEST2", 1, rule),
+               new TestDescriptor(TEST3, "TEST3", 1, rule),
+               new TestDescriptor(TEST4, "TEST4", 0, rule),
+       });
+    }
+
+    private static final String TEST1 =
+        "public class Foo {" + PMD.EOL +
+        " void foo () {" +
+        "  finalize();" +
+        " }" + PMD.EOL +
+        "}";
+
+    private static final String TEST2 =
+        "public class Foo {" + PMD.EOL +
+        " void foo () {" +
+        "  Foo f;" +
+        "  f.finalize();" +
+        " }" + PMD.EOL +
+        "}";
+
+
+    private static final String TEST3 =
+        "public class Foo {" + PMD.EOL +
+        " void foo () {" +
+        "  super.finalize();" +
+        " }" + PMD.EOL +
+        "}";
+
+
+    private static final String TEST4 =
+        "public class Foo {" + PMD.EOL +
+        " void finalize () {" +
+        " }" + PMD.EOL +
+        "}";
+}
