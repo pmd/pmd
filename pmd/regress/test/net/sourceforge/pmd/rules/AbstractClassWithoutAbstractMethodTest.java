106423e2a43c68846f29a95694f1a668d1165c2a
Miguel Griffa <mikkey@users.sourceforge.net>
2005-02-21 17:03:37 +0000

    implemented [ 1122666 ] Abstract class does not contain an abstract method
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@3295 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/rules/AbstractClassWithoutAbstractMethodTest.java
@@ -0,0 +1,42 @@
+package test.net.sourceforge.pmd.rules;
+
+import net.sourceforge.pmd.PMD;
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.RuleSetNotFoundException;
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
+
+public class AbstractClassWithoutAbstractMethodTest extends SimpleAggregatorTst {
+
+    private Rule rule;
+
+    public void setUp() throws RuleSetNotFoundException {
+        rule = findRule("rulesets/newrules.xml", "AbstractClassWithoutAbstractMethod");
+    }
+
+    public void testAll() {
+       runTests(new TestDescriptor[] {
+       		new TestDescriptor(TEST1, "TEST1", 0, rule),
+			new TestDescriptor(TEST2, "TEST2", 1, rule),
+			new TestDescriptor(TEST3, "TEST3", 1, rule),
+			new TestDescriptor(TEST4, "TEST4", 0, rule),
+       });
+    }
+
+    private static final String TEST1 =
+    "public class Foo {}";
+
+    private static final String TEST2 =
+        "public abstract class Foo {}";
+
+    private static final String TEST3 =
+        "public abstract class Foo {" + PMD.EOL +
+        "   int bar() {} " + PMD.EOL + 
+        "}";
+
+    private static final String TEST4 =
+        "public abstract class Foo {" + PMD.EOL +
+        "   abstract int bar(); " + PMD.EOL + 
+        "}";
+
+}
