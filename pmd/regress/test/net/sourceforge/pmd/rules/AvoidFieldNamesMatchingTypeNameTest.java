9ac0ecceb693491a5f9784f6f1ac4a63b50d1568
Miguel Griffa <mikkey@users.sourceforge.net>
2005-02-21 14:34:25 +0000

    implemented [ 1143788 ] TypeAndFieldHaveSameName
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@3289 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/rules/AvoidFieldNamesMatchingTypeNameTest.java
@@ -0,0 +1,52 @@
+/**
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+*/
+package test.net.sourceforge.pmd.rules;
+
+import net.sourceforge.pmd.PMD;
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.RuleSetNotFoundException;
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
+
+public class AvoidFieldNamesMatchingTypeNameTest extends SimpleAggregatorTst {
+
+    private Rule rule;
+
+    public void setUp() throws RuleSetNotFoundException {
+        rule = findRule("rulesets/newrules.xml", "AvoidFieldNamesMatchingTypeName");
+    }
+    
+    public void testAll() {
+    	runTests(new TestDescriptor[] {
+    			new TestDescriptor(TEST1, "TEST1", 1, rule),
+				new TestDescriptor(TEST2, "TEST2", 1, rule),
+				new TestDescriptor(TEST3, "TEST3", 0, rule),
+				new TestDescriptor(TEST4, "TEST4", 1, rule),
+    	});
+    }
+    
+    private static final String TEST1 =
+    	"public class Foo {" + PMD.EOL +
+		"   int foo;" + PMD.EOL +
+		"} ";
+    
+    private static final String TEST2 =
+    	"public class Foo {" + PMD.EOL +
+		"   String foo;" + PMD.EOL +
+		"} ";
+
+    private static final String TEST3 =
+    	"public class Foo {" + PMD.EOL +
+		"   class Bar {" + PMD.EOL +
+		"		Foo foo;" + PMD.EOL +
+		"   }" + PMD.EOL +
+		"} ";
+    
+    private static final String TEST4 =
+    	"public class Foo {" + PMD.EOL +
+		"   class Bar {" + PMD.EOL +
+		"		String bar;" + PMD.EOL +
+		"   }" + PMD.EOL +
+		"} ";
+}
---
+++ pmd/src/net/sourceforge/pmd/rules/AvoidFieldNamesMatchingTypeName.java
@@ -0,0 +1,40 @@
+/**
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+package net.sourceforge.pmd.rules;
+
+import net.sourceforge.pmd.AbstractRule;
+import net.sourceforge.pmd.RuleContext;
+import net.sourceforge.pmd.ast.ASTFieldDeclaration;
+import net.sourceforge.pmd.ast.ASTInterfaceDeclaration;
+import net.sourceforge.pmd.ast.ASTUnmodifiedClassDeclaration;
+
+
+
+public class AvoidFieldNamesMatchingTypeName extends AbstractRule {
+	
+	/* (non-Javadoc)
+	 * @see net.sourceforge.pmd.ast.JavaParserVisitor#visit(net.sourceforge.pmd.ast.ASTInterfaceDeclaration, java.lang.Object)
+	 */
+	public Object visit(ASTInterfaceDeclaration node, Object data) {
+		// Skip interfaces
+		return super.visit(node, data);
+	}
+	
+	/* (non-Javadoc)
+	 * @see net.sourceforge.pmd.ast.JavaParserVisitor#visit(net.sourceforge.pmd.ast.ASTFieldDeclaration, java.lang.Object)
+	 */
+	public Object visit(ASTFieldDeclaration node, Object data) {
+		String varName = node.getVariableName();
+		if (varName!=null) {
+			varName = varName.toLowerCase();
+			ASTUnmodifiedClassDeclaration cl = (ASTUnmodifiedClassDeclaration) node.getFirstParentOfType(ASTUnmodifiedClassDeclaration.class);
+			if (cl!=null && cl.getImage() != null) {
+				if (varName.equals(cl.getImage().toLowerCase())) {
+					addViolation((RuleContext) data, node.getBeginLine());
+				}
+			}
+		}
+		return data;
+	}
+}
