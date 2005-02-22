40146a8a713c18873f38bfe2ad8a5892fd4df9b6
Miguel Griffa <mikkey@users.sourceforge.net>
2005-02-22 16:00:32 +0000

    added test case for bug 985989
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@3303 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/regress/test/net/sourceforge/pmd/rules/ConstructorCallsOverridableMethodTest.java
+++ pmd/regress/test/net/sourceforge/pmd/rules/ConstructorCallsOverridableMethodTest.java
@@ -24,6 +24,7 @@ public class ConstructorCallsOverridableMethodTest extends SimpleAggregatorTst {
            new TestDescriptor(TEST7, "method in anonymous inner class is ok", 0, rule),
            new TestDescriptor(TEST8, "bug report 975407", 0, rule),
 		   new TestDescriptor(BUG_994400, "bug report 994400, reports violation on abstract method", 0, rule),
+		   //FIXME new TestDescriptor(BUG_985989, "bug report 985989, ", 1, rule),
        });
     }
 
@@ -103,5 +104,29 @@ public class ConstructorCallsOverridableMethodTest extends SimpleAggregatorTst {
         " }" + PMD.EOL +
         " abstract void bar() {}" + PMD.EOL +
         "}";
-    
+    private static final String BUG_985989 = "public class Test {" + PMD.EOL + 
+    		"public static class SeniorClass {" + PMD.EOL + 
+    		"  public SeniorClass(){" + PMD.EOL + 
+    		"    toString(); //may throw NullPointerException if overridden" + PMD.EOL + 
+    		"  }" + PMD.EOL + 
+    		"  public String toString(){" + PMD.EOL + 
+    		"    return \"IAmSeniorClass\";" + PMD.EOL + 
+    		"  }" + PMD.EOL + 
+    		"}" + PMD.EOL + 
+    		"public static class JuniorClass extends SeniorClass {" + PMD.EOL + 
+    		"  private String name;" + PMD.EOL + 
+    		"  public JuniorClass(){" + PMD.EOL + 
+    		"    super(); //Automatic call leads to NullPointerException" + PMD.EOL + 
+    		"    name = \"JuniorClass\";" + PMD.EOL + 
+    		"  }" + PMD.EOL + 
+    		"  public String toString(){" + PMD.EOL + 
+    		"    return name.toUpperCase();" + PMD.EOL + 
+    		"  }" + PMD.EOL + 
+    		"}" + PMD.EOL + 
+    		"public static void main (String[] args) {" + PMD.EOL + 
+    		"  System.out.println(\": \"+new SeniorClass());" + PMD.EOL + 
+    		"  System.out.println(\": \"+new JuniorClass());" + PMD.EOL + 
+    		"} // end of main ()" + PMD.EOL + 
+    		"" + PMD.EOL + 
+    		"}";
 }
