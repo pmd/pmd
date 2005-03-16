e7d0ba95baa39dd515b63449f126c89b5dcc50e0
Miguel Griffa <mikkey@users.sourceforge.net>
2005-03-16 15:54:38 +0000

    implemented [ 1151286 ] Use ArrayList instead of Vector
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@3389 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/rules/UseArrayListInsteadOfVectorTest.java
@@ -0,0 +1,55 @@
+package test.net.sourceforge.pmd.rules;
+
+import net.sourceforge.pmd.PMD;
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.RuleSetNotFoundException;
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
+
+public class UseArrayListInsteadOfVectorTest extends SimpleAggregatorTst {
+
+    private Rule rule;
+
+    public void setUp() throws RuleSetNotFoundException {
+        rule = findRule("rulesets/newrules.xml", "UseArrayListInsteadOfVector");
+    }
+
+    public void testAll() {
+       runTests(new TestDescriptor[] {
+               new TestDescriptor(TEST0, "TEST0", 0, rule),
+               new TestDescriptor(TEST1, "TEST1", 1, rule),
+               new TestDescriptor(TEST2, "TEST2", 1, rule),
+               new TestDescriptor(TEST3, "TEST3", 1, rule),
+       });
+    }
+
+
+    private static final String TEST0 =
+    "public class Bar {" + PMD.EOL +
+    "void x(){" +
+    "List v = new ArrayList(); " +
+    "}" + PMD.EOL +
+    "}";
+    
+    private static final String TEST1 =
+    "public class Bar {" + PMD.EOL +
+    "void x(){" +
+    "Vector v = new Vector(); " +
+    "}" + PMD.EOL +
+    "}";
+
+    private static final String TEST2 =
+    "public class Bar {" + PMD.EOL +
+    "Vector v = new Vector(); " +
+    "void x(){" +
+    "}" + PMD.EOL +
+    "}";
+
+    private static final String TEST3 =
+        "public class Bar {" + PMD.EOL +
+        "List v = new Vector(); " +
+        "void x(){" +
+        "}" + PMD.EOL +
+        "}";
+
+}
