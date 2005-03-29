34f70395c2f1d265aa9f98c0b3ad91fd503e1c23
Tom Copeland <tomcopeland@users.sourceforge.net>
2005-03-29 21:47:14 +0000

    Added new SimplifyStartsWith rule, thx to Miguel for the idea
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@3412 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/rules/optimization/SimplifyStartsWithTest.java
@@ -0,0 +1,45 @@
+package test.net.sourceforge.pmd.rules.optimization;
+
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.RuleSetNotFoundException;
+import net.sourceforge.pmd.PMD;
+
+public class SimplifyStartsWithTest extends SimpleAggregatorTst {
+    private Rule rule;
+
+    public void setUp() throws RuleSetNotFoundException {
+        // on release this can go in optimizations ruleset
+        rule = findRule("rulesets/newrules.xml", "SimplifyStartsWith");
+    }
+
+    public void testAll() {
+       runTests(new TestDescriptor[] {
+               new TestDescriptor(TEST1, "failure case", 1, rule),
+               new TestDescriptor(TEST2, "startsWith multiple chars", 0, rule),
+               new TestDescriptor(TEST3, "startsWith defined on some other class, doesn't take a String", 0, rule),
+       });
+    }
+
+    private static final String TEST1 =
+        "public class Foo {" + PMD.EOL +
+        " public boolean bar(String x) {" + PMD.EOL +
+        "  return x.startsWith(\"x\");" + PMD.EOL +
+        " }" + PMD.EOL +
+        "}";
+
+    private static final String TEST2 =
+        "public class Foo {" + PMD.EOL +
+        " public boolean bar(Fiddle x) {" + PMD.EOL +
+        "  return x.startsWith(\"abc\");" + PMD.EOL +
+        " }" + PMD.EOL +
+        "}";
+
+    private static final String TEST3 =
+        "public class Foo {" + PMD.EOL +
+        " public boolean bar(Fiddle x) {" + PMD.EOL +
+        "  return x.startsWith(123);" + PMD.EOL +
+        " }" + PMD.EOL +
+        "}";
+}
