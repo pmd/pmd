34c8d7e86a8618ebc05a0ced20419ae59c246c79
Tom Copeland <tomcopeland@users.sourceforge.net>
2005-03-30 22:19:13 +0000

    Implemented UnnecessaryParentheses
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@3413 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/rules/UnnecessaryParenthesesTest.java
@@ -0,0 +1,52 @@
+package test.net.sourceforge.pmd.rules;
+
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.PMD;
+
+public class UnnecessaryParenthesesTest extends SimpleAggregatorTst{
+    private Rule rule;
+
+    public void setUp() throws Exception {
+        rule = findRule("rulesets/newrules.xml", "UnnecessaryParentheses");
+    }
+
+    public void testAll() {
+       runTests(new TestDescriptor[] {
+           new TestDescriptor(TEST1, "failure case, returning literal", 1, rule),
+           new TestDescriptor(TEST2, "ok, complex expression", 0, rule),
+           new TestDescriptor(TEST3, "bad, returning variable in parens", 1, rule),
+           new TestDescriptor(TEST4, "ok, returning comparison", 0, rule),
+       });
+    }
+
+    private static final String TEST1 =
+    " public class Foo {" + PMD.EOL +
+    "  int bar() {" + PMD.EOL +
+    "   return (2); " + PMD.EOL +
+    "  }" + PMD.EOL +
+    " }";
+
+    private static final String TEST2 =
+    " public class Foo {" + PMD.EOL +
+    "  int bar() {" + PMD.EOL +
+    "   return (2+2); " + PMD.EOL +
+    "  }" + PMD.EOL +
+    " }";
+
+    private static final String TEST3 =
+    " public class Foo {" + PMD.EOL +
+    "  int bar(int y) {" + PMD.EOL +
+    "   return (y); " + PMD.EOL +
+    "  }" + PMD.EOL +
+    " }";
+
+    private static final String TEST4 =
+    " public class Foo {" + PMD.EOL +
+    "  int bar(int y) {" + PMD.EOL +
+    "   return (x=y); " + PMD.EOL +
+    "  }" + PMD.EOL +
+    " }";
+
+}
