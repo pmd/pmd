6149424457cf801f1c3a35183f6ff3a364f6c9f1
Tom Copeland <tomcopeland@users.sourceforge.net>
2005-04-15 14:27:07 +0000

    Added UseStringBufferForStringAppends; thanks to Eric Olander for the new rule!
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@3435 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/rules/optimization/UseStringBufferForStringAppendsTest.java
@@ -0,0 +1,40 @@
+package test.net.sourceforge.pmd.rules.optimization;
+
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.RuleSetNotFoundException;
+import net.sourceforge.pmd.PMD;
+
+public class UseStringBufferForStringAppendsTest extends SimpleAggregatorTst {
+    private Rule rule;
+
+    public void setUp() throws RuleSetNotFoundException {
+        // on release this can go in optimizations ruleset
+        rule = findRule("rulesets/newrules.xml", "UseStringBufferForStringAppends");
+    }
+
+    public void testAll() {
+       runTests(new TestDescriptor[] {
+               new TestDescriptor(TEST1, "failure case", 1, rule),
+               new TestDescriptor(TEST2, "startsWith multiple chars", 0, rule),
+       });
+    }
+
+    private static final String TEST1 =
+        "public class Foo {" + PMD.EOL +
+        " public void bar() {" + PMD.EOL +
+        "  String x;" + PMD.EOL +
+        "  x = \"foo\";" + PMD.EOL +
+        "  x += \"bar\";" + PMD.EOL +
+        " }" + PMD.EOL +
+        "}";
+
+    private static final String TEST2 =
+        "public class Foo {" + PMD.EOL +
+        " public boolean bar(Fiddle x) {" + PMD.EOL +
+        "  return x.startsWith(\"abc\");" + PMD.EOL +
+        " }" + PMD.EOL +
+        "}";
+
+}
