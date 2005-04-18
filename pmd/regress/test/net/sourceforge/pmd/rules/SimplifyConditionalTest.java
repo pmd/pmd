9a01cb74e69dd7ced51935c078d0e8f15d65f4cd
Tom Copeland <tomcopeland@users.sourceforge.net>
2005-04-18 12:31:28 +0000

    Improved SimplifyConditional from Wouter Zelle, thanks Wouter!
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@3441 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/regress/test/net/sourceforge/pmd/rules/SimplifyConditionalTest.java
+++ pmd/regress/test/net/sourceforge/pmd/rules/SimplifyConditionalTest.java
@@ -19,6 +19,7 @@ public class SimplifyConditionalTest extends SimpleAggregatorTst{
            new TestDescriptor(TEST1, "failure case", 1, rule),
            new TestDescriptor(TEST2, "ok", 0, rule),
            new TestDescriptor(TEST3, "transpose x and null, still bad", 1, rule),
+           new TestDescriptor(TEST4, "conditional or and !(instanceof)", 1, rule),
        });
     }
 
@@ -40,6 +41,13 @@ public class SimplifyConditionalTest extends SimpleAggregatorTst{
     "public class Foo {" + PMD.EOL +
     " void bar(Object x) {" + PMD.EOL +
     "  if (null != x && x instanceof String) {}" + PMD.EOL +
+    " }" + PMD.EOL +
+    "}";
+
+    private static final String TEST4 =
+    "public class Foo {" + PMD.EOL +
+    " void bar(Object x) {" + PMD.EOL +
+    "  if (x == null || !(x instanceof String)) {}" + PMD.EOL +
     " }" + PMD.EOL +
     "}";
 
