e6b127e1d531f0e8b94d36d3c35e9a3f99d0b01f
Tom Copeland <tomcopeland@users.sourceforge.net>
2005-04-18 02:14:48 +0000

    Added SimplifyConditional, thx to Eric Olander and Wouter Zelle for the code!
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@3440 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/rules/SimplifyConditionalTest.java
@@ -0,0 +1,46 @@
+package test.net.sourceforge.pmd.rules;
+
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.RuleSetNotFoundException;
+import net.sourceforge.pmd.PMD;
+
+public class SimplifyConditionalTest extends SimpleAggregatorTst{
+    private Rule rule;
+
+    public void setUp() throws RuleSetNotFoundException {
+        // put in design ruleset
+        rule = findRule("rulesets/newrules.xml", "SimplifyConditional");
+    }
+
+    public void testAll() {
+       runTests(new TestDescriptor[] {
+           new TestDescriptor(TEST1, "failure case", 1, rule),
+           new TestDescriptor(TEST2, "ok", 0, rule),
+           new TestDescriptor(TEST3, "transpose x and null, still bad", 1, rule),
+       });
+    }
+
+    private static final String TEST1 =
+    "public class Foo {" + PMD.EOL +
+    " void bar(Object x) {" + PMD.EOL +
+    "  if (x != null && x instanceof String) {}" + PMD.EOL +
+    " }" + PMD.EOL +
+    "}";
+
+    private static final String TEST2 =
+    "public class Foo {" + PMD.EOL +
+    " void bar(Object x) {" + PMD.EOL +
+    "  if (x instanceof String) {}" + PMD.EOL +
+    " }" + PMD.EOL +
+    "}";
+
+    private static final String TEST3 =
+    "public class Foo {" + PMD.EOL +
+    " void bar(Object x) {" + PMD.EOL +
+    "  if (null != x && x instanceof String) {}" + PMD.EOL +
+    " }" + PMD.EOL +
+    "}";
+
+}
