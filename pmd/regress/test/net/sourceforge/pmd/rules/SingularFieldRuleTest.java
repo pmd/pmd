d5c6a30827fe28fa5dad54aea4c468b9da96acf3
Tom Copeland <tomcopeland@users.sourceforge.net>
2005-04-28 21:34:14 +0000

    Checking in SingularField, thanks to Eric Olander for the code!
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@3459 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/rules/SingularFieldRuleTest.java
@@ -0,0 +1,76 @@
+package test.net.sourceforge.pmd.rules;
+
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.RuleSetNotFoundException;
+import net.sourceforge.pmd.PMD;
+
+public class SingularFieldRuleTest extends SimpleAggregatorTst {
+    private Rule rule;
+
+    public void setUp() throws RuleSetNotFoundException {
+        // put in controversial ruleset
+        rule = findRule("rulesets/newrules.xml", "SingularField");
+    }
+
+    public void testAll() {
+       runTests(new TestDescriptor[] {
+           new TestDescriptor(TEST1, "failure case", 1, rule),
+           new TestDescriptor(TEST2, "ok", 0, rule),
+           new TestDescriptor(TEST3, "second method uses 'this'", 0, rule),
+           new TestDescriptor(TEST4, "skip publics", 0, rule),
+           new TestDescriptor(TEST5, "skip statics", 0, rule),
+       });
+    }
+
+    private static final String TEST1 =
+    "public class Foo {" + PMD.EOL +
+    " private int x;" + PMD.EOL +
+    " int bar(int y) {" + PMD.EOL +
+    "  x = y + 5; " + PMD.EOL +
+    "  return x;" + PMD.EOL +
+    " }" + PMD.EOL +
+    "}";
+
+    private static final String TEST2 =
+    "public class Foo {" + PMD.EOL +
+    " private int x;" + PMD.EOL +
+    " void setX(int x) {" + PMD.EOL +
+    "  this.x = x;" + PMD.EOL +
+    " }" + PMD.EOL +
+    " int getX() {" + PMD.EOL +
+    "  return x;" + PMD.EOL +
+    " }" + PMD.EOL +
+    "}";
+
+    private static final String TEST3 =
+    "public class Foo {" + PMD.EOL +
+    " private int x;" + PMD.EOL +
+    " void setX(int x) {" + PMD.EOL +
+    "  this.x = x;" + PMD.EOL +
+    " }" + PMD.EOL +
+    " int getX() {" + PMD.EOL +
+    "  return this.x;" + PMD.EOL +
+    " }" + PMD.EOL +
+    "}";
+
+    private static final String TEST4 =
+    "public class Foo {" + PMD.EOL +
+    " public int x;" + PMD.EOL +
+    " int bar(int y) {" + PMD.EOL +
+    "  x = y + 5; " + PMD.EOL +
+    "  return x;" + PMD.EOL +
+    " }" + PMD.EOL +
+    "}";
+
+    private static final String TEST5 =
+    "public class Foo {" + PMD.EOL +
+    " private static int x;" + PMD.EOL +
+    " int bar(int y) {" + PMD.EOL +
+    "  x = y + 5; " + PMD.EOL +
+    "  return x;" + PMD.EOL +
+    " }" + PMD.EOL +
+    "}";
+
+}
---
+++ pmd/src/net/sourceforge/pmd/rules/SingularField.java
@@ -0,0 +1,52 @@
+/*
+ * SingularField.java
+ *
+ * Created on April 17, 2005, 9:49 PM
+ */
+
+package net.sourceforge.pmd.rules;
+
+import java.text.MessageFormat;
+import java.util.List;
+import net.sourceforge.pmd.AbstractRule;
+import net.sourceforge.pmd.RuleContext;
+import net.sourceforge.pmd.ast.ASTClassOrInterfaceDeclaration;
+import net.sourceforge.pmd.ast.ASTConstructorDeclaration;
+import net.sourceforge.pmd.ast.ASTFieldDeclaration;
+import net.sourceforge.pmd.ast.ASTMethodDeclaration;
+import net.sourceforge.pmd.ast.ASTMethodDeclarator;
+import net.sourceforge.pmd.ast.ASTVariableDeclaratorId;
+import org.jaxen.JaxenException;
+
+/**
+ *
+ * @author Eric Olander
+ */
+public class SingularField extends AbstractRule {
+    
+    public Object visit(ASTFieldDeclaration node, Object data) {
+        if (node.isPrivate() && !node.isStatic()) {
+            List list = node.findChildrenOfType(ASTVariableDeclaratorId.class);
+            ASTVariableDeclaratorId decl = (ASTVariableDeclaratorId)list.get(0);
+            String name = decl.getImage();
+            String path = "//MethodDeclaration[.//PrimaryExpression[.//Name[@Image = \""+name+"\" or substring-before(@Image, \".\") = \""+name+"\"] or .//PrimarySuffix[@Image = \""+name+"\"]]] |" +
+                     "//ConstructorDeclaration[.//PrimaryExpression[.//Name[@Image = \""+name+"\" or substring-before(@Image, \".\") = \""+name+"\"] or .//PrimarySuffix[@Image = \""+name+"\"]]]";
+            try {
+                List nodes = node.findChildNodesWithXPath(path);
+                if (nodes.size() == 1) {
+                    String method;
+                    if (nodes.get(0) instanceof ASTMethodDeclaration) {
+                        method = ((ASTMethodDeclarator)((ASTMethodDeclaration)nodes.get(0)).findChildrenOfType(ASTMethodDeclarator.class).get(0)).getImage();
+                    } else {
+                        method = ((ASTClassOrInterfaceDeclaration)((ASTConstructorDeclaration)nodes.get(0)).getFirstParentOfType(ASTClassOrInterfaceDeclaration.class)).getImage();
+                    }
+                    ((RuleContext)data).getReport().addRuleViolation(createRuleViolation((RuleContext) data, decl, MessageFormat.format(getMessage(), new Object[]{name, method})));
+                }
+            } catch (JaxenException je) {
+                je.printStackTrace();   
+            }
+        }
+        return data;
+    }
+    
+}
