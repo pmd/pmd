9d187c1099c339c38704a70cb0d3590fcd9e23b2
Tom Copeland <tomcopeland@users.sourceforge.net>
2005-05-05 14:15:17 +0000

    Implemented RFE 1193979 - BooleanInstantiation now catches cases like Boolean.valueOf(true)
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@3467 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/regress/test/net/sourceforge/pmd/rules/BooleanInstantiationRuleTest.java
+++ pmd/regress/test/net/sourceforge/pmd/rules/BooleanInstantiationRuleTest.java
@@ -22,6 +22,8 @@ public class BooleanInstantiationRuleTest extends SimpleAggregatorTst {
            new TestDescriptor(TEST1, "simple failure case", 1, rule),
            new TestDescriptor(TEST2, "new java.lang.Boolean", 1, rule),
            new TestDescriptor(TEST3, "ok", 0, rule),
+           new TestDescriptor(TEST4, "don't use Boolean.valueOf() with literal", 2, rule),
+           new TestDescriptor(TEST5, "valueOf() with variable is fine", 0, rule),
        });
     }
 
@@ -38,6 +40,17 @@ public class BooleanInstantiationRuleTest extends SimpleAggregatorTst {
     private static final String TEST3 =
     "public class Foo {" + PMD.EOL +
     " Boolean b = Boolean.TRUE;" + PMD.EOL +
+    "}";
+
+    private static final String TEST4 =
+    "public class Foo {" + PMD.EOL +
+    " Boolean b = Boolean.valueOf(true);" + PMD.EOL +
+    " Boolean b = Boolean.valueOf(false);" + PMD.EOL +
+    "}";
+
+    private static final String TEST5 =
+    "public class Foo {" + PMD.EOL +
+    " Boolean b = Boolean.valueOf(x);" + PMD.EOL +
     "}";
 
 }
