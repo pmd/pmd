f62a8157dd04c41f00f122e8daf89d3b0b216c49
Tom Copeland <tomcopeland@users.sourceforge.net>
2005-05-27 15:07:37 +0000

    New Javadocs thx to Wim Deblauwe
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@3507 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/net/sourceforge/pmd/RuleSet.java
+++ pmd/src/net/sourceforge/pmd/RuleSet.java
@@ -8,20 +8,36 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * This class represents a Set of rules.
+ * @see Rule
+ */
 public class RuleSet {
 
     private Set rules = new HashSet();
     private String name;
     private String description;
 
+	/**
+	 * Returns the number of rules in this ruleset
+	 * @return an int representing the number of rules
+	 */
     public int size() {
         return rules.size();
     }
 
+	/**
+	 * Add a new rule to this ruleset
+	 * @param rule the rule to be added
+	 */
     public void addRule(Rule rule) {
         rules.add(rule);
     }
 
+	/**
+	 * Returns the actual Set of rules in this ruleset
+	 * @return a Set with the rules. All objects are of type {@link Rule}
+	 */
     public Set getRules() {
         return rules;
     }
@@ -52,6 +68,13 @@ public class RuleSet {
         return false;
     }
 
+	/**
+	 * Returns the Rule with the given name
+	 *
+	 * @param ruleName the name of the rule to find
+	 * @return
+	 * @throws RuntimeException when the rule with the given name cannot be found
+	 */
     public Rule getRuleByName(String ruleName) {
         for (Iterator i = rules.iterator(); i.hasNext();) {
             Rule r = (Rule) i.next();
@@ -62,6 +85,11 @@ public class RuleSet {
         throw new RuntimeException("Couldn't find rule named " + ruleName + " in the ruleset " + name);
     }
 
+	/**
+	 * Add a whole RuleSet to this RuleSet
+	 *
+	 * @param ruleSet the RuleSet to add
+	 */
     public void addRuleSet(RuleSet ruleSet) {
         rules.addAll(ruleSet.getRules());
     }
@@ -74,18 +102,38 @@ public class RuleSet {
         }
     }
 
+	/**
+	 * Gives the name of this ruleset
+	 *
+	 * @return a String representing the name
+	 */
     public String getName() {
         return name;
     }
 
+	/**
+	 * Set the name of this ruleset
+	 *
+	 * @param name a String representing the name
+	 */
     public void setName(String name) {
         this.name = name;
     }
 
+	/**
+	 * Gives the description of this ruleset
+	 *
+	 * @return a String representing the description
+	 */
     public String getDescription() {
         return description;
     }
 
+	/**
+	 * Set the description of this ruleset
+	 *
+	 * @param description a String representing the description
+	 */
     public void setDescription(String description) {
         this.description = description;
     }
