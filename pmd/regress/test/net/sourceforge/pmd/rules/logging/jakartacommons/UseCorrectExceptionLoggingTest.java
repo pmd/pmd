ffb85aab42d06b3294f355b98fd3e23908ea1a96
Tom Copeland <tomcopeland@users.sourceforge.net>
2005-06-06 17:49:07 +0000

    New UseCorrectExceptionLogging from Wim Deblauwe, thanks Wim!
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@3535 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/rules/logging/jakartacommons/UseCorrectExceptionLoggingTest.java
@@ -0,0 +1,45 @@
+package test.net.sourceforge.pmd.rules.logging.jakartacommons;
+
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.RuleSetNotFoundException;
+import net.sourceforge.pmd.PMD;
+
+public class UseCorrectExceptionLoggingTest extends SimpleAggregatorTst {
+
+    private Rule rule;
+
+    public void setUp() throws RuleSetNotFoundException {
+        rule = findRule("rulesets/newrules.xml", "UseCorrectExceptionLogging");
+    }
+
+    public void testAll() {
+       runTests(new TestDescriptor[] {
+           new TestDescriptor(TEST1, "ok", 0, rule),
+           new TestDescriptor(TEST2, "failure case", 1, rule),
+       });
+    }
+
+    private static final String TEST1 =
+    "public class Foo {" + PMD.EOL +
+    " static final Log _LOG = LogFactory.getLog( Main.class );" + PMD.EOL +
+    " void foo() {" + PMD.EOL +
+    "  try {} catch (OtherException oe) {" + PMD.EOL +
+    "   _LOG.error(oe.getMessage(), oe);" + PMD.EOL +
+    "  }" + PMD.EOL +
+    " }" + PMD.EOL +
+    "}";
+
+    private static final String TEST2 =
+    "public class Foo {" + PMD.EOL +
+    " static final Log _LOG = LogFactory.getLog( Main.class );" + PMD.EOL +
+    " void foo() {" + PMD.EOL +
+    "  try {} catch (Exception e) {" + PMD.EOL +
+    "   _LOG.error(e);" + PMD.EOL +
+    "  } " + PMD.EOL +
+    " }" + PMD.EOL +
+    "}";
+
+
+}
