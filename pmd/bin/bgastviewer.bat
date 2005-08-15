0b384a4d3c76861862b11154d21c004d94980e68
Tom Copeland <tomcopeland@users.sourceforge.net>
2005-08-15 20:03:07 +0000

    Removed various viewer remnants
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@3782 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/bin/bgastviewer.bat
+++
@@ -1,1 +0,0 @@
-java -cp ../build;../lib/pmd-3.2.jar;../lib/xercesImpl-2.6.2.jar;../lib/xmlParserAPIs-2.6.2.jar;../lib/jaxen-core-1.0-fcs.jar;../lib/saxpath-1.0-fcs.jar net.sourceforge.pmd.util.viewer.Viewer
