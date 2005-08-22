cea3404ccd6ffbedb61e2e92bbf168672ee6fd97
Tom Copeland <tomcopeland@users.sourceforge.net>
2005-08-22 13:12:22 +0000

    Added PackageCase and NoPackage to the naming ruleset; thanks to Peter Frandsen for the code!
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@3794 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/rules/NoPackageTest.java
@@ -0,0 +1,39 @@
+package test.net.sourceforge.pmd.rules;
+
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.RuleSetNotFoundException;
+import net.sourceforge.pmd.PMD;
+
+public class NoPackageTest extends SimpleAggregatorTst{
+
+    private Rule rule;
+
+    public void setUp() throws RuleSetNotFoundException {
+        rule = findRule("naming", "NoPackage");
+    }
+
+    public void testAll() {
+       runTests(new TestDescriptor[] {
+           new TestDescriptor(TEST1, "bad", 1, rule),
+           new TestDescriptor(TEST2, "good", 0, rule),
+           new TestDescriptor(TEST3, "nested package", 0, rule),
+       });
+    }
+
+    private static final String TEST1 =
+    "public class Foo {" + PMD.EOL +
+    "}";
+
+    private static final String TEST2 =
+    "package foo;" + PMD.EOL +
+    "public class Foo {" + PMD.EOL +
+    "}";
+
+    private static final String TEST3 =
+    "package foo.bar;" + PMD.EOL +
+    "public class Foo {" + PMD.EOL +
+    "}";
+
+}
---
+++ pmd/regress/test/net/sourceforge/pmd/rules/PackageCaseTest.java
@@ -0,0 +1,35 @@
+package test.net.sourceforge.pmd.rules;
+
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.RuleSetNotFoundException;
+import net.sourceforge.pmd.PMD;
+
+public class PackageCaseTest extends SimpleAggregatorTst{
+
+    private Rule rule;
+
+    public void setUp() throws RuleSetNotFoundException {
+        rule = findRule("naming", "PackageCase");
+    }
+
+    public void testAll() {
+       runTests(new TestDescriptor[] {
+           new TestDescriptor(TEST1, "bad", 1, rule),
+           new TestDescriptor(TEST2, "good", 0, rule),
+       });
+    }
+
+    private static final String TEST1 =
+    "package foo.BarBuz;" + PMD.EOL +
+    "public class Foo {" + PMD.EOL +
+    "}";
+
+    private static final String TEST2 =
+    "package foo.bar;" + PMD.EOL +
+    "public class Foo {" + PMD.EOL +
+    "}";
+
+
+}
