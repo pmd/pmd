7f8a17702c00579bb33aaa475465972991542148
Tom Copeland <tomcopeland@users.sourceforge.net>
2005-10-24 19:18:47 +0000

    rls preps
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@3921 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd-emacs/prep_release.sh
+++ pmd-emacs/prep_release.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+rm -rf pmd-emacs-0.6
 mkdir pmd-emacs-0.6
 cp src/elisp/pmd.el CHANGELOG INSTALL LICENSE.txt pmd-emacs-0.6
+zip -q -r pmd-emacs-0.6.zip pmd-emacs-0.6/
+rm -rf pmd-emacs-0.6
