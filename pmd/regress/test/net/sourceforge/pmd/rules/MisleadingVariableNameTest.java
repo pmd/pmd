d2d6e2565313faf3db50d68a72bf44e02f8787f1
Tom Copeland <tomcopeland@users.sourceforge.net>
2005-11-04 14:08:15 +0000

    Implemented MisleadingVariableName in the naming ruleset
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@3960 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/rules/MisleadingVariableNameTest.java
@@ -0,0 +1,43 @@
+package test.net.sourceforge.pmd.rules;
+
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.RuleSetNotFoundException;
+import net.sourceforge.pmd.PMD;
+
+public class MisleadingVariableNameTest  extends SimpleAggregatorTst {
+    private Rule rule;
+
+    public void setUp() throws RuleSetNotFoundException {
+        rule = findRule("naming", "MisleadingVariableName");
+    }
+
+    public void testAll() {
+       runTests(new TestDescriptor[] {
+           new TestDescriptor(TEST1, "misnamed param", 1, rule),
+           new TestDescriptor(TEST2, "misnamed local", 1, rule),
+	       new TestDescriptor(TEST3, "all's well", 0, rule),
+       });
+    }
+
+    private static final String TEST1 =
+    "public class Foo {" + PMD.EOL +
+	"	void main(String m_foo) {" + PMD.EOL +
+	"	}" + PMD.EOL +
+    "}";
+
+    private static final String TEST2 =
+    "public class Foo {" + PMD.EOL +
+	"	void main() {" + PMD.EOL +
+	"	 int m_foo = 42;" + PMD.EOL +
+	"	}" + PMD.EOL +
+    "}";
+
+    private static final String TEST3 =
+    "public class Foo {" + PMD.EOL +
+	"	private int m_bar;" + PMD.EOL +
+	"	public static final int m_buz = 42;" + PMD.EOL +
+	"	private void buz(String biz) {}" + PMD.EOL +
+    "}";
+}
