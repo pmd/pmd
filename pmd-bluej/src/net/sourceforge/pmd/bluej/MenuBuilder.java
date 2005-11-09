3cc172f79c9f19b91fc99847541ba686fa5125fa
Tom Copeland <tomcopeland@users.sourceforge.net>
2005-11-09 17:44:12 +0000

    Basic implementation; gloms report into a JOptionPane, no prefs
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@3973 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd-bluej/src/net/sourceforge/pmd/bluej/MenuBuilder.java
+++ pmd-bluej/src/net/sourceforge/pmd/bluej/MenuBuilder.java
@@ -9,110 +9,71 @@ import bluej.extensions.editor.TextLocation;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
+import java.io.StringReader;
+import java.util.Iterator;
 
-/* This class shows how you can bind different menus to different parts of BlueJ
- * Remember:
- * - getToolsMenuItem, getClassMenuItem and getObjectMenuItem may be called by BlueJ at any time.
- * - They must generate a new JMenuItem each time they are called.
- * - No reference to the JMenuItem should be stored in the extension.
- * - You must be quick in generating your menu.
- */
+import net.sourceforge.pmd.RuleSet;
+import net.sourceforge.pmd.RuleContext;
+import net.sourceforge.pmd.PMD;
+import net.sourceforge.pmd.RuleSetFactory;
+import net.sourceforge.pmd.TargetJDK1_5;
+import net.sourceforge.pmd.TargetJDK1_4;
+import net.sourceforge.pmd.RuleViolation;
+
 public class MenuBuilder extends MenuGenerator {
-    private BPackage curPackage;
-    private BClass curClass;
-    private BObject curObject;
 
-    public JMenuItem getToolsMenuItem(BPackage aPackage) {
-        return new JMenuItem(new SimpleAction("Click Tools", "Tools menu:"));
-    }
+    private BClass curClass;
 
     public JMenuItem getClassMenuItem(BClass aClass) {
-        JMenu jm = new JMenu("Simple Extension");
-        jm.add(new JMenuItem(new SimpleAction("Click Class", "Class menu:")));
-        jm.add(new JMenuItem(new EditAction()));
+        JMenu jm = new JMenu("PMD");
+        jm.add(new JMenuItem(new SimpleAction("Check code", "Class menu:")));
         return jm;
     }
 
-    public JMenuItem getObjectMenuItem(BObject anObject) {
-        return new JMenuItem(new SimpleAction("Click Object", "Object menu:"));
-    }
-
-    // These methods will be called when
-    // each of the different menus are about to be invoked.
-    public void notifyPostToolsMenu(BPackage bp, JMenuItem jmi) {
-        System.out.println("Post on Tools menu");
-        curPackage = bp ; curClass = null ; curObject = null;
-    }
-
     public void notifyPostClassMenu(BClass bc, JMenuItem jmi) {
-        System.out.println("Post on Class menu");
-        curPackage = null ; curClass = bc ; curObject = null;
-    }
-
-    public void notifyPostObjectMenu(BObject bo, JMenuItem jmi) {
-        System.out.println("Post on Object menu");
-        curPackage = null ; curClass = null ; curObject = bo;
-    }
-
-    // A utility method which pops up a dialog detailing the objects
-    // involved in the current (SimpleAction) menu invocation.
-    private void showCurrentStatus(String header) {
-        try {
-            if (curObject != null)
-                curClass = curObject.getBClass();
-            if (curClass != null)
-                curPackage = curClass.getPackage();
-
-            String msg = header;
-            if (curPackage != null)
-                msg += "\nCurrent Package = " + curPackage;
-            if (curClass != null)
-                msg += "\nCurrent Class = " + curClass;
-            if (curObject != null)
-                msg += "\nCurrent Object = " + curObject;
-            JOptionPane.showMessageDialog(null, msg);
-        } catch (Exception exc) { }
-    }
-
-    // A method to add a comment at the end of the current class, using the Editor API
-    private void addComment() {
-        Editor classEditor = null;
-        try {
-            classEditor = curClass.getEditor();
-        } catch (Exception e) { }
-        if(classEditor == null) {
-            System.out.println("Can't create Editor for " + curClass);
-            return;
-        }
-
-        int textLen = classEditor.getTextLength();
-        TextLocation lastLine = classEditor.getTextLocationFromOffset(textLen);
-        lastLine.setColumn(0);
-        // The TextLocation now points before the first character of the last line of the current text
-        // which we'll assume contains the closing } bracket for the class
-        classEditor.setText(lastLine, lastLine, "// Comment added by SimpleExtension\n");
+        curClass = bc;
     }
 
     // The nested class that instantiates the different (simple) menus.
     class SimpleAction extends AbstractAction {
         private String msgHeader;
-
         public SimpleAction(String menuName, String msg) {
             putValue(AbstractAction.NAME, menuName);
             msgHeader = msg;
         }
         public void actionPerformed(ActionEvent anEvent) {
-            showCurrentStatus(msgHeader);
-        }
-    }
+            try {
 
-    // And the nested class which implements the editor interaction menu
-    class EditAction extends AbstractAction {
-        public EditAction() {
-            putValue(AbstractAction.NAME, "Add comment");
-        }
-        public void actionPerformed(ActionEvent e) {
-            addComment();
+                int textLen = curClass.getEditor().getTextLength();
+                TextLocation lastLine = curClass.getEditor().getTextLocationFromOffset(textLen);
+                String code = curClass.getEditor().getText(new TextLocation(0,0), lastLine);
+
+                StringBuffer msg = new StringBuffer("");
+
+                RuleSetFactory rsf = new RuleSetFactory();
+                RuleSet rs = new RuleSet();
+                rs.addRuleSet(rsf.createRuleSet("rulesets/basic.xml"));
+                rs.addRuleSet(rsf.createRuleSet("rulesets/unusedcode.xml"));
+                RuleContext ctx = new RuleContext();
+                ctx.setSourceCodeFilename("[no filename]");
+                StringReader reader = new StringReader(code);
+                new PMD(new TargetJDK1_4()).processFile(reader, rs, ctx);
+                int violations = ctx.getReport().size();
+
+                msg.append(" " + violations + " rule violation(s)");
+
+                msg.append(System.getProperty("line.separator"));
+                for (Iterator i = ctx.getReport().iterator(); i.hasNext();) {
+                    RuleViolation rv = (RuleViolation)i.next();
+                    msg.append(rv.getDescription());
+                    msg.append(System.getProperty("line.separator"));
+                }
+
+
+                JOptionPane.showMessageDialog(null, msgHeader + msg);
+            } catch (Exception exc) {
+                exc.printStackTrace();
+            }
         }
     }
 }
