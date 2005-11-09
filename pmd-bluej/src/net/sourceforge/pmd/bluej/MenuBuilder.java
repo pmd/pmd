ea99691120a7d4bec776426ee146c7b483b70de6
Tom Copeland <tomcopeland@users.sourceforge.net>
2005-11-09 20:41:43 +0000

    Stripping out unused code
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@3975 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd-bluej/src/net/sourceforge/pmd/bluej/MenuBuilder.java
+++ pmd-bluej/src/net/sourceforge/pmd/bluej/MenuBuilder.java
@@ -1,32 +1,34 @@
 package net.sourceforge.pmd.bluej;
 
 import bluej.extensions.BClass;
-import bluej.extensions.BObject;
-import bluej.extensions.BPackage;
 import bluej.extensions.MenuGenerator;
-import bluej.extensions.editor.Editor;
+import bluej.extensions.BlueJ;
 import bluej.extensions.editor.TextLocation;
+import net.sourceforge.pmd.PMD;
+import net.sourceforge.pmd.RuleContext;
+import net.sourceforge.pmd.RuleSet;
+import net.sourceforge.pmd.RuleSetFactory;
+import net.sourceforge.pmd.RuleViolation;
+import net.sourceforge.pmd.TargetJDK1_4;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
+import java.awt.Frame;
 import java.io.StringReader;
 import java.util.Iterator;
-
-import net.sourceforge.pmd.RuleSet;
-import net.sourceforge.pmd.RuleContext;
-import net.sourceforge.pmd.PMD;
-import net.sourceforge.pmd.RuleSetFactory;
-import net.sourceforge.pmd.TargetJDK1_5;
-import net.sourceforge.pmd.TargetJDK1_4;
-import net.sourceforge.pmd.RuleViolation;
 
 public class MenuBuilder extends MenuGenerator {
 
     private BClass curClass;
+    private Frame frame;
+
+    public MenuBuilder(Frame frame) {
+        this.frame = frame;
+    }
 
     public JMenuItem getClassMenuItem(BClass aClass) {
         JMenu jm = new JMenu("PMD");
-        jm.add(new JMenuItem(new SimpleAction("Check code", "Class menu:")));
+        jm.add(new JMenuItem(new SimpleAction("Check code")));
         return jm;
     }
 
@@ -36,11 +38,11 @@ public class MenuBuilder extends MenuGenerator {
 
     // The nested class that instantiates the different (simple) menus.
     class SimpleAction extends AbstractAction {
-        private String msgHeader;
-        public SimpleAction(String menuName, String msg) {
+
+        public SimpleAction(String menuName) {
             putValue(AbstractAction.NAME, menuName);
-            msgHeader = msg;
         }
+
         public void actionPerformed(ActionEvent anEvent) {
             try {
 
@@ -48,31 +50,35 @@ public class MenuBuilder extends MenuGenerator {
                 TextLocation lastLine = curClass.getEditor().getTextLocationFromOffset(textLen);
                 String code = curClass.getEditor().getText(new TextLocation(0,0), lastLine);
 
-                StringBuffer msg = new StringBuffer("");
-
                 RuleSetFactory rsf = new RuleSetFactory();
                 RuleSet rs = new RuleSet();
                 rs.addRuleSet(rsf.createRuleSet("rulesets/basic.xml"));
                 rs.addRuleSet(rsf.createRuleSet("rulesets/unusedcode.xml"));
                 RuleContext ctx = new RuleContext();
-                ctx.setSourceCodeFilename("[no filename]");
+                ctx.setSourceCodeFilename(curClass.getJavaClass().getName());
                 StringReader reader = new StringReader(code);
                 new PMD(new TargetJDK1_4()).processFile(reader, rs, ctx);
-                int violations = ctx.getReport().size();
 
-                msg.append(" " + violations + " rule violation(s)");
-
-                msg.append(System.getProperty("line.separator"));
-                for (Iterator i = ctx.getReport().iterator(); i.hasNext();) {
-                    RuleViolation rv = (RuleViolation)i.next();
-                    msg.append(rv.getDescription());
+                StringBuffer msg = new StringBuffer("");
+                if (ctx.getReport().size() == 0) {
+                    msg.append("No problems found!");
+                } else {
+                    if (ctx.getReport().size() == 1) {
+                        msg.append(" " + ctx.getReport().size() + " problem found");
+                    } else {
+                        msg.append(" " + ctx.getReport().size() + " problems found");
+                    }
                     msg.append(System.getProperty("line.separator"));
+                    for (Iterator i = ctx.getReport().iterator(); i.hasNext();) {
+                        RuleViolation rv = (RuleViolation)i.next();
+                        msg.append("Line " + rv.getNode().getBeginLine() + ": " + rv.getDescription());
+                        msg.append(System.getProperty("line.separator"));
+                    }
                 }
-
-
-                JOptionPane.showMessageDialog(null, msgHeader + msg);
+                JOptionPane.showMessageDialog(frame, msg);
             } catch (Exception exc) {
                 exc.printStackTrace();
+                JOptionPane.showMessageDialog(frame, "Couldn't run PMD: " + exc.getMessage());
             }
         }
     }
--- pmd-bluej/src/net/sourceforge/pmd/bluej/PMDExtension.java
+++ pmd-bluej/src/net/sourceforge/pmd/bluej/PMDExtension.java
@@ -2,37 +2,20 @@ package net.sourceforge.pmd.bluej;
 
 import bluej.extensions.BlueJ;
 import bluej.extensions.Extension;
-import bluej.extensions.ExtensionException;
-import bluej.extensions.event.PackageEvent;
-import bluej.extensions.event.PackageListener;
 
 import java.net.URL;
 
-public class PMDExtension extends Extension implements PackageListener {
+public class PMDExtension extends Extension {
 
     public void startup (BlueJ bluej) {
-        // Register a generator for menu items
-        bluej.setMenuGenerator(new MenuBuilder());
 
-        // Register a "preferences" panel generator
+        bluej.setMenuGenerator(new MenuBuilder(bluej.getCurrentFrame()));
+/*
         Preferences myPreferences = new Preferences(bluej);
         bluej.setPreferenceGenerator(myPreferences);
-
-        // Listen for BlueJ events at the "package" level
-        bluej.addPackageListener(this);
+*/
     }
 
-    public void packageOpened ( PackageEvent ev ) {
-        try {
-            System.out.println ("Project " + ev.getPackage().getProject().getName() + " opened.");
-        } catch (ExtensionException e) {
-            System.out.println("Project closed by BlueJ");
-        }
-    }  
-  
-    public void packageClosing ( PackageEvent ev ) {
-    }  
-    
     public boolean isCompatible () {
         return true; 
     }
@@ -45,10 +28,6 @@ public class PMDExtension extends Extension implements PackageListener {
         return ("PMD");
     }
 
-    public void terminate() {
-        System.out.println ("PMD extension terminates");
-    }
-    
     public String getDescription () {
         return "PMD extension - finds unused code, empty blocks, and more!";
     }
@@ -56,7 +35,6 @@ public class PMDExtension extends Extension implements PackageListener {
     public URL getURL () {
         try {
             return new URL("http://pmd.sf.net/");
-            //return new URL("http://pmd.sf.net/bluejextension.html");
         } catch ( Exception e ) {
             System.out.println ("Can't get PMD extension URL: "+e.getMessage());
             return null;
