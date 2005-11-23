1e63b5bc7d3340cfeb9280cc6852b892c1125bf5
Tom Copeland <tomcopeland@users.sourceforge.net>
2005-11-23 21:17:07 +0000

    Added Allan's AvoidEnumAsIdentifier rule; thanks Allan!
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@4006 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/rules/migrating/AvoidEnumAsIdentifierTest.java
@@ -0,0 +1,30 @@
+package test.net.sourceforge.pmd.rules.migrating;
+
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.RuleSetNotFoundException;
+import net.sourceforge.pmd.PMD;
+
+public class AvoidEnumAsIdentifierTest extends SimpleAggregatorTst {
+
+    private Rule rule;
+
+    public void setUp() throws RuleSetNotFoundException {
+        rule = findRule("migrating", "AvoidEnumAsIdentifier");
+    }
+
+    public void testAll() {
+       runTests(new TestDescriptor[] {
+           new TestDescriptor(TEST1, "variable and param named enum", 2, rule),
+       });
+    }
+
+    private static final String TEST1 =
+    "public class Foo {" + PMD.EOL +
+    " void bar(String enum) {" + PMD.EOL +
+    "  String enum = \"hi\";" + PMD.EOL +
+    " }" + PMD.EOL +
+    "}";
+
+}
