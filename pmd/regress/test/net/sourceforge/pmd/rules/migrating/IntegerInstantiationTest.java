2098512e0197b6714307e49547f009fa174d3318
Tom Copeland <tomcopeland@users.sourceforge.net>
2005-12-01 22:11:14 +0000

    Applied Allan Caplan's IntegerInstantiation new rule patch, thanks Allan!
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@4026 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/rules/migrating/IntegerInstantiationTest.java
@@ -0,0 +1,34 @@
+package test.net.sourceforge.pmd.rules.migrating;
+
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.RuleSetNotFoundException;
+import net.sourceforge.pmd.PMD;
+
+public class IntegerInstantiationTest extends SimpleAggregatorTst {
+
+    private Rule rule;
+
+    public void setUp() throws RuleSetNotFoundException {
+        rule = findRule("migrating", "IntegerInstantiation");
+    }
+
+    public void testAll() {
+       runTests(new TestDescriptor[] {
+           new TestDescriptor(TEST1, "new Integer(), bad", 1, rule),
+           new TestDescriptor(TEST2, "Integer.valueOf(), ok", 0, rule),
+       });
+    }
+
+    private static final String TEST1 =
+    "public class Foo {" + PMD.EOL +
+    "  Integer i = new Integer(42);" + PMD.EOL +
+    "}";
+
+    private static final String TEST2 =
+    "public class Foo {" + PMD.EOL +
+    "  Integer i = Integer.valueOf(\"42\");" + PMD.EOL +
+    "}";
+
+}
