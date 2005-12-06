e36d15c239304ebe91d0fc3751bb5d04c5018752
Tom Copeland <tomcopeland@users.sourceforge.net>
2005-12-06 15:08:12 +0000

    Implemented UseAssertNullInsteadOfAssertTrue; thx to Wim Deblauwe for the suggestion!
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@4039 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/rules/junit/UseAssertNullInsteadOfAssertTrueTest.java
@@ -0,0 +1,46 @@
+package test.net.sourceforge.pmd.rules.junit;
+
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.RuleSetNotFoundException;
+import net.sourceforge.pmd.PMD;
+
+public class UseAssertNullInsteadOfAssertTrueTest extends SimpleAggregatorTst {
+    private Rule rule;
+
+    public void setUp() throws RuleSetNotFoundException {
+        rule = findRule("junit", "UseAssertNullInsteadOfAssertTrue");
+    }
+    public void testAll() throws Throwable {
+
+       runTests(new TestDescriptor[] {
+           new TestDescriptor(TEST1, "assertTrue with null", 1, rule),
+           new TestDescriptor(TEST2, "assertFalse with != null", 1, rule),
+           new TestDescriptor(TEST3, "assertTrue with x == y", 0, rule),
+       });
+    }
+
+    private static final String TEST1 =
+    "public class Foo {" + PMD.EOL +
+    " public void test1() {" + PMD.EOL +
+    "  assertTrue(a==null);" + PMD.EOL +
+    " }" + PMD.EOL +
+    "}";
+
+    private static final String TEST2 =
+    "public class Foo {" + PMD.EOL +
+    " public void test1() {" + PMD.EOL +
+    "  assertFalse(a != null);" + PMD.EOL +
+    " }" + PMD.EOL +
+    "}";
+
+    private static final String TEST3 =
+    "public class Foo {" + PMD.EOL +
+    " public void test1() {" + PMD.EOL +
+    "  assertTrue(a == b);" + PMD.EOL +
+    " }" + PMD.EOL +
+    "}";
+
+}
+
