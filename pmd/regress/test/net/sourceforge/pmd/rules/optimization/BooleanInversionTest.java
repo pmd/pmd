ded50b090f7206a283728ff39ae4a26b881f8fa0
Tom Copeland <tomcopeland@users.sourceforge.net>
2006-01-05 14:48:11 +0000

    Added Allan Caplan's implementation of BooleanInversion; thanks Allan!
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@4115 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/rules/optimization/BooleanInversionTest.java
@@ -0,0 +1,52 @@
+package test.net.sourceforge.pmd.rules.optimization;
+
+import net.sourceforge.pmd.PMD;
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.RuleSetNotFoundException;
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
+
+public class BooleanInversionTest extends SimpleAggregatorTst {
+
+    private Rule rule;
+
+    public void setUp() throws RuleSetNotFoundException {
+        rule = findRule("optimizations", "BooleanInversion");
+    }
+
+    public void testAll() {
+       runTests(new TestDescriptor[] {
+               new TestDescriptor(TEST1, "failure, unary assignment", 1, rule),
+               new TestDescriptor(TEST2, "pass, bitwise assignment", 0, rule),
+               new TestDescriptor(TEST3, "pass, not a straight unary inversion", 0, rule),
+       });
+    }
+
+    private static final String TEST1 =
+        "public class Foo {" + PMD.EOL +
+        " public void bar() {" + PMD.EOL +
+        "   boolean b = false;" + PMD.EOL +
+        "   boolean c = false;" + PMD.EOL +
+        "   b = !c;" + PMD.EOL +
+        " }" + PMD.EOL +
+        "}";
+
+    private static final String TEST2 =
+        "public class Foo {" + PMD.EOL +
+        " public void bar() {" + PMD.EOL +
+        "   boolean b = false;" + PMD.EOL +
+        "   boolean c = false;" + PMD.EOL +
+        "   b ^= c;" + PMD.EOL +
+        " }" + PMD.EOL +
+        "}";
+
+    private static final String TEST3 =
+        "public class Foo {" + PMD.EOL +
+        " public void bar() {" + PMD.EOL +
+        "   boolean b = false;" + PMD.EOL +
+        "   boolean c = false;" + PMD.EOL +
+        "   b &= !c;" + PMD.EOL +
+        " }" + PMD.EOL +
+        "}";
+
+}
