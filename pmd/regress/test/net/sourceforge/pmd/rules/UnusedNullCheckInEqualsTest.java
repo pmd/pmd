1b6991153f855a3d666ae2593aef3c25f372d6c2
Tom Copeland <tomcopeland@users.sourceforge.net>
2006-01-07 01:28:09 +0000

    Checking in Fabio Insaccanebbia's UnusedNullCheckInEquals rule, thanks Fabio!
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@4124 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/rules/UnusedNullCheckInEqualsTest.java
@@ -0,0 +1,55 @@
+package test.net.sourceforge.pmd.rules;
+
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.RuleSetNotFoundException;
+import net.sourceforge.pmd.PMD;
+
+public class UnusedNullCheckInEqualsTest extends SimpleAggregatorTst {
+
+    private Rule rule;
+
+    public void setUp() throws RuleSetNotFoundException {
+        rule = findRule("basic", "UnusedNullCheckInEquals");
+    }
+
+    public void testAll() {
+        System.out.println(TEST4);
+       runTests(new TestDescriptor[] {
+           new TestDescriptor(TEST1, "failure case", 1, rule),
+           new TestDescriptor(TEST2, "different var, 'tis ok", 0, rule),
+           new TestDescriptor(TEST3, "proper usage", 0, rule),
+           //new TestDescriptor(TEST4, "shouldn't this fail?", 1, rule),
+       });
+    }
+
+    private static final String TEST1 =
+    "public class Foo {" + PMD.EOL +
+    " public void bar() {" + PMD.EOL +
+    "  if (x != null && foo.getBar().equals(x)) {} " + PMD.EOL +
+    " }" + PMD.EOL +
+    "}";
+
+    private static final String TEST2 =
+    "public class Foo {" + PMD.EOL +
+    " public void bar() {" + PMD.EOL +
+    "  if (x != null && foo.equals(y)) {} " + PMD.EOL +
+    " }" + PMD.EOL +
+    "}";
+
+    private static final String TEST3 =
+    "public class Foo {" + PMD.EOL +
+    " public void bar() {" + PMD.EOL +
+    "  if (x != null && x.equals(y)) {} " + PMD.EOL +
+    " }" + PMD.EOL +
+    "}";
+
+    private static final String TEST4 =
+    "public class Foo {" + PMD.EOL +
+    " public void bar() {" + PMD.EOL +
+    "  if (x != null && y.equals(x)) {} " + PMD.EOL +
+    " }" + PMD.EOL +
+    "}";
+
+}
