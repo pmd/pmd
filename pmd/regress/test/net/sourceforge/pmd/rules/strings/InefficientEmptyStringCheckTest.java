709988e74e52dadb8a75c18fdc7c86230c3f91d9
Allan Caplan <allancaplan@users.sourceforge.net>
2006-02-03 01:04:18 +0000

    This is the rule proposed by Brian in http://sourceforge.net/forum/forum.php?thread_id=1431090&forum_id=188194
    This code finds any case where
    
    
    string.trim().length()==0
    
    comparison is made. Code only looks for 0 specifically,
    so if a static instance variable is used to specify 0
    it's ignored.
    
    
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@4190 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/rules/strings/InefficientEmptyStringCheckTest.java
@@ -0,0 +1,114 @@
+/**
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+package test.net.sourceforge.pmd.rules.strings;
+
+import net.sourceforge.pmd.PMD;
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.rules.strings.InefficientEmptyStringCheck;
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
+
+public class InefficientEmptyStringCheckTest extends SimpleAggregatorTst {
+
+    private Rule rule;
+
+    public void setUp() throws Exception {
+        rule = findRule("strings", "InefficientEmptyStringCheck");
+    }
+
+    public void testAll() {
+        
+        runTests(new TestDescriptor[] { new TestDescriptor(TEST1, "test is ok, ok", 0, rule),
+                new TestDescriptor(TEST2, "String.trim.length is called, should have failed", 1, rule),
+                new TestDescriptor(TEST3, "String.trim.length not is called, ok", 0, rule),
+                new TestDescriptor(TEST4, "String.trim.length is called, should have failed", 1, rule),
+                new TestDescriptor(TEST5, "String.trim.length is called, assigned to int, ok", 0, rule),
+                new TestDescriptor(TEST6, "String.trim.length is called, assigned to boolean, should have failed", 1, rule),
+                new TestDescriptor(TEST7, "Using trim.length to find the length and compare to 1, OK", 0, rule),
+                new TestDescriptor(TEST8, "Passes trim().length() and 0 to another method", 0, rule),
+                new TestDescriptor(TEST9, "Compares the length against a mathematical function", 0, rule),
+        });
+    }
+
+    private static final String TEST1 = 
+        "public class Foo {" + PMD.EOL + 
+        "    void bar() {" + PMD.EOL + 
+        "        String foo = \"foo\";" + PMD.EOL + 
+        "        if(foo.length() == 0) {" + PMD.EOL + 
+        "        // this is bad" + PMD.EOL + 
+        "        }" + PMD.EOL + 
+        "    }" + PMD.EOL + 
+        "}";
+
+    private static final String TEST2 = 
+        "public class Foo {" + PMD.EOL + 
+        "    void bar() {" + PMD.EOL + 
+        "        String foo = \"foo\";" + PMD.EOL + 
+        "        if(foo.trim().length() == 0) {" + PMD.EOL + 
+        "        // this is bad" + PMD.EOL + 
+        "        }" + PMD.EOL + 
+        "    }" + PMD.EOL + 
+        "}";
+
+    private static final String TEST3 = 
+        "public class Foo {" + PMD.EOL + 
+        "    void bar() {" + PMD.EOL + 
+        "        String foo = \"foo\";" + PMD.EOL + 
+        "        if(foo.trim().equals(\"\")) {" + PMD.EOL + 
+        "        }" + PMD.EOL + 
+        "    }" + PMD.EOL + 
+        "}";
+
+
+    private static final String TEST4 = 
+        "public class Foo {" + PMD.EOL + 
+        "    void bar() {" + PMD.EOL + 
+        "        String foo = \"foo\";" + PMD.EOL + 
+        "        while(foo.trim().length()==0) {" + PMD.EOL + 
+        "        }" + PMD.EOL + 
+        "    }" + PMD.EOL + 
+        "}";
+
+    private static final String TEST5 = 
+        "public class Foo {" + PMD.EOL + 
+        "    void bar() {" + PMD.EOL + 
+        "        String foo = \"foo\";" + PMD.EOL + 
+        "        int i = foo.trim().length();" + PMD.EOL + 
+        "    }" + PMD.EOL + 
+        "}";
+
+    private static final String TEST6 = 
+        "public class Foo {" + PMD.EOL + 
+        "    void bar() {" + PMD.EOL + 
+        "        String foo = \"foo\";" + PMD.EOL + 
+        "        boolean b = foo.trim().length()==0;" + PMD.EOL + 
+        "    }" + PMD.EOL + 
+        "}";
+
+    private static final String TEST7 = 
+        "public class Foo {" + PMD.EOL + 
+        "    void bar() {" + PMD.EOL + 
+        "        String foo = \"foo\";" + PMD.EOL + 
+        "        boolean b = foo.trim().length()==1;" + PMD.EOL + 
+        "    }" + PMD.EOL + 
+        "}";
+
+    private static final String TEST8 = 
+        "public class Foo {" + PMD.EOL + 
+        "    void bar() {" + PMD.EOL + 
+        "        String foo = \"foo\";" + PMD.EOL + 
+        "        boolean b = foo(foo.trim().length(),0);" + PMD.EOL + 
+        "    }" + PMD.EOL + 
+        "}";
+
+    private static final String TEST9 = 
+        "public class Foo {" + PMD.EOL + 
+        "    void bar() {" + PMD.EOL + 
+        "        String foo = \"foo\";" + PMD.EOL + 
+        "        boolean b = foo(foo.trim().length()==(2-1));" + PMD.EOL + 
+        "    }" + PMD.EOL + 
+        "}";
+
+
+}
---
+++ pmd/src/net/sourceforge/pmd/rules/strings/InefficientEmptyStringCheck.java
@@ -0,0 +1,118 @@
+package net.sourceforge.pmd.rules.strings;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import net.sourceforge.pmd.AbstractRule;
+import net.sourceforge.pmd.ast.ASTEqualityExpression;
+import net.sourceforge.pmd.ast.ASTLiteral;
+import net.sourceforge.pmd.ast.ASTPrimitiveType;
+import net.sourceforge.pmd.ast.ASTVariableDeclaratorId;
+import net.sourceforge.pmd.ast.Node;
+import net.sourceforge.pmd.ast.SimpleNode;
+import net.sourceforge.pmd.symboltable.NameOccurrence;
+import net.sourceforge.pmd.symboltable.VariableNameDeclaration;
+
+/**
+ * This rule finds code which inefficiently determines empty strings. This code
+ * 
+ * <pre>
+ *     if(str.trim().length()==0){....
+ * </pre>
+ * 
+ * is quite inefficient as trim() causes a new String to be created. Smarter
+ * code to check for an empty string would be:
+ * 
+ * <pre>
+ * Character.isWhitespace(str.charAt(i));
+ * </pre>
+ * 
+ * @author acaplan
+ * 
+ */
+public class InefficientEmptyStringCheck extends AbstractRule {
+
+    public Object visit(ASTVariableDeclaratorId node, Object data) {
+        SimpleNode nameNode = node.getTypeNameNode();
+        if (nameNode instanceof ASTPrimitiveType) {
+            return data;
+        }
+
+        if (!"String".equals(node.getNameDeclaration().getTypeImage())) {
+            return data;
+        }
+
+        Map mapDeclars = node.getScope().getVariableDeclarations();
+        for (Iterator i = mapDeclars.keySet().iterator(); i.hasNext();) {
+            VariableNameDeclaration varNameDecl = (VariableNameDeclaration) i
+                    .next();
+            if (!varNameDecl.getImage().equals(node.getImage())) {
+                continue;
+            }
+            List usage = (List) mapDeclars.get(varNameDecl);
+            for (Iterator j = usage.iterator(); j.hasNext();) {
+                NameOccurrence occ = (NameOccurrence) j.next();
+                if (!isStringLength(occ)) {
+                    continue;
+                }
+                ASTEqualityExpression equality = (ASTEqualityExpression) occ
+                        .getLocation().getFirstParentOfType(
+                                ASTEqualityExpression.class);
+                if (equality != null && isCompareZero(equality)) {
+                    addViolation(data, occ.getLocation());
+                }
+            }
+        }
+        return data;
+    }
+
+    /**
+     * We only need to report if this is comparing against 0
+     * 
+     * @param equality
+     * @return true if this is comparing to 0 else false
+     */
+    private boolean isCompareZero(ASTEqualityExpression equality) {
+        return (checkComparison(equality, 0) || checkComparison(equality, 1));
+
+    }
+
+    /**
+     * Determine if we're dealing with String.length method
+     * 
+     * @param occ
+     *            The name occurance
+     * @return true if it's String.length, else false
+     */
+    private boolean isStringLength(NameOccurrence occ) {
+        if (occ.getNameForWhichThisIsAQualifier() != null
+                && occ.getNameForWhichThisIsAQualifier().getImage().indexOf(
+                        "trim") != -1) {
+            Node pExpression = occ.getLocation().jjtGetParent().jjtGetParent();
+            if (pExpression.jjtGetNumChildren() >= 3
+                    && "length"
+                            .equals(((SimpleNode) pExpression.jjtGetChild(2))
+                                    .getImage())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Checks if the equality expression passed in is of comparing against the
+     * value passed in as i
+     * 
+     * @param equality
+     * @param i
+     *            The ordinal in the equality expression to check
+     * @return true if the value in position i is 0, else false
+     */
+    private boolean checkComparison(ASTEqualityExpression equality, int i) {
+        return (equality.jjtGetChild(i).jjtGetChild(0).jjtGetChild(0) instanceof ASTLiteral && "0"
+                .equals(((SimpleNode) equality.jjtGetChild(i).jjtGetChild(0)
+                        .jjtGetChild(0)).getImage()));
+    }
+
+}
