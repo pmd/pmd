ac80c6705a48ca434ec8268bcb94a5a71a21d88c
Tom Copeland <tomcopeland@users.sourceforge.net>
2006-02-11 03:35:07 +0000

    Checking in latest generated grammar
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@4221 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/net/sourceforge/pmd/ast/JavaParserConstants.java
+++ pmd/src/net/sourceforge/pmd/ast/JavaParserConstants.java
@@ -3,250 +3,250 @@ package net.sourceforge.pmd.ast;
 
 public interface JavaParserConstants {
 
-    int EOF = 0;
-    int SINGLE_LINE_COMMENT = 6;
-    int FORMAL_COMMENT = 9;
-    int MULTI_LINE_COMMENT = 10;
-    int ABSTRACT = 12;
-    int BOOLEAN = 13;
-    int BREAK = 14;
-    int BYTE = 15;
-    int CASE = 16;
-    int CATCH = 17;
-    int CHAR = 18;
-    int CLASS = 19;
-    int CONST = 20;
-    int CONTINUE = 21;
-    int _DEFAULT = 22;
-    int DO = 23;
-    int DOUBLE = 24;
-    int ELSE = 25;
-    int EXTENDS = 26;
-    int FALSE = 27;
-    int FINAL = 28;
-    int FINALLY = 29;
-    int FLOAT = 30;
-    int FOR = 31;
-    int GOTO = 32;
-    int IF = 33;
-    int IMPLEMENTS = 34;
-    int IMPORT = 35;
-    int INSTANCEOF = 36;
-    int INT = 37;
-    int INTERFACE = 38;
-    int LONG = 39;
-    int NATIVE = 40;
-    int NEW = 41;
-    int NULL = 42;
-    int PACKAGE = 43;
-    int PRIVATE = 44;
-    int PROTECTED = 45;
-    int PUBLIC = 46;
-    int RETURN = 47;
-    int SHORT = 48;
-    int STATIC = 49;
-    int SUPER = 50;
-    int SWITCH = 51;
-    int SYNCHRONIZED = 52;
-    int THIS = 53;
-    int THROW = 54;
-    int THROWS = 55;
-    int TRANSIENT = 56;
-    int TRUE = 57;
-    int TRY = 58;
-    int VOID = 59;
-    int VOLATILE = 60;
-    int WHILE = 61;
-    int STRICTFP = 62;
-    int INTEGER_LITERAL = 63;
-    int DECIMAL_LITERAL = 64;
-    int HEX_LITERAL = 65;
-    int OCTAL_LITERAL = 66;
-    int FLOATING_POINT_LITERAL = 67;
-    int EXPONENT = 68;
-    int CHARACTER_LITERAL = 69;
-    int STRING_LITERAL = 70;
-    int IDENTIFIER = 71;
-    int LETTER = 72;
-    int PART_LETTER = 73;
-    int LPAREN = 74;
-    int RPAREN = 75;
-    int LBRACE = 76;
-    int RBRACE = 77;
-    int LBRACKET = 78;
-    int RBRACKET = 79;
-    int SEMICOLON = 80;
-    int COMMA = 81;
-    int DOT = 82;
-    int AT = 83;
-    int ASSIGN = 84;
-    int LT = 85;
-    int BANG = 86;
-    int TILDE = 87;
-    int HOOK = 88;
-    int COLON = 89;
-    int EQ = 90;
-    int LE = 91;
-    int GE = 92;
-    int NE = 93;
-    int SC_OR = 94;
-    int SC_AND = 95;
-    int INCR = 96;
-    int DECR = 97;
-    int PLUS = 98;
-    int MINUS = 99;
-    int STAR = 100;
-    int SLASH = 101;
-    int BIT_AND = 102;
-    int BIT_OR = 103;
-    int XOR = 104;
-    int REM = 105;
-    int LSHIFT = 106;
-    int PLUSASSIGN = 107;
-    int MINUSASSIGN = 108;
-    int STARASSIGN = 109;
-    int SLASHASSIGN = 110;
-    int ANDASSIGN = 111;
-    int ORASSIGN = 112;
-    int XORASSIGN = 113;
-    int REMASSIGN = 114;
-    int LSHIFTASSIGN = 115;
-    int RSIGNEDSHIFTASSIGN = 116;
-    int RUNSIGNEDSHIFTASSIGN = 117;
-    int ELLIPSIS = 118;
-    int RUNSIGNEDSHIFT = 119;
-    int RSIGNEDSHIFT = 120;
-    int GT = 121;
+  int EOF = 0;
+  int SINGLE_LINE_COMMENT = 6;
+  int FORMAL_COMMENT = 9;
+  int MULTI_LINE_COMMENT = 10;
+  int ABSTRACT = 12;
+  int BOOLEAN = 13;
+  int BREAK = 14;
+  int BYTE = 15;
+  int CASE = 16;
+  int CATCH = 17;
+  int CHAR = 18;
+  int CLASS = 19;
+  int CONST = 20;
+  int CONTINUE = 21;
+  int _DEFAULT = 22;
+  int DO = 23;
+  int DOUBLE = 24;
+  int ELSE = 25;
+  int EXTENDS = 26;
+  int FALSE = 27;
+  int FINAL = 28;
+  int FINALLY = 29;
+  int FLOAT = 30;
+  int FOR = 31;
+  int GOTO = 32;
+  int IF = 33;
+  int IMPLEMENTS = 34;
+  int IMPORT = 35;
+  int INSTANCEOF = 36;
+  int INT = 37;
+  int INTERFACE = 38;
+  int LONG = 39;
+  int NATIVE = 40;
+  int NEW = 41;
+  int NULL = 42;
+  int PACKAGE = 43;
+  int PRIVATE = 44;
+  int PROTECTED = 45;
+  int PUBLIC = 46;
+  int RETURN = 47;
+  int SHORT = 48;
+  int STATIC = 49;
+  int SUPER = 50;
+  int SWITCH = 51;
+  int SYNCHRONIZED = 52;
+  int THIS = 53;
+  int THROW = 54;
+  int THROWS = 55;
+  int TRANSIENT = 56;
+  int TRUE = 57;
+  int TRY = 58;
+  int VOID = 59;
+  int VOLATILE = 60;
+  int WHILE = 61;
+  int STRICTFP = 62;
+  int INTEGER_LITERAL = 63;
+  int DECIMAL_LITERAL = 64;
+  int HEX_LITERAL = 65;
+  int OCTAL_LITERAL = 66;
+  int FLOATING_POINT_LITERAL = 67;
+  int EXPONENT = 68;
+  int CHARACTER_LITERAL = 69;
+  int STRING_LITERAL = 70;
+  int IDENTIFIER = 71;
+  int LETTER = 72;
+  int PART_LETTER = 73;
+  int LPAREN = 74;
+  int RPAREN = 75;
+  int LBRACE = 76;
+  int RBRACE = 77;
+  int LBRACKET = 78;
+  int RBRACKET = 79;
+  int SEMICOLON = 80;
+  int COMMA = 81;
+  int DOT = 82;
+  int AT = 83;
+  int ASSIGN = 84;
+  int LT = 85;
+  int BANG = 86;
+  int TILDE = 87;
+  int HOOK = 88;
+  int COLON = 89;
+  int EQ = 90;
+  int LE = 91;
+  int GE = 92;
+  int NE = 93;
+  int SC_OR = 94;
+  int SC_AND = 95;
+  int INCR = 96;
+  int DECR = 97;
+  int PLUS = 98;
+  int MINUS = 99;
+  int STAR = 100;
+  int SLASH = 101;
+  int BIT_AND = 102;
+  int BIT_OR = 103;
+  int XOR = 104;
+  int REM = 105;
+  int LSHIFT = 106;
+  int PLUSASSIGN = 107;
+  int MINUSASSIGN = 108;
+  int STARASSIGN = 109;
+  int SLASHASSIGN = 110;
+  int ANDASSIGN = 111;
+  int ORASSIGN = 112;
+  int XORASSIGN = 113;
+  int REMASSIGN = 114;
+  int LSHIFTASSIGN = 115;
+  int RSIGNEDSHIFTASSIGN = 116;
+  int RUNSIGNEDSHIFTASSIGN = 117;
+  int ELLIPSIS = 118;
+  int RUNSIGNEDSHIFT = 119;
+  int RSIGNEDSHIFT = 120;
+  int GT = 121;
 
-    int DEFAULT = 0;
-    int IN_FORMAL_COMMENT = 1;
-    int IN_MULTI_LINE_COMMENT = 2;
+  int DEFAULT = 0;
+  int IN_FORMAL_COMMENT = 1;
+  int IN_MULTI_LINE_COMMENT = 2;
 
-    String[] tokenImage = {
-        "<EOF>",
-        "\" \"",
-        "\"\\t\"",
-        "\"\\n\"",
-        "\"\\r\"",
-        "\"\\f\"",
-        "<SINGLE_LINE_COMMENT>",
-        "<token of kind 7>",
-        "\"/*\"",
-        "\"*/\"",
-        "\"*/\"",
-        "<token of kind 11>",
-        "\"abstract\"",
-        "\"boolean\"",
-        "\"break\"",
-        "\"byte\"",
-        "\"case\"",
-        "\"catch\"",
-        "\"char\"",
-        "\"class\"",
-        "\"const\"",
-        "\"continue\"",
-        "\"default\"",
-        "\"do\"",
-        "\"double\"",
-        "\"else\"",
-        "\"extends\"",
-        "\"false\"",
-        "\"final\"",
-        "\"finally\"",
-        "\"float\"",
-        "\"for\"",
-        "\"goto\"",
-        "\"if\"",
-        "\"implements\"",
-        "\"import\"",
-        "\"instanceof\"",
-        "\"int\"",
-        "\"interface\"",
-        "\"long\"",
-        "\"native\"",
-        "\"new\"",
-        "\"null\"",
-        "\"package\"",
-        "\"private\"",
-        "\"protected\"",
-        "\"public\"",
-        "\"return\"",
-        "\"short\"",
-        "\"static\"",
-        "\"super\"",
-        "\"switch\"",
-        "\"synchronized\"",
-        "\"this\"",
-        "\"throw\"",
-        "\"throws\"",
-        "\"transient\"",
-        "\"true\"",
-        "\"try\"",
-        "\"void\"",
-        "\"volatile\"",
-        "\"while\"",
-        "\"strictfp\"",
-        "<INTEGER_LITERAL>",
-        "<DECIMAL_LITERAL>",
-        "<HEX_LITERAL>",
-        "<OCTAL_LITERAL>",
-        "<FLOATING_POINT_LITERAL>",
-        "<EXPONENT>",
-        "<CHARACTER_LITERAL>",
-        "<STRING_LITERAL>",
-        "<IDENTIFIER>",
-        "<LETTER>",
-        "<PART_LETTER>",
-        "\"(\"",
-        "\")\"",
-        "\"{\"",
-        "\"}\"",
-        "\"[\"",
-        "\"]\"",
-        "\";\"",
-        "\",\"",
-        "\".\"",
-        "\"@\"",
-        "\"=\"",
-        "\"<\"",
-        "\"!\"",
-        "\"~\"",
-        "\"?\"",
-        "\":\"",
-        "\"==\"",
-        "\"<=\"",
-        "\">=\"",
-        "\"!=\"",
-        "\"||\"",
-        "\"&&\"",
-        "\"++\"",
-        "\"--\"",
-        "\"+\"",
-        "\"-\"",
-        "\"*\"",
-        "\"/\"",
-        "\"&\"",
-        "\"|\"",
-        "\"^\"",
-        "\"%\"",
-        "\"<<\"",
-        "\"+=\"",
-        "\"-=\"",
-        "\"*=\"",
-        "\"/=\"",
-        "\"&=\"",
-        "\"|=\"",
-        "\"^=\"",
-        "\"%=\"",
-        "\"<<=\"",
-        "\">>=\"",
-        "\">>>=\"",
-        "\"...\"",
-        "\">>>\"",
-        "\">>\"",
-        "\">\"",
-        "\"\\u001a\"",
-        "\"~[]\"",
-    };
+  String[] tokenImage = {
+    "<EOF>",
+    "\" \"",
+    "\"\\t\"",
+    "\"\\n\"",
+    "\"\\r\"",
+    "\"\\f\"",
+    "<SINGLE_LINE_COMMENT>",
+    "<token of kind 7>",
+    "\"/*\"",
+    "\"*/\"",
+    "\"*/\"",
+    "<token of kind 11>",
+    "\"abstract\"",
+    "\"boolean\"",
+    "\"break\"",
+    "\"byte\"",
+    "\"case\"",
+    "\"catch\"",
+    "\"char\"",
+    "\"class\"",
+    "\"const\"",
+    "\"continue\"",
+    "\"default\"",
+    "\"do\"",
+    "\"double\"",
+    "\"else\"",
+    "\"extends\"",
+    "\"false\"",
+    "\"final\"",
+    "\"finally\"",
+    "\"float\"",
+    "\"for\"",
+    "\"goto\"",
+    "\"if\"",
+    "\"implements\"",
+    "\"import\"",
+    "\"instanceof\"",
+    "\"int\"",
+    "\"interface\"",
+    "\"long\"",
+    "\"native\"",
+    "\"new\"",
+    "\"null\"",
+    "\"package\"",
+    "\"private\"",
+    "\"protected\"",
+    "\"public\"",
+    "\"return\"",
+    "\"short\"",
+    "\"static\"",
+    "\"super\"",
+    "\"switch\"",
+    "\"synchronized\"",
+    "\"this\"",
+    "\"throw\"",
+    "\"throws\"",
+    "\"transient\"",
+    "\"true\"",
+    "\"try\"",
+    "\"void\"",
+    "\"volatile\"",
+    "\"while\"",
+    "\"strictfp\"",
+    "<INTEGER_LITERAL>",
+    "<DECIMAL_LITERAL>",
+    "<HEX_LITERAL>",
+    "<OCTAL_LITERAL>",
+    "<FLOATING_POINT_LITERAL>",
+    "<EXPONENT>",
+    "<CHARACTER_LITERAL>",
+    "<STRING_LITERAL>",
+    "<IDENTIFIER>",
+    "<LETTER>",
+    "<PART_LETTER>",
+    "\"(\"",
+    "\")\"",
+    "\"{\"",
+    "\"}\"",
+    "\"[\"",
+    "\"]\"",
+    "\";\"",
+    "\",\"",
+    "\".\"",
+    "\"@\"",
+    "\"=\"",
+    "\"<\"",
+    "\"!\"",
+    "\"~\"",
+    "\"?\"",
+    "\":\"",
+    "\"==\"",
+    "\"<=\"",
+    "\">=\"",
+    "\"!=\"",
+    "\"||\"",
+    "\"&&\"",
+    "\"++\"",
+    "\"--\"",
+    "\"+\"",
+    "\"-\"",
+    "\"*\"",
+    "\"/\"",
+    "\"&\"",
+    "\"|\"",
+    "\"^\"",
+    "\"%\"",
+    "\"<<\"",
+    "\"+=\"",
+    "\"-=\"",
+    "\"*=\"",
+    "\"/=\"",
+    "\"&=\"",
+    "\"|=\"",
+    "\"^=\"",
+    "\"%=\"",
+    "\"<<=\"",
+    "\">>=\"",
+    "\">>>=\"",
+    "\"...\"",
+    "\">>>\"",
+    "\">>\"",
+    "\">\"",
+    "\"\\u001a\"",
+    "\"~[]\"",
+  };
 
 }
--- pmd/src/net/sourceforge/pmd/ast/JavaParserTreeConstants.java
+++ pmd/src/net/sourceforge/pmd/ast/JavaParserTreeConstants.java
@@ -2,227 +2,228 @@
 
 package net.sourceforge.pmd.ast;
 
-public interface JavaParserTreeConstants {
-    public int JJTCOMPILATIONUNIT = 0;
-    public int JJTPACKAGEDECLARATION = 1;
-    public int JJTIMPORTDECLARATION = 2;
-    public int JJTMODIFIERS = 3;
-    public int JJTTYPEDECLARATION = 4;
-    public int JJTCLASSORINTERFACEDECLARATION = 5;
-    public int JJTEXTENDSLIST = 6;
-    public int JJTIMPLEMENTSLIST = 7;
-    public int JJTENUMDECLARATION = 8;
-    public int JJTENUMBODY = 9;
-    public int JJTENUMCONSTANT = 10;
-    public int JJTTYPEPARAMETERS = 11;
-    public int JJTTYPEPARAMETER = 12;
-    public int JJTTYPEBOUND = 13;
-    public int JJTCLASSORINTERFACEBODY = 14;
-    public int JJTCLASSORINTERFACEBODYDECLARATION = 15;
-    public int JJTFIELDDECLARATION = 16;
-    public int JJTVARIABLEDECLARATOR = 17;
-    public int JJTVARIABLEDECLARATORID = 18;
-    public int JJTVARIABLEINITIALIZER = 19;
-    public int JJTARRAYINITIALIZER = 20;
-    public int JJTMETHODDECLARATION = 21;
-    public int JJTMETHODDECLARATOR = 22;
-    public int JJTFORMALPARAMETERS = 23;
-    public int JJTFORMALPARAMETER = 24;
-    public int JJTCONSTRUCTORDECLARATION = 25;
-    public int JJTEXPLICITCONSTRUCTORINVOCATION = 26;
-    public int JJTINITIALIZER = 27;
-    public int JJTTYPE = 28;
-    public int JJTREFERENCETYPE = 29;
-    public int JJTCLASSORINTERFACETYPE = 30;
-    public int JJTTYPEARGUMENTS = 31;
-    public int JJTTYPEARGUMENT = 32;
-    public int JJTWILDCARDBOUNDS = 33;
-    public int JJTPRIMITIVETYPE = 34;
-    public int JJTRESULTTYPE = 35;
-    public int JJTNAME = 36;
-    public int JJTNAMELIST = 37;
-    public int JJTEXPRESSION = 38;
-    public int JJTASSIGNMENTOPERATOR = 39;
-    public int JJTCONDITIONALEXPRESSION = 40;
-    public int JJTCONDITIONALOREXPRESSION = 41;
-    public int JJTCONDITIONALANDEXPRESSION = 42;
-    public int JJTINCLUSIVEOREXPRESSION = 43;
-    public int JJTEXCLUSIVEOREXPRESSION = 44;
-    public int JJTANDEXPRESSION = 45;
-    public int JJTEQUALITYEXPRESSION = 46;
-    public int JJTINSTANCEOFEXPRESSION = 47;
-    public int JJTRELATIONALEXPRESSION = 48;
-    public int JJTSHIFTEXPRESSION = 49;
-    public int JJTADDITIVEEXPRESSION = 50;
-    public int JJTMULTIPLICATIVEEXPRESSION = 51;
-    public int JJTUNARYEXPRESSION = 52;
-    public int JJTPREINCREMENTEXPRESSION = 53;
-    public int JJTPREDECREMENTEXPRESSION = 54;
-    public int JJTUNARYEXPRESSIONNOTPLUSMINUS = 55;
-    public int JJTCASTLOOKAHEAD = 56;
-    public int JJTPOSTFIXEXPRESSION = 57;
-    public int JJTCASTEXPRESSION = 58;
-    public int JJTPRIMARYEXPRESSION = 59;
-    public int JJTMEMBERSELECTOR = 60;
-    public int JJTPRIMARYPREFIX = 61;
-    public int JJTPRIMARYSUFFIX = 62;
-    public int JJTLITERAL = 63;
-    public int JJTBOOLEANLITERAL = 64;
-    public int JJTNULLLITERAL = 65;
-    public int JJTARGUMENTS = 66;
-    public int JJTARGUMENTLIST = 67;
-    public int JJTALLOCATIONEXPRESSION = 68;
-    public int JJTARRAYDIMSANDINITS = 69;
-    public int JJTSTATEMENT = 70;
-    public int JJTLABELEDSTATEMENT = 71;
-    public int JJTBLOCK = 72;
-    public int JJTBLOCKSTATEMENT = 73;
-    public int JJTLOCALVARIABLEDECLARATION = 74;
-    public int JJTEMPTYSTATEMENT = 75;
-    public int JJTSTATEMENTEXPRESSION = 76;
-    public int JJTSWITCHSTATEMENT = 77;
-    public int JJTSWITCHLABEL = 78;
-    public int JJTIFSTATEMENT = 79;
-    public int JJTWHILESTATEMENT = 80;
-    public int JJTDOSTATEMENT = 81;
-    public int JJTFORSTATEMENT = 82;
-    public int JJTFORINIT = 83;
-    public int JJTSTATEMENTEXPRESSIONLIST = 84;
-    public int JJTFORUPDATE = 85;
-    public int JJTBREAKSTATEMENT = 86;
-    public int JJTCONTINUESTATEMENT = 87;
-    public int JJTRETURNSTATEMENT = 88;
-    public int JJTTHROWSTATEMENT = 89;
-    public int JJTSYNCHRONIZEDSTATEMENT = 90;
-    public int JJTTRYSTATEMENT = 91;
-    public int JJTCATCHSTATEMENT = 92;
-    public int JJTFINALLYSTATEMENT = 93;
-    public int JJTASSERTSTATEMENT = 94;
-    public int JJTRUNSIGNEDSHIFT = 95;
-    public int JJTRSIGNEDSHIFT = 96;
-    public int JJTANNOTATION = 97;
-    public int JJTNORMALANNOTATION = 98;
-    public int JJTMARKERANNOTATION = 99;
-    public int JJTSINGLEMEMBERANNOTATION = 100;
-    public int JJTMEMBERVALUEPAIRS = 101;
-    public int JJTMEMBERVALUEPAIR = 102;
-    public int JJTMEMBERVALUE = 103;
-    public int JJTMEMBERVALUEARRAYINITIALIZER = 104;
-    public int JJTANNOTATIONTYPEDECLARATION = 105;
-    public int JJTANNOTATIONTYPEBODY = 106;
-    public int JJTANNOTATIONTYPEMEMBERDECLARATION = 107;
-    public int JJTDEFAULTVALUE = 108;
+public interface JavaParserTreeConstants
+{
+  public int JJTCOMPILATIONUNIT = 0;
+  public int JJTPACKAGEDECLARATION = 1;
+  public int JJTIMPORTDECLARATION = 2;
+  public int JJTMODIFIERS = 3;
+  public int JJTTYPEDECLARATION = 4;
+  public int JJTCLASSORINTERFACEDECLARATION = 5;
+  public int JJTEXTENDSLIST = 6;
+  public int JJTIMPLEMENTSLIST = 7;
+  public int JJTENUMDECLARATION = 8;
+  public int JJTENUMBODY = 9;
+  public int JJTENUMCONSTANT = 10;
+  public int JJTTYPEPARAMETERS = 11;
+  public int JJTTYPEPARAMETER = 12;
+  public int JJTTYPEBOUND = 13;
+  public int JJTCLASSORINTERFACEBODY = 14;
+  public int JJTCLASSORINTERFACEBODYDECLARATION = 15;
+  public int JJTFIELDDECLARATION = 16;
+  public int JJTVARIABLEDECLARATOR = 17;
+  public int JJTVARIABLEDECLARATORID = 18;
+  public int JJTVARIABLEINITIALIZER = 19;
+  public int JJTARRAYINITIALIZER = 20;
+  public int JJTMETHODDECLARATION = 21;
+  public int JJTMETHODDECLARATOR = 22;
+  public int JJTFORMALPARAMETERS = 23;
+  public int JJTFORMALPARAMETER = 24;
+  public int JJTCONSTRUCTORDECLARATION = 25;
+  public int JJTEXPLICITCONSTRUCTORINVOCATION = 26;
+  public int JJTINITIALIZER = 27;
+  public int JJTTYPE = 28;
+  public int JJTREFERENCETYPE = 29;
+  public int JJTCLASSORINTERFACETYPE = 30;
+  public int JJTTYPEARGUMENTS = 31;
+  public int JJTTYPEARGUMENT = 32;
+  public int JJTWILDCARDBOUNDS = 33;
+  public int JJTPRIMITIVETYPE = 34;
+  public int JJTRESULTTYPE = 35;
+  public int JJTNAME = 36;
+  public int JJTNAMELIST = 37;
+  public int JJTEXPRESSION = 38;
+  public int JJTASSIGNMENTOPERATOR = 39;
+  public int JJTCONDITIONALEXPRESSION = 40;
+  public int JJTCONDITIONALOREXPRESSION = 41;
+  public int JJTCONDITIONALANDEXPRESSION = 42;
+  public int JJTINCLUSIVEOREXPRESSION = 43;
+  public int JJTEXCLUSIVEOREXPRESSION = 44;
+  public int JJTANDEXPRESSION = 45;
+  public int JJTEQUALITYEXPRESSION = 46;
+  public int JJTINSTANCEOFEXPRESSION = 47;
+  public int JJTRELATIONALEXPRESSION = 48;
+  public int JJTSHIFTEXPRESSION = 49;
+  public int JJTADDITIVEEXPRESSION = 50;
+  public int JJTMULTIPLICATIVEEXPRESSION = 51;
+  public int JJTUNARYEXPRESSION = 52;
+  public int JJTPREINCREMENTEXPRESSION = 53;
+  public int JJTPREDECREMENTEXPRESSION = 54;
+  public int JJTUNARYEXPRESSIONNOTPLUSMINUS = 55;
+  public int JJTCASTLOOKAHEAD = 56;
+  public int JJTPOSTFIXEXPRESSION = 57;
+  public int JJTCASTEXPRESSION = 58;
+  public int JJTPRIMARYEXPRESSION = 59;
+  public int JJTMEMBERSELECTOR = 60;
+  public int JJTPRIMARYPREFIX = 61;
+  public int JJTPRIMARYSUFFIX = 62;
+  public int JJTLITERAL = 63;
+  public int JJTBOOLEANLITERAL = 64;
+  public int JJTNULLLITERAL = 65;
+  public int JJTARGUMENTS = 66;
+  public int JJTARGUMENTLIST = 67;
+  public int JJTALLOCATIONEXPRESSION = 68;
+  public int JJTARRAYDIMSANDINITS = 69;
+  public int JJTSTATEMENT = 70;
+  public int JJTLABELEDSTATEMENT = 71;
+  public int JJTBLOCK = 72;
+  public int JJTBLOCKSTATEMENT = 73;
+  public int JJTLOCALVARIABLEDECLARATION = 74;
+  public int JJTEMPTYSTATEMENT = 75;
+  public int JJTSTATEMENTEXPRESSION = 76;
+  public int JJTSWITCHSTATEMENT = 77;
+  public int JJTSWITCHLABEL = 78;
+  public int JJTIFSTATEMENT = 79;
+  public int JJTWHILESTATEMENT = 80;
+  public int JJTDOSTATEMENT = 81;
+  public int JJTFORSTATEMENT = 82;
+  public int JJTFORINIT = 83;
+  public int JJTSTATEMENTEXPRESSIONLIST = 84;
+  public int JJTFORUPDATE = 85;
+  public int JJTBREAKSTATEMENT = 86;
+  public int JJTCONTINUESTATEMENT = 87;
+  public int JJTRETURNSTATEMENT = 88;
+  public int JJTTHROWSTATEMENT = 89;
+  public int JJTSYNCHRONIZEDSTATEMENT = 90;
+  public int JJTTRYSTATEMENT = 91;
+  public int JJTCATCHSTATEMENT = 92;
+  public int JJTFINALLYSTATEMENT = 93;
+  public int JJTASSERTSTATEMENT = 94;
+  public int JJTRUNSIGNEDSHIFT = 95;
+  public int JJTRSIGNEDSHIFT = 96;
+  public int JJTANNOTATION = 97;
+  public int JJTNORMALANNOTATION = 98;
+  public int JJTMARKERANNOTATION = 99;
+  public int JJTSINGLEMEMBERANNOTATION = 100;
+  public int JJTMEMBERVALUEPAIRS = 101;
+  public int JJTMEMBERVALUEPAIR = 102;
+  public int JJTMEMBERVALUE = 103;
+  public int JJTMEMBERVALUEARRAYINITIALIZER = 104;
+  public int JJTANNOTATIONTYPEDECLARATION = 105;
+  public int JJTANNOTATIONTYPEBODY = 106;
+  public int JJTANNOTATIONTYPEMEMBERDECLARATION = 107;
+  public int JJTDEFAULTVALUE = 108;
 
 
-    public String[] jjtNodeName = {
-        "CompilationUnit",
-        "PackageDeclaration",
-        "ImportDeclaration",
-        "Modifiers",
-        "TypeDeclaration",
-        "ClassOrInterfaceDeclaration",
-        "ExtendsList",
-        "ImplementsList",
-        "EnumDeclaration",
-        "EnumBody",
-        "EnumConstant",
-        "TypeParameters",
-        "TypeParameter",
-        "TypeBound",
-        "ClassOrInterfaceBody",
-        "ClassOrInterfaceBodyDeclaration",
-        "FieldDeclaration",
-        "VariableDeclarator",
-        "VariableDeclaratorId",
-        "VariableInitializer",
-        "ArrayInitializer",
-        "MethodDeclaration",
-        "MethodDeclarator",
-        "FormalParameters",
-        "FormalParameter",
-        "ConstructorDeclaration",
-        "ExplicitConstructorInvocation",
-        "Initializer",
-        "Type",
-        "ReferenceType",
-        "ClassOrInterfaceType",
-        "TypeArguments",
-        "TypeArgument",
-        "WildcardBounds",
-        "PrimitiveType",
-        "ResultType",
-        "Name",
-        "NameList",
-        "Expression",
-        "AssignmentOperator",
-        "ConditionalExpression",
-        "ConditionalOrExpression",
-        "ConditionalAndExpression",
-        "InclusiveOrExpression",
-        "ExclusiveOrExpression",
-        "AndExpression",
-        "EqualityExpression",
-        "InstanceOfExpression",
-        "RelationalExpression",
-        "ShiftExpression",
-        "AdditiveExpression",
-        "MultiplicativeExpression",
-        "UnaryExpression",
-        "PreIncrementExpression",
-        "PreDecrementExpression",
-        "UnaryExpressionNotPlusMinus",
-        "CastLookahead",
-        "PostfixExpression",
-        "CastExpression",
-        "PrimaryExpression",
-        "MemberSelector",
-        "PrimaryPrefix",
-        "PrimarySuffix",
-        "Literal",
-        "BooleanLiteral",
-        "NullLiteral",
-        "Arguments",
-        "ArgumentList",
-        "AllocationExpression",
-        "ArrayDimsAndInits",
-        "Statement",
-        "LabeledStatement",
-        "Block",
-        "BlockStatement",
-        "LocalVariableDeclaration",
-        "EmptyStatement",
-        "StatementExpression",
-        "SwitchStatement",
-        "SwitchLabel",
-        "IfStatement",
-        "WhileStatement",
-        "DoStatement",
-        "ForStatement",
-        "ForInit",
-        "StatementExpressionList",
-        "ForUpdate",
-        "BreakStatement",
-        "ContinueStatement",
-        "ReturnStatement",
-        "ThrowStatement",
-        "SynchronizedStatement",
-        "TryStatement",
-        "CatchStatement",
-        "FinallyStatement",
-        "AssertStatement",
-        "RUNSIGNEDSHIFT",
-        "RSIGNEDSHIFT",
-        "Annotation",
-        "NormalAnnotation",
-        "MarkerAnnotation",
-        "SingleMemberAnnotation",
-        "MemberValuePairs",
-        "MemberValuePair",
-        "MemberValue",
-        "MemberValueArrayInitializer",
-        "AnnotationTypeDeclaration",
-        "AnnotationTypeBody",
-        "AnnotationTypeMemberDeclaration",
-        "DefaultValue",
-    };
+  public String[] jjtNodeName = {
+    "CompilationUnit",
+    "PackageDeclaration",
+    "ImportDeclaration",
+    "Modifiers",
+    "TypeDeclaration",
+    "ClassOrInterfaceDeclaration",
+    "ExtendsList",
+    "ImplementsList",
+    "EnumDeclaration",
+    "EnumBody",
+    "EnumConstant",
+    "TypeParameters",
+    "TypeParameter",
+    "TypeBound",
+    "ClassOrInterfaceBody",
+    "ClassOrInterfaceBodyDeclaration",
+    "FieldDeclaration",
+    "VariableDeclarator",
+    "VariableDeclaratorId",
+    "VariableInitializer",
+    "ArrayInitializer",
+    "MethodDeclaration",
+    "MethodDeclarator",
+    "FormalParameters",
+    "FormalParameter",
+    "ConstructorDeclaration",
+    "ExplicitConstructorInvocation",
+    "Initializer",
+    "Type",
+    "ReferenceType",
+    "ClassOrInterfaceType",
+    "TypeArguments",
+    "TypeArgument",
+    "WildcardBounds",
+    "PrimitiveType",
+    "ResultType",
+    "Name",
+    "NameList",
+    "Expression",
+    "AssignmentOperator",
+    "ConditionalExpression",
+    "ConditionalOrExpression",
+    "ConditionalAndExpression",
+    "InclusiveOrExpression",
+    "ExclusiveOrExpression",
+    "AndExpression",
+    "EqualityExpression",
+    "InstanceOfExpression",
+    "RelationalExpression",
+    "ShiftExpression",
+    "AdditiveExpression",
+    "MultiplicativeExpression",
+    "UnaryExpression",
+    "PreIncrementExpression",
+    "PreDecrementExpression",
+    "UnaryExpressionNotPlusMinus",
+    "CastLookahead",
+    "PostfixExpression",
+    "CastExpression",
+    "PrimaryExpression",
+    "MemberSelector",
+    "PrimaryPrefix",
+    "PrimarySuffix",
+    "Literal",
+    "BooleanLiteral",
+    "NullLiteral",
+    "Arguments",
+    "ArgumentList",
+    "AllocationExpression",
+    "ArrayDimsAndInits",
+    "Statement",
+    "LabeledStatement",
+    "Block",
+    "BlockStatement",
+    "LocalVariableDeclaration",
+    "EmptyStatement",
+    "StatementExpression",
+    "SwitchStatement",
+    "SwitchLabel",
+    "IfStatement",
+    "WhileStatement",
+    "DoStatement",
+    "ForStatement",
+    "ForInit",
+    "StatementExpressionList",
+    "ForUpdate",
+    "BreakStatement",
+    "ContinueStatement",
+    "ReturnStatement",
+    "ThrowStatement",
+    "SynchronizedStatement",
+    "TryStatement",
+    "CatchStatement",
+    "FinallyStatement",
+    "AssertStatement",
+    "RUNSIGNEDSHIFT",
+    "RSIGNEDSHIFT",
+    "Annotation",
+    "NormalAnnotation",
+    "MarkerAnnotation",
+    "SingleMemberAnnotation",
+    "MemberValuePairs",
+    "MemberValuePair",
+    "MemberValue",
+    "MemberValueArrayInitializer",
+    "AnnotationTypeDeclaration",
+    "AnnotationTypeBody",
+    "AnnotationTypeMemberDeclaration",
+    "DefaultValue",
+  };
 }
