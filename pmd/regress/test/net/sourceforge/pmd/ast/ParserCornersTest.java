04dc041faff60b341b2631e0a70c287353d95c61
Tom Copeland <tomcopeland@users.sourceforge.net>
2006-02-13 16:52:15 +0000

    Added test for bug fix
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@4234 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/ast/ParserCornersTest.java
@@ -0,0 +1,25 @@
+package test.net.sourceforge.pmd.ast;
+
+import test.net.sourceforge.pmd.testframework.ParserTst;
+import net.sourceforge.pmd.PMD;
+import net.sourceforge.pmd.TargetJDK1_4;
+
+import java.io.StringReader;
+
+public class ParserCornersTest extends ParserTst {
+
+    public final void testGetFirstASTNameImageNull() throws Throwable {
+        new TargetJDK1_4().createParser(new StringReader(ABSTRACT_METHOD_LEVEL_CLASS_DECL)).CompilationUnit();
+    }
+
+    private static final String ABSTRACT_METHOD_LEVEL_CLASS_DECL =
+            "public class Test {" + PMD.EOL +
+            "  void bar() {" + PMD.EOL +
+            "   abstract class X { public abstract void f(); }" + PMD.EOL +
+            "   class Y extends X { public void f() {" + PMD.EOL +
+            "    new Y().f();" + PMD.EOL +
+            "   }}" + PMD.EOL +
+            "  }" + PMD.EOL +
+            "}";
+
+}
