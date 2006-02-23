075fca024ca3d0ede31e7f8a67975c0d2cccc643
Tom Copeland <tomcopeland@users.sourceforge.net>
2006-02-23 14:59:32 +0000

    Fixed bug 1435218 - LoggerIsNotStaticFinal no longer reports false positives for local variables.
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@4257 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/regress/test/net/sourceforge/pmd/rules/logging/java/LoggerIsNotStaticFinalTest.java
+++ pmd/regress/test/net/sourceforge/pmd/rules/logging/java/LoggerIsNotStaticFinalTest.java
@@ -22,27 +22,35 @@ public class LoggerIsNotStaticFinalTest extends SimpleAggregatorTst {
             new TestDescriptor(TEST1, "ok", 0, rule),
             new TestDescriptor(TEST2, "two bad loggers", 2, rule),
             new TestDescriptor(TEST3, "ok with internal class", 0, rule),
+            new TestDescriptor(TEST4, "ok with local var", 0, rule),
         });
     }
 
     private static final String TEST1 =
-            "public class Foo {" + PMD.EOL +
-            " static final Logger log;" + PMD.EOL +
-            "}";
+    "public class Foo {" + PMD.EOL +
+    " static final Logger log;" + PMD.EOL +
+    "}";
 
     private static final String TEST2 =
-            "public class Foo {" + PMD.EOL +
-            " static final Logger log;" + PMD.EOL +
-            " Logger log1;" + PMD.EOL +
-            " Logger log2;" + PMD.EOL +
-            "}";
+    "public class Foo {" + PMD.EOL +
+    " static final Logger log;" + PMD.EOL +
+    " Logger log1;" + PMD.EOL +
+    " Logger log2;" + PMD.EOL +
+    "}";
 
     private static final String TEST3 =
-            "public class Foo {" + PMD.EOL +
-            " static final Logger log;" + PMD.EOL +
-            " static class c { " + PMD.EOL +
-            "  static final Logger log;" + PMD.EOL +
-            " } " + PMD.EOL +
-            "}";
+    "public class Foo {" + PMD.EOL +
+    " static final Logger log;" + PMD.EOL +
+    " static class c { " + PMD.EOL +
+    "  static final Logger log;" + PMD.EOL +
+    " } " + PMD.EOL +
+    "}";
+
+    private static final String TEST4 =
+    "public class Foo {" + PMD.EOL +
+    " void bar() {" + PMD.EOL +
+    " Logger log1;" + PMD.EOL +
+    " }" + PMD.EOL +
+    "}";
 
 }
