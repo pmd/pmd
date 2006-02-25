0f6fbf8404fc488d36d067a409fdd3258e87f5b1
Allan Caplan <allancaplan@users.sourceforge.net>
2006-02-25 01:04:11 +0000

    New Rule: UnsynchronizedStaticDateFormatter
    Rule checks for static (Simple)DateFormatter instances which are accessed from outside synchronized blocks.
    
    Doing so in a multithreaded environment can have negative side effects when both threads access the object at the same time
    
    
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@4263 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/rules/design/UnsynchronizedStaticDateFormatterTest.java
@@ -0,0 +1,83 @@
+/**
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+*/
+package test.net.sourceforge.pmd.rules.design;
+
+import net.sourceforge.pmd.PMD;
+import net.sourceforge.pmd.Rule;
+
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
+
+
+public class UnsynchronizedStaticDateFormatterTest extends SimpleAggregatorTst  {
+
+    private Rule rule;
+
+    public void setUp() {
+        rule = findRule("design", "UnsynchronizedStaticDateFormatter");
+    }
+
+    public void testAll() throws Exception{
+        runTests(new TestDescriptor[] { 
+                new TestDescriptor(TEST1, "Format called from non-synchronized block", 1, rule),
+                new TestDescriptor(TEST2, "2, No call to format", 0, rule),
+                new TestDescriptor(TEST3, "3, Inside synchronized, OK", 0, rule),
+                new TestDescriptor(TEST4, "4, Inside synchronized, OK", 0, rule),
+                new TestDescriptor(TEST5, "5, Use DateFormat, ok", 0, rule),
+                new TestDescriptor(TEST6, "6, Use DateFormat, fail", 1, rule),
+        });
+    }
+
+    private static final String TEST1 =
+        "public class Foo {" + PMD.EOL +
+        "    private static final SimpleDateFormat sdf = new SimpleDateFormat();" + PMD.EOL +
+        "    void bar() {" + PMD.EOL +
+        "        sdf.format();" + PMD.EOL +
+        "    }" + PMD.EOL +
+        "}";
+
+    private static final String TEST2 =
+        "public class Foo {" + PMD.EOL +
+        "    private final SimpleDateFormat sdf = new SimpleDateFormat();" + PMD.EOL +
+        "    void bar() {" + PMD.EOL +
+        "    }" + PMD.EOL +
+        "}";
+
+    private static final String TEST3 =
+        "public class Foo {" + PMD.EOL +
+        "    private static final SimpleDateFormat sdf = new SimpleDateFormat();" + PMD.EOL +
+        "    synchronized void bar() {" + PMD.EOL +
+        "        sdf.format();" + PMD.EOL +
+        "    }" + PMD.EOL +
+        "}";
+
+
+    private static final String TEST4 =
+        "public class Foo {" + PMD.EOL +
+        "    private static final SimpleDateFormat sdf = new SimpleDateFormat();" + PMD.EOL +
+        "    public void bar() {" + PMD.EOL +
+        "        synchronized (sdf) {" + PMD.EOL +
+        "            sdf.format();" + PMD.EOL +
+        "        }" + PMD.EOL +
+        "    }" + PMD.EOL +
+        "}";
+    
+
+    private static final String TEST5 =
+        "public class Foo {" + PMD.EOL +
+        "    private static final DateFormat sdf = new DateFormat();" + PMD.EOL +
+        "    synchronized void bar() {" + PMD.EOL +
+        "        sdf.format();" + PMD.EOL +
+        "    }" + PMD.EOL +
+        "}";
+
+    private static final String TEST6 =
+        "public class Foo {" + PMD.EOL +
+        "    private static final DateFormat sdf = new DateFormat();" + PMD.EOL +
+        "    void bar() {" + PMD.EOL +
+        "        sdf.format();" + PMD.EOL +
+        "    }" + PMD.EOL +
+        "}";
+
+}
---
+++ pmd/src/net/sourceforge/pmd/rules/design/UnsynchronizedStaticDateFormatter.java
@@ -0,0 +1,63 @@
+/**
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+package net.sourceforge.pmd.rules.design;
+
+import net.sourceforge.pmd.AbstractRule;
+import net.sourceforge.pmd.ast.ASTClassOrInterfaceType;
+import net.sourceforge.pmd.ast.ASTFieldDeclaration;
+import net.sourceforge.pmd.ast.ASTMethodDeclaration;
+import net.sourceforge.pmd.ast.ASTSynchronizedStatement;
+import net.sourceforge.pmd.ast.ASTVariableDeclaratorId;
+import net.sourceforge.pmd.ast.SimpleNode;
+import net.sourceforge.pmd.symboltable.NameOccurrence;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Using a DateFormatter (SimpleDateFormatter) which is static can cause
+ * unexpected results when used in a multi threaded environment. This rule will
+ * find static (Simple)DateFormatters which are used in an unsynchronized
+ * manner.
+ * 
+ * @link http://developer.java.sun.com/developer/bugParade/bugs/4093418.html
+ * @link http://developer.java.sun.com/developer/bugParade/bugs/4228335.html
+ * @link http://developer.java.sun.com/developer/bugParade/bugs/4261469.html
+ * @see RFE1020790 - Check for SimpleDateFormat as singleton http://sourceforge.net/tracker/index.php?func=detail&aid=1020790&group_id=56262&atid=479924
+ * @author Allan Caplan
+ */
+public class UnsynchronizedStaticDateFormatter extends AbstractRule {
+
+    private static Set targets = new HashSet();
+    static {
+        targets.add("DateFormat");
+        targets.add("SimpleDateFormat");
+        targets.add("java.text.DateFormat");
+        targets.add("java.text.SimpleDateFormat");
+    }
+
+    public Object visit(ASTFieldDeclaration node, Object data) {
+        if (!node.isStatic()) {
+            return data;
+        }
+        ASTClassOrInterfaceType cit = (ASTClassOrInterfaceType) node.getFirstChildOfType(ASTClassOrInterfaceType.class);
+        if (cit == null || !targets.contains(cit.getImage())) {
+            return data;
+        }
+        ASTVariableDeclaratorId var = (ASTVariableDeclaratorId) node.getFirstChildOfType(ASTVariableDeclaratorId.class);
+        for (Iterator i = var.getUsages().iterator(); i.hasNext();) {
+            NameOccurrence occ = (NameOccurrence) i.next();
+            SimpleNode n = occ.getLocation();
+            if ((ASTSynchronizedStatement) n.getFirstParentOfType(ASTSynchronizedStatement.class) != null) {
+                continue;
+            }
+            ASTMethodDeclaration method = (ASTMethodDeclaration) n.getFirstParentOfType(ASTMethodDeclaration.class);
+            if (method != null && !method.isSynchronized()) {
+                addViolation(data, n);
+            }
+        }
+        return data;
+    }
+}
