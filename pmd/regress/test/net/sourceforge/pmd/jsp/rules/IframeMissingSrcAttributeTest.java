f9be47c09ca3673710325f055c992370f9153057
Allan Caplan <allancaplan@users.sourceforge.net>
2006-02-26 02:05:50 +0000

    Adding 2 JSP rules
    - IframeMissingSrcAttribute tests for IFRAMEs which are missing the SRC attribute. IE will display an annoying popup if you access a page which has an IFRAME without a src element over SSL.
    
    - NoHtmlComments finds all HTML comments. These comments are useless in a production environment. If a comment is required, consider changing to a JSP comment.
    
    
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@4266 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/regress/test/net/sourceforge/pmd/jsp/rules/IframeMissingSrcAttributeTest.java
@@ -0,0 +1,29 @@
+package test.net.sourceforge.pmd.jsp.rules;
+
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.SourceType;
+
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
+
+public class IframeMissingSrcAttributeTest extends SimpleAggregatorTst {
+
+    private Rule rule;
+
+    public void setUp() {
+        rule = findRule("basic-jsp.xml", "IframeMissingSrcAttribute");
+    }
+
+    public void testAll() {
+        runTests(new TestDescriptor[]{
+            new TestDescriptor(TEST1_OK, "1, iframe exists with src attribute", 0, rule),
+            new TestDescriptor(TEST2_FAIL, "2, iframe is missing src attribute", 1, rule),
+        }, SourceType.JSP);
+    }
+
+    private static final String TEST1_OK =
+            "<html><body><iframe src=\"foo.html\"></iframe></body></html>";
+
+    private static final String TEST2_FAIL =
+        "<html><body><iframe></iframe></body></html>";
+}
---
+++ pmd/regress/test/net/sourceforge/pmd/jsp/rules/NoHtmlCommentsTest.java
@@ -0,0 +1,33 @@
+package test.net.sourceforge.pmd.jsp.rules;
+
+import net.sourceforge.pmd.Rule;
+import net.sourceforge.pmd.SourceType;
+
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
+
+public class NoHtmlCommentsTest extends SimpleAggregatorTst {
+
+    private Rule rule;
+
+    public void setUp() {
+        rule = findRule("basic-jsp.xml", "NoHtmlComments");
+    }
+
+    public void testAll() {
+        runTests(new TestDescriptor[]{
+            new TestDescriptor(TEST1_OK, "No HTML comments", 0, rule),
+            new TestDescriptor(TEST2_FAIL, "HTML Comment", 1, rule),
+            new TestDescriptor(TEST3_OK, "JSP Comments", 0, rule),
+        }, SourceType.JSP);
+    }
+
+    private static final String TEST1_OK =
+            "<html><body></body></html>";
+
+    private static final String TEST2_FAIL =
+        "<html><body><!-- HTML Comment --></body></html>";
+
+    private static final String TEST3_OK =
+        "<html><body><%-- JSP Comment --%></body></html>";
+}
