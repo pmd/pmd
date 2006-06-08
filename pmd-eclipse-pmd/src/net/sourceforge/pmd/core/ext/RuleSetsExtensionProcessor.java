e9b41166c4d1fd8e8db25455892f943e4e284ae4
Philippe Herlin <phherlin@users.sourceforge.net>
2006-06-08 21:46:58 +0000

    Remove usage API unknown by RSA (no consequence as it was for logging only)
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@4412 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd-eclipse-pmd/src/net/sourceforge/pmd/core/ext/RuleSetsExtensionProcessor.java
+++ pmd-eclipse-pmd/src/net/sourceforge/pmd/core/ext/RuleSetsExtensionProcessor.java
@@ -54,6 +54,9 @@ import org.eclipse.core.runtime.Platform;
  * @version $Revision$
  * 
  * $Log$
+ * Revision 1.2  2006/06/08 21:46:58  phherlin
+ * Remove usage API unknown by RSA (no consequence as it was for logging only)
+ *
  * Revision 1.1  2005/07/02 14:33:05  phherlin
  * Implement the RuleSets extension point
  *
@@ -98,7 +101,7 @@ public class RuleSetsExtensionProcessor {
             extension.registerDefaultRuleSets(this.ruleSetManager.getDefaultRuleSets());
             
         } else {
-            PMDCorePlugin.getDefault().log(IStatus.ERROR, "Extension " + element.getNamespace() + "." + element.getName() + " is not an instance of IRuleSetsExtension", null);
+            PMDCorePlugin.getDefault().log(IStatus.ERROR, "Extension " + element.getName() + " is not an instance of IRuleSetsExtension", null);
         }
     }
 
