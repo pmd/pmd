da3fbd613b648a8a39d83c220286697e6f003b5b
Allan Caplan <allancaplan@users.sourceforge.net>
2006-07-09 00:38:11 +0000

    Performance improvement for the UselessOperationOnImmutable rule.
    Scanning PMD src goes from 1 minute down to 13 seconds.
    
    
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@4457 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/src/net/sourceforge/pmd/rules/UselessOperationOnImmutable.java
@@ -0,0 +1,67 @@
+package net.sourceforge.pmd.rules;
+
+import net.sourceforge.pmd.AbstractRule;
+import net.sourceforge.pmd.ast.ASTExpression;
+import net.sourceforge.pmd.ast.ASTLocalVariableDeclaration;
+import net.sourceforge.pmd.ast.ASTName;
+import net.sourceforge.pmd.ast.ASTType;
+import net.sourceforge.pmd.ast.ASTVariableDeclaratorId;
+import net.sourceforge.pmd.symboltable.NameOccurrence;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * An operation on an Immutable object (BigDecimal or BigInteger) won't change
+ * the object itself. The result of the operation is a new object. Therefore,
+ * ignoring the operation result is an error.
+ */
+public class UselessOperationOnImmutable extends AbstractRule {
+
+    /**
+     * These are the methods which are immutable
+     */
+    private static final List targetMethods = Arrays.asList(new String[] { ".add", ".multiply", ".divide", ".subtract", ".setScale", ".negate", ".movePointLeft", ".movePointRight", ".pow", ".shiftLeft", ".shiftRight" });
+
+    /**
+     * These are the classes that the rule can apply to
+     */
+    private static final List targetClasses = Arrays.asList(new String[] { "java.math.BigDecimal", "BigDecimal", "java.math.BigInteger", "BigInteger" });
+
+    public Object visit(ASTLocalVariableDeclaration node, Object data) {
+
+        ASTVariableDeclaratorId var = getDeclaration(node);
+        if (var == null) {
+            return super.visit(node, data);
+        }
+        String variableName = var.getImage();
+        for (Iterator it = var.getUsages().iterator(); it.hasNext();) {
+            NameOccurrence no = (NameOccurrence) it.next();
+            ASTName sn = (ASTName) no.getLocation();
+            if (!sn.jjtGetParent().jjtGetParent().jjtGetParent().getClass().equals(ASTExpression.class)) {
+                String methodCall = sn.getImage().substring(variableName.length());
+                if (targetMethods.contains(methodCall)) {
+                    addViolation(data, sn);
+                }
+            }
+        }
+        return super.visit(node, data);
+    }
+
+    /**
+     * This method checks the variable declaration if it is on a class we care
+     * about. If it is, it returns the DeclaratorId
+     * 
+     * @param node
+     *            The ASTLocalVariableDeclaration which is a problem
+     * @return ASTVariableDeclaratorId
+     */
+    private ASTVariableDeclaratorId getDeclaration(ASTLocalVariableDeclaration node) {
+        ASTType type = (ASTType) node.jjtGetChild(0);
+        if (targetClasses.contains(type.getTypeImage())) {
+            return (ASTVariableDeclaratorId) node.jjtGetChild(1).jjtGetChild(0);
+        }
+        return null;
+    }
+}
