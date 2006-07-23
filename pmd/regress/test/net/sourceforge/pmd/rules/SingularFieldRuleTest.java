7f1359d2b8c388522e4979a96601a490e1802386
Allan Caplan <allancaplan@users.sourceforge.net>
2006-07-23 23:58:15 +0000

    Performance improvement for the SingularField rule.
    Scanning PMD src goes from 2minutes down to 25 seconds. Performance improvement is huge on larger sources - 7,000 classes scanned in 4 minutes down from 55!
    
    Also fixed defect 1526530 SingularField False Negativesrule now detects problems inside static and constructor blocks
    
    
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@4472 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/regress/test/net/sourceforge/pmd/rules/SingularFieldRuleTest.java
+++ pmd/regress/test/net/sourceforge/pmd/rules/SingularFieldRuleTest.java
@@ -22,6 +22,18 @@ public class SingularFieldRuleTest extends SimpleAggregatorTst {
             new TestDescriptor(TEST5, "skip statics", 0, rule),
             new TestDescriptor(TEST6, "unused fields shouldn't show up", 0, rule),
             new TestDescriptor(TEST7, "inner class", 0, rule),
+            new TestDescriptor(TEST8, "initialized in constructor", 0, rule),
+            new TestDescriptor(TEST9, "failure case in inner class", 1, rule),
+            new TestDescriptor(TEST10, "failure case with Object", 1, rule),
+            new TestDescriptor(TEST11, "failure case in static inner class", 1, rule),
+            new TestDescriptor(TEST12, "ok, shouldn't catch unused variable", 0, rule),
+            new TestDescriptor(TEST13, "failure case with self-instantiation", 1, rule),
+            new TestDescriptor(TEST14, "Reuse variable name as params in method calls", 1, rule),
+            new TestDescriptor(TEST15, "failure case in two static inner classes", 1, rule),
+            new TestDescriptor(TEST16, "OK, instantiates own self internally", 0, rule),
+            new TestDescriptor(TEST17, "failure, variable accessed twice in same method", 1, rule),
+            new TestDescriptor(TEST18, "failure, static", 1, rule),
+            new TestDescriptor(TEST19, "failure, second method re-uses class level name", 1, rule),
         });
     }
 
@@ -98,4 +110,143 @@ public class SingularFieldRuleTest extends SimpleAggregatorTst {
             " }" + PMD.EOL +
             "}";
 
+    private static final String TEST8 =
+            "public class Foo {" + PMD.EOL +
+            " private int x;" + PMD.EOL +
+            " public Foo(int y) {" + PMD.EOL +
+            "  x = y; " + PMD.EOL +
+            " }" + PMD.EOL +
+            " int bar(int y) {" + PMD.EOL +
+            "  x = y + 5; " + PMD.EOL +
+            "  return x;" + PMD.EOL +
+            " }" + PMD.EOL +
+            "}";
+
+    private static final String TEST9 =
+        "public class Foo {" + PMD.EOL +
+        " class Bar {" + PMD.EOL +
+        " private int x;" + PMD.EOL +
+        " int bar(int y) {" + PMD.EOL +
+        "  x = y + 5; " + PMD.EOL +
+        "  return x;" + PMD.EOL +
+        " }" + PMD.EOL +
+        " }" + PMD.EOL +
+        "}";
+
+    private static final String TEST10 =
+        "public class Foo {" + PMD.EOL +
+        " private Integer x = null;" + PMD.EOL +
+        " private Integer getFoo() {" + PMD.EOL +
+        "  if(x == null){; " + PMD.EOL +
+        "      x = new Integer(1);; " + PMD.EOL +
+        "  }; " + PMD.EOL +
+        "  return x;" + PMD.EOL +
+        " }" + PMD.EOL +
+        "}";
+
+    private static final String TEST11 =
+        "public class Foo {" + PMD.EOL +
+        " static class Bar {" + PMD.EOL +
+        " private int x;" + PMD.EOL +
+        " int bar(int y) {" + PMD.EOL +
+        "   this.x = y + 5; " + PMD.EOL +
+        " }" + PMD.EOL +
+        " }" + PMD.EOL +
+        "}";
+    
+
+    private static final String TEST12 =
+            "public class Foo {" + PMD.EOL +
+            " private int x;" + PMD.EOL +
+            " void bar(int y) {" + PMD.EOL +
+            " }" + PMD.EOL +
+            "}";
+
+    private static final String TEST13 =
+        "public class Foo {" + PMD.EOL +
+        " private Integer x = new Integer(1);" + PMD.EOL +
+        " private Integer getFoo() {" + PMD.EOL +
+        "  return x;" + PMD.EOL +
+        " }" + PMD.EOL +
+        "}";
+
+    private static final String TEST14 =
+        "public class Foo {" + PMD.EOL +
+        " private Integer x = new Integer(1);" + PMD.EOL +
+        " public Foo(Integer x) {" + PMD.EOL +
+        " }" + PMD.EOL +
+        " private void getFoo(Integer x) {" + PMD.EOL +
+        "  this.x = x;" + PMD.EOL +
+        " }" + PMD.EOL +
+        "}";
+
+    private static final String TEST15 =
+        "public class Foo {" + PMD.EOL +
+        " static class Bar1 {" + PMD.EOL +
+        " private int x;" + PMD.EOL +
+        " int bar(int y) {" + PMD.EOL +
+        "   this.x = y + 5; " + PMD.EOL +
+        " }" + PMD.EOL +
+        " }" + PMD.EOL +
+        " static class Bar2 {" + PMD.EOL +
+        " private int x;" + PMD.EOL +
+        " int getX() {" + PMD.EOL +
+        "   return x; " + PMD.EOL +
+        " }" + PMD.EOL +
+        " int setX(int y) {" + PMD.EOL +
+        "   this.x = y + 5; " + PMD.EOL +
+        " }" + PMD.EOL +
+        " }" + PMD.EOL +
+        "}";
+    
+    private static final String TEST16 =
+        "public class Foo {" + PMD.EOL +
+        " private int x;" + PMD.EOL +
+        " int bar(int y) {" + PMD.EOL +
+        "  x = y + 5; " + PMD.EOL +
+        "  return x;" + PMD.EOL +
+        " }" + PMD.EOL +
+        " private void bar() {" + PMD.EOL +
+        "  Foo foo = new Foo();" + PMD.EOL +
+        "  foo.x = new Integer(5);" + PMD.EOL +
+        " }" + PMD.EOL +
+        "}";
+
+
+    private static final String TEST17 =
+            "public class Foo {" + PMD.EOL +
+            " private Map map = new SomeMap();" + PMD.EOL +
+            " private boolean bar(Object o) {" + PMD.EOL +
+            "     boolean ret = true;" + PMD.EOL +
+            "     if(super.isTrue) {" + PMD.EOL +
+            "     if(map.get(o) != null) {" + PMD.EOL +
+            "         ret = false; " + PMD.EOL +
+            "     } else {" + PMD.EOL +
+            "         map.put(o,o); " + PMD.EOL +
+            "     } " + PMD.EOL +
+            "     } " + PMD.EOL +
+            "     return ret; " + PMD.EOL +
+            " }" + PMD.EOL +
+            "}";
+    
+    private static final String TEST18 =
+        "public class Foo {" + PMD.EOL +
+        " private int x;" + PMD.EOL +
+        " static {" + PMD.EOL +
+        "  x = 5; " + PMD.EOL +
+        " }" + PMD.EOL +
+        "}";
+
+    private static final String TEST19 =
+        "public class Foo {" + PMD.EOL +
+        " private int x;" + PMD.EOL +
+        " void setX(int x) {" + PMD.EOL +
+        "  this.x = x;" + PMD.EOL +
+        " }" + PMD.EOL +
+        " int doX() {" + PMD.EOL +
+        "  int x = 5;" + PMD.EOL +
+        "  return x*3;" + PMD.EOL +
+        " }" + PMD.EOL +
+        "}";
+    
 }
--- pmd/src/net/sourceforge/pmd/rules/SingularField.java
+++ pmd/src/net/sourceforge/pmd/rules/SingularField.java
@@ -3,19 +3,17 @@
  *
  * Created on April 17, 2005, 9:49 PM
  */
-
 package net.sourceforge.pmd.rules;
 
+import java.util.List;
+
 import net.sourceforge.pmd.AbstractRule;
-import net.sourceforge.pmd.ast.ASTClassOrInterfaceDeclaration;
 import net.sourceforge.pmd.ast.ASTConstructorDeclaration;
 import net.sourceforge.pmd.ast.ASTFieldDeclaration;
 import net.sourceforge.pmd.ast.ASTMethodDeclaration;
-import net.sourceforge.pmd.ast.ASTMethodDeclarator;
 import net.sourceforge.pmd.ast.ASTVariableDeclaratorId;
-import org.jaxen.JaxenException;
-
-import java.util.List;
+import net.sourceforge.pmd.ast.SimpleNode;
+import net.sourceforge.pmd.symboltable.NameOccurrence;
 
 /**
  * @author Eric Olander
@@ -25,31 +23,35 @@ public class SingularField extends AbstractRule {
     public Object visit(ASTFieldDeclaration node, Object data) {
         if (node.isPrivate() && !node.isStatic()) {
             List list = node.findChildrenOfType(ASTVariableDeclaratorId.class);
-            ASTVariableDeclaratorId decl = (ASTVariableDeclaratorId) list.get(0);
-            String name = decl.getImage();
-            String path = "//MethodDeclaration[.//PrimaryExpression[.//Name[@Image = \"" + name + "\" or substring-before(@Image, \".\") = \"" + name + "\"] or .//PrimarySuffix[@Image = \"" + name + "\"]]] |" +
-                    "//ConstructorDeclaration[.//PrimaryExpression[.//Name[@Image = \"" + name + "\" or substring-before(@Image, \".\") = \"" + name + "\"] or .//PrimarySuffix[@Image = \"" + name + "\"]]]";
-            try {
-                List nodes = node.findChildNodesWithXPath(path);
-                if (nodes.size() == 1) {
-                    String method;
-                    if (nodes.get(0) instanceof ASTMethodDeclaration) {
-                        method = ((ASTMethodDeclarator) ((ASTMethodDeclaration) nodes.get(0)).findChildrenOfType(ASTMethodDeclarator.class).get(0)).getImage();
-                    } else {
-                        ASTConstructorDeclaration astConstructorDeclaration = (ASTConstructorDeclaration) nodes.get(0);
-                        ASTClassOrInterfaceDeclaration astClassOrInterfaceDeclaration = (ASTClassOrInterfaceDeclaration) astConstructorDeclaration.getFirstParentOfType(ASTClassOrInterfaceDeclaration.class);
-                        if (astClassOrInterfaceDeclaration == null) {
-                            return data;
-                        }
-                        method = astClassOrInterfaceDeclaration.getImage();
+            ASTVariableDeclaratorId declaration = (ASTVariableDeclaratorId) list.get(0);
+            List usages = declaration.getUsages();
+            SimpleNode decl = null;
+            boolean violation = true;
+            for (int ix = 0; ix < usages.size(); ix++) {
+                NameOccurrence no = (NameOccurrence) usages.get(ix);
+                SimpleNode location = no.getLocation();
+
+                SimpleNode method = (SimpleNode) location.getFirstParentOfType(ASTMethodDeclaration.class);
+                if (method == null) {
+                    method = (SimpleNode) location.getFirstParentOfType(ASTConstructorDeclaration.class);
+                    if (method == null) {
+                        continue;
                     }
-                    addViolation(data, decl, new Object[]{name, method});
                 }
-            } catch (JaxenException je) {
-                je.printStackTrace();
+
+                if (decl == null) {
+                    decl = method;
+                    continue;
+                } else if (decl != method) {
+
+                    violation = false;
+                }
+            }
+
+            if (violation && usages.size() != 0) {
+                addViolation(data, node, new Object[] { declaration.getImage() });
             }
         }
         return data;
     }
-
 }
