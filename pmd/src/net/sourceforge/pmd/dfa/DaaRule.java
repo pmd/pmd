f9ce45ff1ef9cca55ff7b7f7f1314bf58e02aedb
Tom Copeland <tomcopeland@users.sourceforge.net>
2006-08-23 13:19:33 +0000

    Some fixes from Sven Jacob for the DFA layer
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@4496 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/net/sourceforge/pmd/dfa/DaaRule.java
+++ pmd/src/net/sourceforge/pmd/dfa/DaaRule.java
@@ -6,6 +6,7 @@ package net.sourceforge.pmd.dfa;
 import net.sourceforge.pmd.AbstractRule;
 import net.sourceforge.pmd.RuleContext;
 import net.sourceforge.pmd.ast.ASTMethodDeclaration;
+import net.sourceforge.pmd.ast.SimpleNode;
 import net.sourceforge.pmd.dfa.pathfinder.CurrentPath;
 import net.sourceforge.pmd.dfa.pathfinder.DAAPathFinder;
 import net.sourceforge.pmd.dfa.pathfinder.Executable;
@@ -57,25 +58,49 @@ public class DaaRule extends AbstractRule implements Executable {
                     Object o = hash.get(va.getVariableName());
                     if (o != null) {
                         List array = (List) o;
+                        // get the last access type
                         int last = ((Integer) array.get(0)).intValue();
-                        // TODO - at some point investigate and possibly reintroduce this line2 thing
-                        //int line2 = ((Integer) array.get(1)).intValue();
-/*
+
                         if (va.accessTypeMatches(last) && va.isDefinition()) { // DD
-                            this.rc.getReport().addRuleViolation(createRuleViolation(rc, inode.getSimpleNode(), va.getVariableName(), "DD"));
+                        	if (inode.getSimpleNode() != null) {
+                        		// preventing NullpointerException
+                        		addDaaViolation(rc, inode.getSimpleNode(), "DD", va.getVariableName());
+                        	}
                         } else if (last == VariableAccess.UNDEFINITION && va.isReference()) { // UR
-                            this.rc.getReport().addRuleViolation(createRuleViolation(rc, inode.getSimpleNode(), va.getVariableName(), "UR"));
+                        	if (inode.getSimpleNode() != null) {
+                        		// preventing NullpointerException                        		
+                        		addDaaViolation(rc, inode.getSimpleNode(), "UR", va.getVariableName());
+                        	}
                         } else if (last == VariableAccess.DEFINITION && va.isUndefinition()) { // DU
-                            this.rc.getReport().addRuleViolation(createRuleViolation(rc, inode.getSimpleNode(), va.getVariableName(), "DU"));
+                        	if (inode.getSimpleNode() != null) {
+                        		addDaaViolation(rc, inode.getSimpleNode(), "DU", va.getVariableName());
+                        	} else {
+                        		// undefinition outside, get the node of the definition
+                        		SimpleNode lastSimpleNode = (SimpleNode)array.get(1);
+                        		if (lastSimpleNode != null) {
+                            		addDaaViolation(rc, lastSimpleNode, "DU", va.getVariableName());                           		
+                            	}
+                        	}
                         }
-*/
                     }
                     List array = new ArrayList();
                     array.add(new Integer(va.getAccessType()));
-                    array.add(new Integer(inode.getLine()));
+                    array.add(inode.getSimpleNode());
                     hash.put(va.getVariableName(), array);
                 }
             }
         }
+    }
+    
+    /**
+     * Adds a daa violation to the report.
+     *
+     * @param ctx  the RuleContext
+     * @param node the node that produces the violation
+     * @param msg  specific message to put in the report
+     */
+    private final void addDaaViolation(Object data, SimpleNode node, String msg, String var) {
+        RuleContext ctx = (RuleContext) data;
+        ctx.getReport().addRuleViolation(new DaaRuleViolation(this, ctx, node, msg, var));
     }
 }
