7d1ae38e9e2057d1d4e2d479d59ec7620f8c9faf
Allan Caplan <allancaplan@users.sourceforge.net>
2006-08-24 13:45:19 +0000

    New rule - dexter riley mentioned here http://sourceforge.net/forum/forum.php?thread_id=1548419&forum_id=188192
    I had been working on it, it's Pretty Much Done, but checking in so he can give it a go
    
    
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@4498 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/src/net/sourceforge/pmd/rules/migration/UnnecessaryCast.java
@@ -0,0 +1,77 @@
+package net.sourceforge.pmd.rules.migration;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import net.sourceforge.pmd.AbstractRule;
+import net.sourceforge.pmd.ast.ASTCastExpression;
+import net.sourceforge.pmd.ast.ASTClassOrInterfaceType;
+import net.sourceforge.pmd.ast.ASTFieldDeclaration;
+import net.sourceforge.pmd.ast.ASTLocalVariableDeclaration;
+import net.sourceforge.pmd.ast.ASTName;
+import net.sourceforge.pmd.ast.ASTVariableDeclaratorId;
+import net.sourceforge.pmd.ast.SimpleNode;
+import net.sourceforge.pmd.symboltable.NameOccurrence;
+
+public class UnnecessaryCast extends AbstractRule {
+
+    private static Set implClassNames = new HashSet();
+
+    static {
+        implClassNames.add("List");
+        implClassNames.add("Set");
+        implClassNames.add("Map");
+        implClassNames.add("java.util.List");
+        implClassNames.add("java.util.Set");
+        implClassNames.add("java.util.Map");
+        implClassNames.add("ArrayList");
+        implClassNames.add("HashSet");
+        implClassNames.add("HashMap");
+        implClassNames.add("LinkedHashMap");
+        implClassNames.add("LinkedHashSet");
+        implClassNames.add("TreeSet");
+        implClassNames.add("TreeMap");
+        implClassNames.add("Vector");
+        implClassNames.add("java.util.ArrayList");
+        implClassNames.add("java.util.HashSet");
+        implClassNames.add("java.util.HashMap");
+        implClassNames.add("java.util.LinkedHashMap");
+        implClassNames.add("java.util.LinkedHashSet");
+        implClassNames.add("java.util.TreeSet");
+        implClassNames.add("java.util.TreeMap");
+        implClassNames.add("java.util.Vector");
+    }
+
+    public Object visit(ASTLocalVariableDeclaration node, Object data) {
+        return process(node, data);
+    }
+
+    public Object visit(ASTFieldDeclaration node, Object data) {
+        return process(node, data);
+    }
+
+    private Object process(SimpleNode node, Object data) {
+        ASTClassOrInterfaceType cit = (ASTClassOrInterfaceType) node.getFirstChildOfType(ASTClassOrInterfaceType.class);
+        if (cit == null || !implClassNames.contains(cit.getImage())) {
+            return data;
+        }
+        cit = (ASTClassOrInterfaceType) cit.getFirstChildOfType(ASTClassOrInterfaceType.class);
+        if (cit == null) {
+            return data;
+        }
+        ASTVariableDeclaratorId decl = (ASTVariableDeclaratorId) node
+                .getFirstChildOfType(ASTVariableDeclaratorId.class);
+        List usages = decl.getUsages();
+        for (Iterator iter = usages.iterator(); iter.hasNext();) {
+            NameOccurrence no = (NameOccurrence) iter.next();
+            ASTName name = (ASTName) no.getLocation();
+            SimpleNode n = (SimpleNode) name.jjtGetParent().jjtGetParent().jjtGetParent();
+            if (ASTCastExpression.class.equals(n.getClass())) {
+                addViolation(data, n);
+            }
+        }
+        return null;
+    }
+}
