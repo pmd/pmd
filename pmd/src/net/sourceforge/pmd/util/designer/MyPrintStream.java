358f94eb651222500227e9934724ef2434c9e27a
Brian Remedios <hooperbloob@users.sourceforge.net>
2006-09-23 16:11:24 +0000

    minor optimization
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@4534 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/net/sourceforge/pmd/util/designer/MyPrintStream.java
+++ pmd/src/net/sourceforge/pmd/util/designer/MyPrintStream.java
@@ -4,16 +4,18 @@ import java.io.PrintStream;
 
 public class MyPrintStream extends PrintStream {
 
+    private StringBuffer buf = new StringBuffer();
+	
+	private static final String LINE_SEPARATOR = System.getProperty("line.separator");
+	
     public MyPrintStream() {
         super(System.out);
     }
 
-    private StringBuffer buf = new StringBuffer();
-
     public void println(String s) {
         super.println(s);
         buf.append(s);
-        buf.append(System.getProperty("line.separator"));
+        buf.append(LINE_SEPARATOR);
     }
 
     public String getString() {
