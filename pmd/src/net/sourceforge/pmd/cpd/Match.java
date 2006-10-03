9678c7f9f83ea16f4238798d58a8f9241ddc3db3
Brian Remedios <hooperbloob@users.sourceforge.net>
2006-10-03 01:49:03 +0000

    Migrated MatchLength comparator to live with the others
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@4550 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/net/sourceforge/pmd/cpd/Match.java
+++ pmd/src/net/sourceforge/pmd/cpd/Match.java
@@ -47,6 +47,14 @@ public class Match implements Comparable {
     	}
     };
     
+    public static final Comparator LengthComparator = new Comparator() {
+        public int compare(Object o1, Object o2) {
+            Match m1 = (Match) o1;
+            Match m2 = (Match) o2;
+            return m2.getLineCount() - m1.getLineCount();
+        }
+    };
+    
     public static class MatchCode {
 
         private int first;
