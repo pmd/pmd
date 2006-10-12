5503e6244b002fe39406ea86732b8e530073f177
Allan Caplan <allancaplan@users.sourceforge.net>
2006-10-12 18:16:17 +0000

    Removing DontMixJsfWithJspText test - it was empty, and there is no DontMixJsfWithJsp rule.
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@4630 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/regress/test/net/sourceforge/pmd/jsp/rules/DontMixJsfWithJspText.java
+++
@@ -1,7 +0,0 @@
-package test.net.sourceforge.pmd.jsp.rules;
-
-import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
-
-public class DontMixJsfWithJspText extends SimpleAggregatorTst {
-
-}
