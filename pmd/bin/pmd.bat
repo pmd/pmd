ad0bec0843f6fd3933d9dd63fbf33d4970864d24
Tom Copeland <tomcopeland@users.sourceforge.net>
2006-10-19 15:28:27 +0000

    Some tweaks from Aaron Optimizer Digulla
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@4704 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/bin/pmd.bat
+++ pmd/bin/pmd.bat
@@ -1,5 +1,2 @@
 @echo off
-rem set FILE=%1%
-rem set FORMAT=%2%
-rem set RULESETFILES=%3%
-java -cp %CLASSPATH%;..\lib\pmd-3.8.jar;..\lib\jaxen-1.1-beta-10.jar;..\lib\jakarta-oro-2.0.8.jar net.sourceforge.pmd.PMD %*
+java -cp ..\lib\pmd-3.8.jar;..\lib\jaxen-1.1-beta-10.jar;..\lib\jakarta-oro-2.0.8.jar;%CLASSPATH% net.sourceforge.pmd.PMD %*
