2aa188e17ede8971c1e486c0526563fff2b6a67f
Tom Copeland <tomcopeland@users.sourceforge.net>
2006-10-20 19:56:36 +0000

    The remainder of the fixes from Aaron, thanks much!
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@4718 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/bin/pmd.bat
+++ pmd/bin/pmd.bat
@@ -1,2 +1,2 @@
 @echo off
-java -cp ..\lib\pmd-3.8.jar;..\lib\jaxen-1.1-beta-10.jar;..\lib\jakarta-oro-2.0.8.jar;%CLASSPATH% net.sourceforge.pmd.PMD %*
+java -cp %~dp0\..\lib\pmd-3.8.jar;%~dp0\..\lib\jaxen-1.1-beta-10.jar;%~dp0\..\lib\jakarta-oro-2.0.8.jar;%CLASSPATH% net.sourceforge.pmd.PMD %*
