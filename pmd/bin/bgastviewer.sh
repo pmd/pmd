c36123e4017b737ff4f93455952aa0fa53e47ee4
Xavier Le Vourch <xlv@users.sourceforge.net>
2006-11-01 22:35:57 +0000

    asm updated to official 3.0 release
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@4774 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/bin/bgastviewer.sh
+++ pmd/bin/bgastviewer.sh
@@ -1,1 +1,1 @@
-java -cp ../build:../lib/asm-3.0_RC1.jar:../lib/pmd-3.8.jar:../lib/xercesImpl-2.6.2.jar:../lib/xmlParserAPIs-2.6.2.jar:../lib/jaxen-1.1-beta-10.jar net.sourceforge.pmd.util.viewer.Viewer
+java -cp ../build:../lib/asm-3.0.jar:../lib/pmd-3.8.jar:../lib/xercesImpl-2.6.2.jar:../lib/xmlParserAPIs-2.6.2.jar:../lib/jaxen-1.1-beta-10.jar net.sourceforge.pmd.util.viewer.Viewer
