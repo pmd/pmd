ab2f5a6a4f44e47628b805bb9b6e2c8981b07bca
Wouter Zelle <wfzelle@users.sourceforge.net>
2006-11-30 21:27:41 +0000

    cleanup of test using reinitialization in XML test-data file
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@4850 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/regress/test/net/sourceforge/pmd/rules/CloseResourceTest.java
+++ pmd/regress/test/net/sourceforge/pmd/rules/CloseResourceTest.java
@@ -5,32 +5,16 @@ package test.net.sourceforge.pmd.rules;
 
 import net.sourceforge.pmd.Rule;
 import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
-import test.net.sourceforge.pmd.testframework.TestDescriptor;
 
 public class CloseResourceTest extends SimpleAggregatorTst {
     private Rule rule;
-    private TestDescriptor[] tests;
 
     public void setUp() {
         rule = findRule("design", "CloseResource");
-        tests = extractTestsFromXml(rule);
         rule.addProperty("types", "Connection,Statement,ResultSet");  //Default
     }
 
     public void testAll() {
-        runTests(new TestDescriptor[] {tests[0], tests[1], tests[2], tests[3], tests[4]});
+        runTests(rule);
     }
-
-    public void testMultipleProperties(){
-        runTest(tests[5]);
-    }
-    
-    public void testTypes(){
-        runTest(tests[6]);
-    }
-    
-    public void testPropertySetter(){
-        runTest(tests[7]);
-    }
-
 }
--- pmd/regress/test/net/sourceforge/pmd/rules/EmptyCatchBlockRuleTest.java
+++ pmd/regress/test/net/sourceforge/pmd/rules/EmptyCatchBlockRuleTest.java
@@ -1,33 +1,19 @@
+/**
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+package test.net.sourceforge.pmd.rules;
 
- /**
-  * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
-  */
- package test.net.sourceforge.pmd.rules;
- 
- import net.sourceforge.pmd.Rule;
- import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
- import test.net.sourceforge.pmd.testframework.TestDescriptor;
- 
- public class EmptyCatchBlockRuleTest extends SimpleAggregatorTst {
-     private Rule rule;
-     private TestDescriptor[] tests;
- 
-     public void setUp() {
-         rule = findRule("basic", "EmptyCatchBlock");
-         tests = extractTestsFromXml(rule);
-     }
- 
-     public void testAll() {
-         runTests(tests);
-     }
- 
-     public void testCommentedBlocksAllowed() {
-         rule.addProperty("allowCommentedBlocks", "true");
-         runTests(new TestDescriptor[]{
-             new TestDescriptor(tests[7].getCode(), "single-line comment is OK", 0, rule),
-             new TestDescriptor(tests[8].getCode(), "multiple-line comment is OK", 0, rule),
-             new TestDescriptor(tests[9].getCode(), "Javadoc comment is OK", 0, rule),
-         });
-     }
- }
- 
+import net.sourceforge.pmd.Rule;
+import test.net.sourceforge.pmd.testframework.SimpleAggregatorTst;
+
+public class EmptyCatchBlockRuleTest extends SimpleAggregatorTst {
+    private Rule rule;
+
+    public void setUp() {
+        rule = findRule("basic", "EmptyCatchBlock");
+    }
+
+    public void testAll() {
+        runTests(rule);
+    }
+}
