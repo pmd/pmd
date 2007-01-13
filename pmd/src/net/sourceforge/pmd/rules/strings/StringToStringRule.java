f308f8c4cc19f4a9f2e39ac1a9b5dc24d9cd0254
Allan Caplan <allancaplan@users.sourceforge.net>
2007-01-13 03:55:30 +0000

    1634078 - False positive in StringToString
    StringToString rule wasn't taking arrays into account.
    
    
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@4944 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/net/sourceforge/pmd/rules/strings/StringToStringRule.java
+++ pmd/src/net/sourceforge/pmd/rules/strings/StringToStringRule.java
@@ -4,6 +4,7 @@
 package net.sourceforge.pmd.rules.strings;
 
 import net.sourceforge.pmd.AbstractRule;
+import net.sourceforge.pmd.ast.ASTName;
 import net.sourceforge.pmd.ast.ASTVariableDeclaratorId;
 import net.sourceforge.pmd.symboltable.NameOccurrence;
 
@@ -15,10 +16,15 @@ public class StringToStringRule extends AbstractRule {
         if (!node.getNameDeclaration().getTypeImage().equals("String")) {
             return data;
         }
+        boolean isArray = node.isArray();
         for (Iterator i = node.getUsages().iterator(); i.hasNext();) {
             NameOccurrence occ = (NameOccurrence) i.next();
-            if (occ.getNameForWhichThisIsAQualifier() != null && occ.getNameForWhichThisIsAQualifier().getImage().indexOf("toString") != -1) {
-                addViolation(data, occ.getLocation());
+            if (occ.getNameForWhichThisIsAQualifier() != null) {
+                if (!isArray && occ.getNameForWhichThisIsAQualifier().getImage().indexOf("toString") != -1) {
+                    addViolation(data, occ.getLocation());
+                } else if (isArray && occ.getNameForWhichThisIsAQualifier().getLocation() != null && !ASTName.class.equals(occ.getNameForWhichThisIsAQualifier().getLocation().getClass())) {
+                    addViolation(data, occ.getLocation());
+                }
             }
         }
         return data;
