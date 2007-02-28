976027415e41bf9d5d15fc55f85e5781a13bd4f6
Wouter Zelle <wfzelle@users.sourceforge.net>
2007-02-28 21:26:03 +0000

    Classpath now references the updated retroweaver jar
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@5065 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/java14/bin/cygwin-run.sh
+++ pmd/java14/bin/cygwin-run.sh
@@ -10,7 +10,7 @@ TOPDIR=../..
 VERSION=3.9
 PMDJAR=$TOPDIR/java14/lib/pmd14-$VERSION.jar
 JARPATH=$TOPDIR/lib/asm-3.0.jar:$TOPDIR/lib/jaxen-1.1.jar
-RWPATH=$TOPDIR/java14/lib/retroweaver-rt-2.0Beta3.jar:$TOPDIR/java14/lib/backport-util-concurrent.jar
+RWPATH=$TOPDIR/java14/lib/retroweaver-rt-2.0Beta4.jar:$TOPDIR/java14/lib/backport-util-concurrent.jar
 PMD_CLASSPATH=$JARPATH:$RWPATH
 if $cygwin; then
   FILE=`cygpath --windows "$FILE"`
