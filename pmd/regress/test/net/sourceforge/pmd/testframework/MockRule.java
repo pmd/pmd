905c69c55ee88bb1d13dc5053824a9a184b07ef2
Wouter Zelle <wfzelle@users.sourceforge.net>
2007-03-19 21:04:52 +0000

    cleanups
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@5080 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/regress/test/net/sourceforge/pmd/testframework/MockRule.java
+++ pmd/regress/test/net/sourceforge/pmd/testframework/MockRule.java
@@ -9,19 +9,15 @@ public class MockRule extends AbstractRule {
 
     public MockRule(String name, String description, String message, String ruleSetName) {
         super();
-        this.name = name;
-        this.description = description;
-        this.message = message;
-        this.ruleSetName = ruleSetName;
+        setName(name);
+        setDescription(description);
+        setMessage(message);
+        setRuleSetName(ruleSetName);
     }
 
     public MockRule(String name, String description, String message, String ruleSetName, int priority) {
-        super();
-        this.name = name;
-        this.description = description;
-        this.message = message;
-        this.ruleSetName = ruleSetName;
-        this.priority = priority;
+        this(name, description, message, ruleSetName);
+        setPriority(priority);
     }
 
     public MockRule() {
