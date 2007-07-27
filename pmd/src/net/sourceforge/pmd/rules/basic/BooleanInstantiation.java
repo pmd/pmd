e8a48cc6ba40e929b6077a245e522bdc2399b521
Wouter Zelle <wfzelle@users.sourceforge.net>
2007-07-27 15:19:50 +0000

    Merged patch 1752540 to fix bug 1744065 (false positive for custom Boolean)
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@5434 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/net/sourceforge/pmd/rules/basic/BooleanInstantiation.java
+++ pmd/src/net/sourceforge/pmd/rules/basic/BooleanInstantiation.java
@@ -5,6 +5,7 @@ import net.sourceforge.pmd.ast.ASTAllocationExpression;
 import net.sourceforge.pmd.ast.ASTArrayDimsAndInits;
 import net.sourceforge.pmd.ast.ASTBooleanLiteral;
 import net.sourceforge.pmd.ast.ASTClassOrInterfaceType;
+import net.sourceforge.pmd.ast.ASTImportDeclaration;
 import net.sourceforge.pmd.ast.ASTLiteral;
 import net.sourceforge.pmd.ast.ASTName;
 import net.sourceforge.pmd.ast.ASTPrimaryExpression;
@@ -14,58 +15,83 @@ import net.sourceforge.pmd.ast.ASTPrimarySuffix;
 /**
  * Avoid instantiating Boolean objects; you can reference Boolean.TRUE,
  * Boolean.FALSE, or call Boolean.valueOf() instead.
- * 
+ *
  * <pre>
- *  public class Foo { 
+ *  public class Foo {
  *       Boolean bar = new Boolean("true");    // just do a Boolean
  *       bar = Boolean.TRUE;                   //ok
- *       Boolean buz = Boolean.valueOf(false); // just do a Boolean buz = Boolean.FALSE; 
+ *       Boolean buz = Boolean.valueOf(false); // just do a Boolean buz = Boolean.FALSE;
  *  }
  * </pre>
  */
 public class BooleanInstantiation extends AbstractRule {
 
+	/*
+	 *  see bug 1744065 : If somebody create it owns Boolean, the rule should not be triggered
+	 *   Therefore, we use this boolean to flag if the source code contains such an import
+	 *
+	 */
+	private boolean customBoolean;
+
+	public BooleanInstantiation() {
+		customBoolean = false;
+	}
+
+	public Object visit(ASTImportDeclaration decl,Object data) {
+		// If the import actually import a Boolean class that overrides java.lang.Boolean
+		if ( decl.getImportedName().endsWith("Boolean") && ! decl.getImportedName().equals("java.lang"))
+		{
+			customBoolean = true;
+		}
+		return super.visit(decl, data);
+	}
+
     public Object visit(ASTAllocationExpression node, Object data) {
 
-        if (node.findChildrenOfType(ASTArrayDimsAndInits.class).size() > 0) {
-            return super.visit(node, data);
-        }
-        String typeName = ((ASTClassOrInterfaceType) node.jjtGetChild(0)).getImage();
-        if ("Boolean".equals(typeName) || "java.lang.Boolean".equals(typeName)) {
-            super.addViolation(data, node);
-            return data;
-        }
+    	if ( ! customBoolean ) {
+	        if (node.findChildrenOfType(ASTArrayDimsAndInits.class).size() > 0) {
+	            return super.visit(node, data);
+	        }
+	        String typeName = ((ASTClassOrInterfaceType) node.jjtGetChild(0)).getImage();
+	        if ("Boolean".equals(typeName) || "java.lang.Boolean".equals(typeName)) {
+	            super.addViolation(data, node);
+	            return data;
+	        }
+    	}
         return super.visit(node, data);
     }
 
     public Object visit(ASTPrimaryPrefix node, Object data) {
 
-        if (node.jjtGetNumChildren() == 0 || !node.jjtGetChild(0).getClass().equals(ASTName.class)) {
-            return super.visit(node, data);
-        }
+    	if ( ! customBoolean )
+    	{
+	        if (node.jjtGetNumChildren() == 0 || !node.jjtGetChild(0).getClass().equals(ASTName.class)) {
+	            return super.visit(node, data);
+	        }
 
-        if ("Boolean.valueOf".equals(((ASTName) node.jjtGetChild(0)).getImage())
-                || "java.lang.Boolean.valueOf".equals(((ASTName) node.jjtGetChild(0)).getImage())) {
-            ASTPrimaryExpression parent = (ASTPrimaryExpression) node.jjtGetParent();
-            ASTPrimarySuffix suffix = parent.getFirstChildOfType(ASTPrimarySuffix.class);
-            if (suffix == null) {
-                return super.visit(node, data);
-            }
-            ASTPrimaryPrefix prefix = suffix.getFirstChildOfType(ASTPrimaryPrefix.class);
-            if (prefix == null) {
-                return super.visit(node, data);
-            }
+	        if ("Boolean.valueOf".equals(((ASTName) node.jjtGetChild(0)).getImage())
+	                || "java.lang.Boolean.valueOf".equals(((ASTName) node.jjtGetChild(0)).getImage())) {
+	            ASTPrimaryExpression parent = (ASTPrimaryExpression) node.jjtGetParent();
+	            ASTPrimarySuffix suffix = parent.getFirstChildOfType(ASTPrimarySuffix.class);
+	            if (suffix == null) {
+	                return super.visit(node, data);
+	            }
+	            ASTPrimaryPrefix prefix = suffix.getFirstChildOfType(ASTPrimaryPrefix.class);
+	            if (prefix == null) {
+	                return super.visit(node, data);
+	            }
 
-            if (prefix.getFirstChildOfType(ASTBooleanLiteral.class) != null) {
-                super.addViolation(data, node);
-                return data;
-            }
-            ASTLiteral literal = prefix.getFirstChildOfType(ASTLiteral.class);
-            if (literal != null && ("\"true\"".equals(literal.getImage()) || "\"false\"".equals(literal.getImage()))) {
-                super.addViolation(data, node);
-                return data;
-            }
-        }
+	            if (prefix.getFirstChildOfType(ASTBooleanLiteral.class) != null) {
+	                super.addViolation(data, node);
+	                return data;
+	            }
+	            ASTLiteral literal = prefix.getFirstChildOfType(ASTLiteral.class);
+	            if (literal != null && ("\"true\"".equals(literal.getImage()) || "\"false\"".equals(literal.getImage()))) {
+	                super.addViolation(data, node);
+	                return data;
+	            }
+	        }
+    	}
         return super.visit(node, data);
     }
 }
