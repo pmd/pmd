dc0dcb9df136c3cb88a5c8c10146d876657e31d2
Wouter Zelle <wfzelle@users.sourceforge.net>
2007-07-29 18:34:13 +0000

    Merged patch 1757669 to fix bug 1522517
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@5436 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/net/sourceforge/pmd/rules/UselessOverridingMethod.java
+++ pmd/src/net/sourceforge/pmd/rules/UselessOverridingMethod.java
@@ -3,6 +3,9 @@
  */
 package net.sourceforge.pmd.rules;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import net.sourceforge.pmd.AbstractRule;
 import net.sourceforge.pmd.ast.ASTArgumentList;
 import net.sourceforge.pmd.ast.ASTArguments;
@@ -11,21 +14,40 @@ import net.sourceforge.pmd.ast.ASTClassOrInterfaceBodyDeclaration;
 import net.sourceforge.pmd.ast.ASTClassOrInterfaceDeclaration;
 import net.sourceforge.pmd.ast.ASTFormalParameter;
 import net.sourceforge.pmd.ast.ASTFormalParameters;
+import net.sourceforge.pmd.ast.ASTImplementsList;
 import net.sourceforge.pmd.ast.ASTMethodDeclaration;
 import net.sourceforge.pmd.ast.ASTMethodDeclarator;
 import net.sourceforge.pmd.ast.ASTName;
+import net.sourceforge.pmd.ast.ASTNameList;
 import net.sourceforge.pmd.ast.ASTPrimaryExpression;
 import net.sourceforge.pmd.ast.ASTPrimaryPrefix;
 import net.sourceforge.pmd.ast.ASTPrimarySuffix;
+import net.sourceforge.pmd.ast.ASTResultType;
 import net.sourceforge.pmd.ast.ASTStatement;
 import net.sourceforge.pmd.ast.ASTVariableDeclaratorId;
 import net.sourceforge.pmd.ast.Node;
 import net.sourceforge.pmd.ast.SimpleNode;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.jaxen.JaxenException;
 
+/**
+ * @author Romain Pelisse, bugfix for [ 1522517 ] False +: UselessOverridingMethod
+ */
 public class UselessOverridingMethod extends AbstractRule {
+	private List<String> exceptions;
+	private static final String CLONE = "clone";
+	private static final String OBJECT = "Object";
+
+	public UselessOverridingMethod()
+	{
+        exceptions = new ArrayList<String>(1);
+        exceptions.add("CloneNotSupportedException");
+	}
+
+	public Object visit(ASTImplementsList clz, Object data)
+	{
+		return super.visit(clz,data);
+	}
 
     public Object visit(ASTClassOrInterfaceDeclaration clz, Object data) {
         if (clz.isInterface()) {
@@ -34,12 +56,72 @@ public class UselessOverridingMethod extends AbstractRule {
         return super.visit(clz, data);
     }
 
+    //TODO: this method should be externalize into an utility class, shouldn't it ?
+    private boolean isMethodType(ASTMethodDeclaration node,String methodType)
+    {
+    	boolean result = false;
+    	ASTResultType type = node.getResultType();
+    	if ( node != null && type != null ) {
+    		List results = null;
+            try {
+	            results = type.findChildNodesWithXPath("./Type/ReferenceType/ClassOrInterfaceType[@Image = '" + methodType + "']");
+            }
+            catch (JaxenException e) {
+	            e.printStackTrace();
+            }
+    		if ( results != null && results.size() > 0 ) {
+    			result = true;
+    		}
+    	}
+    	return result;
+    }
+
+    //TODO: this method should be externalize into an utility class, shouldn't it ?
+    private boolean isMethodThrowingType(ASTMethodDeclaration node, List<String> exceptedExceptions) {
+    	boolean result = false;
+	    ASTNameList thrownsExceptions = node.getFirstChildOfType(ASTNameList.class);
+	    if ( thrownsExceptions != null ) {
+	    	List<ASTName> names = thrownsExceptions.findChildrenOfType(ASTName.class);
+	    	for ( ASTName name : names ) {
+	    		for ( String exceptedException : exceptedExceptions) {
+		    		if ( exceptedException.equals(name.getImage()) )
+		    			result = true;
+	    		}
+	    	}
+	    }
+	    return result;
+    }
+
+	private boolean hasArguments(ASTMethodDeclaration node) {
+		boolean result = false;
+		try
+		{
+			List parameters = node.findChildNodesWithXPath("./MethodDeclarator/FormalParameters/*");
+			if ( parameters != null && parameters.size() < 0 ) {
+				result = true;
+			}
+		} catch (JaxenException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
     public Object visit(ASTMethodDeclaration node, Object data) {
         // Can skip abstract methods and methods whose only purpose is to
         // guarantee that the inherited method is not changed by finalizing
         // them.
         if (node.isAbstract() || node.isFinal() || node.isNative() || node.isSynchronized()) {
             return super.visit(node, data);
+        }
+        // We can also skip the 'clone' method as they are generally
+        // 'useless' but as it is considered a 'good practise' to
+        // implement them anyway ( see bug 1522517)
+        if ( CLONE.equals(node.getMethodName()) && node.isPublic() &&
+        	 ! this.hasArguments(node) &&
+        	 this.isMethodType(node, OBJECT) &&
+        	 this.isMethodThrowingType(node,exceptions) )
+        {
+        	return super.visit(node,data);
         }
 
         ASTBlock block = node.getBlock();
@@ -116,7 +198,9 @@ public class UselessOverridingMethod extends AbstractRule {
         return super.visit(node, data);
     }
 
-    public <T> List<T> findFirstDegreeChildrenOfType(SimpleNode n, Class<T> targetType) {
+
+
+	public <T> List<T> findFirstDegreeChildrenOfType(SimpleNode n, Class<T> targetType) {
         List<T> l = new ArrayList<T>();
         lclFindChildrenOfType(n, targetType, l);
         return l;
