94ac956d771ff33f9e295f9b0bf350bfea4a4f37
Allan Caplan <allancaplan@users.sourceforge.net>
2007-08-13 14:55:59 +0000

    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@5463 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/src/net/sourceforge/pmd/jaxen/TypeOfFunction.java
@@ -0,0 +1,53 @@
+package net.sourceforge.pmd.jaxen;
+
+import net.sourceforge.pmd.ast.ASTClassOrInterfaceType;
+import net.sourceforge.pmd.ast.Node;
+
+import org.jaxen.Context;
+import org.jaxen.Function;
+import org.jaxen.FunctionCallException;
+import org.jaxen.SimpleFunctionContext;
+import org.jaxen.XPathFunctionContext;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TypeOfFunction implements Function {
+
+    public static void registerSelfInSimpleContext() {
+        ((SimpleFunctionContext) XPathFunctionContext.getInstance()).registerFunction(null, "typeof", new TypeOfFunction());
+    }
+
+    // TEST //ClassOrInterfaceType[typeof(@Image, 'java.lang.String')]
+    public Object call(Context context, List args) throws FunctionCallException {
+        if (args.isEmpty()) {
+            return Boolean.FALSE;
+        }
+        Node n = (Node) context.getNodeSet().get(0);
+        if (n instanceof ASTClassOrInterfaceType) {
+            List attributes = (List) args.get(0);
+            Attribute attr = (Attribute) attributes.get(0);
+            Class type = ((ASTClassOrInterfaceType) n).getType();
+            String typeName = (String) args.get(1);
+            String shortName = (args.size() > 2) ? (String) args.get(2) : "";
+            if (type == null) {
+                return typeName.equals(attr.getValue()) || shortName.equals(attr.getValue());
+            }
+            if (type.getName().equals(typeName) || type.getName().equals(attr.getValue())) {
+                return Boolean.TRUE;
+            }
+            List<Class> implementors = Arrays.asList(type.getInterfaces());
+            if (implementors.contains(type)) {
+                return Boolean.TRUE;
+            }
+            Class superC = type.getSuperclass();
+            while (superC != null && !superC.equals(Object.class)) {
+                if (superC.getName().equals(typeName)) {
+                    return Boolean.TRUE;
+                }
+                superC = superC.getSuperclass();
+            }
+        }
+        return Boolean.FALSE;
+    }
+}
