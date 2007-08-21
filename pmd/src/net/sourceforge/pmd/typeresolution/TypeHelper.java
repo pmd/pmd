2413f78fbe25cea7fd9ab0216721ad4ff7bee508
Allan Caplan <allancaplan@users.sourceforge.net>
2007-08-21 12:37:59 +0000

    First changes moving PMD to Type Resolution
    - All string checks for String / StringBuffer have been replaced with Type Resolution friendly checks
    
    - New helper class to centralize this check
    
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@5470 51baf565-9d33-0410-a72c-fc3788e3496d

---
+++ pmd/src/net/sourceforge/pmd/typeresolution/TypeHelper.java
@@ -0,0 +1,46 @@
+package net.sourceforge.pmd.typeresolution;
+
+import java.util.Arrays;
+import java.util.List;
+
+import net.sourceforge.pmd.ast.ASTClassOrInterfaceType;
+import net.sourceforge.pmd.ast.SimpleNode;
+import net.sourceforge.pmd.ast.TypeNode;
+import net.sourceforge.pmd.jaxen.Attribute;
+import net.sourceforge.pmd.symboltable.VariableNameDeclaration;
+
+public class TypeHelper {
+
+    public static boolean isA(TypeNode n, Class clazz) {
+        return subclasses(n, clazz);
+    }
+
+    public static boolean isA(VariableNameDeclaration vnd, Class clazz) {
+        Class type = vnd.getType();
+        return ((type != null && type.equals(clazz)) || (type == null && (clazz.getSimpleName().equals(vnd.getTypeImage()) || clazz.getName().equals(vnd.getTypeImage()))));
+    }
+
+    public static boolean subclasses(TypeNode n, Class clazz) {
+        Class type = n.getType();
+        if (type == null) {
+            return (clazz.getSimpleName().equals(((SimpleNode) n).getImage()) || clazz.getName().equals(((SimpleNode) n).getImage()));
+        }
+
+        if (type.equals(clazz)) {
+            return Boolean.TRUE;
+        }
+
+        List<Class> implementors = Arrays.asList(type.getInterfaces());
+        if (implementors.contains(clazz)) {
+            return Boolean.TRUE;
+        }
+        Class superC = type.getSuperclass();
+        while (superC != null && !superC.equals(Object.class)) {
+            if (superC.equals(clazz)) {
+                return Boolean.TRUE;
+            }
+            superC = superC.getSuperclass();
+        }
+        return Boolean.FALSE;
+    }
+}
