6565fdd28ec3bce86af1abaf34f2fb90d3be317d
Wouter Zelle <wfzelle@users.sourceforge.net>
2007-08-24 11:39:02 +0000

    Fixed bug 1702782 - false positive in UselessOperationOnImmutable when an Immutable on which an operation is performed is compareTo'd
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@5479 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/net/sourceforge/pmd/rules/UselessOperationOnImmutable.java
+++ pmd/src/net/sourceforge/pmd/rules/UselessOperationOnImmutable.java
@@ -1,13 +1,18 @@
 package net.sourceforge.pmd.rules;
 
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 import net.sourceforge.pmd.AbstractRule;
 import net.sourceforge.pmd.ast.ASTConditionalExpression;
 import net.sourceforge.pmd.ast.ASTExpression;
 import net.sourceforge.pmd.ast.ASTLocalVariableDeclaration;
+import net.sourceforge.pmd.ast.ASTPrimaryExpression;
+import net.sourceforge.pmd.ast.ASTPrimarySuffix;
 import net.sourceforge.pmd.ast.ASTType;
 import net.sourceforge.pmd.ast.ASTVariableDeclaratorId;
+import net.sourceforge.pmd.ast.Node;
 import net.sourceforge.pmd.ast.SimpleNode;
 import net.sourceforge.pmd.symboltable.NameOccurrence;
 import net.sourceforge.pmd.util.CollectionUtil;
@@ -18,7 +23,6 @@ import net.sourceforge.pmd.util.CollectionUtil;
  * ignoring the operation result is an error.
  */
 public class UselessOperationOnImmutable extends AbstractRule {
-
     /**
      * These are the methods which are immutable
      */
@@ -40,8 +44,10 @@ public class UselessOperationOnImmutable extends AbstractRule {
             // FIXME - getUsages will return everything with the same name as the variable, 
             // see JUnit test, case 6. Changing to SimpleNode below, revisit when getUsages is fixed
             SimpleNode sn = no.getLocation();
-            Class parentClass = sn.jjtGetParent().jjtGetParent().jjtGetParent().getClass();
-            if (!(parentClass.equals(ASTExpression.class) || parentClass.equals(ASTConditionalExpression.class))) {
+            Node primaryExpression = sn.jjtGetParent().jjtGetParent();
+			Class<? extends Node> parentClass = primaryExpression.jjtGetParent().getClass();
+            if (!(parentClass.equals(ASTExpression.class) || parentClass.equals(ASTConditionalExpression.class) || 
+            		hasComparisons(primaryExpression))) {
                 String methodCall = sn.getImage().substring(variableName.length());
                 if (targetMethods.contains(methodCall)) {
                     addViolation(data, sn);
@@ -50,6 +56,23 @@ public class UselessOperationOnImmutable extends AbstractRule {
         }
         return super.visit(node, data);
     }
+
+    /**
+     * Check whether the Immutable is compareTo'd something
+     */
+	private boolean hasComparisons(Node primaryExpression) {
+		if (primaryExpression.getClass().equals(ASTPrimaryExpression.class)) {
+			List<ASTPrimarySuffix> suffixes = ((ASTPrimaryExpression)primaryExpression).findChildrenOfType(ASTPrimarySuffix.class);
+			for (Iterator<ASTPrimarySuffix> iterator = suffixes.iterator(); iterator.hasNext();) {
+				ASTPrimarySuffix suffix = iterator.next();
+				if ("compareTo".equals(suffix.getImage()))
+					return true;
+			}
+		} else {
+			//Some weird usage of the Immutable
+		}
+		return false;	//No comparison
+	}
 
     /**
      * This method checks the variable declaration if it is on a class we care
