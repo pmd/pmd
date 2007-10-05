3aac6046b4491c10910a554af2b99604735fd08a
Xavier Le Vourch <xlv@users.sourceforge.net>
2007-10-05 18:18:09 +0000

    retroweaver updated to 2.0.1 version
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@5508 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/java14/bin/cygwin-run.sh
+++ pmd/java14/bin/cygwin-run.sh
@@ -10,7 +10,7 @@ TOPDIR=../..
 VERSION=4.0
 PMDJAR=$TOPDIR/java14/lib/pmd14-$VERSION.jar
 JARPATH=$TOPDIR/lib/asm-3.0.jar:$TOPDIR/lib/jaxen-1.1.jar
-RWPATH=$TOPDIR/java14/lib/retroweaver-rt-2.0Beta4.jar:$TOPDIR/java14/lib/backport-util-concurrent.jar
+RWPATH=$TOPDIR/java14/lib/retroweaver-rt-2.0.1.jar:$TOPDIR/java14/lib/backport-util-concurrent.jar
 PMD_CLASSPATH=$JARPATH:$RWPATH
 if $cygwin; then
   FILE=`cygpath --windows "$FILE"`
