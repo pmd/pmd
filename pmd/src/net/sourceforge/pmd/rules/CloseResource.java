115365cd083dfe8d5c87e5fe63dddc5533d37af6
Romain Pelisse <rpelisse@users.sourceforge.net>
2007-10-09 00:53:44 +0000

    Adding a bunch of new rules (see changelog), with the proper unit-testing.
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@5510 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/net/sourceforge/pmd/rules/CloseResource.java
+++ pmd/src/net/sourceforge/pmd/rules/CloseResource.java
@@ -3,139 +3,172 @@
  */
 package net.sourceforge.pmd.rules;
 
-import net.sourceforge.pmd.AbstractRule;
-import net.sourceforge.pmd.PropertyDescriptor;
-import net.sourceforge.pmd.ast.ASTBlock;
-import net.sourceforge.pmd.ast.ASTClassOrInterfaceType;
-import net.sourceforge.pmd.ast.ASTCompilationUnit;
-import net.sourceforge.pmd.ast.ASTLocalVariableDeclaration;
-import net.sourceforge.pmd.ast.ASTMethodDeclaration;
-import net.sourceforge.pmd.ast.ASTName;
-import net.sourceforge.pmd.ast.ASTReferenceType;
-import net.sourceforge.pmd.ast.ASTTryStatement;
-import net.sourceforge.pmd.ast.ASTType;
-import net.sourceforge.pmd.ast.ASTVariableDeclaratorId;
-import net.sourceforge.pmd.ast.Node;
-import net.sourceforge.pmd.properties.StringProperty;
+import java.util.*;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.StringTokenizer;
+import net.sourceforge.pmd.*;
+import net.sourceforge.pmd.ast.*;
 
 /**
- * Makes sure you close your database connections. It does this by
- * looking for code patterned like this:
- * <pre>
- *  Connection c = X;
- *  try {
- *   // do stuff, and maybe catch something
- *  } finally {
- *   c.close();
- *  }
- * </pre>
+ *
+ *
+ * @author Pierre MATHIEU
+ * @author Romain PELISSE, belaran@gmail.com
+ *
  */
-public class CloseResource extends AbstractRule {
+public class CloseResource extends AbstractRule
+{
+  private List types = new ArrayList();
+  private List closers = new ArrayList();
 
-    private Set<String> types = new HashSet<String>();
+  /**
+   *
+   * @param node
+   * @param data
+   * @return
+   */
+  public Object visit(ASTCompilationUnit node, Object data)
+  {
+    if (!importsPackage(node))
+    {
+      return data;
+    }
+    if (types.isEmpty())
+    {
+      for (StringTokenizer st = new StringTokenizer(getStringProperty("types"), ","); st.hasMoreTokens();)
+      {
+        types.add(st.nextToken());
+      }
+    }
+    if (closers.isEmpty())
+    {
+      for (StringTokenizer st = new StringTokenizer(getStringProperty("closers"), ","); st.hasMoreTokens();)
+      {
+        closers.add(st.nextToken());
+      }
+    }
+    return super.visit(node, data);
+  }
 
-    private Set<String> closeTargets = new HashSet<String>();
-    private static final PropertyDescriptor closeTargetsDescriptor = new StringProperty("closeTargets",
-            "Methods which may close this resource", "", 1.0f);
+  /**
+   *
+   * @param node
+   * @param data
+   * @return
+   */
+  public Object visit(ASTMethodDeclaration node, Object data)
+  {
+    List vars = node.findChildrenOfType(ASTLocalVariableDeclaration.class);
+    List ids = new Vector();
+    List clazzes = new Vector();
 
-    private static final PropertyDescriptor typesDescriptor = new StringProperty("types",
-            "Types that are affected by this rule", "", 2.0f);
+    // find all variable references to Connection objects
+    for (Iterator it = vars.iterator(); it.hasNext();)
+    {
+      ASTLocalVariableDeclaration var = (ASTLocalVariableDeclaration)it.next();
+      ASTType type = (ASTType)var.jjtGetChild(0);
 
-    private static final Map<String, PropertyDescriptor> propertyDescriptorsByName = asFixedMap(new PropertyDescriptor[] { typesDescriptor, closeTargetsDescriptor });
-
-    protected Map<String, PropertyDescriptor> propertiesByName() {
-        return propertyDescriptorsByName;
+      if (type.jjtGetChild(0) instanceof ASTReferenceType)
+      {
+        ASTReferenceType ref = (ASTReferenceType)type.jjtGetChild(0);
+        if (ref.jjtGetChild(0) instanceof ASTClassOrInterfaceType)
+        {
+          ASTClassOrInterfaceType clazz = (ASTClassOrInterfaceType)ref.jjtGetChild(0);
+          if (types.contains(clazz.getImage()))
+          {
+            ASTVariableDeclaratorId id = (ASTVariableDeclaratorId)var.jjtGetChild(1).jjtGetChild(0);
+            ids.add(id);
+            clazzes.add(clazz.getImage());
+          }
+        }
+      }
     }
 
-    public Object visit(ASTCompilationUnit node, Object data) {
-        if (closeTargets.isEmpty() && getStringProperty(closeTargetsDescriptor) != null) {
-            for (StringTokenizer st = new StringTokenizer(getStringProperty(closeTargetsDescriptor), ","); st.hasMoreTokens();) {
-                closeTargets.add(st.nextToken());
-            }
+    // if there are connections, ensure each is closed.
+    for (int i = 0; i < ids.size(); i++)
+    {
+      ASTVariableDeclaratorId x = (ASTVariableDeclaratorId)ids.get(i);
+      String clazz = (String)clazzes.get(i);
+      ensureClosed(clazz, (ASTLocalVariableDeclaration)x.jjtGetParent().jjtGetParent(), x, data);
+    }
+    return data;
+  }
+
+  /**
+   *
+   * @param className
+   * @param var
+   * @param id
+   * @param data
+   */
+  private void ensureClosed(String className, ASTLocalVariableDeclaration var, ASTVariableDeclaratorId id, Object data)
+  {
+    // What are the chances of a Connection being instantiated in a
+    // for-loop init block? Anyway, I'm lazy!
+    String closer = (String)closers.get(types.indexOf(className));
+    String target = id.getImage() + "." + closer;
+    Node n = var;
+
+    while (!((n = n.jjtGetParent()) instanceof ASTBlock));
+
+    ASTBlock top = (ASTBlock)n;
+
+    List tryblocks = new Vector();
+    top.findChildrenOfType(ASTTryStatement.class, tryblocks, true);
+
+    boolean closed = false;
+
+    // look for try blocks below the line the variable was
+    // introduced and make sure there is a .close call in a finally
+    // block.
+    for (Iterator it = tryblocks.iterator(); it.hasNext();)
+    {
+      ASTTryStatement t = (ASTTryStatement)it.next();
+
+      if ((t.getBeginLine() > id.getBeginLine()) && (t.hasFinally()))
+      {
+        ASTBlock f = (ASTBlock)t.getFinally().jjtGetChild(0);
+        List names = new ArrayList();
+        f.findChildrenOfType(ASTName.class, names, true);
+        for (Iterator it2 = names.iterator(); it2.hasNext();)
+        {
+          if (((ASTName)it2.next()).getImage().equals(target))
+          {
+            closed = true;
+          }
         }
-        if (types.isEmpty() && getStringProperty(typesDescriptor) != null) {
-            for (StringTokenizer st = new StringTokenizer(getStringProperty(typesDescriptor), ","); st.hasMoreTokens();) {
-                types.add(st.nextToken());
-            }
-        }
-        return super.visit(node, data);
+      }
     }
 
-    public Object visit(ASTMethodDeclaration node, Object data) {
-        List<ASTLocalVariableDeclaration> vars = node.findChildrenOfType(ASTLocalVariableDeclaration.class);
-        List<ASTVariableDeclaratorId> ids = new ArrayList<ASTVariableDeclaratorId>();
-
-        // find all variable references to Connection objects
-        for (ASTLocalVariableDeclaration var: vars) {
-            ASTType type = var.getTypeNode();
-
-            if (type.jjtGetChild(0) instanceof ASTReferenceType) {
-                ASTReferenceType ref = (ASTReferenceType) type.jjtGetChild(0);
-                if (ref.jjtGetChild(0) instanceof ASTClassOrInterfaceType) {
-                    ASTClassOrInterfaceType clazz = (ASTClassOrInterfaceType) ref.jjtGetChild(0);
-                    if (types.contains(clazz.getImage())) {
-                        ASTVariableDeclaratorId id = (ASTVariableDeclaratorId) var.jjtGetChild(1).jjtGetChild(0);
-                        ids.add(id);
-                    }
-                }
-            }
-        }
-
-        // if there are connections, ensure each is closed.
-        for (ASTVariableDeclaratorId x : ids) {
-            ensureClosed((ASTLocalVariableDeclaration) x.jjtGetParent().jjtGetParent(), x, data);
-        }
-        return data;
+    // if all is not well, complain
+    if (!closed)
+    {
+      ASTType type = (ASTType)var.jjtGetChild(0);
+      ASTReferenceType ref = (ASTReferenceType)type.jjtGetChild(0);
+      ASTClassOrInterfaceType clazz = (ASTClassOrInterfaceType)ref.jjtGetChild(0);
+      addViolation(data, id, clazz.getImage());
     }
+  }
 
-    private void ensureClosed(ASTLocalVariableDeclaration var,
-                              ASTVariableDeclaratorId id, Object data) {
-        // What are the chances of a Connection being instantiated in a
-        // for-loop init block? Anyway, I'm lazy!
-        String target = id.getImage() + ".close";
-        Node n = var;
-
-        while (!(n instanceof ASTBlock)) {
-            n = n.jjtGetParent();
-        }
-
-        ASTBlock top = (ASTBlock) n;
-
-        List<ASTTryStatement> tryblocks = new ArrayList<ASTTryStatement>();
-        top.findChildrenOfType(ASTTryStatement.class, tryblocks, true);
-
-        boolean closed = false;
-
-        // look for try blocks below the line the variable was
-        // introduced and make sure there is a .close call in a finally
-        // block.
-        for (ASTTryStatement t : tryblocks) {
-            if ((t.getBeginLine() > id.getBeginLine()) && (t.hasFinally())) {
-                ASTBlock f = (ASTBlock) t.getFinally().jjtGetChild(0);
-                List<ASTName> names = new ArrayList<ASTName>();
-                f.findChildrenOfType(ASTName.class, names, true);
-                for (ASTName oName : names) {
-                    String name = oName.getImage();
-                    if (name.equals(target) || closeTargets.contains(name)) {
-                        closed = true;
-                    }
-                }
-            }
-        }
-
-        // if all is not well, complain
-        if (!closed) {
-            ASTType type = (ASTType) var.jjtGetChild(0);
-            ASTReferenceType ref = (ASTReferenceType) type.jjtGetChild(0);
-            ASTClassOrInterfaceType clazz = (ASTClassOrInterfaceType) ref.jjtGetChild(0);
-            addViolation(data, id, clazz.getImage());
-        }
+  /**
+   *
+   * @param node
+   * @return
+   */
+  private boolean importsPackage(ASTCompilationUnit node)
+  {
+    List nodes = node.findChildrenOfType(ASTImportDeclaration.class);
+    for (Iterator i = nodes.iterator(); i.hasNext();)
+    {
+      ASTImportDeclaration n = (ASTImportDeclaration)i.next();
+      if (n.getPackageName().startsWith("java.sql"))
+      {
+        return true;
+      }
+      if (n.getPackageName().startsWith("com.renault.dao"))
+      {
+        return true;
+      }
     }
+    return false;
+  }
 }
