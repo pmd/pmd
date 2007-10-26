701095c41b44ea8404b20df77c3774ff25cbf26b
Tom Copeland <tomcopeland@users.sourceforge.net>
2007-10-26 02:08:30 +0000

    tweak
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@5589 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/net/sourceforge/pmd/AbstractRule.java
+++ pmd/src/net/sourceforge/pmd/AbstractRule.java
@@ -409,7 +409,7 @@ public abstract class AbstractRule extends JavaParserVisitorAdapter implements R
     /**
      * Adds a violation to the report.
      *
-     * @param ctx   the RuleContext
+     * @param data   the RuleContext
      * @param node  the node that produces the violation
      * @param embed a variable to embed in the rule violation message
      */
@@ -421,7 +421,7 @@ public abstract class AbstractRule extends JavaParserVisitorAdapter implements R
     /**
      * Adds a violation to the report.
      *
-     * @param ctx  the RuleContext
+     * @param data  the RuleContext
      * @param node the node that produces the violation, may be null, in which case all line and column info will be set to zero
      * @param args objects to embed in the rule violation message
      */
