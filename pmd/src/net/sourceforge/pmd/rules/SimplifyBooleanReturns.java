810a6d5d07ebdb221c9afd91480083ab131a366d
Xavier Le Vourch <xlv@users.sourceforge.net>
2008-03-01 03:43:55 +0000

    bug fixes in SimplifyBooleanReturns:
    NPE during parsing
    
    only methods returning booleans should be checked
    
    test case was incorrect (not a boolean method)
    
    
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@5835 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/net/sourceforge/pmd/rules/SimplifyBooleanReturns.java
+++ pmd/src/net/sourceforge/pmd/rules/SimplifyBooleanReturns.java
@@ -7,16 +7,37 @@ import net.sourceforge.pmd.AbstractRule;
 import net.sourceforge.pmd.ast.ASTBlock;
 import net.sourceforge.pmd.ast.ASTBlockStatement;
 import net.sourceforge.pmd.ast.ASTBooleanLiteral;
-import net.sourceforge.pmd.ast.ASTLiteral;
 import net.sourceforge.pmd.ast.ASTIfStatement;
+import net.sourceforge.pmd.ast.ASTLiteral;
+import net.sourceforge.pmd.ast.ASTMethodDeclaration;
+import net.sourceforge.pmd.ast.ASTPrimaryExpression;
+import net.sourceforge.pmd.ast.ASTPrimaryPrefix;
+import net.sourceforge.pmd.ast.ASTPrimitiveType;
+import net.sourceforge.pmd.ast.ASTResultType;
 import net.sourceforge.pmd.ast.ASTReturnStatement;
 import net.sourceforge.pmd.ast.ASTStatement;
 import net.sourceforge.pmd.ast.ASTUnaryExpressionNotPlusMinus;
+import net.sourceforge.pmd.ast.Node;
 import net.sourceforge.pmd.ast.SimpleNode;
-import net.sourceforge.pmd.ast.ASTPrimaryExpression;
-import net.sourceforge.pmd.ast.ASTPrimaryPrefix;
 
 public class SimplifyBooleanReturns extends AbstractRule {
+
+    public Object visit(ASTMethodDeclaration node, Object data) {
+        // only boolean methods should be inspected
+        ASTResultType r = node.getResultType();
+        
+        if (!r.isVoid()) {
+            Node t = r.jjtGetChild(0);
+            if (t.jjtGetNumChildren() == 1) {
+                t = t.jjtGetChild(0);
+                if ((t instanceof ASTPrimitiveType) && ((ASTPrimitiveType) t).isBoolean()) {
+                    return super.visit(node, data);
+                }
+            }
+        }
+        // skip method
+        return data;
+    }
 
     public Object visit(ASTIfStatement node, Object data) {
         // only deal with if..then..else stmts
@@ -29,84 +50,13 @@ public class SimplifyBooleanReturns extends AbstractRule {
             return super.visit(node, data);
         }
 
-        // first case:
-        // If
-        //  Expr
-        //  Statement
-        //   ReturnStatement
-        //  Statement
-        //   ReturnStatement
-        // i.e.,
-        // if (foo)
-        //  return true;
-        // else
-        //  return false;
-
-        // second case:
-        // If
-        //  Expr
-        //  Statement
-        //   ReturnStatement
-        //     UnaryExpressionNotPlusMinus '!'
-        //       Expression E
-        //  Statement
-        //   ReturnStatement
-        //       Expression E
-        // i.e.,
-        // if (foo)
-        //  return !a;
-        // else
-        //  return a;
-
-        // third case
-        // If
-        // Expr
-        // Statement
-        //  Block
-        //   BlockStatement
-        //    Statement
-        //     ReturnStatement
-        // Statement
-        //  Block
-        //   BlockStatement
-        //    Statement
-        //     ReturnStatement
-        // i.e.,
-        // if (foo) {
-        //  return true;
-        // } else {
-        //  return false;
-        // }
-
-        // forth case
-        // If
-        // Expr
-        // Statement
-        //  Block
-        //   BlockStatement
-        //    Statement
-        //     ReturnStatement
-        //       UnaryExpressionNotPlusMinus '!'
-        //         Expression E
-        // Statement
-        //  Block
-        //   BlockStatement
-        //    Statement
-        //     ReturnStatement
-        //      Expression E
-        // i.e.,
-        // if (foo) {
-        //  return !a;
-        // } else {
-        //  return a;
-        // }
-
         // if we have something like
         // if(true) or if(false)
-        if (node.jjtGetChild(0).jjtGetChild(0) instanceof ASTPrimaryExpression &&
+        if (false && // FIXME: disabling moved in first position to avoid NPE but why is this here?
+            node.jjtGetChild(0).jjtGetChild(0) instanceof ASTPrimaryExpression &&
             node.jjtGetChild(0).jjtGetChild(0).jjtGetChild(0) instanceof ASTPrimaryPrefix &&
             node.jjtGetChild(0).jjtGetChild(0).jjtGetChild(0).jjtGetChild(0) instanceof ASTLiteral &&
-            node.jjtGetChild(0).jjtGetChild(0).jjtGetChild(0).jjtGetChild(0).jjtGetChild(0) instanceof ASTBooleanLiteral && false) {
+            node.jjtGetChild(0).jjtGetChild(0).jjtGetChild(0).jjtGetChild(0).jjtGetChild(0) instanceof ASTBooleanLiteral) {
           addViolation(data, node);
         }
         else {
@@ -116,6 +66,18 @@ public class SimplifyBooleanReturns extends AbstractRule {
           if (returnStatement1 instanceof ASTReturnStatement && returnStatement2 instanceof ASTReturnStatement) {
             //if we have 2 return;
             if(isSimpleReturn(returnStatement1) && isSimpleReturn(returnStatement2)) {
+                // first case:
+                // If
+                //  Expr
+                //  Statement
+                //   ReturnStatement
+                //  Statement
+                //   ReturnStatement
+                // i.e.,
+                // if (foo)
+                //  return true;
+                // else
+                //  return false;
               addViolation(data, node);
             }
             else {
@@ -128,6 +90,21 @@ public class SimplifyBooleanReturns extends AbstractRule {
                 //We get the nodes under the '!' operator
                 //If they are the same => error
                 if(isNodesEqualWithUnaryExpression(expression1, expression2)) {
+                    // second case:
+                    // If
+                    //  Expr
+                    //  Statement
+                    //   ReturnStatement
+                    //     UnaryExpressionNotPlusMinus '!'
+                    //       Expression E
+                    //  Statement
+                    //   ReturnStatement
+                    //       Expression E
+                    // i.e.,
+                    // if (foo)
+                    //  return !a;
+                    // else
+                    //  return a;
                   addViolation(data, node);
                 }
               }
@@ -139,17 +116,58 @@ public class SimplifyBooleanReturns extends AbstractRule {
 
             //if we have 2 return;
             if(isSimpleReturn(returnStatement1) && isSimpleReturn(returnStatement2)) {
+                // third case
+                // If
+                // Expr
+                // Statement
+                //  Block
+                //   BlockStatement
+                //    Statement
+                //     ReturnStatement
+                // Statement
+                //  Block
+                //   BlockStatement
+                //    Statement
+                //     ReturnStatement
+                // i.e.,
+                // if (foo) {
+                //  return true;
+                // } else {
+                //  return false;
+                // }
               addViolation(data, node);
             }
             else {
-              SimpleNode expression1 = (SimpleNode)returnStatement1.jjtGetChild(0).jjtGetChild(0).jjtGetChild(0).jjtGetChild(0);
-              SimpleNode expression2 = (SimpleNode)returnStatement2.jjtGetChild(0).jjtGetChild(0).jjtGetChild(0).jjtGetChild(0);
+              SimpleNode expression1 = getDescendant(returnStatement1, 4);
+              SimpleNode expression2 = getDescendant(returnStatement2, 4);
               if(terminatesInBooleanLiteral((SimpleNode)node.jjtGetChild(1).jjtGetChild(0)) && terminatesInBooleanLiteral((SimpleNode) node.jjtGetChild(2).jjtGetChild(0))) {
                 addViolation(data, node);
               } else if (expression1 instanceof ASTUnaryExpressionNotPlusMinus ^ expression2 instanceof ASTUnaryExpressionNotPlusMinus) {
                 //We get the nodes under the '!' operator
                 //If they are the same => error
                 if(isNodesEqualWithUnaryExpression(expression1, expression2)) {
+                    // forth case
+                    // If
+                    // Expr
+                    // Statement
+                    //  Block
+                    //   BlockStatement
+                    //    Statement
+                    //     ReturnStatement
+                    //       UnaryExpressionNotPlusMinus '!'
+                    //         Expression E
+                    // Statement
+                    //  Block
+                    //   BlockStatement
+                    //    Statement
+                    //     ReturnStatement
+                    //      Expression E
+                    // i.e.,
+                    // if (foo) {
+                    //  return !a;
+                    // } else {
+                    //  return a;
+                    // }
                   addViolation(data, node);
                 }
               }
@@ -161,6 +179,20 @@ public class SimplifyBooleanReturns extends AbstractRule {
 
     private boolean hasOneBlockStmt(SimpleNode node) {
         return node.jjtGetChild(0) instanceof ASTBlock && node.jjtGetChild(0).jjtGetNumChildren() == 1 && node.jjtGetChild(0).jjtGetChild(0) instanceof ASTBlockStatement && node.jjtGetChild(0).jjtGetChild(0).jjtGetChild(0) instanceof ASTStatement && node.jjtGetChild(0).jjtGetChild(0).jjtGetChild(0).jjtGetChild(0) instanceof ASTReturnStatement;
+    }
+
+    /**
+     * Returns the first child node going down 'level' levels or null if level is invalid
+     */
+    private SimpleNode getDescendant(SimpleNode node, int level) {
+        Node n = node;
+        for(int i = 0; i < level; i++) {
+            if (n.jjtGetNumChildren() == 0) {
+                return null;
+            }
+            n = n.jjtGetChild(0);
+        }
+        return (SimpleNode) n;
     }
 
     private boolean terminatesInBooleanLiteral(SimpleNode node) {
