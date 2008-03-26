8e193d6f2c6ad90bd4cd3b469af20742a2d0fec7
Xavier Le Vourch <xlv@users.sourceforge.net>
2008-03-26 00:43:37 +0000

    preparing for the 4.2 release
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@5926 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/bin/pmd.bat
+++ pmd/bin/pmd.bat
@@ -1,6 +1,6 @@
 @echo off
 set TOPDIR=%~dp0/..
-set VERSION=4.2rc2
+set VERSION=4.2
 set PMDJAR=%TOPDIR%/lib/pmd-%VERSION%.jar
 set JARPATH=%TOPDIR%/lib/asm-3.1.jar;%TOPDIR%/lib/jaxen-1.1.1.jar
 set OPTS=
