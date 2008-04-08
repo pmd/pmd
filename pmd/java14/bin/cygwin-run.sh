84cad6c1c4d7ff2b6454c43d5c7cb70d97136255
Xavier Le Vourch <xlv@users.sourceforge.net>
2008-04-08 23:34:59 +0000

    retroweaver version was not correct in java14/bin scripts
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/branches/pmd/4.2.x@5993 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/java14/bin/cygwin-run.sh
+++ pmd/java14/bin/cygwin-run.sh
@@ -10,7 +10,7 @@ TOPDIR=../..
 VERSION=4.2
 PMDJAR=$TOPDIR/java14/lib/pmd14-$VERSION.jar
 JARPATH=$TOPDIR/lib/asm-3.1.jar:$TOPDIR/lib/jaxen-1.1.1.jar
-RWPATH=$TOPDIR/java14/lib/retroweaver-rt-2.0.2.jar:$TOPDIR/java14/lib/backport-util-concurrent.jar
+RWPATH=$TOPDIR/java14/lib/retroweaver-rt-2.0.5.jar:$TOPDIR/java14/lib/backport-util-concurrent.jar
 PMD_CLASSPATH=$JARPATH:$RWPATH
 if $cygwin; then
   FILE=`cygpath --windows "$FILE"`
