bbad2dfbc2e795e037ee851eafbacc8fc859f12c
Xavier Le Vourch <xlv@users.sourceforge.net>
2008-04-09 19:34:58 +0000

    preparing 4.2.1 release
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/branches/pmd/4.2.x@6001 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/java14/bin/bgastviewer.sh
+++ pmd/java14/bin/bgastviewer.sh
@@ -1,5 +1,5 @@
 TOPDIR=../..
-VERSION=4.2
+VERSION=4.2.1
 PMDJAR=$TOPDIR/java14/lib/pmd14-$VERSION.jar
 JARPATH=$TOPDIR/lib/asm-3.1.jar:$TOPDIR/lib/jaxen-1.1.1.jar
 RWPATH=$TOPDIR/java14/lib/retroweaver-rt-2.0.5.jar:$TOPDIR/java14/lib/backport-util-concurrent.jar
