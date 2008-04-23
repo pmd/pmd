6edf88fa1c7bf26336a3cb7a28c7a12723127285
Xavier Le Vourch <xlv@users.sourceforge.net>
2008-04-23 22:20:14 +0000

    obsolete file: pmd requires jdk 1.5 to be compiled
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@6053 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/bin/jdk13_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-JAVA_HOME=/usr/local/jdk1.3.1_11 && export JAVA_HOME
-CLASSPATH=$CLASSPATH:/usr/local/junit/junit.jar && export CLASSPATH
-ant clean
-JAVA_HOME=/usr/local/java && export JAVA_HOME
