c2a0302f4795068a6ebfab75aff4ea726173dee2
Ryan Gustafson <rgustav@users.sourceforge.net>
2008-06-12 04:30:14 +0000

    Reformat to conventions, to make a future diff easier to follow.
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@6191 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/net/sourceforge/pmd/lang/rule/RuleReference.java
+++ pmd/src/net/sourceforge/pmd/lang/rule/RuleReference.java
@@ -18,149 +18,149 @@ import net.sourceforge.pmd.util.StringUtil;
  * current underlying value do not override.
  */
 public class RuleReference extends AbstractDelegateRule {
-	private String name;
-	private Properties properties;
-	private String message;
-	private String description;
-	private List<String> examples;
-	private String externalInfoUrl;
-	private RulePriorityEnum priority;
-	private RuleSetReference ruleSetReference;
+    private String name;
+    private Properties properties;
+    private String message;
+    private String description;
+    private List<String> examples;
+    private String externalInfoUrl;
+    private RulePriorityEnum priority;
+    private RuleSetReference ruleSetReference;
 
-	public String getOverriddenName() {
-		return name;
-	}
+    public String getOverriddenName() {
+	return name;
+    }
 
-	@Override
-	public void setName(String name) {
-		// Only override if different than current value, or if already overridden.
-		if (!isSame(name, super.getName()) || this.name != null) {
-			this.name = name;
-			super.setName(name);
-		}
+    @Override
+    public void setName(String name) {
+	// Only override if different than current value, or if already overridden.
+	if (!isSame(name, super.getName()) || this.name != null) {
+	    this.name = name;
+	    super.setName(name);
 	}
+    }
 
-	public Properties getOverriddenProperties() {
-		return properties;
-	}
+    public Properties getOverriddenProperties() {
+	return properties;
+    }
 
-	@Override
-	public void addProperty(String name, String property) {
-		// Only override if different than current value.
-		if (!super.hasProperty(name) || !isSame(property, super.getStringProperty(name))) {
-			if (this.properties == null) {
-				this.properties = new Properties();
-			}
-			this.properties.put(name, property);
-			super.addProperty(name, property);
-		}
+    @Override
+    public void addProperty(String name, String property) {
+	// Only override if different than current value.
+	if (!super.hasProperty(name) || !isSame(property, super.getStringProperty(name))) {
+	    if (this.properties == null) {
+		this.properties = new Properties();
+	    }
+	    this.properties.put(name, property);
+	    super.addProperty(name, property);
 	}
+    }
 
-	@Override
-	public void addProperties(Properties properties) {
-		// Attempt override for each
-		for (Map.Entry<Object, Object> entry : properties.entrySet()) {
-			addProperty((String)entry.getKey(), (String)entry.getValue());
-		}
+    @Override
+    public void addProperties(Properties properties) {
+	// Attempt override for each
+	for (Map.Entry<Object, Object> entry : properties.entrySet()) {
+	    addProperty((String) entry.getKey(), (String) entry.getValue());
 	}
+    }
 
-	public String getOverriddenMessage() {
-		return message;
-	}
+    public String getOverriddenMessage() {
+	return message;
+    }
 
-	@Override
-	public void setMessage(String message) {
-		// Only override if different than current value, or if already overridden.
-		if (!isSame(message, super.getMessage()) || this.message != null) {
-			this.message = message;
-			super.setMessage(message);
-		}
+    @Override
+    public void setMessage(String message) {
+	// Only override if different than current value, or if already overridden.
+	if (!isSame(message, super.getMessage()) || this.message != null) {
+	    this.message = message;
+	    super.setMessage(message);
 	}
+    }
 
-	public String getOverriddenDescription() {
-		return description;
-	}
+    public String getOverriddenDescription() {
+	return description;
+    }
 
-	@Override
-	public void setDescription(String description) {
-		// Only override if different than current value, or if already overridden.
-		if (!isSame(description, super.getDescription()) || this.description != null) {
-			this.description = description;
-			super.setDescription(description);
-		}
+    @Override
+    public void setDescription(String description) {
+	// Only override if different than current value, or if already overridden.
+	if (!isSame(description, super.getDescription()) || this.description != null) {
+	    this.description = description;
+	    super.setDescription(description);
 	}
+    }
 
-	public List<String> getOverriddenExamples() {
-		return examples;
-	}
+    public List<String> getOverriddenExamples() {
+	return examples;
+    }
 
-	@Override
-	public void addExample(String example) {
-		// TODO Meaningful override of examples is hard, because they are merely
-		// a list of strings.  How does one indicate override of a particular
-		// value?  Via index?  Rule.setExample(int, String)?  But the XML format
-		// does not provide a means of overriding by index, not unless you took
-		// the position in the XML file to indicate corresponding index to
-		// override.  But that means you have to override starting from index 0.
-		// This would be so much easier if examples had to have names, like
-		// properties.
-				
-		// Only override if different than current values.
-		if (!contains(super.getExamples(), example)) {
-			if (this.examples == null) {
-				this.examples = new ArrayList<String>(1);
-			}
-			// TODO Fix later. To keep example overrides from being unbounded, we're only going to keep track of the last one.
-			this.examples.clear();
-			this.examples.add(example);
-			super.addExample(example);
-		}
-	}
+    @Override
+    public void addExample(String example) {
+	// TODO Meaningful override of examples is hard, because they are merely
+	// a list of strings.  How does one indicate override of a particular
+	// value?  Via index?  Rule.setExample(int, String)?  But the XML format
+	// does not provide a means of overriding by index, not unless you took
+	// the position in the XML file to indicate corresponding index to
+	// override.  But that means you have to override starting from index 0.
+	// This would be so much easier if examples had to have names, like
+	// properties.
 
-	public String getOverriddenExternalInfoUrl() {
-		return externalInfoUrl;
+	// Only override if different than current values.
+	if (!contains(super.getExamples(), example)) {
+	    if (this.examples == null) {
+		this.examples = new ArrayList<String>(1);
+	    }
+	    // TODO Fix later. To keep example overrides from being unbounded, we're only going to keep track of the last one.
+	    this.examples.clear();
+	    this.examples.add(example);
+	    super.addExample(example);
 	}
+    }
 
-	@Override
-	public void setExternalInfoUrl(String externalInfoUrl) {
-		// Only override if different than current value, or if already overridden.
-		if (!isSame(externalInfoUrl, super.getExternalInfoUrl()) || this.externalInfoUrl != null) {
-			this.externalInfoUrl = externalInfoUrl;
-			super.setExternalInfoUrl(externalInfoUrl);
-		}
-	}
+    public String getOverriddenExternalInfoUrl() {
+	return externalInfoUrl;
+    }
 
-	public RulePriorityEnum getOverriddenPriority() {
-		return priority;
+    @Override
+    public void setExternalInfoUrl(String externalInfoUrl) {
+	// Only override if different than current value, or if already overridden.
+	if (!isSame(externalInfoUrl, super.getExternalInfoUrl()) || this.externalInfoUrl != null) {
+	    this.externalInfoUrl = externalInfoUrl;
+	    super.setExternalInfoUrl(externalInfoUrl);
 	}
+    }
 
-	@Override
-	public void setPriority(RulePriorityEnum priority) {
-		// Only override if different than current value, or if already overridden.
-		if (priority != super.getPriority() || this.priority != null) {
-			this.priority = priority;
-			super.setPriority(priority);
-		}
-	}
+    public RulePriorityEnum getOverriddenPriority() {
+	return priority;
+    }
 
-	public RuleSetReference getRuleSetReference() {
-		return ruleSetReference;
+    @Override
+    public void setPriority(RulePriorityEnum priority) {
+	// Only override if different than current value, or if already overridden.
+	if (priority != super.getPriority() || this.priority != null) {
+	    this.priority = priority;
+	    super.setPriority(priority);
 	}
+    }
 
-	public void setRuleSetReference(RuleSetReference ruleSetReference) {
-		this.ruleSetReference = ruleSetReference;
-	}
+    public RuleSetReference getRuleSetReference() {
+	return ruleSetReference;
+    }
 
-	private static boolean isSame(String s1, String s2) {
-		return StringUtil.isSame(s1, s2, true, false, true);
-	}
+    public void setRuleSetReference(RuleSetReference ruleSetReference) {
+	this.ruleSetReference = ruleSetReference;
+    }
 
-	private static boolean contains(Collection<String> collection, String s1) {
-		for (String s2 : collection) {
-			if (isSame(s1, s2)) {
-				return true;
-			}
-		}
-		return false;
+    private static boolean isSame(String s1, String s2) {
+	return StringUtil.isSame(s1, s2, true, false, true);
+    }
+
+    private static boolean contains(Collection<String> collection, String s1) {
+	for (String s2 : collection) {
+	    if (isSame(s1, s2)) {
+		return true;
+	    }
 	}
+	return false;
+    }
 }
