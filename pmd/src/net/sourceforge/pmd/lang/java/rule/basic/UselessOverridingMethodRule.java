fadb13fefb3cd795ec0e264b2ceba44c71477503
Xavier Le Vourch <xlv@users.sourceforge.net>
2008-06-14 00:36:58 +0000

    Fixed bug 1977230 - false positive: UselessOverridingMethod
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@6204 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/net/sourceforge/pmd/lang/java/rule/basic/UselessOverridingMethodRule.java
+++ pmd/src/net/sourceforge/pmd/lang/java/rule/basic/UselessOverridingMethodRule.java
@@ -158,9 +158,13 @@ public class UselessOverridingMethodRule extends AbstractJavaRule {
 	    return super.visit(node, data);
 	}
 
+	List<ASTPrimarySuffix> primarySuffixList = findFirstDegreeChildrenOfType(primaryExpression, ASTPrimarySuffix.class);
+	if (primarySuffixList.size() != 1) {
+	    // extra method call on result of super method
+	    return super.visit(node, data);
+	}
 	//Process arguments
-	ASTPrimarySuffix primarySuffix = findFirstDegreeChildrenOfType(primaryExpression, ASTPrimarySuffix.class)
-		.get(0);
+	ASTPrimarySuffix primarySuffix = primarySuffixList.get(0);
 	ASTArguments arguments = (ASTArguments) primarySuffix.jjtGetChild(0);
 	ASTFormalParameters formalParameters = (ASTFormalParameters) methodDeclarator.jjtGetChild(0);
 	if (formalParameters.jjtGetNumChildren() != arguments.jjtGetNumChildren()) {
