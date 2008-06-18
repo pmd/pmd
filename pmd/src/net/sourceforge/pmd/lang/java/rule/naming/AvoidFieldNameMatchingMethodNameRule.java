ea83a17be5b08469baae9c31442aed5d9d6635b8
Xavier Le Vourch <xlv@users.sourceforge.net>
2008-06-18 20:00:48 +0000

    optimization of AvoidFieldNameMatchingMethodName
    avoiding traversing the tree multiple times
    
    
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@6229 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/net/sourceforge/pmd/lang/java/rule/naming/AvoidFieldNameMatchingMethodNameRule.java
+++ pmd/src/net/sourceforge/pmd/lang/java/rule/naming/AvoidFieldNameMatchingMethodNameRule.java
@@ -3,8 +3,13 @@
  */
 package net.sourceforge.pmd.lang.java.rule.naming;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import net.sourceforge.pmd.lang.ast.Node;
+import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceBody;
 import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTFieldDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTMethodDeclaration;
@@ -12,6 +17,7 @@ import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
 
 public class AvoidFieldNameMatchingMethodNameRule extends AbstractJavaRule {
 
+    @Override
     public Object visit(ASTClassOrInterfaceDeclaration node, Object data) {
         if (node.isInterface()) {
             return data;
@@ -19,25 +25,30 @@ public class AvoidFieldNameMatchingMethodNameRule extends AbstractJavaRule {
         return super.visit(node, data);
     }
 
-    public Object visit(ASTFieldDeclaration node, Object data) {
-        String varName = node.getVariableName();
-        String fieldDeclaringType = getDeclaringType(node);
-        if (varName != null) {
-            varName = varName.toLowerCase();
-            ASTClassOrInterfaceDeclaration cl = node.getFirstParentOfType(ASTClassOrInterfaceDeclaration.class);
-            if (cl != null) {
-                List<ASTMethodDeclaration> methods = cl.findChildrenOfType(ASTMethodDeclaration.class);
-                for (ASTMethodDeclaration m: methods) {
-                    //Make sure we are comparing fields and methods inside same type
-                    if (fieldDeclaringType.equals(getDeclaringType(m))) {
-                        String n = m.getMethodName();
-                        if (varName.equals(n.toLowerCase())) {
-                            addViolation(data, node);
-                        }
-                    }
-                }
+    @Override
+    public Object visit(ASTClassOrInterfaceBody node, Object data) {
+        int n = node.jjtGetNumChildren();
+        List<ASTFieldDeclaration> fields = new ArrayList<ASTFieldDeclaration>();
+        Set<String> methodNames = new HashSet<String>();
+        for (int i = 0; i < n; i++) {
+            Node child = node.jjtGetChild(i);
+            if (child.jjtGetNumChildren() == 0) {
+                continue;
+            }
+            child = child.jjtGetChild(child.jjtGetNumChildren()-1);
+            if (child instanceof ASTFieldDeclaration) {
+                fields.add((ASTFieldDeclaration) child);
+            } else if (child instanceof ASTMethodDeclaration) {
+                methodNames.add(((ASTMethodDeclaration) child).getMethodName().toLowerCase());
             }
         }
-        return data;
+        for (ASTFieldDeclaration field : fields) {
+            String varName = field.getVariableName().toLowerCase();
+            if (methodNames.contains(varName)) {
+                addViolation(data, field, field.getVariableName());
+            }
+        }
+        return super.visit(node, data);
     }
+
 }
