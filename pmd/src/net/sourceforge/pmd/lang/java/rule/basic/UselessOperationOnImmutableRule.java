0b5403dd5128c31375e3769b702447e0af82da0a
Xavier Le Vourch <xlv@users.sourceforge.net>
2008-06-18 00:43:51 +0000

    fix false positives in UselessOperationOnImmutable
    false positives included string were used in expressions and BigInteger
    as method arguments. The immutable object is ignored only if expression's
    parent is a statement expression.
    
    
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@6227 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/net/sourceforge/pmd/lang/java/rule/basic/UselessOperationOnImmutableRule.java
+++ pmd/src/net/sourceforge/pmd/lang/java/rule/basic/UselessOperationOnImmutableRule.java
@@ -1,17 +1,12 @@
 package net.sourceforge.pmd.lang.java.rule.basic;
 
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import net.sourceforge.pmd.lang.ast.Node;
-import net.sourceforge.pmd.lang.java.ast.ASTConditionalExpression;
-import net.sourceforge.pmd.lang.java.ast.ASTExpression;
 import net.sourceforge.pmd.lang.java.ast.ASTLocalVariableDeclaration;
-import net.sourceforge.pmd.lang.java.ast.ASTPrimaryExpression;
-import net.sourceforge.pmd.lang.java.ast.ASTPrimarySuffix;
+import net.sourceforge.pmd.lang.java.ast.ASTStatementExpression;
 import net.sourceforge.pmd.lang.java.ast.ASTType;
 import net.sourceforge.pmd.lang.java.ast.ASTVariableDeclaratorId;
 import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
@@ -24,12 +19,12 @@ import net.sourceforge.pmd.util.CollectionUtil;
  * ignoring the operation result is an error.
  */
 public class UselessOperationOnImmutableRule extends AbstractJavaRule {
-	
+
     /**
      * These are the BigDecimal methods which are immutable
      */
     private static final Set<String> decMethods = CollectionUtil.asSet(new String[] { ".abs", ".add", ".divide", ".divideToIntegralValue", ".max", ".min", ".movePointLeft", ".movePointRight", ".multiply", ".negate", ".plus", ".pow", ".remainder", ".round", ".scaleByPowerOfTen", ".setScale", ".stripTrailingZeros", ".subtract", ".ulp" });
-	
+
     /**
      * These are the BigInteger methods which are immutable
      */
@@ -53,6 +48,7 @@ public class UselessOperationOnImmutableRule extends AbstractJavaRule {
         mapClasses.put("String", strMethods);
     }
 
+    @Override
     public Object visit(ASTLocalVariableDeclaration node, Object data) {
 
         ASTVariableDeclaratorId var = getDeclaration(node);
@@ -61,13 +57,12 @@ public class UselessOperationOnImmutableRule extends AbstractJavaRule {
         }
         String variableName = var.getImage();
         for (NameOccurrence no: var.getUsages()) {
-            // FIXME - getUsages will return everything with the same name as the variable, 
+            // FIXME - getUsages will return everything with the same name as the variable,
             // see JUnit test, case 6. Changing to Node below, revisit when getUsages is fixed
             Node sn = no.getLocation();
             Node primaryExpression = sn.jjtGetParent().jjtGetParent();
-			Class<? extends Node> parentClass = primaryExpression.jjtGetParent().getClass();
-            if (!(parentClass.equals(ASTExpression.class) || parentClass.equals(ASTConditionalExpression.class) || 
-            		hasComparisons(primaryExpression))) {
+            Class<? extends Node> parentClass = primaryExpression.jjtGetParent().getClass();
+            if (parentClass.equals(ASTStatementExpression.class)) {
                 String methodCall = sn.getImage().substring(variableName.length());
                 ASTType nodeType = node.getTypeNode();
                 if ( nodeType != null ) {
@@ -81,26 +76,9 @@ public class UselessOperationOnImmutableRule extends AbstractJavaRule {
     }
 
     /**
-     * Check whether the Immutable is compareTo'd something
-     */
-	private boolean hasComparisons(Node primaryExpression) {
-		if (primaryExpression.getClass().equals(ASTPrimaryExpression.class)) {
-			List<ASTPrimarySuffix> suffixes = ((ASTPrimaryExpression)primaryExpression).findChildrenOfType(ASTPrimarySuffix.class);
-			for (Iterator<ASTPrimarySuffix> iterator = suffixes.iterator(); iterator.hasNext();) {
-				ASTPrimarySuffix suffix = iterator.next();
-				if ("compareTo".equals(suffix.getImage()))
-					return true;
-			}
-		} else {
-			//Some weird usage of the Immutable
-		}
-		return false;	//No comparison
-	}
-
-    /**
      * This method checks the variable declaration if it is on a class we care
      * about. If it is, it returns the DeclaratorId
-     * 
+     *
      * @param node
      *            The ASTLocalVariableDeclaration which is a problem
      * @return ASTVariableDeclaratorId
