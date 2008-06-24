9e1136b59aa8b658651befed6de579e37c02f1d5
Ryan Gustafson <rgustav@users.sourceforge.net>
2008-06-24 19:59:18 +0000

    AssignmentInOperand enhanced to catch assignment in 'for' condition, as well as use of increment/decrement operators.  Customization properties added to allow assignment in if/while/for, or use of increment/decrement.
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@6269 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/net/sourceforge/pmd/lang/java/rule/controversial/AssignmentInOperandRule.java
+++ pmd/src/net/sourceforge/pmd/lang/java/rule/controversial/AssignmentInOperandRule.java
@@ -1,23 +1,46 @@
 package net.sourceforge.pmd.lang.java.rule.controversial;
 
+import net.sourceforge.pmd.PropertyDescriptor;
 import net.sourceforge.pmd.lang.ast.Node;
 import net.sourceforge.pmd.lang.java.ast.ASTAssignmentOperator;
 import net.sourceforge.pmd.lang.java.ast.ASTExpression;
+import net.sourceforge.pmd.lang.java.ast.ASTForStatement;
 import net.sourceforge.pmd.lang.java.ast.ASTIfStatement;
+import net.sourceforge.pmd.lang.java.ast.ASTPostfixExpression;
+import net.sourceforge.pmd.lang.java.ast.ASTPreDecrementExpression;
+import net.sourceforge.pmd.lang.java.ast.ASTPreIncrementExpression;
 import net.sourceforge.pmd.lang.java.ast.ASTWhileStatement;
 import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
+import net.sourceforge.pmd.lang.rule.properties.BooleanProperty;
 
 public class AssignmentInOperandRule extends AbstractJavaRule {
 
-    public Object visit(ASTExpression node, Object data) {
-        Node parent = node.jjtGetParent();
-        if ((parent instanceof ASTWhileStatement || parent instanceof ASTIfStatement) &&
-                node.hasDescendantOfType(ASTAssignmentOperator.class))
-        {
-            addViolation(data, node);
-            return data;
-        }
-        return super.visit(node, data);
-    }
+    private static final PropertyDescriptor ALLOW_IF_DESCRIPTOR = new BooleanProperty("allowIf",
+	    "Allows assignment within the conditional expression of an if statement.", false, 1.0f);
 
+    private static final PropertyDescriptor ALLOW_FOR_DESCRIPTOR = new BooleanProperty("allowFor",
+	    "Allows assignment within the conditional expression of a for statement.", false, 2.0f);
+
+    private static final PropertyDescriptor ALLOW_WHILE_DESCRIPTOR = new BooleanProperty("allowWhile",
+	    "Allows assignment within the conditional expression of a while statement.", false, 3.0f);
+
+    private static final PropertyDescriptor ALLOW_INCREMENT_DECREMENT_DESCRIPTOR = new BooleanProperty(
+	    "allowIncrementDecrement",
+	    "Allows increment or decrement operators within the conditional expression of an if, for, or while statement.",
+	    false, 4.0f);
+
+    public Object visit(ASTExpression node, Object data) {
+	Node parent = node.jjtGetParent();
+	if (((parent instanceof ASTIfStatement && !getBooleanProperty(ALLOW_IF_DESCRIPTOR))
+		|| (parent instanceof ASTWhileStatement && !getBooleanProperty(ALLOW_WHILE_DESCRIPTOR)) || (parent instanceof ASTForStatement
+		&& parent.jjtGetChild(1) == node && !getBooleanProperty(ALLOW_FOR_DESCRIPTOR)))
+		&& (node.hasDescendantOfType(ASTAssignmentOperator.class) || (!getBooleanProperty(ALLOW_INCREMENT_DECREMENT_DESCRIPTOR) && (node
+			.hasDescendantOfType(ASTPreIncrementExpression.class)
+			|| node.hasDescendantOfType(ASTPreDecrementExpression.class) || node
+			.hasDescendantOfType(ASTPostfixExpression.class))))) {
+	    addViolation(data, node);
+	    return data;
+	}
+	return super.visit(node, data);
+    }
 }
