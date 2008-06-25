0839364579428ef45bfe5879df321e8ae2c086e3
Ryan Gustafson <rgustav@users.sourceforge.net>
2008-06-25 01:06:59 +0000

    Dogfood now contains MissingBreakInSwitch.
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@6274 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/net/sourceforge/pmd/lang/java/rule/controversial/SuspiciousOctalEscapeRule.java
+++ pmd/src/net/sourceforge/pmd/lang/java/rule/controversial/SuspiciousOctalEscapeRule.java
@@ -67,36 +67,10 @@ public class SuspiciousOctalEscapeRule extends AbstractJavaRule {
     }
 
     private boolean isOctal(char c) {
-        switch (c) {
-            case '0':
-            case '1':
-            case '2':
-            case '3':
-            case '4':
-            case '5':
-            case '6':
-            case '7':
-                return true;
-            default:
-                return false;
-        }
+	return c >= '0' && c <= '7';
     }
 
     private boolean isDecimal(char c) {
-        switch (c) {
-            case '0':
-            case '1':
-            case '2':
-            case '3':
-            case '4':
-            case '5':
-            case '6':
-            case '7':
-            case '8':
-            case '9':
-                return true;
-            default:
-                return false;
-        }
+	return c >= '0' && c <= '9';
     }
 }
