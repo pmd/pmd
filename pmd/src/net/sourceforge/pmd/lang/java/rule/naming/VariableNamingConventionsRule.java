492f9264207e2d876c0c75487e828c8e70fe30b7
Ryan Gustafson <rgustav@users.sourceforge.net>
2008-06-30 01:53:11 +0000

    Fix false negative for non-primitive types for VariableNamingConventions, also expanded scope to local and method/constructors, and enhanced customization options to choose between members/locals/parameters (all checked by default).
    Yes, this Rule now reports on things it didn't report on before because of (1) a false negative fix, and (2) expansion of scope to cover locals and parameters.  I believe locals were always intended to be checked, but the implementation was non-functioning.
    
    
    
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@6295 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/net/sourceforge/pmd/lang/java/rule/naming/VariableNamingConventionsRule.java
+++ pmd/src/net/sourceforge/pmd/lang/java/rule/naming/VariableNamingConventionsRule.java
@@ -6,141 +6,197 @@ package net.sourceforge.pmd.lang.java.rule.naming;
 import java.util.Map;
 
 import net.sourceforge.pmd.PropertyDescriptor;
+import net.sourceforge.pmd.lang.ast.Node;
 import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTCompilationUnit;
 import net.sourceforge.pmd.lang.java.ast.ASTFieldDeclaration;
-import net.sourceforge.pmd.lang.java.ast.ASTName;
-import net.sourceforge.pmd.lang.java.ast.ASTPrimitiveType;
-import net.sourceforge.pmd.lang.java.ast.ASTType;
-import net.sourceforge.pmd.lang.java.ast.ASTVariableDeclarator;
+import net.sourceforge.pmd.lang.java.ast.ASTFormalParameter;
+import net.sourceforge.pmd.lang.java.ast.ASTFormalParameters;
+import net.sourceforge.pmd.lang.java.ast.ASTLocalVariableDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTVariableDeclaratorId;
 import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
+import net.sourceforge.pmd.lang.rule.properties.BooleanProperty;
 import net.sourceforge.pmd.lang.rule.properties.StringProperty;
 
 public class VariableNamingConventionsRule extends AbstractJavaRule {
 
+    private boolean checkMembers;
+    private boolean checkLocals;
+    private boolean checkParameters;
     private String[] staticPrefixes;
     private String[] staticSuffixes;
     private String[] memberPrefixes;
     private String[] memberSuffixes;
+    private String[] localPrefixes;
+    private String[] localSuffixes;
+    private String[] parameterPrefixes;
+    private String[] parameterSuffixes;
 
-    private static final PropertyDescriptor staticPrefixesDescriptor = new StringProperty(
-    	"staticPrefix", "Static prefixes", new String[] {""},	1.0f , ','
-    	);
- 
-    private static final PropertyDescriptor staticSuffixesDescriptor = new StringProperty(
-       	"staticSuffix", "Static suffixes", new String[] {""},	2.0f , ','
-       	);    
- 
-    private static final PropertyDescriptor memberPrefixesDescriptor = new StringProperty(
-       	"memberPrefix", "Member prefixes", new String[] {""},	3.0f , ','
-       	);
-    
-    private static final PropertyDescriptor memberSuffixesDescriptor = new StringProperty(
-       	"memberSuffix", "Member suffixes", new String[] {""},	4.0f , ','
-       	);
-    
-    private static final Map<String, PropertyDescriptor> propertyDescriptorsByName = asFixedMap( new PropertyDescriptor[] {
-    	staticPrefixesDescriptor, staticSuffixesDescriptor, 
-    	memberPrefixesDescriptor, memberSuffixesDescriptor
-		});
-    
+    private static final PropertyDescriptor checkMembersDescriptor = new BooleanProperty("checkMembers",
+	    "Check member variables", true, 1.0f);
+
+    private static final PropertyDescriptor checkLocalsDescriptor = new BooleanProperty("checkLocals",
+	    "Check local variables", true, 2.0f);
+
+    private static final PropertyDescriptor checkParametersDescriptor = new BooleanProperty("checkParameters",
+	    "Check constructor and method parameter variables", true, 3.0f);
+
+    private static final PropertyDescriptor staticPrefixesDescriptor = new StringProperty("staticPrefix",
+	    "Static member prefixes", new String[] { "" }, 4.0f, ',');
+
+    private static final PropertyDescriptor staticSuffixesDescriptor = new StringProperty("staticSuffix",
+	    "Static member suffixes", new String[] { "" }, 5.0f, ',');
+
+    private static final PropertyDescriptor memberPrefixesDescriptor = new StringProperty("memberPrefix",
+	    "Instance member prefixes", new String[] { "" }, 6.0f, ',');
+
+    private static final PropertyDescriptor memberSuffixesDescriptor = new StringProperty("memberSuffix",
+	    "Instance member suffixes", new String[] { "" }, 7.0f, ',');
+
+    private static final PropertyDescriptor localPrefixesDescriptor = new StringProperty("localPrefix",
+	    "Local variable prefixes", new String[] { "" }, 8.0f, ',');
+
+    private static final PropertyDescriptor localSuffixesDescriptor = new StringProperty("localSuffix",
+	    "Local variable suffixes", new String[] { "" }, 9.0f, ',');
+
+    private static final PropertyDescriptor parameterPrefixesDescriptor = new StringProperty("parameterPrefix",
+	    "Formal parameter prefixes", new String[] { "" }, 10.0f, ',');
+
+    private static final PropertyDescriptor parameterSuffixesDescriptor = new StringProperty("parameterSuffix",
+	    "Formal parameter suffixes", new String[] { "" }, 11.0f, ',');
+
+    private static final Map<String, PropertyDescriptor> propertyDescriptorsByName = asFixedMap(new PropertyDescriptor[] {
+	    checkMembersDescriptor, checkLocalsDescriptor, checkParametersDescriptor, staticPrefixesDescriptor,
+	    staticSuffixesDescriptor, memberPrefixesDescriptor, memberSuffixesDescriptor, localPrefixesDescriptor,
+	    localSuffixesDescriptor, parameterPrefixesDescriptor, parameterSuffixesDescriptor, });
+
     /**
      * @return Map
      */
     protected Map<String, PropertyDescriptor> propertiesByName() {
-    	return propertyDescriptorsByName;
-    }    
-    
+	return propertyDescriptorsByName;
+    }
+
     public Object visit(ASTCompilationUnit node, Object data) {
-        init();
-        return super.visit(node, data);
+	init();
+	return super.visit(node, data);
     }
 
     protected void init() {
-        staticPrefixes = getStringProperties(staticPrefixesDescriptor);
-        staticSuffixes = getStringProperties(staticSuffixesDescriptor);
-        memberPrefixes = getStringProperties(memberPrefixesDescriptor);
-        memberSuffixes = getStringProperties(memberSuffixesDescriptor);
+	checkMembers = getBooleanProperty(checkMembersDescriptor);
+	checkLocals = getBooleanProperty(checkLocalsDescriptor);
+	checkParameters = getBooleanProperty(checkParametersDescriptor);
+	staticPrefixes = getStringProperties(staticPrefixesDescriptor);
+	staticSuffixes = getStringProperties(staticSuffixesDescriptor);
+	memberPrefixes = getStringProperties(memberPrefixesDescriptor);
+	memberSuffixes = getStringProperties(memberSuffixesDescriptor);
+	localPrefixes = getStringProperties(localPrefixesDescriptor);
+	localSuffixes = getStringProperties(localSuffixesDescriptor);
+	parameterPrefixes = getStringProperties(parameterPrefixesDescriptor);
+	parameterSuffixes = getStringProperties(parameterSuffixesDescriptor);
     }
 
     public Object visit(ASTFieldDeclaration node, Object data) {
-        return checkNames(node, data);
+	if (!checkMembers) {
+	    return data;
+	}
+	boolean isStatic = node.isStatic();
+	boolean isFinal = node.isFinal();
+	// Anything from an interface is necessarily static and final
+	if (node.jjtGetParent().jjtGetParent().jjtGetParent() instanceof ASTClassOrInterfaceDeclaration
+		&& ((ASTClassOrInterfaceDeclaration) node.jjtGetParent().jjtGetParent().jjtGetParent()).isInterface()) {
+	    isStatic = true;
+	    isFinal = true;
+	}
+	return checkVariableDeclarators(node.isStatic() ? staticPrefixes : memberPrefixes, isStatic ? staticSuffixes
+		: memberSuffixes, node, isStatic, isFinal, data);
     }
 
-    private Object checkNames(ASTFieldDeclaration node, Object data) {
-        ASTType childNodeType = (ASTType) node.jjtGetChild(0);
-        String varType = "";
-        if (childNodeType.jjtGetChild(0) instanceof ASTName) {
-            varType = ((ASTName) childNodeType.jjtGetChild(0)).getImage();
-        } else if (childNodeType.jjtGetChild(0) instanceof ASTPrimitiveType) {
-            varType = ((ASTPrimitiveType) childNodeType.jjtGetChild(0)).getImage();
-        }
-        if (varType != null && varType.length() > 0) {
-            //Get the variable name
-            ASTVariableDeclarator childNodeName = (ASTVariableDeclarator) node.jjtGetChild(1);
-            ASTVariableDeclaratorId childNodeId = (ASTVariableDeclaratorId) childNodeName.jjtGetChild(0);
-            String varName = childNodeId.getImage();
-
-            if (varName.equals("serialVersionUID") || (node.isFinal() && !node.isStatic() && !node.isInterfaceMember())) {
-                return data;
-            }
-
-            // static finals (and interface fields, which are implicitly static and final) are
-            // checked for uppercase
-            if ((node.isStatic() && node.isFinal()) || (node.jjtGetParent().jjtGetParent().jjtGetParent() instanceof ASTClassOrInterfaceDeclaration && ((ASTClassOrInterfaceDeclaration) node.jjtGetParent().jjtGetParent().jjtGetParent()).isInterface())) {
-                if (!varName.equals(varName.toUpperCase())) {
-                    addViolationWithMessage(data, childNodeName, "Variables that are final and static should be in all caps.");
-                }
-                return data;
-            }
-
-            String strippedVarName = null;
-            if (node.isStatic()) {
-                strippedVarName = normalizeStaticVariableName(varName);
-            } else {
-                strippedVarName = normalizeMemberVariableName(varName);
-            }
-
-            if (strippedVarName.indexOf('_') >= 0) {
-                addViolationWithMessage(data, childNodeName, "Variables that are not final should not contain underscores (except for underscores in standard prefix/suffix).");
-            }
-            if (Character.isUpperCase(varName.charAt(0))) {
-                addViolationWithMessage(data, childNodeName, "Variables should start with a lowercase character");
-            }
-        }
-        return data;
+    public Object visit(ASTLocalVariableDeclaration node, Object data) {
+	if (!checkLocals) {
+	    return data;
+	}
+	return checkVariableDeclarators(localPrefixes, localSuffixes, node, false, node.isFinal(), data);
     }
 
-    private String normalizeMemberVariableName(String varName) {
-        return stripSuffix(stripPrefix(varName, memberPrefixes), memberSuffixes);
+    public Object visit(ASTFormalParameters node, Object data) {
+	if (!checkParameters) {
+	    return data;
+	}
+	for (ASTFormalParameter formalParameter : node.findChildrenOfType(ASTFormalParameter.class)) {
+	    for (ASTVariableDeclaratorId variableDeclaratorId : formalParameter
+		    .findChildrenOfType(ASTVariableDeclaratorId.class)) {
+		checkVariableDeclaratorId(parameterPrefixes, parameterSuffixes, node, false, formalParameter.isFinal(),
+			variableDeclaratorId, data);
+	    }
+	}
+	return data;
     }
 
-    private String normalizeStaticVariableName(String varName) {
-        return stripSuffix(stripPrefix(varName, staticPrefixes), staticSuffixes);
+    private Object checkVariableDeclarators(String[] prefixes, String[] suffixes, Node root, boolean isStatic,
+	    boolean isFinal, Object data) {
+	for (ASTVariableDeclaratorId variableDeclaratorId : root.findDescendantsOfType(ASTVariableDeclaratorId.class)) {
+	    checkVariableDeclaratorId(prefixes, suffixes, root, isStatic, isFinal, variableDeclaratorId, data);
+	}
+	return data;
     }
 
-    private String stripSuffix(String varName, String[] suffix) {
-        if (suffix != null) {
-            for (int i = 0; i < suffix.length; i++) {
-                if (varName.endsWith(suffix[i])) {
-                    varName = varName.substring(0, varName.length() - suffix[i].length());
-                    break;
-                }
-            }
-        }
-        return varName;
+    private Object checkVariableDeclaratorId(String[] prefixes, String[] suffixes, Node root, boolean isStatic,
+	    boolean isFinal, ASTVariableDeclaratorId variableDeclaratorId, Object data) {
+
+	// Get the variable name
+	String varName = variableDeclaratorId.getImage();
+
+	// Skip serialVersionUID
+	if (varName.equals("serialVersionUID")) {
+	    return data;
+	}
+
+	// Static finals should be uppercase
+	if (isStatic && isFinal) {
+	    if (!varName.equals(varName.toUpperCase())) {
+		addViolationWithMessage(data, variableDeclaratorId,
+			"Variables that are final and static should be in all caps.");
+	    }
+	    return data;
+	} else if (!isFinal) {
+	    String normalizedVarName = normalizeVariableName(varName, prefixes, suffixes);
+
+	    if (normalizedVarName.indexOf('_') >= 0) {
+		addViolationWithMessage(data, variableDeclaratorId,
+			"Variables that are not final should not contain underscores (except for underscores in standard prefix/suffix).");
+	    }
+	    if (Character.isUpperCase(varName.charAt(0))) {
+		addViolationWithMessage(data, variableDeclaratorId, "Variables should start with a lowercase character");
+	    }
+	}
+	return data;
     }
 
-    private String stripPrefix(String varName, String[] prefix) {
-        if (prefix == null) {
-            return varName;
-        }
-        for (int i = 0; i < prefix.length; i++) {
-            if (varName.startsWith(prefix[i])) {
-                return varName.substring(prefix[i].length());
-            }
-        }
-        return varName;
+    private String normalizeVariableName(String varName, String[] prefixes, String[] suffixes) {
+	return stripSuffix(stripPrefix(varName, prefixes), suffixes);
+    }
+
+    private String stripSuffix(String varName, String[] suffixes) {
+	if (suffixes != null) {
+	    for (int i = 0; i < suffixes.length; i++) {
+		if (varName.endsWith(suffixes[i])) {
+		    varName = varName.substring(0, varName.length() - suffixes[i].length());
+		    break;
+		}
+	    }
+	}
+	return varName;
+    }
+
+    private String stripPrefix(String varName, String[] prefixes) {
+	if (prefixes != null) {
+	    for (int i = 0; i < prefixes.length; i++) {
+		if (varName.startsWith(prefixes[i])) {
+		    return varName.substring(prefixes[i].length());
+		}
+	    }
+	}
+	return varName;
     }
 }
