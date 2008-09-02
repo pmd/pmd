1d3db74ee07d1b946a0fa3aa81bd0b2de96e31fc
Xavier Le Vourch <xlv@users.sourceforge.net>
2008-09-02 18:26:09 +0000

    code cleanup: setPublic(), setPrivate(), ... are now deprecated to reflect their deprecated status in the AccessNode interface
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@6436 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/net/sourceforge/pmd/lang/java/ast/AbstractJavaAccessNode.java
+++ pmd/src/net/sourceforge/pmd/lang/java/ast/AbstractJavaAccessNode.java
@@ -2,169 +2,213 @@ package net.sourceforge.pmd.lang.java.ast;
 
 public abstract class AbstractJavaAccessNode extends AbstractJavaNode implements AccessNode {
 
-	private int modifiers;
+    private int modifiers;
 
-	public AbstractJavaAccessNode(int i) {
-		super(i);
-	}
+    public AbstractJavaAccessNode(int i) {
+        super(i);
+    }
 
-	public AbstractJavaAccessNode(JavaParser parser, int i) {
-		super(parser, i);
-	}
+    public AbstractJavaAccessNode(JavaParser parser, int i) {
+        super(parser, i);
+    }
 
-	public int getModifiers() {
-		return this.modifiers;
-	}
+    public int getModifiers() {
+        return this.modifiers;
+    }
 
-	public void setModifiers(int modifiers) {
-		this.modifiers = modifiers;
-	}
+    public void setModifiers(int modifiers) {
+        this.modifiers = modifiers;
+    }
 
-	public boolean isPublic() {
-		return isModifier(PUBLIC);
-	}
+    public boolean isPublic() {
+        return isModifier(PUBLIC);
+    }
 
-	public void setPublic() {
-		setPublic(true);
-	}
+    /**
+     * @deprecated Use setPublic(boolean) instead.
+     */
+    @Deprecated
+    public void setPublic() {
+        setPublic(true);
+    }
 
-	public void setPublic(boolean isPublic) {
-		setModifier(isPublic, PUBLIC);
-	}
+    public void setPublic(boolean isPublic) {
+        setModifier(isPublic, PUBLIC);
+    }
 
-	public boolean isProtected() {
-		return isModifier(PROTECTED);
-	}
+    public boolean isProtected() {
+        return isModifier(PROTECTED);
+    }
 
-	public void setProtected() {
-		setProtected(true);
-	}
+    /**
+     * @deprecated Use setProtected(boolean) instead.
+     */
+    @Deprecated
+    public void setProtected() {
+        setProtected(true);
+    }
 
-	public void setProtected(boolean isProtected) {
-		setModifier(isProtected, PROTECTED);
-	}
+    public void setProtected(boolean isProtected) {
+        setModifier(isProtected, PROTECTED);
+    }
 
-	public boolean isPrivate() {
-		return isModifier(PRIVATE);
-	}
+    public boolean isPrivate() {
+        return isModifier(PRIVATE);
+    }
 
-	public void setPrivate() {
-		setPrivate(true);
-	}
+    /**
+     * @deprecated Use setPrivate(boolean) instead.
+     */
+    @Deprecated
+    public void setPrivate() {
+        setPrivate(true);
+    }
 
-	public void setPrivate(boolean isPrivate) {
-		setModifier(isPrivate, PRIVATE);
-	}
+    public void setPrivate(boolean isPrivate) {
+        setModifier(isPrivate, PRIVATE);
+    }
 
-	public boolean isAbstract() {
-		return isModifier(ABSTRACT);
-	}
+    public boolean isAbstract() {
+        return isModifier(ABSTRACT);
+    }
 
-	public void setAbstract() {
-		setAbstract(true);
-	}
+    /**
+     * @deprecated Use setAbstract(boolean) instead.
+     */
+    @Deprecated
+    public void setAbstract() {
+        setAbstract(true);
+    }
 
-	public void setAbstract(boolean isAbstract) {
-		setModifier(isAbstract, ABSTRACT);
-	}
+    public void setAbstract(boolean isAbstract) {
+        setModifier(isAbstract, ABSTRACT);
+    }
 
-	public boolean isStatic() {
-		return isModifier(STATIC);
-	}
+    public boolean isStatic() {
+        return isModifier(STATIC);
+    }
 
-	public void setStatic() {
-		setStatic(true);
-	}
+    /**
+     * @deprecated Use setStatic(boolean) instead.
+     */
+    @Deprecated
+    public void setStatic() {
+        setStatic(true);
+    }
 
-	public void setStatic(boolean isStatic) {
-		setModifier(isStatic, STATIC);
-	}
+    public void setStatic(boolean isStatic) {
+        setModifier(isStatic, STATIC);
+    }
 
-	public boolean isFinal() {
-		return isModifier(FINAL);
-	}
+    public boolean isFinal() {
+        return isModifier(FINAL);
+    }
 
-	public void setFinal() {
-		setFinal(true);
-	}
+    /**
+     * @deprecated Use setFinal(boolean) instead.
+     */
+    @Deprecated
+    public void setFinal() {
+        setFinal(true);
+    }
 
-	public void setFinal(boolean isFinal) {
-		setModifier(isFinal, FINAL);
-	}
+    public void setFinal(boolean isFinal) {
+        setModifier(isFinal, FINAL);
+    }
 
-	public boolean isSynchronized() {
-		return isModifier(SYNCHRONIZED);
-	}
+    public boolean isSynchronized() {
+        return isModifier(SYNCHRONIZED);
+    }
 
-	public void setSynchronized() {
-		setSynchronized(true);
-	}
+    /**
+     * @deprecated Use setSynchronized(boolean) instead.
+     */
+    @Deprecated
+    public void setSynchronized() {
+        setSynchronized(true);
+    }
 
-	public void setSynchronized(boolean isSynchronized) {
-		setModifier(isSynchronized, SYNCHRONIZED);
-	}
+    public void setSynchronized(boolean isSynchronized) {
+        setModifier(isSynchronized, SYNCHRONIZED);
+    }
 
-	public boolean isNative() {
-		return isModifier(NATIVE);
-	}
+    public boolean isNative() {
+        return isModifier(NATIVE);
+    }
 
-	public void setNative() {
-		setNative(true);
-	}
+    /**
+     * @deprecated Use setNative(boolean) instead.
+     */
+    @Deprecated
+    public void setNative() {
+        setNative(true);
+    }
 
-	public void setNative(boolean isNative) {
-		setModifier(isNative, NATIVE);
-	}
+    public void setNative(boolean isNative) {
+        setModifier(isNative, NATIVE);
+    }
 
-	public boolean isTransient() {
-		return isModifier(TRANSIENT);
-	}
+    public boolean isTransient() {
+        return isModifier(TRANSIENT);
+    }
 
-	public void setTransient() {
-		setTransient(true);
-	}
+    /**
+     * @deprecated Use setTransient(boolean) instead.
+     */
+    @Deprecated
+    public void setTransient() {
+        setTransient(true);
+    }
 
-	public void setTransient(boolean isTransient) {
-		setModifier(isTransient, TRANSIENT);
-	}
+    public void setTransient(boolean isTransient) {
+        setModifier(isTransient, TRANSIENT);
+    }
 
-	public boolean isVolatile() {
-		return isModifier(VOLATILE);
-	}
+    public boolean isVolatile() {
+        return isModifier(VOLATILE);
+    }
 
-	public void setVolatile() {
-		setVolatile(true);
-	}
+    /**
+     * @deprecated Use setVolatile(boolean) instead.
+     */
+    @Deprecated
+    public void setVolatile() {
+        setVolatile(true);
+    }
 
-	public void setVolatile(boolean isVolative) {
-		setModifier(isVolative, VOLATILE);
-	}
+    public void setVolatile(boolean isVolative) {
+        setModifier(isVolative, VOLATILE);
+    }
 
-	public boolean isStrictfp() {
-		return isModifier(STRICTFP);
-	}
+    public boolean isStrictfp() {
+        return isModifier(STRICTFP);
+    }
 
-	public void setStrictfp() {
-		setStrictfp(true);
-	}
+    /**
+     * @deprecated Use setStrictfp(boolean) instead.
+     */
+    @Deprecated
+    public void setStrictfp() {
+        setStrictfp(true);
+    }
 
-	public void setStrictfp(boolean isStrictfp) {
-		setModifier(isStrictfp, STRICTFP);
-	}
+    public void setStrictfp(boolean isStrictfp) {
+        setModifier(isStrictfp, STRICTFP);
+    }
 
-	private final boolean isModifier(int mask) {
-		return (modifiers & mask) == mask;
-	}
+    private final boolean isModifier(int mask) {
+        return (modifiers & mask) == mask;
+    }
 
-	private void setModifier(boolean enable, int mask) {
-		if (enable) {
-			this.modifiers |= mask;
-		} else {
-			this.modifiers &= ~mask;
-		}
-	}
+    private void setModifier(boolean enable, int mask) {
+        if (enable) {
+            this.modifiers |= mask;
+        } else {
+            this.modifiers &= ~mask;
+        }
+    }
 
-	public boolean isPackagePrivate() {
-		return !isPrivate() && !isPublic() && !isProtected();
-	}
+    public boolean isPackagePrivate() {
+        return !isPrivate() && !isPublic() && !isProtected();
+    }
 }
