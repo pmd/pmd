fc173e08c8b0936797441ed2c6aa3c96b24fd68f
Xavier Le Vourch <xlv@users.sourceforge.net>
2008-10-13 18:20:59 +0000

    preparing for next version 4.2.5-SNAPSHOT
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/branches/pmd/4.2.x@6598 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/bin/cpdgui.bat
+++ pmd/bin/cpdgui.bat
@@ -1,6 +1,6 @@
 @echo off
 set TOPDIR=%~dp0/..
-set VERSION=4.2.4
+set VERSION=4.2.5
 set PMDJAR=%TOPDIR%/lib/pmd-%VERSION%.jar
 set JARPATH=%TOPDIR%/lib/asm-3.1.jar;%TOPDIR%/lib/jaxen-1.1.1.jar
 set OPTS=-Xmx512m
