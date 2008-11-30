8917ae0c074595195060710056f33dd6e38b9c6f
Ryan Gustafson <rgustav@users.sourceforge.net>
2008-11-30 20:52:37 +0000

    Add methods for determining common well-known contexts of usage for an ASTName node.
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@6721 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/net/sourceforge/pmd/lang/ecmascript/ast/ASTName.java
+++ pmd/src/net/sourceforge/pmd/lang/ecmascript/ast/ASTName.java
@@ -25,4 +25,50 @@ public class ASTName extends AbstractEcmascriptNode<Name> {
     public boolean isGlobalName() {
 	return !node.isLocalName();
     }
+
+    /**
+     * Returns whether this name node is the name of a function declaration.
+     * @return <code>true</code> if name of a function declaration,
+     * <code>false</code> otherwise.
+     */
+    public boolean isFunctionNodeName() {
+	return (jjtGetParent() instanceof ASTFunctionNode)
+		&& (((ASTFunctionNode) jjtGetParent()).getFunctionName() == this);
+    }
+
+    /**
+     * Returns whether this name node is the name of a function declaration parameter.
+     * @return <code>true</code> if name of a function declaration parameter,
+     * <code>false</code> otherwise.
+     */
+    public boolean isFunctionNodeParameter() {
+	if ((jjtGetParent() instanceof ASTFunctionNode)) {
+	    ASTFunctionNode functionNode = (ASTFunctionNode) jjtGetParent();
+	    for (int i = 0; i < functionNode.getNumParams(); i++) {
+		if (functionNode.getParam(i) == this) {
+		    return true;
+		}
+	    }
+	}
+	return false;
+    }
+
+    /**
+     * Returns whether this name node is the name of a function call.
+     * @return <code>true</code> if name of a function call,
+     * <code>false</code> otherwise.
+     */
+    public boolean isFunctionCallName() {
+	return (jjtGetParent() instanceof ASTFunctionCall) && (((ASTFunctionCall) jjtGetParent()).getTarget() == this);
+    }
+
+    /**
+     * Returns whether this name node is the name of a variable declaration.
+     * @return <code>true</code> if name of a variable declaration,
+     * <code>false</code> otherwise.
+     */
+    public boolean isVariableDeclaration() {
+	return (jjtGetParent() instanceof ASTVariableInitializer)
+		&& (((ASTVariableInitializer) jjtGetParent()).getTarget() == this);
+    }
 }
