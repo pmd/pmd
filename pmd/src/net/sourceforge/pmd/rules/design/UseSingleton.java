0fb697a50b0edc7b1bab37d57220e16b11a19ed0
Xavier Le Vourch <xlv@users.sourceforge.net>
2008-12-11 06:32:25 +0000

    Fixed bug 2404700 - UseSingleton should not act on enums
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/branches/pmd/4.2.x@6746 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/net/sourceforge/pmd/rules/design/UseSingleton.java
+++ pmd/src/net/sourceforge/pmd/rules/design/UseSingleton.java
@@ -4,71 +4,66 @@
 package net.sourceforge.pmd.rules.design;
 
 import net.sourceforge.pmd.AbstractRule;
+import net.sourceforge.pmd.ast.ASTClassOrInterfaceBody;
 import net.sourceforge.pmd.ast.ASTClassOrInterfaceDeclaration;
 import net.sourceforge.pmd.ast.ASTClassOrInterfaceType;
-import net.sourceforge.pmd.ast.ASTCompilationUnit;
 import net.sourceforge.pmd.ast.ASTConstructorDeclaration;
 import net.sourceforge.pmd.ast.ASTFieldDeclaration;
 import net.sourceforge.pmd.ast.ASTMethodDeclaration;
 import net.sourceforge.pmd.ast.ASTResultType;
+import net.sourceforge.pmd.ast.Node;
 
 public class UseSingleton extends AbstractRule {
 
-    private boolean isOK;
-    private int methodCount;
+    @Override
+    public Object visit(ASTClassOrInterfaceBody decl, Object data) {
+        if (decl.jjtGetParent() instanceof ASTClassOrInterfaceDeclaration) {
+            ASTClassOrInterfaceDeclaration parent = (ASTClassOrInterfaceDeclaration) decl.jjtGetParent();
+            if (parent.isAbstract() || parent.isInterface()) {
+                return super.visit(decl, data);
+            }
+            int i = decl.jjtGetNumChildren();
+            int methodCount = 0;
+            boolean isOK = false;
+            while (i > 0) {
+                Node n = decl.jjtGetChild(--i).jjtGetChild(0);
+                if (n instanceof ASTFieldDeclaration) {
+                    if (!((ASTFieldDeclaration) n).isStatic()) {
+                        isOK = true;
+                        break;
+                    }
+                } else if (n instanceof ASTConstructorDeclaration) {
+                    if (((ASTConstructorDeclaration) n).isPrivate()) {
+                        isOK = true;
+                        break;
+                    }
+                } else if (n instanceof ASTMethodDeclaration) {
+                    ASTMethodDeclaration m = (ASTMethodDeclaration) n;
+                    if (!m.isPrivate()) {
+                        methodCount++;
+                    }
+                    if (!m.isStatic()) {
+                        isOK = true;
+                        break;
+                    }
 
-    public Object visit(ASTCompilationUnit cu, Object data) {
-        methodCount = 0;
-        isOK = false;
-        Object result = cu.childrenAccept(this, data);
-        if (!isOK && methodCount > 0) {
-            addViolation(data, cu);
-        }
+                    // TODO use symbol table
+                    if (m.getMethodName().equals("suite")) {
+                        ASTResultType res = m.getResultType();
+                        ASTClassOrInterfaceType c = res.getFirstChildOfType(ASTClassOrInterfaceType.class);
+                        if (c != null && c.hasImageEqualTo("Test")) {
+                            isOK = true;
+                            break;
+                        }
+                    }
 
-        return result;
-    }
-
-    public Object visit(ASTFieldDeclaration decl, Object data) {
-        if (!decl.isStatic()) {
-            isOK = true;
-        }
-        return data;
-    }
-
-    public Object visit(ASTConstructorDeclaration decl, Object data) {
-        if (decl.isPrivate()) {
-            isOK = true;
-        }
-        return data;
-    }
-
-    public Object visit(ASTClassOrInterfaceDeclaration decl, Object data) {
-        if (decl.isAbstract()) {
-            isOK = true;
-        }
-        return super.visit(decl, data);
-    }
-
-    public Object visit(ASTMethodDeclaration decl, Object data) {
-        // Private method does no count
-	if ( ! decl.isPrivate() ) {
-		methodCount++;
-	}
-
-        if (!isOK && !decl.isStatic()  ) {
-            isOK = true;
-        }
-
-        // TODO use symbol table
-        if (decl.getMethodName().equals("suite")) {
-            ASTResultType res = decl.getFirstChildOfType(ASTResultType.class);
-            ASTClassOrInterfaceType c = res.getFirstChildOfType(ASTClassOrInterfaceType.class);
-            if (c != null && c.hasImageEqualTo("Test")) {
-                isOK = true;
+                }
+            }
+            if (!isOK && methodCount > 0) {
+                addViolation(data, decl);
             }
         }
-
-        return data;
+        return super.visit(decl, data);
     }
 
 }
