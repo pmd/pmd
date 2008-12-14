6253bb9c0e415541d7cac04789abcb5e09166502
Xavier Le Vourch <xlv@users.sourceforge.net>
2008-12-14 20:56:14 +0000

    test case for [ 2429017 ] Failed parsing byte[].class.hashCode()
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@6755 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/regress/test/net/sourceforge/pmd/ast/ParserCornersTest.java
+++ pmd/regress/test/net/sourceforge/pmd/ast/ParserCornersTest.java
@@ -5,12 +5,22 @@ import net.sourceforge.pmd.PMD;
 import org.junit.Test;
 
 import test.net.sourceforge.pmd.testframework.ParserTst;
+import test.net.sourceforge.pmd.testframework.TestDescriptor;
 
 public class ParserCornersTest extends ParserTst {
 
     @Test
     public final void testGetFirstASTNameImageNull() throws Throwable {
         parseJava14(ABSTRACT_METHOD_LEVEL_CLASS_DECL);
+    }
+
+    @Test
+    public final void testCastLookaheadProblem() throws Throwable {
+        if (TestDescriptor.inRegressionTestMode()) {
+            // skip this test if we're only running regression tests
+            return;
+        }
+        parseJava14(CAST_LOOKAHEAD_PROBLEM);
     }
 
     private static final String ABSTRACT_METHOD_LEVEL_CLASS_DECL =
@@ -22,6 +32,13 @@ public class ParserCornersTest extends ParserTst {
             "   }}" + PMD.EOL +
             "  }" + PMD.EOL +
             "}";
+
+    private static final String CAST_LOOKAHEAD_PROBLEM =
+        "public class BadClass {" + PMD.EOL +
+        "  public Class foo() {" + PMD.EOL +
+        "    return (byte[].class);" + PMD.EOL +
+        "  }" + PMD.EOL +
+        "}";
 
     public static junit.framework.Test suite() {
         return new junit.framework.JUnit4TestAdapter(ParserCornersTest.class);
