2817991f7d282ab8eac78cf72dcec002de730707
Xavier Le Vourch <xlv@users.sourceforge.net>
2009-02-09 00:52:32 +0000

    preparing 4.2.5 release
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/branches/pmd/4.2.x@6830 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/net/sourceforge/pmd/PMD.java
+++ pmd/src/net/sourceforge/pmd/PMD.java
@@ -48,7 +48,7 @@ import net.sourceforge.pmd.util.FileFinder;
 
 public class PMD {
     public static final String EOL = System.getProperty("line.separator", "\n");
-    public static final String VERSION = "4.2.5-SNAPSHOT";
+    public static final String VERSION = "4.2.5";
     public static final String EXCLUDE_MARKER = "NOPMD";
 
     private static final Logger LOG = Logger.getLogger(PMD.class.getName());
