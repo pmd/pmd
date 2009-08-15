dec39ce09c6c2011b83602b2b25d3c8e47b81c44
Romain Pelisse <rpelisse@users.sourceforge.net>
2009-08-15 16:01:33 +0000

    Fixing bug 2835074 False -: DoubleCheckedLocking with reversed null check
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@6977 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/net/sourceforge/pmd/lang/java/rule/basic/DoubleCheckedLockingRule.java
+++ pmd/src/net/sourceforge/pmd/lang/java/rule/basic/DoubleCheckedLockingRule.java
@@ -114,12 +114,11 @@ public class DoubleCheckedLockingRule extends AbstractJavaRule {
 
     private boolean ifVerify(ASTIfStatement is, String varname) {
         List<ASTPrimaryExpression> finder = is.findDescendantsOfType(ASTPrimaryExpression.class);
-        if (finder.size() > 1) {
-            ASTPrimaryExpression apeLeft = finder.get(0);
-            if (matchName(apeLeft, varname)) {
-                ASTPrimaryExpression apeRight = finder.get(1);
-                if (apeRight.jjtGetNumChildren() == 1 && apeRight.jjtGetChild(0) instanceof ASTPrimaryPrefix) {
-                    ASTPrimaryPrefix pp2 = (ASTPrimaryPrefix) apeRight.jjtGetChild(0);
+        if (finder.size() > 1) { 
+            ASTPrimaryExpression nullStmt = findNonVariableStmt(varname,finder.get(0),finder.get(1));
+            if ( nullStmt != null ) {
+                if ((nullStmt.jjtGetNumChildren() == 1) && (nullStmt.jjtGetChild(0) instanceof ASTPrimaryPrefix)) {
+                    ASTPrimaryPrefix pp2 = (ASTPrimaryPrefix) nullStmt.jjtGetChild(0);
                     if (pp2.jjtGetNumChildren() == 1 && pp2.jjtGetChild(0) instanceof ASTLiteral) {
                         ASTLiteral lit = (ASTLiteral) pp2.jjtGetChild(0);
                         if (lit.jjtGetNumChildren() == 1 && lit.jjtGetChild(0) instanceof ASTNullLiteral) {
@@ -132,7 +131,26 @@ public class DoubleCheckedLockingRule extends AbstractJavaRule {
         return false;
     }
 
-    private boolean matchName(ASTPrimaryExpression ape, String name) {
+    /** 
+     * <p>Sort out if apeLeft or apeRight are variable with the provided 'variableName'.</p>
+     * 
+     * @param variableName
+     * @param apeLeft
+     * @param apeRight
+     * @return reference from either apeLeft or apeRight, if one of them match, or 'null', if none match.
+     */
+	private ASTPrimaryExpression findNonVariableStmt(String variableName,
+			ASTPrimaryExpression apeLeft, ASTPrimaryExpression apeRight) {
+    	if (matchName(apeLeft, variableName) ) {
+    		return apeRight;
+    	}
+    	else if (matchName(apeRight, variableName) ) {
+    		return apeLeft;
+    	}
+		return null;
+	}
+
+	private boolean matchName(ASTPrimaryExpression ape, String name) {
         if (ape.jjtGetNumChildren() == 1 && ape.jjtGetChild(0) instanceof ASTPrimaryPrefix) {
             ASTPrimaryPrefix pp = (ASTPrimaryPrefix) ape.jjtGetChild(0);
             String name2 = getNameFromPrimaryPrefix(pp);
