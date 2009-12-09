79ef48672463944e364e7431b6c43878f6599f74
Dale Anson <daleanson@users.sourceforge.net>
2009-12-09 17:10:47 +0000

    Changed javascript to ecmascript.
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/branches/pmd/4.2.x@7024 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/regress/test/net/sourceforge/pmd/cpd/EcmascriptTokenizerTest.java
+++ pmd/regress/test/net/sourceforge/pmd/cpd/EcmascriptTokenizerTest.java
@@ -5,18 +5,18 @@ package test.net.sourceforge.pmd.cpd;
 
 import static org.junit.Assert.assertEquals;
 import net.sourceforge.pmd.PMD;
-import net.sourceforge.pmd.cpd.JavascriptTokenizer;
+import net.sourceforge.pmd.cpd.EcmascriptTokenizer;
 import net.sourceforge.pmd.cpd.SourceCode;
 import net.sourceforge.pmd.cpd.Tokenizer;
 import net.sourceforge.pmd.cpd.Tokens;
 
 import org.junit.Test;
 
-public class JavascriptTokenizerTest {
+public class EcmascriptTokenizerTest {
 
     @Test
     public void test1() throws Throwable {
-        Tokenizer tokenizer = new JavascriptTokenizer();
+        Tokenizer tokenizer = new EcmascriptTokenizer();
         SourceCode sourceCode = new SourceCode( new SourceCode.StringCodeLoader( getCode1() ) );
         Tokens tokens = new Tokens();
         tokenizer.tokenize( sourceCode, tokens );
@@ -25,7 +25,7 @@ public class JavascriptTokenizerTest {
 
     @Test
     public void test2() throws Throwable {
-        Tokenizer t = new JavascriptTokenizer();
+        Tokenizer t = new EcmascriptTokenizer();
         SourceCode sourceCode = new SourceCode( new SourceCode.StringCodeLoader( getCode2() ) );
         Tokens tokens = new Tokens();
         t.tokenize( sourceCode, tokens );
@@ -59,6 +59,6 @@ public class JavascriptTokenizerTest {
     }
 
     public static junit.framework.Test suite() {
-        return new junit.framework.JUnit4TestAdapter( JavascriptTokenizerTest.class );
+        return new junit.framework.JUnit4TestAdapter( EcmascriptTokenizerTest.class );
     }
 }
