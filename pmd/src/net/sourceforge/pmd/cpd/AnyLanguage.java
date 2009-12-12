b6f25c6a0e8facbe2c94438a1af837d1cab32f90
Dale Anson <daleanson@users.sourceforge.net>
2009-12-12 16:22:39 +0000

    Added vararg to constructor so it matches with super constructor.
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@7028 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/net/sourceforge/pmd/cpd/AnyLanguage.java
+++ pmd/src/net/sourceforge/pmd/cpd/AnyLanguage.java
@@ -4,7 +4,7 @@
 package net.sourceforge.pmd.cpd;
 
 public class AnyLanguage extends AbstractLanguage {
-	public AnyLanguage(String extension) {
+	public AnyLanguage(String... extension) {
 		super(new AnyTokenizer(), extension);
 	}
 }
