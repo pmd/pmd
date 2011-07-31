33be1837535c3cbc34289b280279a991f5e44c8b
Brian Remedios <hooperbloob@users.sourceforge.net>
2011-07-31 07:46:59 +0000

    test class path fix by Andreas Dangel
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@7211 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/regress/test/net/sourceforge/pmd/lang/java/rule/typeresolution/xml/MyTestCase.java
+++ pmd/regress/test/net/sourceforge/pmd/lang/java/rule/typeresolution/xml/MyTestCase.java
@@ -6,6 +6,9 @@ import junit.framework.TestCase;
  * 	Warning, this class ARE not useless. 
  * 	It is used by the some of regression
  * 	tests.
+ *      See file: SignatureDeclareThrowsException.xml
+ *      
+ *      The file is already excluded from maven/surefire.
  */
 public class MyTestCase extends TestCase {
 
