c485378bbdbede52ca51ba6f9a7646ad00c1bfbf
Brian Remedios <hooperbloob@users.sourceforge.net>
2011-09-17 18:53:56 +0000

    updated, formatting. No functional changes
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@7275 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/net/sourceforge/pmd/renderers/HTMLRenderer.java
+++ pmd/src/net/sourceforge/pmd/renderers/HTMLRenderer.java
@@ -31,10 +31,10 @@ public class HTMLRenderer extends AbstractIncrementingRenderer {
     boolean colorize = true;
 
     public HTMLRenderer(Properties properties) {
-	super(NAME, "HTML format.", properties);
-
-	this.linkPrefix = properties.getProperty(LINK_PREFIX);
-	this.linePrefix = properties.getProperty(LINE_PREFIX);
+		super(NAME, "HTML format", properties);
+	
+		linkPrefix = properties.getProperty(LINK_PREFIX);
+		linePrefix = properties.getProperty(LINE_PREFIX);
     }
 
     /**
@@ -45,17 +45,17 @@ public class HTMLRenderer extends AbstractIncrementingRenderer {
      * @throws IOException
      */
     public void renderBody(Writer writer, Report report) throws IOException {
-	writer.write("<center><h3>PMD report</h3></center>");
-	writer.write("<center><h3>Problems found</h3></center>");
-	writer.write("<table align=\"center\" cellspacing=\"0\" cellpadding=\"3\"><tr>" + PMD.EOL
-		+ "<th>#</th><th>File</th><th>Line</th><th>Problem</th></tr>" + PMD.EOL);
-	setWriter(writer);
-	renderFileReport(report);
-	writer.write("</table>");
-	glomProcessingErrors(writer, errors);
-	if (showSuppressedViolations) {
-	    glomSuppressions(writer, suppressed);
-	}
+		writer.write("<center><h3>PMD report</h3></center>");
+		writer.write("<center><h3>Problems found</h3></center>");
+		writer.write("<table align=\"center\" cellspacing=\"0\" cellpadding=\"3\"><tr>" + PMD.EOL
+			+ "<th>#</th><th>File</th><th>Line</th><th>Problem</th></tr>" + PMD.EOL);
+		setWriter(writer);
+		renderFileReport(report);
+		writer.write("</table>");
+		glomProcessingErrors(writer, errors);
+		if (showSuppressedViolations) {
+		    glomSuppressions(writer, suppressed);
+		}
     }
 
     /**
@@ -63,12 +63,12 @@ public class HTMLRenderer extends AbstractIncrementingRenderer {
      */
     @Override
     public void start() throws IOException {
-	Writer writer = getWriter();
-	writer.write("<html><head><title>PMD</title></head><body>" + PMD.EOL);
-	writer.write("<center><h3>PMD report</h3></center>");
-	writer.write("<center><h3>Problems found</h3></center>");
-	writer.write("<table align=\"center\" cellspacing=\"0\" cellpadding=\"3\"><tr>" + PMD.EOL
-		+ "<th>#</th><th>File</th><th>Line</th><th>Problem</th></tr>" + PMD.EOL);
+		Writer writer = getWriter();
+		writer.write("<html><head><title>PMD</title></head><body>" + PMD.EOL);
+		writer.write("<center><h3>PMD report</h3></center>");
+		writer.write("<center><h3>Problems found</h3></center>");
+		writer.write("<table align=\"center\" cellspacing=\"0\" cellpadding=\"3\"><tr>" + PMD.EOL
+			+ "<th>#</th><th>File</th><th>Line</th><th>Problem</th></tr>" + PMD.EOL);
     }
 
     /**
@@ -76,8 +76,8 @@ public class HTMLRenderer extends AbstractIncrementingRenderer {
      */
     @Override
     public void renderFileViolations(Iterator<RuleViolation> violations) throws IOException {
-	Writer writer = getWriter();
-	glomRuleViolations(writer, violations);
+		Writer writer = getWriter();
+		glomRuleViolations(writer, violations);
     }
 
     /**
@@ -85,46 +85,51 @@ public class HTMLRenderer extends AbstractIncrementingRenderer {
      */
     @Override
     public void end() throws IOException {
-	Writer writer = getWriter();
-	writer.write("</table>");
-	glomProcessingErrors(writer, errors);
-	if (showSuppressedViolations) {
-	    glomSuppressions(writer, suppressed);
-	}
-	writer.write("</body></html>" + PMD.EOL);
+		Writer writer = getWriter();
+		writer.write("</table>");
+		glomProcessingErrors(writer, errors);
+		if (showSuppressedViolations) {
+		    glomSuppressions(writer, suppressed);
+		}
+		writer.write("</body></html>" + PMD.EOL);
     }
 
     private void glomRuleViolations(Writer writer, Iterator<RuleViolation> violations) throws IOException {
-	StringBuffer buf = new StringBuffer(500);
-	while (violations.hasNext()) {
-	    RuleViolation rv = violations.next();
-	    buf.setLength(0);
-	    buf.append("<tr");
-	    if (colorize) {
-		buf.append(" bgcolor=\"lightgrey\"");
-	    }
-	    colorize = !colorize;
-	    buf.append("> " + PMD.EOL);
-	    buf.append("<td align=\"center\">" + violationCount + "</td>" + PMD.EOL);
-	    buf.append("<td width=\"*%\">"
-		    + maybeWrap(rv.getFilename(), linePrefix == null ? "" : linePrefix
-			    + Integer.toString(rv.getBeginLine())) + "</td>" + PMD.EOL);
-	    buf.append("<td align=\"center\" width=\"5%\">" + Integer.toString(rv.getBeginLine()) + "</td>" + PMD.EOL);
-
-	    String d = StringUtil.htmlEncode(rv.getDescription());
-
-	    if (rv.getRule().getExternalInfoUrl() != null && rv.getRule().getExternalInfoUrl().length() != 0) {
-		d = "<a href=\"" + rv.getRule().getExternalInfoUrl() + "\">" + d + "</a>";
-	    }
-	    buf.append("<td width=\"*\">" + d + "</td>" + PMD.EOL);
-	    buf.append("</tr>" + PMD.EOL);
-	    writer.write(buf.toString());
-	    violationCount++;
-	}
+	    
+    	StringBuilder buf = new StringBuilder(500);
+	    
+		while (violations.hasNext()) {
+		    RuleViolation rv = violations.next();
+		    buf.setLength(0);
+		    buf.append("<tr");
+		    if (colorize) {
+		    	buf.append(" bgcolor=\"lightgrey\"");
+		    }
+		    colorize = !colorize;
+		    buf.append("> " + PMD.EOL);
+		    buf.append("<td align=\"center\">" + violationCount + "</td>" + PMD.EOL);
+		    buf.append("<td width=\"*%\">"
+			    + maybeWrap(rv.getFilename(), linePrefix == null ? "" : linePrefix
+				    + Integer.toString(rv.getBeginLine())) + "</td>" + PMD.EOL);
+		    buf.append("<td align=\"center\" width=\"5%\">" + Integer.toString(rv.getBeginLine()) + "</td>" + PMD.EOL);
+	
+		    String d = StringUtil.htmlEncode(rv.getDescription());
+	
+		    String infoUrl = rv.getRule().getExternalInfoUrl();
+		    if (StringUtil.isNotEmpty(infoUrl)) {
+				d = "<a href=\"" + infoUrl + "\">" + d + "</a>";
+			    }
+		    buf.append("<td width=\"*\">" + d + "</td>" + PMD.EOL);
+		    buf.append("</tr>" + PMD.EOL);
+		    writer.write(buf.toString());
+		    violationCount++;
+			}
     }
 
     private void glomProcessingErrors(Writer writer, List<Report.ProcessingError> errors) throws IOException {
-	if (!errors.isEmpty()) {
+	
+    	if (errors.isEmpty()) return;
+    	
 	    writer.write("<hr/>");
 	    writer.write("<center><h3>Processing errors</h3></center>");
 	    writer.write("<table align=\"center\" cellspacing=\"0\" cellpadding=\"3\"><tr>" + PMD.EOL
@@ -133,64 +138,61 @@ public class HTMLRenderer extends AbstractIncrementingRenderer {
 	    StringBuffer buf = new StringBuffer(500);
 	    boolean colorize = true;
 	    for (Report.ProcessingError pe : errors) {
-		buf.setLength(0);
-		buf.append("<tr");
-		if (colorize) {
-		    buf.append(" bgcolor=\"lightgrey\"");
-		}
-		colorize = !colorize;
-		buf.append("> " + PMD.EOL);
-		buf.append("<td>" + pe.getFile() + "</td>" + PMD.EOL);
-		buf.append("<td>" + pe.getMsg() + "</td>" + PMD.EOL);
-		buf.append("</tr>" + PMD.EOL);
-		writer.write(buf.toString());
-
-	    }
-	    writer.write("</table>");
-	}
+			buf.setLength(0);
+			buf.append("<tr");
+			if (colorize) {
+			    buf.append(" bgcolor=\"lightgrey\"");
+			}
+			colorize = !colorize;
+			buf.append("> " + PMD.EOL);
+			buf.append("<td>" + pe.getFile() + "</td>" + PMD.EOL);
+			buf.append("<td>" + pe.getMsg() + "</td>" + PMD.EOL);
+			buf.append("</tr>" + PMD.EOL);
+			writer.write(buf.toString());	
+		    }
     }
 
     private void glomSuppressions(Writer writer, List<Report.SuppressedViolation> suppressed) throws IOException {
-	if (!suppressed.isEmpty()) {
-	    writer.write("<hr/>");
-	    writer.write("<center><h3>Suppressed warnings</h3></center>");
-	    writer.write("<table align=\"center\" cellspacing=\"0\" cellpadding=\"3\"><tr>" + PMD.EOL
-		    + "<th>File</th><th>Line</th><th>Rule</th><th>NOPMD or Annotation</th><th>Reason</th></tr>"
-		    + PMD.EOL);
+		if (suppressed.isEmpty())  return;
 
-	    StringBuffer buf = new StringBuffer(500);
-	    boolean colorize = true;
-	    for (Report.SuppressedViolation sv : suppressed) {
-		buf.setLength(0);
-		buf.append("<tr");
-		if (colorize) {
-		    buf.append(" bgcolor=\"lightgrey\"");
-		}
-		colorize = !colorize;
-		buf.append("> " + PMD.EOL);
-		buf.append("<td align=\"left\">" + sv.getRuleViolation().getFilename() + "</td>" + PMD.EOL);
-		buf.append("<td align=\"center\">" + sv.getRuleViolation().getBeginLine() + "</td>" + PMD.EOL);
-		buf.append("<td align=\"center\">" + sv.getRuleViolation().getRule().getName() + "</td>" + PMD.EOL);
-		buf.append("<td align=\"center\">" + (sv.suppressedByNOPMD() ? "NOPMD" : "Annotation") + "</td>"
-			+ PMD.EOL);
-		buf.append("<td align=\"center\">" + (sv.getUserMessage() == null ? "" : sv.getUserMessage()) + "</td>"
-			+ PMD.EOL);
-		buf.append("</tr>" + PMD.EOL);
-		writer.write(buf.toString());
-	    }
-	    writer.write("</table>");
-	}
+		writer.write("<hr/>");
+		writer.write("<center><h3>Suppressed warnings</h3></center>");
+		writer.write("<table align=\"center\" cellspacing=\"0\" cellpadding=\"3\"><tr>" + PMD.EOL
+			    + "<th>File</th><th>Line</th><th>Rule</th><th>NOPMD or Annotation</th><th>Reason</th></tr>"
+			    + PMD.EOL);
+	
+		StringBuilder buf = new StringBuilder(500);
+		boolean colorize = true;
+		for (Report.SuppressedViolation sv : suppressed) {
+			buf.setLength(0);
+			buf.append("<tr");
+			if (colorize) {
+			    buf.append(" bgcolor=\"lightgrey\"");
+			}
+			colorize = !colorize;
+			buf.append("> " + PMD.EOL);
+			buf.append("<td align=\"left\">" + sv.getRuleViolation().getFilename() + "</td>" + PMD.EOL);
+			buf.append("<td align=\"center\">" + sv.getRuleViolation().getBeginLine() + "</td>" + PMD.EOL);
+			buf.append("<td align=\"center\">" + sv.getRuleViolation().getRule().getName() + "</td>" + PMD.EOL);
+			buf.append("<td align=\"center\">" + (sv.suppressedByNOPMD() ? "NOPMD" : "Annotation") + "</td>"
+					+ PMD.EOL);
+			buf.append("<td align=\"center\">" + (sv.getUserMessage() == null ? "" : sv.getUserMessage()) + "</td>"
+					+ PMD.EOL);
+			buf.append("</tr>" + PMD.EOL);
+			writer.write(buf.toString());
+			}
+		writer.write("</table>");
     }
 
     private String maybeWrap(String filename, String line) {
-	if (linkPrefix == null) {
-	    return filename;
-	}
-	String newFileName = filename;
-	int index = filename.lastIndexOf('.');
-	if (index >= 0) {
-	    newFileName = filename.substring(0, index).replace('\\', '/');
-	}
-	return "<a href=\"" + linkPrefix + newFileName + ".html#" + line + "\">" + newFileName + "</a>";
+		if (StringUtil.isEmpty(linkPrefix)) {
+		    return filename;
+		}
+		String newFileName = filename;
+		int index = filename.lastIndexOf('.');
+		if (index >= 0) {
+		    newFileName = filename.substring(0, index).replace('\\', '/');
+		}
+		return "<a href=\"" + linkPrefix + newFileName + ".html#" + line + "\">" + newFileName + "</a>";
     }
 }
