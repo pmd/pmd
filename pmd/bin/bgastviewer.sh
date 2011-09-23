b0a12465364f1768510fe1a1b4064e4c2e52b946
Romain Pelisse <rpelisse@users.sourceforge.net>
2011-09-23 17:32:03 +0000

    clean bgastviewer.sh script
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@7315 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/bin/bgastviewer.sh
+++ pmd/bin/bgastviewer.sh
@@ -1,9 +1,10 @@
-TOPDIR=..
-VERSION=5.0
-PMDJAR=$TOPDIR/lib/pmd-$VERSION.jar
-JARPATH=$TOPDIR/lib/asm-3.2.jar:$TOPDIR/lib/jaxen-1.1.1.jar;$TOPDIR/lib/saxon9.jar;$TOPDIR/lib/js-cvs-12122009.jar
-OPTS=
-MAIN_CLASS=net.sourceforge.pmd.util.viewer.Viewer
+#/bin/sh
 
-java $OPTS -cp $PMDJAR:$JARPATH:$TOPDIR/build $MAIN_CLASS $*
+readonly TOPDIR=.
+readonly VERSION=5.0
+readonly PMDJAR="${TOPDIR}/lib/pmd-${VERSION}.jar"
+readonly JARPATH="${TOPDIR}/lib/asm-3.2.jar:${TOPDIR}/lib/jaxen-1.1.1.jar;${TOPDIR}/lib/saxon9.jar;${TOPDIR}/lib/js-cvs-12122009.jar"
+readonly OPTS="${JAVA_OPTS}"
+readonly MAIN_CLASS="net.sourceforge.pmd.util.viewer.Viewer"
 
+java ${OPTS} -cp "${PMDJAR}:${JARPATH}:${TOPDIR}/build" ${MAIN_CLASS} ${*}
