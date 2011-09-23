62dda9ef0eafc59e178f979209aa77c212175e5f
Romain Pelisse <rpelisse@users.sourceforge.net>
2011-09-23 16:06:39 +0000

    delete now useless script wrapping scp
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@7313 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/bin/scp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-PATH=/usr/local/java/bin/:$PATH
-ANT_OPTS="-Xmx512m -Xms64m"
-PATH=$MAVEN_HOME/bin:$PATH
-CVSROOT=:ext:tomcopeland@cvs.sourceforge.net:/cvsroot/pmd
-CLASSPATH=../build/:../lib/asm-3.2.jar:../lib/jaxen-1.1.1.jar:../lib/junit-4.4.jar:/usr/local/ant/lib/ant.jar:/usr/local/ant/lib/ant-nodeps.jar
-export CLASSPATH PATH CVSROOT ANT_OPTS
