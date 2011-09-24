a85a9cd3c5818fad345e6fd725135c8fe1d79d06
Brian Remedios <hooperbloob@users.sourceforge.net>
2011-09-24 05:15:06 +0000

    StringBuffer -> StringBuilder switchovers
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@7331 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/net/sourceforge/pmd/cpd/VSRenderer.java
+++ pmd/src/net/sourceforge/pmd/cpd/VSRenderer.java
@@ -10,14 +10,16 @@ import java.util.Iterator;
 public class VSRenderer implements Renderer {
 
     public String render(Iterator<Match> matches) {
-        StringBuffer buffer = new StringBuffer(300);
+    	
+        StringBuilder buffer = new StringBuilder(300);
+        
         for (Match match; matches.hasNext();) {
             match = matches.next();
             TokenEntry mark;
             for (Iterator<TokenEntry> iterator = match.iterator(); iterator.hasNext();) {
                 mark = iterator.next();
                 buffer.append(mark.getTokenSrcID());
-                buffer.append("(" + mark.getBeginLine() +"):");
+                buffer.append('(').append(mark.getBeginLine()).append("):");
                 buffer.append(" Between lines " + mark.getBeginLine() + " and " + (mark.getBeginLine()+match.getLineCount()) + PMD.EOL);
             }
         }
