9e23e6fbbe4465cb045acd2d0138d6749f015c84
Brian Remedios <hooperbloob@users.sourceforge.net>
2011-11-23 08:11:46 +0000

    cleanup warnings & formatting, pull out utility method
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@7483 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/main/java/net/sourceforge/pmd/lang/xml/ast/DumpFacade.java
+++ pmd/src/main/java/net/sourceforge/pmd/lang/xml/ast/DumpFacade.java
@@ -8,93 +8,82 @@ import java.util.Iterator;
 import java.util.List;
 
 import net.sourceforge.pmd.lang.ast.xpath.Attribute;
+import net.sourceforge.pmd.util.StringUtil;
 
 public class DumpFacade {
 
-    private PrintWriter writer;
-    private boolean recurse;
+	private PrintWriter writer;
+	private boolean recurse;
 
-    public void initializeWith(Writer writer, String prefix, boolean recurse, XmlNode node) {
-	this.writer = (writer instanceof PrintWriter) ? (PrintWriter) writer : new PrintWriter(writer);
-	this.recurse = recurse;
-	this.dump(node, prefix);
-	try {
-	    writer.flush();
-	} catch (IOException e) {
-	    throw new RuntimeException("Problem flushing PrintWriter.", e);
-	}
-    }
-
-    public Object visit(XmlNode node, Object data) {
-	dump(node, (String) data);
-	if (recurse) {
-	    for (int i = 0; i < node.jjtGetNumChildren(); i++) {
-		visit((XmlNode) node.jjtGetChild(i), data + " ");
-	    }
-	    return data;
-	} else {
-	    return data;
-	}
-    }
-
-    private void dump(XmlNode node, String prefix) {
-	//
-	// Dump format is generally composed of the following items...
-	//
-
-	// 1) Dump prefix
-	writer.print(prefix);
-
-	// 2) JJT Name of the Node
-	writer.print(node.toString());
-
-	//
-	// If there are any additional details, then:
-	// 1) A colon
-	// 2) The Node.getImage() if it is non-empty
-	// 3) Extras in parentheses
-	//
-
-	// Standard image handling
-	String image = node.getImage();
-
-	// Special image handling (e.g. Nodes with normally null images)
-
-	image = escape(image);
-
-	// Extras
-	List<String> extras = new ArrayList<String>();
-	Iterator<Attribute> iterator = node.getAttributeIterator();
-	while (iterator.hasNext()) {
-	    Attribute attribute = iterator.next();
-	    extras.add(attribute.getName() + "=" + escape(attribute.getValue()));
+	public void initializeWith(Writer writer, String prefix, boolean recurse, XmlNode node) {
+		this.writer = (writer instanceof PrintWriter) ? (PrintWriter) writer : new PrintWriter(writer);
+		this.recurse = recurse;
+		this.dump(node, prefix);
+		try {
+			writer.flush();
+		} catch (IOException e) {
+			throw new RuntimeException("Problem flushing PrintWriter.", e);
+		}
 	}
 
-	// Output image and extras
-	if (image != null || !extras.isEmpty()) {
-	    writer.print(":");
-	    if (image != null) {
-		writer.print(image);
-	    }
-	    for (String extra : extras) {
-		writer.print("(");
-		writer.print(extra);
-		writer.print(")");
-	    }
+	public Object visit(XmlNode node, Object data) {
+		dump(node, (String) data);
+		if (recurse) {
+			for (int i = 0; i < node.jjtGetNumChildren(); i++) {
+				visit((XmlNode) node.jjtGetChild(i), data + " ");
+			}
+			return data;
+		} else {
+			return data;
+		}
 	}
 
-	writer.println();
-    }
+	private void dump(XmlNode node, String prefix) {
+		//
+		// Dump format is generally composed of the following items...
+		//
 
-    private static String escape(Object o) {
-	// Replace some whitespace characters so they are visually apparent.
-	if (o == null) {
-	    return null;
+		// 1) Dump prefix
+		writer.print(prefix);
+
+		// 2) JJT Name of the Node
+		writer.print(node.toString());
+
+		//
+		// If there are any additional details, then:
+		// 1) A colon
+		// 2) The Node.getImage() if it is non-empty
+		// 3) Extras in parentheses
+		//
+
+		// Standard image handling
+		String image = node.getImage();
+
+		// Special image handling (e.g. Nodes with normally null images)
+
+		image = StringUtil.escapeWhitespace(image);
+
+		// Extras
+		List<String> extras = new ArrayList<String>();
+		Iterator<Attribute> iterator = node.getAttributeIterator();
+		while (iterator.hasNext()) {
+			Attribute attribute = iterator.next();
+			extras.add(attribute.getName() + "=" + StringUtil.escapeWhitespace(attribute.getValue()));
+		}
+
+		// Output image and extras
+		if (image != null || !extras.isEmpty()) {
+			writer.print(':');
+			if (image != null) {
+				writer.print(image);
+			}
+			for (String extra : extras) {
+				writer.print('(');
+				writer.print(extra);
+				writer.print(')');
+			}
+		}
+
+		writer.println();
 	}
-	String s = String.valueOf(o);
-	s = s.replace("\n", "\\n");
-	s = s.replace("\r", "\\r");
-	s = s.replace("\t", "\\t");
-	return s;
-    }
 }
--- pmd/src/main/java/net/sourceforge/pmd/util/StringUtil.java
+++ pmd/src/main/java/net/sourceforge/pmd/util/StringUtil.java
@@ -182,6 +182,24 @@ public final class StringUtil {
     }
 
     /**
+     * Replace some whitespace characters so they are visually apparent.
+     * 
+     * @param o
+     * @return String
+     */
+	public static String escapeWhitespace(Object o) {
+		
+		if (o == null) {
+			return null;
+		}
+		String s = String.valueOf(o);
+		s = s.replace("\n", "\\n");
+		s = s.replace("\r", "\\r");
+		s = s.replace("\t", "\\t");
+		return s;
+	}
+    
+    /**
      *
      * @param string String
      * @return String
