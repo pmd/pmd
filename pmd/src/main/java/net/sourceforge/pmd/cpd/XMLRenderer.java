970771586cd560561b6455c48dcdf0b8dd89d834
Romain Pelisse <rpelisse@users.sourceforge.net>
2012-02-24 10:35:41 +0000

    Fix 3470274: Using Label for lines in XMLRenderer.
    Also did some code cleaning/refactor of the class.
    
    git-svn-id: https://pmd.svn.sourceforge.net/svnroot/pmd/trunk@7628 51baf565-9d33-0410-a72c-fc3788e3496d

--- pmd/src/main/java/net/sourceforge/pmd/cpd/XMLRenderer.java
+++ pmd/src/main/java/net/sourceforge/pmd/cpd/XMLRenderer.java
@@ -20,79 +20,75 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 /**
- * @author Philippe T'Seyen
+ * @author Philippe T'Seyen - original implementation
+ * @author Romain Pelisse - javax.xml implementation 
+ * 
  */
 public final class XMLRenderer implements Renderer {
-	
-	// FUTURE: Use a XML API - rather than StringBuffer to generate XML Report. 
-    // The most convenient would be to use one shipped in the JRE, and this should
-    // also allow us to get ride of the encode below, as the XML API choosed will
-    // do that for us...
-    public String render(Iterator<Match> matches) {
-    	Document doc = null;
+
+	private Document createDocument() {
     	try {
 			DocumentBuilderFactory factory = DocumentBuilderFactory
 					.newInstance();
 			DocumentBuilder parser = factory.newDocumentBuilder();
-			doc = parser.newDocument();
+			return parser.newDocument();
 		} catch (ParserConfigurationException e) {
 			throw new IllegalStateException(e);
 		}
-		Element root = doc.createElement("pmd-cpd");
-
-		doc.appendChild(root);
-/*        StringBuilder buffer = new StringBuilder(300);
-        buffer.append("<?xml version=\"1.0\" encoding=\"");
-        buffer.append(encoding);
-        buffer.append("\"?>").append(PMD.EOL);
-        buffer.append("<pmd-cpd>").append(PMD.EOL);*/
-        Match match;
-        while (matches.hasNext()) {
-            match = matches.next();
-            Element duplication = doc.createElement("duplication");
-            duplication.setAttribute("lines", match.getLabel());
-            duplication.setAttribute("tokens", String.valueOf(match.getTokenCount()));
-/*            buffer.append("<duplication lines=\"");
-            buffer.append(match.getLineCount());
-            buffer.append("\" tokens=\"");
-            buffer.append(match.getTokenCount());
-            buffer.append("\">").append(PMD.EOL);*/
-
-            TokenEntry mark;
-            for (Iterator<TokenEntry> iterator = match.iterator(); iterator.hasNext();) {
-                mark = iterator.next();
-                Element file = doc.createElement("file");
-                file.setAttribute("line", String.valueOf(mark.getBeginLine()));
-                file.setAttribute("path", mark.getTokenSrcID());
-                duplication.appendChild(file);
-/*                buffer.append("<file line=\"");
-                buffer.append(mark.getBeginLine());
-                buffer.append("\" path=\"");
-                buffer.append(XMLRenderer.encode(mark.getTokenSrcID()));
-                buffer.append("\"/>").append(PMD.EOL);*/
-            }
-            String codeSnipet = match.getSourceCodeSlice();
-            if (codeSnipet != null) {
-            	Element codefragment = doc.createElement("codefragment");
- /*               buffer.append("<codefragment>").append(PMD.EOL);
-                buffer.append("<![CDATA[").append(PMD.EOL);*/
-//                buffer.append(StringUtil.replaceString(codeFragment, "]]>", "]]&gt;")).append(PMD.EOL + "]]>" + PMD.EOL + "</codefragment>" + PMD.EOL);
-                codefragment.appendChild(doc.createCDATASection(codeSnipet));
-                duplication.appendChild(codefragment);
-            }
-            //buffer.append("</duplication>").append(PMD.EOL);
-            root.appendChild(duplication);
-        }
-        // 
+	}
+	
+	private String xmlDocToString(Document doc) {
         try {
-        TransformerFactory tf = TransformerFactory.newInstance();
-        Transformer transformer = tf.newTransformer();
-        transformer.setOutputProperty(OutputKeys.ENCODING, System.getProperty("file.encoding"));
-        StringWriter writer = new StringWriter();
-        transformer.transform(new DOMSource(doc), new StreamResult(writer));
-        return writer.getBuffer().toString().replaceAll("\n|\r", "");
+	        TransformerFactory tf = TransformerFactory.newInstance();
+	        Transformer transformer = tf.newTransformer();
+	        transformer.setOutputProperty(OutputKeys.ENCODING, System.getProperty("file.encoding"));
+	        StringWriter writer = new StringWriter();
+	        transformer.transform(new DOMSource(doc), new StreamResult(writer));
+	        return writer.getBuffer().toString().replaceAll("\n|\r", "");
         }  catch (TransformerException e) {
         	throw new IllegalStateException(e);
 		}
+	}
+	
+    public String render(Iterator<Match> matches) {
+    	Document doc = createDocument();
+		Element root = doc.createElement("pmd-cpd");
+		doc.appendChild(root);
+
+        Match match;
+        while (matches.hasNext()) {
+            match = matches.next();
+            root.appendChild( addCodeSnippet(doc, addFilesToDuplicationElement(doc, createDuplicationElement(doc, match), match), match ) );
+        }
+        return xmlDocToString(doc);
+    }
+    
+    private Element addFilesToDuplicationElement(Document doc, Element duplication, Match match) {
+    	TokenEntry mark;
+        for (Iterator<TokenEntry> iterator = match.iterator(); iterator.hasNext();) {
+            mark = iterator.next();
+            Element file = doc.createElement("file");
+            file.setAttribute("line", String.valueOf(mark.getBeginLine()));
+            file.setAttribute("path", mark.getTokenSrcID());
+            duplication.appendChild(file);
+        }
+        return duplication;
+    }
+
+    private Element addCodeSnippet(Document doc, Element duplication, Match match) {
+        String codeSnipet = match.getSourceCodeSlice();
+        if (codeSnipet != null) {
+        	Element codefragment = doc.createElement("codefragment");
+            codefragment.appendChild(doc.createCDATASection(codeSnipet));
+            duplication.appendChild(codefragment);
+        }
+        return duplication;
+    }
+    
+    private Element createDuplicationElement(Document doc, Match match) {
+        Element duplication = doc.createElement("duplication");
+        duplication.setAttribute("lines", String.valueOf(match.getLineCount()));
+        duplication.setAttribute("tokens", String.valueOf(match.getTokenCount()));
+        return duplication;
     }
 }
