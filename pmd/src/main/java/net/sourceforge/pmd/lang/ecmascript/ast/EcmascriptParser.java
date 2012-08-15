98c18554df4de550eea6e023fbb4522c890fb59e
ryan-gustafson <ryan.gustafson@gmail.com>
2012-08-15 05:42:50 -0500

    Add ecmascript AvoidTrailingComma rule.

--- pmd/src/main/java/net/sourceforge/pmd/lang/ecmascript/ast/EcmascriptParser.java
+++ pmd/src/main/java/net/sourceforge/pmd/lang/ecmascript/ast/EcmascriptParser.java
@@ -5,52 +5,53 @@ package net.sourceforge.pmd.lang.ecmascript.ast;
 
 import java.io.IOException;
 import java.io.Reader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 import net.sourceforge.pmd.lang.ast.ParseException;
 import net.sourceforge.pmd.lang.ecmascript.EcmascriptParserOptions;
 
 import org.mozilla.javascript.CompilerEnvirons;
-import org.mozilla.javascript.ErrorReporter;
 import org.mozilla.javascript.Parser;
-import org.mozilla.javascript.ast.AstNode;
 import org.mozilla.javascript.ast.AstRoot;
 import org.mozilla.javascript.ast.ErrorCollector;
+import org.mozilla.javascript.ast.ParseProblem;
 
 public class EcmascriptParser {
     protected final EcmascriptParserOptions parserOptions;
-    protected Map<AstNode, EcmascriptNode> nodeCache = new HashMap<AstNode, EcmascriptNode>();
 
     public EcmascriptParser(EcmascriptParserOptions parserOptions) {
 	this.parserOptions = parserOptions;
     }
 
-    protected AstRoot parseEcmascript(final Reader reader) throws ParseException {
+    protected AstRoot parseEcmascript(final Reader reader, final List<ParseProblem> parseProblems) throws ParseException {
 	final CompilerEnvirons compilerEnvirons = new CompilerEnvirons();
 	compilerEnvirons.setRecordingComments(parserOptions.isRecordingComments());
 	compilerEnvirons.setRecordingLocalJsDocComments(parserOptions.isRecordingLocalJsDocComments());
 	compilerEnvirons.setLanguageVersion(parserOptions.getRhinoLanguageVersion().getVersion());
 	compilerEnvirons.setIdeMode(true); // Scope's don't appear to get set right without this
+	compilerEnvirons.setWarnTrailingComma(true);
 
-	// TODO Fix hardcode
-	final ErrorReporter errorReporter = new ErrorCollector();
-	final Parser parser = new Parser(compilerEnvirons, errorReporter);
-	nodeCache.clear();
+	// TODO We should do something with Rhino errors...
+	final ErrorCollector errorCollector = new ErrorCollector();
+	final Parser parser = new Parser(compilerEnvirons, errorCollector);
 	try {
 	    // TODO Fix hardcode
 	    final String sourceURI = "unknown";
 	    // TODO Fix hardcode
 	    final int lineno = 0;
-	    return parser.parse(reader, sourceURI, lineno);
+	    AstRoot astRoot = parser.parse(reader, sourceURI, lineno);
+	    parseProblems.addAll(errorCollector.getErrors());
+	    return astRoot;
 	} catch (final IOException e) {
 	    throw new ParseException(e);
 	}
     }
 
     public EcmascriptNode parse(final Reader reader) {
-	final AstRoot astRoot = parseEcmascript(reader);
-	final EcmascriptTreeBuilder treeBuilder = new EcmascriptTreeBuilder();
+	final List<ParseProblem> parseProblems = new ArrayList<ParseProblem>();
+	final AstRoot astRoot = parseEcmascript(reader, parseProblems);
+	final EcmascriptTreeBuilder treeBuilder = new EcmascriptTreeBuilder(parseProblems);
 	return treeBuilder.build(astRoot);
     }
 }
