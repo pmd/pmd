99c71ddcbd487a309ce728aaffb097bd21436cc8
Stuart Turton <Stuart.Turton@oracle-troubadour.co.uk>
2012-10-10 19:28:30 +0100

    PLSQL AST now copes with 11G CONTINUE statements; PMD rule works for simple PLSQL; DFA is not working; PMD symbol handling is very buggy

---
+++ pmd/src/main/java/net/sourceforge/pmd/util/designer/DFAPLSQLGraphRule.java
@@ -0,0 +1,78 @@
+package net.sourceforge.pmd.util.designer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sourceforge.pmd.lang.plsql.ast.ASTInput;
+//import net.sourceforge.pmd.lang.plsql.ast.ASTConstructorDeclaration;
+import net.sourceforge.pmd.lang.plsql.ast.ASTMethodDeclaration;
+import net.sourceforge.pmd.lang.plsql.ast.ASTProgramUnit;
+import net.sourceforge.pmd.lang.plsql.ast.ASTTriggerUnit;
+import net.sourceforge.pmd.lang.plsql.rule.AbstractPLSQLRule;
+
+public class DFAPLSQLGraphRule extends AbstractPLSQLRule {
+
+    private List<ASTMethodDeclaration> methods;
+    private List<ASTProgramUnit> programUnits;
+    private List<ASTTriggerUnit> triggerUnits;
+    // SRT private List<ASTConstructorDeclaration> constructors;
+
+    public DFAPLSQLGraphRule() {
+	super();
+	super.setUsesDFA();
+    }
+
+    public List<ASTMethodDeclaration> getMethods() {
+	return this.methods;
+    }
+
+    public List<ASTProgramUnit> getProgramUnits() {
+	return this.programUnits;
+    }
+
+    public List<ASTTriggerUnit> getTriggerUnits() {
+	return this.triggerUnits;
+    }
+
+    /* SRT public List<ASTConstructorDeclaration> getConstructors() {
+	return this.constructors;
+    } */
+
+    /**
+     * 
+     * @param node
+     * @param data
+     * @return 
+     */
+    @Override
+    public Object visit(ASTMethodDeclaration node, Object data) {
+	methods.add(node);
+	return super.visit(node, data);
+    }
+
+    @Override
+    public Object visit(ASTTriggerUnit node, Object data) {
+	triggerUnits.add(node);
+	return super.visit(node, data);
+    }
+
+    @Override
+    public Object visit(ASTProgramUnit node, Object data) {
+	programUnits.add(node);
+	return super.visit(node, data);
+    }
+    /* @Override
+    public Object visit(ASTConstructorDeclaration node, Object data) {
+	constructors.add(node);
+	return super.visit(node, data);
+    }*/
+
+    @Override
+    public Object visit(ASTInput acu, Object data) {
+	methods = new ArrayList<ASTMethodDeclaration>();
+	programUnits = new ArrayList<ASTProgramUnit>();
+	triggerUnits = new ArrayList<ASTTriggerUnit>();
+	//constructors = new ArrayList<ASTConstructorDeclaration>();
+	return super.visit(acu, data);
+    }
+}
