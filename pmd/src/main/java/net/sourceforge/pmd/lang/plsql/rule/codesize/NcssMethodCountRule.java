0f6ace560b020899d4923bfa93ce7bd02962d990
Stuart Turton <Stuart.Turton@oracle-troubadour.co.uk>
2012-11-20 22:47:20 +0000

    PLSQL: Non-commented Source Statement and Too Many Methods (Package Specification and Object Type Specification

---
+++ pmd/src/main/java/net/sourceforge/pmd/lang/plsql/rule/codesize/NcssMethodCountRule.java
@@ -0,0 +1,31 @@
+package net.sourceforge.pmd.lang.plsql.rule.codesize;
+
+import net.sourceforge.pmd.lang.plsql.ast.ExecutableCode;
+import net.sourceforge.pmd.stat.DataPoint;
+
+/**
+ * Non-commented source statement counter for methods.
+ * 
+ * @author Jason Bennett
+ */
+public class NcssMethodCountRule extends AbstractNcssCountRule {
+
+    /**
+     * Count the size of all non-constructor methods.
+     */
+    public NcssMethodCountRule() {
+	super(ExecutableCode.class);
+	setProperty(MINIMUM_DESCRIPTOR, 100d);
+    }
+
+    @Override
+    public Object visit(ExecutableCode node, Object data) {
+	return super.visit(node, data);
+    }
+
+    @Override
+    public Object[] getViolationParameters(DataPoint point) {
+	return new String[] { ((ExecutableCode) point.getNode()).getMethodName(),
+		String.valueOf((int) point.getScore()) };
+    }
+}
