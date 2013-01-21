7ee96a6b522c62274dbfd13c781cc938e7d42568
Andreas Dangel <adangel@users.sourceforge.net>
2013-01-21 21:18:18 +0100

    pmd: fixed #1055 Please add a colon in the ant output after line,column for Oracle JDeveloper IDE usage

--- pmd/src/main/java/net/sourceforge/pmd/renderers/TextRenderer.java
+++ pmd/src/main/java/net/sourceforge/pmd/renderers/TextRenderer.java
@@ -45,7 +45,7 @@ public class TextRenderer extends AbstractIncrementingRenderer {
 		    RuleViolation rv = violations.next();
 		    buf.append(rv.getFilename());
 		    buf.append(':').append(Integer.toString(rv.getBeginLine()));
-		    buf.append('\t').append(rv.getDescription()).append(PMD.EOL);
+		    buf.append(":\t").append(rv.getDescription()).append(PMD.EOL);
 		    writer.write(buf.toString());
 		}
     }
