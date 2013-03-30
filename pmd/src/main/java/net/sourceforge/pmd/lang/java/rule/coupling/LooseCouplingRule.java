f0d8759b7974569bd554a43827462ea98d89e4ad
Andreas Dangel <adangel@users.sourceforge.net>
2013-03-30 15:04:47 +0100

    pmd: fix #938 False positive on LooseCoupling for overriding methods

--- pmd/src/main/java/net/sourceforge/pmd/lang/java/rule/coupling/LooseCouplingRule.java
+++ pmd/src/main/java/net/sourceforge/pmd/lang/java/rule/coupling/LooseCouplingRule.java
@@ -4,9 +4,13 @@
 package net.sourceforge.pmd.lang.java.rule.coupling;
 
 import net.sourceforge.pmd.lang.ast.Node;
+import net.sourceforge.pmd.lang.java.ast.ASTAnnotation;
+import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceBodyDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceType;
 import net.sourceforge.pmd.lang.java.ast.ASTFieldDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTFormalParameter;
+import net.sourceforge.pmd.lang.java.ast.ASTMarkerAnnotation;
+import net.sourceforge.pmd.lang.java.ast.ASTName;
 import net.sourceforge.pmd.lang.java.ast.ASTResultType;
 import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
 import net.sourceforge.pmd.util.CollectionUtil;
@@ -22,11 +26,28 @@ public class LooseCouplingRule extends AbstractJavaRule {
 //    	});
 
     public Object visit(ASTClassOrInterfaceType node, Object data) {
-        Node parent = node.jjtGetParent().jjtGetParent().jjtGetParent();
+        if (methodHasOverride(node)) {
+            return data;
+        }
+        Node parent = node.getNthParent(3);
         String typeName = node.getImage();
         if (CollectionUtil.isCollectionType(typeName, false) && (parent instanceof ASTFieldDeclaration || parent instanceof ASTFormalParameter || parent instanceof ASTResultType)) {
             addViolation(data, node, typeName);
         }
         return data;
+    }
+
+    private boolean methodHasOverride(Node node) {
+        ASTClassOrInterfaceBodyDeclaration method = node.getFirstParentOfType(ASTClassOrInterfaceBodyDeclaration.class);
+        if (method.jjtGetNumChildren() > 0 && method.jjtGetChild(0) instanceof ASTAnnotation) {
+            ASTMarkerAnnotation marker = method.getFirstDescendantOfType(ASTMarkerAnnotation.class);
+            if (marker != null && marker.getFirstChildOfType(ASTName.class) != null) {
+                ASTName name = marker.getFirstChildOfType(ASTName.class);
+                if ("Override".equals(name.getImage())) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
---
+++ pmd/src/test/java/net/sourceforge/pmd/typeresolution/testdata/DefaultJavaLangImport.java
@@ -0,0 +1,8 @@
+package net.sourceforge.pmd.typeresolution.testdata;
+
+public class DefaultJavaLangImport {
+    @Override
+    public String toString() {
+        return "foo";
+    }
+}
