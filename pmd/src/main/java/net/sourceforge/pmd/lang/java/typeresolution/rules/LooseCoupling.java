9f8246caacb37fb44ec453ad0d38c0938a1a6754
Andreas Dangel <adangel@users.sourceforge.net>
2013-03-30 15:04:47 +0100

    pmd: fix #938 False positive on LooseCoupling for overriding methods

--- pmd/src/main/java/net/sourceforge/pmd/lang/java/typeresolution/rules/LooseCoupling.java
+++ pmd/src/main/java/net/sourceforge/pmd/lang/java/typeresolution/rules/LooseCoupling.java
@@ -4,9 +4,13 @@
 package net.sourceforge.pmd.lang.java.typeresolution.rules;
 
 import net.sourceforge.pmd.lang.ast.Node;
+import net.sourceforge.pmd.lang.java.ast.ASTAnnotation;
+import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceBodyDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceType;
 import net.sourceforge.pmd.lang.java.ast.ASTFieldDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTFormalParameter;
+import net.sourceforge.pmd.lang.java.ast.ASTMarkerAnnotation;
+import net.sourceforge.pmd.lang.java.ast.ASTName;
 import net.sourceforge.pmd.lang.java.ast.ASTResultType;
 import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
 import net.sourceforge.pmd.util.CollectionUtil;
@@ -18,6 +22,9 @@ public class LooseCoupling extends AbstractJavaRule {
 
     @Override
     public Object visit(ASTClassOrInterfaceType node, Object data) {
+    if (methodHasOverride(node)) {
+        return data;
+    }
 	Node parent = node.getNthParent(3);
 	Class<?> clazzType = node.getType();
 	boolean isType = CollectionUtil.isCollectionType(clazzType, false);
@@ -26,5 +33,20 @@ public class LooseCoupling extends AbstractJavaRule {
 	    addViolation(data, node, node.getImage());
 	}
 	return data;
+    }
+
+    private boolean methodHasOverride(Node node) {
+        ASTClassOrInterfaceBodyDeclaration method = node.getFirstParentOfType(ASTClassOrInterfaceBodyDeclaration.class);
+        if (method.jjtGetNumChildren() > 0 && method.jjtGetChild(0) instanceof ASTAnnotation) {
+            ASTMarkerAnnotation marker = method.getFirstDescendantOfType(ASTMarkerAnnotation.class);
+            if (marker != null && marker.getFirstChildOfType(ASTName.class) != null) {
+                ASTName name = marker.getFirstChildOfType(ASTName.class);
+                System.out.println(name.getType());
+                if (name.getType() == Override.class) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
