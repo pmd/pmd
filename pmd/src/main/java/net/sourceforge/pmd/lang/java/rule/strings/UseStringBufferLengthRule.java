46620058e24692e55641715dd5e32e5b067dc8c1
Philip Graf <git@acanda.ch>
2013-09-19 23:33:17 +0200

    pmd: fix #1134 UseStringBufferLength false positives

--- pmd/src/main/java/net/sourceforge/pmd/lang/java/rule/strings/UseStringBufferLengthRule.java
+++ pmd/src/main/java/net/sourceforge/pmd/lang/java/rule/strings/UseStringBufferLengthRule.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Set;
 
 import net.sourceforge.pmd.lang.ast.Node;
+import net.sourceforge.pmd.lang.java.ast.ASTArgumentList;
 import net.sourceforge.pmd.lang.java.ast.ASTLiteral;
 import net.sourceforge.pmd.lang.java.ast.ASTMethodDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTName;
@@ -29,6 +30,7 @@ import net.sourceforge.pmd.lang.java.typeresolution.TypeHelper;
  * </pre>
  *
  * @author acaplan
+ * @author Philip Graf
  */
 public class UseStringBufferLengthRule extends AbstractJavaRule {
 
@@ -66,27 +68,62 @@ public class UseStringBufferLengthRule extends AbstractJavaRule {
         }
         alreadySeen.add(vnd);
 
-        Node parent = decl.jjtGetParent().jjtGetParent();
-        for (int jx = 0; jx < parent.jjtGetNumChildren(); jx++) {
-            Node achild = parent.jjtGetChild(jx);
-            if (isViolation(parent, achild)) {
-                addViolation(data, decl);
-            }
+        if (isViolation(decl)) {
+            addViolation(data, decl);
         }
 
         return data;
     }
 
     /**
-     * Check the given node if it calls either .equals or .length we need to check the target
+     * Returns true for the following violations:
+     * 
+     * <pre>
+     * StringBuffer sb = new StringBuffer(&quot;some string&quot;);
+     * if (sb.toString().equals(&quot;&quot;)) {
+     *     // this is a violation
+     * }
+     * if (sb.toString().length() == 0) {
+     *     // this is a violation
+     * }
+     * if (sb.length() == 0) {
+     *     // this is ok
+     * }
+     * </pre>
      */
-    private boolean isViolation(Node parent, Node achild) {
-        if ("equals".equals(achild.getImage())) {
-            List<ASTLiteral> literals = parent.findDescendantsOfType(ASTLiteral.class);
-            return !literals.isEmpty() && "\"\"".equals(literals.get(0).getImage());
-        } else if ("length".equals(achild.getImage())) {
-            return true;
+    private boolean isViolation(ASTName decl) {
+        // the (grand)parent of a violation has four children
+        Node parent = decl.jjtGetParent().jjtGetParent();
+        if (parent.jjtGetNumChildren() == 4) {
+            // 1. child: sb.toString where sb is a VariableNameDeclaration for a StringBuffer or StringBuilder
+            if (parent.jjtGetChild(0).getFirstChildOfType(ASTName.class).getImage().endsWith(".toString")) {
+                // 2. child: the arguments of toString
+                // no need to check as both StringBuffer and StringBuilder only have one toString method
+                // 3. child: equals or length, 4. child: their arguments
+                return isEqualsViolation(parent) || isLengthViolation(parent);
+            }
         }
         return false;
     }
+    
+    private boolean isEqualsViolation(Node parent) {
+        // 3. child: equals
+        if (parent.jjtGetChild(2).hasImageEqualTo("equals")) {
+            // 4. child: the arguments of equals, there must be exactly one and it must be ""
+            List<ASTArgumentList> argList = parent.jjtGetChild(3).findDescendantsOfType(ASTArgumentList.class);
+            if (argList.size() == 1) {
+                List<ASTLiteral> literals = argList.get(0).findDescendantsOfType(ASTLiteral.class);
+                return literals.size() == 1 && literals.get(0).hasImageEqualTo("\"\"");
+            }
+        }
+        return false;
+    }
+    
+    private boolean isLengthViolation(Node parent) {
+        // 3. child: length
+        return parent.jjtGetChild(2).hasImageEqualTo("length");
+        // 4. child: the arguments of length
+        // no need to check as String has only one length method
+    }
+    
 }
