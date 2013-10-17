858308aa8f13c11512cc298c7bf7234029c5ebb3
Andreas Dangel <adangel@users.sourceforge.net>
2013-10-17 20:37:13 +0200

    pmd: fix #1140 public EcmascriptNode getBody(int index)

--- pmd/src/test/java/net/sourceforge/pmd/lang/ecmascript/ast/EcmascriptParserTestBase.java
+++ pmd/src/test/java/net/sourceforge/pmd/lang/ecmascript/ast/EcmascriptParserTestBase.java
@@ -5,15 +5,30 @@ package net.sourceforge.pmd.lang.ecmascript.ast;
 
 import java.io.Reader;
 import java.io.StringReader;
+import java.io.StringWriter;
 
 import net.sourceforge.pmd.lang.ecmascript.EcmascriptParserOptions;
 
-import org.mozilla.javascript.ast.AstRoot;
-
 public abstract class EcmascriptParserTestBase {
-    public EcmascriptNode<AstRoot> parse(String code) {
+    public ASTAstRoot parse(String code) {
         EcmascriptParser parser = new EcmascriptParser(new EcmascriptParserOptions());
         Reader sourceCode = new StringReader(code);
-        return parser.parse(sourceCode);
+        return (ASTAstRoot)parser.parse(sourceCode);
+    }
+
+    public ASTAstRoot parse18(String code) {
+        EcmascriptParserOptions parserOptions = new EcmascriptParserOptions();
+        parserOptions.setRhinoLanguageVersion(EcmascriptParserOptions.Version.VERSION_1_8);
+        EcmascriptParser parser = new EcmascriptParser(parserOptions);
+        Reader sourceCode = new StringReader(code);
+        return (ASTAstRoot)parser.parse(sourceCode);
+    }
+
+    public String dump(EcmascriptNode<?> node) {
+        DumpFacade dumpFacade = new DumpFacade();
+        StringWriter writer = new StringWriter();
+        dumpFacade.initializeWith(writer, "", true, node);
+        dumpFacade.visit(node, "");
+        return writer.toString();
     }
 }
