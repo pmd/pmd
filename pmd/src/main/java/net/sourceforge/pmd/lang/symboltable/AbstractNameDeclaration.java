1354f6f1fad892bdb9b131f3057fa322ca123b3a
Andreas Dangel <adangel@users.sourceforge.net>
2013-12-21 18:08:58 +0100

    pmd: Add basic/abstract implementations for Scope and NameDeclaration

---
+++ pmd/src/main/java/net/sourceforge/pmd/lang/symboltable/AbstractNameDeclaration.java
@@ -0,0 +1,38 @@
+/**
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+package net.sourceforge.pmd.lang.symboltable;
+
+import net.sourceforge.pmd.lang.ScopedNode;
+
+/**
+ * Base class for all name declarations.
+ */
+public abstract class AbstractNameDeclaration implements NameDeclaration {
+
+    protected ScopedNode node;
+
+    public AbstractNameDeclaration(ScopedNode node) {
+        this.node = node;
+    }
+
+    @Override
+    public ScopedNode getNode() {
+        return node;
+    }
+
+    @Override
+    public String getImage() {
+        return node.getImage();
+    }
+
+    @Override
+    public Scope getScope() {
+        return node.getScope();
+    }
+
+    @Override
+    public String getName() {
+        return getImage();
+    }
+}
---
+++ pmd/src/main/java/net/sourceforge/pmd/lang/symboltable/AbstractScope.java
@@ -0,0 +1,89 @@
+/**
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+package net.sourceforge.pmd.lang.symboltable;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Base class for any {@link Scope}.
+ * Provides useful default implementations.
+ */
+public abstract class AbstractScope implements Scope {
+
+    private Scope parent;
+    private Map<NameDeclaration, List<NameOccurrence>> nameDeclarations = new HashMap<NameDeclaration, List<NameOccurrence>>();
+
+    @Override
+    public Scope getParent() {
+        return parent;
+    }
+
+    @Override
+    public void setParent(Scope parent) {
+        this.parent = parent;
+    }
+
+    @Override
+    public Map<NameDeclaration, List<NameOccurrence>> getDeclarations() {
+        return nameDeclarations;
+    }
+
+    @Override
+    public <T extends NameDeclaration> Map<T, List<NameOccurrence>> getDeclarations(Class<T> clazz) {
+        Map<T, List<NameOccurrence>> result = new HashMap<T, List<NameOccurrence>>();
+        for (Map.Entry<NameDeclaration, List<NameOccurrence>> e : nameDeclarations.entrySet()) {
+            if (clazz.isAssignableFrom(e.getKey().getClass())) {
+                @SuppressWarnings("unchecked") // it's assignable from, so should be ok
+                T cast = (T)e.getKey();
+                result.put(cast, e.getValue());
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public boolean contains(NameOccurrence occ) {
+        for (NameDeclaration d : nameDeclarations.keySet()) {
+            if (d.getImage().equals(occ.getImage())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void addDeclaration(NameDeclaration declaration) {
+        nameDeclarations.put(declaration, new ArrayList<NameOccurrence>());
+    }
+
+    @Override
+    public <T extends Scope> T getEnclosingScope(Class<T> clazz) {
+        T result = null;
+        Scope current = this;
+        while (result == null && current != null) {
+            if (clazz.isAssignableFrom(current.getClass())) {
+                @SuppressWarnings("unchecked")
+                T cast = (T)current;
+                result = cast;
+            }
+            current = current.getParent();
+        }
+        return result;
+    }
+
+    @Override
+    public NameDeclaration addNameOccurrence(NameOccurrence occurrence) {
+        NameDeclaration result = null;
+        for (Map.Entry<NameDeclaration, List<NameOccurrence>> e : nameDeclarations.entrySet()) {
+            if (e.getKey().getImage().equals(occurrence.getImage())) {
+                result = e.getKey();
+                e.getValue().add(occurrence);
+            }
+        }
+        return result;
+    }
+}
