8dd3755db13148eda773463b16b7151d08fe1372
Jan van Nunen <jan.van.nunen@tiobe.com>
2015-01-08 16:04:15 +0100

    Added support for Objective-C to CPD.

---
+++ pmd-objectivec/src/main/java/net/sourceforge/pmd/cpd/ObjectiveCLanguage.java
@@ -0,0 +1,18 @@
+/**
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+package net.sourceforge.pmd.cpd;
+
+
+/**
+ * Defines the Language module for Objective-C
+ */
+public class ObjectiveCLanguage extends AbstractLanguage {
+
+    /**
+     * Creates a new instance of {@link ObjectiveCLanguage} with the default extensions for Objective-C files.
+     */
+    public ObjectiveCLanguage() {
+        super("Objective-C", "objectivec", new ObjectiveCTokenizer(), ".h", ".m");
+    }
+}
---
+++ pmd-objectivec/src/main/java/net/sourceforge/pmd/lang/objectivec/ObjectiveCHandler.java
@@ -0,0 +1,25 @@
+/**
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+package net.sourceforge.pmd.lang.objectivec;
+
+import net.sourceforge.pmd.lang.AbstractLanguageVersionHandler;
+import net.sourceforge.pmd.lang.Parser;
+import net.sourceforge.pmd.lang.ParserOptions;
+import net.sourceforge.pmd.lang.rule.RuleViolationFactory;
+
+/**
+ * Implementation of LanguageVersionHandler for the Objective-C Language.
+ */
+public class ObjectiveCHandler extends AbstractLanguageVersionHandler {
+
+    @Override
+    public RuleViolationFactory getRuleViolationFactory() {
+        throw new UnsupportedOperationException("getRuleViolationFactory() is not supported for Objective-C");
+    }
+
+    @Override
+    public Parser getParser(ParserOptions parserOptions) {
+        return new ObjectiveCParser(parserOptions);
+    }
+}
