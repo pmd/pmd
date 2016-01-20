8c4d307ddd1f271d32572672fb22493da97e7e4f
Andreas Dangel <adangel@users.sourceforge.net>
2016-01-20 22:13:25 +0100

    Refactor command line tests for CPD and create a BaseCPDCLITest class

--- pmd-javascript/src/test/java/net/sourceforge/pmd/cpd/CPDCommandLineInterfaceTest.java
+++ pmd-javascript/src/test/java/net/sourceforge/pmd/cpd/CPDCommandLineInterfaceTest.java
@@ -3,54 +3,26 @@
  */
 package net.sourceforge.pmd.cpd;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import java.io.UnsupportedEncodingException;
+import net.sourceforge.pmd.cli.BaseCPDCLITest;
 
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
-public class CPDCommandLineInterfaceTest {
-    private ByteArrayOutputStream bufferStdout;
-    private PrintStream originalStdout;
-    private PrintStream originalStderr;
-
-    @Before
-    public void setup() throws UnsupportedEncodingException {
-        originalStdout = System.out;
-        originalStderr = System.err;
-        bufferStdout = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(bufferStdout, false, "UTF-8"));
-        System.setErr(System.out);
-    }
-
-    @After
-    public void teardown() {
-        System.setOut(originalStdout);
-        System.setErr(originalStderr);
-    }
-
+public class CPDCommandLineInterfaceTest extends BaseCPDCLITest {
     @Test
-    public void shouldFindDuplicatesWithDifferentFileExtensions() throws Exception {
+    public void shouldFindDuplicatesWithDifferentFileExtensions() {
         runCPD("--minimum-tokens", "5", "--language", "js", "--files", "src/test/resources/net/sourceforge/pmd/cpd/ts/File1.ts",
                 "src/test/resources/net/sourceforge/pmd/cpd/ts/File2.ts");
 
-        String out = bufferStdout.toString("UTF-8");
+        String out = getOutput();
         Assert.assertTrue(out.contains("Found a 9 line (30 tokens) duplication in the following files"));
     }
 
     @Test
-    public void shouldFindNoDuplicatesWithDifferentFileExtensions() throws Exception {
+    public void shouldFindNoDuplicatesWithDifferentFileExtensions() {
         runCPD("--minimum-tokens", "5", "--language", "js", "--files", "src/test/resources/net/sourceforge/pmd/cpd/ts/");
 
-        String out = bufferStdout.toString("UTF-8");
+        String out = getOutput();
         Assert.assertTrue(out.isEmpty());
-    }
-    
-    private void runCPD(String ... args) {
-        System.setProperty(CPDCommandLineInterface.NO_EXIT_AFTER_RUN, "true");
-        CPD.main(args);
     }
 }
---
+++ pmd-test/src/main/java/net/sourceforge/pmd/cli/BaseCPDCLITest.java
@@ -0,0 +1,48 @@
+/**
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+package net.sourceforge.pmd.cli;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+
+import net.sourceforge.pmd.cpd.CPD;
+import net.sourceforge.pmd.cpd.CPDCommandLineInterface;
+
+import org.junit.After;
+import org.junit.Before;
+
+public abstract class BaseCPDCLITest {
+    private ByteArrayOutputStream bufferStdout;
+    private PrintStream originalStdout;
+    private PrintStream originalStderr;
+
+    @Before
+    public void setup() throws UnsupportedEncodingException {
+        originalStdout = System.out;
+        originalStderr = System.err;
+        bufferStdout = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(bufferStdout, false, "UTF-8"));
+        System.setErr(System.out);
+    }
+
+    @After
+    public void teardown() {
+        System.setOut(originalStdout);
+        System.setErr(originalStderr);
+    }
+
+    public final String getOutput() {
+        try {
+            return bufferStdout.toString("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected void runCPD(String ... args) {
+        System.setProperty(CPDCommandLineInterface.NO_EXIT_AFTER_RUN, "true");
+        CPD.main(args);
+    }
+}
