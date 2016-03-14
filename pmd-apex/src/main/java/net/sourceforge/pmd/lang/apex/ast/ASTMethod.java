42273034c7f81aa3092c04caa8953364a3d1663c
Robert Sösemann <robert.soesemann@up2go.com>
2016-03-14 16:37:12 +0100

    First simple ast node adapter

---
+++ pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/ast/ASTMethod.java
@@ -0,0 +1,27 @@
+/**
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+package net.sourceforge.pmd.lang.apex.ast;
+
+import apex.jorje.semantic.ast.member.Method;
+
+public class ASTMethod extends AbstractApexNode<Method> {
+
+	public ASTMethod(Method method) {
+		super(method);
+	}
+
+	/**
+	 * Accept the visitor.
+	 */
+	@Override
+	public Object jjtAccept(ApexParserVisitor visitor, Object data) {
+		return visitor.visit(this, data);
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
---
+++ pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/ast/ASTUserClass.java
@@ -0,0 +1,26 @@
+/**
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+package net.sourceforge.pmd.lang.apex.ast;
+
+import apex.jorje.semantic.ast.compilation.UserClass;
+
+public class ASTUserClass extends AbstractApexNode<UserClass> {
+	public ASTUserClass(UserClass userClass) {
+		super(userClass);
+	}
+
+	/**
+	 * Accept the visitor.
+	 */
+	@Override
+	public Object jjtAccept(ApexParserVisitor visitor, Object data) {
+		return visitor.visit(this, data);
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
