294ae0de921c8dc67e3d9d5780e8665d2777f9d0
Andreas Dangel <adangel@users.sourceforge.net>
2016-03-20 11:16:11 +0100

    Mark UserClass as root node, so that XPath rules will work

--- pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/ast/ASTUserClass.java
+++ pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/ast/ASTUserClass.java
@@ -4,8 +4,9 @@
 package net.sourceforge.pmd.lang.apex.ast;
 
 import apex.jorje.semantic.ast.compilation.UserClass;
+import net.sourceforge.pmd.lang.ast.RootNode;
 
-public class ASTUserClass extends AbstractApexNode<UserClass> {
+public class ASTUserClass extends AbstractApexNode<UserClass> implements RootNode {
     public ASTUserClass(UserClass userClass) {
         super(userClass);
     }
