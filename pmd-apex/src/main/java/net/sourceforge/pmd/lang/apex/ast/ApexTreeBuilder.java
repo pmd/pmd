d739f11ff453603c3b6123af2e51872ca7b6fc2d
Andreas Dangel <adangel@users.sourceforge.net>
2016-03-20 11:14:38 +0100

    Implement the apex visitor methods

--- pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/ast/ApexTreeBuilder.java
+++ pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/ast/ApexTreeBuilder.java
@@ -9,13 +9,87 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
 
-import net.sourceforge.pmd.lang.ast.Node;
 import apex.jorje.semantic.ast.AstNode;
+import apex.jorje.semantic.ast.compilation.AnonymousClass;
 import apex.jorje.semantic.ast.compilation.UserClass;
+import apex.jorje.semantic.ast.compilation.UserClassMethods;
+import apex.jorje.semantic.ast.compilation.UserEnum;
+import apex.jorje.semantic.ast.compilation.UserExceptionMethods;
+import apex.jorje.semantic.ast.compilation.UserInterface;
+import apex.jorje.semantic.ast.compilation.UserTrigger;
+import apex.jorje.semantic.ast.condition.StandardCondition;
+import apex.jorje.semantic.ast.expression.ArrayLoadExpression;
+import apex.jorje.semantic.ast.expression.ArrayStoreExpression;
+import apex.jorje.semantic.ast.expression.AssignmentExpression;
+import apex.jorje.semantic.ast.expression.BinaryExpression;
+import apex.jorje.semantic.ast.expression.BindExpressions;
+import apex.jorje.semantic.ast.expression.BooleanExpression;
+import apex.jorje.semantic.ast.expression.ClassRefExpression;
+import apex.jorje.semantic.ast.expression.DottedExpression;
+import apex.jorje.semantic.ast.expression.InstanceOfExpression;
+import apex.jorje.semantic.ast.expression.JavaMethodCallExpression;
+import apex.jorje.semantic.ast.expression.JavaVariableExpression;
+import apex.jorje.semantic.ast.expression.LiteralExpression;
+import apex.jorje.semantic.ast.expression.MapEntryNode;
+import apex.jorje.semantic.ast.expression.MethodCallExpression;
+import apex.jorje.semantic.ast.expression.NewListInitExpression;
+import apex.jorje.semantic.ast.expression.NewListLiteralExpression;
+import apex.jorje.semantic.ast.expression.NewMapInitExpression;
+import apex.jorje.semantic.ast.expression.NewMapLiteralExpression;
+import apex.jorje.semantic.ast.expression.NewNameValueObjectExpression;
+import apex.jorje.semantic.ast.expression.NewObjectExpression;
+import apex.jorje.semantic.ast.expression.NewSetInitExpression;
+import apex.jorje.semantic.ast.expression.NewSetLiteralExpression;
+import apex.jorje.semantic.ast.expression.PackageVersionExpression;
+import apex.jorje.semantic.ast.expression.PostfixExpression;
+import apex.jorje.semantic.ast.expression.PrefixExpression;
+import apex.jorje.semantic.ast.expression.ReferenceExpression;
+import apex.jorje.semantic.ast.expression.SoqlExpression;
+import apex.jorje.semantic.ast.expression.SoslExpression;
+import apex.jorje.semantic.ast.expression.SuperMethodCallExpression;
+import apex.jorje.semantic.ast.expression.SuperVariableExpression;
+import apex.jorje.semantic.ast.expression.TernaryExpression;
+import apex.jorje.semantic.ast.expression.ThisMethodCallExpression;
+import apex.jorje.semantic.ast.expression.ThisVariableExpression;
+import apex.jorje.semantic.ast.expression.TriggerVariableExpression;
+import apex.jorje.semantic.ast.expression.VariableExpression;
+import apex.jorje.semantic.ast.member.Field;
 import apex.jorje.semantic.ast.member.Method;
+import apex.jorje.semantic.ast.member.Parameter;
+import apex.jorje.semantic.ast.member.Property;
+import apex.jorje.semantic.ast.member.bridge.BridgeMethodCreator;
+import apex.jorje.semantic.ast.modifier.Annotation;
+import apex.jorje.semantic.ast.modifier.AnnotationParameter;
+import apex.jorje.semantic.ast.modifier.ModifierNode;
+import apex.jorje.semantic.ast.statement.BlockStatement;
+import apex.jorje.semantic.ast.statement.BreakStatement;
+import apex.jorje.semantic.ast.statement.CatchBlockStatement;
+import apex.jorje.semantic.ast.statement.ContinueStatement;
+import apex.jorje.semantic.ast.statement.DmlDeleteStatement;
+import apex.jorje.semantic.ast.statement.DmlInsertStatement;
+import apex.jorje.semantic.ast.statement.DmlMergeStatement;
+import apex.jorje.semantic.ast.statement.DmlUndeleteStatement;
+import apex.jorje.semantic.ast.statement.DmlUpdateStatement;
+import apex.jorje.semantic.ast.statement.DmlUpsertStatement;
+import apex.jorje.semantic.ast.statement.DoLoopStatement;
+import apex.jorje.semantic.ast.statement.ExpressionStatement;
+import apex.jorje.semantic.ast.statement.FieldDeclaration;
+import apex.jorje.semantic.ast.statement.FieldDeclarationStatements;
+import apex.jorje.semantic.ast.statement.ForEachStatement;
+import apex.jorje.semantic.ast.statement.ForLoopStatement;
+import apex.jorje.semantic.ast.statement.IfBlockStatement;
+import apex.jorje.semantic.ast.statement.IfElseBlockStatement;
+import apex.jorje.semantic.ast.statement.ReturnStatement;
+import apex.jorje.semantic.ast.statement.RunAsBlockStatement;
+import apex.jorje.semantic.ast.statement.ThrowStatement;
+import apex.jorje.semantic.ast.statement.TryCatchFinallyBlockStatement;
+import apex.jorje.semantic.ast.statement.VariableDeclaration;
+import apex.jorje.semantic.ast.statement.VariableDeclarationStatements;
+import apex.jorje.semantic.ast.statement.WhileLoopStatement;
 import apex.jorje.semantic.ast.visitor.AdditionalPassScope;
 import apex.jorje.semantic.ast.visitor.AstVisitor;
 import apex.jorje.semantic.exception.Errors;
+import net.sourceforge.pmd.lang.ast.Node;
 
 public final class ApexTreeBuilder extends AstVisitor<AdditionalPassScope> {
 
@@ -94,13 +168,400 @@ public final class ApexTreeBuilder extends AstVisitor<AdditionalPassScope> {
 		return node;
 	}
 
-	public boolean visit(AstNode node) {
-		if (parents.peek() == node) {
-			return true;
-		}
-		else {
-			build(node);
-			return false;
-		}
-	}
+    private boolean visit(AstNode node) {
+        if (parents.peek() == node) {
+            return true;
+        } else {
+            build(node);
+            return false;
+        }
+    }
+
+    @Override
+    public boolean visit(AnonymousClass node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(UserEnum node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(UserInterface node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(UserTrigger node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(ArrayLoadExpression node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(ArrayStoreExpression node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(AssignmentExpression node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(BinaryExpression node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(BooleanExpression node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(ClassRefExpression node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(InstanceOfExpression node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(JavaMethodCallExpression node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(JavaVariableExpression node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(LiteralExpression node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(ReferenceExpression node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(MethodCallExpression node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(NewListInitExpression node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(NewMapInitExpression node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(NewSetInitExpression node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(NewListLiteralExpression node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(NewObjectExpression node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(NewSetLiteralExpression node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(NewNameValueObjectExpression node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(PackageVersionExpression node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(PostfixExpression node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(PrefixExpression node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(TernaryExpression node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(StandardCondition node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(TriggerVariableExpression node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(DottedExpression node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(VariableExpression node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(BlockStatement node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(BreakStatement node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(ContinueStatement node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(DmlDeleteStatement node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(DmlInsertStatement node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(DmlMergeStatement node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(DmlUndeleteStatement node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(DmlUpdateStatement node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(DmlUpsertStatement node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(DoLoopStatement node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(ExpressionStatement node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(ForEachStatement node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(ForLoopStatement node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(FieldDeclaration node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(FieldDeclarationStatements node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(IfBlockStatement node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(IfElseBlockStatement node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(ReturnStatement node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(RunAsBlockStatement node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(ThrowStatement node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(VariableDeclaration node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(VariableDeclarationStatements node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(WhileLoopStatement node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(BindExpressions node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(SoqlExpression node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(SoslExpression node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(NewMapLiteralExpression node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(MapEntryNode node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(CatchBlockStatement node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(TryCatchFinallyBlockStatement node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(Property node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(Field node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(Parameter node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(BridgeMethodCreator node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(UserClassMethods node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(UserExceptionMethods node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(Annotation node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(AnnotationParameter node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    /*
+     * ModifierGroup is not an AstNode...
+    @Override
+    public boolean visit(ModifierGroup node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+    */
+
+    @Override
+    public boolean visit(ModifierNode node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(SuperMethodCallExpression node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(ThisMethodCallExpression node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(SuperVariableExpression node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(ThisVariableExpression node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(UserClass node, AdditionalPassScope scope) {
+        return visit(node);
+    }
+
+    @Override
+    public boolean visit(Method node, AdditionalPassScope scope) {
+        return visit(node);
+    }
 }
