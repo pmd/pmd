c310c51ea0d5151575929411e9659b958400355e
Robert Sösemann <robert.soesemann@up2go.com>
2016-03-22 15:53:42 +0100

    Forgot to push

---
+++ pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/ast/ASTReferenceExpression.java
@@ -0,0 +1,10 @@
+package net.sourceforge.pmd.lang.apex.ast;
+
+import apex.jorje.semantic.ast.expression.ReferenceExpression;
+
+public class ASTReferenceExpression extends AbstractApexNode<ReferenceExpression> {
+
+	public ASTReferenceExpression(ReferenceExpression referenceExpression) {
+		super(referenceExpression);
+	}
+}
