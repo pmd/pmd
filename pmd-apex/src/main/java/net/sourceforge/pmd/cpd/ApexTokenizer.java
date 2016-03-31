e3da2619ddccc599eaaf085fad114a5a36c08644
Robert Sösemann <robert.soesemann@up2go.com>
2016-03-31 14:50:31 +0200

    CDP is also running for Apex (CLI and GUI)

--- pmd-apex/src/main/java/net/sourceforge/pmd/cpd/ApexTokenizer.java
+++ pmd-apex/src/main/java/net/sourceforge/pmd/cpd/ApexTokenizer.java
@@ -10,18 +10,15 @@ public class ApexTokenizer extends AbstractTokenizer {
         // setting markers for "string" in apex
         this.stringToken = new ArrayList<>();
         this.stringToken.add("\'");
-        this.stringToken.add("\"");
 
         // setting markers for 'ignorable character' in apex
         this.ignorableCharacter = new ArrayList<>();
-        this.ignorableCharacter.add(";");
 
         // setting markers for 'ignorable string' in apex
         this.ignorableStmt = new ArrayList<>();
+        this.ignorableCharacter.add( ";" );
 
         // strings do indeed span multiple lines in apex
         this.spanMultipleLinesString = false;
-        // the lines do to end with backslashes
-        this.spanMultipleLinesLineContinuationCharacter = '\\';
     }
 }
