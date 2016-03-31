b77fef63385408acfe5f9a14f20661b4155cfff0
Robert Sösemann <robert.soesemann@up2go.com>
2016-03-31 11:17:36 +0200

    Ported very useful AvoidDeeplyNestedIfStmtsRule

---
+++ pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/design/AvoidDeeplyNestedIfStmtsRule.java
@@ -0,0 +1,55 @@
+/**
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+package net.sourceforge.pmd.lang.apex.rule.design;
+
+import net.sourceforge.pmd.lang.apex.ast.ASTUserClass;
+import net.sourceforge.pmd.lang.apex.ast.ASTIfBlockStatement;
+import net.sourceforge.pmd.lang.apex.ast.ASTIfElseBlockStatement;
+import net.sourceforge.pmd.lang.apex.rule.AbstractApexRule;
+import net.sourceforge.pmd.lang.rule.properties.IntegerProperty;
+
+public class AvoidDeeplyNestedIfStmtsRule extends AbstractApexRule {
+
+    private int depth;
+    private int depthLimit;
+    
+    private static final IntegerProperty PROBLEM_DEPTH_DESCRIPTOR = new IntegerProperty(
+    		"problemDepth", 
+    		"The if statement depth reporting threshold",
+    		1, 25,
+    		3,
+    		1.0f
+    		);
+    
+    public AvoidDeeplyNestedIfStmtsRule() {
+    	definePropertyDescriptor(PROBLEM_DEPTH_DESCRIPTOR);
+    }
+  
+    public Object visit(ASTUserClass node, Object data) {
+        depth = 0;
+        depthLimit = getProperty(PROBLEM_DEPTH_DESCRIPTOR);
+        return super.visit(node, data);
+    }
+
+    public Object visit(ASTIfBlockStatement node, Object data) {
+        super.visit(node, data);
+        
+        if (depth == depthLimit) {
+            addViolation(data, node);
+        }
+        depth--;
+        return data;
+    }
+
+    public Object visit(ASTIfElseBlockStatement node, Object data) {
+        depth++;
+        
+        super.visit(node, data);
+        if (depth == depthLimit) {
+            addViolation(data, node);
+        }
+        depth--;
+        return data;
+    }
+}
