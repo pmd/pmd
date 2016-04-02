9a5b2a7dbd1c447e29411b2326e834ef0f978b9f
Andreas Dangel <adangel@users.sourceforge.net>
2016-04-02 12:06:37 +0200

    Add Modifiers information (public, private, ...)

--- pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/ast/AccessNode.java
+++ pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/ast/AccessNode.java
@@ -11,42 +11,26 @@ import net.sourceforge.pmd.lang.ast.Node;
 public interface AccessNode extends Node {
 
     int PUBLIC = 0x0001;
-    int PROTECTED = 0x0002;
-    int PRIVATE = 0x0004;
-    int ABSTRACT = 0x0008;
-    int STATIC = 0x0010;
-    int FINAL = 0x0020;
-    int TRANSIENT = 0x0100;
+    int PRIVATE = 0x0002;
+    int PROTECTED = 0x0004;
+    int STATIC = 0x0008;
+    int FINAL = 0x0010;
+    int TRANSIENT = 0x0080;
+    int ABSTRACT = 0x0400;
 
     int getModifiers();
 
-    void setModifiers(int modifiers);
-
     boolean isPublic();
-
-    void setPublic(boolean isPublic);
 
     boolean isProtected();
 
-    void setProtected(boolean isProtected);
-
     boolean isPrivate();
-
-    void setPrivate(boolean isPrivate);
 
     boolean isAbstract();
 
-    void setAbstract(boolean isAbstract);
-
     boolean isStatic();
-
-    void setStatic(boolean isStatic);
 
     boolean isFinal();
 
-    void setFinal(boolean isFinal);
-
-    void setDefault(boolean isDefault);
-
-    boolean isDefault();
+    boolean isTransient();
 }
