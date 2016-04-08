18bc0dfe9493db17f74d5cf42423ba5156f52f08
Robert Sösemann <robert.soesemann@up2go.com>
2016-04-08 17:13:50 +0200

    Ignore test classes

--- pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/codesize/ExcessiveClassLengthRule.java
+++ pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/codesize/ExcessiveClassLengthRule.java
@@ -5,6 +5,7 @@ package net.sourceforge.pmd.lang.apex.rule.codesize;
 
 import net.sourceforge.pmd.lang.apex.ast.ASTUserClass;
 import net.sourceforge.pmd.lang.apex.rule.design.ExcessiveLengthRule;
+import static apex.jorje.semantic.symbol.type.AnnotationTypeInfos.IS_TEST;
 
 /**
  * This rule detects when a class exceeds a certain threshold. i.e. if a class
@@ -14,5 +15,14 @@ public class ExcessiveClassLengthRule extends ExcessiveLengthRule {
 	public ExcessiveClassLengthRule() {
 		super(ASTUserClass.class);
 		setProperty(MINIMUM_DESCRIPTOR, 1000d);
+	}
+
+	@Override
+	public Object visit(ASTUserClass node, Object data) {
+		if(node.getNode().getModifiers().getModifiers().not(IS_TEST)) {
+			return super.visit(node, data);
+		}
+		
+		return data;
 	}
 }
