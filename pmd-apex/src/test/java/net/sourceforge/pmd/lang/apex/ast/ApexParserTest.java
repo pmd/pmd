380fdcab5470499d87f207e4af67c7530c783d54
Andreas Dangel <adangel@users.sourceforge.net>
2016-04-09 18:54:46 +0200

    Add missing license headers

--- pmd-apex/src/test/java/net/sourceforge/pmd/lang/apex/ast/ApexParserTest.java
+++ pmd-apex/src/test/java/net/sourceforge/pmd/lang/apex/ast/ApexParserTest.java
@@ -1,8 +1,7 @@
-package net.sourceforge.pmd.lang.apex.ast;
-
 /**
  * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
  */
+package net.sourceforge.pmd.lang.apex.ast;
 
 import static org.junit.Assert.assertEquals;
 
