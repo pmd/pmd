d1419f6b77d2871b041665f1a1625f45ca449b02
Andreas Dangel <adangel@users.sourceforge.net>
2016-04-23 18:40:11 +0200

    pom: Add a language module for Maven POM

--- pmd-xml/src/test/java/net/sourceforge/pmd/LanguageVersionTest.java
+++ pmd-xml/src/test/java/net/sourceforge/pmd/LanguageVersionTest.java
@@ -10,6 +10,7 @@ import org.junit.runners.Parameterized.Parameters;
 
 import net.sourceforge.pmd.lang.LanguageRegistry;
 import net.sourceforge.pmd.lang.LanguageVersion;
+import net.sourceforge.pmd.lang.pom.PomLanguageModule;
 import net.sourceforge.pmd.lang.wsdl.WsdlLanguageModule;
 import net.sourceforge.pmd.lang.xml.XmlLanguageModule;
 import net.sourceforge.pmd.lang.xsl.XslLanguageModule;
@@ -25,7 +26,8 @@ public class LanguageVersionTest extends AbstractLanguageVersionTest {
         return Arrays.asList(new Object[][] {
                 { XmlLanguageModule.NAME, XmlLanguageModule.TERSE_NAME, "", LanguageRegistry.getLanguage(XmlLanguageModule.NAME).getDefaultVersion() },
                 { XslLanguageModule.NAME, XslLanguageModule.TERSE_NAME, "", LanguageRegistry.getLanguage(XslLanguageModule.NAME).getDefaultVersion() },
-                { WsdlLanguageModule.NAME, WsdlLanguageModule.TERSE_NAME, "", LanguageRegistry.getLanguage(WsdlLanguageModule.NAME).getDefaultVersion() }
+                { WsdlLanguageModule.NAME, WsdlLanguageModule.TERSE_NAME, "", LanguageRegistry.getLanguage(WsdlLanguageModule.NAME).getDefaultVersion() },
+                { PomLanguageModule.NAME, PomLanguageModule.TERSE_NAME, "", LanguageRegistry.getLanguage(PomLanguageModule.NAME).getDefaultVersion() }
             });
     }
 }
