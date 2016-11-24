61e0fc58752e452b0403841962717954d81948aa
Sebastian Ratz <sebastian.ratz@sap.com>
2016-11-24 12:49:04 +0100

    [java] Fix #1547: UnusedImports: Adjust regex to support underscores

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/imports/UnusedImportsRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/imports/UnusedImportsRule.java
@@ -24,6 +24,28 @@ public class UnusedImportsRule extends AbstractJavaRule {
 
     protected Set<ImportWrapper> imports = new HashSet<>();
 
+    /*
+     * Patterns to match the following constructs:
+     *
+     * @see package.class#member(param, param) label {@linkplain
+     * package.class#member(param, param) label} {@link
+     * package.class#member(param, param) label} {@link package.class#field}
+     * {@value package.class#field}
+     * 
+     * @throws package.class label
+     */
+    private static final Pattern SEE_PATTERN = Pattern
+            .compile("@see\\s+(\\p{Alpha}\\w*)(?:#\\w*(?:\\(([\\w\\s,]*)\\))?)?");
+
+    private static final Pattern LINK_PATTERNS = Pattern
+            .compile("\\{@link(?:plain)?\\s+(\\p{Alpha}\\w*)(?:#\\w*(?:\\(([.\\w\\s,]*)\\))?)?[\\s\\}]");
+
+    private static final Pattern VALUE_PATTERN = Pattern.compile("\\{@value\\s+(\\p{Alpha}\\w*)[\\s#\\}]");
+
+    private static final Pattern THROWS_PATTERN = Pattern.compile("@throws\\s+(\\p{Alpha}\\w*)");
+
+    private static final Pattern[] PATTERNS = { SEE_PATTERN, LINK_PATTERNS, VALUE_PATTERN, THROWS_PATTERN };
+
     @Override
     public Object visit(ASTCompilationUnit node, Object data) {
         imports.clear();
@@ -42,30 +64,6 @@ public class UnusedImportsRule extends AbstractJavaRule {
         }
         return data;
     }
-
-    /*
-     * Patterns to match the following constructs:
-     *
-     * @see  package.class#member(param, param)  label
-     * {@linkplain  package.class#member(param, param)  label}
-     * {@link  package.class#member(param, param)  label}
-     * {@link  package.class#field}
-     * {@value  package.class#field}
-     * @throws package.class label
-     */
-    private static final Pattern SEE_PATTERN = Pattern.compile(
-            "@see\\s+(\\p{Alpha}\\p{Alnum}*)(?:#\\p{Alnum}*(?:\\(([\\w\\s,]*)\\))?)?");
-
-    private static final Pattern LINK_PATTERNS = Pattern.compile(
-            "\\{@link(?:plain)?\\s+(\\p{Alpha}\\p{Alnum}*)(?:#\\p{Alnum}*(?:\\(([.\\w\\s,]*)\\))?)?[\\s\\}]");
-
-    private static final Pattern VALUE_PATTERN = Pattern.compile(
-            "\\{@value\\s+(\\p{Alpha}\\p{Alnum}*)[\\s#\\}]");
-
-    private static final Pattern THROWS_PATTERN = Pattern.compile(
-            "@throws\\s+(\\p{Alpha}\\p{Alnum}*)");
-
-    private static final Pattern[] PATTERNS = { SEE_PATTERN, LINK_PATTERNS, VALUE_PATTERN, THROWS_PATTERN };
 
     private void visitComments(ASTCompilationUnit node) {
         if (imports.isEmpty()) {
