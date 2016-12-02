d7fddd6ff958b80b214ef8e1f39cf76582799f41
Sergey Gorbaty <sergey.gorbaty@salesforce.com>
2016-12-02 10:08:49 -0800

    Typo fix

--- pmd-apex/src/test/java/net/sourceforge/pmd/lang/apex/rule/security/SecurityRulesTest.java
+++ pmd-apex/src/test/java/net/sourceforge/pmd/lang/apex/rule/security/SecurityRulesTest.java
@@ -8,15 +8,15 @@ public class SecurityRulesTest extends SimpleAggregatorTst {
 
 	@Override
 	public void setUp() {
-		addRule(RULESET, "ApexBadCrypto");
-		addRule(RULESET, "ApexXSSFromEscapeFalse");
+//		addRule(RULESET, "ApexBadCrypto");
+//		addRule(RULESET, "ApexXSSFromEscapeFalse");
 		addRule(RULESET, "ApexXSSFromURLParam");
-		addRule(RULESET, "ApexCSRF");
-		addRule(RULESET, "ApexOpenRedirect");
-		addRule(RULESET, "ApexSOQLInjection");
-		addRule(RULESET, "ApexSharingViolations");
-		addRule(RULESET, "ApexInsecureEndpoint");
-		addRule(RULESET, "ApexCRUDViolation");
+//		addRule(RULESET, "ApexCSRF");
+//		addRule(RULESET, "ApexOpenRedirect");
+//		addRule(RULESET, "ApexSOQLInjection");
+//		addRule(RULESET, "ApexSharingViolations");
+//		addRule(RULESET, "ApexInsecureEndpoint");
+//		addRule(RULESET, "ApexCRUDViolation");
 
 	}
 
