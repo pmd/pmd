920a58a04341a68a051ff4eadd55f53f4303af0d
Sergey Gorbaty <sergey.gorbaty@salesforce.com>
2016-12-08 17:49:01 +0100

    Code style fixes

--- pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/security/ApexOpenRedirectRule.java
+++ pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/security/ApexOpenRedirectRule.java
@@ -22,83 +22,83 @@ import net.sourceforge.pmd.lang.apex.rule.AbstractApexRule;
  * @author sergey.gorbaty
  */
 public class ApexOpenRedirectRule extends AbstractApexRule {
-	private final static String PAGEREFERENCE = "PageReference";
-	private final Set<String> listOfStringLiteralVariables = new HashSet<>();
+    private static final String PAGEREFERENCE = "PageReference";
+    private final Set<String> listOfStringLiteralVariables = new HashSet<>();
 
-	public ApexOpenRedirectRule() {
-		setProperty(CODECLIMATE_CATEGORIES, new String[] { "Security" });
-		setProperty(CODECLIMATE_REMEDIATION_MULTIPLIER, 100);
-		setProperty(CODECLIMATE_BLOCK_HIGHLIGHTING, false);
-	}
+    public ApexOpenRedirectRule() {
+        setProperty(CODECLIMATE_CATEGORIES, new String[] { "Security" });
+        setProperty(CODECLIMATE_REMEDIATION_MULTIPLIER, 100);
+        setProperty(CODECLIMATE_BLOCK_HIGHLIGHTING, false);
+    }
 
-	@Override
-	public Object visit(ASTNewObjectExpression node, Object data) {
-		checkNewObjects(node, data);
-		return data;
-	}
+    @Override
+    public Object visit(ASTNewObjectExpression node, Object data) {
+        checkNewObjects(node, data);
+        return data;
+    }
 
-	@Override
-	public Object visit(ASTVariableDeclaration node, Object data) {
-		findSafeLiterals(node);
+    @Override
+    public Object visit(ASTVariableDeclaration node, Object data) {
+        findSafeLiterals(node);
 
-		return data;
-	}
+        return data;
+    }
 
-	private void findSafeLiterals(AbstractApexNode<?> node) {
-		ASTLiteralExpression literal = node.getFirstChildOfType(ASTLiteralExpression.class);
-		if (literal != null) {
-			ASTVariableExpression variable = node.getFirstChildOfType(ASTVariableExpression.class);
-			if (variable != null) {
-				listOfStringLiteralVariables.add(Helper.getFQVariableName(variable));
-			}
-		}
-	}
+    private void findSafeLiterals(AbstractApexNode<?> node) {
+        ASTLiteralExpression literal = node.getFirstChildOfType(ASTLiteralExpression.class);
+        if (literal != null) {
+            ASTVariableExpression variable = node.getFirstChildOfType(ASTVariableExpression.class);
+            if (variable != null) {
+                listOfStringLiteralVariables.add(Helper.getFQVariableName(variable));
+            }
+        }
+    }
 
-	@Override
-	public Object visit(ASTFieldDeclaration node, Object data) {
-		findSafeLiterals(node);
-		return data;
-	}
+    @Override
+    public Object visit(ASTFieldDeclaration node, Object data) {
+        findSafeLiterals(node);
+        return data;
+    }
 
-	/**
-	 * Traverses all new declarations to find PageReferences
-	 * 
-	 * @param node
-	 * @param data
-	 */
-	private void checkNewObjects(ASTNewObjectExpression node, Object data) {
-		ClassTypeRef classRef = (ClassTypeRef) node.getNode().getTypeRef();
-		Identifier identifier = classRef.className.get(0);
+    /**
+     * Traverses all new declarations to find PageReferences
+     * 
+     * @param node
+     * @param data
+     */
+    private void checkNewObjects(ASTNewObjectExpression node, Object data) {
+        ClassTypeRef classRef = (ClassTypeRef) node.getNode().getTypeRef();
+        Identifier identifier = classRef.className.get(0);
 
-		if (identifier.value.equalsIgnoreCase(PAGEREFERENCE)) {
-			getObjectValue(node, data);
-		}
-	}
+        if (identifier.value.equalsIgnoreCase(PAGEREFERENCE)) {
+            getObjectValue(node, data);
+        }
+    }
 
-	/**
-	 * Finds any variables being present in PageReference constructor
-	 * 
-	 * @param node
-	 *            - PageReference
-	 * @param data
-	 * 
-	 */
-	private void getObjectValue(ApexNode<?> node, Object data) {
-		// PageReference(foo);
-		final List<ASTVariableExpression> variableExpressions = node.findChildrenOfType(ASTVariableExpression.class);
-		for (ASTVariableExpression variable : variableExpressions) {
-			StringBuilder sb = new StringBuilder().append(variable.getNode().getDefiningType()).append(":")
-					.append(variable.getNode().getIdentifier().value);
-			if (variable.jjtGetChildIndex() == 0 && !listOfStringLiteralVariables.contains(sb.toString())) {
-				addViolation(data, variable);
-			}
-		}
+    /**
+     * Finds any variables being present in PageReference constructor
+     * 
+     * @param node
+     *            - PageReference
+     * @param data
+     * 
+     */
+    private void getObjectValue(ApexNode<?> node, Object data) {
+        // PageReference(foo);
+        final List<ASTVariableExpression> variableExpressions = node.findChildrenOfType(ASTVariableExpression.class);
+        for (ASTVariableExpression variable : variableExpressions) {
+            StringBuilder sb = new StringBuilder().append(variable.getNode().getDefiningType()).append(":")
+                    .append(variable.getNode().getIdentifier().value);
+            if (variable.jjtGetChildIndex() == 0 && !listOfStringLiteralVariables.contains(sb.toString())) {
+                addViolation(data, variable);
+            }
+        }
 
-		// PageReference(foo + bar)
-		final List<ASTBinaryExpression> binaryExpressions = node.findChildrenOfType(ASTBinaryExpression.class);
-		for (ASTBinaryExpression z : binaryExpressions) {
-			getObjectValue(z, data);
-		}
-	}
+        // PageReference(foo + bar)
+        final List<ASTBinaryExpression> binaryExpressions = node.findChildrenOfType(ASTBinaryExpression.class);
+        for (ASTBinaryExpression z : binaryExpressions) {
+            getObjectValue(z, data);
+        }
+    }
 
 }
