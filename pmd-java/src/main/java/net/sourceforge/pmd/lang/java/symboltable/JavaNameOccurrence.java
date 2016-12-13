b73f4752f2efce109c77c2f1cb528d3cfe208305
Juan Martín Sotuyo Dodero <jmsotuyo@monits.com>
2016-12-13 00:18:33 -0300

    Avoid redundant method calls and improve codebase
     - Avoid making calls within for loops, specially costly ones
        such as `getQualifiedTypeNames`
     - Don't create lists when they are empty.
     - Create lists of proper size to avoid resizing / oversizing
     - I'm seeing a ~5% improvement. We are reaching the point were noise
        makes it hard to detect improvements. We should attack GC cycles soon.

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/symboltable/JavaNameOccurrence.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/symboltable/JavaNameOccurrence.java
@@ -196,7 +196,7 @@ public class JavaNameOccurrence implements NameOccurrence {
      * @return return true if image equal to 'this' or 'super'.
      */
     public boolean isThisOrSuper() {
-        return image != null && (image.equals(THIS) || image.equals(SUPER));
+        return THIS.equals(image) || SUPER.equals(image);
     }
 
     /**
