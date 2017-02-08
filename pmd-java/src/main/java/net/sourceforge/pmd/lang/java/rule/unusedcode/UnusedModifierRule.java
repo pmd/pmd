160522129f7bd7c5c96173f2a1bfe5f30358e48f
Juan Martín Sotuyo Dodero <jmsotuyo@monits.com>
2017-02-08 14:55:00 -0300

    [java] Revamp UnusedModifier
     - This covers a ton of missing cases
     - Fixes #246
     - Fixes #247
     - Fixes #248
     - Notice `isNested` has been improved on `ASTClassOrInterfaceDeclaration`
        to cover being nested in an annotation definition

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/unusedcode/UnusedModifierRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/unusedcode/UnusedModifierRule.java
@@ -5,6 +5,8 @@
 package net.sourceforge.pmd.lang.java.rule.unusedcode;
 
 import net.sourceforge.pmd.lang.ast.Node;
+import net.sourceforge.pmd.lang.java.ast.ASTAnnotationMethodDeclaration;
+import net.sourceforge.pmd.lang.java.ast.ASTAnnotationTypeDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTEnumDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTFieldDeclaration;
@@ -13,37 +15,70 @@ import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
 
 public class UnusedModifierRule extends AbstractJavaRule {
 
-    public Object visit(ASTClassOrInterfaceDeclaration node, Object data) {
+    @Override
+    public Object visit(ASTEnumDeclaration node, Object data) {
+        if (node.isStatic()) {
+            // a static enum
+            addViolation(data, node, getMessage());
+        }
+
+        return super.visit(node, data);
+    }
+
+    public Object visit(ASTAnnotationTypeDeclaration node, Object data) {
+        if (node.isAbstract()) {
+            // an abstract annotation
+            addViolation(data, node, getMessage());
+        }
+
         if (!node.isNested()) {
             return super.visit(node, data);
         }
 
-        ASTClassOrInterfaceDeclaration parentClassOrInterface = node
-                .getFirstParentOfType(ASTClassOrInterfaceDeclaration.class);
-        ASTEnumDeclaration parentEnum = node.getFirstParentOfType(ASTEnumDeclaration.class);
+        Node parent = node.jjtGetParent().jjtGetParent().jjtGetParent();
+        boolean isParentInterfaceOrAnnotation = parent instanceof ASTAnnotationTypeDeclaration ||
+                parent instanceof ASTClassOrInterfaceDeclaration && ((ASTClassOrInterfaceDeclaration) parent).isInterface();
 
-        if (node.isInterface() && node.isPublic()) {
-            // a public interface
-            if (parentClassOrInterface != null && parentClassOrInterface.isInterface()) {
-                // within a interface
-                addViolation(data, node, getMessage());
-            }
+        // a public annotation within an interface or annotation
+        if (node.isPublic() && isParentInterfaceOrAnnotation) {
+            addViolation(data, node, getMessage());
+        }
+
+        if (node.isStatic()) {
+            // a static annotation
+            addViolation(data, node, getMessage());
+        }
+
+        return super.visit(node, data);
+    }
+
+    public Object visit(ASTClassOrInterfaceDeclaration node, Object data) {
+        if (node.isInterface() && node.isAbstract()) {
+            // an abstract interface
+            addViolation(data, node, getMessage());
+        }
+
+        if (!node.isNested()) {
+            return super.visit(node, data);
+        }
+
+        Node parent = node.jjtGetParent().jjtGetParent().jjtGetParent();
+        boolean isParentInterfaceOrAnnotation = parent instanceof ASTAnnotationTypeDeclaration ||
+                parent instanceof ASTClassOrInterfaceDeclaration && ((ASTClassOrInterfaceDeclaration) parent).isInterface();
+
+        // a public interface within an interface or annotation
+        if (node.isInterface() && node.isPublic() && isParentInterfaceOrAnnotation) {
+            addViolation(data, node, getMessage());
         }
 
         if (node.isInterface() && node.isStatic()) {
             // a static interface
-            if (parentClassOrInterface != null || parentEnum != null) {
-                // within a interface, class or enum
-                addViolation(data, node, getMessage());
-            }
+            addViolation(data, node, getMessage());
         }
 
-        if (!node.isInterface() && (node.isPublic() || node.isStatic())) {
-            // a public and/or static class
-            if (parentClassOrInterface != null && parentClassOrInterface.isInterface()) {
-                // within a interface
-                addViolation(data, node, getMessage());
-            }
+        // a public and/or static class within an interface or annotation
+        if (!node.isInterface() && (node.isPublic() || node.isStatic()) && isParentInterfaceOrAnnotation) {
+            addViolation(data, node, getMessage());
         }
 
         return super.visit(node, data);
@@ -63,11 +98,19 @@ public class UnusedModifierRule extends AbstractJavaRule {
         return super.visit(node, data);
     }
 
+    public Object visit(ASTAnnotationMethodDeclaration node, Object data) {
+        if (node.isPublic() || node.isAbstract()) {
+            check(node, data);
+        }
+        return super.visit(node, data);
+    }
+
     private void check(Node fieldOrMethod, Object data) {
         // third ancestor could be an AllocationExpression
         // if this is a method in an anonymous inner class
         Node parent = fieldOrMethod.jjtGetParent().jjtGetParent().jjtGetParent();
-        if (parent instanceof ASTClassOrInterfaceDeclaration
+        if (parent instanceof ASTAnnotationTypeDeclaration ||
+                parent instanceof ASTClassOrInterfaceDeclaration
                 && ((ASTClassOrInterfaceDeclaration) parent).isInterface()) {
             addViolation(data, fieldOrMethod);
         }
