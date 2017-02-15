6a12fcdf63cf9c5725d439a28960097fe3895ba6
Andreas Dangel <adangel@users.sourceforge.net>
2017-02-15 20:35:32 +0100

    Add simple test to check zip file content of binary distribution

--- pmd-dist/src/test/java/net/sourceforge/pmd/it/BinaryDistTest.java
+++ pmd-dist/src/test/java/net/sourceforge/pmd/it/BinaryDistTest.java
@@ -7,6 +7,12 @@ package net.sourceforge.pmd.it;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 import org.junit.Test;
 
@@ -14,9 +20,42 @@ import net.sourceforge.pmd.PMD;
 
 public class BinaryDistTest {
 
+    private File getBinaryDistribution() {
+        return new File(".", "target/pmd-bin-" + PMD.VERSION + ".zip");
+    }
+
     @Test
     public void testFileExistence() {
-        File file = new File(".", "target/pmd-bin-" + PMD.VERSION + ".zip");
+        File file = getBinaryDistribution();
         assertTrue(file.exists());
+    }
+
+    private Set<String> getExpectedFileNames() {
+        Set<String> result = new HashSet<>();
+        String basedir = "pmd-bin-" + PMD.VERSION + "/";
+        result.add(basedir);
+        result.add(basedir + "bin/run.sh");
+        result.add(basedir + "bin/pmd.bat");
+        result.add(basedir + "bin/cpd.bat");
+        result.add(basedir + "lib/pmd-core-" + PMD.VERSION + ".jar");
+        result.add(basedir + "lib/pmd-java-" + PMD.VERSION + ".jar");
+        return result;
+    }
+
+    @Test
+    public void testZipFileContent() throws IOException {
+        Set<String> expectedFileNames = getExpectedFileNames();
+
+        ZipFile zip = new ZipFile(getBinaryDistribution());
+
+        Enumeration<? extends ZipEntry> entries = zip.entries();
+        while (entries.hasMoreElements()) {
+            ZipEntry entry = entries.nextElement();
+            expectedFileNames.remove(entry.getName());
+        }
+
+        zip.close();
+
+        assertTrue(expectedFileNames.isEmpty());
     }
 }
