5c91394619adac230cfc9ad7d860d079e91c4d80
Sergey <sergey.gorbaty@salesforce.com>
2017-02-15 14:53:22 -0800

    XSS in apex:outputText

--- pmd-visualforce/src/main/java/net/sourceforge/pmd/lang/vf/rule/basic/VfUnescapeElRule.java
+++ pmd-visualforce/src/main/java/net/sourceforge/pmd/lang/vf/rule/basic/VfUnescapeElRule.java
@@ -5,8 +5,10 @@
 package net.sourceforge.pmd.lang.vf.rule.basic;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 import net.sourceforge.pmd.lang.vf.ast.ASTAttribute;
+import net.sourceforge.pmd.lang.vf.ast.ASTCompilationUnit;
 import net.sourceforge.pmd.lang.vf.ast.ASTElExpression;
 import net.sourceforge.pmd.lang.vf.ast.ASTElement;
 import net.sourceforge.pmd.lang.vf.ast.ASTUnparsedText;
@@ -18,34 +20,61 @@ import net.sourceforge.pmd.lang.vf.rule.AbstractVFRule;
  */
 public class VfUnescapeElRule extends AbstractVFRule {
 
+    private static final String VALUE = "value";
+    private static final String ESCAPE = "escape";
+    private static final String APEX_OUTPUT_TEXT = "apex:outputText";
+    private static final String FALSE = "false";
+
+    @Override
+    public Object visit(ASTCompilationUnit node, Object data) {
+        // TODO Auto-generated method stub
+        return super.visit(node, data);
+    }
+
     @Override
     public Object visit(ASTElement node, Object data) {
-        if (node.getName().equals("apex:outputText")) {
+        if (node.getName().equalsIgnoreCase(APEX_OUTPUT_TEXT)) {
             final List<ASTAttribute> attributes = node.findChildrenOfType(ASTAttribute.class);
             boolean isUnescaped = false;
             boolean isEL = false;
+            boolean hasPlaceholders = false;
+
             for (ASTAttribute attr : attributes) {
                 if (attr != null) {
                     String name = attr.getName();
                     switch (name) {
-                    case "escape":
+                    case ESCAPE:
                         final ASTUnparsedText text = attr.getFirstDescendantOfType(ASTUnparsedText.class);
                         if (text != null) {
-                            if (text.getImage().equalsIgnoreCase("false")) {
+                            if (text.getImage().equalsIgnoreCase(FALSE)) {
                                 isUnescaped = true;
                             }
                         }
                         break;
-                    case "value":
+                    case VALUE:
                         final ASTElExpression elInVal = attr.getFirstDescendantOfType(ASTElExpression.class);
                         if (elInVal != null) {
                             isEL = true;
                         }
+
+                        final ASTUnparsedText textValue = attr.getFirstDescendantOfType(ASTUnparsedText.class);
+                        if (textValue != null) {
+                            if (Pattern.compile("\\{(\\w|,|\\.|'|:|\\s)*\\}").matcher(textValue.getImage()).matches()) {
+                                hasPlaceholders = true;
+                            }
+                        }
+
                         break;
                     default:
                         break;
                     }
 
+                }
+            }
+
+            if (hasPlaceholders && isUnescaped) {
+                if (hasAnyEL(node)) {
+                    addViolation(data, node);
                 }
             }
 
@@ -55,6 +84,23 @@ public class VfUnescapeElRule extends AbstractVFRule {
         }
 
         return super.visit(node, data);
+    }
+
+    private boolean hasAnyEL(ASTElement node) {
+        final List<ASTElement> innerElements = node.findChildrenOfType(ASTElement.class);
+        for (ASTElement element : innerElements) {
+            if (element.getName().equalsIgnoreCase("apex:param")) {
+                final List<ASTAttribute> innerAttributes = element.findChildrenOfType(ASTAttribute.class);
+                for (ASTAttribute attrib : innerAttributes) {
+                    final ASTElExpression elInVal = attrib.getFirstDescendantOfType(ASTElExpression.class);
+                    if (elInVal != null) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
     }
 
 }
