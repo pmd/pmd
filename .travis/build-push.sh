d8ee6717ee638e0ca8856958e7059bd3e29a9e85
Andreas Dangel <adangel@users.sourceforge.net>
2017-02-25 20:06:30 +0100

    Travis Build fixes * Do only perform a release, if building from tag * Add missing git config * Output github releases request in case of error

--- .travis/build-push.sh
+++ .travis/build-push.sh
@@ -4,10 +4,10 @@ set -ev
 VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.5.0:exec)
 echo "Building PMD ${VERSION} on branch ${TRAVIS_BRANCH}"
 
-if [[ "$VERSION" == *-SNAPSHOT ]]; then
-    mvn deploy -Possrh -B -V
-else
+if [[ "$VERSION" != *-SNAPSHOT && "$TRAVIS_TAG" != "" ]]; then
     mvn deploy -Possrh,pmd-release -B -V
+else
+    mvn deploy -Possrh -B -V
 fi
 
 bash .travis/build-site.sh
@@ -20,9 +20,10 @@ bash .travis/build-site.sh
 )
 
 # Uploading pmd distribution to sourceforge
-rsync -avh pmd-dist/target/pmd-*-${VERSION}.zip target/pmd-doc-${VERSION}.zip ${PMD_SF_USER}@web.sourceforge.net:/home/frs/project/pmd/pmd/${VERSION}/
-rsync -avh src/site/markdown/overview/changelog.md ${PMD_SF_USER}@web.sourceforge.net:/home/frs/project/pmd/pmd/${VERSION}/ReadMe.md
-
+if [ "$TRAVIS_TAG" = "" ]; then
+    rsync -avh pmd-dist/target/pmd-*-${VERSION}.zip target/pmd-doc-${VERSION}.zip ${PMD_SF_USER}@web.sourceforge.net:/home/frs/project/pmd/pmd/${VERSION}/
+    rsync -avh src/site/markdown/overview/changelog.md ${PMD_SF_USER}@web.sourceforge.net:/home/frs/project/pmd/pmd/${VERSION}/ReadMe.md
+fi
 
 if [[ "$VERSION" == *-SNAPSHOT && "$TRAVIS_BRANCH" == "master" ]]; then
     # Uploading snapshot site...
