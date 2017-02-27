5c3089051657003b3876e55b40e473c40f070a37
Sergey <sergey.gorbaty@salesforce.com>
2017-02-27 15:27:57 -0800

    Adding VF to the list of supported languages

--- pmd-dist/src/test/java/net/sourceforge/pmd/it/BinaryDistributionIT.java
+++ pmd-dist/src/test/java/net/sourceforge/pmd/it/BinaryDistributionIT.java
@@ -71,7 +71,7 @@ public class BinaryDistributionIT {
             PMDExecutionResult result;
 
             result = PMDExecutor.runPMD(tempDir, "-h");
-            result.assertPMDExecutionResult(1, "apex, ecmascript, java, jsp, plsql, pom, vm, wsdl, xml, xsl");
+            result.assertPMDExecutionResult(1, "apex, vf, ecmascript, java, jsp, plsql, pom, vm, wsdl, xml, xsl");
 
             result = PMDExecutor.runPMDRules(tempDir, srcDir, "java-basic");
             result.assertPMDExecutionResult(4, "JumbledIncrementer.java:8:");
