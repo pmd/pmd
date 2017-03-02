e98c1eb96fbaf9c345fb29e300dfa93a3a9a5b92
Robert Sösemann <robert.soesemann@up2go.com>
2017-03-02 14:23:22 +0100

    Make Apex XPath rules CodeClimate rules (doesn't fix failing test :-(

--- pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/ApexXPathRule.java
+++ pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/ApexXPathRule.java
@@ -9,8 +9,9 @@ import net.sourceforge.pmd.lang.ParserOptions;
 import net.sourceforge.pmd.lang.apex.ApexLanguageModule;
 import net.sourceforge.pmd.lang.apex.ApexParserOptions;
 import net.sourceforge.pmd.lang.rule.XPathRule;
+import net.sourceforge.pmd.renderers.CodeClimateRule;
 
-public class ApexXPathRule extends XPathRule {
+public class ApexXPathRule extends XPathRule implements CodeClimateRule {
 
     public ApexXPathRule() {
         super.setLanguage(LanguageRegistry.getLanguage(ApexLanguageModule.NAME));
