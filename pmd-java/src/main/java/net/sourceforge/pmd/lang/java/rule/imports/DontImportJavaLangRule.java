b78ed9d1a421ceaf63b929fdfbfaae998c8365c9
Michael Pellegrini <mike.pellegrini@allegiantair.com>
2017-04-10 22:32:52 -0400

    Enhance to allow on-demand imports of inner classes
    - Allow for import java.lang.ProcessBuilder.*
    - Update test cases to test for above scenario

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/imports/DontImportJavaLangRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/imports/DontImportJavaLangRule.java
@@ -8,7 +8,7 @@ import net.sourceforge.pmd.lang.java.ast.ASTImportDeclaration;
 import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
 
 public class DontImportJavaLangRule extends AbstractJavaRule {
-    private static final String IMPORT_JAVA_LANG = "java.lang.";
+    private static final String IMPORT_JAVA_LANG = "java.lang";
 
     @Override
     public Object visit(ASTImportDeclaration node, Object data) {
@@ -19,9 +19,11 @@ public class DontImportJavaLangRule extends AbstractJavaRule {
 
         String img = node.jjtGetChild(0).getImage();
         if (img.startsWith(IMPORT_JAVA_LANG)) {
-            if (img.indexOf('.', IMPORT_JAVA_LANG.length() + 1) != -1) {
-                // Importing from a subpackage / inner class
-                return data;
+            if (!IMPORT_JAVA_LANG.equals(img)) {
+                if (img.indexOf('.', IMPORT_JAVA_LANG.length() + 1) != -1 || node.isImportOnDemand()) {
+                    // Importing from a subpackage / inner class
+                    return data;
+                }
             }
             addViolation(data, node);
         }
