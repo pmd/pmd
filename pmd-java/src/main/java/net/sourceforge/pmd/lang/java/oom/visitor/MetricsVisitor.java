42e0a9f8e83c29a98c150c1b1cdfcc0870b44ace
oowekyala <clement.fournier76@gmail.com>
2017-05-22 19:50:41 +0200

    Added the visitor

---
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/oom/visitor/MetricsVisitor.java
@@ -0,0 +1,16 @@
+/**
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
+package net.sourceforge.pmd.lang.java.oom.visitor;
+
+import net.sourceforge.pmd.lang.java.ast.JavaParserVisitorAdapter;
+
+/**
+ * @author Clément Fournier
+ */
+public class MetricsVisitor extends JavaParserVisitorAdapter {
+
+
+
+}
