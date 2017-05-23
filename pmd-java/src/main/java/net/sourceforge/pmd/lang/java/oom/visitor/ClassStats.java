3ad3df3a700835f26480747bd4a3df0f3c100a65
oowekyala <clement.fournier76@gmail.com>
2017-05-23 17:29:03 +0200

    Implemented some navigation and modification methods on PackageStats

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/oom/visitor/ClassStats.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/oom/visitor/ClassStats.java
@@ -7,21 +7,39 @@ package net.sourceforge.pmd.lang.java.oom.visitor;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
- * Statistics about a class. Gathers information about the contained members and their signatures, subclasses and
- * superclasses.
+ * Statistics about a class. Gathers information about the contained members and their signatures,
+ * subclasses and superclasses.
  *
  * @author Clément Fournier
  */
 public class ClassStats {
-    private Map<OperationSignature, List<String>> operations = new HashMap<>();
-    private Map<FieldSignature, List<String>> fields = new HashMap<>();
+    private Map<OperationSignature, Set<String>> operations = new HashMap<>();
+    private Map<FieldSignature, Set<String>> fields = new HashMap<>();
+    private Map<String, ClassStats> nestedClasses = new HashMap<>();
 
     // References to the hierarchy
     // We store strings so that classes not analysed are ignored
     private String superclass;
     private List<String> subclasses;
+
+
+    /**
+     * Adds a ClassStats to the children of this class. This cannot be a nested class, for that see
+     * {@see PackageStats#addClass}
+     *
+     * @param className Name of the nested class.
+     *
+     * @return The new ClassStats or the ones that were found.
+     */
+    public ClassStats addClass(String className) {
+        if (!nestedClasses.containsKey(className)) {
+            nestedClasses.put(className, new ClassStats());
+        }
+        return nestedClasses.get(className);
+    }
 
     //TODO
 }
--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/oom/visitor/PackageStats.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/oom/visitor/PackageStats.java
@@ -4,14 +4,17 @@
 
 package net.sourceforge.pmd.lang.java.oom.visitor;
 
+import static net.sourceforge.pmd.lang.java.ast.QualifiableNode.QualifiedName;
+
 import java.util.HashMap;
 import java.util.Map;
 
 import net.sourceforge.pmd.lang.java.oom.Metrics;
 
+
 /**
- * Package statistics. This recursive data structure mirrors the package structure of the analysed project and stores
- * information about the classes and subpackages it contains.
+ * Package statistics. This recursive data structure mirrors the package structure of the analysed
+ * project and stores information about the classes and subpackages it contains.
  *
  * @author Clément Fournier
  * @see ClassStats
@@ -20,6 +23,43 @@ public class PackageStats {
 
     private Map<String, PackageStats> subPackages = new HashMap<>();
     private Map<String, ClassStats> classes = new HashMap<>();
+
+    public PackageStats() {
+
+    }
+
+    /**
+     * Adds a class to this package if it doesn't exist yet, and returns it.
+     *
+     * @param className The name of the class
+     *
+     * @return The ClassStats that was created, or found.
+     */
+    public ClassStats addClass(String className) {
+        if (!classes.containsKey(className)) {
+            classes.put(className, new ClassStats());
+        }
+        return classes.get(className);
+    }
+
+    /**
+     * Adds a ClassStats to the hierarchy and returns it. The class can be nested.
+     *
+     * @param qname The qualified name of the class
+     *
+     * @return
+     */
+    public ClassStats addClass(QualifiedName qname) {
+        PackageStats container = createPathFor(qname);
+        ClassStats top = container.addClass(qname.getClasses()[0]);
+        String[] classes = qname.getClasses();
+
+        for (int i = 1; i < classes.length; i++) {
+            top = top.addClass(classes[i]);
+        }
+
+        return top;
+    }
 
     public double getMemo(Metrics.ClassMetricKey key, String qname) {
         // TODO
@@ -35,10 +75,52 @@ public class PackageStats {
         return Double.NaN;
     }
 
-    public PackageStats getSubPackage(String[] qname, int index) {
-        // TODO
-        // recursive navigation method
-        return null;
+    /**
+     * Finds the PackageStats for the package passed as parameter.
+     *
+     * @param qname The qualified name of the resource to fetch the package for.
+     *
+     * @return The PackageStats describing the named resource, or null if it can't be found
+     */
+    public PackageStats getSubPackage(QualifiedName qname) {
+        if (qname.getPackages() == null) {
+            return this; // the toplevel
+        }
+        String[] packagePath = qname.getPackages();
+        PackageStats top = subPackages.get(packagePath[0]);
+
+        for (int i = 1; i < packagePath.length && top != null; i++) {
+            top = top.subPackages.get(packagePath[i]);
+        }
+
+        return top;
+    }
+
+
+    /**
+     * Creates the package hierarchy for this resource if it doesn't exist  and returns the deepest
+     * package that contains the resource.
+     *
+     * @param qname The qualified name of the resource
+     *
+     * @return The deepest package that contains this resource
+     */
+    public PackageStats createPathFor(QualifiedName qname) {
+        if (qname.getPackages() == null) {
+            return this; // the toplevel
+        }
+        String[] packagePath = qname.getPackages();
+        PackageStats top = this;
+
+        for (int i = 0; i < packagePath.length; i++) {
+            if (top.subPackages.get(packagePath[i]) == null) {
+                top.subPackages.put(packagePath[i], new PackageStats());
+            }
+
+            top = top.subPackages.get(packagePath[i]);
+        }
+
+        return top;
     }
 
     public ClassStats getClassStats(String name) {
