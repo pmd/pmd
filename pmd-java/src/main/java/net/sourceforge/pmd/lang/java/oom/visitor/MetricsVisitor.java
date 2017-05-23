f83ee39c652203f430cfb4c5707f86d1c2cbd21b
oowekyala <clement.fournier76@gmail.com>
2017-05-23 21:25:31 +0200

    Visitor with classContext

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/oom/visitor/MetricsVisitor.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/oom/visitor/MetricsVisitor.java
@@ -6,33 +6,58 @@ package net.sourceforge.pmd.lang.java.oom.visitor;
 
 import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTConstructorDeclaration;
+import net.sourceforge.pmd.lang.java.ast.ASTFieldDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTMethodDeclaration;
 import net.sourceforge.pmd.lang.java.ast.JavaParserVisitorAdapter;
+import net.sourceforge.pmd.lang.java.ast.QualifiableNode.QualifiedName;
 
 /**
  * @author Clément Fournier
  */
 public class MetricsVisitor extends JavaParserVisitorAdapter {
 
+    private ClassStats classContext = null;
+
     @Override
     public Object visit(ASTClassOrInterfaceDeclaration node, Object data) {
         PackageStats stats = (PackageStats) data;
-        System.err.println("Visiting class " + node.getQualifiedName());
+
+        classContext = stats.getClassStats(node.getQualifiedName(), true);
+
         return super.visit(node, data);
     }
-
 
     @Override
     public Object visit(ASTConstructorDeclaration node, Object data) {
         PackageStats stats = (PackageStats) data;
-        System.err.println("Visiting constructor " + node.getQualifiedName());
+
+        QualifiedName qname = node.getQualifiedName();
+
+        stats.getClassStats(qname, true).addOperation(qname,
+                OperationSignature.buildFor(node));
+
         return super.visit(node, data);
     }
 
     @Override
     public Object visit(ASTMethodDeclaration node, Object data) {
         PackageStats stats = (PackageStats) data;
-        System.err.println("Visiting method " + node.getQualifiedName());
+
+        QualifiedName qname = node.getQualifiedName();
+
+        stats.getClassStats(qname, true).addOperation(qname,
+                OperationSignature.buildFor(node));
+
         return super.visit(node, data);
     }
+
+    @Override
+    public Object visit(ASTFieldDeclaration node, Object data) {
+        PackageStats stats = (PackageStats) data;
+
+        classContext.addField(node.getVariableName(), FieldSignature.buildFor(node));
+
+        return data; // end recursion
+    }
+
 }
