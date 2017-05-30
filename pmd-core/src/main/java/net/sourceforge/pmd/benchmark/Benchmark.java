255dfd04a87cae0c21d4d749248107a1c860a8a7
oowekyala <clement.fournier76@gmail.com>
2017-05-31 00:40:23 +0200

    Changed benchmark name

--- pmd-core/src/main/java/net/sourceforge/pmd/benchmark/Benchmark.java
+++ pmd-core/src/main/java/net/sourceforge/pmd/benchmark/Benchmark.java
@@ -17,7 +17,7 @@ public enum Benchmark {
     SymbolTable(5, "Symbol table"),
     DFA(6, "DFA"),
     TypeResolution(7, "Type resolution"),
-    MetricsVisitor(8, "Metrics visitor"),
+    MetricsVisitor(8, "Metrics"),
     RuleChainVisit(9, "RuleChain visit"),
     Reporting(10, "Reporting"),
     RuleTotal(11, "Rule total"),
