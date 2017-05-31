fc1c0e5c4b5f21cbce3540aad1537e3572600d1d
oowekyala <clement.fournier76@gmail.com>
2017-05-31 23:00:07 +0200

    Checkstyle fix

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/controversial/DataflowAnomalyAnalysisRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/controversial/DataflowAnomalyAnalysisRule.java
@@ -7,7 +7,6 @@ package net.sourceforge.pmd.lang.java.rule.controversial;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
