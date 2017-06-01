0568de5daf361e901763698e4e5225e830ae75b2
oowekyala <clement.fournier76@gmail.com>
2017-06-01 03:34:19 +0200

    Use name() in place of description in NodeType

--- pmd-core/src/main/java/net/sourceforge/pmd/lang/dfa/NodeType.java
+++ pmd-core/src/main/java/net/sourceforge/pmd/lang/dfa/NodeType.java
@@ -10,55 +10,49 @@ package net.sourceforge.pmd.lang.dfa;
  * @author raik
  */
 public enum NodeType {
-    ROOT("<ROOT>"),
+    ROOT,
 
     /* if - else statements */
-    IF_EXPR("IF_EXPR"),
-    IF_LAST_STATEMENT("IF_LAST_STATEMENT"),
-    IF_LAST_STATEMENT_WITHOUT_ELSE("IF_LAST_STATEMENT_WITHOUT_ELSE"),
-    ELSE_LAST_STATEMENT("ELSE_LAST_STATEMENT"),
+    IF_EXPR,
+    IF_LAST_STATEMENT,
+    IF_LAST_STATEMENT_WITHOUT_ELSE,
+    ELSE_LAST_STATEMENT,
 
     /* while statements */
-    WHILE_EXPR("WHILE_EXPR"),
-    WHILE_LAST_STATEMENT("WHILE_LAST_STATEMENT"),
+    WHILE_EXPR,
+    WHILE_LAST_STATEMENT,
 
     /* switch statements */
-    SWITCH_START("SWITCH_START"),
-    CASE_LAST_STATEMENT("CASE_LAST_STATEMENT"),
-    SWITCH_LAST_DEFAULT_STATEMENT("SWITCH_LAST_DEFAULT_STATEMENT"),
-    SWITCH_END("SWITCH_END"),
+    SWITCH_START,
+    CASE_LAST_STATEMENT,
+    SWITCH_LAST_DEFAULT_STATEMENT,
+    SWITCH_END,
 
     /* for statements */
-    FOR_INIT("FOR_INIT"),
-    FOR_EXPR("FOR_EXPR"),
-    FOR_UPDATE("FOR_UPDATE"),
-    FOR_BEFORE_FIRST_STATEMENT("FOR_BEFORE_FIRST_STATEMENT"),
-    FOR_END("FOR_END"),
+    FOR_INIT,
+    FOR_EXPR,
+    FOR_UPDATE,
+    FOR_BEFORE_FIRST_STATEMENT,
+    FOR_END,
 
     /* do - while statements */
-    DO_BEFORE_FIRST_STATEMENT("DO_BEFORE_FIRST_STATEMENT"),
-    DO_EXPR("DO_EXPR"),
+    DO_BEFORE_FIRST_STATEMENT,
+    DO_EXPR,
 
-    RETURN_STATEMENT("RETURN_STATEMENT"),
-    BREAK_STATEMENT("BREAK_STATEMENT"),
-    CONTINUE_STATEMENT("CONTINUE_STATEMENT"),
+    RETURN_STATEMENT,
+    BREAK_STATEMENT,
+    CONTINUE_STATEMENT,
 
-    LABEL_STATEMENT("LABEL_STATEMENT"),
-    LABEL_LAST_STATEMENT("LABEL_LAST_STATEMENT"),
+    LABEL_STATEMENT,
+    LABEL_LAST_STATEMENT,
 
-    ASSERT_STATEMENT("ASSERT_STATEMENT"),
+    ASSERT_STATEMENT,
 
     // TODO - throw statements?
-    THROW_STATEMENT("THROW_STATEMENT");
-
-    private String description;
-
-    NodeType(String description) {
-        this.description = description;
-    }
+    THROW_STATEMENT;
 
     @Override
     public String toString() {
-        return description;
+        return name();
     }
 }
