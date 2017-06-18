eb9b73e3590b435f95e93f315c4459b49c6b5bba
oowekyala <clement.fournier76@gmail.com>
2017-06-18 23:10:37 +0200

    Added getParameterMap

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/ast/ASTMethodOrConstructorDeclaration.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/ast/ASTMethodOrConstructorDeclaration.java
@@ -4,11 +4,23 @@
 
 package net.sourceforge.pmd.lang.java.ast;
 
+import java.util.Map;
+
 import net.sourceforge.pmd.lang.ast.Node;
 
 /**
  * @author Clément Fournier
  */
 public interface ASTMethodOrConstructorDeclaration extends QualifiableNode, Node, AccessNode, JavaNode {
+
+    /**
+     * Returns a map of parameter names to their type image. Iterating over its keys yields the parameters in the
+     * right order.
+     *
+     * @return A map of parameter names to their type image.
+     */
+    Map<String, String> getParameterMap();
+
+    // TODO could use a default implementation when we change the compiler version to 1.8
 
 }
