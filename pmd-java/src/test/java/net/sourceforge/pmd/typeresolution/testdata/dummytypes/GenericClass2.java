39b3f62d33925807c5fa1c8ac4de5bb3fcca3b7a
Bendegúz Nagy <bendeguz.nagy.rf@gmail.com>
2017-06-27 03:50:53 +0200

    Java, typeres: fix error with raw types
    Issue was with determining the upper bounds of raw types when they had type parameters
    with circular references, example: GenericClass<E extends Foo<E>>

--- pmd-java/src/test/java/net/sourceforge/pmd/typeresolution/testdata/dummytypes/GenericClass2.java
+++ pmd-java/src/test/java/net/sourceforge/pmd/typeresolution/testdata/dummytypes/GenericClass2.java
@@ -7,13 +7,13 @@ package net.sourceforge.pmd.typeresolution.testdata.dummytypes;
 public class GenericClass2<A extends Integer, B extends A, C,
         S extends String,
         D extends GenericClass<A, S>,
-        //, E extends GenericClass<E, E>,
+        E extends GenericClass<E, E>,
         F extends GenericClass2> {
     public A first;
     public B second;
     public C third;
     public D fourth;
-    //public E fifth; // recursion
+    public E fifth; // recursion
     public F sixth; // recursion
     public GenericClass2 rawGeneric;
 }
