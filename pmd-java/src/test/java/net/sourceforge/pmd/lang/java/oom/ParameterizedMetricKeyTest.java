fb64e130f8ad141c876b752935908f5c51af9521
oowekyala <clement.fournier76@gmail.com>
2017-07-11 21:02:06 +0200

    Memoization tests

---
+++ pmd-java/src/test/java/net/sourceforge/pmd/lang/java/oom/ParameterizedMetricKeyTest.java
@@ -0,0 +1,113 @@
+/**
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
+package net.sourceforge.pmd.lang.java.oom;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import net.sourceforge.pmd.lang.java.oom.api.ClassMetric;
+import net.sourceforge.pmd.lang.java.oom.api.ClassMetricKey;
+import net.sourceforge.pmd.lang.java.oom.api.MetricKey;
+import net.sourceforge.pmd.lang.java.oom.api.MetricVersion;
+import net.sourceforge.pmd.lang.java.oom.api.OperationMetricKey;
+
+/**
+ * @author Clément Fournier
+ */
+public class ParameterizedMetricKeyTest {
+
+    @Test
+    public void testIdentity() {
+        for (ClassMetricKey key : ClassMetricKey.values()) {
+            ParameterizedMetricKey key1 = ParameterizedMetricKey.getInstance(key, Version.DUMMY1);
+            ParameterizedMetricKey key2 = ParameterizedMetricKey.getInstance(key, Version.DUMMY1);
+            assertEquals(key1, key2);
+            assertTrue(key1 == key2);
+        }
+
+        for (OperationMetricKey key : OperationMetricKey.values()) {
+            ParameterizedMetricKey key1 = ParameterizedMetricKey.getInstance(key, Version.DUMMY1);
+            ParameterizedMetricKey key2 = ParameterizedMetricKey.getInstance(key, Version.DUMMY1);
+            assertEquals(key1, key2);
+            assertTrue(key1 == key2);
+        }
+    }
+
+
+    @Test
+    public void testVersioning() {
+        for (ClassMetricKey key : ClassMetricKey.values()) {
+            ParameterizedMetricKey key1 = ParameterizedMetricKey.getInstance(key, Version.DUMMY1);
+            ParameterizedMetricKey key2 = ParameterizedMetricKey.getInstance(key, Version.DUMMY2);
+            assertNotEquals(key1, key2);
+            assertFalse(key1 == key2);
+        }
+
+        for (OperationMetricKey key : OperationMetricKey.values()) {
+            ParameterizedMetricKey key1 = ParameterizedMetricKey.getInstance(key, Version.DUMMY1);
+            ParameterizedMetricKey key2 = ParameterizedMetricKey.getInstance(key, Version.DUMMY2);
+            assertNotEquals(key1, key2);
+            assertFalse(key1 == key2);
+        }
+    }
+
+
+    @Test
+    public void testToString() {
+        for (ClassMetricKey key : ClassMetricKey.values()) {
+            ParameterizedMetricKey key1 = ParameterizedMetricKey.getInstance(key, Version.DUMMY1);
+            assertTrue(key1.toString().contains(key1.key.name()));
+            assertTrue(key1.toString().contains(key1.version.name()));
+        }
+    }
+
+
+    @Test
+    public void testAdHocMetricKey() {
+        MetricKey<ClassMetric> adHocKey = new MetricKey<ClassMetric>() {
+            @Override
+            public String name() {
+                return "metric";
+            }
+
+
+            @Override
+            public ClassMetric getCalculator() {
+                return null;
+            }
+        };
+
+        MetricVersion adHocVersion = new MetricVersion() {
+            @Override
+            public String name() {
+                return "version";
+            }
+        };
+
+        ParameterizedMetricKey key1 = ParameterizedMetricKey.getInstance(adHocKey, adHocVersion);
+        ParameterizedMetricKey key2 = ParameterizedMetricKey.getInstance(adHocKey, adHocVersion);
+
+        assertNotNull(key1);
+        assertNotNull(key2);
+        assertTrue(key1 == key2);
+        assertEquals(key1, key2);
+        assertTrue(key1.toString().contains(key1.key.name()));
+        assertTrue(key1.toString().contains(key1.version.name()));
+
+    }
+
+
+    private enum Version implements MetricVersion {
+        DUMMY1,
+        DUMMY2
+    }
+
+
+}
