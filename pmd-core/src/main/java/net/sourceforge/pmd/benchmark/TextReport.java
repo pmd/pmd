583184aae2bcdb847369acca9ea715a042d391ae
Juan Martín Sotuyo Dodero <juansotuyo@gmail.com>
2017-07-24 16:51:02 -0300

    Fix bad imports

--- pmd-core/src/main/java/net/sourceforge/pmd/benchmark/TextReport.java
+++ pmd-core/src/main/java/net/sourceforge/pmd/benchmark/TextReport.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import net.sourceforge.pmd.PMD;
 
--- pmd-core/src/main/java/net/sourceforge/pmd/util/ClasspathClassLoader.java
+++ pmd-core/src/main/java/net/sourceforge/pmd/util/ClasspathClassLoader.java
@@ -18,7 +18,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * Create a ClassLoader which loads classes using a CLASSPATH like String. If
