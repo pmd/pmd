785b04944ca62c6f2670bbce76462f014a62df87
Andreas Dangel <andreas.dangel@adangel.org>
2017-08-19 18:34:54 +0200

    Fix checkstyle

--- pmd-java/src/test/java/net/sourceforge/pmd/typeresolution/ClassTypeResolverTest.java
+++ pmd-java/src/test/java/net/sourceforge/pmd/typeresolution/ClassTypeResolverTest.java
@@ -21,8 +21,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.StringTokenizer;
 
-import org.apache.commons.io.IOUtils;
-
 import org.jaxen.JaxenException;
 import org.junit.Assert;
 import org.junit.Test;
