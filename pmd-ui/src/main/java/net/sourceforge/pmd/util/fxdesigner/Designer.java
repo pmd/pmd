22ee16b09b941097421d91173103a14d3708f1d7
Clément Fournier <clement.fournier76@gmail.com>
2017-08-31 14:29:43 +0200

    Icons

--- pmd-ui/src/main/java/net/sourceforge/pmd/util/fxdesigner/Designer.java
+++ pmd-ui/src/main/java/net/sourceforge/pmd/util/fxdesigner/Designer.java
@@ -5,23 +5,25 @@
 package net.sourceforge.pmd.util.fxdesigner;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 import net.sourceforge.pmd.PMD;
 
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
  * @author Clément Fournier
  */
 public class Designer extends Application {
-
-    public static void main(String[] args) {
-        launch(args);
-    }
 
     @Override
     public void start(Stage stage) throws IOException {
@@ -30,7 +32,34 @@ public class Designer extends Application {
         Scene scene = new Scene(root, 900, 600);
 
         stage.setTitle("PMD Rule Designer (v " + PMD.VERSION + ')');
+        setIcons(stage);
+
         stage.setScene(scene);
         stage.show();
+    }
+
+
+    private void setIcons(Stage primaryStage) {
+        ObservableList<Image> icons = primaryStage.getIcons();
+        final String dirPrefix = "icons/app/";
+        List<String> imageNames = Arrays.asList("pmd-logo.png",
+                                                "pmd-logo_small.png",
+                                                "pmd-logo_tiny.png",
+                                                "pmd-logo_big.png");
+
+        List<Image> images = imageNames.stream()
+                                       .map(s -> dirPrefix + s)
+                                       .map(s -> getClass().getResourceAsStream(s))
+                                       .filter(Objects::nonNull)
+                                       .map(Image::new)
+                                       .collect(Collectors.toList());
+
+
+        icons.addAll(images);
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
