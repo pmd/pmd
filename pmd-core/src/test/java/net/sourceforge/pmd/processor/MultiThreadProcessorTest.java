e734c9cce9b688944712a42cea373ed661637f23
Juan Martín Sotuyo Dodero <juansotuyo@gmail.com>
2017-09-12 17:11:38 -0300

    Add failing test scenario for repeated config error

--- pmd-core/src/test/java/net/sourceforge/pmd/processor/MultiThreadProcessorTest.java
+++ pmd-core/src/test/java/net/sourceforge/pmd/processor/MultiThreadProcessorTest.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -16,33 +17,64 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import net.sourceforge.pmd.PMDConfiguration;
+import net.sourceforge.pmd.Report;
+import net.sourceforge.pmd.Report.ConfigurationError;
 import net.sourceforge.pmd.RuleContext;
 import net.sourceforge.pmd.RuleSetFactory;
 import net.sourceforge.pmd.RuleViolation;
 import net.sourceforge.pmd.ThreadSafeReportListener;
 import net.sourceforge.pmd.lang.ast.Node;
 import net.sourceforge.pmd.lang.rule.AbstractRule;
+import net.sourceforge.pmd.renderers.AbstractAccumulatingRenderer;
 import net.sourceforge.pmd.renderers.Renderer;
 import net.sourceforge.pmd.stat.Metric;
 import net.sourceforge.pmd.util.datasource.DataSource;
 
 public class MultiThreadProcessorTest {
 
-    @Test
-    public void testRulesThreadSafety() {
+    private RuleContext ctx;
+    private MultiThreadProcessor processor;
+    private RuleSetFactory ruleSetFactory;
+    private List<DataSource> files;
+    private SimpleReportListener reportListener;
+    
+    public void setUpForTest(final String ruleset) {
         PMDConfiguration configuration = new PMDConfiguration();
-        configuration.setRuleSets("rulesets/MultiThreadProcessorTest/basic.xml");
+        configuration.setRuleSets(ruleset);
         configuration.setThreads(2);
-        List<DataSource> files = new ArrayList<>();
+        files = new ArrayList<>();
         files.add(new StringDataSource("file1-violation.dummy", "ABC"));
         files.add(new StringDataSource("file2-foo.dummy", "DEF"));
 
-        SimpleReportListener reportListener = new SimpleReportListener();
-        RuleContext ctx = new RuleContext();
+        reportListener = new SimpleReportListener();
+        ctx = new RuleContext();
         ctx.getReport().addListener(reportListener);
 
-        MultiThreadProcessor processor = new MultiThreadProcessor(configuration);
-        RuleSetFactory ruleSetFactory = new RuleSetFactory();
+        processor = new MultiThreadProcessor(configuration);
+        ruleSetFactory = new RuleSetFactory();
+    }
+    
+    @Test
+    public void testRulesDysnfunctionalLog() throws IOException {
+        setUpForTest("rulesets/MultiThreadProcessorTest/dysfunctional.xml");
+        final SimpleRenderer renderer = new SimpleRenderer(null, null);
+        renderer.start();
+        processor.processFiles(ruleSetFactory, files, ctx, Collections.<Renderer>singletonList(renderer));
+        renderer.end();
+
+        final Iterator<ConfigurationError> configErrors = renderer.getReport().configErrors();
+        final ConfigurationError error = configErrors.next();
+        
+        Assert.assertEquals("Dysfunctional rule message not present",
+                DysfunctionalRule.DYSFUNCTIONAL_RULE_REASON, error.issue());
+        Assert.assertEquals("Dysfunctional rule is wrong",
+                DysfunctionalRule.class, error.rule().getClass());
+        Assert.assertFalse("More configuration errors found than expected", configErrors.hasNext());
+    }
+    
+    @Test
+    public void testRulesThreadSafety() {
+        setUpForTest("rulesets/MultiThreadProcessorTest/basic.xml");
         processor.processFiles(ruleSetFactory, files, ctx, Collections.<Renderer>emptyList());
 
         // if the rule is not executed, then maybe a
@@ -104,6 +136,21 @@ public class MultiThreadProcessorTest {
             }
         }
     }
+    
+    public static class DysfunctionalRule extends AbstractRule {
+
+        public static final String DYSFUNCTIONAL_RULE_REASON = "dysfunctional rule is dysfunctional";
+
+        @Override
+        public void apply(List<? extends Node> nodes, RuleContext ctx) {
+            // noop
+        }
+        
+        @Override
+        public String dysfunctionReason() {
+            return DYSFUNCTIONAL_RULE_REASON;
+        }
+    }
 
     private static class SimpleReportListener implements ThreadSafeReportListener {
         public AtomicInteger violations = new AtomicInteger(0);
@@ -115,6 +162,26 @@ public class MultiThreadProcessorTest {
 
         @Override
         public void metricAdded(Metric metric) {
+        }
+    }
+    
+    private static class SimpleRenderer extends AbstractAccumulatingRenderer {
+
+        public SimpleRenderer(String name, String description) {
+            super(name, description);
+        }
+
+        @Override
+        public String defaultFileExtension() {
+            return null;
+        }
+
+        @Override
+        public void end() throws IOException {
+        }
+        
+        public Report getReport() {
+            return report;
         }
     }
 }
