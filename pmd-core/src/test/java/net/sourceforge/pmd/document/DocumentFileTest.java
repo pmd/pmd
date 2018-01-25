21650ba85acabc02d70f3448729de88c680173fa
Clément Fournier <clement.fournier76@gmail.com>
2018-01-26 00:20:38 +0100

    Move util.documents into n.s.pmd.documents
     Fix commit 40ad978 compilation errors

--- pmd-core/src/test/java/net/sourceforge/pmd/document/DocumentFileTest.java
+++ pmd-core/src/test/java/net/sourceforge/pmd/document/DocumentFileTest.java
@@ -2,7 +2,7 @@
  * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
  */
 
-package net.sourceforge.pmd.util.document;
+package net.sourceforge.pmd.document;
 
 import static org.junit.Assert.assertEquals;
 
