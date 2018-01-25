0316462916eeefb6181b3b9cc388aa87834c15ac
Clément Fournier <clement.fournier76@gmail.com>
2018-01-25 03:13:22 +0100

    Checkstyle

--- pmd-core/src/main/java/net/sourceforge/pmd/lang/ast/AbstractNode.java
+++ pmd-core/src/main/java/net/sourceforge/pmd/lang/ast/AbstractNode.java
@@ -453,9 +453,9 @@ public abstract class AbstractNode implements Node {
     @Override
     // @Deprecated // FUTURE 7.0.0 make abstract
     public String getXPathNodeName() {
-        LOG.warning("getXPathNodeName should be overriden in classes derived from AbstractNode. " +
-                            "The implementation is provided for compatibility with existing implementors," +
-                            "but could be declared abstract as soon as release " + PMDVersion.getNextMajorRelease()
+        LOG.warning("getXPathNodeName should be overriden in classes derived from AbstractNode. "
+                            + "The implementation is provided for compatibility with existing implementors,"
+                            + "but could be declared abstract as soon as release " + PMDVersion.getNextMajorRelease()
                             + ".");
         return toString();
     }
