41de94776e2606fb025bf6ea3c5b9242cb340bae
Juan Martín Sotuyo Dodero <juansotuyo@gmail.com>
2018-01-25 21:20:13 -0300

    Properly handle shortnames with comma-separated args
     - Also added unit tests to make sure the behavior is as expected

--- pmd-core/src/main/java/net/sourceforge/pmd/util/datasource/FileDataSource.java
+++ pmd-core/src/main/java/net/sourceforge/pmd/util/datasource/FileDataSource.java
@@ -8,6 +8,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * DataSource implementation to read data from a file.
@@ -38,14 +40,22 @@ public class FileDataSource implements DataSource {
 
     private String glomName(boolean shortNames, String inputFileName, File file) {
         if (shortNames) {
-            if (inputFileName != null && inputFileName.indexOf(',') == -1) {
-                if (new File(inputFileName).isDirectory()) {
-                    return trimAnyPathSep(file.getPath().substring(inputFileName.length()));
-                } else {
-                    if (inputFileName.indexOf(FILE_SEPARATOR.charAt(0)) == -1) {
-                        return inputFileName;
+            if (inputFileName != null) {
+                for (final String prefix : inputFileName.split(",")) {
+                    final Path prefPath = Paths.get(prefix).toAbsolutePath();
+                    final String prefPathString = prefPath.toString();
+                    final String absoluteFilePath = file.getAbsolutePath();
+                    
+                    if (absoluteFilePath.startsWith(prefPathString)) {
+                        if (prefPath.toFile().isDirectory()) {
+                            return trimAnyPathSep(absoluteFilePath.substring(prefPathString.length()));
+                        } else {
+                            if (inputFileName.indexOf(FILE_SEPARATOR.charAt(0)) == -1) {
+                                return inputFileName;
+                            }
+                            return trimAnyPathSep(absoluteFilePath.substring(prefPathString.lastIndexOf(FILE_SEPARATOR)));
+                        }
                     }
-                    return trimAnyPathSep(inputFileName.substring(inputFileName.lastIndexOf(FILE_SEPARATOR)));
                 }
             } else {
                 // if the 'master' file is not specified, just use the file name
