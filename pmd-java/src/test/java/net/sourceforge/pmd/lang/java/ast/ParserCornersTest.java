f86a06be4a674e5e9b44c4d8ec217221e5da6337
Juan Martín Sotuyo Dodero <juansotuyo@gmail.com>
2018-01-29 23:26:42 -0300

    Fix checkstyle

--- pmd-java/src/test/java/net/sourceforge/pmd/lang/java/ast/ParserCornersTest.java
+++ pmd-java/src/test/java/net/sourceforge/pmd/lang/java/ast/ParserCornersTest.java
@@ -41,26 +41,26 @@ public class ParserCornersTest {
      */
     @Test
     public void testDiamondUsageJava8() {
-        parseJava18("public class PMDExceptionTest {\n" + 
-                "  private Component makeUI() {\n" + 
-                "    String[] model = {\"123456\", \"7890\"};\n" + 
-                "    JComboBox<String> comboBox = new JComboBox<>(model);\n" + 
-                "    comboBox.setEditable(true);\n" + 
-                "    comboBox.setEditor(new BasicComboBoxEditor() {\n" + 
-                "      private Component editorComponent;\n" + 
-                "      @Override public Component getEditorComponent() {\n" + 
-                "        if (editorComponent == null) {\n" + 
-                "          JTextField tc = (JTextField) super.getEditorComponent();\n" + 
-                "          editorComponent = new JLayer<>(tc, new ValidationLayerUI<>());\n" + 
-                "        }\n" + 
-                "        return editorComponent;\n" + 
-                "      }\n" + 
-                "    });\n" + 
-                "    JPanel p = new JPanel();\n" + 
-                "    p.add(comboBox);\n" + 
-                "    return p;\n" + 
-                "  }\n" + 
-                "}");
+        parseJava18("public class PMDExceptionTest {\n"
+                + "  private Component makeUI() {\n"
+                + "    String[] model = {\"123456\", \"7890\"};\n"
+                + "    JComboBox<String> comboBox = new JComboBox<>(model);\n"
+                + "    comboBox.setEditable(true);\n"
+                + "    comboBox.setEditor(new BasicComboBoxEditor() {\n"
+                + "      private Component editorComponent;\n"
+                + "      @Override public Component getEditorComponent() {\n"
+                + "        if (editorComponent == null) {\n"
+                + "          JTextField tc = (JTextField) super.getEditorComponent();\n"
+                + "          editorComponent = new JLayer<>(tc, new ValidationLayerUI<>());\n"
+                + "        }\n"
+                + "        return editorComponent;\n"
+                + "      }\n"
+                + "    });\n"
+                + "    JPanel p = new JPanel();\n"
+                + "    p.add(comboBox);\n"
+                + "    return p;\n"
+                + "  }\n"
+                + "}");
     }
 
     @Test
