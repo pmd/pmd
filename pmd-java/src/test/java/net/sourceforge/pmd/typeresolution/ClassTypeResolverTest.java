733c871b9690e787c9137aceb34f4338e2617533
Clément Fournier <clement.fournier76@gmail.com>
2018-02-13 02:46:55 +0100

    Fix nested anonymous class bug with type resolution

--- pmd-java/src/test/java/net/sourceforge/pmd/typeresolution/ClassTypeResolverTest.java
+++ pmd-java/src/test/java/net/sourceforge/pmd/typeresolution/ClassTypeResolverTest.java
@@ -11,6 +11,7 @@ import static net.sourceforge.pmd.lang.java.typeresolution.typedefinition.TypeDe
 import static net.sourceforge.pmd.lang.java.typeresolution.typeinference.InferenceRuleType.LOOSE_INVOCATION;
 import static net.sourceforge.pmd.lang.java.typeresolution.typeinference.InferenceRuleType.SUBTYPE;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
@@ -99,6 +100,7 @@ import net.sourceforge.pmd.typeresolution.testdata.MethodPotentialApplicability;
 import net.sourceforge.pmd.typeresolution.testdata.MethodSecondPhase;
 import net.sourceforge.pmd.typeresolution.testdata.MethodStaticAccess;
 import net.sourceforge.pmd.typeresolution.testdata.MethodThirdPhase;
+import net.sourceforge.pmd.typeresolution.testdata.NestedAllocationExpressions;
 import net.sourceforge.pmd.typeresolution.testdata.NestedAnonymousClass;
 import net.sourceforge.pmd.typeresolution.testdata.Operators;
 import net.sourceforge.pmd.typeresolution.testdata.OverloadedMethodsUsage;
@@ -1778,6 +1780,20 @@ public class ClassTypeResolverTest {
                      forClass(Integer.class));
         assertEquals(inferedMethod.getParameterTypes().get(3),
                      forClass(SuperClassAOther2.class));
+    }
+
+
+    @Test
+    public void testNestedAllocationExpressions() {
+        ASTCompilationUnit acu = parseAndTypeResolveForClass15(NestedAllocationExpressions.class);
+        List<ASTAllocationExpression> allocs = acu.findDescendantsOfType(ASTAllocationExpression.class);
+
+        assertFalse(allocs.get(0).isAnonymousClass());
+        assertEquals(Thread.class, allocs.get(0).getType());
+
+        assertTrue(allocs.get(1).isAnonymousClass());
+        // FUTURE 1.8 use Class.getTypeName() instead of toString
+        assertTrue(allocs.get(1).getType().toString().endsWith("NestedAllocationExpressions$1"));
     }
 
     @Test
