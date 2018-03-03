ba54b1c91a08cb3ec49c40d2fb17950cd09af51b
Juan Martín Sotuyo Dodero <juansotuyo@gmail.com>
2018-03-03 20:02:28 -0300

    Fix failing tests

--- pmd-java/src/test/java/net/sourceforge/pmd/typeresolution/ClassTypeResolverTest.java
+++ pmd-java/src/test/java/net/sourceforge/pmd/typeresolution/ClassTypeResolverTest.java
@@ -38,6 +38,7 @@ import net.sourceforge.pmd.lang.java.ParserTstUtil;
 import net.sourceforge.pmd.lang.java.ast.ASTAllocationExpression;
 import net.sourceforge.pmd.lang.java.ast.ASTArgumentList;
 import net.sourceforge.pmd.lang.java.ast.ASTBooleanLiteral;
+import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceBody;
 import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceType;
 import net.sourceforge.pmd.lang.java.ast.ASTCompilationUnit;
@@ -238,7 +239,9 @@ public class ClassTypeResolverTest {
         Node acu = parseAndTypeResolveForClass(NestedAnonymousClass.class, "1.8");
         ASTAllocationExpression allocationExpression = acu.getFirstDescendantOfType(ASTAllocationExpression.class);
         ASTAllocationExpression nestedAllocation
-                = allocationExpression.getFirstDescendantOfType(ASTAllocationExpression.class);
+                = allocationExpression.getFirstChildOfType(ASTClassOrInterfaceBody.class) // get the declaration
+                .jjtGetChild(0).jjtGetChild(1) // into the declaration, through the boundary
+                .getFirstDescendantOfType(ASTAllocationExpression.class);
         TypeNode child = (TypeNode) nestedAllocation.jjtGetChild(0);
         Assert.assertTrue(Converter.class.isAssignableFrom(child.getType()));
         Assert.assertSame(String.class, child.getTypeDefinition().getGenericType(0).getType());
