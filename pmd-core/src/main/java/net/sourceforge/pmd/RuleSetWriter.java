5972c32ecf03533fbdadf3cf3eca68a5f913e676
Andreas Dangel <andreas.dangel@adangel.org>
2018-03-09 10:01:00 +0100

    Fix checkstyle

--- pmd-core/src/main/java/net/sourceforge/pmd/RuleSetWriter.java
+++ pmd-core/src/main/java/net/sourceforge/pmd/RuleSetWriter.java
@@ -11,7 +11,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.FactoryConfigurationError;
