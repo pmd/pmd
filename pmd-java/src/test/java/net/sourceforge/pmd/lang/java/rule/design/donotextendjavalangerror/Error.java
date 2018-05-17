6d1d5e596341e44db00802e50420bd0070c8737c
Akshat Bahety <akshatbahety@gmail.com>
2018-05-17 12:44:33 +0530

    Code Style Changes

--- pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/design/donotextendjavalangerror/Error.java
+++ pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/design/donotextendjavalangerror/Error.java
@@ -4,9 +4,9 @@
 
 package net.sourceforge.pmd.lang.java.rule.design.donotextendjavalangerror;
 
-/*
-    @author Akshat Bahety
-    @since 6.4.0
+/**
+ * @author Akshat Bahety
+ * @since 6.4.0
  */
 
 public class Error {
