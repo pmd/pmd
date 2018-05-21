f14fbd02b57dc65b46ebe520b9ff92bc5289d4fd
Andreas Dangel <andreas.dangel@adangel.org>
2018-05-21 12:46:29 +0200

    Rework InsecureCryptoIvRule to use RuleChain on AllocationExpression No need for Util. Use Symbol Table instead.

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/security/InsecureCryptoIvRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/security/InsecureCryptoIvRule.java
@@ -4,22 +4,19 @@
 
 package net.sourceforge.pmd.lang.java.rule.security;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
+import net.sourceforge.pmd.lang.ast.Node;
+import net.sourceforge.pmd.lang.java.ast.ASTAllocationExpression;
+import net.sourceforge.pmd.lang.java.ast.ASTArgumentList;
+import net.sourceforge.pmd.lang.java.ast.ASTArguments;
 import net.sourceforge.pmd.lang.java.ast.ASTArrayInitializer;
-import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceBodyDeclaration;
+import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceType;
 import net.sourceforge.pmd.lang.java.ast.ASTLiteral;
-import net.sourceforge.pmd.lang.java.ast.ASTLocalVariableDeclaration;
-import net.sourceforge.pmd.lang.java.ast.ASTPrimitiveType;
-import net.sourceforge.pmd.lang.java.ast.ASTReferenceType;
-import net.sourceforge.pmd.lang.java.ast.ASTVariableDeclarator;
+import net.sourceforge.pmd.lang.java.ast.ASTName;
+import net.sourceforge.pmd.lang.java.ast.ASTPrimaryPrefix;
 import net.sourceforge.pmd.lang.java.ast.ASTVariableInitializer;
 import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
 import net.sourceforge.pmd.lang.java.symboltable.VariableNameDeclaration;
-import net.sourceforge.pmd.lang.symboltable.NameOccurrence;
+import net.sourceforge.pmd.lang.java.typeresolution.TypeHelper;
 
 /**
  * Finds hardcoded static Initialization Vectors vectors used with cryptographic
@@ -38,73 +35,56 @@ import net.sourceforge.pmd.lang.symboltable.NameOccurrence;
 public class InsecureCryptoIvRule extends AbstractJavaRule {
 
     public InsecureCryptoIvRule() {
-        addRuleChainVisit(ASTClassOrInterfaceBodyDeclaration.class);
+        addRuleChainVisit(ASTAllocationExpression.class);
     }
 
     @Override
-    public Object visit(ASTClassOrInterfaceBodyDeclaration node, Object data) {
-        Set<ASTLocalVariableDeclaration> foundLocalVars = new HashSet<>();
-        // find new javax.crypto.spec.IvParameterSpec(...)
-        Set<String> passedInIvVarNames = Util.findVariablesPassedToAnyParam(node, javax.crypto.spec.IvParameterSpec.class);
+    public Object visit(ASTAllocationExpression node, Object data) {
+        ASTClassOrInterfaceType declClassName = node.getFirstChildOfType(ASTClassOrInterfaceType.class);
+        if (declClassName != null && TypeHelper.isA(declClassName, javax.crypto.spec.IvParameterSpec.class)) {
+            Node firstArgument = null;
 
-        List<ASTLocalVariableDeclaration> localVars = node.findDescendantsOfType(ASTLocalVariableDeclaration.class);
-        for (ASTLocalVariableDeclaration localVar : localVars) {
-            foundLocalVars.addAll(extractPrimitiveTypes(localVar));
+            ASTArguments arguments = node.getFirstChildOfType(ASTArguments.class);
+            if (arguments.getArgumentCount() > 0) {
+                firstArgument = arguments.getFirstChildOfType(ASTArgumentList.class).jjtGetChild(0);
+            }
+
+            if (firstArgument != null) {
+                ASTPrimaryPrefix prefix = firstArgument.getFirstDescendantOfType(ASTPrimaryPrefix.class);
+                validateProperIv(data, prefix);
+            }
+        }
+        return super.visit(node, data);
+    }
+
+    private void validateProperIv(Object data, ASTPrimaryPrefix firstArgumentExpression) {
+        if (firstArgumentExpression == null) {
+            return;
         }
 
-        Map<VariableNameDeclaration, List<NameOccurrence>> globalDecls = node.getScope()
-                .getDeclarations(VariableNameDeclaration.class);
-
-        for (VariableNameDeclaration fieldVar : globalDecls.keySet()) {
-            if (passedInIvVarNames.contains(fieldVar.getNode().getImage())) {
-                ASTVariableDeclarator var = fieldVar.getNode().getFirstParentOfType(ASTVariableDeclarator.class);
-                if (var != null) {
-                    validateProperIv(data, var.getFirstDescendantOfType(ASTVariableInitializer.class));
+        // named variable
+        ASTName namedVar = firstArgumentExpression.getFirstDescendantOfType(ASTName.class);
+        if (namedVar != null) {
+            // find where it's declared, if possible
+            if (namedVar != null && namedVar.getNameDeclaration() instanceof VariableNameDeclaration) {
+                VariableNameDeclaration varDecl = (VariableNameDeclaration) namedVar.getNameDeclaration();
+                ASTVariableInitializer initializer = varDecl.getAccessNodeParent().getFirstDescendantOfType(ASTVariableInitializer.class);
+                if (initializer != null) {
+                    validateProperIv(data, initializer.getFirstDescendantOfType(ASTPrimaryPrefix.class));
                 }
             }
         }
 
-        for (ASTLocalVariableDeclaration foundLocalVar : foundLocalVars) {
-            if (passedInIvVarNames.contains(foundLocalVar.getVariableName())) {
-                validateProperIv(data, foundLocalVar.getFirstDescendantOfType(ASTVariableInitializer.class));
-            }
-        }
-
-        return data;
-    }
-
-    private Set<ASTLocalVariableDeclaration> extractPrimitiveTypes(ASTLocalVariableDeclaration localVar) {
-        List<ASTPrimitiveType> types = localVar.findDescendantsOfType(ASTPrimitiveType.class);
-        Set<ASTLocalVariableDeclaration> retVal = new HashSet<>();
-        extractPrimitiveTypesInner(retVal, localVar, types);
-
-        return retVal;
-    }
-
-    private <T> void extractPrimitiveTypesInner(Set<T> retVal, T field, List<ASTPrimitiveType> types) {
-        for (ASTPrimitiveType type : types) {
-            if (type.hasImageEqualTo("byte")) {
-                ASTReferenceType parent = type.getFirstParentOfType(ASTReferenceType.class);
-                if (parent != null) {
-                    retVal.add(field);
-                }
-            }
-        }
-    }
-
-    private void validateProperIv(Object data, ASTVariableInitializer varInit) {
         // hard coded array
-        ASTArrayInitializer arrayInit = varInit.getFirstDescendantOfType(ASTArrayInitializer.class);
+        ASTArrayInitializer arrayInit = firstArgumentExpression.getFirstDescendantOfType(ASTArrayInitializer.class);
         if (arrayInit != null) {
-            addViolation(data, varInit);
+            addViolation(data, firstArgumentExpression);
         }
 
         // string literal
-        ASTLiteral literal = varInit.getFirstDescendantOfType(ASTLiteral.class);
+        ASTLiteral literal = firstArgumentExpression.getFirstDescendantOfType(ASTLiteral.class);
         if (literal != null && literal.isStringLiteral()) {
-            addViolation(data, varInit);
+            addViolation(data, firstArgumentExpression);
         }
-
     }
-
 }
