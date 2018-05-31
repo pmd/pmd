f65e67e3bde0d78a8998786030d61adbbd24df74
Clément Fournier <clement.fournier76@gmail.com>
2018-05-31 17:52:31 +0000

    Add space

--- pmd-doc/src/main/java/net/sourceforge/pmd/docs/RuleDocGenerator.java
+++ pmd-doc/src/main/java/net/sourceforge/pmd/docs/RuleDocGenerator.java
@@ -391,7 +391,7 @@ public class RuleDocGenerator {
 
                     if (rule instanceof XPathRule || rule instanceof RuleReference && ((RuleReference) rule).getRule() instanceof XPathRule) {
                         lines.add("**This rule is defined by the following XPath expression:**");
-                        lines.add("```xpath");
+                        lines.add("``` xpath");
                         lines.addAll(toLines(StringUtils.stripToEmpty(rule.getProperty(XPathRule.XPATH_DESCRIPTOR))));
                         lines.add("```");
                     } else {
