8ee39deacee45cfadaab420f01274c8bb4825df9
Juan Martín Sotuyo Dodero <juansotuyo@gmail.com>
2018-06-23 21:51:28 -0300

    Upgrade to 6.0.0 categories

--- pmd-apex/src/test/java/net/sourceforge/pmd/lang/apex/rule/errorprone/ErrorProneRulesTest.java
+++ pmd-apex/src/test/java/net/sourceforge/pmd/lang/apex/rule/errorprone/ErrorProneRulesTest.java
@@ -10,13 +10,11 @@ public class ErrorProneRulesTest extends SimpleAggregatorTst {
 
     private static final String RULESET = "category/apex/errorprone.xml";
 
-    
-
-
     @Override
     public void setUp() {
         addRule(RULESET, "AvoidDirectAccessTriggerMap");
         addRule(RULESET, "AvoidHardcodingId");
+        addRule(RULESET, "AvoidNonExistentAnnotations");
         addRule(RULESET, "EmptyCatchBlock");
         addRule(RULESET, "EmptyIfStmt");
         addRule(RULESET, "EmptyStatementBlock");
