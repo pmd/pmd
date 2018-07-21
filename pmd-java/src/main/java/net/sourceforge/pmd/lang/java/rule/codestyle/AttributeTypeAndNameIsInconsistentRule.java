58446a2ecd2b687fb1213c18921878db41faa84b
Andreas Dangel <andreas.dangel@adangel.org>
2018-07-21 14:02:25 +0200

    Fix runtime errors (NPE, ClassCastException, IndexOutOfBounds)

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/codestyle/AttributeTypeAndNameIsInconsistentRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/codestyle/AttributeTypeAndNameIsInconsistentRule.java
@@ -1,105 +1,109 @@
 package net.sourceforge.pmd.lang.java.rule.codestyle;
 
-import net.sourceforge.pmd.lang.java.ast.*;
-import net.sourceforge.pmd.lang.java.rule.*;
+import net.sourceforge.pmd.lang.java.ast.ASTFieldDeclaration;
+import net.sourceforge.pmd.lang.java.ast.ASTLocalVariableDeclaration;
+import net.sourceforge.pmd.lang.java.ast.ASTType;
+import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
 
-public class AttributeTypeAndNameIsInconsistentRule extends AbstractJavaRule 
-{	
-    public Object visit(ASTFieldDeclaration node, Object data) 
-    {	
+public class AttributeTypeAndNameIsInconsistentRule extends AbstractJavaRule
+{
+    @Override
+    public Object visit(ASTFieldDeclaration node, Object data)
+    {
         String nameOfField = node.getVariableName();
-        ASTType t = (ASTType) node.jjtGetChild(0); // Is first child always ASTType
+        ASTType t = node.getFirstChildOfType(ASTType.class);
 
         /**************** Type Should Be Boolean As Name Suggests *********************/
-        
-        if(nameOfField.startsWith("is") && (nameOfField.charAt(2) > 64) && (nameOfField.charAt(2) < 91))  // after is a capital letter expected to not addViolation to a field called isotherm or so
+
+        if(nameOfField.startsWith("is") &&  nameOfField.length() > 2 && (nameOfField.charAt(2) > 64) && (nameOfField.charAt(2) < 91))  // after is a capital letter expected to not addViolation to a field called isotherm or so
 
         {
-	       	 if(!(t.getType().getName().equals("boolean")))
+	       	 if(t != null & !(t.getType().getName().equals("boolean")))
 	       	 {
 	       		 addViolation(data, node);
 	       	 }
         }
-        
-        else if((nameOfField.startsWith("has") || nameOfField.startsWith("can")) && 
+
+        else if((nameOfField.startsWith("has") || nameOfField.startsWith("can")) && nameOfField.length() > 3 &&
         		(nameOfField.charAt(3) > 64) && (nameOfField.charAt(3) < 91))
         {
-	       	 if(!(t.getType().getName().equals("boolean")))
+	       	 if(t != null & !(t.getType().getName().equals("boolean")))
 	       	 {
 	       		 addViolation(data, node);
 	       	 }
         }
-        
-        else if((nameOfField.startsWith("have") || nameOfField.startsWith("will")) && 
+
+        else if((nameOfField.startsWith("have") || nameOfField.startsWith("will")) && nameOfField.length() > 4 &&
         		(nameOfField.charAt(4) > 64) && (nameOfField.charAt(4) < 91))
         {
-	       	 if(!(t.getType().getName().equals("boolean")))
+	       	 if(t != null & !(t.getType().getName().equals("boolean")))
 	       	 {
 	       		 addViolation(data, node);
 	       	 }
         }
-        
-        else if(nameOfField.startsWith("should") && 
+
+        else if(nameOfField.startsWith("should") && nameOfField.length() > 6 &&
         		(nameOfField.charAt(6) > 64) && (nameOfField.charAt(6) < 91))
         {
-	       	 if(!(t.getType().getName().equals("boolean")))
+	       	 if(t != null & !(t.getType().getName().equals("boolean")))
 	       	 {
 	       		 addViolation(data, node);
 	       	 }
         }
-               
+
         /***************************************************************************/
-         		 
+
         return super.visit(node,data);
     }
-    
-    
-    public Object visit(ASTLocalVariableDeclaration node, Object data) 
-    {	
+
+
+    @Override
+    public Object visit(ASTLocalVariableDeclaration node, Object data)
+    {
         String nameOfField = node.getVariableName();
-        ASTType t = (ASTType) node.jjtGetChild(0); // Is first child always ASTType
+        ASTType t = node.getFirstChildOfType(ASTType.class);
 
         /**************** Type Should Be Boolean As Name Suggests *********************/
-        
-        if(nameOfField.startsWith("is") && (nameOfField.charAt(2) > 64) && (nameOfField.charAt(2) < 91))  // after is a capital letter expected to not addViolation to a local variable called isotherm or so
+
+        if(nameOfField.startsWith("is") && nameOfField.length() > 2 && (nameOfField.charAt(2) > 64) && (nameOfField.charAt(2) < 91))  // after is a capital letter expected to not addViolation to a local variable called isotherm or so
 
         {
-	       	 if(!(t.getType().getName().equals("boolean")))
+	       	 if(t != null & !(t.getType().getName().equals("boolean")))
 	       	 {
 	       		 addViolation(data, node);
 	       	 }
         }
-        
-        else if((nameOfField.startsWith("has") || nameOfField.startsWith("can")) && 
+
+        else if((nameOfField.startsWith("has") || nameOfField.startsWith("can")) && nameOfField.length() > 3 &&
         		(nameOfField.charAt(3) > 64) && (nameOfField.charAt(3) < 91))
         {
-	       	 if(!(t.getType().getName().equals("boolean")))
+	       	 if(t != null & !(t.getType().getName().equals("boolean")))
 	       	 {
 	       		 addViolation(data, node);
 	       	 }
         }
-        
-        else if((nameOfField.startsWith("have") || nameOfField.startsWith("will")) && 
+
+        else if((nameOfField.startsWith("have") || nameOfField.startsWith("will")) && nameOfField.length() > 4 &&
         		(nameOfField.charAt(4) > 64) && (nameOfField.charAt(4) < 91))
         {
-	       	 if(!(t.getType().getName().equals("boolean")))
+	       	 if(t != null & !(t.getType().getName().equals("boolean")))
 	       	 {
 	       		 addViolation(data, node);
 	       	 }
         }
-        
-        else if(nameOfField.startsWith("should") && 
+
+        else if(nameOfField.startsWith("should") && nameOfField.length() > 6 &&
         		(nameOfField.charAt(6) > 64) && (nameOfField.charAt(6) < 91))
         {
-	       	 if(!(t.getType().getName().equals("boolean")))
+	       	 if(t != null & !(t.getType().getName().equals("boolean")))
 	       	 {
 	       		 addViolation(data, node);
 	       	 }
         }
-               
+
         /***************************************************************************/
-         		 
+
         return super.visit(node,data);
     }
-        
+
 }
--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/codestyle/MethodTypeAndNameIsInconsistentRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/codestyle/MethodTypeAndNameIsInconsistentRule.java
@@ -1,89 +1,92 @@
 
 package net.sourceforge.pmd.lang.java.rule.codestyle;
 
-import net.sourceforge.pmd.lang.java.ast.*;
-import net.sourceforge.pmd.lang.java.rule.*;
+import net.sourceforge.pmd.lang.java.ast.ASTMethodDeclaration;
+import net.sourceforge.pmd.lang.java.ast.ASTResultType;
+import net.sourceforge.pmd.lang.java.ast.ASTType;
+import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
 
-public class MethodTypeAndNameIsInconsistentRule extends AbstractJavaRule 
+public class MethodTypeAndNameIsInconsistentRule extends AbstractJavaRule
 {
-    public Object visit(ASTMethodDeclaration node, Object data) 
-    {	
+    @Override
+    public Object visit(ASTMethodDeclaration node, Object data)
+    {
         String nameOfMethod = node.getMethodName();
         ASTType t = null;
         ASTResultType rt = null;
         if(node.getResultType().jjtGetNumChildren() != 0) //for non-void methods
         {
-            t = (ASTType) node.getResultType().jjtGetChild(0); // Is first child always ASTType
+            t = node.getResultType().getFirstChildOfType(ASTType.class);
             rt = node.getResultType();
         }
         else
         {
-        	rt = node.getResultType();  // for void methods   	
+        	rt = node.getResultType();  // for void methods
         }
-        
+
         /**************** Should Return Boolean As Name Suggests *********************/
-        
-        if(nameOfMethod.startsWith("is") && (nameOfMethod.charAt(2) > 64) && (nameOfMethod.charAt(2) < 91))  // after is a capital letter expected to not addViolation to a method called isotherm or so
+
+        if(nameOfMethod.startsWith("is") && nameOfMethod.length() > 2 && (nameOfMethod.charAt(2) > 64) && (nameOfMethod.charAt(2) < 91))  // after is a capital letter expected to not addViolation to a method called isotherm or so
 
         {
-	       	 if(!(t.getType().getName().equals("boolean")))
+	       	 if(t != null && !(t.getType().getName().equals("boolean")))
 	       	 {
 	       		 addViolation(data, node);
 	       	 }
         }
-        
-        else if((nameOfMethod.startsWith("has") || nameOfMethod.startsWith("can")) && 
+
+        else if((nameOfMethod.startsWith("has") || nameOfMethod.startsWith("can")) && nameOfMethod.length() > 3 &&
         		(nameOfMethod.charAt(3) > 64) && (nameOfMethod.charAt(3) < 91))
         {
-	       	 if(!(t.getType().getName().equals("boolean")))
+	       	 if(t != null && !(t.getType().getName().equals("boolean")))
 	       	 {
 	       		 addViolation(data, node);
 	       	 }
         }
-        
-        else if((nameOfMethod.startsWith("have") || nameOfMethod.startsWith("will")) && 
+
+        else if((nameOfMethod.startsWith("have") || nameOfMethod.startsWith("will")) && nameOfMethod.length() > 4 &&
         		(nameOfMethod.charAt(4) > 64) && (nameOfMethod.charAt(4) < 91))
         {
-	       	 if(!(t.getType().getName().equals("boolean")))
+	       	 if(t != null && !(t.getType().getName().equals("boolean")))
 	       	 {
 	       		 addViolation(data, node);
 	       	 }
         }
-        
-        else if(nameOfMethod.startsWith("should") && 
+
+        else if(nameOfMethod.startsWith("should") && nameOfMethod.length() > 6 &&
         		(nameOfMethod.charAt(6) > 64) && (nameOfMethod.charAt(6) < 91))
         {
-	       	 if(!(t.getType().getName().equals("boolean")))
+	       	 if(t != null && !(t.getType().getName().equals("boolean")))
 	       	 {
 	       		 addViolation(data, node);
 	       	 }
         }
-        
-        
+
+
         /***************************************************************************/
-        
+
         /**************** Should Return Void As Name Suggests *********************/
-        
-        if(nameOfMethod.startsWith("set") && (nameOfMethod.charAt(3) > 64) && (nameOfMethod.charAt(3) < 91))  // after set a capital letter expected to not addViolation to a method called settings or so
+
+        if(nameOfMethod.startsWith("set") && nameOfMethod.length() > 3 && (nameOfMethod.charAt(3) > 64) && (nameOfMethod.charAt(3) < 91))  // after set a capital letter expected to not addViolation to a method called settings or so
         {
-	       	 if(!(rt.isVoid())) //set method shouldnt return any type except void linguistically
+	       	 if(t != null && !(rt.isVoid())) //set method shouldnt return any type except void linguistically
 	       	 {
 	       		 addViolation(data, node);
 	       	 }
         }
-        
+
         /***************************************************************************/
 
         /******* Should Return A Type As Name Suggests But It Returns void *********/
-        
-        if(nameOfMethod.startsWith("get") && (nameOfMethod.charAt(3) > 64) && (nameOfMethod.charAt(3) < 91))  // after get a capital letter expected to not addViolation to a method called getaways or so
+
+        if(nameOfMethod.startsWith("get") && nameOfMethod.length() > 3 && (nameOfMethod.charAt(3) > 64) && (nameOfMethod.charAt(3) < 91))  // after get a capital letter expected to not addViolation to a method called getaways or so
         {
 	       	 if(rt.isVoid()) //get method shouldnt return void linguistically
 	       	 {
 	       		 addViolation(data, node);
 	       	 }
         }
-        
+
         if(nameOfMethod.contains("To"))  // To in the middle somewhere
         {
 	       	 if(rt.isVoid()) //a transform method shouldnt return void linguistically
@@ -91,8 +94,8 @@ public class MethodTypeAndNameIsInconsistentRule extends AbstractJavaRule
 	       		 addViolation(data, node);
 	       	 }
         }
-        
-        else if(nameOfMethod.startsWith("to") && (nameOfMethod.charAt(2) > 64) && (nameOfMethod.charAt(2) < 91))  // after to a capital letter expected to not addViolation to a method called tokenize or so
+
+        else if(nameOfMethod.startsWith("to") && nameOfMethod.length() > 2 && (nameOfMethod.charAt(2) > 64) && (nameOfMethod.charAt(2) < 91))  // after to a capital letter expected to not addViolation to a method called tokenize or so
         {
 	       	 if(rt.isVoid()) //a transform method shouldnt return void linguistically
 	       	 {
@@ -101,7 +104,7 @@ public class MethodTypeAndNameIsInconsistentRule extends AbstractJavaRule
         }
 
         /***************************************************************************/
-         		 
+
         return super.visit(node,data);
     }
 }
