707781600f84a483b489aeaff659f8e8551921fb
Andreas Dangel <andreas.dangel@adangel.org>
2018-07-29 19:50:53 +0200

    Use openjfx dependencies from maven central
    * Can use the release flag of maven-compiler-plugin again
    * Make the openjfx dependencies as provided, so that they
      don't end up in pmd-bin-*.zip

--- pmd-dist/src/main/scripts/run.sh
+++ pmd-dist/src/main/scripts/run.sh
@@ -74,34 +74,6 @@ check_lib_dir() {
   fi
 }
 
-jre_specific_vm_options() {
-  full_ver=$(java -version 2>&1)
-  # java_ver is eg "18" for java 1.8, "90" for java 9.0, "100" for java 10.0.x
-  java_ver=$(echo $full_ver | sed -n '{
-      # replace early access versions, e.g. 11-ea with 11.0.0
-      s/-ea/.0.0/
-      # replace versions such as 10 with 10.0.0
-      s/version "\([0-9]\{1,\}\)"/version "\1.0.0"/
-      # extract the major and minor parts of the version
-      s/^.* version "\(.*\)\.\(.*\)\..*".*$/\1\2/p
-  }')
-  options=""
-
-  if [ "$java_ver" -ge 90 ] && [ "${APPNAME}" = "designer" ]
-  then # open internal module of javafx to reflection (for our TreeViewWrapper)
-    options="--add-opens javafx.controls/javafx.scene.control.skin=ALL-UNNAMED"
-    # The rest here is for RichtextFX
-    options+=" --add-opens javafx.graphics/javafx.scene.text=ALL-UNNAMED"
-    options+=" --add-opens javafx.graphics/com.sun.javafx.scene.text=ALL-UNNAMED"
-    options+=" --add-opens javafx.graphics/com.sun.javafx.text=ALL-UNNAMED"
-    options+=" --add-opens javafx.graphics/com.sun.javafx.geom=ALL-UNNAMED"
-    # Warn of remaining illegal accesses
-    options+=" --illegal-access=warn"
-  fi
-
-  echo $options
-}
-
 readonly APPNAME="${1}"
 if [ -z "${APPNAME}" ]; then
     usage
@@ -156,5 +128,5 @@ cygwin_paths
 
 java_heapsize_settings
 
-java ${HEAPSIZE} $(jre_specific_vm_options) -cp "${classpath}" "${CLASSNAME}" "$@"
+java ${HEAPSIZE} -cp "${classpath}" "${CLASSNAME}" "$@"
 
