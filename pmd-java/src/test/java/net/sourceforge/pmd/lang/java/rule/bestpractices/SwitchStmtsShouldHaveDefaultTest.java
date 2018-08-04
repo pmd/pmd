92ce218c424673c207b17543f5840fdd10b65c2b
Andreas Dangel <andreas.dangel@adangel.org>
2018-08-04 19:21:17 +0200

    [java] Split the rule tests for best practices

---
+++ pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/bestpractices/SwitchStmtsShouldHaveDefaultTest.java
@@ -0,0 +1,9 @@
+/**
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
+package net.sourceforge.pmd.lang.java.rule.bestpractices;
+
+public class SwitchStmtsShouldHaveDefaultTest extends BestPracticesRulesTest {
+    // no additional unit tests
+}
