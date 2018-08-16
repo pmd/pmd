ec011cc885453fc7868332ee1975ce526d26338a
Andreas Dangel <andreas.dangel@microdoc.com>
2018-08-16 17:19:22 +0200

    Add additional test cases for expressions in where clauses

---
+++ pmd-plsql/src/test/java/net/sourceforge/pmd/lang/plsql/ast/WhereClauseTest.java
@@ -0,0 +1,21 @@
+/**
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
+package net.sourceforge.pmd.lang.plsql.ast;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import net.sourceforge.pmd.lang.plsql.AbstractPLSQLParserTst;
+
+public class WhereClauseTest extends AbstractPLSQLParserTst {
+
+    @Test
+    @Ignore
+    public void testFunctionCall() throws Exception {
+        String code = IOUtils.toString(this.getClass().getResourceAsStream("WhereClause.pls"));
+        ASTInput input = parsePLSQL(code);
+    }
+}
