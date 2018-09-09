fe83b9f5a4ce9cb89fbad89fd6dc85b2d862fcd7
Andreas Dangel <andreas.dangel@adangel.org>
2018-09-09 10:54:21 +0200

    [java] Split the rule tests for error prone

---
+++ pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/errorprone/InvalidSlf4jMessageFormatTest.java
@@ -0,0 +1,9 @@
+/**
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
+package net.sourceforge.pmd.lang.java.rule.errorprone;
+
+public class InvalidSlf4jMessageFormatTest extends ErrorProneRulesTest {
+    // no additional unit tests
+}
