3185956f75b6e94efb38ab7fb33a72c021b7c68b
Andreas Dangel <andreas.dangel@adangel.org>
2018-09-09 10:47:52 +0200

    [java] Split the rule tests for design

--- pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/design/DesignRulesTest.java
+++ pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/design/DesignRulesTest.java
@@ -11,7 +11,7 @@ import net.sourceforge.pmd.testframework.SimpleAggregatorTst;
 /**
  * Tests all the rules, that are in the design category.
  */
-public class DesignRulesTest extends SimpleAggregatorTst {
+public abstract class DesignRulesTest extends SimpleAggregatorTst {
 
     private static final String RULESET = "category/java/design.xml";
 
@@ -21,72 +21,11 @@ public class DesignRulesTest extends SimpleAggregatorTst {
         return rule;
     }
 
+    // missing rule: PositionalIteratorRule
+    // missing rule: TooManyHttpFilter
+
     @Override
     public void setUp() {
-        addRule(RULESET, "AbstractClassWithoutAnyMethod");
-        addRule(RULESET, "AvoidCatchingGenericException");
-        addRule(RULESET, "AvoidDeeplyNestedIfStmts");
-        addRule(RULESET, "AvoidRethrowingException");
-        addRule(RULESET, "AvoidThrowingNewInstanceOfSameException");
-        addRule(RULESET, "AvoidThrowingNullPointerException");
-        addRule(RULESET, "AvoidThrowingRawExceptionTypes");
-        addRule(RULESET, "ClassWithOnlyPrivateConstructorsShouldBeFinal");
-        addRule(RULESET, "CollapsibleIfStatements");
-        addRule(RULESET, "CouplingBetweenObjects");
-        addRule(RULESET, "CyclomaticComplexity");
-        addRule(RULESET, "DataClass");
-        addRule(RULESET, "DoNotExtendJavaLangError");
-        addRule(RULESET, "ExceptionAsFlowControl");
-        addRule(RULESET, "ExcessiveClassLength");
-        addRule(RULESET, "ExcessiveImports");
-        addRule(RULESET, "ExcessiveMethodLength");
-        addRule(RULESET, "ExcessiveParameterList");
-        addRule(RULESET, "ExcessivePublicCount");
-        addRule(RULESET, "FinalFieldCouldBeStatic");
-        addRule(RULESET, "GodClass");
-        addRule(RULESET, "ImmutableField");
-        addRule(RULESET, "LawOfDemeter");
-        addRule(RULESET, "LogicInversion");
-        addRule(RULESET, "LoosePackageCoupling");
-        addRule(RULESET, "ModifiedCyclomaticComplexity");
-        addRule(RULESET, "NcssConstructorCount");
-        addRule(RULESET, "NcssCount");
-        addRule(RULESET, "NcssMethodCount");
-        addRule(RULESET, "NcssTypeCount");
-        addRule(RULESET, "NPathComplexity");
-        addRule(RULESET, "SignatureDeclareThrowsException");
-        addRule(RULESET, "SimplifiedTernary");
-        addRule(RULESET, "SimplifyBooleanAssertion");
-        addRule(RULESET, "SimplifyBooleanExpressions");
-        addRule(RULESET, "SimplifyBooleanReturns");
-        addRule(RULESET, "SimplifyConditional");
-        addRule(RULESET, "SingularField");
-        addRule(RULESET, "StdCyclomaticComplexity");
-        addRule(RULESET, "SwitchDensity");
-        addRule(RULESET, "TooManyFields");
-        addRule(RULESET, "TooManyMethods");
-        addRule(RULESET, "UselessOverridingMethod");
-        addRule(RULESET, "UseObjectForClearerAPI");
-        addRule(RULESET, "UseUtilityClass");
-
-        // addRule(RULESET, "PositionalIteratorRule"); This rule does not yet
-        // exist
-        // addRule(RULESET, "TooManyHttpFilter"); This rule does not yet exist
-    }
-    
-    public static class Throwable extends java.lang.Throwable {
-        private static final long serialVersionUID = 1798165250043760600L;
-    }
-
-    public static class Exception extends java.lang.Throwable {
-        private static final long serialVersionUID = -2518308549741147689L;
-    }
-
-    public static class RuntimeException extends java.lang.Throwable {
-        private static final long serialVersionUID = 6341520923058239682L;
-    }
-
-    public static class Error extends java.lang.Throwable {
-        private static final long serialVersionUID = -6965602141393320558L;
+        addRule(RULESET, getClass().getSimpleName().replaceFirst("Test$", ""));
     }
 }
