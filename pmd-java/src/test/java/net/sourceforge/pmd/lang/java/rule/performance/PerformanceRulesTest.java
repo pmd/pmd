5db7b174d997f09445a17f3eabcb8fb11cf49444
Andreas Dangel <andreas.dangel@adangel.org>
2018-09-09 10:59:48 +0200

    [java] Split the rule tests for performance

--- pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/performance/PerformanceRulesTest.java
+++ pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/performance/PerformanceRulesTest.java
@@ -9,42 +9,12 @@ import net.sourceforge.pmd.testframework.SimpleAggregatorTst;
 /**
  * Rule tests for the performance category
  */
-public class PerformanceRulesTest extends SimpleAggregatorTst {
+public abstract class PerformanceRulesTest extends SimpleAggregatorTst {
 
     private static final String RULESET = "category/java/performance.xml";
 
     @Override
     public void setUp() {
-        addRule(RULESET, "AddEmptyString");
-        addRule(RULESET, "AppendCharacterWithChar");
-        addRule(RULESET, "AvoidArrayLoops");
-        addRule(RULESET, "AvoidFileStream");
-        addRule(RULESET, "AvoidInstantiatingObjectsInLoops");
-        addRule(RULESET, "AvoidUsingShortType");
-        addRule(RULESET, "BigIntegerInstantiation");
-        addRule(RULESET, "BooleanInstantiation");
-        addRule(RULESET, "ByteInstantiation");
-        addRule(RULESET, "ConsecutiveAppendsShouldReuse");
-        addRule(RULESET, "ConsecutiveLiteralAppends");
-        addRule(RULESET, "InefficientEmptyStringCheck");
-        addRule(RULESET, "InefficientStringBuffering");
-        addRule(RULESET, "InsufficientStringBufferDeclaration");
-        addRule(RULESET, "IntegerInstantiation");
-        addRule(RULESET, "LongInstantiation");
-        addRule(RULESET, "OptimizableToArrayCall");
-        addRule(RULESET, "RedundantFieldInitializer");
-        addRule(RULESET, "ShortInstantiation");
-        addRule(RULESET, "SimplifyStartsWith");
-        addRule(RULESET, "StringInstantiation");
-        addRule(RULESET, "StringToString");
-        addRule(RULESET, "TooFewBranchesForASwitchStatement");
-        addRule(RULESET, "UnnecessaryWrapperObjectCreation");
-        addRule(RULESET, "UseArrayListInsteadOfVector");
-        addRule(RULESET, "UseArraysAsList");
-        addRule(RULESET, "UseIndexOfChar");
-        addRule(RULESET, "UselessStringValueOf");
-        addRule(RULESET, "UseStringBufferForStringAppends");
-        addRule(RULESET, "UseStringBufferLength");
+        addRule(RULESET, getClass().getSimpleName().replaceFirst("Test$", ""));
     }
-
 }
