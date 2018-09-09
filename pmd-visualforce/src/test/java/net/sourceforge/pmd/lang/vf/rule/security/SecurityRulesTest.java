872413b85eaa59d541f91181136339f5abb4d7c4
Andreas Dangel <andreas.dangel@adangel.org>
2018-09-09 11:25:03 +0200

    [vf] Split the rule tests

--- pmd-visualforce/src/test/java/net/sourceforge/pmd/lang/vf/rule/security/SecurityRulesTest.java
+++ pmd-visualforce/src/test/java/net/sourceforge/pmd/lang/vf/rule/security/SecurityRulesTest.java
@@ -6,13 +6,12 @@ package net.sourceforge.pmd.lang.vf.rule.security;
 
 import net.sourceforge.pmd.testframework.SimpleAggregatorTst;
 
-public class SecurityRulesTest extends SimpleAggregatorTst {
+public abstract class SecurityRulesTest extends SimpleAggregatorTst {
 
     private static final String RULESET = "category/vf/security.xml";
 
     @Override
     public void setUp() {
-        addRule(RULESET, "VfCsrf");
-        addRule(RULESET, "VfUnescapeEl");
+        addRule(RULESET, getClass().getSimpleName().replaceFirst("Test$", ""));
     }
 }
---
+++ pmd-visualforce/src/test/java/net/sourceforge/pmd/lang/vf/rule/security/VfCsrfTest.java
@@ -0,0 +1,9 @@
+/**
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
+package net.sourceforge.pmd.lang.vf.rule.security;
+
+public class VfCsrfTest extends SecurityRulesTest {
+    // no additional unit tests
+}
