463e26834155eb3974c3570aa9957e7afa4d0405
Clément Fournier <clement.fournier76@gmail.com>
2018-10-22 22:56:11 +0200

    Remove unused rule classes

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/documentation/JavadocRule.java
+++
@@ -1,9 +0,0 @@
-/**
- * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
- */
-
-package net.sourceforge.pmd.lang.java.rule.documentation;
-
-public class JavadocRule extends AbstractCommentRule {
-
-}
