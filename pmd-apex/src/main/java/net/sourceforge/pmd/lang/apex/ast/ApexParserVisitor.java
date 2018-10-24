9af7ea18b5676db814006e448935dbd37c56fdd9
Andreas Dangel <andreas.dangel@microdoc.com>
2018-10-24 08:59:08 +0200

    Directly delegate to visit(AbstractApexNodeBase) Deprecate visit(ApexNode<?>)

--- pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/ast/ApexParserVisitor.java
+++ pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/ast/ApexParserVisitor.java
@@ -7,6 +7,11 @@ package net.sourceforge.pmd.lang.apex.ast;
 public interface ApexParserVisitor {
     Object visit(AbstractApexNodeBase node, Object data);
 
+    /**
+     * @deprecated This visit method will be removed with PMD 7.0.0. Use {@link #visit(AbstractApexNodeBase, Object)}
+     *             instead. This method would not visit all nodes, e.g. ASTFormalComment would not be covered.
+     */
+    @Deprecated // will be removed with PMD 7.0.0
     Object visit(ApexNode<?> node, Object data);
 
     Object visit(ASTAnnotation node, Object data);
