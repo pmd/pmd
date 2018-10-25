0d9080abfd7ad43594562e35557b2298da899202
Will Herrmann <will.herrmann@pearson.com>
2018-10-25 16:46:06 -0500

    Extracting ternary statement into private method

--- pmd-core/src/main/java/net/sourceforge/pmd/cpd/FileReporter.java
+++ pmd-core/src/main/java/net/sourceforge/pmd/cpd/FileReporter.java
@@ -6,6 +6,7 @@ package net.sourceforge.pmd.cpd;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -37,13 +38,14 @@ public class FileReporter {
     }
 
     public void report(String content) throws ReportException {
-        try {
-            try (OutputStream outputStream = reportFile == null ? System.out : new FileOutputStream(reportFile);
-                 Writer writer = new BufferedWriter(new OutputStreamWriter(outputStream, encoding))) {
-                writer.write(content);
-            }
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(getOutputStream(), encoding))) {
+            writer.write(content);
         } catch (IOException ioe) {
             throw new ReportException(ioe);
         }
+    }
+
+    private OutputStream getOutputStream() throws FileNotFoundException {
+        return reportFile == null ? System.out : new FileOutputStream(reportFile);
     }
 }
