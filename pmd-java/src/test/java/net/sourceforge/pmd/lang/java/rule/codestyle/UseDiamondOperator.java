8810e58a319df2452d5fc9436e916f0f68fd2638
hemanshu070 <hemanshu070@gmail.com>
2018-12-20 23:13:52 +0530

    This is the change regarding the usediamondoperator #1517

---
+++ pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/codestyle/UseDiamondOperator.java
@@ -0,0 +1,7 @@
+package net.sourceforge.pmd.lang.java.rule.codestyle;
+
+import net.sourceforge.pmd.testframework.PmdRuleTst;
+
+public class UseDiamondOperator extends PmdRuleTst {
+    // no additional unit tests
+}
