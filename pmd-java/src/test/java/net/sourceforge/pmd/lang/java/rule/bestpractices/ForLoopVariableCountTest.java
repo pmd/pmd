f35fab2774cd5a9c8f6efe49a948623455e4037b
Andreas Dangel <andreas.dangel@adangel.org>
2018-12-22 23:03:18 +0100

    Move new rule ForLoopVariableCount to bestpractices

--- pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/bestpractices/ForLoopVariableCountTest.java
+++ pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/bestpractices/ForLoopVariableCountTest.java
@@ -2,7 +2,7 @@
  * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
  */
 
-package net.sourceforge.pmd.lang.java.rule.codestyle;
+package net.sourceforge.pmd.lang.java.rule.bestpractices;
 
 import net.sourceforge.pmd.testframework.PmdRuleTst;
 
