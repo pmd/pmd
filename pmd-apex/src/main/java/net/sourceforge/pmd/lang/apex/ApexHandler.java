0fec3640a30bd92e6a2d2a0f31888365edaa36e7
Clément Fournier <clement.fournier76@gmail.com>
2019-01-06 03:17:00 +0100

    Implement for Apex

--- pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/ApexHandler.java
+++ pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/ApexHandler.java
@@ -5,20 +5,33 @@
 package net.sourceforge.pmd.lang.apex;
 
 import java.io.Writer;
+import java.util.Arrays;
+import java.util.List;
 
 import net.sourceforge.pmd.lang.AbstractLanguageVersionHandler;
 import net.sourceforge.pmd.lang.Parser;
 import net.sourceforge.pmd.lang.ParserOptions;
 import net.sourceforge.pmd.lang.VisitorStarter;
 import net.sourceforge.pmd.lang.XPathHandler;
+import net.sourceforge.pmd.lang.apex.ast.ASTMethod;
+import net.sourceforge.pmd.lang.apex.ast.ASTUserClassOrInterface;
 import net.sourceforge.pmd.lang.apex.ast.ApexNode;
 import net.sourceforge.pmd.lang.apex.ast.DumpFacade;
+import net.sourceforge.pmd.lang.apex.metrics.ApexMetricsComputer;
+import net.sourceforge.pmd.lang.apex.metrics.api.ApexClassMetricKey;
+import net.sourceforge.pmd.lang.apex.metrics.api.ApexOperationMetricKey;
 import net.sourceforge.pmd.lang.apex.multifile.ApexMultifileVisitorFacade;
 import net.sourceforge.pmd.lang.apex.rule.ApexRuleViolationFactory;
 import net.sourceforge.pmd.lang.ast.xpath.DefaultASTXPathHandler;
+import net.sourceforge.pmd.lang.metrics.LanguageMetricsProvider;
+import net.sourceforge.pmd.lang.metrics.internal.AbstractLanguageMetricsProvider;
 import net.sourceforge.pmd.lang.rule.RuleViolationFactory;
 
+
 public class ApexHandler extends AbstractLanguageVersionHandler {
+
+    private final ApexMetricsProvider myMetricsProvider = new ApexMetricsProvider();
+
 
     @Override
     public VisitorStarter getMultifileFacade() {
@@ -51,4 +64,31 @@ public class ApexHandler extends AbstractLanguageVersionHandler {
         return rootNode -> new DumpFacade().initializeWith(writer, prefix, recurse, (ApexNode<?>) rootNode);
     }
 
+
+    @Override
+    public LanguageMetricsProvider<ASTUserClassOrInterface<?>, ASTMethod> getLanguageMetricsProvider() {
+        return myMetricsProvider;
+    }
+
+
+    private static class ApexMetricsProvider extends AbstractLanguageMetricsProvider<ASTUserClassOrInterface<?>, ASTMethod> {
+
+        @SuppressWarnings("unchecked")
+        ApexMetricsProvider() {
+            // a wild double cast
+            super((Class<ASTUserClassOrInterface<?>>) (Object) ASTUserClassOrInterface.class, ASTMethod.class, ApexMetricsComputer.getInstance());
+        }
+
+
+        @Override
+        public List<ApexClassMetricKey> getAvailableTypeMetrics() {
+            return Arrays.asList(ApexClassMetricKey.values());
+        }
+
+
+        @Override
+        public List<ApexOperationMetricKey> getAvailableOperationMetrics() {
+            return Arrays.asList(ApexOperationMetricKey.values());
+        }
+    }
 }
