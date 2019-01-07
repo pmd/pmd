f922bcac0e682a87e54dfdd733c8a64b7ea3718b
Clément Fournier <clement.fournier76@gmail.com>
2019-01-07 12:23:46 +0100

    Finish Future approach

--- pmd-doc/src/main/java/net/sourceforge/pmd/docs/DeadLinksChecker.java
+++ pmd-doc/src/main/java/net/sourceforge/pmd/docs/DeadLinksChecker.java
@@ -15,21 +15,33 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import org.apache.commons.io.IOUtils;
+
 
 /**
  * Checks links to local pages for non-existing link-targets.
  */
 public class DeadLinksChecker {
+    private static final Logger LOG = Logger.getLogger(DeadLinksChecker.class.getName());
 
     private static final String CHECK_EXTERNAL_LINKS_PROPERTY = "pmd.doc.checkExternalLinks";
     private static final boolean CHECK_EXTERNAL_LINKS = Boolean.parseBoolean(System.getProperty(CHECK_EXTERNAL_LINKS_PROPERTY));
@@ -43,7 +55,7 @@ public class DeadLinksChecker {
 
     // list of link targets, where the link detection doesn't work
     private static final Pattern EXCLUDED_LINK_TARGETS = Pattern.compile(
-            "^pmd_userdocs_cli_reference\\.html.*" // anchors in the CLI reference are a plain HTML include
+        "^pmd_userdocs_cli_reference\\.html.*" // anchors in the CLI reference are a plain HTML include
     );
 
     // the link is actually pointing to a file in the pmd project
@@ -51,44 +63,38 @@ public class DeadLinksChecker {
 
     // don't check links to PMD bugs/issues/pull-requests  (performance optimization)
     private static final List<String> IGNORED_URL_PREFIXES = Collections.unmodifiableList(Arrays.asList(
-            "https://github.com/pmd/pmd/issues/",
-            "https://github.com/pmd/pmd/pull/",
-            "https://sourceforge.net/p/pmd/bugs/"   
+        "https://github.com/pmd/pmd/issues/",
+        "https://github.com/pmd/pmd/pull/",
+        "https://sourceforge.net/p/pmd/bugs/"
     ));
 
     // prevent checking the same link multiple times
-    private final Map<String, Integer> linkResultCache = new HashMap<>();
+    private final Map<String, Integer> linkResultCache = new ConcurrentHashMap<>();
 
-    public static void main(String[] args) throws IOException {
-        final Path rootDirectory = Paths.get(args[0]).resolve("..").toRealPath();
+    private final ExecutorService executorService = Executors.newCachedThreadPool();
 
-        DeadLinksChecker deadLinksChecker = new DeadLinksChecker();
-        deadLinksChecker.checkDeadLinks(rootDirectory);
-    }
 
     public void checkDeadLinks(Path rootDirectory) {
         final Path pagesDirectory = rootDirectory.resolve("docs/pages");
 
         if (!Files.isDirectory(pagesDirectory)) {
-            System.err.println("can't check for dead links, didn't find \"pages\" directory at: " + pagesDirectory);
+            LOG.warning("can't check for dead links, didn't find \"pages\" directory at: " + pagesDirectory);
             System.exit(1);
         }
 
         // read all .md-files in the pages directory
         final List<Path> mdFiles = listMdFiles(pagesDirectory);
 
-
+        // Stores file path to the future deadlinks. If a future evaluates to null, the link is not dead
+        final Map<Path, List<Future<String>>> fileToDeadLinks = new HashMap<>();
         // make a list of all valid link targets
         final Set<String> htmlPages = extractLinkTargets(mdFiles);
 
-        // buffer the report to not have it broken up by error messages while checking links
-        final List<String> deadLinksReport = new ArrayList<>();
-
         // scan all .md-files for dead local links
-        Path errorFile = null;
         int scannedFiles = 0;
         int foundExternalLinks = 0;
         int checkedExternalLinks = 0;
+
         for (Path mdFile : mdFiles) {
             final String pageContent = fileToString(mdFile);
             scannedFiles++;
@@ -99,12 +105,11 @@ public class DeadLinksChecker {
                 final String line = lines[index];
                 final int lineNo = index + 1;
 
-
                 final Matcher matcher = LOCAL_LINK_PATTERN.matcher(line);
                 linkCheck:
                 while (matcher.find()) {
-                    String linkTarget = matcher.group(1);
-                    linkTarget = linkTarget.replaceAll("^/+", ""); // remove the leading "/"
+                    final String linkText = matcher.group();
+                    final String linkTarget = matcher.group(1).replaceAll("^/+", ""); // remove the leading "/"
                     boolean linkOk;
 
                     if (linkTarget.startsWith(LOCAL_FILE_PREFIX)) {
@@ -116,25 +121,34 @@ public class DeadLinksChecker {
                         final Path localFile = rootDirectory.resolve(localLinkPart);
                         linkOk = Files.isRegularFile(localFile);
                         if (!linkOk) {
-                            System.err.println("local file not found: " + localFile);
-                            System.err.println("  linked by: " + linkTarget);
+                            LOG.warning("local file not found: " + localFile);
+                            LOG.warning("  linked by: " + linkTarget);
                         }
 
                     } else if (linkTarget.startsWith("http://") || linkTarget.startsWith("https://")) {
                         foundExternalLinks++;
-                        for (String ignoredUrlPrefix : IGNORED_URL_PREFIXES) {
-                            if (linkTarget.startsWith(ignoredUrlPrefix)) {
-                                System.out.println("not checking link: " + linkTarget);
-                                continue linkCheck;
-                            }
-                        }
+
                         if (!CHECK_EXTERNAL_LINKS) {
-                            System.out.println("ignoring check of external url: " + linkTarget);
+                            LOG.finer("ignoring check of external url: " + linkTarget);
                             continue;
                         }
 
+                        for (String ignoredUrlPrefix : IGNORED_URL_PREFIXES) {
+                            if (linkTarget.startsWith(ignoredUrlPrefix)) {
+                                LOG.finer("not checking link: " + linkTarget);
+                                continue linkCheck;
+                            }
+                        }
+
                         checkedExternalLinks++;
-                        linkOk = checkExternalLink(linkTarget);
+                        linkOk = true;
+
+                        // process in parallel
+                        // It's important not to use the matcher in the callable!
+                        // It may be exhausted at the time of execution
+                        addDeadLink(fileToDeadLinks, mdFile, executorService.submit(() -> externalLinkIsDead(linkTarget)
+                                                                         ? String.format("%8d: %s", lineNo, linkText)
+                                                                         : null));
 
                     } else {
                         // ignore local anchors
@@ -151,18 +165,16 @@ public class DeadLinksChecker {
                     }
 
                     if (!linkOk) {
-                        if (errorFile == null) {
-                            deadLinksReport.add("Found dead link(s):");
-                        }
-                        if (!mdFile.equals(errorFile)) {
-                            deadLinksReport.add(String.valueOf(mdFile));
-                            errorFile = mdFile;
-                        }
-                        deadLinksReport.add(String.format("%8d: %s", lineNo, matcher.group()));
+                        addDeadLink(fileToDeadLinks, mdFile, new FutureTask<>(() -> String.format("%8d: %s", lineNo, linkText)));
                     }
                 }
             }
         }
+
+        executorService.shutdown();
+
+        // all appenders have been pushed
+
         System.out.println("Scanned " + scannedFiles + " files for dead links.");
         System.out.println("  Found " + foundExternalLinks + " external links, " + checkedExternalLinks + " of those where checked.");
 
@@ -170,15 +182,51 @@ public class DeadLinksChecker {
             System.out.println("External links weren't checked, set -D" + CHECK_EXTERNAL_LINKS_PROPERTY + "=true to enable it.");
         }
 
-        if (!deadLinksReport.isEmpty()) {
-            for (String line : deadLinksReport) {
-                System.err.println(line);
-            }
-            throw new AssertionError("dead links detected");
+        Map<Path, List<String>> joined = joinFutures(fileToDeadLinks);
+
+        if (joined.isEmpty()) {
+            System.out.println("No errors found!");
         } else {
-            System.out.println("no errors found!");
+            System.err.println("Found dead link(s):");
+            for (Path file : joined.keySet()) {
+                System.err.println(rootDirectory.relativize(file).toString());
+                joined.get(file).forEach(System.err::println);
+            }
+            throw new AssertionError("Dead links detected");
         }
     }
+
+
+    private Map<Path, List<String>> joinFutures(Map<Path, List<Future<String>>> map) {
+        Map<Path, List<String>> joined = new HashMap<>();
+
+        for (Path p : map.keySet()) {
+
+            List<String> evaluatedResult = map.get(p).stream()
+                                              .map(f -> {
+                                                  try {
+                                                      return f.get();
+                                                  } catch (InterruptedException | ExecutionException e) {
+                                                      e.printStackTrace();
+                                                      return null;
+                                                  }
+                                              })
+                                              .filter(Objects::nonNull)
+                                              .sorted(Comparator.naturalOrder())
+                                              .collect(Collectors.toList());
+
+            if (!evaluatedResult.isEmpty()) {
+                joined.put(p, evaluatedResult);
+            }
+        }
+        return joined;
+    }
+
+
+    private void addDeadLink(Map<Path, List<Future<String>>> fileToDeadLinks, Path file, Future<String> line) {
+        fileToDeadLinks.computeIfAbsent(file, k -> new ArrayList<>()).add(line);
+    }
+
 
     private Set<String> extractLinkTargets(List<Path> mdFiles) {
         final Set<String> htmlPages = new HashSet<>();
@@ -192,7 +240,7 @@ public class DeadLinksChecker {
             }
 
             final String pageUrl = permalinkMatcher.group(1)
-                    .replaceAll("^/+", ""); // remove the leading "/"
+                                                   .replaceAll("^/+", ""); // remove the leading "/"
 
             // add the root page
             htmlPages.add(pageUrl);
@@ -201,9 +249,9 @@ public class DeadLinksChecker {
             final Matcher captionMatcher = MD_CAPTION.matcher(pageContent);
             while (captionMatcher.find()) {
                 final String anchor = captionMatcher.group(1)
-                        .toLowerCase(Locale.ROOT)
-                        .replaceAll("[^a-z0-9_]+", "-") // replace all non-alphanumeric characters with dashes
-                        .replaceAll("^-+|-+$", ""); // trim leading or trailing dashes
+                                                    .toLowerCase(Locale.ROOT)
+                                                    .replaceAll("[^a-z0-9_]+", "-") // replace all non-alphanumeric characters with dashes
+                                                    .replaceAll("^-+|-+$", ""); // trim leading or trailing dashes
 
                 htmlPages.add(pageUrl + "#" + anchor);
             }
@@ -211,18 +259,20 @@ public class DeadLinksChecker {
         return htmlPages;
     }
 
+
     private List<Path> listMdFiles(Path pagesDirectory) {
         final List<Path> mdFiles = new ArrayList<>();
         try {
             Files.walk(pagesDirectory)
-                    .filter(Files::isRegularFile)
-                    .filter(path -> path.toString().endsWith(".md"))
-                    .forEach(mdFiles::add);
+                 .filter(Files::isRegularFile)
+                 .filter(path -> path.toString().endsWith(".md"))
+                 .forEach(mdFiles::add);
         } catch (IOException ex) {
             throw new RuntimeException("error listing files in " + pagesDirectory, ex);
         }
         return mdFiles;
     }
+
 
     private String fileToString(Path mdFile) {
         try (InputStream inputStream = Files.newInputStream(mdFile)) {
@@ -232,11 +282,12 @@ public class DeadLinksChecker {
         }
     }
 
-    private boolean checkExternalLink(String url) {
-        System.out.println("checking url: " + url + " ...");
+
+    private boolean externalLinkIsDead(String url) {
+        LOG.fine("checking url: " + url + " ...");
         if (linkResultCache.containsKey(url)) {
-            System.out.println("response: HTTP " + linkResultCache.get(url) + " (CACHED)");
-            return linkResultCache.get(url) < 400;
+            LOG.fine("response: HTTP " + linkResultCache.get(url) + " (CACHED) on " + url);
+            return linkResultCache.get(url) >= 400;
         }
 
         try {
@@ -252,17 +303,27 @@ public class DeadLinksChecker {
                 response += ", Location: " + httpURLConnection.getHeaderField("Location");
             }
 
-            System.out.println("response: " + response);
+            LOG.fine("response: " + response + " on " + url);
             linkResultCache.put(url, responseCode);
 
             // success (HTTP 2xx) or redirection (HTTP 3xx)
-            return responseCode < 400;
+            return responseCode >= 400;
 
         } catch (IOException ex) {
-            System.out.println("response: " + ex.getClass().getName() + " " + ex.getMessage());
+            LOG.fine("response: " + ex.getClass().getName() + " on " + url + " : " + ex.getMessage());
             linkResultCache.put(url, 599);
-            return false;
+            return true;
         }
     }
+
+
+    public static void main(String[] args) throws IOException {
+        final Path rootDirectory = Paths.get(args[0]).resolve("..").toRealPath();
+
+        DeadLinksChecker deadLinksChecker = new DeadLinksChecker();
+        deadLinksChecker.checkDeadLinks(rootDirectory);
+    }
+
+
 
 }
