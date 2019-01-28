6f2cd5fe29fdfa92a03c0dc3db998684b7704c41
Shubham <shubham.null@gmail.com>
2019-01-28 23:02:59 +0530

    Test cases added
    Updated testcases.

---
+++ pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/bestpractices/unusedimports/Base.java
@@ -0,0 +1,10 @@
+/**
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
+package net.sourceforge.pmd.lang.java.rule.bestpractices.unusedimports;
+
+public class Base {
+    void method(java.io.File file) {
+    }
+}
