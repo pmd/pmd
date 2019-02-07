6a81a63dd84ea472f4a8d3764e30accb9124a686
IDoCodingStuffs <sahinvictor@gmail.com>
2019-02-07 12:45:21 -0500

    Initial stub implementation for basic example mentioned in issue

---
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/codestyle/ArrayDeclarationVerbosenessRule.java
@@ -0,0 +1,19 @@
+package net.sourceforge.pmd.lang.java.rule.codestyle;
+
+import net.sourceforge.pmd.lang.java.ast.ASTLocalVariableDeclaration;
+import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
+
+public class ArrayDeclarationVerbosenessRule extends AbstractJavaRule {
+
+  @Override
+  public Object visit(ASTLocalVariableDeclaration node, Object data) {
+
+    if (node.isArray()) {
+      if (node.getImage().matches("(new).+\\{"))
+        addViolation(data, node);
+      return super.visit(node, data);
+    }
+
+    return super.visit(node, data);
+  }
+}
