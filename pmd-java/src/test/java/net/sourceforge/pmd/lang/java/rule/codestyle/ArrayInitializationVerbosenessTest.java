adabe19ec441e2008e4d1e2da8871856c9e1bf4c
IDoCodingStuffs <sahinvictor@gmail.com>
2019-02-08 20:38:42 -0500

    Rename test class

--- pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/codestyle/ArrayInitializationVerbosenessTest.java
+++ pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/codestyle/ArrayInitializationVerbosenessTest.java
@@ -6,6 +6,6 @@ package net.sourceforge.pmd.lang.java.rule.codestyle;
 
 import net.sourceforge.pmd.testframework.PmdRuleTst;
 
-public class ArrayInitializationVerbosenessRuleTest extends PmdRuleTst {
+public class ArrayInitializationVerbosenessTest extends PmdRuleTst {
   // no additional unit tests
 }
