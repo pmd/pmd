4f9c8f7f6086ab8e7159662089d144052c1303a5
Clément Fournier <clement.fournier76@gmail.com>
2019-02-16 12:02:21 +0100

    Add tests

--- pmd-java/src/test/kotlin/net/sourceforge/pmd/lang/java/ast/ASTClassOrInterfaceTypeTest.kt
+++ pmd-java/src/test/kotlin/net/sourceforge/pmd/lang/java/ast/ASTClassOrInterfaceTypeTest.kt
@@ -10,7 +10,7 @@ import io.kotlintest.specs.FunSpec
 class ASTClassOrInterfaceTypeTest : FunSpec({
 
 
-    testGroup("Test non-recursive ClassOrInterfaceTypes") {
+    testGroup("Test non-recursive COITs") {
 
         "java.util.List" should matchType<ASTClassOrInterfaceType> {
             it.typeImage shouldBe "java.util.List"
@@ -23,6 +23,77 @@ class ASTClassOrInterfaceTypeTest : FunSpec({
                 child<ASTTypeArgument> {
                     child<ASTClassOrInterfaceType> {
                         it.typeImage shouldBe "F"
+                    }
+                }
+            }
+        }
+    }
+
+    testGroup("Test recursive COITs") {
+
+        "java.util.Map.@Foo Entry<K, V>" should matchType<ASTClassOrInterfaceType> {
+            it.typeImage shouldBe "java.util.Map.Entry"
+            it.image shouldBe "Entry"
+
+            it.leftHandSide shouldBePresent child {
+                it.typeImage shouldBe "java.util.Map"
+            }
+
+            child<ASTAnnotation> {
+                it.annotationName shouldBe "Foo"
+
+                child<ASTMarkerAnnotation> {
+                    child<ASTName> {  }
+                }
+            }
+
+            child<ASTTypeArguments> {
+                child<ASTTypeArgument> {
+                    child<ASTClassOrInterfaceType> {
+                        it.typeImage shouldBe "K"
+                    }
+                }
+
+                child<ASTTypeArgument> {
+                    child<ASTClassOrInterfaceType> {
+                        it.typeImage shouldBe "V"
+                    }
+                }
+            }
+        }
+
+        "Foo<K>.@A Bar.Brew<V>" should matchType<ASTClassOrInterfaceType> {
+
+            it.typeImage shouldBe "Foo.Bar.Brew"
+
+            it.leftHandSide shouldBePresent child {
+                it.typeImage shouldBe "Foo.Bar"
+
+                it.leftHandSide shouldBePresent child {
+                    it.typeImage shouldBe "Foo"
+
+                    child<ASTTypeArguments> {
+                        child<ASTTypeArgument> {
+                            child<ASTClassOrInterfaceType> {
+                                it.typeImage shouldBe "K"
+                            }
+                        }
+                    }
+                }
+
+                child<ASTAnnotation> {
+                    it.annotationName shouldBe "A"
+
+                    child<ASTMarkerAnnotation> {
+                        child<ASTName> { }
+                    }
+                }
+            }
+
+            child<ASTTypeArguments> {
+                child<ASTTypeArgument> {
+                    child<ASTClassOrInterfaceType> {
+                        it.typeImage shouldBe "V"
                     }
                 }
             }
