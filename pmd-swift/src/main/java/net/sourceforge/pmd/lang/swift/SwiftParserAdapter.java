47bc813cafc35b1df4a7124465bf0b96da42cd55
lsoncini <lsoncini@itba.edu.ar>
2019-02-25 14:33:09 -0300

    added AntlrBaseParser adapter and SwiftParserAdapter

---
+++ pmd-swift/src/main/java/net/sourceforge/pmd/lang/swift/SwiftParserAdapter.java
@@ -0,0 +1,43 @@
+/**
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
+package net.sourceforge.pmd.lang.swift;
+
+import java.io.IOException;
+import java.io.Reader;
+
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Lexer;
+import org.antlr.v4.runtime.Parser;
+
+import net.sourceforge.pmd.lang.ParserOptions;
+import net.sourceforge.pmd.lang.antlr.AntlrBaseParser;
+import net.sourceforge.pmd.lang.swift.antlr4.SwiftLexer;
+import net.sourceforge.pmd.lang.swift.antlr4.SwiftParser;
+
+/**
+ * Adapter for the SwiftParser.
+ */
+public class SwiftParserAdapter extends AntlrBaseParser {
+
+    public SwiftParserAdapter(final ParserOptions parserOptions) {
+        super(parserOptions);
+    }
+
+    @Override
+    protected Lexer getLexer(final Reader source) throws IOException {
+        return new SwiftLexer(CharStreams.fromReader(source));
+    }
+
+    @Override
+    protected Parser getParser(final Lexer lexer) {
+        return new SwiftParser(new CommonTokenStream(lexer));
+    }
+
+    @Override
+    public boolean canParse() {
+        return true;
+    }
+}
