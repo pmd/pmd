18296125a38b3b13c58c39f9746054ec987fdbdc
David Burström <davidburstrom@spotify.com>
2019-03-10 10:36:31 +0100

    Rename test class so that it can be picked up by Surefire

--- pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/bestpractices/WhileLoopWithLiteralBooleanTest.java
+++ pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/bestpractices/WhileLoopWithLiteralBooleanTest.java
@@ -6,6 +6,6 @@ package net.sourceforge.pmd.lang.java.rule.bestpractices;
 
 import net.sourceforge.pmd.testframework.PmdRuleTst;
 
-public class WhileLoopWithLiteralBoolean extends PmdRuleTst {
+public class WhileLoopWithLiteralBooleanTest extends PmdRuleTst {
     // no additional unit tests
 }
