430bd23a83c58ad7cacf02170ae2b111e03818d6
Andreas Dangel <andreas.dangel@microdoc.com>
2019-03-12 16:40:49 +0100

    [plsql] AvoidTabCharacter: Add property "eachLine"

--- pmd-plsql/src/main/java/net/sourceforge/pmd/lang/plsql/rule/codestyle/AvoidTabCharacterRule.java
+++ pmd-plsql/src/main/java/net/sourceforge/pmd/lang/plsql/rule/codestyle/AvoidTabCharacterRule.java
@@ -10,24 +10,37 @@ import java.io.StringReader;
 
 import net.sourceforge.pmd.lang.plsql.ast.ASTInput;
 import net.sourceforge.pmd.lang.plsql.rule.AbstractPLSQLRule;
+import net.sourceforge.pmd.properties.PropertyDescriptor;
+import net.sourceforge.pmd.properties.PropertyFactory;
 
 public class AvoidTabCharacterRule extends AbstractPLSQLRule {
 
+    private static final PropertyDescriptor<Boolean> EACH_LINE = PropertyFactory.booleanProperty("eachLine")
+            .desc("Whether to report each line with a tab character or only the first line")
+            .defaultValue(false)
+            .build();
+
     public AvoidTabCharacterRule() {
+        definePropertyDescriptor(EACH_LINE);
         addRuleChainVisit(ASTInput.class);
     }
 
     @Override
     public Object visit(ASTInput node, Object data) {
+        boolean eachLine = getProperty(EACH_LINE);
+
         try (BufferedReader in = new BufferedReader(new StringReader(node.getSourcecode()))) {
             String line;
             int lineNumber = 0;
             while ((line = in.readLine()) != null) {
                 lineNumber++;
                 if (line.indexOf('\t') != -1) {
-                    addViolationWithMessage(data, null, "Tab characters are not allowed. Use spaces for intendation",
+                    addViolationWithMessage(data, null, "Tab characters are not allowed. Use spaces for indentation",
                             lineNumber, lineNumber);
-                    break;
+
+                    if (!eachLine) {
+                        break;
+                    }
                 }
             }
         } catch (IOException e) {
