65cac51445f8160876705a18d00c84d70c3aa7be
Andreas Dangel <andreas.dangel@adangel.org>
2019-03-15 20:51:53 +0100

    [java] Add Java12 support

--- pmd-java/src/test/java/net/sourceforge/pmd/lang/java/ast/Java12Test.java
+++ pmd-java/src/test/java/net/sourceforge/pmd/lang/java/ast/Java12Test.java
@@ -34,6 +34,11 @@ public class Java12Test {
         ASTCompilationUnit compilationUnit = ParserTstUtil.parseAndTypeResolveJava("12",
                 loadSource("MultipleCaseLabels.java"));
         Assert.assertNotNull(compilationUnit);
+        ASTSwitchStatement switchStatement = compilationUnit.getFirstDescendantOfType(ASTSwitchStatement.class);
+        Assert.assertTrue(switchStatement.jjtGetChild(0) instanceof ASTExpression);
+        Assert.assertTrue(switchStatement.jjtGetChild(1) instanceof ASTSwitchLabel);
+        ASTSwitchLabel switchLabel = switchStatement.getFirstChildOfType(ASTSwitchLabel.class);
+        Assert.assertEquals(3, switchLabel.findChildrenOfType(ASTExpression.class).size());
     }
 
     @Test(expected = ParseException.class)
@@ -46,6 +51,23 @@ public class Java12Test {
         ASTCompilationUnit compilationUnit = ParserTstUtil.parseAndTypeResolveJava("12",
                 loadSource("SwitchRules.java"));
         Assert.assertNotNull(compilationUnit);
+        ASTSwitchStatement switchStatement = compilationUnit.getFirstDescendantOfType(ASTSwitchStatement.class);
+        Assert.assertTrue(switchStatement.jjtGetChild(0) instanceof ASTExpression);
+        Assert.assertTrue(switchStatement.jjtGetChild(1) instanceof ASTSwitchLabeledRule);
+        ASTSwitchLabeledRule switchLabeledRule = (ASTSwitchLabeledRule) switchStatement.jjtGetChild(1);
+        Assert.assertEquals(2, switchLabeledRule.jjtGetNumChildren());
+        Assert.assertTrue(switchLabeledRule.jjtGetChild(0) instanceof ASTSwitchLabel);
+        Assert.assertTrue(switchLabeledRule.jjtGetChild(1) instanceof ASTExpression);
+
+        switchLabeledRule = (ASTSwitchLabeledRule) switchStatement.jjtGetChild(4);
+        Assert.assertEquals(2, switchLabeledRule.jjtGetNumChildren());
+        Assert.assertTrue(switchLabeledRule.jjtGetChild(0) instanceof ASTSwitchLabel);
+        Assert.assertTrue(switchLabeledRule.jjtGetChild(1) instanceof ASTBlock);
+
+        switchLabeledRule = (ASTSwitchLabeledRule) switchStatement.jjtGetChild(5);
+        Assert.assertEquals(2, switchLabeledRule.jjtGetNumChildren());
+        Assert.assertTrue(switchLabeledRule.jjtGetChild(0) instanceof ASTSwitchLabel);
+        Assert.assertTrue(switchLabeledRule.jjtGetChild(1) instanceof ASTThrowStatement);
     }
 
     @Test(expected = ParseException.class)
@@ -58,6 +80,11 @@ public class Java12Test {
         ASTCompilationUnit compilationUnit = ParserTstUtil.parseAndTypeResolveJava("12",
                 loadSource("SwitchExpressions.java"));
         Assert.assertNotNull(compilationUnit);
+
+        ASTSwitchExpression switchExpression = compilationUnit.getFirstDescendantOfType(ASTSwitchExpression.class);
+        Assert.assertEquals(6, switchExpression.jjtGetNumChildren());
+        Assert.assertTrue(switchExpression.jjtGetChild(0) instanceof ASTExpression);
+        Assert.assertEquals(5, switchExpression.findChildrenOfType(ASTSwitchLabeledRule.class).size());
     }
 
 }
--- pmd-java/src/test/resources/net/sourceforge/pmd/lang/java/ast/jdkversiontests/java12/SwitchRules.java
+++ pmd-java/src/test/resources/net/sourceforge/pmd/lang/java/ast/jdkversiontests/java12/SwitchRules.java
@@ -14,7 +14,8 @@ public class SwitchRules {
           case MONDAY, FRIDAY, SUNDAY -> System.out.println("  6");
           case TUESDAY                -> System.out.println("  7");
           case THURSDAY, SATURDAY     -> System.out.println("  8");
-          case WEDNESDAY              -> System.out.println("  9");
+          case WEDNESDAY              -> { System.out.println("  9"); }
+          default                     -> throw new IllegalArgumentException();
         }
     }
 }
