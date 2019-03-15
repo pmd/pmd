335d1384246af74108b3ec9d499d8ad8e05d7630
Andreas Dangel <andreas.dangel@adangel.org>
2019-03-15 21:05:21 +0100

    Introduce env variable PMD_JAVA_OPTS That way, additional java options can be set for executing PMD

--- pmd-dist/src/main/resources/scripts/cpd.bat
+++ pmd-dist/src/main/resources/scripts/cpd.bat
@@ -3,4 +3,4 @@ set TOPDIR=%~dp0..
 set OPTS=
 set MAIN_CLASS=net.sourceforge.pmd.cpd.CPD
 
-java -classpath "%TOPDIR%\lib\*" %OPTS% %MAIN_CLASS% %*
+java %PMD_JAVA_OPTS% -classpath "%TOPDIR%\lib\*" %OPTS% %MAIN_CLASS% %*
