96c885b33c207c2edfacca0152f5344490b657d6
Clément Fournier <clement.fournier76@gmail.com>
2019-03-21 14:29:53 +0100

    Test wildcard type

---
+++ pmd-java/src/test/kotlin/net/sourceforge/pmd/lang/java/ast/ASTWildcardTypeTest.kt
@@ -0,0 +1,35 @@
+/*
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
+/*
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
+package net.sourceforge.pmd.lang.java.ast
+
+import net.sourceforge.pmd.lang.ast.test.shouldBe
+import net.sourceforge.pmd.lang.java.ast.JavaVersion.Companion.Latest
+import net.sourceforge.pmd.lang.java.ast.JavaVersion.J1_5
+
+/**
+ * @author Clément Fournier
+ * @since 7.0.0
+ */
+class ASTWildcardTypeTest : ParserTestSpec({
+
+    parserTest("Test simple names", javaVersions = J1_5..Latest) {
+
+        "List<? extends B>" should matchType<ASTWildcardType> {
+
+            it::isUpperBound shouldBe true
+            it::isLowerBound shouldBe false
+
+            it::getTypeBoundNode shouldBe child<ASTClassOrInterfaceType> {
+                it::getTypeImage shouldBe "B"
+            }
+        }
+
+    }
+
+})
