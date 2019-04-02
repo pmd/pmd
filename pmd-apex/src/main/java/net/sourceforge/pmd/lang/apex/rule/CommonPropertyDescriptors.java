2bcda3d6c47d1dd896f7013a3a55899f3cc299ee
Clément Fournier <clement.fournier76@gmail.com>
2019-04-02 05:32:41 +0200

    Port ExcessiveClassLength

---
+++ pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/CommonPropertyDescriptors.java
@@ -0,0 +1,23 @@
+/*
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
+package net.sourceforge.pmd.lang.apex.rule;
+
+import net.sourceforge.pmd.properties.PropertyBuilder.GenericPropertyBuilder;
+import net.sourceforge.pmd.properties.PropertyFactory;
+
+/**
+ * @author Clément Fournier
+ */
+public final class CommonPropertyDescriptors {
+
+    private CommonPropertyDescriptors() {
+
+    }
+
+    public static GenericPropertyBuilder<Integer> reportLevelProperty() {
+        return PropertyFactory.intProperty("reportLevel");
+    }
+
+}
---
+++ pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/design/AbstractExcessiveLengthRule.java
@@ -0,0 +1,59 @@
+/**
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
+package net.sourceforge.pmd.lang.apex.rule.design;
+
+import net.sourceforge.pmd.lang.apex.ast.ApexNode;
+import net.sourceforge.pmd.lang.apex.rule.AbstractApexRule;
+import net.sourceforge.pmd.lang.apex.rule.CommonPropertyDescriptors;
+import net.sourceforge.pmd.properties.PropertyDescriptor;
+
+
+/**
+ * Abstract class for rules counting the length of some node.
+ *
+ * @author Clément Fournier
+ * @since 6.7.0
+ */
+abstract class AbstractExcessiveLengthRule<T extends ApexNode<?>> extends AbstractApexRule {
+
+
+    private final PropertyDescriptor<Integer> reportLevel =
+        CommonPropertyDescriptors.reportLevelProperty()
+                                 .desc("Threshold above which a node is reported")
+                                 .defaultValue(defaultReportLevel()).build();
+
+
+    protected AbstractExcessiveLengthRule(Class<T> nodeType) {
+        definePropertyDescriptor(reportLevel);
+        addRuleChainVisit(nodeType);
+    }
+
+
+    protected abstract int defaultReportLevel();
+
+
+    /** Return true if the node should be ignored. */
+    protected boolean isIgnored(T node) {
+        return false;
+    }
+
+
+    @Override
+    public Object visit(ApexNode<?> node, Object data) {
+        @SuppressWarnings("unchecked")
+        T t = (T) node;
+        // since we only visit this node, it's ok
+
+        if (!isIgnored(t)) {
+            if (t.getEndLine() - t.getBeginLine() > getProperty(reportLevel)) {
+                addViolation(data, node);
+            }
+        }
+
+        return data;
+    }
+
+
+}
