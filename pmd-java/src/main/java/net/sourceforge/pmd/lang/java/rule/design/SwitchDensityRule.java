2a2337c9dae138cdd9741b2870c9d058a33f136d
Clément Fournier <clement.fournier76@gmail.com>
2019-04-02 07:23:33 +0200

    Port SwitchDensity

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/design/SwitchDensityRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/design/SwitchDensityRule.java
@@ -4,16 +4,11 @@
 
 package net.sourceforge.pmd.lang.java.rule.design;
 
-import static net.sourceforge.pmd.properties.constraints.NumericConstraints.positive;
-
 import net.sourceforge.pmd.lang.java.ast.ASTExpression;
 import net.sourceforge.pmd.lang.java.ast.ASTStatement;
 import net.sourceforge.pmd.lang.java.ast.ASTSwitchLabel;
 import net.sourceforge.pmd.lang.java.ast.ASTSwitchStatement;
 import net.sourceforge.pmd.lang.java.ast.JavaParserVisitorAdapter;
-import net.sourceforge.pmd.lang.java.rule.AbstractJavaRulechainRule;
-import net.sourceforge.pmd.properties.PropertyDescriptor;
-import net.sourceforge.pmd.properties.PropertyFactory;
 
 /**
  * Switch Density - This is the number of statements over the number of
@@ -25,28 +20,20 @@ import net.sourceforge.pmd.properties.PropertyFactory;
  *
  * @author David Dixon-Peugh
  */
-public class SwitchDensityRule extends AbstractJavaRulechainRule {
-
-    private static final PropertyDescriptor<Double> REPORT_LEVEL =
-        // can't use CommonPropertyDescriptors because we need a double property
-        PropertyFactory.doubleProperty("minimum")
-                       .desc("Threshold above which a node is reported")
-                       .require(positive())
-                       .defaultValue(10d)
-                       .build();
+public class SwitchDensityRule extends AbstractCounterCheckRule<ASTSwitchStatement> {
 
     public SwitchDensityRule() {
         super(ASTSwitchStatement.class);
-        definePropertyDescriptor(REPORT_LEVEL);
     }
 
     @Override
-    public Object visit(ASTSwitchStatement node, Object data) {
-        double density = new SwitchDensityVisitor().compute(node);
-        if (density >= getProperty(REPORT_LEVEL)) {
-            addViolation(data, node);
-        }
-        return super.visit(node, data);
+    protected int defaultReportLevel() {
+        return 10;
+    }
+
+    @Override
+    protected boolean isViolation(ASTSwitchStatement node, int reportLevel) {
+        return new SwitchDensityVisitor().compute(node) >= reportLevel;
     }
 
     private static class SwitchDensityVisitor extends JavaParserVisitorAdapter {
