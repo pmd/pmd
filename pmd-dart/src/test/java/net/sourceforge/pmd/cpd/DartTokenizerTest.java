1e2e591ac4db5135151e29be1ed15c9e18c7b2cd
Maikel Steneker <maikel.steneker@tiobe.com>
2019-04-24 15:43:53 +0200

    Added unit test for string containing escaped quotes.

--- pmd-dart/src/test/java/net/sourceforge/pmd/cpd/DartTokenizerTest.java
+++ pmd-dart/src/test/java/net/sourceforge/pmd/cpd/DartTokenizerTest.java
@@ -31,6 +31,7 @@ public class DartTokenizerTest extends AbstractTokenizerTest {
     public static Collection<Object[]> data() {
         return Arrays.asList(
                 new Object[] { "comment.dart", 5 },
+                new Object[] { "escaped_string.dart", 17 },
                 new Object[] { "increment.dart", 185 },
                 new Object[] { "imports.dart", 1 }
         );
