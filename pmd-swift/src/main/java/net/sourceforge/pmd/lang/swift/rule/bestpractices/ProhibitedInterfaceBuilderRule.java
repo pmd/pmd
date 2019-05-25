e984eff27b33be4288c1af4a4b6ade35dc454f5c
Matias Fraga <matias.fraga@mercadolibre.com>
2019-05-25 00:15:23 -0300

    Add rule visitor

--- pmd-swift/src/main/java/net/sourceforge/pmd/lang/swift/rule/bestpractices/ProhibitedInterfaceBuilderRule.java
+++ pmd-swift/src/main/java/net/sourceforge/pmd/lang/swift/rule/bestpractices/ProhibitedInterfaceBuilderRule.java
@@ -6,30 +6,41 @@ package net.sourceforge.pmd.lang.swift.rule.bestpractices;
 
 import java.util.List;
 
+import net.sourceforge.pmd.lang.ast.Node;
 import net.sourceforge.pmd.lang.swift.AbstractSwiftRule;
 import net.sourceforge.pmd.lang.swift.antlr4.SwiftParser;
 
 public class ProhibitedInterfaceBuilderRule extends AbstractSwiftRule<Boolean> {
-    @Override
-    public Boolean visitFunctionDeclaration(SwiftParser.FunctionDeclarationContext ctx) {
-        Boolean isOverride = this.visitFunctionHead(ctx.functionHead());
-        if (isOverride) {
-            addViolation(data, ctx);
-        }
-        System.out.println();
-        return true;
-    }
+
+    private static final String IBACTION = "@IBAction";
+    private static final String IBOUTLET = "@IBOutlet";
 
     @Override
     public Boolean visitFunctionHead(SwiftParser.FunctionHeadContext ctx) {
-        if (ctx == null) {
+        if (ctx == null || ctx.attributes() == null) {
             return false;
         }
-        SwiftParser.DeclarationModifiersContext declarationModifiers = ctx.declarationModifiers();
-        if (declarationModifiers == null) {
+
+        return visitDeclarationHead(ctx, ctx.attributes().attribute(), IBACTION);
+    }
+
+    @Override
+    public Boolean visitVariableDeclarationHead(final SwiftParser.VariableDeclarationHeadContext ctx) {
+        if (ctx == null || ctx.attributes() == null) {
             return false;
         }
-        List<SwiftParser.DeclarationModifierContext> modifiers = ctx.declarationModifiers().declarationModifier();
-        return modifiers.stream().anyMatch(modifier -> modifier.getText().equals("override"));
+
+        return visitDeclarationHead(ctx, ctx.attributes().attribute(), IBOUTLET);
+    }
+
+    private boolean visitDeclarationHead(final Node node, final List<SwiftParser.AttributeContext> attributes,
+        final String match) {
+
+        final boolean violate = attributes.stream().anyMatch(atr -> match.equals(atr.getText()));
+        if (violate) {
+            addViolation(data, node);
+        }
+
+        return violate;
     }
 }
