9758f3afb035deeb3ce54c6da65963f3ace5400a
Andreas Dangel <andreas.dangel@pmd-code.org>
2019-05-31 19:49:19 +0200

    Move lombok annotations to UnusedPrivateFieldRule and SingularFieldRule Add additional test cases for ImmutableField and BeanMembersShouldSerialize

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/AbstractLombokAwareRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/AbstractLombokAwareRule.java
@@ -41,7 +41,6 @@ public class AbstractLombokAwareRule extends AbstractIgnoredAnnotationRule {
         LOMBOK_ANNOTATIONS.add("lombok.AllArgsConstructor");
         LOMBOK_ANNOTATIONS.add("lombok.NoArgsConstructor");
         LOMBOK_ANNOTATIONS.add("lombok.Builder");
-        LOMBOK_ANNOTATIONS.add("lombok.experimental.Delegate");
     }
 
     @Override
