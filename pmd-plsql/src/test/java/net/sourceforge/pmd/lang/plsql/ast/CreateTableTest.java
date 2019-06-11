727684885702a0e8bbda8875b4dbb247f4dad45d
Hugo Araya Nash <kabroxiko@gmail.com>
2019-06-11 14:33:09 -0400

    [plsql] Add Table InlineConstraint

--- pmd-plsql/src/test/java/net/sourceforge/pmd/lang/plsql/ast/CreateTableTest.java
+++ pmd-plsql/src/test/java/net/sourceforge/pmd/lang/plsql/ast/CreateTableTest.java
@@ -21,4 +21,12 @@ public class CreateTableTest extends AbstractPLSQLParserTst {
         ASTInput input = parsePLSQL(code);
         Assert.assertNotNull(input);
     }
+
+    @Test
+    public void parseCreateOrganizedTable() throws Exception {
+        String code = IOUtils.toString(this.getClass().getResourceAsStream("CreateOrganizedTable.pls"),
+                StandardCharsets.UTF_8);
+        ASTInput input = parsePLSQL(code);
+        Assert.assertNotNull(input);
+    }
 }
