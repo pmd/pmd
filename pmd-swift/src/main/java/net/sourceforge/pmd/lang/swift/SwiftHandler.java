56040b403cc3060560247fab8da837cb27240b26
Matias Fraga <matias.fraga@mercadolibre.com>
2019-06-20 18:30:29 -0300

    Remove deprecated method

--- pmd-swift/src/main/java/net/sourceforge/pmd/lang/swift/SwiftHandler.java
+++ pmd-swift/src/main/java/net/sourceforge/pmd/lang/swift/SwiftHandler.java
@@ -4,12 +4,9 @@
 
 package net.sourceforge.pmd.lang.swift;
 
-import java.io.Writer;
-
 import net.sourceforge.pmd.lang.AbstractPmdLanguageVersionHandler;
 import net.sourceforge.pmd.lang.Parser;
 import net.sourceforge.pmd.lang.ParserOptions;
-import net.sourceforge.pmd.lang.VisitorStarter;
 import net.sourceforge.pmd.lang.XPathHandler;
 import net.sourceforge.pmd.lang.antlr.AntlrRuleViolationFactory;
 import net.sourceforge.pmd.lang.ast.xpath.DefaultASTXPathHandler;
@@ -30,12 +27,5 @@ public class SwiftHandler extends AbstractPmdLanguageVersionHandler {
     @Override
     public Parser getParser(final ParserOptions parserOptions) {
         return new SwiftParserAdapter(parserOptions);
-    }
-
-    @Override
-    public VisitorStarter getDumpFacade(final Writer writer, final String prefix, final boolean recurse) {
-        return rootNode -> {
-            // TODO: implement dump for AntlrBaseNode
-        };
     }
 }
