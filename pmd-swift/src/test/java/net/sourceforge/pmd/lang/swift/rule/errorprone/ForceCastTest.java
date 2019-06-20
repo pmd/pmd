0f57fa460fa871097997d3191c8ddec4256eeaf6
Matias Fraga <matias.fraga@mercadolibre.com>
2019-06-20 20:10:56 -0300

    remove unused method

--- pmd-swift/src/test/java/net/sourceforge/pmd/lang/swift/rule/errorprone/ForceCastTest.java
+++ pmd-swift/src/test/java/net/sourceforge/pmd/lang/swift/rule/errorprone/ForceCastTest.java
@@ -4,17 +4,8 @@
 
 package net.sourceforge.pmd.lang.swift.rule.errorprone;
 
-import java.util.Collections;
-import java.util.List;
-import net.sourceforge.pmd.Rule;
 import net.sourceforge.pmd.testframework.PmdRuleTst;
 
 public class ForceCastTest extends PmdRuleTst {
     // no additional unit tests
-
-    @Override
-    protected List<Rule> getRules() {
-        final Rule rule = findRule("category/swift/errorprone.xml", "ForceCast");
-        return Collections.singletonList(rule);
-    }
 }
--- pmd-swift/src/test/java/net/sourceforge/pmd/lang/swift/rule/errorprone/ForceTryTest.java
+++ pmd-swift/src/test/java/net/sourceforge/pmd/lang/swift/rule/errorprone/ForceTryTest.java
@@ -4,17 +4,8 @@
 
 package net.sourceforge.pmd.lang.swift.rule.errorprone;
 
-import java.util.Collections;
-import java.util.List;
-import net.sourceforge.pmd.Rule;
 import net.sourceforge.pmd.testframework.PmdRuleTst;
 
 public class ForceTryTest extends PmdRuleTst {
     // no additional unit tests
-
-    @Override
-    protected List<Rule> getRules() {
-        final Rule rule = findRule("category/swift/errorprone.xml", "ForceTry");
-        return Collections.singletonList(rule);
-    }
 }
