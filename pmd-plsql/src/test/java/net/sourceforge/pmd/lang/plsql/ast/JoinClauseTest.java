fdb31686f00b9d4e34943fdf08a6b6994303d466
Andreas Dangel <andreas.dangel@microdoc.com>
2019-07-19 12:21:04 +0200

    Fixes #1902 [pslql] ParseException when parsing (+)

--- pmd-plsql/src/test/java/net/sourceforge/pmd/lang/plsql/ast/JoinClauseTest.java
+++ pmd-plsql/src/test/java/net/sourceforge/pmd/lang/plsql/ast/JoinClauseTest.java
@@ -5,6 +5,7 @@
 package net.sourceforge.pmd.lang.plsql.ast;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.io.IOUtils;
@@ -128,5 +129,21 @@ public class JoinClauseTest extends AbstractPLSQLParserTst {
         String code = IOUtils.toString(this.getClass().getResourceAsStream("InnerJoinSubquery.pls"),
                 StandardCharsets.UTF_8);
         ASTInput input = parsePLSQL(code);
+    }
+
+    @Test
+    public void testJoinOperator() throws Exception {
+        String code = IOUtils.toString(this.getClass().getResourceAsStream("JoinOperator.pls"),
+                StandardCharsets.UTF_8);
+        ASTInput input = parsePLSQL(code);
+        List<ASTSimpleExpression> expressions = input.findDescendantsOfType(ASTSimpleExpression.class);
+        List<ASTSimpleExpression> expressionsWithJoinOperator = new ArrayList<ASTSimpleExpression>();
+        for (ASTSimpleExpression exp : expressions) {
+            if (exp.hasJoinOperator()) {
+                expressionsWithJoinOperator.add(exp);
+            }
+        }
+        Assert.assertEquals(4, expressionsWithJoinOperator.size());
+        Assert.assertEquals("h.opp_id", expressionsWithJoinOperator.get(3).getImage());
     }
 }
