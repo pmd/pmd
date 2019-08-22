fc3dc340f4f482b56eff8da60b030e04500e7b10
Piotr Szymanski <Piotr.Szymanski@sollers.eu>
2019-08-22 11:31:50 +0200

    tests for multiple ddl comands in single file

---
+++ pmd-plsql/src/test/java/net/sourceforge/pmd/lang/plsql/ast/DDLCommandsTest.java
@@ -0,0 +1,26 @@
+/**
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
+package net.sourceforge.pmd.lang.plsql.ast;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import net.sourceforge.pmd.lang.plsql.AbstractPLSQLParserTst;
+
+public class DDLCommandsTest extends AbstractPLSQLParserTst {
+
+    @Test
+    public void parseDDLCommands() throws Exception {
+        String code = IOUtils.toString(this.getClass().getResourceAsStream("DDLCommands.sql"),
+                StandardCharsets.UTF_8);
+        ASTInput input = parsePLSQL(code);
+        List<ASTDDLCommand> ddlcommands = input.findDescendantsOfType(ASTDDLCommand.class);
+        Assert.assertEquals(3, ddlcommands.size());
+    }
+}
---
+++ pmd-plsql/src/test/resources/net/sourceforge/pmd/lang/plsql/ast/DDLCommands.sql
@@ -0,0 +1,5 @@
+COMMENT ON COLUMN employees.job_id IS 'abbreviated job title';
+
+COMMENT ON COLUMN employees.job_id IS 'abbreviated job title';
+
+COMMENT ON COLUMN employees.job_id IS 'abbreviated job title';
