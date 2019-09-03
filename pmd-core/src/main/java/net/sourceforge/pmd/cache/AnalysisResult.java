5db5c62bb2a52c1d3cec54e5398a9a34185c76ec
Andreas Dangel <andreas.dangel@pmd-code.org>
2019-09-03 20:21:23 +0200

    [core] analysis cache: Use always different checksum
    If the checksum couldn't be determined, then we use now
    System.currentTimeMillis. This should result in a different
    checksum everytime the same file is analyzed, so that it
    appears dirty. In that case, the cached results won't be used.

--- pmd-core/src/main/java/net/sourceforge/pmd/cache/AnalysisResult.java
+++ pmd-core/src/main/java/net/sourceforge/pmd/cache/AnalysisResult.java
@@ -53,7 +53,10 @@ public class AnalysisResult {
             // the analysis will fail and report the error on it's own since the checksum won't match
         }
 
-        return 0;
+        // we couldn't read the file, maybe the file doesn't exist
+        // in any case, we can't use the cache. Returning here the timestamp should make
+        // sure, we see that the file changed every time we analyze it.
+        return System.currentTimeMillis();
     }
 
     public long getFileChecksum() {
