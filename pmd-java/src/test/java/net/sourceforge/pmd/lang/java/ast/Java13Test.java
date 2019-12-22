80534a7e69dd8305746bf6ee0ef92eb407c4ba32
Clément Fournier <clement.fournier76@gmail.com>
2019-12-22 19:53:10 +0100

    Fix line endings

--- pmd-java/src/test/java/net/sourceforge/pmd/lang/java/ast/Java13Test.java
+++ pmd-java/src/test/java/net/sourceforge/pmd/lang/java/ast/Java13Test.java
@@ -71,12 +71,12 @@ public class Java13Test {
             ASTLiteral literal = literals.get(i);
             Assert.assertTrue(literal.isTextBlock());
         }
-        Assert.assertEquals("\"\"\"\n"
-                                + "                <html>\n"
-                                + "                    <body>\n"
-                                + "                        <p>Hello, world</p>\n"
-                                + "                    </body>\n"
-                                + "                </html>\n"
+        Assert.assertEquals("\"\"\"\r\n"
+                                + "                <html>\r\n"
+                                + "                    <body>\r\n"
+                                + "                        <p>Hello, world</p>\r\n"
+                                + "                    </body>\r\n"
+                                + "                </html>\r\n"
                                 + "                \"\"\"",
                             literals.get(0).getImage());
         Assert.assertFalse(literals.get(8).isTextBlock());
