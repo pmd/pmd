e345e811bee1c5260490866612af3ab583863911
Andreas Dangel <andreas.dangel@pmd-code.org>
2019-12-24 11:37:41 +0100

    [plsql] Use ReadPastNextOccurrence rather than SkipPastNextToken
    The token SQLPLUS_TERMINATOR is useless, since the token ";" is
    matched before. So, we always skipped till the end of the file.
    
    Improved the image of the skipped nodes, so that it can be of some
    use.
    
    Note: This will fix the problem #2009, but it doesn't parse
    DDL Commands fully. It just skips past the next ";".

--- pmd-plsql/src/test/java/net/sourceforge/pmd/lang/plsql/ast/MultipleDDLStatementsTest.java
+++ pmd-plsql/src/test/java/net/sourceforge/pmd/lang/plsql/ast/MultipleDDLStatementsTest.java
@@ -21,7 +21,7 @@ public class MultipleDDLStatementsTest extends AbstractPLSQLParserTst {
                 StandardCharsets.UTF_8);
         ASTInput input = parsePLSQL(code);
         List<ASTDDLCommand> ddlcommands = input.findDescendantsOfType(ASTDDLCommand.class);
-        Assert.assertEquals(3, ddlcommands.size());
+        Assert.assertEquals(4, ddlcommands.size());
         List<ASTComment> comments = input.findDescendantsOfType(ASTComment.class);
         Assert.assertEquals(3, comments.size());
         Assert.assertEquals("'abbreviated job title'", comments.get(0).getFirstChildOfType(ASTStringLiteral.class).getImage());
--- pmd-plsql/src/test/resources/net/sourceforge/pmd/lang/plsql/ast/DDLCommands.sql
+++ pmd-plsql/src/test/resources/net/sourceforge/pmd/lang/plsql/ast/DDLCommands.sql
@@ -2,4 +2,6 @@ COMMENT ON COLUMN employees.job_id IS 'abbreviated job title';
 
 COMMENT ON COLUMN employees.job_id IS 'abbreviated job title';
 
+DROP TABLE employees;
+
 COMMENT ON COLUMN employees.job_id IS 'abbreviated job title'; 
