7d7857b5e62263bf8ae55007af5246054e278afa
Clément Fournier <clement.fournier76@gmail.com>
2020-01-17 19:21:10 +0100

    Make renderers configurable from CLI

--- pmd-core/src/main/java/net/sourceforge/pmd/util/treeexport/TreeExportCli.java
+++ pmd-core/src/main/java/net/sourceforge/pmd/util/treeexport/TreeExportCli.java
@@ -10,6 +10,11 @@ import java.io.Reader;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringEscapeUtils;
 
 import net.sourceforge.pmd.annotation.Experimental;
 import net.sourceforge.pmd.lang.Language;
@@ -17,6 +22,7 @@ import net.sourceforge.pmd.lang.LanguageRegistry;
 import net.sourceforge.pmd.lang.LanguageVersionHandler;
 import net.sourceforge.pmd.lang.Parser;
 import net.sourceforge.pmd.lang.ast.Node;
+import net.sourceforge.pmd.properties.PropertyDescriptor;
 import net.sourceforge.pmd.properties.PropertySource;
 
 import com.beust.jcommander.JCommander;
@@ -31,6 +37,8 @@ public class TreeExportCli {
     private String language;
     @Parameter(names = { "--encoding", "-e" }, description = "Encoding of the source file.")
     private String encoding = StandardCharsets.UTF_8.name();
+    @Parameter(names = {"-P"}, arity = 2, description = "Specify a property for the renderer.")
+    private Map<String, String> properties = new HashMap<>();
 
     @Parameter(names = { "--help", "-h" }, description = "Display usage.", help = true)
     private boolean help;
@@ -38,11 +46,12 @@ public class TreeExportCli {
     @Parameter(description = "The file to dump", required = true)
     private String file;
 
+
     public static void main(String[] args) throws IOException {
         TreeExportCli cli = new TreeExportCli();
         JCommander jcommander = JCommander.newBuilder()
-                .addObject(cli)
-                .build();
+                                          .addObject(cli)
+                                          .build();
         try {
             jcommander.parse(args);
         } catch (ParameterException e) {
@@ -55,8 +64,27 @@ public class TreeExportCli {
             cli.usage(jcommander);
             System.exit(0);
         }
-        cli.run();
+
+
+        TreeRendererDescriptor descriptor = TreeRenderers.findById(cli.format);
+        if (descriptor == null) {
+            throw cli.bail("Unknown format '" + cli.format + "'");
+        }
+
+        PropertySource bundle = descriptor.newPropertyBundle();
+
+        for (String key : cli.properties.keySet()) {
+            PropertyDescriptor<?> d = bundle.getPropertyDescriptor(key);
+            if (d == null) {
+                throw cli.bail("Unknown property '" + key + "'");
+            }
+
+            setProperty(d, bundle, cli.properties.get(key));
+        }
+
+        cli.run(descriptor.produceRenderer(bundle));
     }
+
 
     private void usage(JCommander commander) {
         StringBuilder sb = new StringBuilder();
@@ -71,7 +99,7 @@ public class TreeExportCli {
         sb.append(System.lineSeparator());
         sb.append("Available formats: ");
         for (TreeRendererDescriptor t : TreeRenderers.registeredRenderers()) {
-            sb.append(t.id()).append(' ');
+            describeRenderer(30, t, sb);
         }
         sb.append(System.lineSeparator())
             .append(System.lineSeparator());
@@ -82,6 +110,51 @@ public class TreeExportCli {
         System.err.print(sb);
     }
 
+    private void describeRenderer(int marginWidth, TreeRendererDescriptor descriptor, StringBuilder sb) {
+
+
+        sb.append(String.format("%-" + marginWidth + "s%s", descriptor.id(), descriptor.description()))
+          .append(System.lineSeparator());
+
+        List<PropertyDescriptor<?>> props = descriptor.newPropertyBundle().getPropertyDescriptors();
+
+        if (!props.isEmpty()) {
+
+            sb.append(String.format("%-" + marginWidth + "s", "+ Properties"))
+              .append(System.lineSeparator());
+
+            for (PropertyDescriptor<?> prop : props) {
+                sb.append(String.format(
+                    "  + %-" + marginWidth + "s%s %s",
+                    prop.name(), prop.description(), "(default " + getDefault(prop) + ")"))
+                  .append(System.lineSeparator());
+            }
+        } else {
+            sb.append(System.lineSeparator());
+        }
+    }
+
+    private <T> String getDefault(PropertyDescriptor<T> prop) {
+        return StringEscapeUtils.escapeJava(prop.asDelimitedString(prop.defaultValue()));
+    }
+
+    private void run(TreeRenderer renderer) throws IOException {
+        printWarning();
+
+        LanguageVersionHandler languageHandler = LanguageRegistry.findLanguageByTerseName(language)
+                .getDefaultVersion().getLanguageVersionHandler();
+        Parser parser = languageHandler.getParser(languageHandler.getDefaultParserOptions());
+
+
+        try (Reader reader = Files.newBufferedReader(new File(file).toPath(), Charset.forName(encoding))) {
+            Node root = parser.parse(file, reader);
+            languageHandler.getQualifiedNameResolutionFacade(this.getClass().getClassLoader()).start(root);
+
+
+            renderer.renderSubtree(root, System.out);
+        }
+    }
+
     private void printWarning() {
         System.err.println("-------------------------------------------------------------------------------");
         System.err.println("This command line utility is experimental. It might change at any time without");
@@ -89,20 +162,15 @@ public class TreeExportCli {
         System.err.println("-------------------------------------------------------------------------------");
     }
 
-    private void run() throws IOException {
-        printWarning();
+    private static <T> void setProperty(PropertyDescriptor<T> descriptor, PropertySource bundle, String value) {
+        bundle.setProperty(descriptor, descriptor.valueFrom(value));
+    }
 
-        LanguageVersionHandler languageHandler = LanguageRegistry.findLanguageByTerseName(language)
-                .getDefaultVersion().getLanguageVersionHandler();
-        Parser parser = languageHandler.getParser(languageHandler.getDefaultParserOptions());
 
-        try (Reader reader = Files.newBufferedReader(new File(file).toPath(), Charset.forName(encoding))) {
-            Node root = parser.parse(file, reader);
-            languageHandler.getQualifiedNameResolutionFacade(this.getClass().getClassLoader()).start(root);
-
-            TreeRendererDescriptor treeRenderer = TreeRenderers.findById(format);
-            PropertySource bundle = treeRenderer.newPropertyBundle();
-            treeRenderer.produceRenderer(bundle).renderSubtree(root, System.out);
-        }
+    private RuntimeException bail(String message) {
+        System.err.println(message);
+        System.err.println("Use --help to obtain information about available formats");
+        System.exit(1);
+        return new RuntimeException();
     }
 }
