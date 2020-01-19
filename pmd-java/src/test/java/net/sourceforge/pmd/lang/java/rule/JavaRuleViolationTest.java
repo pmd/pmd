b5b0bcb9214e79bdaab4ba93f33cc71b7a0e30a8
Clément Fournier <clement.fournier76@gmail.com>
2020-01-19 08:27:29 +0100

    Replace more direct usages of parser

--- pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/JavaRuleViolationTest.java
+++ pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/JavaRuleViolationTest.java
@@ -6,28 +6,24 @@ package net.sourceforge.pmd.lang.java.rule;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.StringReader;
 import java.util.List;
 
 import org.junit.Test;
 
 import net.sourceforge.pmd.RuleContext;
-import net.sourceforge.pmd.lang.LanguageRegistry;
-import net.sourceforge.pmd.lang.LanguageVersionHandler;
-import net.sourceforge.pmd.lang.ParserOptions;
-import net.sourceforge.pmd.lang.java.JavaLanguageModule;
+import net.sourceforge.pmd.lang.java.JavaParsingHelper;
 import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTCompilationUnit;
 import net.sourceforge.pmd.lang.java.ast.ASTFieldDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTFormalParameter;
 import net.sourceforge.pmd.lang.java.ast.ASTImportDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTMethodDeclaration;
-import net.sourceforge.pmd.lang.java.symboltable.ScopeAndDeclarationFinder;
 
 /**
  * @author Philip Graf
  */
 public class JavaRuleViolationTest {
+
     /**
      * Verifies that {@link JavaRuleViolation} sets the variable name for an
      * {@link ASTFormalParameter} node.
@@ -42,14 +38,7 @@ public class JavaRuleViolationTest {
     }
 
     private ASTCompilationUnit parse(final String code) {
-        final LanguageVersionHandler languageVersionHandler = LanguageRegistry.getLanguage(JavaLanguageModule.NAME)
-                .getDefaultVersion().getLanguageVersionHandler();
-        final ParserOptions options = languageVersionHandler.getDefaultParserOptions();
-        final ASTCompilationUnit ast = (ASTCompilationUnit) languageVersionHandler.getParser(options).parse(null,
-                new StringReader(code));
-        // set scope of AST nodes
-        ast.jjtAccept(new ScopeAndDeclarationFinder(), null);
-        return ast;
+        return JavaParsingHelper.WITH_PROCESSING.parse(code);
     }
 
     /**
