adbe5eb55319d4ed0792eaf30998c87a424afe4e
kris-scheibe <k.scheibe@posteo.de>
2020-02-09 07:23:13 +0100

    make AppendCharacterWithCharRule ignore String literals, which are part of an expression, such as in StringBuffer.append("X".repeat(5));

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/performance/AppendCharacterWithCharRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/performance/AppendCharacterWithCharRule.java
@@ -6,6 +6,8 @@ package net.sourceforge.pmd.lang.java.rule.performance;
 
 import net.sourceforge.pmd.lang.java.ast.ASTBlockStatement;
 import net.sourceforge.pmd.lang.java.ast.ASTLiteral;
+import net.sourceforge.pmd.lang.java.ast.ASTPrimaryExpression;
+import net.sourceforge.pmd.lang.java.ast.ASTPrimarySuffix;
 import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
 
 /**
@@ -34,6 +36,13 @@ public class AppendCharacterWithCharRule extends AbstractJavaRule {
             if (!InefficientStringBufferingRule.isInStringBufferOperation(node, 8, "append")) {
                 return data;
             }
+
+            // ignore, if the literal is part of an expression, such as "X".repeat(5)
+            final ASTPrimaryExpression primaryExpression = (ASTPrimaryExpression) node.getNthParent(2);
+            if (primaryExpression != null && primaryExpression.getFirstChildOfType(ASTPrimarySuffix.class) != null) {
+                return data;
+            }
+
             addViolation(data, node);
         }
         return data;
