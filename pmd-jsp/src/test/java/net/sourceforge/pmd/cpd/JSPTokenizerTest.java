c96f9b17348f2eaa3dbfdd404fa9231aa6821494
Clément Fournier <clement.fournier76@gmail.com>
2020-02-15 09:05:09 +0100

    Fix tests

--- pmd-jsp/src/test/java/net/sourceforge/pmd/cpd/JSPTokenizerTest.java
+++ pmd-jsp/src/test/java/net/sourceforge/pmd/cpd/JSPTokenizerTest.java
@@ -10,9 +10,6 @@ import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
-import net.sourceforge.pmd.lang.jsp.ast.JspParserConstants;
-
-
 public class JSPTokenizerTest {
 
     @Test
@@ -20,34 +17,49 @@ public class JSPTokenizerTest {
         JSPTokenizer tokenizer = new JSPTokenizer();
         Tokens tokenEntries = new Tokens();
         String code = IOUtils.toString(JSPTokenizerTest.class.getResourceAsStream("scriptletWithString.jsp"),
-                StandardCharsets.UTF_8);
+                                       StandardCharsets.UTF_8);
         SourceCode sourceCode = new SourceCode(new SourceCode.StringCodeLoader(code));
         tokenizer.tokenize(sourceCode, tokenEntries);
 
-        int[] expectedTokens = new int[] {
-            JspParserConstants.JSP_COMMENT_START,
-            JspParserConstants.JSP_COMMENT_CONTENT,
-            JspParserConstants.JSP_COMMENT_END,
-            JspParserConstants.JSP_SCRIPTLET_START,
-            JspParserConstants.JSP_SCRIPTLET,
-            JspParserConstants.JSP_SCRIPTLET_END,
-            JspParserConstants.JSP_SCRIPTLET_START,
-            JspParserConstants.JSP_SCRIPTLET,
-            JspParserConstants.JSP_SCRIPTLET_END,
-            JspParserConstants.EOF,
-        };
+        /*
+        <%--
+BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+--%>
+
+<%
+String nodeContent = "<% %>";
+%>
+<%
+<![cdata[
+String nodeContent = "<% %>";
+]]>
+%>
+
+         */
+
+        String[] expectedTokens = new String[] {
+            "<%--",
+            "\n"
+                + "BSD-style license; for more info see http://pmd.sourceforge.net/license.html\n",
+            "--%>",
+            "<%",
+            "\nString nodeContent = \"<% %>\";\n",
+            "%>",
+            "<%",
+            "\n<![cdata[\n"
+                + "String nodeContent = \"<% %>\";\n"
+                + "]]>\n",
+            "%>",
+            "",
+            };
+
+
+
         Assert.assertEquals(expectedTokens.length, tokenEntries.getTokens().size());
         for (int i = 0; i < expectedTokens.length - 1; i++) {
-            Assert.assertEquals(String.valueOf(expectedTokens[i]), tokenEntries.getTokens().get(i).toString());
+            TokenEntry tokenEntry = tokenEntries.getTokens().get(i);
+            Assert.assertEquals(expectedTokens[i], tokenEntry.toString());
         }
-        Assert.assertEquals("<JSP_SCRIPTLET>", getTokenImage(tokenEntries.getTokens().get(4)));
     }
 
-    private static String getTokenImage(TokenEntry token) {
-        int kind = 0;
-        if (token != TokenEntry.EOF) {
-            kind = Integer.parseInt(token.toString());
-        }
-        return JspParserConstants.tokenImage[kind];
-    }
 }
