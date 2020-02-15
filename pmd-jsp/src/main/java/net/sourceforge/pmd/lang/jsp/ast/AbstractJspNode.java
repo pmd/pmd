b5bc047b2152b41d78113958799d67c2e9af3d2e
Clément Fournier <clement.fournier76@gmail.com>
2020-02-15 09:16:49 +0100

    Fix jsp node class visibility

--- pmd-jsp/src/main/java/net/sourceforge/pmd/lang/jsp/ast/AbstractJspNode.java
+++ pmd-jsp/src/main/java/net/sourceforge/pmd/lang/jsp/ast/AbstractJspNode.java
@@ -6,21 +6,11 @@ package net.sourceforge.pmd.lang.jsp.ast;
 
 import net.sourceforge.pmd.lang.ast.impl.javacc.AbstractJjtreeNode;
 
-public class AbstractJspNode extends AbstractJjtreeNode<JspNode> implements JspNode {
+abstract class AbstractJspNode extends AbstractJjtreeNode<JspNode> implements JspNode {
 
     public AbstractJspNode(int id) {
         super(id);
     }
-
-
-    /**
-     * Accept the visitor. *
-     */
-    @Override
-    public Object jjtAccept(JspParserVisitor visitor, Object data) {
-        return visitor.visit(this, data);
-    }
-
 
     @Override
     public String getXPathNodeName() {
--- pmd-jsp/src/main/java/net/sourceforge/pmd/lang/jsp/rule/codestyle/DuplicateJspImportsRule.java
+++ pmd-jsp/src/main/java/net/sourceforge/pmd/lang/jsp/rule/codestyle/DuplicateJspImportsRule.java
@@ -5,12 +5,10 @@
 package net.sourceforge.pmd.lang.jsp.rule.codestyle;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import java.util.StringTokenizer;
 
 import net.sourceforge.pmd.RuleContext;
-import net.sourceforge.pmd.lang.ast.Node;
 import net.sourceforge.pmd.lang.jsp.ast.ASTJspDirectiveAttribute;
 import net.sourceforge.pmd.lang.jsp.rule.AbstractJspRule;
 import net.sourceforge.pmd.lang.rule.ImportWrapper;
@@ -20,14 +18,8 @@ public class DuplicateJspImportsRule extends AbstractJspRule {
     private Set<ImportWrapper> imports = new HashSet<>();
 
     @Override
-    public void apply(List<? extends Node> nodes, RuleContext ctx) {
-        /*
-         * TODO: This method is a hack! It's overriding the parent's method
-         * because the JSP parsing doesn't seem to hit ASTCompilationUnit
-         * properly
-         */
+    public void start(RuleContext ctx) {
         imports.clear();
-        super.apply(nodes, ctx);
     }
 
     @Override
