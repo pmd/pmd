0967e437671f53aab2aa6a7bfb830698109eb411
Gwilym Kuiper <gwilym@gearset.com>
2020-02-18 11:02:31 +0000

    Add class level cognitive complexity measures

---
+++ pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/metrics/impl/ClassCognitiveComplexityMetric.java
@@ -0,0 +1,19 @@
+package net.sourceforge.pmd.lang.apex.metrics.impl;
+
+import net.sourceforge.pmd.lang.apex.ast.ASTUserClassOrInterface;
+import net.sourceforge.pmd.lang.apex.metrics.ApexMetrics;
+import net.sourceforge.pmd.lang.apex.metrics.api.ApexOperationMetricKey;
+import net.sourceforge.pmd.lang.metrics.MetricOptions;
+import net.sourceforge.pmd.lang.metrics.ResultOption;
+
+/**
+ * The sum of the cognitive complexities of all the methods within a class.
+ *
+ * @author Gwilym Kuiper
+ */
+public class ClassCognitiveComplexityMetric extends AbstractApexClassMetric {
+    @Override
+    public double computeFor(ASTUserClassOrInterface<?> node, MetricOptions options) {
+        return ApexMetrics.get(ApexOperationMetricKey.COGNITIVE, node, ResultOption.SUM);
+    }
+}
