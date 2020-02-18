cea2081628dee5c1cfb9222596efbc30539c48c5
Gwilym Kuiper <gwilym@gearset.com>
2020-02-18 11:51:46 +0000

    Cognitive complexity for nested if statements

--- pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/metrics/impl/visitors/CognitiveComplexityVisitor.java
+++ pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/metrics/impl/visitors/CognitiveComplexityVisitor.java
@@ -8,21 +8,39 @@ import net.sourceforge.pmd.lang.apex.ast.*;
 public class CognitiveComplexityVisitor extends ApexParserVisitorAdapter {
     public static class State {
         private int complexity = 0;
+        private int nestingLevel = 0;
 
         public double getComplexity() {
             return complexity;
         }
 
-        void increaseComplexity() {
+        void structureComplexity() {
             complexity += 1;
+        }
+
+        void nestingComplexity() {
+            complexity += nestingLevel;
+        }
+
+        void increaseNestingLevel() {
+            nestingLevel++;
+        }
+
+        void decreaseNestingLevel() {
+            nestingLevel--;
         }
     }
 
     @Override
     public Object visit(ASTIfBlockStatement node, Object data) {
         State state = (State) data;
-        state.increaseComplexity();
+        state.structureComplexity();
+        state.nestingComplexity();
+
+        state.increaseNestingLevel();
         super.visit(node, data);
+        state.decreaseNestingLevel();
+
         return data;
     }
 }
