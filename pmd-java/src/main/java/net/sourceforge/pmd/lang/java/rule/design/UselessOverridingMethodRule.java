f0f06b8d840de9e59506aef6b1ffe0ddcb7ca5a1
Andreas Dangel <andreas.dangel@pmd-code.org>
2020-02-20 15:47:53 +0100

    [java] Refactor/simplify UselessOverridingMethod

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/design/UselessOverridingMethodRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/design/UselessOverridingMethodRule.java
@@ -17,12 +17,10 @@ import net.sourceforge.pmd.lang.java.ast.ASTArguments;
 import net.sourceforge.pmd.lang.java.ast.ASTBlock;
 import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceBodyDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceDeclaration;
-import net.sourceforge.pmd.lang.java.ast.ASTCompilationUnit;
 import net.sourceforge.pmd.lang.java.ast.ASTFormalParameter;
 import net.sourceforge.pmd.lang.java.ast.ASTFormalParameters;
 import net.sourceforge.pmd.lang.java.ast.ASTMarkerAnnotation;
 import net.sourceforge.pmd.lang.java.ast.ASTMethodDeclaration;
-import net.sourceforge.pmd.lang.java.ast.ASTMethodDeclarator;
 import net.sourceforge.pmd.lang.java.ast.ASTName;
 import net.sourceforge.pmd.lang.java.ast.ASTNameList;
 import net.sourceforge.pmd.lang.java.ast.ASTPrimaryExpression;
@@ -30,6 +28,7 @@ import net.sourceforge.pmd.lang.java.ast.ASTPrimaryPrefix;
 import net.sourceforge.pmd.lang.java.ast.ASTPrimarySuffix;
 import net.sourceforge.pmd.lang.java.ast.ASTResultType;
 import net.sourceforge.pmd.lang.java.ast.ASTStatement;
+import net.sourceforge.pmd.lang.java.ast.ASTType;
 import net.sourceforge.pmd.lang.java.ast.ASTVariableDeclaratorId;
 import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
 import net.sourceforge.pmd.properties.PropertyDescriptor;
@@ -40,31 +39,19 @@ import net.sourceforge.pmd.properties.PropertyDescriptor;
  *         UselessOverridingMethod
  */
 public class UselessOverridingMethodRule extends AbstractJavaRule {
-    private final List<String> exceptions;
-    private boolean ignoreAnnotations;
-    private static final String CLONE = "clone";
-    private static final String OBJECT = "Object";
+    private static final String CLONE_METHOD_NAME = "clone";
 
-    // TODO extend AbstractIgnoredAnnotationsRule node
+    // TODO extend AbstractIgnoredAnnotationRule node
     // TODO ignore if there is javadoc
-    private static final PropertyDescriptor<Boolean> IGNORE_ANNOTATIONS_DESCRIPTOR = booleanProperty("ignoreAnnotations").defaultValue(false).desc("Ignore annotations").build();
+    private static final PropertyDescriptor<Boolean> IGNORE_ANNOTATIONS_DESCRIPTOR =
+            booleanProperty("ignoreAnnotations")
+            .defaultValue(false)
+            .desc("Ignore annotations")
+            .build();
 
 
     public UselessOverridingMethodRule() {
         definePropertyDescriptor(IGNORE_ANNOTATIONS_DESCRIPTOR);
-
-        exceptions = new ArrayList<>(1);
-        exceptions.add("CloneNotSupportedException");
-    }
-
-    @Override
-    public Object visit(ASTCompilationUnit node, Object data) {
-        init();
-        return super.visit(node, data);
-    }
-
-    private void init() {
-        ignoreAnnotations = getProperty(IGNORE_ANNOTATIONS_DESCRIPTOR);
     }
 
     @Override
@@ -76,35 +63,27 @@ public class UselessOverridingMethodRule extends AbstractJavaRule {
     }
 
     // TODO: this method should be externalize into an utility class, shouldn't it ?
-    private boolean isMethodType(ASTMethodDeclaration node, String methodType) {
-        boolean result = false;
+    private boolean isMethodResultType(ASTMethodDeclaration node, Class<?> resultType) {
         ASTResultType type = node.getResultType();
-        if (type != null) {
-            result = type.hasDescendantMatchingXPath(
-                    "./Type/ReferenceType/ClassOrInterfaceType[@Image = '" + methodType + "']");
+        if (type != null && type.getChild(0) instanceof ASTType) {
+            Class<?> resolvedResultType = ((ASTType) type.getChild(0)).getType();
+            return resultType.equals(resolvedResultType);
         }
-        return result;
+        return false;
     }
 
     // TODO: this method should be externalize into an utility class, shouldn't it ?
-    private boolean isMethodThrowingType(ASTMethodDeclaration node, List<String> exceptedExceptions) {
-        boolean result = false;
-        ASTNameList thrownsExceptions = node.getFirstChildOfType(ASTNameList.class);
+    private boolean isMethodThrowingType(ASTMethodDeclaration node, Class<? extends Exception> exceptionType) {
+        ASTNameList thrownsExceptions = node.getThrows();
         if (thrownsExceptions != null) {
             List<ASTName> names = thrownsExceptions.findChildrenOfType(ASTName.class);
             for (ASTName name : names) {
-                for (String exceptedException : exceptedExceptions) {
-                    if (exceptedException.equals(name.getImage())) {
-                        result = true;
-                    }
+                if (name.getType() != null && name.getType() == exceptionType) {
+                    return true;
                 }
             }
         }
-        return result;
-    }
-
-    private boolean hasArguments(ASTMethodDeclaration node) {
-        return node.hasDescendantMatchingXPath("./MethodDeclarator/FormalParameters/*");
+        return false;
     }
 
     @Override
@@ -118,8 +97,7 @@ public class UselessOverridingMethodRule extends AbstractJavaRule {
         // We can also skip the 'clone' method as they are generally
         // 'useless' but as it is considered a 'good practice' to
         // implement them anyway ( see bug 1522517)
-        if (CLONE.equals(node.getName()) && node.isPublic() && !this.hasArguments(node)
-                && this.isMethodType(node, OBJECT) && this.isMethodThrowingType(node, exceptions)) {
+        if (isCloneMethod(node)) {
             return super.visit(node, data);
         }
 
@@ -142,41 +120,38 @@ public class UselessOverridingMethodRule extends AbstractJavaRule {
         if (statementGrandChild instanceof ASTPrimaryExpression) {
             primaryExpression = (ASTPrimaryExpression) statementGrandChild;
         } else {
-            List<ASTPrimaryExpression> primaryExpressions = findFirstDegreeChildrenOfType(statementGrandChild,
-                    ASTPrimaryExpression.class);
+            List<ASTPrimaryExpression> primaryExpressions = statementGrandChild
+                    .findChildrenOfType(ASTPrimaryExpression.class);
             if (primaryExpressions.size() != 1) {
                 return super.visit(node, data);
             }
             primaryExpression = primaryExpressions.get(0);
         }
 
-        ASTPrimaryPrefix primaryPrefix = findFirstDegreeChildrenOfType(primaryExpression, ASTPrimaryPrefix.class)
-                .get(0);
+        ASTPrimaryPrefix primaryPrefix = primaryExpression.getFirstChildOfType(ASTPrimaryPrefix.class);
         if (!primaryPrefix.usesSuperModifier()) {
             return super.visit(node, data);
         }
 
-        List<ASTPrimarySuffix> primarySuffixList = findFirstDegreeChildrenOfType(primaryExpression,
-                ASTPrimarySuffix.class);
+        List<ASTPrimarySuffix> primarySuffixList = primaryExpression.findChildrenOfType(ASTPrimarySuffix.class);
         if (primarySuffixList.size() != 2) {
             // extra method call on result of super method
             return super.visit(node, data);
         }
 
-        ASTMethodDeclarator methodDeclarator = findFirstDegreeChildrenOfType(node, ASTMethodDeclarator.class).get(0);
         ASTPrimarySuffix primarySuffix = primarySuffixList.get(0);
-        if (!primarySuffix.hasImageEqualTo(methodDeclarator.getImage())) {
+        if (!primarySuffix.hasImageEqualTo(node.getName())) {
             return super.visit(node, data);
         }
         // Process arguments
         primarySuffix = primarySuffixList.get(1);
         ASTArguments arguments = (ASTArguments) primarySuffix.getChild(0);
-        ASTFormalParameters formalParameters = (ASTFormalParameters) methodDeclarator.getChild(0);
+        ASTFormalParameters formalParameters = node.getFormalParameters();
         if (formalParameters.getNumChildren() != arguments.getNumChildren()) {
             return super.visit(node, data);
         }
 
-        if (!ignoreAnnotations) {
+        if (!getProperty(IGNORE_ANNOTATIONS_DESCRIPTOR)) {
             ASTClassOrInterfaceBodyDeclaration parent = (ASTClassOrInterfaceBodyDeclaration) node.getParent();
             for (int i = 0; i < parent.getNumChildren(); i++) {
                 Node n = parent.getChild(i);
@@ -190,6 +165,11 @@ public class UselessOverridingMethodRule extends AbstractJavaRule {
                     return super.visit(node, data);
                 }
             }
+        }
+
+        // different number of args
+        if (arguments.size() != node.getArity()) {
+            return super.visit(node, data);
         }
 
         if (arguments.size() > 0) {
@@ -213,14 +193,9 @@ public class UselessOverridingMethodRule extends AbstractJavaRule {
                     return super.visit(node, data);
                 }
 
-                if (formalParameters.getNumChildren() < i + 1) {
-                    return super.visit(node, data); // different number of args
-                }
-
                 ASTName argumentName = (ASTName) argumentPrimaryPrefixChild;
                 ASTFormalParameter formalParameter = (ASTFormalParameter) formalParameters.getChild(i);
-                ASTVariableDeclaratorId variableId = findFirstDegreeChildrenOfType(formalParameter,
-                        ASTVariableDeclaratorId.class).get(0);
+                ASTVariableDeclaratorId variableId = formalParameter.getFirstChildOfType(ASTVariableDeclaratorId.class);
                 if (!argumentName.hasImageEqualTo(variableId.getImage())) {
                     // The arguments are not simply passed through
                     return super.visit(node, data);
@@ -236,6 +211,15 @@ public class UselessOverridingMethodRule extends AbstractJavaRule {
         addViolation(data, node, getMessage());
 
         return super.visit(node, data);
+    }
+
+    private boolean isCloneMethod(ASTMethodDeclaration node) {
+        boolean isCloneAndPublic = CLONE_METHOD_NAME.equals(node.getName()) && node.isPublic();
+        boolean hasNoParameters = node.getArity() == 0;
+        return isCloneAndPublic
+                && hasNoParameters
+                && this.isMethodResultType(node, Object.class)
+                && this.isMethodThrowingType(node, CloneNotSupportedException.class);
     }
 
     private boolean modifiersChanged(ASTMethodDeclaration node) {
@@ -275,31 +259,11 @@ public class UselessOverridingMethodRule extends AbstractJavaRule {
         return declaredMethod != null && overriddenModifiers != declaredMethod.getModifiers();
     }
 
-    public <T> List<T> findFirstDegreeChildrenOfType(Node n, Class<T> targetType) {
-        List<T> l = new ArrayList<>();
-        lclFindChildrenOfType(n, targetType, l);
-        return l;
-    }
-
-    private <T> void lclFindChildrenOfType(Node node, Class<T> targetType, List<T> results) {
-        if (node.getClass().equals(targetType)) {
-            results.add((T) node);
-        }
-
-        if (node instanceof ASTClassOrInterfaceDeclaration && ((ASTClassOrInterfaceDeclaration) node).isNested()) {
-            return;
-        }
-
-        if (node instanceof ASTClassOrInterfaceBodyDeclaration
-                && ((ASTClassOrInterfaceBodyDeclaration) node).isAnonymousInnerClass()) {
-            return;
-        }
-
-        for (int i = 0; i < node.getNumChildren(); i++) {
-            Node child = node.getChild(i);
-            if (child.getClass().equals(targetType)) {
-                results.add((T) child);
-            }
-        }
+    /**
+     * @deprecated this method will be removed. Just use {@link Node#findChildrenOfType(Class)} directly.
+     */
+    @Deprecated
+    public <T> List<T> findFirstDegreeChaildrenOfType(Node n, Class<T> targetType) {
+        return n.findChildrenOfType(targetType);
     }
 }
