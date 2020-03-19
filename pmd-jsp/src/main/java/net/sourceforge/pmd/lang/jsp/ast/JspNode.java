8baaef915b3a6356eb6fbfc4f93beba0a9a3c3f6
Andreas Dangel <andreas.dangel@pmd-code.org>
2020-03-19 15:16:32 +0100

    [jsp] License headers

--- pmd-jsp/src/main/java/net/sourceforge/pmd/lang/jsp/ast/JspNode.java
+++ pmd-jsp/src/main/java/net/sourceforge/pmd/lang/jsp/ast/JspNode.java
@@ -1,4 +1,4 @@
-/**
+/*
  * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
  */
 
@@ -10,7 +10,7 @@ import net.sourceforge.pmd.lang.ast.NodeStream;
 public interface JspNode extends Node {
 
     /**
-     * Accept the visitor. *
+     * Accept the visitor.
      */
     Object jjtAccept(JspParserVisitor visitor, Object data);
 
