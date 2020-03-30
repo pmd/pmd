80c26bf537717b96844067d4dd9e2f10ccb67c75
Gwilym Kuiper <gwilym@gearset.com>
2020-03-30 11:44:09 +0100

    Add tests and fix up issues found while writing them

--- pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/bestpractices/UnusedLocalVariableRule.java
+++ pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/bestpractices/UnusedLocalVariableRule.java
@@ -1,6 +1,6 @@
 package net.sourceforge.pmd.lang.apex.rule.bestpractices;
 
-import net.sourceforge.pmd.lang.apex.ast.ASTMethod;
+import net.sourceforge.pmd.lang.apex.ast.ASTBlockStatement;
 import net.sourceforge.pmd.lang.apex.ast.ASTVariableDeclaration;
 import net.sourceforge.pmd.lang.apex.ast.ASTVariableExpression;
 import net.sourceforge.pmd.lang.apex.rule.AbstractApexRule;
@@ -12,10 +12,13 @@ public class UnusedLocalVariableRule extends AbstractApexRule {
     public Object visit(ASTVariableDeclaration node, Object data) {
         String variableName = node.getImage();
 
-        ASTMethod containerMethod = node.getFirstParentOfType(ASTMethod.class);
-        List<ASTVariableExpression> potentialUsages = containerMethod.findChildrenOfType(ASTVariableExpression.class);
+        ASTBlockStatement variableContext = node.getFirstParentOfType(ASTBlockStatement.class);
+        List<ASTVariableExpression> potentialUsages = variableContext.findDescendantsOfType(ASTVariableExpression.class);
 
         for (ASTVariableExpression usage : potentialUsages) {
+            if (usage.getParent() == node) {
+                continue;
+            }
             if (usage.getImage().equals(variableName)) {
                 return data;
             }
---
+++ pmd-apex/src/test/java/net/sourceforge/pmd/lang/apex/rule/bestpractices/UnusedLocalVariableTest.java
@@ -0,0 +1,7 @@
+package net.sourceforge.pmd.lang.apex.rule.bestpractices;
+
+import net.sourceforge.pmd.testframework.PmdRuleTst;
+
+public class UnusedLocalVariableTest extends PmdRuleTst {
+    // no additional unit tests
+}
