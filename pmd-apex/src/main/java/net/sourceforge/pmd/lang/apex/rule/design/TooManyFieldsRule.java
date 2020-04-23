96e9127eae68ee861e3696fc199911f897ec3813
Clément Fournier <clement.fournier76@gmail.com>
2020-04-23 19:33:13 +0200

    Cleanup TooManyFieldsRule
    It used a descendant check unnecessarily,
    can use the rulechain now

--- pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/design/TooManyFieldsRule.java
+++ pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/design/TooManyFieldsRule.java
@@ -6,24 +6,17 @@ package net.sourceforge.pmd.lang.apex.rule.design;
 
 import static net.sourceforge.pmd.properties.constraints.NumericConstraints.positive;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import net.sourceforge.pmd.lang.apex.ast.ASTField;
 import net.sourceforge.pmd.lang.apex.ast.ASTUserClass;
 import net.sourceforge.pmd.lang.apex.rule.AbstractApexRule;
-import net.sourceforge.pmd.lang.ast.Node;
 import net.sourceforge.pmd.properties.PropertyDescriptor;
 import net.sourceforge.pmd.properties.PropertyFactory;
-import net.sourceforge.pmd.util.NumericConstants;
 
 public class TooManyFieldsRule extends AbstractApexRule {
 
     private static final int DEFAULT_MAXFIELDS = 15;
-
-    private Map<String, Integer> stats;
-    private Map<String, ASTUserClass> nodes;
 
     private static final PropertyDescriptor<Integer> MAX_FIELDS_DESCRIPTOR
             = PropertyFactory.intProperty("maxfields")
@@ -35,6 +28,7 @@ public class TooManyFieldsRule extends AbstractApexRule {
 
     public TooManyFieldsRule() {
         definePropertyDescriptor(MAX_FIELDS_DESCRIPTOR);
+        addRuleChainVisit(ASTUserClass.class);
 
         setProperty(CODECLIMATE_CATEGORIES, "Complexity");
         setProperty(CODECLIMATE_REMEDIATION_MULTIPLIER, 200);
@@ -44,39 +38,19 @@ public class TooManyFieldsRule extends AbstractApexRule {
     @Override
     public Object visit(ASTUserClass node, Object data) {
 
-        int maxFields = getProperty(MAX_FIELDS_DESCRIPTOR);
+        List<ASTField> fields = node.findChildrenOfType(ASTField.class);
 
-        stats = new HashMap<>(5);
-        nodes = new HashMap<>(5);
-
-        List<ASTField> fields = node.findDescendantsOfType(ASTField.class);
-
+        int val = 0;
         for (ASTField field : fields) {
             if (field.getModifiers().isFinal() && field.getModifiers().isStatic()) {
                 continue;
             }
-            ASTUserClass clazz = field.getFirstParentOfType(ASTUserClass.class);
-            if (clazz != null) {
-                bumpCounterFor(clazz);
-            }
+            val++;
         }
-        for (Map.Entry<String, Integer> entry : stats.entrySet()) {
-            int val = entry.getValue();
-            Node n = nodes.get(entry.getKey());
-            if (val > maxFields) {
-                addViolation(data, n);
-            }
+        if (val > getProperty(MAX_FIELDS_DESCRIPTOR)) {
+            addViolation(data, node);
         }
         return data;
     }
 
-    private void bumpCounterFor(ASTUserClass clazz) {
-        String key = clazz.getImage();
-        if (!stats.containsKey(key)) {
-            stats.put(key, NumericConstants.ZERO);
-            nodes.put(key, clazz);
-        }
-        Integer i = stats.get(key) + 1;
-        stats.put(key, i);
-    }
 }
