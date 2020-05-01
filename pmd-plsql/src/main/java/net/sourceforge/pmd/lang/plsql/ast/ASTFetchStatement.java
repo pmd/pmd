301cb8ca338c7b269cca5e07a8fe169b0dcc2416
Clément Fournier <clement.fournier76@gmail.com>
2020-05-01 16:20:05 +0200

    Fix merge

--- pmd-plsql/src/main/java/net/sourceforge/pmd/lang/plsql/ast/ASTFetchStatement.java
+++ pmd-plsql/src/main/java/net/sourceforge/pmd/lang/plsql/ast/ASTFetchStatement.java
@@ -4,22 +4,13 @@
 
 package net.sourceforge.pmd.lang.plsql.ast;
 
-import net.sourceforge.pmd.annotation.InternalApi;
-
-public class ASTFetchStatement extends AbstractPLSQLNode {
+public final class ASTFetchStatement extends AbstractPLSQLNode {
     private boolean bulkcollect;
     private boolean limit;
 
-    @Deprecated
-    @InternalApi
-    public ASTFetchStatement(int id) {
-        super(id);
-    }
 
-    @Deprecated
-    @InternalApi
-    public ASTFetchStatement(PLSQLParser p, int id) {
-        super(p, id);
+    ASTFetchStatement(int id) {
+        super(id);
     }
 
     void setBulkCollect(boolean bulkcollect) {
--- pmd-plsql/src/main/java/net/sourceforge/pmd/lang/plsql/ast/ASTSqlStatement.java
+++ pmd-plsql/src/main/java/net/sourceforge/pmd/lang/plsql/ast/ASTSqlStatement.java
@@ -4,24 +4,14 @@
 
 package net.sourceforge.pmd.lang.plsql.ast;
 
-import net.sourceforge.pmd.annotation.InternalApi;
-
-public class ASTSqlStatement extends AbstractPLSQLNode {
+public final class ASTSqlStatement extends AbstractPLSQLNode {
 
     private Type type;
 
     public enum Type { COMMIT, ROLLBACK, SAVEPOINT, SET_TRANSACTION, LOCK_TABLE, MERGE }
 
-    @Deprecated
-    @InternalApi
-    public ASTSqlStatement(int id) {
+    ASTSqlStatement(int id) {
         super(id);
-    }
-
-    @Deprecated
-    @InternalApi
-    public ASTSqlStatement(PLSQLParser p, int id) {
-        super(p, id);
     }
 
     void setType(Type type) {
