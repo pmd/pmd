230b041c28ae7f719e3141cb9de3985c1b2d9e85
xioayuge <45328272+xioayuge@users.noreply.github.com>
2020-05-31 19:48:58 +0800

    Update LawOfDemeterRule.java

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/design/LawOfDemeterRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/design/LawOfDemeterRule.java
@@ -146,7 +146,9 @@ public class LawOfDemeterRule extends AbstractJavaRule {
                 if (firstMethodCallInChain.isNotBuilder()) {
                     List<ASTPrimarySuffix> suffixes = findSuffixesWithoutArguments(expression);
                     for (ASTPrimarySuffix suffix : suffixes) {
-                        result.add(new MethodCall(expression, suffix));
+                        if (!expression.hasDescendantOfType(ASTCastExpression.class)) {
+                            result.add(new MethodCall(expression, suffix));
+                        }
                     }
                 }
             }
@@ -175,7 +177,7 @@ public class LawOfDemeterRule extends AbstractJavaRule {
         private static List<ASTPrimarySuffix> findSuffixesWithoutArguments(ASTPrimaryExpression expr) {
             List<ASTPrimarySuffix> result = new ArrayList<>();
             if (hasRealPrefix(expr)) {
-                List<ASTPrimarySuffix> suffixes = expr.findDescendantsOfType(ASTPrimarySuffix.class);
+                List<ASTPrimarySuffix> suffixes = expr.findChildrenOfType(ASTPrimarySuffix.class);
                 for (ASTPrimarySuffix suffix : suffixes) {
                     if (!suffix.isArguments()) {
                         result.add(suffix);
