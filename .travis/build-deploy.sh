f4880f9318e981fc78e024f28d1e894734dbf49a
João Ferreira <jtjeferreira@gmail.com>
2020-05-31 11:56:43 +0100

    refactor scala modules configuration

--- .travis/build-deploy.sh
+++ .travis/build-deploy.sh
@@ -26,7 +26,6 @@ elif travis_isPullRequest; then
 
     log_info "This is a pull-request build"
     ./mvnw verify $MVN_BUILD_FLAGS
-    ./mvnw clean verify $MVN_BUILD_FLAGS -pl pmd-scala -DscalaVersion=2.13
 
     regression-tester_executeDanger
 
@@ -43,7 +42,6 @@ elif travis_isPush; then
 
         # Build and deploy to ossrh / maven-central
         ./mvnw deploy -Possrh,sign,pmd-release $MVN_BUILD_FLAGS
-        ./mvnw clean deploy -Possrh,sign,pmd-release $MVN_BUILD_FLAGS -pl pmd-scala -DscalaVersion=2.13
         echo -e "\n\n"
 
         # Deploy to github releases
@@ -59,7 +57,6 @@ elif travis_isPush; then
     elif [[ "${VERSION}" == *-SNAPSHOT ]]; then
         log_info "This is a snapshot build"
         ./mvnw deploy -Possrh,sign $MVN_BUILD_FLAGS
-        ./mvnw clean deploy -Possrh,sign $MVN_BUILD_FLAGS -pl pmd-scala -DscalaVersion=2.13
         
         # Deploy to sourceforge files
         sourceforge_uploadFile "${VERSION}" "pmd-dist/target/pmd-bin-${VERSION}.zip"
