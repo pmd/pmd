9bbb6aab4d25d0c2abc3a7928856f6a330ab4785
Mykhailo Palahuta <michaeller.2012@gmail.com>
2020-07-16 19:21:22 +0300

    AvoidCallingFinalize: constructor false negative fix

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/errorprone/AvoidCallingFinalizeRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/errorprone/AvoidCallingFinalizeRule.java
@@ -4,11 +4,9 @@
 
 package net.sourceforge.pmd.lang.java.rule.errorprone;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
-import net.sourceforge.pmd.lang.java.ast.ASTBlock;
 import net.sourceforge.pmd.lang.java.ast.ASTMethodDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTPrimaryExpression;
 import net.sourceforge.pmd.lang.java.ast.ASTPrimaryPrefix;
@@ -21,55 +19,29 @@ public class AvoidCallingFinalizeRule extends AbstractJavaRule {
     private static final Pattern FINALIZE_METHOD_PATTERN = Pattern.compile("^(.+\\.)?finalize$");
 
     @Override
-    public Object visit(ASTBlock block, Object data) {
-        List<ASTPrimaryExpression> finalizeMethodCalls = getIncorrectFinalizeMethodCalls(block);
-        for (ASTPrimaryExpression finalizeMethodCall : finalizeMethodCalls) {
-            addViolation(data, finalizeMethodCall);
+    public Object visit(ASTPrimaryExpression primaryExpression, Object data) {
+        if (isIncorrectFinalizeMethodCall(primaryExpression)) {
+            addViolation(data, primaryExpression);
         }
         return data;
     }
 
-    private List<ASTPrimaryExpression> getIncorrectFinalizeMethodCalls(ASTBlock block) {
-        if (isFinalizeMethodBlock(block)) {
-            return getNotSuperFinalizeMethodCalls(block);
-        }
-        return getFinalizeMethodCalls(block);
+    private boolean isIncorrectFinalizeMethodCall(ASTPrimaryExpression primaryExpression) {
+        return isFinalizeMethodCall(primaryExpression)
+                && (isNotInFinalizeMethod(primaryExpression) || isNotSuperMethodCall(primaryExpression));
     }
 
-    private boolean isFinalizeMethodBlock(ASTBlock block) {
-        ASTMethodDeclaration methodDeclaration = block.getFirstParentOfType(ASTMethodDeclaration.class);
-        return methodDeclaration != null && isFinalizeMethodDeclaration(methodDeclaration);
+    private boolean isNotInFinalizeMethod(ASTPrimaryExpression primaryExpression) {
+        ASTMethodDeclaration methodDeclaration = primaryExpression.getFirstParentOfType(ASTMethodDeclaration.class);
+        return methodDeclaration == null || isNotFinalizeMethodDeclaration(methodDeclaration);
+    }
+
+    private boolean isNotFinalizeMethodDeclaration(ASTMethodDeclaration methodDeclaration) {
+        return !isFinalizeMethodDeclaration(methodDeclaration);
     }
 
     private boolean isFinalizeMethodDeclaration(ASTMethodDeclaration methodDeclaration) {
         return "finalize".equals(methodDeclaration.getName()) && methodDeclaration.getArity() == 0;
-    }
-
-    private List<ASTPrimaryExpression> getNotSuperFinalizeMethodCalls(ASTBlock block) {
-        List<ASTPrimaryExpression> finalizeMethodCalls = getFinalizeMethodCalls(block);
-        List<ASTPrimaryExpression> notSuperCalls = new ArrayList<>();
-        for (ASTPrimaryExpression finalizeMethodCall : finalizeMethodCalls) {
-            if (isNotSuperMethodCall(finalizeMethodCall)) {
-                notSuperCalls.add(finalizeMethodCall);
-            }
-        }
-        return notSuperCalls;
-    }
-
-    private boolean isNotSuperMethodCall(ASTPrimaryExpression primaryExpression) {
-        ASTPrimaryPrefix primaryPrefix = primaryExpression.getFirstChildOfType(ASTPrimaryPrefix.class);
-        return primaryPrefix == null || !primaryPrefix.usesSuperModifier();
-    }
-
-    private List<ASTPrimaryExpression> getFinalizeMethodCalls(ASTBlock block) {
-        List<ASTPrimaryExpression> primaryExpressions = block.findDescendantsOfType(ASTPrimaryExpression.class);
-        List<ASTPrimaryExpression> finalizeMethodCalls = new ArrayList<>();
-        for (ASTPrimaryExpression primaryExpression : primaryExpressions) {
-            if (isFinalizeMethodCall(primaryExpression)) {
-                finalizeMethodCalls.add(primaryExpression);
-            }
-        }
-        return finalizeMethodCalls;
     }
 
     private boolean isFinalizeMethodCall(ASTPrimaryExpression primaryExpression) {
@@ -97,5 +69,10 @@ public class AvoidCallingFinalizeRule extends AbstractJavaRule {
             return primarySuffixes.get(lastSuffixIndex).getArgumentCount();
         }
         return -1;
+    }
+
+    private boolean isNotSuperMethodCall(ASTPrimaryExpression primaryExpression) {
+        ASTPrimaryPrefix primaryPrefix = primaryExpression.getFirstChildOfType(ASTPrimaryPrefix.class);
+        return primaryPrefix == null || !primaryPrefix.usesSuperModifier();
     }
 }
