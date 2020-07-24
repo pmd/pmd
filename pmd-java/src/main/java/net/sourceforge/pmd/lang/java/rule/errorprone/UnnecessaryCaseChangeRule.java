b8d878b4012de2211c253af185284ba3ae7943a2
Mykhailo Palahuta <michaeller.2012@gmail.com>
2020-07-24 11:25:14 +0300

    [java] UnnecessaryCaseChange can not detect the case like: foo.equals(bar.toLowerCase())

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/errorprone/UnnecessaryCaseChangeRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/errorprone/UnnecessaryCaseChangeRule.java
@@ -4,83 +4,105 @@
 
 package net.sourceforge.pmd.lang.java.rule.errorprone;
 
-import net.sourceforge.pmd.lang.ast.Node;
+import static java.util.Arrays.asList;
+
+import java.util.List;
+
+import net.sourceforge.pmd.lang.java.ast.ASTArgumentList;
+import net.sourceforge.pmd.lang.java.ast.ASTArguments;
 import net.sourceforge.pmd.lang.java.ast.ASTName;
 import net.sourceforge.pmd.lang.java.ast.ASTPrimaryExpression;
 import net.sourceforge.pmd.lang.java.ast.ASTPrimaryPrefix;
 import net.sourceforge.pmd.lang.java.ast.ASTPrimarySuffix;
+import net.sourceforge.pmd.lang.java.ast.JavaNode;
 import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
 
 public class UnnecessaryCaseChangeRule extends AbstractJavaRule {
 
+    private static final List<String> CASE_CHANGING_METHODS = asList("toLowerCase", "toUpperCase");
+    private static final List<String> EQUALITY_METHODS = asList("equals", "equalsIgnoreCase");
+
     @Override
-    public Object visit(ASTPrimaryExpression exp, Object data) {
-        int n = exp.getNumChildren();
-        if (n < 4) {
-            return data;
+    public Object visit(ASTPrimaryExpression expr, Object data) {
+        int caseChangingCallIndex = getCaseChangingMethodCallIndex(expr);
+        if (caseChangingCallIndex != -1) {
+            int chainedMethodCallIndex = caseChangingCallIndex + 2;
+            if (hasEqualsMethodCallChainedAtPosition(expr, chainedMethodCallIndex)
+                    || isArgumentOfEqualsMethodCall(expr)) {
+                addViolation(data, expr);
+            }
         }
-
-        int first = getBadPrefixOrNull(exp, n);
-        if (first == -1) {
-            return data;
-        }
-
-        String second = getBadSuffixOrNull(exp, first + 2);
-        if (second == null) {
-            return data;
-        }
-
-        if (!(exp.getChild(first + 1) instanceof ASTPrimarySuffix)) {
-            return data;
-        }
-        ASTPrimarySuffix methodCall = (ASTPrimarySuffix) exp.getChild(first + 1);
-        if (!methodCall.isArguments() || methodCall.getArgumentCount() > 0) {
-            return data;
-        }
-
-        addViolation(data, exp);
-        return data;
+        return super.visit(expr, data);
     }
 
-    private int getBadPrefixOrNull(ASTPrimaryExpression exp, int childrenCount) {
-        // verify PrimaryPrefix/Name[ends-with(@Image, 'toUpperCase']
-        for (int i = 0; i < childrenCount - 3; i++) {
-            Node child = exp.getChild(i);
-            String image;
-            if (child instanceof ASTPrimaryPrefix) {
-                if (child.getNumChildren() != 1 || !(child.getChild(0) instanceof ASTName)) {
-                    continue;
-                }
-
-                ASTName name = (ASTName) child.getChild(0);
-                image = name.getImage();
-            } else if (child instanceof ASTPrimarySuffix) {
-                image = ((ASTPrimarySuffix) child).getImage();
-            } else {
-                continue;
-            }
-
-            if (image == null || !(image.endsWith("toUpperCase") || image.endsWith("toLowerCase"))) {
-                continue;
-            } else {
-                return i;
+    private int getCaseChangingMethodCallIndex(ASTPrimaryExpression expr) {
+        List<JavaNode> exprNodes = expr.findChildrenOfType(JavaNode.class);
+        for (int callArgsIndex = 1; callArgsIndex < exprNodes.size(); callArgsIndex++) {
+            int callIndex = callArgsIndex - 1;
+            if (isCaseChangingMethodCall(exprNodes.get(callIndex), exprNodes.get(callArgsIndex))) {
+                return callIndex;
             }
         }
         return -1;
     }
 
-    private String getBadSuffixOrNull(ASTPrimaryExpression exp, int equalsPosition) {
-        // verify PrimarySuffix[@Image='equals']
-        if (!(exp.getChild(equalsPosition) instanceof ASTPrimarySuffix)) {
-            return null;
-        }
-
-        ASTPrimarySuffix suffix = (ASTPrimarySuffix) exp.getChild(equalsPosition);
-        if (suffix.getImage() == null
-                || !(suffix.hasImageEqualTo("equals") || suffix.hasImageEqualTo("equalsIgnoreCase"))) {
-            return null;
-        }
-        return suffix.getImage();
+    private boolean isCaseChangingMethodCall(JavaNode methodCall, JavaNode methodCallArgs) {
+        String methodName = getCalledMethodName(methodCall);
+        int methodArgsCount = getCalledMethodArgsCount(methodCallArgs);
+        return methodName != null
+                && isNameOfCaseChangingMethod(methodName) && methodArgsCount == 0;
     }
 
+    private String getCalledMethodName(JavaNode methodCall) {
+        if (methodCall instanceof ASTPrimaryPrefix) {
+            ASTName methodName = methodCall.getFirstDescendantOfType(ASTName.class);
+            return methodName != null ? methodName.getImage() : null;
+        }
+        return methodCall.getImage();
+    }
+
+    private boolean isNameOfCaseChangingMethod(String methodName) {
+        for (String caseChangingMethod : CASE_CHANGING_METHODS) {
+            if (methodName.endsWith(caseChangingMethod)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean hasEqualsMethodCallChainedAtPosition(ASTPrimaryExpression expr, int pos) {
+        int argsPos = pos + 1;
+        if (hasNodeAtPosition(expr, argsPos)) {
+            return isEqualsMethodCall(expr.getChild(pos), expr.getChild(argsPos));
+        }
+        return false;
+    }
+
+    private boolean hasNodeAtPosition(ASTPrimaryExpression expr, int pos) {
+        return pos < expr.getNumChildren();
+    }
+
+    private boolean isArgumentOfEqualsMethodCall(ASTPrimaryExpression expr) {
+        JavaNode parent = expr.getParent().getParent();
+        if (parent instanceof ASTArgumentList) {
+            ASTPrimarySuffix parentMethodCallArgs = parent.getFirstParentOfType(ASTPrimarySuffix.class);
+            List<JavaNode> parentNodes = parentMethodCallArgs.getParent().findChildrenOfType(JavaNode.class);
+            int parentMethodCallIndex = parentNodes.indexOf(parentMethodCallArgs) - 1;
+            JavaNode parentMethodCall = parentNodes.get(parentMethodCallIndex);
+            return isEqualsMethodCall(parentMethodCall, parentMethodCallArgs);
+        }
+        return false;
+    }
+
+    private boolean isEqualsMethodCall(JavaNode methodCall, JavaNode methodCallArgs) {
+        String methodName = methodCall.getImage();
+        int methodArgsCount = getCalledMethodArgsCount(methodCallArgs);
+        return methodName != null
+                && EQUALITY_METHODS.contains(methodName) && methodArgsCount == 1;
+    }
+
+    private int getCalledMethodArgsCount(JavaNode methodCall) {
+        ASTArguments args = methodCall.getFirstDescendantOfType(ASTArguments.class);
+        return args != null ? args.size() : -1;
+    }
 }
