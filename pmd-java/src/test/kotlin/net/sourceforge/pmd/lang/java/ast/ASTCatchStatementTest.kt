acf58f1887aa678ea28be4928d51d1370120f2a9
Andreas Dangel <andreas.dangel@pmd-code.org>
2020-07-31 20:26:27 +0200

    Check for license headers in kotlin source files

--- pmd-java/src/test/kotlin/net/sourceforge/pmd/lang/java/ast/ASTCatchStatementTest.kt
+++ pmd-java/src/test/kotlin/net/sourceforge/pmd/lang/java/ast/ASTCatchStatementTest.kt
@@ -1,3 +1,7 @@
+/*
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
 package net.sourceforge.pmd.lang.java.ast
 
 import io.kotest.matchers.collections.shouldContainExactly
