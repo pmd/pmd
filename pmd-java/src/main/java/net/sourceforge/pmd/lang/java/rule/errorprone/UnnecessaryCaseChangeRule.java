eca547b5054814933f87f0718e0432bc2e27e5da
Mykhailo Palahuta <michaeller.2012@gmail.com>
2020-08-02 00:54:27 +0300

    UnnecessaryCaseChangeRule: nested expressions traversing fix

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/errorprone/UnnecessaryCaseChangeRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/errorprone/UnnecessaryCaseChangeRule.java
@@ -8,12 +8,9 @@ import static java.util.Arrays.asList;
 
 import java.util.List;
 
-import net.sourceforge.pmd.lang.java.ast.ASTArgumentList;
 import net.sourceforge.pmd.lang.java.ast.ASTArguments;
 import net.sourceforge.pmd.lang.java.ast.ASTName;
 import net.sourceforge.pmd.lang.java.ast.ASTPrimaryExpression;
-import net.sourceforge.pmd.lang.java.ast.ASTPrimaryPrefix;
-import net.sourceforge.pmd.lang.java.ast.ASTPrimarySuffix;
 import net.sourceforge.pmd.lang.java.ast.JavaNode;
 import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
 
@@ -24,48 +21,81 @@ public class UnnecessaryCaseChangeRule extends AbstractJavaRule {
 
     @Override
     public Object visit(ASTPrimaryExpression expr, Object data) {
-        int caseChangingCallIndex = getCaseChangingMethodCallIndex(expr);
-        if (caseChangingCallIndex != -1) {
-            int chainedMethodCallIndex = caseChangingCallIndex + 2;
-            if (hasEqualsMethodCallChainedAtPosition(expr, chainedMethodCallIndex)
-                    || isArgumentOfEqualsMethodCall(expr)) {
-                addViolation(data, expr);
-                return data;
-            }
+        if (hasUnnecessaryCaseChange(expr)) {
+            addViolation(data, expr);
         }
         return super.visit(expr, data);
     }
 
-    private int getCaseChangingMethodCallIndex(ASTPrimaryExpression expr) {
-        for (int callArgsIndex = 1; callArgsIndex < expr.getNumChildren(); callArgsIndex++) {
-            JavaNode methodCallArgs = expr.getChild(callArgsIndex);
-            int callIndex = callArgsIndex - 1;
+    private boolean hasUnnecessaryCaseChange(ASTPrimaryExpression expr) {
+        int equalsMethodCallIndex = getEqualsMethodCallIndex(expr);
+        if (equalsMethodCallIndex != -1) {
+            int equalsMethodCallArgsIndex = equalsMethodCallIndex + 1;
+            ASTPrimaryExpression equalsCallArgs = getMethodCallArgsAtPosition(expr, equalsMethodCallArgsIndex);
+            return anyHasCaseChangingMethodCall(expr, equalsCallArgs);
+        }
+        return false;
+    }
+
+    private int getEqualsMethodCallIndex(ASTPrimaryExpression expr) {
+        for (int callIndex = 0; callIndex < expr.getNumChildren(); callIndex++) {
             JavaNode methodCall = expr.getChild(callIndex);
-            if (isCaseChangingMethodCall(methodCall, methodCallArgs)) {
+            if (isEqualsMethodCall(methodCall)) {
                 return callIndex;
             }
         }
         return -1;
     }
 
-    private boolean isCaseChangingMethodCall(JavaNode methodCall, JavaNode methodCallArgs) {
-        String methodName = getCalledMethodName(methodCall);
-        int methodArgsCount = getCalledMethodArgsCount(methodCallArgs);
-        return isNameOfCaseChangingMethod(methodName) && methodArgsCount == 0;
+    private boolean isEqualsMethodCall(JavaNode methodCall) {
+        return calledMethodHasNameFromList(methodCall, EQUALITY_METHODS);
     }
 
-    private String getCalledMethodName(JavaNode methodCall) {
-        if (methodCall instanceof ASTPrimaryPrefix) {
-            ASTName methodName = methodCall.getFirstDescendantOfType(ASTName.class);
-            return methodName != null ? methodName.getImage() : null;
+    private ASTPrimaryExpression getMethodCallArgsAtPosition(ASTPrimaryExpression expr, int argsPos) {
+        if (hasChildAtPosition(expr, argsPos)) {
+            JavaNode methodCallArgs = expr.getChild(argsPos);
+            return methodCallArgs.getFirstDescendantOfType(ASTPrimaryExpression.class);
         }
-        return methodCall.getImage();
+        return null;
     }
 
-    private boolean isNameOfCaseChangingMethod(String methodName) {
+    private boolean hasChildAtPosition(ASTPrimaryExpression expr, int pos) {
+        return expr.getNumChildren() > pos;
+    }
+
+    private boolean anyHasCaseChangingMethodCall(ASTPrimaryExpression ... exprs) {
+        for (ASTPrimaryExpression expr : exprs) {
+            if (expr != null && hasCaseChangingMethodCall(expr)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean hasCaseChangingMethodCall(ASTPrimaryExpression expr) {
+        for (int callArgsIndex = 1; callArgsIndex < expr.getNumChildren(); callArgsIndex++) {
+            JavaNode methodCall = expr.getChild(callArgsIndex - 1);
+            JavaNode methodCallArgs = expr.getChild(callArgsIndex);
+            if (isCaseChangingMethodCall(methodCall, methodCallArgs)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isCaseChangingMethodCall(JavaNode methodCall, JavaNode methodCallArgs) {
+        if (calledMethodHasNameFromList(methodCall, CASE_CHANGING_METHODS)) {
+            ASTArguments args = methodCallArgs.getFirstDescendantOfType(ASTArguments.class);
+            return args != null && args.size() == 0;
+        }
+        return false;
+    }
+
+    private boolean calledMethodHasNameFromList(JavaNode methodCall, List<String> nameList) {
+        String methodName = getCalledMethodName(methodCall);
         if (methodName != null) {
-            for (String caseChangingMethod : CASE_CHANGING_METHODS) {
-                if (methodName.endsWith(caseChangingMethod)) {
+            for (String nameFromList : nameList) {
+                if (methodName.endsWith(nameFromList)) {
                     return true;
                 }
             }
@@ -73,45 +103,20 @@ public class UnnecessaryCaseChangeRule extends AbstractJavaRule {
         return false;
     }
 
-    private boolean hasEqualsMethodCallChainedAtPosition(ASTPrimaryExpression expr, int pos) {
-        int argsPos = pos + 1;
-        if (hasNodeAtPosition(expr, argsPos)) {
-            JavaNode chainedMethodCall = expr.getChild(pos);
-            JavaNode chainedMethodCallArgs = expr.getChild(argsPos);
-            return isEqualsMethodCall(chainedMethodCall, chainedMethodCallArgs);
+    private String getCalledMethodName(JavaNode methodCall) {
+        String methodName = methodCall.getImage();
+        if (methodName == null) {
+            ASTName name = methodCall.getFirstDescendantOfType(ASTName.class);
+            return name != null ? methodNameFromCallImage(name.getImage()) : null;
         }
-        return false;
+        return methodName;
     }
 
-    private boolean hasNodeAtPosition(ASTPrimaryExpression expr, int pos) {
-        return pos < expr.getNumChildren();
-    }
-
-    private boolean isArgumentOfEqualsMethodCall(ASTPrimaryExpression expr) {
-        ASTPrimarySuffix parentMethodCallArgs = getParentMethodCallArgsSuffix(expr);
-        if (parentMethodCallArgs != null) {
-            List<JavaNode> parentNodes = parentMethodCallArgs.getParent().findChildrenOfType(JavaNode.class);
-            int parentMethodCallIndex = parentNodes.indexOf(parentMethodCallArgs) - 1;
-            JavaNode parentMethodCall = parentNodes.get(parentMethodCallIndex);
-            return isEqualsMethodCall(parentMethodCall, parentMethodCallArgs);
+    private String methodNameFromCallImage(String methodCallImage) {
+        if (methodCallImage.contains(".")) {
+            String[] callParts = methodCallImage.split("\\.");
+            return callParts[1];
         }
-        return false;
-    }
-
-    private ASTPrimarySuffix getParentMethodCallArgsSuffix(ASTPrimaryExpression expr) {
-        JavaNode parent = expr.getParent().getParent(); // ASTArgumentList/ASTExpression/ASTPrimaryExpression
-        return parent instanceof ASTArgumentList
-                ? parent.getFirstParentOfType(ASTPrimarySuffix.class)
-                : null;
-    }
-
-    private boolean isEqualsMethodCall(JavaNode methodCall, JavaNode methodCallArgs) {
-        int methodArgsCount = getCalledMethodArgsCount(methodCallArgs);
-        return EQUALITY_METHODS.contains(methodCall.getImage()) && methodArgsCount == 1;
-    }
-
-    private int getCalledMethodArgsCount(JavaNode methodCallArgs) {
-        ASTArguments args = methodCallArgs.getFirstDescendantOfType(ASTArguments.class);
-        return args != null ? args.size() : -1;
+        return methodCallImage;
     }
 }
