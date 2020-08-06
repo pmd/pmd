5fd0c07e083796c10662d30dc07cf6afc44bcd42
Clément Fournier <clement.fournier76@gmail.com>
2020-08-06 23:23:37 +0200

    Update CompareObjectsWithEquals

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/errorprone/CompareObjectsWithEqualsRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/errorprone/CompareObjectsWithEqualsRule.java
@@ -4,110 +4,52 @@
 
 package net.sourceforge.pmd.lang.java.rule.errorprone;
 
-import net.sourceforge.pmd.lang.ast.Node;
-import net.sourceforge.pmd.lang.java.ast.ASTAllocationExpression;
-import net.sourceforge.pmd.lang.java.ast.ASTEqualityExpression;
-import net.sourceforge.pmd.lang.java.ast.ASTInitializer;
-import net.sourceforge.pmd.lang.java.ast.ASTName;
-import net.sourceforge.pmd.lang.java.ast.ASTPrimaryPrefix;
-import net.sourceforge.pmd.lang.java.ast.ASTPrimarySuffix;
-import net.sourceforge.pmd.lang.java.ast.ASTReferenceType;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import net.sourceforge.pmd.lang.java.ast.ASTArrayAllocation;
+import net.sourceforge.pmd.lang.java.ast.ASTConstructorCall;
+import net.sourceforge.pmd.lang.java.ast.ASTExpression;
+import net.sourceforge.pmd.lang.java.ast.ASTInfixExpression;
 import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
-import net.sourceforge.pmd.lang.java.symboltable.VariableNameDeclaration;
+import net.sourceforge.pmd.lang.java.typeresolution.TypeHelper;
+import net.sourceforge.pmd.lang.rule.RuleTargetSelector;
 
 public class CompareObjectsWithEqualsRule extends AbstractJavaRule {
 
-    private boolean hasName(Node n) {
-        return n.getNumChildren() > 0 && n.getChild(0) instanceof ASTName;
+
+    @Override
+    protected @NonNull RuleTargetSelector buildTargetSelector() {
+        return RuleTargetSelector.forTypes(ASTInfixExpression.class);
     }
 
-    /**
-     * Indicate whether this node is allocating a new object.
-     *
-     * @param n
-     *            node that might be allocating a new object
-     * @return true if child 0 is an AllocationExpression
-     */
-    private boolean isAllocation(Node n) {
-        return n.getNumChildren() > 0 && n.getChild(0) instanceof ASTAllocationExpression
-                && n.getParent().getNumChildren() == 1;
+    /** Indicate whether this node is allocating a new object. */
+    private boolean isAllocation(ASTExpression n) {
+        return n instanceof ASTConstructorCall || n instanceof ASTArrayAllocation;
     }
 
     @Override
-    public Object visit(ASTEqualityExpression node, Object data) {
-        Node c0 = node.getChild(0).getChild(0);
-        Node c1 = node.getChild(1).getChild(0);
+    public Object visit(ASTInfixExpression node, Object data) {
+        if (!node.getOperator().isEquality()) {
+            return data;
+        }
+        ASTExpression left = node.getLeftOperand();
+        ASTExpression right = node.getRightOperand();
 
         // If either side is allocating a new object, there's no way an
         // equals expression is correct
-        if (isAllocation(c0) || isAllocation(c1)) {
+        if (isAllocation(left) || isAllocation(right)) {
             addViolation(data, node);
             return data;
         }
 
-        // skip if either child is not a simple name
-        if (!hasName(c0) || !hasName(c1)) {
+        if (left.getTypeMirror().isPrimitive() || right.getTypeMirror().isPrimitive()
+            || TypeHelper.isA(left, Enum.class) || TypeHelper.isA(right, Enum.class)
+            || TypeHelper.isA(left, Class.class) || TypeHelper.isA(right, Class.class)) {
             return data;
         }
-
-        // skip if either is a qualified name
-        if (isQualifiedName(c0.getChild(0).getImage()) || isQualifiedName(c1.getChild(0).getImage())) {
-            return data;
-        }
-
-        // skip if either is part of a qualified name
-        if (isPartOfQualifiedName(node.getChild(0)) || isPartOfQualifiedName(node.getChild(1))) {
-            return data;
-        }
-
-        // skip static initializers... missing some cases here
-        if (!node.getParentsOfType(ASTInitializer.class).isEmpty()) {
-            return data;
-        }
-
-        ASTName n0 = (ASTName) c0.getChild(0);
-        ASTName n1 = (ASTName) c1.getChild(0);
-
-        if (n0.getNameDeclaration() instanceof VariableNameDeclaration
-                && n1.getNameDeclaration() instanceof VariableNameDeclaration) {
-            VariableNameDeclaration nd0 = (VariableNameDeclaration) n0.getNameDeclaration();
-            VariableNameDeclaration nd1 = (VariableNameDeclaration) n1.getNameDeclaration();
-
-            // skip array dereferences... this misses some cases
-            // FIXME catch comparisons btwn array elements of reference types
-            if (nd0.isArray() || nd1.isArray()) {
-                return data;
-            }
-
-            if (nd0.isReferenceType() && nd1.isReferenceType()) {
-                ASTReferenceType type0 = ((Node) nd0.getAccessNodeParent())
-                        .getFirstDescendantOfType(ASTReferenceType.class);
-                ASTReferenceType type1 = ((Node) nd1.getAccessNodeParent())
-                        .getFirstDescendantOfType(ASTReferenceType.class);
-                // skip, if it is an enum
-                if (type0.getType() != null && type0.getType().equals(type1.getType())
-                    // It may be a custom enum class or an explicit Enum class usage
-                    && (type0.getType().isEnum() || type0.getType() == java.lang.Enum.class)) {
-                    return data;
-                }
-
-                addViolation(data, node);
-            }
-        }
+        addViolation(data, node);
 
         return data;
     }
 
-    /**
-     * Checks whether the given node contains a qualified name, consisting of
-     * one ASTPrimaryPrefix and one or more ASTPrimarySuffix nodes.
-     *
-     * @param node
-     *            the node
-     * @return <code>true</code> if it is a qualified name
-     */
-    private boolean isPartOfQualifiedName(Node node) {
-        return node.getChild(0) instanceof ASTPrimaryPrefix
-                && !node.findChildrenOfType(ASTPrimarySuffix.class).isEmpty();
-    }
 }
