d1c49baae1d0e418cee47c2e464ab5a9c6294755
Andreas Dangel <andreas.dangel@pmd-code.org>
2020-08-20 11:02:55 +0200

    Fix checkstyle
    (note, this needs an updated build-tools)

--- pmd-kotlin/src/test/resources/net/sourceforge/pmd/lang/kotlin/cpd/testdata/tabWidth.kt
+++ pmd-kotlin/src/test/resources/net/sourceforge/pmd/lang/kotlin/cpd/testdata/tabWidth.kt
@@ -1,5 +1,11 @@
+/*
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
 var x = 0
 
 fun increment() {
+// CHECKSTYLE:OFF  - the tab character is the test...
 	x += 1
+// CHECKSTYLE:ON
 }
