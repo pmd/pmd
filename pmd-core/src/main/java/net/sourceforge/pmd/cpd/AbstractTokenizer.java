21aeabdf4547f2dd46e82badb065fc8bbc03e433
Clément Fournier <clement.fournier76@gmail.com>
2020-09-01 17:48:07 +0200

    Deprecate AbstractTokenizer

--- pmd-core/src/main/java/net/sourceforge/pmd/cpd/AbstractTokenizer.java
+++ pmd-core/src/main/java/net/sourceforge/pmd/cpd/AbstractTokenizer.java
@@ -7,11 +7,17 @@ package net.sourceforge.pmd.cpd;
 import java.util.List;
 import java.util.Locale;
 
+import net.sourceforge.pmd.annotation.DeprecatedUntil700;
+
 /**
  *
  * @author Zev Blut zb@ubit.com
  * @author Romain PELISSE belaran@gmail.com
+ *
+ * @deprecated Use an {@link AnyTokenizer} instead, it's basically as powerful.
  */
+@Deprecated
+@DeprecatedUntil700
 public abstract class AbstractTokenizer implements Tokenizer {
 
     // FIXME depending on subclasses to assign local vars is rather fragile -
