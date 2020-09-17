8052d87727abd6c8900bd0ecabe4e361fb19ec53
Clément Fournier <clement.fournier76@gmail.com>
2020-09-17 23:07:46 +0200

    Fix compil

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/ast/TokenUtils.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/ast/TokenUtils.java
@@ -4,7 +4,6 @@
 
 package net.sourceforge.pmd.lang.java.ast;
 
-import java.util.Comparator;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.Objects;
@@ -22,37 +21,17 @@ final class TokenUtils {
 
     // mind: getBeginLine and getEndLine on JavaccToken are now very slow.
 
-    /**
-     * Assumes no two tokens overlap, and that the two tokens are from
-     * the same document.
-     */
-    private static final Comparator<GenericToken> TOKEN_POS_COMPARATOR
-        = Comparator.comparingInt(GenericToken::getStartInDocument);
-
     private TokenUtils() {
 
     }
 
-    public static int compare(GenericToken t1, GenericToken t2) {
-        return TOKEN_POS_COMPARATOR.compare(t1, t2);
-    }
 
-    public static boolean isBefore(GenericToken t1, GenericToken t2) {
-        return t1.getStartInDocument() < t2.getStartInDocument();
-    }
-
-    public static boolean isAfter(GenericToken t1, GenericToken t2) {
-        return t1.getStartInDocument() > t2.getStartInDocument();
-
-    }
-
-
-    public static <T extends GenericToken> T nthFollower(T token, int n) {
+    public static <T extends GenericToken<T>> T nthFollower(T token, int n) {
         if (n < 0) {
             throw new IllegalArgumentException("Negative index?");
         }
         while (n-- > 0 && token != null) {
-            token = (T) token.getNext();
+            token = token.getNext();
         }
         if (token == null) {
             throw new NoSuchElementException("No such token");
@@ -77,8 +56,8 @@ final class TokenUtils {
      * @throws NoSuchElementException If there's less than n tokens to the left of the anchor.
      */
     // test only
-    public static <T extends GenericToken> T nthPrevious(T startHint, T anchor, int n) {
-        if (compare(startHint, anchor) >= 0) {
+    public static <T extends GenericToken<T>> T nthPrevious(T startHint, T anchor, int n) {
+        if (startHint.compareTo(anchor) >= 0) {
             throw new IllegalStateException("Wrong left hint, possibly not left enough");
         }
         if (n <= 0) {
@@ -88,12 +67,12 @@ final class TokenUtils {
         T target = startHint;
         T current = startHint;
         while (current != null && !current.equals(anchor)) {
-            current = (T) current.getNext();
+            current = current.getNext();
             // wait "n" iterations before starting to advance the target
             // then advance "target" at the same rate as "current", but
             // "n" tokens to the left
             if (numAway == n) {
-                target = (T) target.getNext();
+                target = target.getNext();
             } else {
                 numAway++;
             }
