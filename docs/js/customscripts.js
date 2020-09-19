ebddad5d26614a5fa2abf5edc41cfceec5c489b5
Andreas Dangel <andreas.dangel@pmd-code.org>
2020-09-19 21:21:35 +0200

    [doc] Fix sidebar quirks

--- docs/js/customscripts.js
+++ docs/js/customscripts.js
@@ -1,16 +1,16 @@
-
-$('#mysidebar').height($(".nav").height());
-
-
 $( document ).ready(function() {
 
-    //this script says, if the height of the viewport is greater than 800px, then insert affix class, which makes the nav bar float in a fixed
-    // position as your scroll. if you have a lot of nav items, this height may not work for you.
+    $('#mysidebar').height($(".nav").height());
+
+    // this script says, if the height of the viewport is greater than 800px, then insert position-fixed class,
+    // which makes the nav bar float in a fixed position as your scroll. If you have a lot of nav items,
+    // this height may not work for you.
     var h = $(window).height();
     //console.log (h);
     if (h > 800) {
-        $( "#mysidebar" ).attr("class", "nav affix");
+        $( "#mysidebar" ).attr("class", "nav position-fixed");
     }
+
     // activate tooltips. although this is a bootstrap js function, it must be activated this way in your theme.
     $('[data-toggle="tooltip"]').tooltip({
         placement : 'top'
