5c047b0e3e55999ea9aab0b4317cc1c976a93d60
Clément Fournier <clement.fournier76@gmail.com>
2020-09-28 20:13:59 +0200

    Fix java tests

--- pmd-core/src/main/java/net/sourceforge/pmd/lang/rule/RuleViolationFactory.java
+++ pmd-core/src/main/java/net/sourceforge/pmd/lang/rule/RuleViolationFactory.java
@@ -8,7 +8,6 @@ import java.text.MessageFormat;
 
 import org.apache.commons.lang3.StringUtils;
 import org.checkerframework.checker.nullness.qual.NonNull;
-import org.checkerframework.checker.nullness.qual.Nullable;
 
 import net.sourceforge.pmd.Report.SuppressedViolation;
 import net.sourceforge.pmd.Rule;
@@ -26,6 +25,10 @@ import net.sourceforge.pmd.lang.ast.Node;
  * instances through {@link LanguageVersionHandler#getRuleViolationFactory()}.
  */
 public interface RuleViolationFactory {
+    // todo, upcoming refactorings will remove the deprecated addViolation
+    //  and move the formatting logic into RuleContext
+
+
     /**
      * Adds a violation to the report.
      *
@@ -47,9 +50,8 @@ public interface RuleViolationFactory {
 
 
     @Deprecated
-    default void addViolation(RuleContext ruleContext, Rule rule, @Nullable Node node, @NonNull String message, int beginLine, int endLine, Object[] args) {
-        String formattedMessage = formatMessage(message, args);
-        RuleViolation rv = createViolation(rule, node, ruleContext.getSourceCodeFilename(), formattedMessage);
+    default void addViolation(RuleContext ruleContext, Rule rule, @NonNull Node node, @NonNull String message, int beginLine, int endLine, @NonNull Object[] args) {
+        RuleViolation rv = formatViolation(rule, node, ruleContext.getSourceCodeFilename(), message, args);
         ((ParametricRuleViolation<?>) rv).setLines(beginLine, endLine);
         SuppressedViolation suppressed = suppressOrNull(node, rv);
         if (suppressed != null) {
@@ -60,17 +62,21 @@ public interface RuleViolationFactory {
     }
 
 
-    default RuleViolation createViolation(Rule rule, @NonNull Node location, @NonNull String filename, @NonNull String formattedMessage) {
-        return null;
+    RuleViolation createViolation(Rule rule, @NonNull Node location, @NonNull String filename, @NonNull String formattedMessage);
+
+    default RuleViolation formatViolation(Rule rule, @NonNull Node location, @NonNull String filename, @NonNull String messageTemplate, @NonNull Object[] formatArgs) {
+        return createViolation(rule, location, filename, formatMessage(messageTemplate, formatArgs));
     }
 
 
     SuppressedViolation suppressOrNull(Node location, RuleViolation violation);
 
-    static String formatMessage(@NonNull String message, Object[] args) {
+    static String formatMessage(@NonNull String message, @NonNull Object[] args) {
+        // todo merge that with the formatting that ParametricRuleViolation does...
+
         // Escape PMD specific variable message format, specifically the {
         // in the ${, so MessageFormat doesn't bitch.
         final String escapedMessage = StringUtils.replace(message, "${", "$'{'");
-        return MessageFormat.format(escapedMessage, args != null ? args : new Object[0]);
+        return MessageFormat.format(escapedMessage, args);
     }
 }
