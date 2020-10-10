1150e61ddd648b20a5a1ca81b7a2d36fdcfca73e
Andreas Dangel <andreas.dangel@pmd-code.org>
2020-10-10 12:50:12 +0200

    [java] Rename DoNotCallSystemExit -> DoNotTerminateVM

--- pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/errorprone/DoNotTerminateVM.java
+++ pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/errorprone/DoNotTerminateVM.java
@@ -1,4 +1,4 @@
-/**
+/*
  * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
  */
 
@@ -6,6 +6,6 @@ package net.sourceforge.pmd.lang.java.rule.errorprone;
 
 import net.sourceforge.pmd.testframework.PmdRuleTst;
 
-public class DoNotCallSystemExitTest extends PmdRuleTst {
+public class DoNotTerminateVM extends PmdRuleTst {
     // no additional unit tests
 }
