e3804da978946f6d73fa7a38e24e047146e64b60
Clément Fournier <clement.fournier76@gmail.com>
2020-10-13 23:04:43 +0200

    Update and rename DoNotTerminateVM.java to DoNotTerminateVMTest.java

--- pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/errorprone/DoNotTerminateVMTest.java
+++ pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/errorprone/DoNotTerminateVMTest.java
@@ -6,6 +6,6 @@ package net.sourceforge.pmd.lang.java.rule.errorprone;
 
 import net.sourceforge.pmd.testframework.PmdRuleTst;
 
-public class DoNotTerminateVM extends PmdRuleTst {
+public class DoNotTerminateVMTest extends PmdRuleTst {
     // no additional unit tests
 }
