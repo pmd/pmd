0745789bb72ce2a283b469a6d96d10de1241994a
Andreas Dangel <andreas.dangel@pmd-code.org>
2020-10-22 17:58:06 +0200

    [apex] Remove deprecated rules from quickstart.xml, restore Test

--- pmd-apex/src/test/java/net/sourceforge/pmd/lang/apex/DefaultRulesetTest.java
+++ pmd-apex/src/test/java/net/sourceforge/pmd/lang/apex/DefaultRulesetTest.java
@@ -4,12 +4,9 @@
 
 package net.sourceforge.pmd.lang.apex;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Handler;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
-import java.util.regex.Pattern;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -20,13 +17,13 @@ import org.junit.contrib.java.lang.system.SystemErrRule;
 import net.sourceforge.pmd.RulePriority;
 import net.sourceforge.pmd.RuleSet;
 import net.sourceforge.pmd.RuleSetFactory;
-import net.sourceforge.pmd.util.ResourceLoader;
+import net.sourceforge.pmd.RulesetsFactoryUtils;
 
 public class DefaultRulesetTest {
     @Rule
     public final SystemErrRule systemErrRule = new SystemErrRule().enableLog().muteForSuccessfulTests();
 
-    private RuleSetFactory factory = new RuleSetFactory(new ResourceLoader(), RulePriority.LOW, true, false);
+    private RuleSetFactory factory = RulesetsFactoryUtils.createFactory(RulePriority.LOW, true, false);
 
     @Test
     public void loadDefaultRuleset() throws Exception {
@@ -44,11 +41,10 @@ public class DefaultRulesetTest {
 
     @Test
     public void loadQuickstartRuleset() throws Exception {
-        List<String> logRecords = new ArrayList<>();
         Logger.getLogger(RuleSetFactory.class.getName()).addHandler(new Handler() {
             @Override
             public void publish(LogRecord record) {
-                logRecords.add(record.getMessage());
+                Assert.fail("No Logging expected: " + record.getMessage());
             }
 
             @Override
@@ -61,15 +57,5 @@ public class DefaultRulesetTest {
         });
         RuleSet ruleset = factory.createRuleSet("rulesets/apex/quickstart.xml");
         Assert.assertNotNull(ruleset);
-
-        // Expect 6 log records that correspond to the 3 deprecated rules.
-        // Each deprecated rule has two log lines of the form
-        // Discontinue using Rule name category/apex/performance.xml/AvoidSoqlInLoops as it is scheduled for removal from PMD. PMD 7.0.0 will remove support for this Rule.
-        // Use Rule name category/apex/performance.xml/AvoidSoqlInLoops instead of the deprecated Rule name rulesets/apex/quickstart.xml/AvoidSoqlInLoops. PMD 7.0.0 will remove support for this deprecated Rule name usage.
-        Assert.assertEquals(logRecords.toString(), 6, logRecords.size());
-        Pattern expectedDeprecatedRuleMessages = Pattern.compile(".*(AvoidDmlStatementsInLoops|AvoidSoqlInLoops|AvoidSoslInLoops) (as it is scheduled for removal from PMD|instead of the deprecated Rule name).*");
-        for (String logRecord : logRecords) {
-            Assert.assertTrue(logRecord, expectedDeprecatedRuleMessages.matcher(logRecord).matches());
-        }
     }
 }
