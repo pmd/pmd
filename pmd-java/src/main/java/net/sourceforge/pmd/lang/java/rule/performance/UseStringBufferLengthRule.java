16a44152db6beda0a08ad7e89e9c0ffffc32aaea
Clément Fournier <clement.fournier76@gmail.com>
2020-10-30 11:18:37 +0100

    Update UseStringBufferLength

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/performance/UseStringBufferLengthRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/performance/UseStringBufferLengthRule.java
@@ -4,18 +4,10 @@
 
 package net.sourceforge.pmd.lang.java.rule.performance;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-import net.sourceforge.pmd.lang.ast.Node;
-import net.sourceforge.pmd.lang.java.ast.ASTArgumentList;
-import net.sourceforge.pmd.lang.java.ast.ASTLiteral;
-import net.sourceforge.pmd.lang.java.ast.ASTMethodDeclaration;
-import net.sourceforge.pmd.lang.java.ast.ASTName;
-import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
-import net.sourceforge.pmd.lang.java.symboltable.VariableNameDeclaration;
-import net.sourceforge.pmd.lang.symboltable.NameDeclaration;
+import net.sourceforge.pmd.lang.java.ast.ASTMethodCall;
+import net.sourceforge.pmd.lang.java.ast.JavaNode;
+import net.sourceforge.pmd.lang.java.rule.AbstractJavaRulechainRule;
+import net.sourceforge.pmd.lang.java.types.TypeTestUtil;
 
 /**
  * This rule finds places where StringBuffer.toString() is called just to see if
@@ -34,99 +26,30 @@ import net.sourceforge.pmd.lang.symboltable.NameDeclaration;
  * @author acaplan
  * @author Philip Graf
  */
-public class UseStringBufferLengthRule extends AbstractJavaRule {
+public class UseStringBufferLengthRule extends AbstractJavaRulechainRule {
 
-    // FIXME Need to remove this somehow.
-    /*
-     * Specifically, we need a symbol tree that can be traversed downwards, so
-     * that instead of visiting each name and then visiting the declaration for
-     * that name, we should visit all the declarations and check their usages.
-     * With that in place, this rule would be reduced to: - find all
-     * StringBuffer declarations - check each usage - flag those that involve
-     * variable.toString()
-     */
-    private Set<NameDeclaration> alreadySeen = new HashSet<>();
-
-    @Override
-    public Object visit(ASTMethodDeclaration acu, Object data) {
-        alreadySeen.clear();
-        return super.visit(acu, data);
+    public UseStringBufferLengthRule() {
+        super(ASTMethodCall.class);
     }
 
     @Override
-    public Object visit(ASTName decl, Object data) {
-        if (!decl.getImage().endsWith("toString")) {
-            return data;
+    public Object visit(ASTMethodCall call, Object data) {
+        if (call.getMethodName().equals("toString")
+            && call.getArguments().size() == 0
+            && TypeTestUtil.isA(CharSequence.class, call.getQualifier())
+            && !TypeTestUtil.isA(String.class, call.getQualifier())
+            && isLengthCall(call.getParent())) {
+            addViolation(data, call.getParent());
         }
-        NameDeclaration nd = decl.getNameDeclaration();
-        if (nd == null) {
-            return data;
-        }
-        if (alreadySeen.contains(nd) || !(nd instanceof VariableNameDeclaration)
-                || !ConsecutiveLiteralAppendsRule.isStringBuilderOrBuffer(((VariableNameDeclaration) nd).getDeclaratorId())) {
-            return data;
-        }
-        alreadySeen.add(nd);
-
-        if (isViolation(decl)) {
-            addViolation(data, decl);
-        }
-
         return data;
     }
 
-    /**
-     * Returns true for the following violations:
-     *
-     * <pre>
-     * StringBuffer sb = new StringBuffer(&quot;some string&quot;);
-     * if (sb.toString().equals(&quot;&quot;)) {
-     *     // this is a violation
-     * }
-     * if (sb.toString().length() == 0) {
-     *     // this is a violation
-     * }
-     * if (sb.length() == 0) {
-     *     // this is ok
-     * }
-     * </pre>
-     */
-    private boolean isViolation(ASTName decl) {
-        // the (grand)parent of a violation has four children
-        Node parent = decl.getParent().getParent();
-        if (parent.getNumChildren() == 4) {
-            // 1. child: sb.toString where sb is a VariableNameDeclaration for a
-            // StringBuffer or StringBuilder
-            if (parent.getChild(0).getFirstChildOfType(ASTName.class).getImage().endsWith(".toString")) {
-                // 2. child: the arguments of toString
-                // no need to check as both StringBuffer and StringBuilder only
-                // have one toString method
-                // 3. child: equals or length, 4. child: their arguments
-                return isEqualsViolation(parent) || isLengthViolation(parent);
-            }
+    private boolean isLengthCall(JavaNode node) {
+        if (node instanceof ASTMethodCall) {
+            ASTMethodCall call = (ASTMethodCall) node;
+            return call.getMethodName().equals("length")
+                && call.getArguments().size() == 0;
         }
         return false;
     }
-
-    private boolean isEqualsViolation(Node parent) {
-        // 3. child: equals
-        if (parent.getChild(2).hasImageEqualTo("equals")) {
-            // 4. child: the arguments of equals, there must be exactly one and
-            // it must be ""
-            List<ASTArgumentList> argList = parent.getChild(3).findDescendantsOfType(ASTArgumentList.class);
-            if (argList.size() == 1) {
-                List<ASTLiteral> literals = argList.get(0).findDescendantsOfType(ASTLiteral.class);
-                return literals.size() == 1 && literals.get(0).hasImageEqualTo("\"\"");
-            }
-        }
-        return false;
-    }
-
-    private boolean isLengthViolation(Node parent) {
-        // 3. child: length
-        return parent.getChild(2).hasImageEqualTo("length");
-        // 4. child: the arguments of length
-        // no need to check as String has only one length method
-    }
-
 }
