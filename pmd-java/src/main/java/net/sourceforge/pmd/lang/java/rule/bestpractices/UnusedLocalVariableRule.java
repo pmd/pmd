bbc71da28926ca6f0242eb7fe31f2298a031ba8a
Clément Fournier <clement.fournier76@gmail.com>
2020-10-30 02:05:38 +0100

    Update UnusedLocalVariable

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/bestpractices/UnusedLocalVariableRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/bestpractices/UnusedLocalVariableRule.java
@@ -4,46 +4,42 @@
 
 package net.sourceforge.pmd.lang.java.rule.bestpractices;
 
-import java.util.List;
+import org.checkerframework.checker.nullness.qual.NonNull;
 
+import net.sourceforge.pmd.lang.java.ast.ASTArgumentList;
+import net.sourceforge.pmd.lang.java.ast.ASTAssignableExpr.ASTNamedReferenceExpr;
+import net.sourceforge.pmd.lang.java.ast.ASTAssignableExpr.AccessType;
 import net.sourceforge.pmd.lang.java.ast.ASTLocalVariableDeclaration;
-import net.sourceforge.pmd.lang.java.ast.ASTVariableDeclarator;
+import net.sourceforge.pmd.lang.java.ast.ASTUnaryExpression;
 import net.sourceforge.pmd.lang.java.ast.ASTVariableDeclaratorId;
 import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
-import net.sourceforge.pmd.lang.java.symboltable.JavaNameOccurrence;
-import net.sourceforge.pmd.lang.symboltable.NameOccurrence;
+import net.sourceforge.pmd.lang.rule.RuleTargetSelector;
+import net.sourceforge.pmd.util.CollectionUtil;
 
 public class UnusedLocalVariableRule extends AbstractJavaRule {
 
-    public UnusedLocalVariableRule() {
-        addRuleChainVisit(ASTLocalVariableDeclaration.class);
+    @Override
+    protected @NonNull RuleTargetSelector buildTargetSelector() {
+        return RuleTargetSelector.forTypes(ASTLocalVariableDeclaration.class);
     }
 
     @Override
     public Object visit(ASTLocalVariableDeclaration decl, Object data) {
-        for (int i = 0; i < decl.getNumChildren(); i++) {
-            if (!(decl.getChild(i) instanceof ASTVariableDeclarator)) {
-                continue;
-            }
-            ASTVariableDeclaratorId node = (ASTVariableDeclaratorId) decl.getChild(i).getChild(0);
-            // TODO this isArray() check misses some cases
-            // need to add DFAish code to determine if an array
-            // is initialized locally or gotten from somewhere else
-            if (!node.getNameDeclaration().isArray() && !actuallyUsed(node.oldGetUsages())) {
-                addViolation(data, node, node.getNameDeclaration().getImage());
+        for (ASTVariableDeclaratorId varId : decl.getVarIds()) {
+            if (hasReadUsage(varId)) {
+                addViolation(data, varId, varId.getName());
             }
         }
         return data;
     }
 
-    private boolean actuallyUsed(List<NameOccurrence> usages) {
-        for (NameOccurrence occ : usages) {
-            JavaNameOccurrence jocc = (JavaNameOccurrence) occ;
-            if (!jocc.isOnLeftHandSide()) {
-                return true;
-            }
-        }
-        return false;
+    static boolean hasReadUsage(ASTVariableDeclaratorId varId) {
+        return CollectionUtil.none(varId.getUsages(), UnusedLocalVariableRule::isReadUsage);
     }
 
+    private static boolean isReadUsage(ASTNamedReferenceExpr expr) {
+        return expr.getAccessType() == AccessType.READ
+            // foo(x++)
+            || expr.getParent() instanceof ASTUnaryExpression && expr.getParent().getParent() instanceof ASTArgumentList;
+    }
 }
