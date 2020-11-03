7d6e7b1522e39eb4daa5e35713664e81d65b2efc
Clément Fournier <clement.fournier76@gmail.com>
2020-11-03 07:47:13 +0100

    Support field access

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/codestyle/UnnecessaryFullyQualifiedNameRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/codestyle/UnnecessaryFullyQualifiedNameRule.java
@@ -7,22 +7,29 @@ package net.sourceforge.pmd.lang.java.rule.codestyle;
 import static net.sourceforge.pmd.properties.PropertyFactory.booleanProperty;
 
 import java.util.List;
+import java.util.function.BiPredicate;
+import java.util.function.Function;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 import net.sourceforge.pmd.internal.util.AssertionUtil;
 import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceType;
+import net.sourceforge.pmd.lang.java.ast.ASTFieldAccess;
 import net.sourceforge.pmd.lang.java.ast.ASTMethodCall;
 import net.sourceforge.pmd.lang.java.ast.ASTTypeExpression;
+import net.sourceforge.pmd.lang.java.ast.JavaNode;
 import net.sourceforge.pmd.lang.java.rule.AbstractJavaRulechainRule;
+import net.sourceforge.pmd.lang.java.symbols.JAccessibleElementSymbol;
 import net.sourceforge.pmd.lang.java.symbols.JClassSymbol;
+import net.sourceforge.pmd.lang.java.symbols.JElementSymbol;
+import net.sourceforge.pmd.lang.java.symbols.JFieldSymbol;
 import net.sourceforge.pmd.lang.java.symbols.JTypeDeclSymbol;
 import net.sourceforge.pmd.lang.java.symbols.table.JSymbolTable;
 import net.sourceforge.pmd.lang.java.symbols.table.ScopeInfo;
+import net.sourceforge.pmd.lang.java.symbols.table.coreimpl.ShadowChain;
 import net.sourceforge.pmd.lang.java.symbols.table.coreimpl.ShadowChainIterator;
 import net.sourceforge.pmd.lang.java.types.JMethodSig;
-import net.sourceforge.pmd.lang.java.types.JTypeMirror;
 import net.sourceforge.pmd.properties.PropertyDescriptor;
 
 public class UnnecessaryFullyQualifiedNameRule extends AbstractJavaRulechainRule {
@@ -55,13 +62,13 @@ public class UnnecessaryFullyQualifiedNameRule extends AbstractJavaRulechainRule
         ASTClassOrInterfaceType next = deepest;
         ScopeInfo bestReason = null;
         if (next.isFullyQualified()) {
-            bestReason = checkMeansSame(next);
+            bestReason = typeMeansSame(next);
         }
 
         // try to find the longest prefix that can be removed
         while (bestReason != null && segmentIsIrrelevant(next) && next.getParent() instanceof ASTClassOrInterfaceType) {
             ASTClassOrInterfaceType nextParent = (ASTClassOrInterfaceType) next.getParent();
-            ScopeInfo newBestReason = checkMeansSame(nextParent);
+            ScopeInfo newBestReason = typeMeansSame(nextParent);
             if (newBestReason == null) {
                 break;
             } else {
@@ -70,24 +77,36 @@ public class UnnecessaryFullyQualifiedNameRule extends AbstractJavaRulechainRule
             }
         }
 
-        // maybe a method call can still take precedence?
-        if (getProperty(REPORT_METHODS)
-            && next.getParent() instanceof ASTTypeExpression
-            && next.getParent().getParent() instanceof ASTMethodCall) {
-            ASTMethodCall methodCall = (ASTMethodCall) next.getParent().getParent();
-            if (methodCall.getExplicitTypeArguments() == null
-                && methodProbablyMeansSame(methodCall)) {
-                // we don't actually know where the method came from
-                String simpleName = formatMethodName(next, methodCall);
-                String unnecessary = produceQualifier(deepest, next, true);
-                addViolation(data, next, new Object[] {unnecessary, simpleName, ""});
-                return null;
+        // maybe a method call/field can still take precedence
+        if (next.getParent() instanceof ASTTypeExpression) {
+
+            JavaNode opa = next.getParent().getParent();
+            if (getProperty(REPORT_METHODS) && opa instanceof ASTMethodCall) {
+                ASTMethodCall methodCall = (ASTMethodCall) opa;
+                if (methodCall.getExplicitTypeArguments() == null
+                    && methodProbablyMeansSame(methodCall)) {
+                    // we don't actually know where the method came from
+                    String simpleName = formatMemberName(next, methodCall.getMethodType().getSymbol());
+                    String unnecessary = produceQualifier(deepest, next, true);
+                    addViolation(data, next, new Object[] {unnecessary, simpleName, ""});
+                    return null;
+                }
+            } else if (getProperty(REPORT_FIELDS) && opa instanceof ASTFieldAccess) {
+                ASTFieldAccess fieldAccess = (ASTFieldAccess) opa;
+                ScopeInfo reasonForFieldInScope = fieldMeansSame(fieldAccess);
+                if (reasonForFieldInScope != null) {
+                    String simpleName = formatMemberName(next, fieldAccess.getReferencedSym());
+                    String reasonToString = unnecessaryReasonWrapper(reasonForFieldInScope);
+                    String unnecessary = produceQualifier(deepest, next, true);
+                    addViolation(data, next, new Object[] {unnecessary, simpleName, reasonToString});
+                    return null;
+                }
             }
         }
 
         if (bestReason != null) {
             String simpleName = next.getSimpleName();
-            String reasonToString = unnecessaryReasonForType(bestReason);
+            String reasonToString = unnecessaryReasonWrapper(bestReason);
             String unnecessary = produceQualifier(deepest, next, false);
             addViolation(data, next, new Object[] {unnecessary, simpleName, reasonToString});
         }
@@ -122,11 +141,11 @@ public class UnnecessaryFullyQualifiedNameRule extends AbstractJavaRulechainRule
      * Checks that the type name can be referred to by simple name in the
      * given scope, which means, that the qualification can be dropped.
      * If the symbol table for types yields the same symbol when referred
-     * to by name, then this is true.
+     * to by simple name, then this is true.
      *
      * @return The reason why the type is in scope. Null if it's not in scope.
      */
-    private static @Nullable ScopeInfo checkMeansSame(@NonNull ASTClassOrInterfaceType typeNode) {
+    private static @Nullable ScopeInfo typeMeansSame(@NonNull ASTClassOrInterfaceType typeNode) {
         JTypeDeclSymbol sym = typeNode.getTypeMirror().getSymbol();
         if (sym == null || sym.isUnresolved()) {
             return null;
@@ -136,22 +155,13 @@ public class UnnecessaryFullyQualifiedNameRule extends AbstractJavaRulechainRule
         if (symTable.variables().resolveFirst(sym.getSimpleName()) != null) {
             return null; //name is obscured: https://docs.oracle.com/javase/specs/jls/se15/html/jls-6.html#jls-6.4.2
         }
-        ShadowChainIterator<JTypeMirror, ScopeInfo> iter =
-            symTable.types().iterateResults(sym.getSimpleName());
 
-        if (iter.hasNext()) {
-            iter.next();
-            List<JTypeMirror> results = iter.getResults();
-            if (results.size() == 1) { // otherwise ambiguous
-                if (sym.equals(results.get(0).getSymbol())) {
-                    return iter.getScopeTag();
-                }
-            }
-            // not unnecessary
-            return null;
-        }
-        // unknown symbol
-        return null;
+        return fieldOrTypeMeansSame(
+            sym,
+            typeNode.getSymbolTable(),
+            JSymbolTable::types,
+            (s, t) -> s.equals(t.getSymbol())
+        );
     }
 
     private static boolean methodProbablyMeansSame(ASTMethodCall call) {
@@ -174,15 +184,52 @@ public class UnnecessaryFullyQualifiedNameRule extends AbstractJavaRulechainRule
         return true;
     }
 
-    private static String formatMethodName(ASTClassOrInterfaceType qualifier, ASTMethodCall call) {
-        JClassSymbol methodOwner = call.getMethodType().getSymbol().getEnclosingClass();
-        if (!methodOwner.equals(qualifier.getTypeMirror().getSymbol())) {
-            return methodOwner.getSimpleName() + "::" + call.getMethodName();
+    private static ScopeInfo fieldMeansSame(ASTFieldAccess field) {
+        JFieldSymbol sym = field.getReferencedSym();
+        if (sym == null || sym.isUnresolved()) {
+            return null;
         }
-        return call.getMethodName();
+
+        return fieldOrTypeMeansSame(
+            sym,
+            field.getSymbolTable(),
+            JSymbolTable::variables,
+            (s, t) -> s.equals(t.getSymbol())
+        );
     }
 
-    private static String unnecessaryReasonForType(ScopeInfo scopeInfo) {
+    private static <S extends JElementSymbol, T> ScopeInfo fieldOrTypeMeansSame(@NonNull S originalSym,
+                                                                                JSymbolTable symTable,
+                                                                                Function<JSymbolTable, ShadowChain<T, ScopeInfo>> shadowChainGetter,
+                                                                                BiPredicate<S, T> areEqual) {
+
+        ShadowChainIterator<T, ScopeInfo> iter =
+            shadowChainGetter.apply(symTable).iterateResults(originalSym.getSimpleName());
+
+        if (iter.hasNext()) {
+            iter.next();
+            List<T> results = iter.getResults();
+            if (results.size() == 1) { // otherwise ambiguous
+                if (areEqual.test(originalSym, results.get(0))) {
+                    return iter.getScopeTag();
+                }
+            }
+            // not unnecessary
+            return null;
+        }
+        // unknown symbol
+        return null;
+    }
+
+    private static String formatMemberName(ASTClassOrInterfaceType qualifier, JAccessibleElementSymbol call) {
+        JClassSymbol methodOwner = call.getEnclosingClass();
+        if (methodOwner != null && !methodOwner.equals(qualifier.getTypeMirror().getSymbol())) {
+            return methodOwner.getSimpleName() + "::" + call.getSimpleName();
+        }
+        return call.getSimpleName();
+    }
+
+    private static String unnecessaryReasonWrapper(ScopeInfo scopeInfo) {
         return " because it is " + unnecessaryReason(scopeInfo);
     }
 
