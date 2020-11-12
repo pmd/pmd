c7022c4881a206e8b914a44eeb4287972a6b5955
Gunther Schrijvers <gunther@familieschrijvers.be>
2020-11-12 13:05:03 +0100

    [core] Corrected constant naming from SEPERATOR to SEPARATOR.

--- pmd-core/src/main/java/net/sourceforge/pmd/renderers/TextRenderer.java
+++ pmd-core/src/main/java/net/sourceforge/pmd/renderers/TextRenderer.java
@@ -16,9 +16,9 @@ import net.sourceforge.pmd.RuleViolation;
  */
 public class TextRenderer extends AbstractIncrementingRenderer {
 
-    private static final Character SMALL_SEPERATOR = ':';
-    private static final String MEDIUM_SEPERATOR = ":\t";
-    private static final String LARGE_SEPERATOR = "\t-\t";
+    private static final Character SMALL_SEPARATOR = ':';
+    private static final String MEDIUM_SEPARATOR = ":\t";
+    private static final String LARGE_SEPARATOR = "\t-\t";
 
     public static final String NAME = "text";
 
@@ -39,9 +39,9 @@ public class TextRenderer extends AbstractIncrementingRenderer {
             buf.setLength(0);
             RuleViolation rv = violations.next();
             buf.append(determineFileName(rv.getFilename()));
-            buf.append(SMALL_SEPERATOR).append(rv.getBeginLine());
-            buf.append(MEDIUM_SEPERATOR).append(rv.getDescription());
-            buf.append(MEDIUM_SEPERATOR).append(rv.getRule().getName()).append(PMD.EOL);
+            buf.append(SMALL_SEPARATOR).append(rv.getBeginLine());
+            buf.append(MEDIUM_SEPARATOR).append(rv.getDescription());
+            buf.append(MEDIUM_SEPARATOR).append(rv.getRule().getName()).append(PMD.EOL);
             writer.write(buf.toString());
         }
     }
@@ -53,7 +53,7 @@ public class TextRenderer extends AbstractIncrementingRenderer {
         for (Report.ProcessingError error : errors) {
             buf.setLength(0);
             buf.append(determineFileName(error.getFile()));
-            buf.append(LARGE_SEPERATOR).append(error.getMsg()).append(PMD.EOL);
+            buf.append(LARGE_SEPARATOR).append(error.getMsg()).append(PMD.EOL);
             writer.write(buf.toString());
         }
 
@@ -69,7 +69,7 @@ public class TextRenderer extends AbstractIncrementingRenderer {
         for (Report.ConfigurationError error : configErrors) {
             buf.setLength(0);
             buf.append(error.rule().getName());
-            buf.append(LARGE_SEPERATOR).append(error.issue()).append(PMD.EOL);
+            buf.append(LARGE_SEPARATOR).append(error.issue()).append(PMD.EOL);
             writer.write(buf.toString());
         }
     }
