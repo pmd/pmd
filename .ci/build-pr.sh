9877ea7f96afd3a71b5e8d645eda4242ab46b47b
Andreas Dangel <andreas.dangel@pmd-code.org>
2020-11-13 14:44:39 +0100

    Add logging groups

--- .ci/build-pr.sh
+++ .ci/build-pr.sh
@@ -10,13 +10,17 @@ set -e
 
 maven_dependencies_resolve
 
+log_group_start "Building with maven"
 ./mvnw -e -V clean verify
+log_group_end
+
 
 # Danger is executed only on the linux runner
 case "$(uname)" in
     Linux*)
-        log_info "Executing danger..."
+        log_group_start "Executing danger"
         regression_tester_setup_ci
         regression_tester_executeDanger
+        log_group_end
         ;;
 esac
