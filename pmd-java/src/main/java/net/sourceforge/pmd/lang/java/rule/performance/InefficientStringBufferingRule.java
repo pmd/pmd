2621e9c63687e03661016c720cc1324f3183ae89
Clément Fournier <clement.fournier76@gmail.com>
2020-11-18 17:48:10 +0100

    Update InefficientStringBuffering

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/performance/InefficientStringBufferingRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/performance/InefficientStringBufferingRule.java
@@ -4,31 +4,18 @@
 
 package net.sourceforge.pmd.lang.java.rule.performance;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
+import net.sourceforge.pmd.RuleContext;
 import net.sourceforge.pmd.lang.ast.Node;
-import net.sourceforge.pmd.lang.java.ast.ASTAdditiveExpression;
-import net.sourceforge.pmd.lang.java.ast.ASTAllocationExpression;
 import net.sourceforge.pmd.lang.java.ast.ASTArgumentList;
-import net.sourceforge.pmd.lang.java.ast.ASTBlockStatement;
-import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceType;
-import net.sourceforge.pmd.lang.java.ast.ASTConditionalExpression;
-import net.sourceforge.pmd.lang.java.ast.ASTFormalParameter;
-import net.sourceforge.pmd.lang.java.ast.ASTLiteral;
-import net.sourceforge.pmd.lang.java.ast.ASTLocalVariableDeclaration;
-import net.sourceforge.pmd.lang.java.ast.ASTName;
-import net.sourceforge.pmd.lang.java.ast.ASTPrimaryExpression;
-import net.sourceforge.pmd.lang.java.ast.ASTPrimaryPrefix;
-import net.sourceforge.pmd.lang.java.ast.ASTPrimarySuffix;
-import net.sourceforge.pmd.lang.java.ast.ASTPrimitiveType;
-import net.sourceforge.pmd.lang.java.ast.ASTStatementExpression;
-import net.sourceforge.pmd.lang.java.ast.ASTType;
-import net.sourceforge.pmd.lang.java.ast.AccessNode;
-import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
-import net.sourceforge.pmd.lang.java.symboltable.VariableNameDeclaration;
+import net.sourceforge.pmd.lang.java.ast.ASTConstructorCall;
+import net.sourceforge.pmd.lang.java.ast.ASTExpression;
+import net.sourceforge.pmd.lang.java.ast.ASTList;
+import net.sourceforge.pmd.lang.java.ast.ASTMethodCall;
+import net.sourceforge.pmd.lang.java.ast.InvocationNode;
+import net.sourceforge.pmd.lang.java.rule.AbstractJavaRulechainRule;
+import net.sourceforge.pmd.lang.java.rule.internal.JavaRuleUtil;
 import net.sourceforge.pmd.lang.java.types.TypeTestUtil;
+import net.sourceforge.pmd.lang.java.types.TypeTestUtil.InvocationMatcher;
 
 /**
  * How this rule works: find additive expressions: + check that the addition is
@@ -37,240 +24,49 @@ import net.sourceforge.pmd.lang.java.types.TypeTestUtil;
  *
  * @author mgriffa
  */
-public class InefficientStringBufferingRule extends AbstractJavaRule {
+public class InefficientStringBufferingRule extends AbstractJavaRulechainRule {
+
+    private static final InvocationMatcher APPEND_MATCHER = InvocationMatcher.parse("java.lang.AbstractStringBuilder#append(_*)");
 
     public InefficientStringBufferingRule() {
-        addRuleChainVisit(ASTAdditiveExpression.class);
+        super(ASTConstructorCall.class, ASTMethodCall.class);
     }
 
     @Override
-    public Object visit(ASTAdditiveExpression node, Object data) {
-        if (node.getParent() instanceof ASTConditionalExpression || node.getNthParent(2) instanceof ASTConditionalExpression) {
-            // ignore concats in ternary expressions
-            return data;
+    public Object visit(ASTMethodCall node, Object data) {
+        if (APPEND_MATCHER.matchesCall(node)) {
+            checkArgument(node.getArguments(), (RuleContext) data);
         }
-
-        ASTBlockStatement bs = node.getFirstParentOfType(ASTBlockStatement.class);
-        if (bs == null) {
-            return data;
-        }
-
-        int immediateLiterals = 0;
-        int immediateStringLiterals = 0;
-        List<ASTLiteral> nodes = node.findDescendantsOfType(ASTLiteral.class);
-        for (ASTLiteral literal : nodes) {
-            if (literal.getNthParent(3) instanceof ASTAdditiveExpression) {
-                immediateLiterals++;
-                if (literal.isStringLiteral()) {
-                    immediateStringLiterals++;
-                }
-            }
-            if (literal.isIntLiteral() || literal.isFloatLiteral() || literal.isDoubleLiteral()
-                    || literal.isLongLiteral()) {
-                return data;
-            }
-        }
-
-        boolean onlyStringLiterals = immediateLiterals == immediateStringLiterals
-                && immediateLiterals == node.getNumChildren();
-        if (onlyStringLiterals) {
-            return data;
-        }
-
-        // if literal + final, return
-        List<ASTName> nameNodes = node.findDescendantsOfType(ASTName.class);
-        for (ASTName name : nameNodes) {
-            if (name.getNameDeclaration() != null && name.getNameDeclaration() instanceof VariableNameDeclaration) {
-                VariableNameDeclaration vnd = (VariableNameDeclaration) name.getNameDeclaration();
-                AccessNode accessNodeParent = vnd.getAccessNodeParent();
-                if (accessNodeParent.isFinal()) {
-                    return data;
-                }
-            }
-        }
-
-        // if literal primitive type and not strings variables, then return
-        boolean stringFound = false;
-        for (ASTName name : nameNodes) {
-            if (!isPrimitiveType(name) && isStringType(name)) {
-                stringFound = true;
-                break;
-            }
-        }
-        if (!stringFound && immediateStringLiterals == 0) {
-            return data;
-        }
-
-        if (bs.isAllocation()) {
-            for (Iterator<ASTName> iterator = nameNodes.iterator(); iterator.hasNext();) {
-                ASTName name = iterator.next();
-                if (!name.getImage().endsWith("length")) {
-                    break;
-                } else if (!iterator.hasNext()) {
-                    return data; // All names end with length
-                }
-            }
-
-            if (isAllocatedStringBuffer(node)) {
-                addViolation(data, node);
-            } else if (isInStringBufferOperationChain(node, "append")) {
-                addViolation(data, node);
-            }
-        } else if (isInStringBufferOperationChain(node, "append")) {
-            addViolation(data, node);
-        }
-        return data;
+        return null;
     }
 
-    private boolean isStringType(ASTName name) {
-        ASTType type = getTypeNode(name);
-        if (type != null) {
-            List<ASTClassOrInterfaceType> types = type.findDescendantsOfType(ASTClassOrInterfaceType.class);
-            if (!types.isEmpty()) {
-                return TypeTestUtil.isA(String.class, types.get(0));
-            }
+    @Override
+    public Object visit(ASTConstructorCall node, Object data) {
+        if (TypeTestUtil.isA(StringBuilder.class, node.getTypeNode())
+            || TypeTestUtil.isA(StringBuffer.class, node.getTypeNode())) {
+            checkArgument(node.getArguments(), (RuleContext) data);
         }
-        return false;
+        return null;
     }
 
-    private boolean isPrimitiveType(ASTName name) {
-        ASTType type = getTypeNode(name);
-        return type != null && !type.findChildrenOfType(ASTPrimitiveType.class).isEmpty();
+    private void checkArgument(ASTArgumentList argList, RuleContext ctx) {
+        ASTExpression arg = ASTList.singleOrNull(argList);
+
+        if (JavaRuleUtil.isStringConcatExpr(arg)
+            // ignore concatenations that produce constants
+            && !arg.isCompileTimeConstant()) {
+            addViolation(ctx, arg);
+        }
     }
 
-    private ASTType getTypeNode(ASTName name) {
-        ASTType result = null;
-        if (name.getNameDeclaration() instanceof VariableNameDeclaration) {
-            VariableNameDeclaration vnd = (VariableNameDeclaration) name.getNameDeclaration();
-            if (vnd.getAccessNodeParent() instanceof ASTLocalVariableDeclaration) {
-                ASTLocalVariableDeclaration l = (ASTLocalVariableDeclaration) vnd.getAccessNodeParent();
-                result = l.getTypeNode();
-            } else if (vnd.getAccessNodeParent() instanceof ASTFormalParameter) {
-                ASTFormalParameter p = (ASTFormalParameter) vnd.getAccessNodeParent();
-                result = p.getTypeNode();
-            }
-        }
-        return result;
-    }
-
-    static boolean isInStringBufferOperationChain(Node node, String methodName) {
-        ASTPrimaryExpression expr = node.getFirstParentOfType(ASTPrimaryExpression.class);
-        MethodCallChain methodCalls = MethodCallChain.wrap(expr);
-        while (expr != null && methodCalls == null) {
-            expr = expr.getFirstParentOfType(ASTPrimaryExpression.class);
-            methodCalls = MethodCallChain.wrap(expr);
-        }
-
-        if (methodCalls != null && !methodCalls.isExactlyOfAnyType(StringBuffer.class, StringBuilder.class)) {
-            methodCalls = null;
-        }
-
-        return methodCalls != null && methodCalls.getMethodNames().contains(methodName);
-    }
-
-    /**
-     * @deprecated will be removed with PMD 7
-     */
-    @Deprecated
-    protected static boolean isInStringBufferOperation(Node node, int length, String methodName) {
-        if (!(node.getNthParent(length) instanceof ASTStatementExpression)) {
+    public static boolean isInStringBufferOperationChain(Node node, String append) {
+        // todo this was replaced by something that doesn't really work
+        if (!(node instanceof ASTExpression)) {
             return false;
         }
-        ASTStatementExpression s = node.getFirstParentOfType(ASTStatementExpression.class);
-        if (s == null) {
-            return false;
-        }
-        ASTName n = s.getFirstDescendantOfType(ASTName.class);
-        if (n == null || !n.getImage().contains(methodName)
-                || !(n.getNameDeclaration() instanceof VariableNameDeclaration)) {
-            return false;
-        }
+        Node parent = node.getParent();
 
-        // TODO having to hand-code this kind of dredging around is ridiculous
-        // we need something to support this in the framework
-        // but, "for now" (tm):
-        // if more than one arg to append(), skip it
-        ASTArgumentList argList = s.getFirstDescendantOfType(ASTArgumentList.class);
-        if (argList == null || argList.getNumChildren() > 1) {
-            return false;
-        }
-        return ConsecutiveLiteralAppendsRule.isStringBuilderOrBuffer(((VariableNameDeclaration) n.getNameDeclaration()).getDeclaratorId());
-    }
-
-    private boolean isAllocatedStringBuffer(ASTAdditiveExpression node) {
-        ASTAllocationExpression ao = node.getFirstParentOfType(ASTAllocationExpression.class);
-        if (ao == null) {
-            return false;
-        }
-        // note that the child can be an ArrayDimsAndInits, for example, from
-        // java.lang.FloatingDecimal: t = new int[ nWords+wordcount+1 ];
-        ASTClassOrInterfaceType an = ao.getFirstChildOfType(ASTClassOrInterfaceType.class);
-        return ConsecutiveLiteralAppendsRule.isStringBuilderOrBuffer(an);
-    }
-
-    private static class MethodCallChain {
-        private final ASTPrimaryExpression primary;
-
-        private MethodCallChain(ASTPrimaryExpression primary) {
-            this.primary = primary;
-        }
-
-        // Note: The impl here is technically not correct: The type of a method call
-        // chain is the result of the last method called, not the type of the
-        // first receiver object (== PrimaryPrefix).
-        boolean isExactlyOfAnyType(Class<?> clazz, Class<?> ... clazzes) {
-            ASTPrimaryPrefix typeNode = getTypeNode();
-
-            if (TypeTestUtil.isExactlyA(clazz, typeNode)) {
-                return true;
-            }
-            if (clazzes != null) {
-                for (Class<?> c : clazzes) {
-                    if (TypeTestUtil.isExactlyA(c, typeNode)) {
-                        return true;
-                    }
-                }
-            }
-            return false;
-        }
-
-        ASTPrimaryPrefix getTypeNode() {
-            return primary.getFirstChildOfType(ASTPrimaryPrefix.class);
-        }
-
-        List<String> getMethodNames() {
-            List<String> methodNames = new ArrayList<>();
-
-            ASTPrimaryPrefix prefix = getTypeNode();
-            ASTName name = prefix.getFirstChildOfType(ASTName.class);
-            if (name != null) {
-                String firstMethod = name.getImage();
-                int dot = firstMethod.lastIndexOf('.');
-                if (dot != -1) {
-                    firstMethod = firstMethod.substring(dot + 1);
-                }
-                methodNames.add(firstMethod);
-            }
-
-            for (ASTPrimarySuffix suffix : primary.findChildrenOfType(ASTPrimarySuffix.class)) {
-                if (suffix.getImage() != null) {
-                    methodNames.add(suffix.getImage());
-                }
-            }
-            return methodNames;
-        }
-
-        static MethodCallChain wrap(ASTPrimaryExpression primary) {
-            if (primary != null && isMethodCall(primary)) {
-                return new MethodCallChain(primary);
-            }
-            return null;
-        }
-
-        private static boolean isMethodCall(ASTPrimaryExpression primary) {
-            return primary.getNumChildren() >= 2
-                    && primary.getChild(0) instanceof ASTPrimaryPrefix
-                    && primary.getChild(1) instanceof ASTPrimarySuffix;
-        }
+        return parent instanceof ASTMethodCall
+            && APPEND_MATCHER.matchesCall((InvocationNode) parent);
     }
 }
