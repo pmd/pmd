8de0e171fd8e79934c256f74d9c27be31a858b82
Andreas Dangel <andreas.dangel@pmd-code.org>
2020-12-11 19:55:03 +0100

    Remove unused import

--- pmd-core/src/main/java/net/sourceforge/pmd/RuleSetLoader.java
+++ pmd-core/src/main/java/net/sourceforge/pmd/RuleSetLoader.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Properties;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import net.sourceforge.pmd.lang.Language;
