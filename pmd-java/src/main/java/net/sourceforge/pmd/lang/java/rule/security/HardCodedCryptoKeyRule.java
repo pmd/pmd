facc71166ec5c84dd000b76108dbecf682748c2e
Andreas Dangel <andreas.dangel@pmd-code.org>
2021-01-28 10:13:47 +0100

    [java] Update HardCodedCryptoKeyRule

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/security/HardCodedCryptoKeyRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/security/HardCodedCryptoKeyRule.java
@@ -1,21 +1,18 @@
-/**
+/*
  * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
  */
 
 package net.sourceforge.pmd.lang.java.rule.security;
 
-import net.sourceforge.pmd.lang.ast.Node;
-import net.sourceforge.pmd.lang.java.ast.ASTAllocationExpression;
 import net.sourceforge.pmd.lang.java.ast.ASTArgumentList;
-import net.sourceforge.pmd.lang.java.ast.ASTArguments;
+import net.sourceforge.pmd.lang.java.ast.ASTArrayAllocation;
 import net.sourceforge.pmd.lang.java.ast.ASTArrayInitializer;
-import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceType;
-import net.sourceforge.pmd.lang.java.ast.ASTLiteral;
-import net.sourceforge.pmd.lang.java.ast.ASTName;
-import net.sourceforge.pmd.lang.java.ast.ASTPrimaryPrefix;
-import net.sourceforge.pmd.lang.java.ast.ASTVariableInitializer;
-import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
-import net.sourceforge.pmd.lang.java.symboltable.VariableNameDeclaration;
+import net.sourceforge.pmd.lang.java.ast.ASTConstructorCall;
+import net.sourceforge.pmd.lang.java.ast.ASTExpression;
+import net.sourceforge.pmd.lang.java.ast.ASTStringLiteral;
+import net.sourceforge.pmd.lang.java.ast.ASTVariableAccess;
+import net.sourceforge.pmd.lang.java.ast.ASTVariableDeclaratorId;
+import net.sourceforge.pmd.lang.java.rule.AbstractJavaRulechainRule;
 import net.sourceforge.pmd.lang.java.types.TypeTestUtil;
 
 /**
@@ -25,27 +22,20 @@ import net.sourceforge.pmd.lang.java.types.TypeTestUtil;
  * @author sergeygorbaty
  * @since 6.4.0
  */
-public class HardCodedCryptoKeyRule extends AbstractJavaRule {
+public class HardCodedCryptoKeyRule extends AbstractJavaRulechainRule {
 
     private static final Class<?> SECRET_KEY_SPEC = javax.crypto.spec.SecretKeySpec.class;
 
     public HardCodedCryptoKeyRule() {
-        addRuleChainVisit(ASTAllocationExpression.class);
+        super(ASTConstructorCall.class);
     }
 
     @Override
-    public Object visit(ASTAllocationExpression node, Object data) {
-        if (TypeTestUtil.isA(SECRET_KEY_SPEC, node.getFirstChildOfType(ASTClassOrInterfaceType.class))) {
-            Node firstArgument = null;
-
-            ASTArguments arguments = node.getFirstChildOfType(ASTArguments.class);
+    public Object visit(ASTConstructorCall node, Object data) {
+        if (TypeTestUtil.isA(SECRET_KEY_SPEC, node)) {
+            ASTArgumentList arguments = node.getArguments();
             if (arguments.size() > 0) {
-                firstArgument = arguments.getFirstChildOfType(ASTArgumentList.class).getChild(0);
-            }
-
-            if (firstArgument != null) {
-                ASTPrimaryPrefix prefix = firstArgument.getFirstDescendantOfType(ASTPrimaryPrefix.class);
-                validateProperKeyArgument(data, prefix);
+                validateProperKeyArgument(data, arguments.get(0));
             }
         }
         return data;
@@ -55,38 +45,27 @@ public class HardCodedCryptoKeyRule extends AbstractJavaRule {
      * Recursively resolves the argument again, if the variable initializer
      * is itself a expression.
      *
-     * Then checks the expression for being a string literal or array
-     *
-     * @param data
-     * @param firstArgumentExpression
+     * <p>Then checks the expression for being a string literal or array
      */
-    private void validateProperKeyArgument(Object data, ASTPrimaryPrefix firstArgumentExpression) {
-        if (firstArgumentExpression == null) {
-            return;
+    private void validateProperKeyArgument(Object data, ASTExpression firstArgumentExpression) {
+        // named variable
+        if (firstArgumentExpression instanceof ASTVariableAccess) {
+            ASTVariableAccess varAccess = (ASTVariableAccess) firstArgumentExpression;
+            ASTVariableDeclaratorId varDecl = varAccess.getSignature().getSymbol().tryGetNode();
+            validateProperKeyArgument(data, varDecl.getInitializer());
         }
 
-        // named variable
-        ASTName namedVar = firstArgumentExpression.getFirstDescendantOfType(ASTName.class);
-        if (namedVar != null) {
-            // find where it's declared, if possible
-            if (namedVar != null && namedVar.getNameDeclaration() instanceof VariableNameDeclaration) {
-                VariableNameDeclaration varDecl = (VariableNameDeclaration) namedVar.getNameDeclaration();
-                ASTVariableInitializer initializer = varDecl.getAccessNodeParent().getFirstDescendantOfType(ASTVariableInitializer.class);
-                if (initializer != null) {
-                    validateProperKeyArgument(data, initializer.getFirstDescendantOfType(ASTPrimaryPrefix.class));
-                }
+        // hard coded array ASTArrayAllocation
+        if (firstArgumentExpression instanceof ASTArrayAllocation) {
+            ASTArrayInitializer arrayInit = ((ASTArrayAllocation) firstArgumentExpression).getArrayInitializer();
+            if (arrayInit != null) {
+                addViolation(data, arrayInit);
             }
         }
 
-        // hard coded array
-        ASTArrayInitializer arrayInit = firstArgumentExpression.getFirstDescendantOfType(ASTArrayInitializer.class);
-        if (arrayInit != null) {
-            addViolation(data, arrayInit);
-        }
-
         // string literal
-        ASTLiteral literal = firstArgumentExpression.getFirstDescendantOfType(ASTLiteral.class);
-        if (literal != null && literal.isStringLiteral()) {
+        ASTStringLiteral literal = firstArgumentExpression.descendants(ASTStringLiteral.class).first();
+        if (literal != null) {
             addViolation(data, literal);
         }
     }
