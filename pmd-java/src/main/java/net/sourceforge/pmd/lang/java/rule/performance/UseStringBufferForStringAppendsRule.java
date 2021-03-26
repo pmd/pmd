6488e1152307999fe01c7449bf4811a0803db5ca
Andreas Dangel <andreas.dangel@pmd-code.org>
2021-03-26 16:28:46 +0100

    [java] Update rule UseStringBufferForStringAppends

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/performance/UseStringBufferForStringAppendsRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/performance/UseStringBufferForStringAppendsRule.java
@@ -4,30 +4,22 @@
 
 package net.sourceforge.pmd.lang.java.rule.performance;
 
-import java.util.Objects;
-
 import net.sourceforge.pmd.lang.ast.Node;
-import net.sourceforge.pmd.lang.java.ast.ASTArgumentList;
-import net.sourceforge.pmd.lang.java.ast.ASTAssignmentOperator;
-import net.sourceforge.pmd.lang.java.ast.ASTConditionalExpression;
-import net.sourceforge.pmd.lang.java.ast.ASTDoStatement;
-import net.sourceforge.pmd.lang.java.ast.ASTEqualityExpression;
+import net.sourceforge.pmd.lang.java.ast.ASTAssignableExpr.ASTNamedReferenceExpr;
+import net.sourceforge.pmd.lang.java.ast.ASTAssignableExpr.AccessType;
 import net.sourceforge.pmd.lang.java.ast.ASTFieldDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTForStatement;
+import net.sourceforge.pmd.lang.java.ast.ASTForeachStatement;
 import net.sourceforge.pmd.lang.java.ast.ASTFormalParameter;
-import net.sourceforge.pmd.lang.java.ast.ASTName;
-import net.sourceforge.pmd.lang.java.ast.ASTPrimaryExpression;
-import net.sourceforge.pmd.lang.java.ast.ASTStatementExpression;
+import net.sourceforge.pmd.lang.java.ast.ASTLoopStatement;
 import net.sourceforge.pmd.lang.java.ast.ASTVariableDeclaratorId;
-import net.sourceforge.pmd.lang.java.ast.ASTWhileStatement;
-import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
+import net.sourceforge.pmd.lang.java.rule.AbstractJavaRulechainRule;
 import net.sourceforge.pmd.lang.java.types.TypeTestUtil;
-import net.sourceforge.pmd.lang.symboltable.NameOccurrence;
 
-public class UseStringBufferForStringAppendsRule extends AbstractJavaRule {
+public class UseStringBufferForStringAppendsRule extends AbstractJavaRulechainRule {
 
     public UseStringBufferForStringAppendsRule() {
-        addRuleChainVisit(ASTVariableDeclaratorId.class);
+        super(ASTVariableDeclaratorId.class);
     }
 
     /**
@@ -39,81 +31,31 @@ public class UseStringBufferForStringAppendsRule extends AbstractJavaRule {
     @Override
     public Object visit(ASTVariableDeclaratorId node, Object data) {
         if (!TypeTestUtil.isA(String.class, node) || node.hasArrayType()
-                || node.getNthParent(3) instanceof ASTForStatement) {
+                || node.getNthParent(3) instanceof ASTForStatement
+                || node.getNthParent(3) instanceof ASTForeachStatement) {
             return data;
         }
 
-        // Remember how often we the variable has been used
+        // Remember how often the variable has been used
         int usageCounter = 0;
 
-        for (NameOccurrence no : node.getUsages()) {
-            Node name = no.getLocation();
-            ASTStatementExpression statement = name.getFirstParentOfType(ASTStatementExpression.class);
-            if (statement == null) {
-                continue;
-            }
-            ASTArgumentList argList = name.getFirstParentOfType(ASTArgumentList.class);
-            if (argList != null && argList.getFirstParentOfType(ASTStatementExpression.class) == statement) {
-                // used in method call
-                continue;
-            }
-            ASTEqualityExpression equality = name.getFirstParentOfType(ASTEqualityExpression.class);
-            if (equality != null && equality.getFirstParentOfType(ASTStatementExpression.class) == statement) {
-                // used in condition
-                continue;
-            }
+        for (ASTNamedReferenceExpr usage : node.getLocalUsages()) {
             if ((node.getNthParent(2) instanceof ASTFieldDeclaration
                     || node.getParent() instanceof ASTFormalParameter)
-                    && isNotWithinLoop(name)) {
+                    && isNotWithinLoop(usage)) {
                 // ignore if the field or formal parameter is *not* used within loops
                 continue;
             }
-            ASTConditionalExpression conditional = name.getFirstParentOfType(ASTConditionalExpression.class);
 
-            if (conditional != null) {
-                Node thirdParent = name.getNthParent(3);
-                Node fourthParent = name.getNthParent(4);
-                if ((Objects.equals(thirdParent, conditional) || Objects.equals(fourthParent, conditional))
-                        && conditional.getFirstParentOfType(ASTStatementExpression.class) == statement) {
-                    // is used in ternary as only option (not appended to other
-                    // string)
-                    continue;
-                }
-            }
-            if (statement.getNumChildren() > 0 && statement.getChild(0) instanceof ASTPrimaryExpression) {
-                ASTName astName = statement.getChild(0).getFirstDescendantOfType(ASTName.class);
-                if (astName != null) {
-                    ASTAssignmentOperator assignmentOperator = statement
-                            .getFirstDescendantOfType(ASTAssignmentOperator.class);
-                    if (astName.equals(name)) {
-                        if (assignmentOperator != null && assignmentOperator.isCompound()) {
-                            if (isWithinLoop(name)) {
-                                // always report within a loop
-                                addViolation(data, assignmentOperator);
-                            } else {
-                                usageCounter++;
-                                if (usageCounter > 1) {
-                                    // only report, if it is not the first time
-                                    addViolation(data, assignmentOperator);
-                                }
-                            }
-                        }
-                    } else if (astName.hasImageEqualTo(name.getImage())) {
-                        if (assignmentOperator != null && !assignmentOperator.isCompound()) {
-                            if (isWithinLoop(name)) {
-                                // always report within a loop
-                                addViolation(data, assignmentOperator);
-                            } else {
-                                usageCounter++;
-                                if (usageCounter > 1) {
-                                    // only report, if it is not the first time
-                                    addViolation(data, assignmentOperator);
-                                }
-                            }
-                        } else if (assignmentOperator != null && assignmentOperator.isCompound()
-                                && usageCounter >= 1) {
-                            addViolation(data, assignmentOperator);
-                        }
+            if (usage.getAccessType() == AccessType.WRITE) {
+                if (isWithinLoop(usage)) {
+                    // always report within a loop
+                    addViolation(data, usage);
+                } else {
+                    usageCounter++;
+                    if (usageCounter > 1) {
+                        // only report, if it is not the first time
+                        addViolation(data, usage);
                     }
                 }
             }
@@ -122,9 +64,7 @@ public class UseStringBufferForStringAppendsRule extends AbstractJavaRule {
     }
 
     private boolean isNotWithinLoop(Node name) {
-        return name.getFirstParentOfType(ASTForStatement.class) == null
-                && name.getFirstParentOfType(ASTWhileStatement.class) == null
-                && name.getFirstParentOfType(ASTDoStatement.class) == null;
+        return name.ancestors(ASTLoopStatement.class).isEmpty();
     }
 
     private boolean isWithinLoop(Node name) {
