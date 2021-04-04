79ccde716a30e3b3ff7ced953097bd32dbb9db2d
Clément Fournier <clement.fournier76@gmail.com>
2021-04-04 17:49:45 +0200

    Update OnlyOneReturn rule

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/codestyle/OnlyOneReturnRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/codestyle/OnlyOneReturnRule.java
@@ -4,10 +4,7 @@
 
 package net.sourceforge.pmd.lang.java.rule.codestyle;
 
-import java.util.Iterator;
-import java.util.List;
-
-import net.sourceforge.pmd.lang.ast.Node;
+import net.sourceforge.pmd.lang.ast.NodeStream;
 import net.sourceforge.pmd.lang.java.ast.ASTMethodDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTReturnStatement;
 import net.sourceforge.pmd.lang.java.rule.AbstractJavaRulechainRule;
@@ -21,23 +18,15 @@ public class OnlyOneReturnRule extends AbstractJavaRulechainRule {
     @Override
     public Object visit(ASTMethodDeclaration node, Object data) {
         if (node.getBody() == null) {
-            return data;
+            return null;
         }
 
-        node.getBody().descendants(ASTReturnStatement.class)
+        NodeStream<ASTReturnStatement> returnsExceptLast =
+            node.getBody().descendants(ASTReturnStatement.class).dropLast(1);
 
-        List<ASTReturnStatement> returnNodes = node.findDescendantsOfType(ASTReturnStatement.class);
-
-        if (returnNodes.size() > 1) {
-            for (Iterator<ASTReturnStatement> i = returnNodes.iterator(); i.hasNext();) {
-                Node problem = i.next();
-                // skip the last one, it's OK
-                if (!i.hasNext()) {
-                    continue;
-                }
-                addViolation(data, problem);
-            }
+        for (ASTReturnStatement returnStmt : returnsExceptLast) {
+            addViolation(data, returnStmt);
         }
-        return data;
+        return null;
     }
 }
