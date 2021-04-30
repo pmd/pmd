fdb1420c5d0f148ece740689e1a203d8ebdcd04a
Vsevolod <seva151201@gmail.com>
2021-04-30 19:47:03 +0700

    #2987 New Rule: Public and protected static fields must be final

---
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/security/StaticFieldMustBeFinal.java
@@ -0,0 +1,11 @@
+/**
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
+package net.sourceforge.pmd.lang.java.rule.security;
+
+import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
+
+public class StaticFieldMustBeFinal extends AbstractJavaRule {
+    // no additional unit tests
+}
