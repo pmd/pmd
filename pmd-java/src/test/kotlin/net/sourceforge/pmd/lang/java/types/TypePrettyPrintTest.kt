987629160f6749deccfc5dc2599ff7d9f0297434
Clément Fournier <clement.fournier76@gmail.com>
2021-05-05 14:24:12 +0200

    Improve TypePrettyPrint tests

---
+++ pmd-java/src/test/kotlin/net/sourceforge/pmd/lang/java/types/TypePrettyPrintTest.kt
@@ -0,0 +1,86 @@
+/*
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
+package net.sourceforge.pmd.lang.java.types
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import net.sourceforge.pmd.lang.java.ast.ParserTestCtx
+
+/**
+ * @author Clément Fournier
+ */
+class TypePrettyPrintTest : FunSpec({
+
+    val ts = testTypeSystem
+
+    test("Test toString") {
+
+        ts.arrayType(ts.INT).apply {
+            toString() shouldBe "int[]"
+        }
+    }
+
+    test("wildcards") {
+
+        ts.wildcard(true, ts.OBJECT).apply {
+            toString() shouldBe "?"
+        }
+
+        ts.wildcard(true, ts.CLONEABLE).apply {
+            toString() shouldBe "? extends java.lang.Cloneable"
+        }
+
+        ts.wildcard(false, ts.CLONEABLE).apply {
+            toString() shouldBe "? super java.lang.Cloneable"
+        }
+    }
+
+    test("pretty print with simple names") {
+
+        fun JTypeMirror.pp() = TypePrettyPrint.prettyPrintWithSimpleNames(this)
+
+        with(TypeDslOf(ts)) {
+            ts.OBJECT.pp() shouldBe "Object"
+            ts.OBJECT.toArray().pp() shouldBe "Object[]"
+            Map::class[TypePrettyPrintTest::class.raw, ts.INT.box()].pp() shouldBe "Map<TypesToStringTest, Integer>"
+        }
+    }
+
+    test("pretty print with tvar qualifiers") {
+
+        val acu = ParserTestCtx().parser.withProcessing(true).parse("""
+            package p;
+            class Foo<A,B> {
+                <T> T method(A a, B b) {}
+            }
+        """.trimIndent()
+        )
+
+
+        fun JTypeVisitable.pp() = TypePrettyPrint.prettyPrintWithTvarQualifier(this)
+
+        acu.declaredMethodSignatures()[0].pp() shouldBe "p.Foo<Foo#A, Foo#B>.<method#T> method(Foo#A, Foo#B) -> method#T"
+
+    }
+
+    test("pretty print with no method header") {
+
+        val acu = ParserTestCtx().parser.withProcessing(true).parse("""
+            package p;
+            class Foo<A,B> {
+                <T> T method(A a, B b) {}
+            }
+        """.trimIndent()
+        )
+
+
+        fun JMethodSig.pp() = TypePrettyPrint.prettyPrint(this, false)
+
+        acu.declaredMethodSignatures()[0].pp() shouldBe "method(A, B) -> T"
+
+    }
+})
+
+
