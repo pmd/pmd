43d925050334d0de48b1b5598213d412ca6bd52b
Clément Fournier <clement.fournier76@gmail.com>
2021-05-08 12:44:25 +0200

    Save 'broken' class files in regular source tree
    Since we have the classpath abstraction, we can just
    mask off some class files to get a broken classpath
    for use in tests.

---
+++ pmd-java/src/test/java/javasymbols/brokenclasses/BrokenGeneric.java
@@ -0,0 +1,23 @@
+
+/*
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
+package javasymbols.brokenclasses;
+
+import net.sourceforge.pmd.lang.java.symbols.internal.asm.Classpath;
+
+/**
+ * For this test we exclude SuperItf.class from the {@link Classpath}
+ * to mimic an incomplete classpath.
+ *
+ * @see net.sourceforge.pmd.lang.java.symbols.internal.asm.BrokenClasspathTest
+ */
+public class BrokenGeneric<T0, T1>
+    extends SuperKlass<T0, T0>
+    implements SuperItf<T1, T1> {
+}
+
+class SuperKlass<A, B> { }
+
+interface SuperItf<A, B> { }
