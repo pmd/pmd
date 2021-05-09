2ea0946e868bad6b68418b60484d0f36461adf18
Clément Fournier <clement.fournier76@gmail.com>
2021-05-09 20:24:36 +0200

    Refine annotations of UnusedPrivateField

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/bestpractices/UnusedPrivateFieldRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/bestpractices/UnusedPrivateFieldRule.java
@@ -7,38 +7,48 @@ package net.sourceforge.pmd.lang.java.rule.bestpractices;
 import static net.sourceforge.pmd.util.CollectionUtil.setOf;
 
 import java.util.List;
+import java.util.Set;
 
 import net.sourceforge.pmd.lang.java.ast.ASTAnyTypeDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTFieldDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTVariableDeclaratorId;
 import net.sourceforge.pmd.lang.java.ast.AccessNode.Visibility;
-import net.sourceforge.pmd.lang.java.ast.Annotatable;
 import net.sourceforge.pmd.lang.java.ast.JavaNode;
 import net.sourceforge.pmd.lang.java.rule.AbstractJavaRulechainRule;
 import net.sourceforge.pmd.lang.java.rule.internal.JavaPropertyUtil;
 import net.sourceforge.pmd.lang.java.rule.internal.JavaRuleUtil;
 import net.sourceforge.pmd.properties.PropertyDescriptor;
-import net.sourceforge.pmd.util.CollectionUtil;
 
 public class UnusedPrivateFieldRule extends AbstractJavaRulechainRule {
 
-    private static final PropertyDescriptor<List<String>> IGNORED_ANNOTATIONS =
+    private static final Set<String> INVALIDATING_CLASS_ANNOT = setOf(
+        "lombok.Builder",
+        "lombok.EqualsAndHashCode",
+        "lombok.Getter",
+        "lombok.Setter",
+        "lombok.Data",
+        "lombok.Value"
+    );
+
+    private static final PropertyDescriptor<List<String>> IGNORED_FIELD_ANNOTATIONS =
         JavaPropertyUtil.ignoredAnnotationsDescriptor(
-            CollectionUtil.union(JavaRuleUtil.LOMBOK_ANNOTATIONS,
-                                 setOf("java.lang.Deprecated",
-                                       "javafx.fxml.FXML"))
+            "lombok.Setter",
+            "lombok.Getter",
+            "java.lang.Deprecated",
+            "lombok.experimental.Delegate",
+            "javafx.fxml.FXML"
         );
 
     public UnusedPrivateFieldRule() {
         super(ASTAnyTypeDeclaration.class);
-        definePropertyDescriptor(IGNORED_ANNOTATIONS);
+        definePropertyDescriptor(IGNORED_FIELD_ANNOTATIONS);
     }
 
     @Override
     public Object visitJavaNode(JavaNode node, Object data) {
         if (node instanceof ASTAnyTypeDeclaration) {
             ASTAnyTypeDeclaration type = (ASTAnyTypeDeclaration) node;
-            if (hasIgnoredAnnotation(type)) {
+            if (JavaRuleUtil.hasAnyAnnotation(type, INVALIDATING_CLASS_ANNOT)) {
                 return null;
             }
 
@@ -59,10 +69,7 @@ public class UnusedPrivateFieldRule extends AbstractJavaRulechainRule {
         return field.getVisibility() != Visibility.V_PRIVATE
             || JavaRuleUtil.isSerialPersistentFields(field)
             || JavaRuleUtil.isSerialVersionUID(field)
-            || hasIgnoredAnnotation(field);
+            || JavaRuleUtil.hasAnyAnnotation(field, getProperty(IGNORED_FIELD_ANNOTATIONS));
     }
 
-    private boolean hasIgnoredAnnotation(Annotatable node) {
-        return JavaRuleUtil.hasAnyAnnotation(node, getProperty(IGNORED_ANNOTATIONS));
-    }
 }
--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/internal/JavaPropertyUtil.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/internal/JavaPropertyUtil.java
@@ -6,8 +6,10 @@ package net.sourceforge.pmd.lang.java.rule.internal;
 
 import static net.sourceforge.pmd.properties.PropertyFactory.stringListProperty;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 
 import net.sourceforge.pmd.properties.PropertyDescriptor;
@@ -23,9 +25,11 @@ public final class JavaPropertyUtil {
 
 
     public static PropertyDescriptor<List<String>> ignoredAnnotationsDescriptor(Collection<String> defaults) {
+        List<String> sortedDefaults = new ArrayList<>(defaults);
+        sortedDefaults.sort(Comparator.naturalOrder());
         return stringListProperty("ignoredAnnotations")
             .desc("Fully qualified names of the annotation types that should be ignored by this rule")
-            .defaultValue(defaults)
+            .defaultValue(sortedDefaults)
             .build();
     }
 
