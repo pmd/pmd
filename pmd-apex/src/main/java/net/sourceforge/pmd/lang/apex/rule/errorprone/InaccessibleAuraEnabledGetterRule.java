f70fd1823833e45c99b85f9e732a392f8a910e36
pozil <philippe.ozil@gmail.com>
2021-06-02 20:51:52 +0200

    Applied proper code formatting

--- pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/errorprone/InaccessibleAuraEnabledGetterRule.java
+++ pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/errorprone/InaccessibleAuraEnabledGetterRule.java
@@ -13,46 +13,49 @@ import net.sourceforge.pmd.lang.apex.ast.ASTProperty;
 import net.sourceforge.pmd.lang.apex.rule.AbstractApexRule;
 
 /**
- * In the Summer '21 release, a mandatory security update enforces access modifiers on Apex properties in Lightning component markup.
- * The update prevents access to private or protected Apex getters from Aura and Lightning Web Components.
+ * In the Summer '21 release, a mandatory security update enforces access
+ * modifiers on Apex properties in Lightning component markup. The update
+ * prevents access to private or protected Apex getters from Aura and Lightning
+ * Web Components.
+ * 
  * @author p.ozil
  */
 public class InaccessibleAuraEnabledGetterRule extends AbstractApexRule {
 
-	public InaccessibleAuraEnabledGetterRule() {
-		addRuleChainVisit(ASTProperty.class);
-	}
-	
-	@Override
+    public InaccessibleAuraEnabledGetterRule() {
+        addRuleChainVisit(ASTProperty.class);
+    }
+
+    @Override
     public Object visit(ASTProperty node, Object data) {
-		// Find @AuraEnabled property
-		ASTModifierNode propModifiers = node.getModifiers();
-		if (hasAuraEnabledAnnotation(propModifiers)) {
-			// Find getters/setters if any
-			List<ASTMethod> methods = node.findChildrenOfType(ASTMethod.class);
-			for (ASTMethod method : methods) {
-				// Find getter method
-				if (!"void".equals(method.getReturnType())) {
-					// Ensure getter is not private or protected
-					ASTModifierNode methodModifiers = method.getModifiers();
-					if (isPrivate(methodModifiers) || isProtected(methodModifiers)) {
-						addViolation(data, node);
-					}
-				}
-			}
-		}
-		return data;
-	}
-	
-	private boolean isPrivate(ASTModifierNode modifierNode) {
+        // Find @AuraEnabled property
+        ASTModifierNode propModifiers = node.getModifiers();
+        if (hasAuraEnabledAnnotation(propModifiers)) {
+            // Find getters/setters if any
+            List<ASTMethod> methods = node.findChildrenOfType(ASTMethod.class);
+            for (ASTMethod method : methods) {
+                // Find getter method
+                if (!"void".equals(method.getReturnType())) {
+                    // Ensure getter is not private or protected
+                    ASTModifierNode methodModifiers = method.getModifiers();
+                    if (isPrivate(methodModifiers) || isProtected(methodModifiers)) {
+                        addViolation(data, node);
+                    }
+                }
+            }
+        }
+        return data;
+    }
+
+    private boolean isPrivate(ASTModifierNode modifierNode) {
         return modifierNode != null && modifierNode.isPrivate();
     }
-	
-	private boolean isProtected(ASTModifierNode modifierNode) {
+
+    private boolean isProtected(ASTModifierNode modifierNode) {
         return modifierNode != null && modifierNode.isProtected();
     }
-	
-	private boolean hasAuraEnabledAnnotation(ASTModifierNode modifierNode) {
+
+    private boolean hasAuraEnabledAnnotation(ASTModifierNode modifierNode) {
         List<ASTAnnotation> annotations = modifierNode.findChildrenOfType(ASTAnnotation.class);
         for (ASTAnnotation annotation : annotations) {
             if (annotation.hasImageEqualTo("AuraEnabled")) {
