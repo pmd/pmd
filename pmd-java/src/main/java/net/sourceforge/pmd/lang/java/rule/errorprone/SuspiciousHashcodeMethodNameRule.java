207b3f3819a045d77167c0d439b386c3360386cc
Clément Fournier <clement.fournier76@gmail.com>
2021-07-07 16:17:54 +0200

    Update SuspiciousHashcodeMethodName

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/errorprone/SuspiciousHashcodeMethodNameRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/errorprone/SuspiciousHashcodeMethodNameRule.java
@@ -4,36 +4,28 @@
 
 package net.sourceforge.pmd.lang.java.rule.errorprone;
 
-import net.sourceforge.pmd.lang.ast.Node;
 import net.sourceforge.pmd.lang.java.ast.ASTMethodDeclaration;
-import net.sourceforge.pmd.lang.java.ast.ASTMethodDeclarator;
-import net.sourceforge.pmd.lang.java.ast.ASTPrimitiveType;
-import net.sourceforge.pmd.lang.java.ast.ASTResultType;
-import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
+import net.sourceforge.pmd.lang.java.rule.AbstractJavaRulechainRule;
+import net.sourceforge.pmd.lang.java.rule.internal.JavaRuleUtil;
+import net.sourceforge.pmd.lang.java.types.JPrimitiveType.PrimitiveTypeKind;
 
-public class SuspiciousHashcodeMethodNameRule extends AbstractJavaRule {
+public class SuspiciousHashcodeMethodNameRule extends AbstractJavaRulechainRule {
+
+    public SuspiciousHashcodeMethodNameRule() {
+        super(ASTMethodDeclaration.class);
+    }
 
     @Override
     public Object visit(ASTMethodDeclaration node, Object data) {
-        /*
-         * original XPath rule was //MethodDeclaration [ResultType
-         * //PrimitiveType [@Image='int'] [//MethodDeclarator [@Image='hashcode'
-         * or @Image='HashCode' or @Image='Hashcode']
-         * [not(FormalParameters/*)]]]
-         */
+        String name = node.getName();
+        if ("hashcode".equalsIgnoreCase(name)
+            && !"hashCode".equals(name)
+            && node.getArity() == 0
+            && JavaRuleUtil.isPrimitiveType(node.getResultTypeNode(), PrimitiveTypeKind.INT)) {
 
-        ASTResultType type = node.getResultType();
-        ASTMethodDeclarator decl = node.getFirstChildOfType(ASTMethodDeclarator.class);
-        String name = decl.getImage();
-        if ("hashcode".equalsIgnoreCase(name) && !"hashCode".equals(name)
-                && decl.getChild(0).getNumChildren() == 0 && type.getNumChildren() != 0) {
-            Node t = type.getChild(0).getChild(0);
-            if (t instanceof ASTPrimitiveType && "int".equals(t.getImage())) {
-                addViolation(data, node);
-                return data;
-            }
+            addViolation(data, node);
         }
-        return super.visit(node, data);
+        return null;
     }
 
 }
