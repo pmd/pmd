8b98c861a2659a554f554be1cef1deb1f01912da
Clément Fournier <clement.fournier76@gmail.com>
2021-08-09 19:08:44 +0200

    Update rule UseUtilityClass

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/design/UseUtilityClassRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/design/UseUtilityClassRule.java
@@ -6,98 +6,86 @@ package net.sourceforge.pmd.lang.java.rule.design;
 
 import static net.sourceforge.pmd.util.CollectionUtil.setOf;
 
-import java.util.Collection;
+import java.util.Set;
 
-import net.sourceforge.pmd.lang.ast.Node;
 import net.sourceforge.pmd.lang.java.ast.ASTAnnotation;
-import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceBody;
+import net.sourceforge.pmd.lang.java.ast.ASTAssignableExpr.ASTNamedReferenceExpr;
+import net.sourceforge.pmd.lang.java.ast.ASTBodyDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceDeclaration;
-import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceType;
 import net.sourceforge.pmd.lang.java.ast.ASTConstructorDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTFieldDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTMemberValuePair;
 import net.sourceforge.pmd.lang.java.ast.ASTMethodDeclaration;
-import net.sourceforge.pmd.lang.java.ast.ASTResultType;
-import net.sourceforge.pmd.lang.java.rule.AbstractLombokAwareRule;
+import net.sourceforge.pmd.lang.java.ast.AccessNode.Visibility;
+import net.sourceforge.pmd.lang.java.ast.JavaNode;
+import net.sourceforge.pmd.lang.java.rule.AbstractJavaRulechainRule;
+import net.sourceforge.pmd.lang.java.rule.internal.JavaRuleUtil;
 import net.sourceforge.pmd.lang.java.types.TypeTestUtil;
 
-public class UseUtilityClassRule extends AbstractLombokAwareRule {
+public class UseUtilityClassRule extends AbstractJavaRulechainRule {
 
-    @Override
-    protected Collection<String> defaultSuppressionAnnotations() {
-        return setOf("lombok.experimental.UtilityClass");
+    private static final Set<String> IGNORED_CLASS_ANNOT = setOf(
+        "lombok.experimental.UtilityClass",
+        "org.junit.runner.RunWith" // for suites and such
+    );
+
+    public UseUtilityClassRule() {
+        super(ASTClassOrInterfaceDeclaration.class);
     }
 
     @Override
-    public Object visit(ASTClassOrInterfaceDeclaration node, Object data) {
-        if (hasIgnoredAnnotation(node)) {
+    public Object visit(ASTClassOrInterfaceDeclaration klass, Object data) {
+        if (JavaRuleUtil.hasAnyAnnotation(klass, IGNORED_CLASS_ANNOT)
+            || TypeTestUtil.isA("junit.framework.TestSuite", klass) // suite method is ok
+            || klass.isInterface()
+            || klass.isAbstract()
+            || klass.getSuperClassTypeNode() != null
+            || klass.getSuperInterfaceTypeNodes().nonEmpty()
+            //    || JavaRuleUtil.isUtilityClass(node)
+        ) {
             return data;
         }
-        return super.visit(node, data);
-    }
 
-    @Override
-    public Object visit(ASTClassOrInterfaceBody decl, Object data) {
-        Object result = super.visit(decl, data);
-
-        if (decl.getParent() instanceof ASTClassOrInterfaceDeclaration) {
-            ASTClassOrInterfaceDeclaration parent = (ASTClassOrInterfaceDeclaration) decl.getParent();
-            if (parent.isAbstract() || parent.isInterface() || parent.getSuperClassTypeNode() != null) {
-                return result;
+        boolean hasAnyMethods = false;
+        boolean hasNonPrivateCtor = false;
+        boolean hasAnyCtor = false;
+        for (ASTBodyDeclaration declaration : klass.getDeclarations()) {
+            if (declaration instanceof ASTFieldDeclaration
+                && !((ASTFieldDeclaration) declaration).isStatic()) {
+                return null;
             }
-
-            if (hasLombokNoArgsConstructor(parent)) {
-                return result;
-            }
-
-            int i = decl.getNumChildren();
-            int methodCount = 0;
-            boolean isOK = false;
-            while (i > 0) {
-                Node p = decl.getChild(--i);
-                if (p.getNumChildren() == 0) {
-                    continue;
-                }
-                Node n = skipAnnotations(p);
-                if (n instanceof ASTFieldDeclaration) {
-                    if (!((ASTFieldDeclaration) n).isStatic()) {
-                        isOK = true;
-                        break;
-                    }
-                } else if (n instanceof ASTConstructorDeclaration) {
-                    if (((ASTConstructorDeclaration) n).isPrivate()) {
-                        isOK = true;
-                        break;
-                    }
-                } else if (n instanceof ASTMethodDeclaration) {
-                    ASTMethodDeclaration m = (ASTMethodDeclaration) n;
-                    if (!m.isPrivate()) {
-                        methodCount++;
-                    }
-                    if (!m.isStatic()) {
-                        isOK = true;
-                        break;
-                    }
-
-                    // TODO use symbol table
-                    if ("suite".equals(m.getName())) {
-                        ASTResultType res = m.getResultType();
-                        ASTClassOrInterfaceType c = res.getFirstDescendantOfType(ASTClassOrInterfaceType.class);
-                        if (c != null && c.hasImageEqualTo("Test")) {
-                            isOK = true;
-                            break;
-                        }
-                    }
+            if (declaration instanceof ASTConstructorDeclaration) {
+                hasAnyCtor = true;
+                if (((ASTConstructorDeclaration) declaration).getVisibility() != Visibility.V_PRIVATE) {
+                    hasNonPrivateCtor = true;
                 }
             }
-            if (!isOK && methodCount > 0) {
-                addViolation(data, decl);
+
+            if (declaration instanceof ASTMethodDeclaration) {
+                if (((ASTMethodDeclaration) declaration).getVisibility() != Visibility.V_PRIVATE) {
+                    hasAnyMethods = true;
+                }
+                if (!((ASTMethodDeclaration) declaration).isStatic()) {
+                    return null;
+                }
             }
         }
-        return result;
+
+        // account for default ctor
+        hasNonPrivateCtor |= !hasAnyCtor
+            && klass.getVisibility() != Visibility.V_PRIVATE
+            && !hasLombokPrivateCtor(klass);
+
+
+        String message;
+        if (hasAnyMethods && hasNonPrivateCtor) {
+            message = "This utility class has a non-private constructor";
+            addViolationWithMessage(data, klass, message);
+        }
+        return null;
     }
 
-    private boolean hasLombokNoArgsConstructor(ASTClassOrInterfaceDeclaration parent) {
+    private boolean hasLombokPrivateCtor(ASTClassOrInterfaceDeclaration parent) {
         // check if there's a lombok no arg private constructor, if so skip the rest of the rules
 
         return parent.getDeclaredAnnotations()
@@ -105,18 +93,16 @@ public class UseUtilityClassRule extends AbstractLombokAwareRule {
                      .flatMap(ASTAnnotation::getMembers)
                      // to set the access level of a constructor in lombok, you set the access property on the annotation
                      .filterMatching(ASTMemberValuePair::getName, "access")
-                     .map(ASTMemberValuePair::getValue)
                      // This is from the AccessLevel enum in Lombok
                      // if the constructor is found and the accesslevel is private no need to check anything else
-                     .any(it -> "PRIVATE".equals(it.getImage()));
+                     .any(it -> isAccessToVarWithName(it.getValue(), "PRIVATE"));
     }
 
-    private Node skipAnnotations(Node p) {
-        int index = 0;
-        Node n = p.getChild(index++);
-        while (n instanceof ASTAnnotation && index < p.getNumChildren()) {
-            n = p.getChild(index++);
+    private static boolean isAccessToVarWithName(JavaNode node, String name) {
+        if (node instanceof ASTNamedReferenceExpr) {
+            return ((ASTNamedReferenceExpr) node).getName().equals(name);
         }
-        return n;
+        return false;
     }
+
 }
