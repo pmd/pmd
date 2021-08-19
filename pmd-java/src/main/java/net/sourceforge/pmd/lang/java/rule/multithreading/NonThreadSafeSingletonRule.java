dd9a757596c91ce7b87d4d8ed9b10b7a25dc5e86
Andreas Dangel <andreas.dangel@pmd-code.org>
2021-08-19 16:11:25 +0200

    [java] Fix false positive with valid double checked locking pattern

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/multithreading/NonThreadSafeSingletonRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/multithreading/NonThreadSafeSingletonRule.java
@@ -6,9 +6,9 @@ package net.sourceforge.pmd.lang.java.rule.multithreading;
 
 import static net.sourceforge.pmd.properties.PropertyFactory.booleanProperty;
 
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
 
 import net.sourceforge.pmd.RuleContext;
 import net.sourceforge.pmd.lang.java.ast.ASTAssignableExpr;
@@ -26,19 +26,19 @@ import net.sourceforge.pmd.lang.java.symbols.JFieldSymbol;
 import net.sourceforge.pmd.lang.java.symbols.JVariableSymbol;
 import net.sourceforge.pmd.properties.PropertyDescriptor;
 
-
 public class NonThreadSafeSingletonRule extends AbstractJavaRulechainRule {
 
-    private static final PropertyDescriptor<Boolean> CHECK_NON_STATIC_METHODS_DESCRIPTOR =
-            booleanProperty("checkNonStaticMethods")
+
+    private static final PropertyDescriptor<Boolean> CHECK_NON_STATIC_METHODS_DESCRIPTOR = booleanProperty(
+            "checkNonStaticMethods")
                     .desc("Check for non-static methods.  Do not set this to false and checkNonStaticFields to true.")
                     .defaultValue(true).build();
-    private static final PropertyDescriptor<Boolean> CHECK_NON_STATIC_FIELDS_DESCRIPTOR =
-            booleanProperty("checkNonStaticFields")
+    private static final PropertyDescriptor<Boolean> CHECK_NON_STATIC_FIELDS_DESCRIPTOR = booleanProperty(
+            "checkNonStaticFields")
                     .desc("Check for non-static fields.  Do not set this to true and checkNonStaticMethods to false.")
                     .defaultValue(false).build();
 
-    private Map<String, ASTFieldDeclaration> fieldDecls = new HashMap<>();
+    private Set<String> fields = new HashSet<>();
 
     private boolean checkNonStaticMethods = true;
     private boolean checkNonStaticFields = true;
@@ -49,56 +49,62 @@ public class NonThreadSafeSingletonRule extends AbstractJavaRulechainRule {
         definePropertyDescriptor(CHECK_NON_STATIC_FIELDS_DESCRIPTOR);
     }
 
+
     @Override
     public void start(RuleContext ctx) {
-        fieldDecls.clear();
+        fields.clear();
         checkNonStaticMethods = getProperty(CHECK_NON_STATIC_METHODS_DESCRIPTOR);
         checkNonStaticFields = getProperty(CHECK_NON_STATIC_FIELDS_DESCRIPTOR);
     }
+
 
     @Override
     public Object visit(ASTFieldDeclaration node, Object data) {
         if (checkNonStaticFields || node.hasModifiers(JModifier.STATIC)) {
             for (ASTVariableDeclaratorId varId : node.getVarIds()) {
-                fieldDecls.put(varId.getName(), node);
+                fields.add(varId.getName());
             }
         }
         return data;
     }
 
+
     @Override
     public Object visit(ASTMethodDeclaration node, Object data) {
-        if (checkNonStaticMethods && !node.hasModifiers(JModifier.STATIC) || node.hasModifiers(JModifier.SYNCHRONIZED)) {
+        if (checkNonStaticMethods && !node.hasModifiers(JModifier.STATIC)
+                || node.hasModifiers(JModifier.SYNCHRONIZED)) {
             return data;
         }
 
         List<ASTIfStatement> ifStatements = node.descendants(ASTIfStatement.class).toList();
         for (ASTIfStatement ifStatement : ifStatements) {
-            if (ifStatement.ancestors(ASTSynchronizedStatement.class).isEmpty()) {
-                if (ifStatement.getCondition().descendants(ASTNullLiteral.class).isEmpty()) {
+            if (ifStatement.getCondition().descendants(ASTNullLiteral.class).isEmpty()) {
+                continue;
+            }
+            ASTNamedReferenceExpr n = ifStatement.getCondition().descendants(ASTNamedReferenceExpr.class).first();
+            if (n == null || !fields.contains(n.getName())) {
+                continue;
+            }
+            List<ASTAssignmentExpression> assignments = ifStatement.descendants(ASTAssignmentExpression.class).toList();
+            boolean violation = false;
+            for (ASTAssignmentExpression assignment : assignments) {
+                if (assignment.ancestors(ASTSynchronizedStatement.class).nonEmpty()) {
                     continue;
                 }
-                ASTNamedReferenceExpr n = ifStatement.getCondition().descendants(ASTNamedReferenceExpr.class).first();
-                if (n == null || !fieldDecls.containsKey(n.getName())) {
-                    continue;
-                }
-                List<ASTAssignmentExpression> assignments = ifStatement.descendants(ASTAssignmentExpression.class).toList();
-                boolean violation = false;
-                for (ASTAssignmentExpression assignment : assignments) {
-                    ASTAssignableExpr left = assignment.getLeftOperand();
-                    if (left instanceof ASTNamedReferenceExpr) {
-                        JVariableSymbol referencedSym = ((ASTNamedReferenceExpr) left).getReferencedSym();
-                        if (referencedSym instanceof JFieldSymbol) {
-                            String name = ((ASTNamedReferenceExpr) left).getName();
-                            if (fieldDecls.containsKey(name)) {
-                                violation = true;
-                            }
+
+                ASTAssignableExpr left = assignment.getLeftOperand();
+                if (left instanceof ASTNamedReferenceExpr) {
+                    JVariableSymbol referencedSym = ((ASTNamedReferenceExpr) left).getReferencedSym();
+                    if (referencedSym instanceof JFieldSymbol) {
+                        String name = ((ASTNamedReferenceExpr) left).getName();
+                        if (fields.contains(name)) {
+                            violation = true;
                         }
                     }
                 }
-                if (violation) {
-                    addViolation(data, ifStatement);
-                }
+            }
+            if (violation) {
+                addViolation(data, ifStatement);
             }
         }
         return data;
