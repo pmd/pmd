1ae9d17b667f24f95b4f877e762d6f8a1c136667
Clément Fournier <clement.fournier76@gmail.com>
2021-09-15 18:25:04 +0200

    checkstyle issues

--- pmd-kotlin/src/main/java/net/sourceforge/pmd/lang/kotlin/ast/KotlinNode.java
+++ pmd-kotlin/src/main/java/net/sourceforge/pmd/lang/kotlin/ast/KotlinNode.java
@@ -1,4 +1,9 @@
+/*
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
 package net.sourceforge.pmd.lang.kotlin.ast;
+
 import net.sourceforge.pmd.lang.ast.impl.antlr4.AntlrNode;
 
 /**
