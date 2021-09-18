86332e8a57b39188ab8e22b69ae7421c2ab9c302
Clément Fournier <clement.fournier76@gmail.com>
2021-09-18 23:44:53 +0200

    Refactor LawOfDemeter to give better semantics to iterator/collections

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/design/LawOfDemeterRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/design/LawOfDemeterRule.java
@@ -4,17 +4,21 @@
 
 package net.sourceforge.pmd.lang.java.rule.design;
 
-import java.util.Iterator;
-import java.util.List;
+import java.util.Collection;
 
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 import net.sourceforge.pmd.lang.java.ast.ASTAssignableExpr.ASTNamedReferenceExpr;
 import net.sourceforge.pmd.lang.java.ast.ASTExpression;
+import net.sourceforge.pmd.lang.java.ast.ASTForeachStatement;
 import net.sourceforge.pmd.lang.java.ast.ASTMethodCall;
 import net.sourceforge.pmd.lang.java.rule.AbstractJavaRulechainRule;
 import net.sourceforge.pmd.lang.java.rule.internal.DataflowPass;
+import net.sourceforge.pmd.lang.java.rule.internal.DataflowPass.AssignmentEntry;
+import net.sourceforge.pmd.lang.java.rule.internal.DataflowPass.DataflowResult;
 import net.sourceforge.pmd.lang.java.rule.internal.DataflowPass.ReachingDefinitionSet;
+import net.sourceforge.pmd.lang.java.rule.internal.JavaRuleUtil;
+import net.sourceforge.pmd.lang.java.types.InvocationMatcher;
 import net.sourceforge.pmd.lang.java.types.JClassType;
 import net.sourceforge.pmd.lang.java.types.TypeTestUtil;
 import net.sourceforge.pmd.util.CollectionUtil;
@@ -38,6 +42,8 @@ import net.sourceforge.pmd.util.OptionalBool;
  *
  */
 public class LawOfDemeterRule extends AbstractJavaRulechainRule {
+
+    private static final InvocationMatcher ITERATOR_NEXT = InvocationMatcher.parse("java.util.Iterator#next()");
 
     public LawOfDemeterRule() {
         super(ASTMethodCall.class);
@@ -80,11 +86,45 @@ public class LawOfDemeterRule extends AbstractJavaRulechainRule {
             return OptionalBool.UNKNOWN;
         }
 
-        return OptionalBool.definitely(CollectionUtil.any(
-            reaching.getReaching(),
-            def -> def.getRhsAsExpression() instanceof ASTMethodCall
-                && !TypeTestUtil.isA(Iterator.class, def.getRhsAsExpression())
-                && !TypeTestUtil.isA(List.class, ((ASTMethodCall) def.getRhsAsExpression()).getQualifier())
-        ));
+        return OptionalBool.definitely(CollectionUtil.any(reaching.getReaching(), this::isForeign));
     }
+
+    private boolean isForeign(AssignmentEntry def) {
+        if (def.isForeachVar()) {
+            ASTForeachStatement foreach = def.getVarId().ancestors(ASTForeachStatement.class).firstOrThrow();
+            return isForeign(foreach.getIterableExpr());
+        }
+        // formal parameters are not foreign otherwise we couldn't call any methods on them
+        // if (def.getVarId().isFormalParameter()) {
+        //     return true;
+        // }
+        return isForeign(def.getRhsAsExpression());
+    }
+
+    private boolean isForeign(@Nullable ASTExpression expr) {
+        if (expr == null) {
+            return false;
+        }
+        if (expr instanceof ASTMethodCall) {
+            if (ITERATOR_NEXT.matchesCall(expr)
+                || TypeTestUtil.isA(Collection.class, ((ASTMethodCall) expr).getQualifier())) {
+                // an iterator.next() is as foreign as the qualifier
+                // a list call is as foreign as the list
+                // ie, pure data containers are "transparent"
+                return isForeign(((ASTMethodCall) expr).getQualifier());
+            }
+            return true;
+        } else if (expr instanceof ASTNamedReferenceExpr) {
+            DataflowResult dataflow = DataflowPass.getDataflowResult(expr.getRoot());
+            ReachingDefinitionSet reaching = dataflow.getReachingDefinitions((ASTNamedReferenceExpr) expr);
+            if (reaching.isNotFullyKnown()) {
+                // fields of this instance are not foreign
+                return !JavaRuleUtil.isRefToFieldOfThisInstance(expr);
+            }
+
+            return CollectionUtil.any(reaching.getReaching(), this::isForeign);
+        }
+        return false;
+    }
+
 }
