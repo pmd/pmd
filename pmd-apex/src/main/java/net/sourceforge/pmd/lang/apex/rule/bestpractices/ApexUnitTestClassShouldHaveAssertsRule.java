230fc4c805367a13afd6ca2c928b2489a175eb1b
Scott Wells <scott@illuminatedcloud.com>
2021-10-20 11:40:35 -0500

    Issue 1089 - Added a new configuration property, 'additionalAssertMethodPattern', that when specified is also evaluated against each invoked method name to determine whether it represents a test assertion. This will provide support for those who use testing facades such as sirono-common's Assert, or who encapsulate common assertions in local utility methods.

--- pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/bestpractices/ApexUnitTestClassShouldHaveAssertsRule.java
+++ pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/bestpractices/ApexUnitTestClassShouldHaveAssertsRule.java
@@ -4,11 +4,15 @@
 
 package net.sourceforge.pmd.lang.apex.rule.bestpractices;
 
+import static net.sourceforge.pmd.properties.PropertyFactory.stringProperty;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import net.sourceforge.pmd.lang.apex.ast.ASTBlockStatement;
 import net.sourceforge.pmd.lang.apex.ast.ASTMethod;
@@ -16,6 +20,7 @@ import net.sourceforge.pmd.lang.apex.ast.ASTMethodCallExpression;
 import net.sourceforge.pmd.lang.apex.ast.ASTStatement;
 import net.sourceforge.pmd.lang.apex.ast.ApexNode;
 import net.sourceforge.pmd.lang.apex.rule.AbstractApexUnitTestRule;
+import net.sourceforge.pmd.properties.PropertyDescriptor;
 
 /**
  * Apex unit tests should have System.assert methods in them
@@ -30,6 +35,18 @@ public class ApexUnitTestClassShouldHaveAssertsRule extends AbstractApexUnitTest
         ASSERT_METHODS.add("system.assert");
         ASSERT_METHODS.add("system.assertequals");
         ASSERT_METHODS.add("system.assertnotequals");
+        // Fully-qualified variants...rare but still valid/possible
+        ASSERT_METHODS.add("system.system.assert");
+        ASSERT_METHODS.add("system.system.assertequals");
+        ASSERT_METHODS.add("system.system.assertnotequals");
+    }
+
+    private static final PropertyDescriptor<String> ADDITIONAL_ASSERT_METHOD_PATTERN_DESCRIPTOR =
+        stringProperty("additionalAssertMethodPattern")
+            .desc("A regular expression for one or more custom test assertion method patterns.").defaultValue("").build();
+
+    public ApexUnitTestClassShouldHaveAssertsRule() {
+        definePropertyDescriptor(ADDITIONAL_ASSERT_METHOD_PATTERN_DESCRIPTOR);
     }
 
     @Override
@@ -55,6 +72,26 @@ public class ApexUnitTestClassShouldHaveAssertsRule extends AbstractApexUnitTest
             if (ASSERT_METHODS.contains(methodCallExpression.getFullMethodName().toLowerCase(Locale.ROOT))) {
                 isAssertFound = true;
                 break;
+            }
+        }
+
+        // If we didn't find assert method invocations the simple way and we have a configured pattern, try it
+        if (!isAssertFound) {
+            final String additionalAssertPattern = getProperty(ADDITIONAL_ASSERT_METHOD_PATTERN_DESCRIPTOR);
+            if (additionalAssertPattern != null && !additionalAssertPattern.trim().isEmpty()) {
+                try {
+                    final Pattern compiledPattern = Pattern.compile(additionalAssertPattern, Pattern.CASE_INSENSITIVE);
+                    for (final ASTMethodCallExpression methodCallExpression : methodCalls) {
+                        final String fullMethodName = methodCallExpression.getFullMethodName();
+                        final Matcher assertMethodMatcher = compiledPattern.matcher(fullMethodName);
+                        if (assertMethodMatcher.matches()) {
+                            isAssertFound = true;
+                            break;
+                        }
+                    }
+                } catch (Exception e) {
+                    throw new IllegalArgumentException("Invalid assert method pattern '" + additionalAssertPattern + "' - " + e.getMessage());
+                }
             }
         }
 
