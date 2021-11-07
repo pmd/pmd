5ec56a91ef21e9dc386fffe2e367f0d37c05bd6f
Clément Fournier <clement.fournier76@gmail.com>
2021-11-07 21:35:45 +0100

    Output a deprecation warning

--- pmd-core/src/main/java/net/sourceforge/pmd/cli/PmdParametersParseResult.java
+++ pmd-core/src/main/java/net/sourceforge/pmd/cli/PmdParametersParseResult.java
@@ -4,6 +4,11 @@
 
 package net.sourceforge.pmd.cli;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Objects;
 
 import net.sourceforge.pmd.PMDConfiguration;
@@ -20,15 +25,19 @@ public final class PmdParametersParseResult {
 
     private final PMDParameters result;
     private final ParameterException error;
+    private final Map<String, String> deprecatedOptionsUsed;
 
-    PmdParametersParseResult(PMDParameters result) {
+    PmdParametersParseResult(PMDParameters result,
+                             Map<String, String> deprecatedOptionsUsed) {
         this.result = Objects.requireNonNull(result);
+        this.deprecatedOptionsUsed = deprecatedOptionsUsed;
         this.error = null;
     }
 
-    PmdParametersParseResult(ParameterException error) {
+    PmdParametersParseResult(ParameterException error, Map<String, String> deprecatedOptionsUsed) {
         this.result = null;
         this.error = Objects.requireNonNull(error);
+        this.deprecatedOptionsUsed = deprecatedOptionsUsed;
     }
 
     /** Returns true if parsing failed. */
@@ -62,6 +71,15 @@ public final class PmdParametersParseResult {
     }
 
     /**
+     * Returns a map of deprecated CLI options used by the command that
+     * created this instance. Each key is a deprecated option that was used,
+     * and the value is a suggested replacement (a piece of English text).
+     */
+    public Map<String, String> getDeprecatedOptionsUsed() {
+        return deprecatedOptionsUsed;
+    }
+
+    /**
      * Returns the resulting configuration if parsing succeeded and not {@link #isHelp().
      * Otherwise returns null.
      */
@@ -88,9 +106,50 @@ public final class PmdParametersParseResult {
 
         try {
             jcommander.parse(args);
-            return new PmdParametersParseResult(result);
+            return new PmdParametersParseResult(result, filterDeprecatedOptions(args));
         } catch (ParameterException e) {
-            return new PmdParametersParseResult(e);
+            return new PmdParametersParseResult(e, filterDeprecatedOptions(args));
         }
+    }
+
+    private static Map<String, String> filterDeprecatedOptions(String... args) {
+        Map<String, String> argSet = new HashMap<>(SUGGESTED_REPLACEMENT);
+        argSet.keySet().retainAll(new HashSet<>(Arrays.asList(args)));
+        return Collections.unmodifiableMap(argSet);
+    }
+
+    /** Map of deprecated option to suggested replacement. */
+    private static final Map<String, String> SUGGESTED_REPLACEMENT;
+
+    static {
+        Map<String, String> m = new HashMap<>();
+
+        m.put("-rulesets", "--rulesets (or -R)");
+        m.put("-uri", "--uri");
+        m.put("-dir", "--dir (or -d)");
+        m.put("-filelist", "--file-list");
+        m.put("-ignorelist", "--ignore-list");
+        m.put("-format", "--format (or -f)");
+        m.put("-debug", "--debug");
+        m.put("-verbose", "--verbose");
+        m.put("-help", "--help");
+        m.put("-encoding", "--encoding");
+        m.put("-threads", "--threads");
+        m.put("-benchmark", "--benchmark");
+        m.put("-stress", "--stress");
+        m.put("-shortnames", "--short-names");
+        m.put("-showsuppressed", "--show-suppressed");
+        m.put("-suppressmarker", "--suppress-marker");
+        m.put("-minimumpriority", "--minimum-priority");
+        m.put("-property", "--property");
+        m.put("-reportfile", "--report-file");
+        m.put("-force-language", "--force-language");
+        m.put("-auxclasspath", "--aux-classpath");
+        m.put("-failOnViolation", "--fail-on-violation");
+        m.put("--failOnViolation", "--fail-on-violation");
+        m.put("-norulesetcompatibility", "--no-ruleset-compatibility");
+        m.put("-cache", "--cache");
+        m.put("-no-cache", "--no-cache");
+        SUGGESTED_REPLACEMENT = Collections.unmodifiableMap(m);
     }
 }
