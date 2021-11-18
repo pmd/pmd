d7774be75e6de8ef9394fc7b1044b064e5e91b3c
Andreas Dangel <andreas.dangel@pmd-code.org>
2021-11-18 11:10:53 +0100

    [dist] Add java17, make ant test fail

--- .ci/build.sh
+++ .ci/build.sh
@@ -9,14 +9,15 @@ SCRIPT_INCLUDES="log.bash utils.bash setup-secrets.bash openjdk.bash maven.bash 
 source "$(dirname "$0")/inc/fetch_ci_scripts.bash" && fetch_ci_scripts
 
 function build() {
-    pmd_ci_log_group_start "Prepare Java 7+11, Bundler"
+    pmd_ci_log_group_start "Prepare Java 7+11+17, Bundler"
         pmd_ci_openjdk_install_adoptopenjdk 11
         pmd_ci_openjdk_setdefault 11
         PMD_MAVEN_EXTRA_OPTS=()
         if [ "$(pmd_ci_utils_get_os)" = "linux" ]; then
             pmd_ci_log_info "Install openjdk7 for integration tests"
             pmd_ci_openjdk_install_zuluopenjdk 7
-            PMD_MAVEN_EXTRA_OPTS=(-Djava7.home="${HOME}/openjdk7")
+            pmd_ci_openjdk_install_adoptopenjdk 17
+            PMD_MAVEN_EXTRA_OPTS=(-Djava7.home="${HOME}/openjdk7" -Djava17.home="${HOME}/openjdk17")
         fi
         pmd_ci_build_setup_bundler
     pmd_ci_log_group_end
--- pmd-dist/src/test/java/net/sourceforge/pmd/it/AntIT.java
+++ pmd-dist/src/test/java/net/sourceforge/pmd/it/AntIT.java
@@ -10,15 +10,26 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.SystemUtils;
+import org.junit.Assume;
 import org.junit.Test;
-import org.shaded.apache.commons.io.FileUtils;
 
 import net.sourceforge.pmd.PMDVersion;
 
+/**
+ * This test calls ant in a fake terminal to make sure we have a {@link java.io.Console} connected.
+ * This however only works under linux.
+ * <p>
+ * See <a href="# https://stackoverflow.com/questions/1401002/how-to-trick-an-application-into-thinking-its-stdout-is-a-terminal-not-a-pipe/20401674#20401674">How to trick an application into thinking its stdout is a terminal, not a pipe</a>.
+ */
 public class AntIT extends AbstractBinaryDistributionTest {
+
     @Test
     public void runAnt() throws IOException, InterruptedException {
+        Assume.assumeTrue(SystemUtils.IS_OS_UNIX);
+
         String antBasepath = new File("target/ant").getAbsolutePath();
         String pmdHome = tempDir.resolve(PMD_BIN_PREFIX + PMDVersion.VERSION).toAbsolutePath().toString();
         File antTestProjectFolder = prepareAntTestProjectFolder();
@@ -39,8 +50,11 @@ public class AntIT extends AbstractBinaryDistributionTest {
 
     private ExecutionResult runAnt(String antLibPath, String pmdHomePath, File antTestProjectFolder)
             throws IOException, InterruptedException {
-        ProcessBuilder pb = new ProcessBuilder(System.getenv("JAVA_HOME") + "/bin/java", "-cp", antLibPath + "/*",
-                "-jar", antLibPath + "/ant-launcher.jar", "-Dpmd.home=" + pmdHomePath);
+        String cmd = System.getenv("JAVA_HOME") + "/bin/java" + " -cp \"" + antLibPath + "/*\""
+                + " -jar " + antLibPath + "/ant-launcher.jar -Dpmd.home=" + pmdHomePath;
+
+        // https://stackoverflow.com/questions/1401002/how-to-trick-an-application-into-thinking-its-stdout-is-a-terminal-not-a-pipe/20401674#20401674
+        ProcessBuilder pb = new ProcessBuilder("script", "-qfec", cmd, "/dev/null");
         pb.directory(antTestProjectFolder);
         pb.redirectErrorStream(true);
 
