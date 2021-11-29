4cf3d848e3328384387d7201d197b0e83d89e3a5
Andreas Dangel <andreas.dangel@pmd-code.org>
2021-11-29 11:39:23 +0100

    [java] UnusedLocalVariable - fix false positive with try-with-resources

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/bestpractices/UnusedLocalVariableRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/bestpractices/UnusedLocalVariableRule.java
@@ -4,26 +4,24 @@
 
 package net.sourceforge.pmd.lang.java.rule.bestpractices;
 
-import org.checkerframework.checker.nullness.qual.NonNull;
-
 import net.sourceforge.pmd.lang.java.ast.ASTLocalVariableDeclaration;
+import net.sourceforge.pmd.lang.java.ast.ASTResource;
 import net.sourceforge.pmd.lang.java.ast.ASTVariableDeclaratorId;
-import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
+import net.sourceforge.pmd.lang.java.rule.AbstractJavaRulechainRule;
 import net.sourceforge.pmd.lang.java.rule.internal.JavaRuleUtil;
-import net.sourceforge.pmd.lang.rule.RuleTargetSelector;
 
-public class UnusedLocalVariableRule extends AbstractJavaRule {
+public class UnusedLocalVariableRule extends AbstractJavaRulechainRule {
 
-    @Override
-    protected @NonNull RuleTargetSelector buildTargetSelector() {
-        return RuleTargetSelector.forTypes(ASTLocalVariableDeclaration.class);
+    public UnusedLocalVariableRule() {
+        super(ASTLocalVariableDeclaration.class);
     }
 
     @Override
     public Object visit(ASTLocalVariableDeclaration decl, Object data) {
         for (ASTVariableDeclaratorId varId : decl.getVarIds()) {
             if (JavaRuleUtil.isNeverUsed(varId)
-                && !JavaRuleUtil.isExplicitUnusedVarName(varId.getName())) {
+                && !JavaRuleUtil.isExplicitUnusedVarName(varId.getName())
+                && !(varId.ancestors().get(2) instanceof ASTResource)) {
                 addViolation(data, varId, varId.getName());
             }
         }
