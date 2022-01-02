cd0196bc23bf16e675cc0c4dd648cc73882785a1
Clément Fournier <clement.fournier76@gmail.com>
2022-01-02 17:18:51 +0100

    Remove dep on AbstractIgnoredAnnotationRule

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/errorprone/BeanMembersShouldSerializeRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/errorprone/BeanMembersShouldSerializeRule.java
@@ -6,13 +6,11 @@ package net.sourceforge.pmd.lang.java.rule.errorprone;
 
 import static net.sourceforge.pmd.properties.PropertyFactory.stringProperty;
 
-import java.util.Arrays;
-import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
-import org.checkerframework.checker.nullness.qual.NonNull;
 
 import net.sourceforge.pmd.RuleContext;
 import net.sourceforge.pmd.lang.ast.NodeStream;
@@ -20,35 +18,33 @@ import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTFieldDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTMethodDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTVariableDeclaratorId;
+import net.sourceforge.pmd.lang.java.ast.Annotatable;
 import net.sourceforge.pmd.lang.java.ast.JModifier;
-import net.sourceforge.pmd.lang.java.rule.AbstractIgnoredAnnotationRule;
+import net.sourceforge.pmd.lang.java.rule.AbstractJavaRulechainRule;
+import net.sourceforge.pmd.lang.java.rule.internal.JavaPropertyUtil;
 import net.sourceforge.pmd.lang.java.rule.internal.JavaRuleUtil;
-import net.sourceforge.pmd.lang.rule.RuleTargetSelector;
 import net.sourceforge.pmd.properties.PropertyDescriptor;
 
-public class BeanMembersShouldSerializeRule extends AbstractIgnoredAnnotationRule {
+public class BeanMembersShouldSerializeRule extends AbstractJavaRulechainRule {
 
     private String prefixProperty;
 
+
+    private static final PropertyDescriptor<List<String>> IGNORED_ANNOTATIONS_DESCRIPTOR
+        = JavaPropertyUtil.ignoredAnnotationsDescriptor(
+        "lombok.Data",
+        "lombok.Getter",
+        "lombok.Value"
+    );
+
+
     private static final PropertyDescriptor<String> PREFIX_DESCRIPTOR = stringProperty("prefix")
-            .desc("A variable prefix to skip, i.e., m_").defaultValue("").build();
+        .desc("A variable prefix to skip, i.e., m_").defaultValue("").build();
 
     public BeanMembersShouldSerializeRule() {
+        super(ASTClassOrInterfaceDeclaration.class);
         definePropertyDescriptor(PREFIX_DESCRIPTOR);
-    }
-
-    @Override
-    protected @NonNull RuleTargetSelector buildTargetSelector() {
-        return RuleTargetSelector.forTypes(ASTClassOrInterfaceDeclaration.class);
-    }
-
-    @Override
-    protected Collection<String> defaultSuppressionAnnotations() {
-        return Arrays.asList(
-            "lombok.Data",
-            "lombok.Getter",
-            "lombok.Value"
-        );
+        definePropertyDescriptor(IGNORED_ANNOTATIONS_DESCRIPTOR);
     }
 
     @Override
@@ -100,5 +96,9 @@ public class BeanMembersShouldSerializeRule extends AbstractIgnoredAnnotationRul
             return img.substring(prefixProperty.length());
         }
         return img;
+    }
+
+    private boolean hasIgnoredAnnotation(Annotatable node) {
+        return node.isAnyAnnotationPresent(getProperty(IGNORED_ANNOTATIONS_DESCRIPTOR));
     }
 }
