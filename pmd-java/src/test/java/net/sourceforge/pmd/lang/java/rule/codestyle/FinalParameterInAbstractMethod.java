59422ffb55a60ca2a4b6b79325cd522115edfd1e
Vincent Galloy <galloy.v@gmail.com>
2022-01-06 17:30:57 +0100

    Fix test class name

--- pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/codestyle/FinalParameterInAbstractMethod.java
+++ pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/codestyle/FinalParameterInAbstractMethod.java
@@ -6,6 +6,6 @@ package net.sourceforge.pmd.lang.java.rule.codestyle;
 
 import net.sourceforge.pmd.testframework.PmdRuleTst;
 
-public class FinalParameterInAbstractMethod extends PmdRuleTst {
+public class FinalParameterInAbstractMethodTest extends PmdRuleTst {
     // no additional unit tests
 }
