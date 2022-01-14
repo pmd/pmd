83d5a136c8c6789d0f02f542760f7dcc05e34443
Andreas Dangel <andreas.dangel@pmd-code.org>
2022-01-14 12:02:49 +0100

    [java] ClassWithOnlyPrivateConstructorsShouldBeFinal - consider nested
    Nested classes should only be flagged, if they are not instantiated.

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/design/ClassWithOnlyPrivateConstructorsShouldBeFinalRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/design/ClassWithOnlyPrivateConstructorsShouldBeFinalRule.java
@@ -6,12 +6,18 @@ package net.sourceforge.pmd.lang.java.rule.design;
 
 import static net.sourceforge.pmd.lang.java.ast.AccessNode.Visibility.V_PRIVATE;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import net.sourceforge.pmd.lang.java.ast.ASTAnyTypeDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceDeclaration;
+import net.sourceforge.pmd.lang.java.ast.ASTConstructorCall;
 import net.sourceforge.pmd.lang.java.ast.ASTConstructorDeclaration;
-import net.sourceforge.pmd.lang.java.ast.ASTMethodDeclaration;
+import net.sourceforge.pmd.lang.java.ast.AccessNode.Visibility;
 import net.sourceforge.pmd.lang.java.ast.JModifier;
 import net.sourceforge.pmd.lang.java.rule.AbstractJavaRulechainRule;
+import net.sourceforge.pmd.lang.java.symbols.JConstructorSymbol;
+import net.sourceforge.pmd.lang.java.symbols.JExecutableSymbol;
 import net.sourceforge.pmd.lang.java.types.TypeTestUtil;
 
 public class ClassWithOnlyPrivateConstructorsShouldBeFinalRule extends AbstractJavaRulechainRule {
@@ -25,8 +31,8 @@ public class ClassWithOnlyPrivateConstructorsShouldBeFinalRule extends AbstractJ
         if (node.isRegularClass()
             && !node.hasModifiers(JModifier.FINAL)
             && hasOnlyPrivateCtors(node)
-            && hasNoSubclasses(node)
-            && hasNoAbstractMethods(node)) {
+            && isNotInstantiated(node)
+            && hasNoSubclasses(node)) {
             addViolation(data, node);
         }
         return null;
@@ -49,9 +55,32 @@ public class ClassWithOnlyPrivateConstructorsShouldBeFinalRule extends AbstractJ
             || node.getDeclarations(ASTConstructorDeclaration.class).nonEmpty());
     }
 
-    private boolean hasNoAbstractMethods(ASTClassOrInterfaceDeclaration node) {
-        return node.getDeclarations()
-            .filterIs(ASTMethodDeclaration.class)
-            .none(m -> m.hasModifiers(JModifier.ABSTRACT));
+    private boolean isNotInstantiated(ASTClassOrInterfaceDeclaration node) {
+        List<JExecutableSymbol> constructorCalls = node.getRoot().descendants(ASTConstructorCall.class)
+                .toStream()
+                .map(call -> call.getMethodType().getSymbol())
+                .collect(Collectors.toList());
+
+        List<ASTConstructorDeclaration> constructors = node.descendants(ASTConstructorDeclaration.class)
+                .toList();
+
+        for (JExecutableSymbol call : constructorCalls) {
+            if (
+                // an explicitly defined constructor is used
+                constructors.contains(call.tryGetNode())
+
+                // or the implicitly defined default ctor is used
+                || node.isNested() && node.getVisibility() == Visibility.V_PRIVATE
+                        && isDefaultConstructorCall(node, call)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isDefaultConstructorCall(ASTClassOrInterfaceDeclaration node, JExecutableSymbol constructorCall) {
+        return constructorCall instanceof JConstructorSymbol
+                && constructorCall.getArity() == 0
+                && constructorCall.getEnclosingClass().tryGetNode() == node;
     }
 }
