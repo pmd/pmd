5d30c39ce74f3576004d4ddd81658b2bc5769290
Clément Fournier <clement.fournier76@gmail.com>
2022-01-23 19:31:08 +0100

    checkstyle

--- pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/codestyle/unnecessaryimport/ConcFlow.java
+++ pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/codestyle/unnecessaryimport/ConcFlow.java
@@ -3,14 +3,16 @@
  */
 
 
-
 package net.sourceforge.pmd.lang.java.rule.codestyle.unnecessaryimport;
 
 /**
  * Minimized version of https://github.com/openjdk/jdk/blob/jdk-11%2B28/src/java.base/share/classes/java/util/concurrent/Flow.java
  */
 public class ConcFlow {
-    public interface Subscription {}
-    public interface Subscriber<T> {}
-    public interface Publisher<T> {}
+
+    public interface Subscription { }
+
+    public interface Subscriber<T> { }
+
+    public interface Publisher<T> { }
 }
