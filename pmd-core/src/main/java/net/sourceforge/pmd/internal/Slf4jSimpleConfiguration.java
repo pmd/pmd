7b702bd6ebd3c629671e2db3ce1bfebddcc80ace
Andreas Dangel <andreas.dangel@pmd-code.org>
2022-02-18 16:44:31 +0100

    [core] Add jul-to-slf4j bridge
    This is at least needed for apex jorje, but might be handy
    for any other library that logs through jul.

--- pmd-core/src/main/java/net/sourceforge/pmd/internal/Slf4jSimpleConfiguration.java
+++ pmd-core/src/main/java/net/sourceforge/pmd/internal/Slf4jSimpleConfiguration.java
@@ -9,6 +9,7 @@ import java.lang.reflect.Method;
 import org.slf4j.ILoggerFactory;
 import org.slf4j.LoggerFactory;
 import org.slf4j.LoggerFactoryFriend;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.slf4j.event.Level;
 
 public final class Slf4jSimpleConfiguration {
@@ -34,8 +35,14 @@ public final class Slf4jSimpleConfiguration {
 
         LoggerFactoryFriend.reset();
     }
-    
+
     public static void disableLogging(Class<?> clazz) {
         System.setProperty("org.slf4j.simpleLogger.log." + clazz.getName(), "off");
+    }
+
+    public static void installJulBridge() {
+        if (!SLF4JBridgeHandler.isInstalled()) {
+            SLF4JBridgeHandler.install();
+        }
     }
 }
