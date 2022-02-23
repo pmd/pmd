4aa7b56faba29170b61457d6467068c8bdb7e3ad
Clément Fournier <clement.fournier76@gmail.com>
2022-02-24 00:02:17 +0100

    Update reference files

---
+++ pmd-java/src/test/java/net/sourceforge/pmd/lang/java/ast/AllJavaAstTreeDumpTest.java
@@ -0,0 +1,22 @@
+/**
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
+package net.sourceforge.pmd.lang.java.ast;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+    ParserCornersTest.class,
+    Java15TreeDumpTest.class,
+    Java16PreviewTreeDumpTest.class,
+    Java16TreeDumpTest.class,
+    Java17PreviewTreeDumpTest.class,
+    Java17TreeDumpTest.class
+})
+public class AllJavaAstTreeDumpTest {
+
+}
