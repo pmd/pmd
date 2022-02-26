e27e595e582ee3462db1a6bc07cd38a4476331eb
Clément Fournier <clement.fournier76@gmail.com>
2022-02-26 23:56:44 +0100

    Add back tests that disappeared by mistake

--- pmd-java/src/test/java/net/sourceforge/pmd/lang/java/ast/ParserCornersTest.java
+++ pmd-java/src/test/java/net/sourceforge/pmd/lang/java/ast/ParserCornersTest.java
@@ -136,11 +136,11 @@ public class ParserCornersTest extends BaseJavaTreeDumpTest {
      */
     @Test
     public void testGenericsProblem() {
-        String code = "public class Test {\n" +
-            " public void test() {\n" +
-            "   String o = super.<String> doStuff(\"\");\n" +
-            " }\n" +
-            "}";
+        String code = "public class Test {\n"
+            + " public void test() {\n"
+            + "   String o = super.<String> doStuff(\"\");\n"
+            + " }\n"
+            + "}";
         java5.parse(code);
         java7.parse(code);
     }
@@ -266,10 +266,10 @@ public class ParserCornersTest extends BaseJavaTreeDumpTest {
     @Test
     public void testMethodReferenceConfused() {
         ASTCompilationUnit ast = java.parseResource("MethodReferenceConfused.java", "10");
-        ASTVariableDeclaratorId someVarNameSameAsMethodReference = AstTestUtil.varId(ast, "someVarNameSameAsMethodReference");
+        ASTVariableDeclaratorId varWithMethodName = AstTestUtil.varId(ast, "method");
         ASTVariableDeclaratorId someObject = AstTestUtil.varId(ast, "someObject");
 
-        assertThat(someVarNameSameAsMethodReference.getLocalUsages(), empty());
+        assertThat(varWithMethodName.getLocalUsages(), empty());
         assertThat(someObject.getLocalUsages(), hasSize(1));
         ASTNamedReferenceExpr usage = someObject.getLocalUsages().get(0);
         assertThat(usage.getParent(), instanceOf(ASTCastExpression.class));
