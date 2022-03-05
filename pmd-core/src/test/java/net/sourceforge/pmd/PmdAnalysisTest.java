e948152d2cb1550e859a9d0227058b8893cc3a70
Clément Fournier <clement.fournier76@gmail.com>
2022-03-05 12:12:48 +0100

    Add mockito tests

--- pmd-core/src/test/java/net/sourceforge/pmd/PmdAnalysisTest.java
+++ pmd-core/src/test/java/net/sourceforge/pmd/PmdAnalysisTest.java
@@ -5,9 +5,18 @@
 package net.sourceforge.pmd;
 
 import static org.hamcrest.Matchers.empty;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.io.IOException;
 
 import org.hamcrest.MatcherAssert;
 import org.junit.Test;
+import org.mockito.ArgumentMatchers;
+
+import net.sourceforge.pmd.renderers.Renderer;
 
 /**
  * @author Clément Fournier
@@ -22,6 +31,23 @@ public class PmdAnalysisTest {
             MatcherAssert.assertThat(pmd.rulesets(), empty());
             MatcherAssert.assertThat(pmd.renderers(), empty());
         }
+    }
+
+    @Test
+    public void testRendererStart() throws IOException {
+        PMDConfiguration config = new PMDConfiguration();
+        config.setInputPaths("sample-source/dummy");
+        Renderer renderer = mock(Renderer.class);
+        try (PmdAnalysis pmd = PmdAnalysis.create(config)) {
+            pmd.addRenderer(renderer);
+            verify(renderer, never()).start();
+            pmd.performAnalysis();
+        }
+
+        verify(renderer, times(1)).renderFileReport(ArgumentMatchers.<Report>any());
+        verify(renderer, times(1)).start();
+        verify(renderer, times(1)).end();
+        verify(renderer, times(1)).flush();
     }
 
 }
