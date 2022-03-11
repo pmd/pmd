5dc797bae530f4f5cc75142c1508db607f1a4f02
Andreas Dangel <andreas.dangel@pmd-code.org>
2022-03-11 10:52:38 +0100

    Fix build after merge

--- pmd-kotlin/src/test/java/net/sourceforge/pmd/lang/kotlin/ast/KotlinParsingHelper.java
+++ pmd-kotlin/src/test/java/net/sourceforge/pmd/lang/kotlin/ast/KotlinParsingHelper.java
@@ -14,7 +14,7 @@ import net.sourceforge.pmd.lang.kotlin.KotlinLanguageModule;
  */
 public class KotlinParsingHelper extends BaseParsingHelper<KotlinParsingHelper, KotlinParser.KtKotlinFile> {
 
-    public static final KotlinParsingHelper DEFAULT = new KotlinParsingHelper(Params.getDefaultNoProcess());
+    public static final KotlinParsingHelper DEFAULT = new KotlinParsingHelper(Params.getDefault());
 
 
     public KotlinParsingHelper(@NotNull Params params) {
