9513c505fb4e5f975de61686b33ccaf6b56a9c07
Clément Fournier <clement.fournier76@gmail.com>
2022-04-15 19:02:04 +0200

    checkstyle

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/bestpractices/UnusedPrivateFieldRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/bestpractices/UnusedPrivateFieldRule.java
@@ -5,7 +5,6 @@
 package net.sourceforge.pmd.lang.java.rule.bestpractices;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
