d9984ec92fad5c2d463f43a7009a0b84dc3d837f
Andreas Dangel <andreas.dangel@pmd-code.org>
2022-07-22 21:01:25 +0200

    [test] Document deprecated attribute useAuxClasspath in schema

--- pmd-test/src/main/java/net/sourceforge/pmd/testframework/RuleTst.java
+++ pmd-test/src/main/java/net/sourceforge/pmd/testframework/RuleTst.java
@@ -308,7 +308,7 @@ public abstract class RuleTst {
     /**
      * Extract a set of tests from an XML file. The file should be
      * ./xml/RuleName.xml relative to the test class. The format is defined in
-     * test-data.xsd.
+     * rule-tests_1_0_0.xsd in pmd-test-schema.
      */
     RuleTestCollection parseTestCollection(Rule rule) {
         String testsFileName = getCleanRuleName(rule);
