f6e51b1457bec5f4664da727577d5c20575ace36
Andreas Dangel <andreas.dangel@pmd-code.org>
2022-11-17 17:09:43 +0100

    [java] NonSerializableClass - new property "checkAbstractTypes"
    By default, ignore abstract types like abstract classes, interfaces, generic types and java.lang.Object.

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/errorprone/NonSerializableClassRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/errorprone/NonSerializableClassRule.java
@@ -4,15 +4,18 @@
 
 package net.sourceforge.pmd.lang.java.rule.errorprone;
 
+import static net.sourceforge.pmd.properties.PropertyFactory.booleanProperty;
 import static net.sourceforge.pmd.properties.PropertyFactory.stringProperty;
 
 import java.io.Externalizable;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.lang.reflect.Modifier;
 
 import net.sourceforge.pmd.lang.java.ast.ASTAnyTypeBodyDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTAnyTypeDeclaration;
+import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceType;
 import net.sourceforge.pmd.lang.java.ast.ASTFieldDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTFormalParameter;
 import net.sourceforge.pmd.lang.java.ast.ASTMethodDeclaration;
@@ -20,16 +23,25 @@ import net.sourceforge.pmd.lang.java.ast.ASTType;
 import net.sourceforge.pmd.lang.java.ast.ASTVariableDeclaratorId;
 import net.sourceforge.pmd.lang.java.ast.TypeNode;
 import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
+import net.sourceforge.pmd.lang.java.typeresolution.typedefinition.TypeDefinitionType;
 import net.sourceforge.pmd.lang.java.types.TypeTestUtil;
 import net.sourceforge.pmd.properties.PropertyDescriptor;
 
+// Note: This rule has been formerly known as "BeanMembersShouldSerialize".
 public class NonSerializableClassRule extends AbstractJavaRule {
 
     private static final PropertyDescriptor<String> PREFIX_DESCRIPTOR = stringProperty("prefix")
             .desc("deprecated! A variable prefix to skip, i.e., m_").defaultValue("").build();
+    private static final PropertyDescriptor<Boolean> CHECK_ABSTRACT_TYPES = booleanProperty("checkAbstractTypes")
+            .desc("Enable to verify fields with abstract types like abstract classes, interfaces, generic types "
+                + "or java.lang.Object. Enabling this might lead to more false positives, since the concrete "
+                + "runtime type can actually be serializable.")
+            .defaultValue(false)
+            .build();
 
     public NonSerializableClassRule() {
         definePropertyDescriptor(PREFIX_DESCRIPTOR);
+        definePropertyDescriptor(CHECK_ABSTRACT_TYPES);
         addRuleChainVisit(ASTVariableDeclaratorId.class);
     }
 
@@ -83,8 +95,26 @@ public class NonSerializableClassRule extends AbstractJavaRule {
     }
 
     private boolean isNotSerializable(TypeNode node) {
-        return !TypeTestUtil.isA(Serializable.class, node)
-                && node.getType() != null && !node.getType().isPrimitive();
+        Class<?> type = node.getType();
+        boolean notSerializable = !TypeTestUtil.isA(Serializable.class, node)
+                && type != null
+                && !type.isPrimitive();
+        if (!getProperty(CHECK_ABSTRACT_TYPES) && type != null) {
+            // exclude java.lang.Object, interfaces, abstract classes, and generic types
+            notSerializable &= !TypeTestUtil.isExactlyA(Object.class, node)
+                    && !type.isInterface()
+                    && !Modifier.isAbstract(type.getModifiers())
+                    && !isGenericType(node);
+        }
+        return notSerializable;
+    }
+
+    private boolean isGenericType(TypeNode node) {
+        ASTClassOrInterfaceType typeRef = node.getFirstParentOfType(ASTFieldDeclaration.class).getFirstDescendantOfType(ASTClassOrInterfaceType.class);
+        if (typeRef != null && typeRef.getTypeDefinition() != null) {
+            return typeRef.getTypeDefinition().getDefinitionType() != TypeDefinitionType.EXACT;
+        }
+        return false;
     }
 
     private String getTypeName(Class<?> clazz) {
