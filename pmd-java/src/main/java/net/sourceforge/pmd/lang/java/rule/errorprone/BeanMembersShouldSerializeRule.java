8a5d9641763214647294e6f39364492744895dff
Andreas Dangel <andreas.dangel@pmd-code.org>
2022-11-25 18:30:39 +0100

    [java] Restore BeanMembersShouldSerializeRule as deprecated class

---
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/errorprone/BeanMembersShouldSerializeRule.java
@@ -0,0 +1,12 @@
+/*
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
+package net.sourceforge.pmd.lang.java.rule.errorprone;
+
+/**
+ * @deprecated Please use {@link NonSerializableClassRule} instead.
+ */
+@Deprecated
+public class BeanMembersShouldSerializeRule extends NonSerializableClassRule {
+}
