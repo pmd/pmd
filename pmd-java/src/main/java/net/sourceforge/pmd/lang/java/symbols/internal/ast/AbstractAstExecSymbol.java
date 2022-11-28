8980a8da9f2ef4816bbb45ab633def8639eba110
Juan Martín Sotuyo Dodero <juansotuyo@gmail.com>
2022-11-28 17:05:01 -0300

    Parse annotations in ctors/methods in AST

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/symbols/internal/ast/AbstractAstExecSymbol.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/symbols/internal/ast/AbstractAstExecSymbol.java
@@ -4,15 +4,20 @@
 
 package net.sourceforge.pmd.lang.java.symbols.internal.ast;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
 
+import net.sourceforge.pmd.lang.ast.NodeStream;
+import net.sourceforge.pmd.lang.java.ast.ASTAnnotation;
 import net.sourceforge.pmd.lang.java.ast.ASTList;
 import net.sourceforge.pmd.lang.java.ast.ASTMethodOrConstructorDeclaration;
 import net.sourceforge.pmd.lang.java.symbols.JClassSymbol;
 import net.sourceforge.pmd.lang.java.symbols.JExecutableSymbol;
 import net.sourceforge.pmd.lang.java.symbols.JFormalParamSymbol;
+import net.sourceforge.pmd.lang.java.symbols.SymbolicValue.SymAnnot;
 import net.sourceforge.pmd.lang.java.types.JTypeMirror;
 import net.sourceforge.pmd.lang.java.types.Substitution;
 import net.sourceforge.pmd.util.CollectionUtil;
@@ -25,7 +30,8 @@ abstract class AbstractAstExecSymbol<T extends ASTMethodOrConstructorDeclaration
     implements JExecutableSymbol {
 
     private final JClassSymbol owner;
-    private List<JFormalParamSymbol> formals;
+    private final List<JFormalParamSymbol> formals;
+    private final List<SymAnnot> declaredAnnotations;
 
     protected AbstractAstExecSymbol(T node, AstSymFactory factory, JClassSymbol owner) {
         super(node, factory);
@@ -35,6 +41,15 @@ abstract class AbstractAstExecSymbol<T extends ASTMethodOrConstructorDeclaration
             node.getFormalParameters(),
             p -> new AstFormalParamSym(p.getVarId(), factory, this)
         );
+        
+        NodeStream<ASTAnnotation> annotStream = node.getDeclaredAnnotations();
+        if (annotStream.isEmpty()) {
+            declaredAnnotations = Collections.emptyList();
+        } else {
+            final List<SymAnnot> annotations = new ArrayList<>();
+            annotStream.forEach(n -> annotations.add(new AstSymbolicAnnot(n)));
+            declaredAnnotations = Collections.unmodifiableList(annotations);
+        }
     }
 
     @Override
@@ -54,6 +69,11 @@ abstract class AbstractAstExecSymbol<T extends ASTMethodOrConstructorDeclaration
             ASTList.orEmpty(node.getThrowsList()),
             t -> t.getTypeMirror().subst(subst)
         );
+    }
+    
+    @Override
+    public List<SymAnnot> getDeclaredAnnotations() {
+        return declaredAnnotations;
     }
     
 
