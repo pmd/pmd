bce0331fe789b63c815f9edbc9971651ea4a1504
Tarush Singh <tarush680@gmail.com>
2022-12-01 16:51:43 +0530

    check added for User and system mode in validateCRUDCheckPresent

--- pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/security/ApexCRUDViolationRule.java
+++ pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/security/ApexCRUDViolationRule.java
@@ -666,7 +666,7 @@ public class ApexCRUDViolationRule extends AbstractApexRule {
                 return true;
             }
             if (isImproperDMLCheck && !userMode && !systemMode) {
-                addViolation(data, node);
+                addViolationWithMessage(data, node, "This CRUD statement uses explicit system mode");
                 return true;
             }
         } else {
