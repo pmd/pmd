ebe4ebe71c109ffe0e3fab14a124fc23b01642be
Andreas Dangel <andreas.dangel@pmd-code.org>
2022-12-31 16:26:31 +0100

    Fix compile errors

--- pmd-java/src/test/java/net/sourceforge/pmd/lang/java/ast/ASTCompactConstructorDeclarationTest.java
+++ pmd-java/src/test/java/net/sourceforge/pmd/lang/java/ast/ASTCompactConstructorDeclarationTest.java
@@ -7,11 +7,13 @@ package net.sourceforge.pmd.lang.java.ast;
 import org.junit.Assert;
 import org.junit.Test;
 
+import net.sourceforge.pmd.lang.java.BaseParserTest;
+
 public class ASTCompactConstructorDeclarationTest extends BaseParserTest {
 
     @Test
     public void compactConstructorWithLambda() {
-        ASTCompactConstructorDeclaration compactConstructor = getNodes(ASTCompactConstructorDeclaration.class,
+        ASTCompactConstructorDeclaration compactConstructor = java.getNodes(ASTCompactConstructorDeclaration.class,
                 "import java.util.Objects;"
                     + "record RecordWithLambdaInCompactConstructor(String foo) {"
                     + "     RecordWithLambdaInCompactConstructor {"
