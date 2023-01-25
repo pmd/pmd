869752bc342e44cf321fc98fcea96ec0159cd5ae
Clément Fournier <clement.fournier76@gmail.com>
2023-01-25 12:09:08 +0100

    Shortcut equivalence check if ctdecl is unresolved

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/codestyle/UseDiamondOperatorRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/codestyle/UseDiamondOperatorRule.java
@@ -133,16 +133,8 @@ public class UseDiamondOperatorRule extends AbstractJavaRulechainRule {
         MethodCallSite fakeCallSite = infer.newCallSite(mirror, targetType);
         infer.inferInvocationRecursively(fakeCallSite);
 
-        try {
-            return mirror.isEquivalentToUnderlyingAst()
-                && topmostContext.acceptsType(mirror.getInferredType());
-        } catch (IllegalStateException e) {
-            /*
-             * overload resolution may complaint it's incomplete if there are missing types.
-             * The missing type info may be relevant to the user, but this exception is not, swallow it
-             */
-            return false;
-        }
+        return mirror.isEquivalentToUnderlyingAst()
+            && topmostContext.acceptsType(mirror.getInferredType());
     }
 
 
