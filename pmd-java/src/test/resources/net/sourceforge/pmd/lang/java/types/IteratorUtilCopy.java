adf6766288b5191a12f1c879e513e3d79bf93ed6
Andreas Dangel <andreas.dangel@microdoc.com>
2023-01-30 10:42:26 +0100

    Fix unit tests

--- pmd-java/src/test/resources/net/sourceforge/pmd/lang/java/types/IteratorUtilCopy.java
+++ pmd-java/src/test/resources/net/sourceforge/pmd/lang/java/types/IteratorUtilCopy.java
@@ -22,7 +22,6 @@ import java.util.function.Predicate;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
-import net.sourceforge.pmd.util.AssertionUtil;
 
 
 public final class IteratorUtilCopy {
