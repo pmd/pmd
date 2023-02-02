94c8c4ba0cafae1071732f8837a4e7c91f22e993
Andreas Dangel <andreas.dangel@pmd-code.org>
2023-02-02 10:45:26 +0100

    Fix unit tests

--- pmd-kotlin/src/test/java/net/sourceforge/pmd/lang/kotlin/LanguageVersionTest.java
+++ pmd-kotlin/src/test/java/net/sourceforge/pmd/lang/kotlin/LanguageVersionTest.java
@@ -13,7 +13,7 @@ class LanguageVersionTest extends AbstractLanguageVersionTest {
 
     static Collection<TestDescriptor> data() {
         return Arrays.asList(
-                new TestDescriptor(KotlinLanguageModule.NAME, KotlinLanguageModule.TERSE_NAME, "",
+                new TestDescriptor(KotlinLanguageModule.NAME, KotlinLanguageModule.TERSE_NAME, "1.6-rfc+0.1",
                     getLanguage(KotlinLanguageModule.NAME).getDefaultVersion()),
                 new TestDescriptor(KotlinLanguageModule.NAME, KotlinLanguageModule.TERSE_NAME, "1.6",
                     getLanguage(KotlinLanguageModule.NAME).getDefaultVersion()));
