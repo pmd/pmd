710f9cf2b699266b30f68024b9aee0c91ee9f957
Andreas Dangel <andreas.dangel@pmd-code.org>
2023-03-17 09:37:35 +0100

    Use AbstractRendererTest#EOL

--- pmd-core/src/test/java/net/sourceforge/pmd/renderers/VBHTMLRendererTest.java
+++ pmd-core/src/test/java/net/sourceforge/pmd/renderers/VBHTMLRendererTest.java
@@ -16,80 +16,80 @@ class VBHTMLRendererTest extends AbstractRendererTest {
 
     @Override
     String getExpected() {
-        return "<html><head><title>PMD</title></head><style type=\"text/css\"><!--" + System.lineSeparator()
+        return "<html><head><title>PMD</title></head><style type=\"text/css\"><!--" + EOL
                 + "body { background-color: white; font-family:verdana, arial, helvetica, geneva; font-size: 16px; font-style: italic; color: black; }"
-                + System.lineSeparator()
+                + EOL
                 + ".title { font-family: verdana, arial, helvetica,geneva; font-size: 12px; font-weight:bold; color: white; }"
-                + System.lineSeparator()
+                + EOL
                 + ".body { font-family: verdana, arial, helvetica, geneva; font-size: 12px; font-weight:plain; color: black; }"
-                + System.lineSeparator() + "#TableHeader { background-color: #003366; }" + System.lineSeparator()
-                + "#RowColor1 { background-color: #eeeeee; }" + System.lineSeparator() + "#RowColor2 { background-color: white; }"
-                + System.lineSeparator()
+                + EOL + "#TableHeader { background-color: #003366; }" + EOL
+                + "#RowColor1 { background-color: #eeeeee; }" + EOL + "#RowColor2 { background-color: white; }"
+                + EOL
                 + "--></style><body><center><table border=\"0\" width=\"80%\"><tr id=TableHeader><td colspan=\"2\"><font class=title>&nbsp;" + getSourceCodeFilename() + "</font></tr>"
-                + System.lineSeparator()
+                + EOL
                 + "<tr id=RowColor2><td width=\"50\" align=\"right\"><font class=body>1&nbsp;&nbsp;&nbsp;</font></td><td><font class=body>blah</font></td></tr>"
-                + System.lineSeparator() + "</table><br></center></body></html>" + System.lineSeparator();
+                + EOL + "</table><br></center></body></html>" + EOL;
     }
 
     @Override
     String getExpectedEmpty() {
-        return "<html><head><title>PMD</title></head><style type=\"text/css\"><!--" + System.lineSeparator()
+        return "<html><head><title>PMD</title></head><style type=\"text/css\"><!--" + EOL
                 + "body { background-color: white; font-family:verdana, arial, helvetica, geneva; font-size: 16px; font-style: italic; color: black; }"
-                + System.lineSeparator()
+                + EOL
                 + ".title { font-family: verdana, arial, helvetica,geneva; font-size: 12px; font-weight:bold; color: white; }"
-                + System.lineSeparator()
+                + EOL
                 + ".body { font-family: verdana, arial, helvetica, geneva; font-size: 12px; font-weight:plain; color: black; }"
-                + System.lineSeparator() + "#TableHeader { background-color: #003366; }" + System.lineSeparator()
-                + "#RowColor1 { background-color: #eeeeee; }" + System.lineSeparator() + "#RowColor2 { background-color: white; }"
-                + System.lineSeparator() + "--></style><body><center><br></center></body></html>" + System.lineSeparator();
+                + EOL + "#TableHeader { background-color: #003366; }" + EOL
+                + "#RowColor1 { background-color: #eeeeee; }" + EOL + "#RowColor2 { background-color: white; }"
+                + EOL + "--></style><body><center><br></center></body></html>" + EOL;
     }
 
     @Override
     String getExpectedMultiple() {
-        return "<html><head><title>PMD</title></head><style type=\"text/css\"><!--" + System.lineSeparator()
+        return "<html><head><title>PMD</title></head><style type=\"text/css\"><!--" + EOL
                 + "body { background-color: white; font-family:verdana, arial, helvetica, geneva; font-size: 16px; font-style: italic; color: black; }"
-                + System.lineSeparator()
+                + EOL
                 + ".title { font-family: verdana, arial, helvetica,geneva; font-size: 12px; font-weight:bold; color: white; }"
-                + System.lineSeparator()
+                + EOL
                 + ".body { font-family: verdana, arial, helvetica, geneva; font-size: 12px; font-weight:plain; color: black; }"
-                + System.lineSeparator() + "#TableHeader { background-color: #003366; }" + System.lineSeparator()
-                + "#RowColor1 { background-color: #eeeeee; }" + System.lineSeparator() + "#RowColor2 { background-color: white; }"
-                + System.lineSeparator()
+                + EOL + "#TableHeader { background-color: #003366; }" + EOL
+                + "#RowColor1 { background-color: #eeeeee; }" + EOL + "#RowColor2 { background-color: white; }"
+                + EOL
                 + "--></style><body><center><table border=\"0\" width=\"80%\"><tr id=TableHeader><td colspan=\"2\"><font class=title>&nbsp;" + getSourceCodeFilename() + "</font></tr>"
-                + System.lineSeparator()
+                + EOL
                 + "<tr id=RowColor2><td width=\"50\" align=\"right\"><font class=body>1&nbsp;&nbsp;&nbsp;</font></td><td><font class=body>blah</font></td></tr>"
-                + System.lineSeparator()
+                + EOL
                 + "<tr id=RowColor1><td width=\"50\" align=\"right\"><font class=body>1&nbsp;&nbsp;&nbsp;</font></td><td><font class=body>blah</font></td></tr>"
-                + System.lineSeparator() + "</table><br></center></body></html>" + System.lineSeparator();
+                + EOL + "</table><br></center></body></html>" + EOL;
     }
 
     @Override
     String getExpectedError(ProcessingError error) {
-        return "<html><head><title>PMD</title></head><style type=\"text/css\"><!--" + System.lineSeparator()
+        return "<html><head><title>PMD</title></head><style type=\"text/css\"><!--" + EOL
                 + "body { background-color: white; font-family:verdana, arial, helvetica, geneva; font-size: 16px; font-style: italic; color: black; }"
-                + System.lineSeparator()
+                + EOL
                 + ".title { font-family: verdana, arial, helvetica,geneva; font-size: 12px; font-weight:bold; color: white; }"
-                + System.lineSeparator()
+                + EOL
                 + ".body { font-family: verdana, arial, helvetica, geneva; font-size: 12px; font-weight:plain; color: black; }"
-                + System.lineSeparator() + "#TableHeader { background-color: #003366; }" + System.lineSeparator()
-                + "#RowColor1 { background-color: #eeeeee; }" + System.lineSeparator() + "#RowColor2 { background-color: white; }"
-                + System.lineSeparator()
+                + EOL + "#TableHeader { background-color: #003366; }" + EOL
+                + "#RowColor1 { background-color: #eeeeee; }" + EOL + "#RowColor2 { background-color: white; }"
+                + EOL
                 + "--></style><body><center><br><table border=\"0\" width=\"80%\"><tr id=TableHeader><td colspan=\"2\"><font class=title>&nbsp;Problems found</font></td></tr><tr id=RowColor2><td><font class=body>"
-                + error.getFile() + "</font></td><td><font class=body><pre>" + error.getDetail() + "</pre></font></td></tr></table></center></body></html>" + System.lineSeparator();
+                + error.getFile() + "</font></td><td><font class=body><pre>" + error.getDetail() + "</pre></font></td></tr></table></center></body></html>" + EOL;
     }
 
     @Override
     String getExpectedError(ConfigurationError error) {
-        return "<html><head><title>PMD</title></head><style type=\"text/css\"><!--" + System.lineSeparator()
+        return "<html><head><title>PMD</title></head><style type=\"text/css\"><!--" + EOL
                 + "body { background-color: white; font-family:verdana, arial, helvetica, geneva; font-size: 16px; font-style: italic; color: black; }"
-                + System.lineSeparator()
+                + EOL
                 + ".title { font-family: verdana, arial, helvetica,geneva; font-size: 12px; font-weight:bold; color: white; }"
-                + System.lineSeparator()
+                + EOL
                 + ".body { font-family: verdana, arial, helvetica, geneva; font-size: 12px; font-weight:plain; color: black; }"
-                + System.lineSeparator() + "#TableHeader { background-color: #003366; }" + System.lineSeparator()
-                + "#RowColor1 { background-color: #eeeeee; }" + System.lineSeparator() + "#RowColor2 { background-color: white; }"
-                + System.lineSeparator()
+                + EOL + "#TableHeader { background-color: #003366; }" + EOL
+                + "#RowColor1 { background-color: #eeeeee; }" + EOL + "#RowColor2 { background-color: white; }"
+                + EOL
                 + "--></style><body><center><br><table border=\"0\" width=\"80%\"><tr id=TableHeader><td colspan=\"2\"><font class=title>&nbsp;Configuration problems found</font></td></tr><tr id=RowColor2><td><font class=body>"
-                + error.rule().getName() + "</font></td><td><font class=body>" + error.issue() + "</font></td></tr></table></center></body></html>" + System.lineSeparator();
+                + error.rule().getName() + "</font></td><td><font class=body>" + error.issue() + "</font></td></tr></table></center></body></html>" + EOL;
     }
 }
