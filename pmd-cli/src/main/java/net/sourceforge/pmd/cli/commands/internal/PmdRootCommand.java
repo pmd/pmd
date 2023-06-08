7062d795777b10ff77130cb389425d848e4bf277
Juan Martín Sotuyo Dodero <juansotuyo@gmail.com>
2023-06-08 10:13:46 -0300

    Fix indentation

--- pmd-cli/src/main/java/net/sourceforge/pmd/cli/commands/internal/PmdRootCommand.java
+++ pmd-cli/src/main/java/net/sourceforge/pmd/cli/commands/internal/PmdRootCommand.java
@@ -16,7 +16,7 @@ import picocli.CommandLine.IVersionProvider;
     exitCodeList = { "0:Successful analysis, no violations found", "1:An unexpected error occurred during execution",
         "2:Usage error, please refer to the command help", "4:Successful analysis, at least 1 violation found" },
     subcommands = { PmdCommand.class, CpdCommand.class, DesignerCommand.class,
-            CpdGuiCommand.class, TreeExportCommand.class, GenerateCompletion.class })
+        CpdGuiCommand.class, TreeExportCommand.class, GenerateCompletion.class })
 public class PmdRootCommand {
 
 }
