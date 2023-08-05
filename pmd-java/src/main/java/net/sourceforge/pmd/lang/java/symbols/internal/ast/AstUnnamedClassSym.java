38a5e827e95f4e9450d14fe13fcdaf8dc8999013
Andreas Dangel <andreas.dangel@pmd-code.org>
2023-08-05 20:04:53 +0200

    Fix code duplication

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/symbols/internal/ast/AstUnnamedClassSym.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/symbols/internal/ast/AstUnnamedClassSym.java
@@ -10,7 +10,6 @@ import java.util.EnumSet;
 import java.util.List;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
-import org.checkerframework.checker.nullness.qual.Nullable;
 
 import net.sourceforge.pmd.lang.java.ast.ASTCompilationUnit;
 import net.sourceforge.pmd.lang.java.ast.ASTFieldDeclaration;
@@ -18,23 +17,16 @@ import net.sourceforge.pmd.lang.java.ast.ASTMethodDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTVariableDeclaratorId;
 import net.sourceforge.pmd.lang.java.ast.JModifier;
 import net.sourceforge.pmd.lang.java.symbols.JClassSymbol;
-import net.sourceforge.pmd.lang.java.symbols.JConstructorSymbol;
-import net.sourceforge.pmd.lang.java.symbols.JExecutableSymbol;
 import net.sourceforge.pmd.lang.java.symbols.JFieldSymbol;
 import net.sourceforge.pmd.lang.java.symbols.JMethodSymbol;
-import net.sourceforge.pmd.lang.java.symbols.JTypeDeclSymbol;
-import net.sourceforge.pmd.lang.java.types.JClassType;
-import net.sourceforge.pmd.lang.java.types.JTypeVar;
-import net.sourceforge.pmd.lang.java.types.Substitution;
-import net.sourceforge.pmd.lang.java.types.TypeSystem;
+import net.sourceforge.pmd.lang.java.symbols.internal.EmptyClassSymbol;
 
-class AstUnnamedClassSym implements JClassSymbol {
-    private ASTCompilationUnit node;
+class AstUnnamedClassSym extends EmptyClassSymbol implements JClassSymbol {
     private final List<JMethodSymbol> declaredMethods;
     private final List<JFieldSymbol> declaredFields;
 
     AstUnnamedClassSym(ASTCompilationUnit node, AstSymFactory factory) {
-        this.node = node;
+        super(node::getTypeSystem);
 
         final List<JMethodSymbol> myMethods = new ArrayList<>();
         final List<JFieldSymbol> myFields = new ArrayList<>();
@@ -58,11 +50,6 @@ class AstUnnamedClassSym implements JClassSymbol {
     }
 
     @Override
-    public @Nullable JClassSymbol getEnclosingClass() {
-        return null;
-    }
-
-    @Override
     public @NonNull String getPackageName() {
         return "";
     }
@@ -73,28 +60,8 @@ class AstUnnamedClassSym implements JClassSymbol {
     }
 
     @Override
-    public @Nullable String getCanonicalName() {
-        return null;
-    }
-
-    @Override
-    public @Nullable JExecutableSymbol getEnclosingMethod() {
-        return null;
-    }
-
-    @Override
-    public List<JClassSymbol> getDeclaredClasses() {
-        return Collections.emptyList();
-    }
-
-    @Override
     public List<JMethodSymbol> getDeclaredMethods() {
         return declaredMethods;
-    }
-
-    @Override
-    public List<JConstructorSymbol> getConstructors() {
-        return Collections.emptyList();
     }
 
     @Override
@@ -103,77 +70,7 @@ class AstUnnamedClassSym implements JClassSymbol {
     }
 
     @Override
-    public List<JClassType> getSuperInterfaceTypes(Substitution substitution) {
-        return Collections.emptyList();
-    }
-
-    @Override
-    public @Nullable JClassType getSuperclassType(Substitution substitution) {
-        return null;
-    }
-
-    @Override
-    public @Nullable JClassSymbol getSuperclass() {
-        return null;
-    }
-
-    @Override
-    public List<JClassSymbol> getSuperInterfaces() {
-        return Collections.emptyList();
-    }
-
-    @Override
-    public @Nullable JTypeDeclSymbol getArrayComponent() {
-        return null;
-    }
-
-    @Override
-    public boolean isArray() {
-        return false;
-    }
-
-    @Override
-    public boolean isPrimitive() {
-        return false;
-    }
-
-    @Override
-    public boolean isEnum() {
-        return false;
-    }
-
-    @Override
-    public boolean isRecord() {
-        return false;
-    }
-
-    @Override
-    public boolean isAnnotation() {
-        return false;
-    }
-
-    @Override
-    public boolean isLocalClass() {
-        return false;
-    }
-
-    @Override
-    public boolean isAnonymousClass() {
-        return false;
-    }
-
-    @Override
-    public TypeSystem getTypeSystem() {
-        return node.getTypeSystem();
-    }
-
-    @Override
     public @NonNull String getSimpleName() {
         return "<UnnamedClass>";
-    }
-
-    @Override
-    public List<JTypeVar> getTypeParameters() {
-        return Collections.emptyList();
     }
 }
