79d6e8bc48318121b6f6e176d2124aaf1912c56d
Andreas Dangel <andreas.dangel@pmd-code.org>
2023-08-12 11:30:20 +0200

    [core] NumericConstraints - add tests for above/below

--- pmd-core/src/test/java/net/sourceforge/pmd/properties/NumericConstraintsTest.java
+++ pmd-core/src/test/java/net/sourceforge/pmd/properties/NumericConstraintsTest.java
@@ -61,4 +61,29 @@ class NumericConstraintsTest {
         assertNotNull(errorAsString(constraint, -0.1f));
         assertNotNull(errorAsString(constraint, -0.1d));
     }
+
+    @Test
+    void testBelow() {
+        PropertyConstraint<Integer> constraint = NumericConstraints.below(5);
+        assertNull(errorAsString(constraint, 5));
+        assertNull(errorAsString(constraint, 3));
+        assertNull(errorAsString(constraint, 1));
+        assertNull(errorAsString(constraint, 0));
+        assertNull(errorAsString(constraint, -1));
+        assertEquals("'6' should be smaller or equal to 5", errorAsString(constraint, 6));
+        assertNotNull(errorAsString(constraint, 10));
+    }
+
+    @Test
+    void testAbove() {
+        PropertyConstraint<Double> constraint = NumericConstraints.above(2.5);
+        assertNull(errorAsString(constraint, 2.5));
+        assertNull(errorAsString(constraint, 3d));
+        assertNull(errorAsString(constraint, 3.5));
+        assertEquals("'2.0' should be greater or equal to 2.5", errorAsString(constraint, 2d));
+        assertNotNull(errorAsString(constraint, 1.5));
+        assertNotNull(errorAsString(constraint, 1d));
+        assertNotNull(errorAsString(constraint, 0.0));
+        assertNotNull(errorAsString(constraint, -5d));
+    }
 }
