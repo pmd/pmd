5200cc9e8a5ed610a9a82c9396547ad07ca0591a
Andreas Dangel <andreas.dangel@pmd-code.org>
2023-08-24 09:32:01 +0200

    [cli] Fix javadoc

--- pmd-cli/src/main/java/net/sourceforge/pmd/cli/commands/typesupport/internal/CpdLanguageTypeSupport.java
+++ pmd-cli/src/main/java/net/sourceforge/pmd/cli/commands/typesupport/internal/CpdLanguageTypeSupport.java
@@ -7,9 +7,9 @@ package net.sourceforge.pmd.cli.commands.typesupport.internal;
 import net.sourceforge.pmd.lang.LanguageRegistry;
 
 /**
- * Provider of candidates / conversion support for supported PMD languages.
- * 
- * Beware, the help will report this on runtime, and be accurate to available
+ * Provider of candidates / conversion support for supported CPD languages.
+ *
+ * <p>Beware, the help will report this on runtime, and be accurate to available
  * modules in the classpath, but autocomplete will include all available at build time.
  */
 public class CpdLanguageTypeSupport extends LanguageTypeSupport {
