940585de9a59b2c0cf8a8058fc6271d3cca34f8f
Wener <marvin.wener@tiobe.com>
2023-09-12 11:05:21 +0200

    Update TypeScriptTokenizerTest.java

--- pmd-javascript/src/test/java/net/sourceforge/pmd/lang/typescript/cpd/TypeScriptTokenizerTest.java
+++ pmd-javascript/src/test/java/net/sourceforge/pmd/lang/typescript/cpd/TypeScriptTokenizerTest.java
@@ -7,6 +7,7 @@ package net.sourceforge.pmd.lang.typescript.cpd;
 import org.junit.jupiter.api.Test;
 
 import net.sourceforge.pmd.cpd.test.CpdTextComparisonTest;
+import net.sourceforge.pmd.lang.ecmascript.cpd.EcmascriptTokenizer;
 import net.sourceforge.pmd.lang.typescript.TsLanguageModule;
 
 class TypeScriptTokenizerTest extends CpdTextComparisonTest {
@@ -23,5 +24,22 @@ class TypeScriptTokenizerTest extends CpdTextComparisonTest {
     @Test
     void apiSampleWatchTest() {
         doTest("APISample_Watch");
+    }
+    
+    @Test
+    void testDecorators() throws IOException {
+        Tokenizer t = new EcmascriptTokenizer();
+        SourceCode sourceCode = new SourceCode(new SourceCode.StringCodeLoader("// A simple decorator" + PMD.EOL
+                + "@annotation" + PMD.EOL
+                + "class MyClass { }" + PMD.EOL
+                + "" + PMD.EOL
+                + "function annotation(target) {" + PMD.EOL
+                + "   // Add a property on target" + PMD.EOL
+                + "   target.annotated = true;" + PMD.EOL
+                + "}" + PMD.EOL
+        ));
+        final Tokens tokens = new Tokens();
+        t.tokenize(sourceCode, tokens);
+        assertEquals("@annotation", tokens.getTokens().get(0).toString());
     }
 }
