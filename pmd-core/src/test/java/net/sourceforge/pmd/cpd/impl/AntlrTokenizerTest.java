7b70035ca6d94b5b94aad434877ab178bb285fe0
Wener <marvin.wener@tiobe.com>
2023-09-12 07:59:56 +0200

    Added filter for BOM characters that are in an unexpected place.

---
+++ pmd-core/src/test/java/net/sourceforge/pmd/cpd/impl/AntlrTokenizerTest.java
@@ -0,0 +1,41 @@
+/*
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
+package net.sourceforge.pmd.cpd.impl;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.Lexer;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AntlrTokenizerTest {
+
+    static class AntlrTokenizerTester extends AntlrTokenizer {
+        @Override
+        protected Lexer getLexerForSource(CharStream charStream) {
+            return null;
+        }
+    }
+    
+    @Test
+    void testStreamWithBOM() {
+        CharStream inputStream = CharStreams.fromString("//Header Comment\n\uFEFFusing System");
+        CharStream expectedStream = CharStreams.fromString("//Header Comment\nusing System");
+        final AntlrTokenizerTester genericToken = new AntlrTokenizerTester();
+        final CharStream outputStream = genericToken.bomFilter(inputStream);
+
+        assertEquals(outputStream.toString(), expectedStream.toString());
+    }
+
+    @Test
+    void testStreamWithoutBOM() {
+        CharStream inputStream = CharStreams.fromString("//Header Comment\nusing System");
+        final AntlrTokenizerTester genericToken = new AntlrTokenizerTester();
+        final CharStream outputStream = genericToken.bomFilter(inputStream);
+
+        assertEquals(outputStream.toString(), inputStream.toString());
+    }
+}
