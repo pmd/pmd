e0b3d206c29b53b353a747cbd90c88b78a2d0fa4
Wener <marvin.wener@tiobe.com>
2023-09-12 10:50:05 +0200

    Create decorator.ts

---
+++ pmd-javascript/src/test/resources/net/sourceforge/pmd/lang/typescript/cpd/testdata/decorator.ts
@@ -0,0 +1,24 @@
+class Point {
+  private _x: number;
+  private _y: number;
+  constructor(x: number, y: number) {
+    this._x = x;
+    this._y = y;
+  }
+ 
+  @configurable(false)
+  get x() {
+    return this._x;
+  }
+ 
+  @configurable(false)
+  get y() {
+    return this._y;
+  }
+}
+
+function configurable(value: boolean) {
+  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    descriptor.configurable = value;
+  };
+}
