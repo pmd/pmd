9f4d870fa514a90b51c5871f8cc9122c2ca58a24
Andreas Dangel <andreas.dangel@pmd-code.org>
2024-01-16 20:23:09 +0100

    [java] SingletonClassReturningNewInstance - fix double assignment case
    Fixes #932

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/errorprone/SingletonClassReturningNewInstanceRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/errorprone/SingletonClassReturningNewInstanceRule.java
@@ -6,12 +6,15 @@ package net.sourceforge.pmd.lang.java.rule.errorprone;
 
 import net.sourceforge.pmd.lang.ast.NodeStream;
 import net.sourceforge.pmd.lang.ast.NodeStream.DescendantNodeStream;
+import net.sourceforge.pmd.lang.java.ast.ASTAssignableExpr;
+import net.sourceforge.pmd.lang.java.ast.ASTAssignmentExpression;
 import net.sourceforge.pmd.lang.java.ast.ASTConstructorCall;
 import net.sourceforge.pmd.lang.java.ast.ASTMethodDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTReturnStatement;
 import net.sourceforge.pmd.lang.java.ast.ASTVariableAccess;
 import net.sourceforge.pmd.lang.java.ast.internal.JavaAstUtils;
 import net.sourceforge.pmd.lang.java.rule.AbstractJavaRulechainRule;
+import net.sourceforge.pmd.lang.java.symbols.JVariableSymbol;
 
 public class SingletonClassReturningNewInstanceRule extends AbstractJavaRulechainRule {
 
@@ -37,6 +40,38 @@ public class SingletonClassReturningNewInstanceRule extends AbstractJavaRulechai
     }
 
     private boolean returnsLocalVariables(NodeStream<ASTReturnStatement> returns) {
-        return returns.children(ASTVariableAccess.class).filter(JavaAstUtils::isReferenceToLocal).nonEmpty();
+        return returns.children(ASTVariableAccess.class)
+                .filter(JavaAstUtils::isReferenceToLocal)
+                .filterNot(this::isDoubleAssignment)
+                .nonEmpty();
     }
+
+    private boolean isDoubleAssignment(ASTVariableAccess variableAccess) {
+        // search in the whole method
+        return variableAccess.ancestors(ASTMethodDeclaration.class)
+                .descendants(ASTVariableAccess.class)
+                // for any writes
+                .filter(v -> v.getAccessType() == ASTAssignableExpr.AccessType.WRITE)
+                // to the same variable
+                .filter(v -> JavaAstUtils.isReferenceToSameVar(variableAccess, v))
+                // assignment from a constructor call (next sibling = right hand side)
+                .filter(v -> v.getNextSibling() instanceof ASTConstructorCall)
+                // and double assignment
+                .filter(v -> v.getParent() instanceof ASTAssignmentExpression && v.getParent().getParent() instanceof ASTAssignmentExpression)
+                .filter(v -> {
+                    ASTAssignmentExpression rightAssignment = (ASTAssignmentExpression) v.getParent();
+                    ASTAssignmentExpression leftAssignment = (ASTAssignmentExpression) v.getParent().getParent();
+
+                    boolean fromConstructor = rightAssignment.getRightOperand() instanceof ASTConstructorCall;
+                    boolean fromRightToLeft = leftAssignment.getRightOperand() == rightAssignment;
+                    boolean leftIsField = false;
+                    if (leftAssignment.getLeftOperand() instanceof ASTAssignableExpr.ASTNamedReferenceExpr) {
+                        JVariableSymbol symbol = ((ASTAssignableExpr.ASTNamedReferenceExpr) leftAssignment.getLeftOperand()).getReferencedSym();
+                        leftIsField = symbol.isField();
+                    }
+                    return fromConstructor && fromRightToLeft && leftIsField;
+                })
+                .nonEmpty();
+    }
+
 }
