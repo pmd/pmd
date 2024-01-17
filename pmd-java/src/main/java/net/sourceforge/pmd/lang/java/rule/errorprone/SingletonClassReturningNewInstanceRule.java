023e51e67fe3f02136fab1fbdc57f98038e39a9c
Andreas Dangel <andreas.dangel@pmd-code.org>
2024-01-17 18:02:45 +0100

    [java] SingletonClassReturningNewInstance - variant 2 (#932)

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/errorprone/SingletonClassReturningNewInstanceRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/errorprone/SingletonClassReturningNewInstanceRule.java
@@ -54,22 +54,46 @@ public class SingletonClassReturningNewInstanceRule extends AbstractJavaRulechai
                 .filter(v -> v.getAccessType() == ASTAssignableExpr.AccessType.WRITE)
                 // to the same variable
                 .filter(v -> JavaAstUtils.isReferenceToSameVar(variableAccess, v))
-                // assignment from a constructor call (next sibling = right hand side)
-                .filter(v -> v.getNextSibling() instanceof ASTConstructorCall)
-                // and double assignment
-                .filter(v -> v.getParent() instanceof ASTAssignmentExpression && v.getParent().getParent() instanceof ASTAssignmentExpression)
+                // assignment from a constructor call (next sibling = right hand side) or another Assignment
+                .filter(v -> v.getNextSibling() instanceof ASTConstructorCall || v.getNextSibling() instanceof ASTAssignmentExpression)
+                // check for both variants
                 .filter(v -> {
-                    ASTAssignmentExpression rightAssignment = (ASTAssignmentExpression) v.getParent();
-                    ASTAssignmentExpression leftAssignment = (ASTAssignmentExpression) v.getParent().getParent();
+                    boolean variant1 = false;
+                    boolean variant2 = false;
 
-                    boolean fromConstructor = rightAssignment.getRightOperand() instanceof ASTConstructorCall;
-                    boolean fromRightToLeft = leftAssignment.getRightOperand() == rightAssignment;
-                    boolean leftIsField = false;
-                    if (leftAssignment.getLeftOperand() instanceof ASTAssignableExpr.ASTNamedReferenceExpr) {
-                        JVariableSymbol symbol = ((ASTAssignableExpr.ASTNamedReferenceExpr) leftAssignment.getLeftOperand()).getReferencedSym();
-                        leftIsField = symbol.isField();
+                    // check variant 1: field = localVar = new Singleton()
+                    if (v.getNextSibling() instanceof ASTConstructorCall) {
+                        if (v.getParent() instanceof ASTAssignmentExpression && v.getParent().getParent() instanceof ASTAssignmentExpression) {
+                            ASTAssignmentExpression leftAssignment = (ASTAssignmentExpression) v.getParent().getParent();
+                            ASTAssignmentExpression rightAssignment = (ASTAssignmentExpression) v.getParent();
+
+                            boolean fromConstructor = rightAssignment.getRightOperand() instanceof ASTConstructorCall;
+                            boolean fromRightToLeft = leftAssignment.getRightOperand() == rightAssignment;
+                            boolean leftIsField = false;
+                            if (leftAssignment.getLeftOperand() instanceof ASTAssignableExpr.ASTNamedReferenceExpr) {
+                                JVariableSymbol symbol = ((ASTAssignableExpr.ASTNamedReferenceExpr) leftAssignment.getLeftOperand()).getReferencedSym();
+                                leftIsField = symbol != null && symbol.isField();
+                            }
+                            variant1 = fromConstructor && fromRightToLeft && leftIsField;
+                        }
+
+                    // check variant 2: localVar = field = new Singleton()
+                    } else if (v.getNextSibling() instanceof ASTAssignmentExpression) {
+                        if (v.getParent() instanceof ASTAssignmentExpression) {
+                            ASTAssignmentExpression leftAssignment = (ASTAssignmentExpression) v.getParent();
+                            ASTAssignmentExpression rightAssignment = (ASTAssignmentExpression) v.getNextSibling();
+
+                            boolean fromConstructor = rightAssignment.getRightOperand() instanceof ASTConstructorCall;
+                            boolean fromRightToLeft = leftAssignment.getRightOperand() == rightAssignment;
+                            boolean rightIsField = false;
+                            if (rightAssignment.getLeftOperand() instanceof ASTAssignableExpr.ASTNamedReferenceExpr) {
+                                JVariableSymbol symbol = ((ASTAssignableExpr.ASTNamedReferenceExpr) rightAssignment.getLeftOperand()).getReferencedSym();
+                                rightIsField = symbol != null && symbol.isField();
+                            }
+                            variant2 = fromConstructor && fromRightToLeft && rightIsField;
+                        }
                     }
-                    return fromConstructor && fromRightToLeft && leftIsField;
+                    return variant1 || variant2;
                 })
                 .nonEmpty();
     }
