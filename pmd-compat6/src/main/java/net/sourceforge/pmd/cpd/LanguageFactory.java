b223b1a672439fc0707e279326b1ce3255513da0
Andreas Dangel <andreas.dangel@pmd-code.org>
2024-02-17 18:39:50 +0100

    [compat6] Support other cpd languages like cs

--- pmd-compat6/src/main/java/net/sourceforge/pmd/cpd/LanguageFactory.java
+++ pmd-compat6/src/main/java/net/sourceforge/pmd/cpd/LanguageFactory.java
@@ -10,6 +10,11 @@
 package net.sourceforge.pmd.cpd;
 
 import java.util.Properties;
+import java.util.stream.Collectors;
+
+import net.sourceforge.pmd.lang.LanguagePropertyBundle;
+import net.sourceforge.pmd.lang.LanguageRegistry;
+import net.sourceforge.pmd.properties.PropertyDescriptor;
 
 public final class LanguageFactory {
     private LanguageFactory() {
@@ -17,6 +22,44 @@ public final class LanguageFactory {
     }
 
     public static Language createLanguage(String name, Properties properties) {
-        throw new UnsupportedOperationException();
+        CpdCapableLanguage cpdLanguage = (CpdCapableLanguage) LanguageRegistry.CPD.getLanguageById(name);
+        if (cpdLanguage != null) {
+            return new CpdLanguageAdapter(cpdLanguage, properties);
+        }
+        throw new UnsupportedOperationException("Language " + name + " is not supported");
+    }
+
+    public static class CpdLanguageAdapter extends AbstractLanguage {
+        private CpdCapableLanguage language;
+
+        public CpdLanguageAdapter(CpdCapableLanguage cpdCapableLanguage, Properties properties) {
+            super(cpdCapableLanguage.getName(), cpdCapableLanguage.getId(), createLexer(cpdCapableLanguage, properties), convertExtensions(cpdCapableLanguage));
+            this.language = cpdCapableLanguage;
+        }
+
+        private static Tokenizer createLexer(CpdCapableLanguage cpdCapableLanguage, Properties properties) {
+            LanguagePropertyBundle propertyBundle = cpdCapableLanguage.newPropertyBundle();
+            for (String propName : properties.stringPropertyNames()) {
+                PropertyDescriptor<?> propertyDescriptor = propertyBundle.getPropertyDescriptor(propName);
+                if (propertyDescriptor != null) {
+                    setProperty(propertyBundle, propertyDescriptor, properties.getProperty(propName));
+                }
+            }
+            CpdLexer cpdLexer = cpdCapableLanguage.createCpdLexer(propertyBundle);
+            return cpdLexer::tokenize;
+        }
+
+        private static <T> void setProperty(LanguagePropertyBundle propertyBundle, PropertyDescriptor<T> propertyDescriptor, String stringValue) {
+            T value = propertyDescriptor.serializer().fromString(stringValue);
+            propertyBundle.setProperty(propertyDescriptor, value);
+        }
+
+        private static String[] convertExtensions(CpdCapableLanguage cpdCapableLanguage) {
+            return cpdCapableLanguage.getExtensions().stream().map(s -> "." + s).collect(Collectors.toList()).toArray(new String[0]);
+        }
+
+        public CpdCapableLanguage getLanguage() {
+            return language;
+        }
     }
 }
