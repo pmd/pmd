b01d630e2e2dbfe263a99a1e9687c2d84df7efb9
Sashko <908526+oleksandr-shvets@users.noreply.github.com>
2024-02-22 01:48:03 +0200

    revert UnnecessaryBlockTest.java

--- pmd-javascript/src/test/java/net/sourceforge/pmd/lang/ecmascript/rule/codestyle/UnnecessaryBlockTest.java
+++ pmd-javascript/src/test/java/net/sourceforge/pmd/lang/ecmascript/rule/codestyle/UnnecessaryBlockTest.java
@@ -3,30 +3,5 @@ package net.sourceforge.pmd.lang.ecmascript.rule.codestyle;
 import net.sourceforge.pmd.testframework.PmdRuleTst;
 
 public class UnnecessaryBlockTest extends PmdRuleTst {
-    @Override
-    public void setUp() {
-        // Set up any required configurations or resources before running the tests
-    }
-
-
-    public void testUnnecessaryBlockInImportStatement() {
-        String code = "import { foo } from 'bar';\n" +
-                      "{\n" +
-                      "    // Unnecessary block\n" +
-                      "}\n";
-
-        // Assert that the PMD rule does not flag the unnecessary block within import statement
-        addSourceCodeTest(code, 0);
-    }
-
-    public void testUnnecessaryBlockInDestructuringAssignment() {
-        String code = "const { a, b } = obj;\n" +
-                      "{\n" +
-                      "    // Unnecessary block\n" +
-                      "}\n";
-
-        // Assert that the PMD rule does not flag the unnecessary block within destructuring assignment
-        addSourceCodeTest(code, 0);
-    }
-
+    
 }
