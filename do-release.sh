65d4d26dbfc4175c58095185ff47c5b0d5966c8b
Andreas Dangel <andreas.dangel@pmd-code.org>
2024-03-08 18:55:32 +0100

    [doc] Update release notes - API Changes
    - Move API changes already to pmd7 release notes

--- do-release.sh
+++ do-release.sh
@@ -117,9 +117,6 @@ echo
 echo "*   Update **pmd-apex/src/main/resources/rulesets/apex/quickstart.xml** and"
 echo "    **pmd-java/src/main/resources/rulesets/java/quickstart.xml** with the new rules."
 echo
-echo "*   Update **docs/pages/next_major_development.md** with the API changes for"
-echo "    the new release based on the release notes. Also add any deprecated rules to the list."
-echo
 echo "*   Update **../pmd.github.io/_config.yml** to mention the new release"
 echo
 echo "*   Update property \`pmd-designer.version\` in **pom.xml** to reference the version, that will be released"
