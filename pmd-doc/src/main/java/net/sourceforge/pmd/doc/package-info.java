7979570d39909195e173de05450fb1acf8b3eec5
Andreas Dangel <andreas.dangel@pmd-code.org>
2024-03-22 09:12:17 +0100

    [doc] Create empty class so that javadoc jar is created
    This needs to be done to fulfill the requirement of maven
    central, that every artefact has to have a javadoc jar.
    However, pmd-doc only contains internal classes.

---
+++ pmd-doc/src/main/java/net/sourceforge/pmd/doc/package-info.java
@@ -0,0 +1,8 @@
+/*
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
+/**
+ * This module contains only internal code to generate the rule documentation pages.
+ */
+package net.sourceforge.pmd.doc;
