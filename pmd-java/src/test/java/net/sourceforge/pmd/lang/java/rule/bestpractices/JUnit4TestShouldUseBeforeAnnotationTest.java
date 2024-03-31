86328b7ad16b8a73af0cf86cbedadd7595eeedea
Juan Martín Sotuyo Dodero <juan.sotuyo@pedidosya.com>
2024-03-31 02:24:02 -0300

    Add test cases for #1592

--- pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/bestpractices/JUnit4TestShouldUseBeforeAnnotationTest.java
+++ pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/bestpractices/JUnit4TestShouldUseBeforeAnnotationTest.java
@@ -4,8 +4,22 @@
 
 package net.sourceforge.pmd.lang.java.rule.bestpractices;
 
+import org.junit.After;
+import org.junit.Before;
+
 import net.sourceforge.pmd.test.PmdRuleTst;
 
 class JUnit4TestShouldUseBeforeAnnotationTest extends PmdRuleTst {
     // no additional unit tests
+
+    public static class BaseTest {
+        @Before
+        public void setUp() {
+            // A setup code
+        }
+        @After
+        public void tearDown() {
+            // A tear down code
+        }
+    }
 }
