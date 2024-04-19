1231a54300c086d65932e7b70540ebe8e38607fe
Juan Martín Sotuyo Dodero <juan.sotuyo@pedidosya.com>
2024-04-19 08:56:48 -0300

    Remove unsused imports

--- pmd-java/src/test/java/net/sourceforge/pmd/lang/java/JavaAttributesPrinter.java
+++ pmd-java/src/test/java/net/sourceforge/pmd/lang/java/JavaAttributesPrinter.java
@@ -4,17 +4,11 @@
 
 package net.sourceforge.pmd.lang.java;
 
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 import org.checkerframework.checker.nullness.qual.NonNull;
 
 import net.sourceforge.pmd.lang.ast.Node;
 import net.sourceforge.pmd.lang.java.ast.ASTExpression;
 import net.sourceforge.pmd.lang.java.ast.ASTMethodDeclaration;
-import net.sourceforge.pmd.lang.java.ast.ASTModifierList;
-import net.sourceforge.pmd.lang.java.ast.JModifier;
 import net.sourceforge.pmd.lang.java.ast.ModifierOwner;
 import net.sourceforge.pmd.lang.rule.xpath.Attribute;
 import net.sourceforge.pmd.lang.test.ast.RelevantAttributePrinter;
