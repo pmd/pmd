41ca25950354cbfad8bf46d0d1840e7f1127fdc6
Andreas Dangel <andreas.dangel@pmd-code.org>
2024-06-27 17:01:58 +0200

    [apex] AvoidNonRestrictiveQueries - handle sub-queries separately

--- pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/performance/AvoidNonRestrictiveQueriesRule.java
+++ pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/performance/AvoidNonRestrictiveQueriesRule.java
@@ -4,33 +4,66 @@
 
 package net.sourceforge.pmd.lang.apex.rule.performance;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.checkerframework.checker.nullness.qual.NonNull;
+
 import net.sourceforge.pmd.lang.apex.ast.ASTSoqlExpression;
 import net.sourceforge.pmd.lang.apex.rule.AbstractApexRule;
+import net.sourceforge.pmd.lang.rule.RuleTargetSelector;
+import net.sourceforge.pmd.reporting.RuleContext;
 
 public class AvoidNonRestrictiveQueriesRule extends AbstractApexRule {
     private static final Pattern RESTRICTIVE_PATTERN = Pattern.compile("(where )|(limit )", Pattern.CASE_INSENSITIVE);
     private static final Pattern SELECT_PATTERN = Pattern.compile("(select )", Pattern.CASE_INSENSITIVE);
 
     @Override
-    public Object visit(ASTSoqlExpression node, Object data) {
-        Integer occurencesSelect = 0;
-        Integer occurencesWhereOrLimit = 0;
+    protected @NonNull RuleTargetSelector buildTargetSelector() {
+        return RuleTargetSelector.forTypes(ASTSoqlExpression.class);
+    }
 
+    @Override
+    public Object visit(ASTSoqlExpression node, Object data) {
         String query = node.getQuery();
-        Matcher matcherRestrictive = RESTRICTIVE_PATTERN.matcher(query);
-        Matcher matcherSelect = SELECT_PATTERN.matcher(query);
-        while (matcherRestrictive.find()) {
-            occurencesWhereOrLimit++;
+
+        Pattern subQueryPattern = Pattern.compile("(?i)\\(\\s*select\\s+[^)]+\\)");
+        Matcher subQueryMatcher = subQueryPattern.matcher(query);
+        List<String> subQueries = new ArrayList<>();
+        StringBuffer queryWithoutSubQueries = new StringBuffer(query.length());
+        while (subQueryMatcher.find()) {
+            subQueries.add(subQueryMatcher.group());
+            subQueryMatcher.appendReplacement(queryWithoutSubQueries, "replaced_subquery");
         }
-        while (matcherSelect.find()) {
-            occurencesSelect++;
+        subQueryMatcher.appendTail(queryWithoutSubQueries);
+
+        verifyQuery(asCtx(data), node, queryWithoutSubQueries.toString(), "");
+
+        for (String subQuery : subQueries) {
+            verifyQuery(asCtx(data), node, subQuery, "sub-");
         }
-        if (occurencesSelect > occurencesWhereOrLimit) {
-            asCtx(data).addViolation(node);
-        }
+
         return data;
+    }
+
+    private void verifyQuery(RuleContext ctx, ASTSoqlExpression node, String query, String messageArgument) {
+        int occurrencesSelect = countOccurrences(SELECT_PATTERN, query);
+        int occurrencesWhereOrLimit = countOccurrences(RESTRICTIVE_PATTERN, query);
+
+        if (occurrencesSelect > 0 && occurrencesWhereOrLimit == 0) {
+            ctx.addViolation(node, messageArgument);
+        }
+
+    }
+
+    private int countOccurrences(Pattern pattern, String s) {
+        int occurrences = 0;
+        Matcher matcher = pattern.matcher(s);
+        while (matcher.find()) {
+            occurrences++;
+        }
+        return occurrences;
     }
 }
