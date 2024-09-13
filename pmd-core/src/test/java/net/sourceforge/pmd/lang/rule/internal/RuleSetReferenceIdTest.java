47f204cb199a571465af836377205b6382c81cff
Andreas Dangel <andreas.dangel@pmd-code.org>
2024-09-13 10:37:37 +0200

    Fix unit test

--- pmd-core/src/test/java/net/sourceforge/pmd/lang/rule/internal/RuleSetReferenceIdTest.java
+++ pmd-core/src/test/java/net/sourceforge/pmd/lang/rule/internal/RuleSetReferenceIdTest.java
@@ -91,7 +91,7 @@ class RuleSetReferenceIdTest {
                 new RuleSetReferenceId("rulesets/java/basic.xml/EmptyCatchBlock"));
         assertRuleSetReferenceId(false, null, false, "EmptyCatchBlock", "EmptyCatchBlock",
                 new RuleSetReferenceId("EmptyCatchBlock"));
-        assertRuleSetReferenceId(true, "https://raw.githubusercontent.com/pmd/pmd/main/pmd-java/src/main/resources/rulesets/java/quickstart.xml", false, "ConstantsInInterface", "https://raw.githubusercontent.com/pmd/pmd/master/pmd-java/src/main/resources/rulesets/java/quickstart.xml/ConstantsInInterface",
+        assertRuleSetReferenceId(true, "https://raw.githubusercontent.com/pmd/pmd/main/pmd-java/src/main/resources/rulesets/java/quickstart.xml", false, "ConstantsInInterface", "https://raw.githubusercontent.com/pmd/pmd/main/pmd-java/src/main/resources/rulesets/java/quickstart.xml/ConstantsInInterface",
                 new RuleSetReferenceId("https://raw.githubusercontent.com/pmd/pmd/main/pmd-java/src/main/resources/rulesets/java/quickstart.xml/ConstantsInInterface"));
         assertRuleSetReferenceId(true, "https://example.org/ruleset/MyRule", true, null, "https://example.org/ruleset/MyRule",
                 new RuleSetReferenceId("https://example.org/ruleset/MyRule"));
