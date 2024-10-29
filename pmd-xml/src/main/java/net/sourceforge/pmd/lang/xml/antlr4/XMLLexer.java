69a92de52dddd720270f1a76ff27a9ccc5e8050c
Juan Martín Sotuyo Dodero <juan.sotuyo@pedidosya.com>
2024-10-28 22:10:21 -0300

    Have pmd-xml Lexer in line with other antlr grammars
     - The package is no longer antlr4, but ast, as is in all other modules
     - We keep a deprecated proxy for backwards compatibility
     - We annotate the generated classes as such to ignore them from
       coverage reports

---
+++ pmd-xml/src/main/java/net/sourceforge/pmd/lang/xml/antlr4/XMLLexer.java
@@ -0,0 +1,17 @@
+package net.sourceforge.pmd.lang.xml.antlr4;
+
+import org.antlr.v4.runtime.CharStream;
+
+/**
+ * Backwards compatible bridge. The XMLLexer was moved to align it with other PMD modules.
+ * This class will be removed in PMD 8.0.0.
+ * Use {@link net.sourceforge.pmd.lang.xml.ast.XMLLexer} directly instead.
+ *
+ * @deprecated
+ */
+@Deprecated
+public class XMLLexer extends net.sourceforge.pmd.lang.xml.ast.XMLLexer {
+    public XMLLexer(CharStream input) {
+        super(input);
+    }
+}
