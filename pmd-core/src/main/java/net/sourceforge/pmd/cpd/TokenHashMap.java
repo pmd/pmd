351e503b50366875b1cb67dad99e8700c9c5210e
Clément Fournier <clement.fournier@tu-dresden.de>
2024-12-11 15:58:37 +0100

    Save some memory on lambda allocation

--- pmd-core/src/main/java/net/sourceforge/pmd/cpd/TokenHashMap.java
+++ pmd-core/src/main/java/net/sourceforge/pmd/cpd/TokenHashMap.java
@@ -6,6 +6,7 @@ package net.sourceforge.pmd.cpd;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiFunction;
 
 import net.sourceforge.pmd.cpd.TokenFileSet.SmallTokenEntry;
 import net.sourceforge.pmd.util.AssertionUtil;
@@ -21,11 +22,14 @@ class TokenHashMap {
     private final Int2ObjectMap<Object> markGroups;
     //    private final Map<Integer, Object> markGroups;
     private final List<List<SmallTokenEntry>> listSink;
+    // this is shared to save memory on the lambda allocation, which is significant
+    private final BiFunction<Object, Object, Object> mergeFunction;
 
     TokenHashMap(int size) {
         markGroups = new Int2ObjectOpenHashMap<>(size);
         //        markGroups = new HashMap<>(size);
         listSink = new ArrayList<>();
+        mergeFunction = this::mergeEntries;
     }
 
     public List<List<SmallTokenEntry>> getFinalMatches() {
@@ -44,29 +48,32 @@ class TokenHashMap {
      * @param thisEntry The current token
      */
     void addTokenToHashTable(int hash, SmallTokenEntry thisEntry) {
-        markGroups.merge(hash, thisEntry, (curEntry, thisEntry2) -> {
-            if (curEntry instanceof SmallTokenEntry) {
-                SmallTokenEntry curTok = (SmallTokenEntry) curEntry;
-                if (curTok.hasSamePrevToken(thisEntry)) {
-                    // part of a larger match, yeet them out
-                    return null;
-                }
-                List<SmallTokenEntry> arr = new ArrayList<>(2);
-                arr.add(curTok);
-                arr.add(thisEntry);
-                listSink.add(arr);
-                return arr;
-            } else if (curEntry instanceof List) {
-                @SuppressWarnings("unchecked")
-                List<SmallTokenEntry> list = (List<SmallTokenEntry>) curEntry;
-                boolean hadMatch = list.removeIf(thisEntry::hasSamePrevToken);
-                if (!hadMatch) {
-                    list.add(thisEntry);
-                }
-                return list;
+        markGroups.merge(hash, thisEntry, this.mergeFunction);
+    }
+
+    private Object mergeEntries(Object curEntry, Object thisEntry) {
+        SmallTokenEntry newEntry = (SmallTokenEntry) thisEntry;
+        if (curEntry instanceof SmallTokenEntry) {
+            SmallTokenEntry curTok = (SmallTokenEntry) curEntry;
+            if (curTok.hasSamePrevToken(newEntry)) {
+                // part of a larger match, yeet them out
+                return null;
             }
-            throw AssertionUtil.shouldNotReachHere("Unexpected token entry type: " + curEntry.getClass());
-        });
+            List<SmallTokenEntry> arr = new ArrayList<>(2);
+            arr.add(curTok);
+            arr.add(newEntry);
+            listSink.add(arr);
+            return arr;
+        } else if (curEntry instanceof List) {
+            @SuppressWarnings("unchecked")
+            List<SmallTokenEntry> list = (List<SmallTokenEntry>) curEntry;
+            boolean hadMatch = list.removeIf(newEntry::hasSamePrevToken);
+            if (!hadMatch) {
+                list.add(newEntry);
+            }
+            return list;
+        }
+        throw AssertionUtil.shouldNotReachHere("Unexpected token entry type: " + curEntry.getClass());
     }
 
 }
