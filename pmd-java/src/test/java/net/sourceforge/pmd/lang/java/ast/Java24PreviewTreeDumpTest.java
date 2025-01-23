7dc9d1424394a07c5bb850c31d1f06d08e2dffb2
Andreas Dangel <andreas.dangel@pmd-code.org>
2025-01-23 10:15:20 +0100

    [java] Add tests for JEP 488
    JEP 488: Primitive Types in Patterns, instanceof, and switch (Second Preview)

--- pmd-java/src/test/java/net/sourceforge/pmd/lang/java/ast/Java24PreviewTreeDumpTest.java
+++ pmd-java/src/test/java/net/sourceforge/pmd/lang/java/ast/Java24PreviewTreeDumpTest.java
@@ -4,6 +4,13 @@
 
 package net.sourceforge.pmd.lang.java.ast;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import net.sourceforge.pmd.lang.ast.ParseException;
 import net.sourceforge.pmd.lang.java.BaseJavaTreeDumpTest;
 import net.sourceforge.pmd.lang.java.JavaParsingHelper;
 import net.sourceforge.pmd.lang.test.ast.BaseParsingHelper;
@@ -19,4 +26,14 @@ class Java24PreviewTreeDumpTest extends BaseJavaTreeDumpTest {
         return java24p;
     }
 
+    @Test
+    void jep488PrimitiveTypesInPatternsInstanceofAndSwitch() {
+        doTest("Jep488_PrimitiveTypesInPatternsInstanceofAndSwitch");
+    }
+
+    @Test
+    void jep488PrimitiveTypesInPatternsInstanceofAndSwitchBeforeJava24Preview() {
+        ParseException thrown = assertThrows(ParseException.class, () -> java24.parseResource("Jep488_PrimitiveTypesInPatternsInstanceofAndSwitch.java"));
+        assertThat(thrown.getMessage(), containsString("Primitive types in patterns instanceof and switch is a preview feature of JDK 24, you should select your language version accordingly"));
+    }
 }
