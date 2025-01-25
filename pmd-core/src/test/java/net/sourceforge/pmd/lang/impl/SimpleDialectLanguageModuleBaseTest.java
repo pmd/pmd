2830e1af7a0813dea5836964b1b9a9e2eca2e579
Juan Martín Sotuyo Dodero <juan.sotuyo@pedidosya.com>
2025-01-24 21:06:34 -0300

    Test dialect promises

---
+++ pmd-core/src/test/java/net/sourceforge/pmd/lang/impl/SimpleDialectLanguageModuleBaseTest.java
@@ -0,0 +1,35 @@
+package net.sourceforge.pmd.lang.impl;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+
+import net.sourceforge.pmd.lang.DummyLanguageDialectModule;
+import net.sourceforge.pmd.lang.DummyLanguageModule;
+import net.sourceforge.pmd.lang.LanguageProcessor;
+import net.sourceforge.pmd.lang.rule.xpath.impl.XPathFunctionDefinition;
+
+class SimpleDialectLanguageModuleBaseTest {
+
+    @Test
+    void baseLanguageXPathFunctionAvailable() throws Exception {
+        DummyLanguageModule lang = DummyLanguageModule.getInstance();
+        DummyLanguageDialectModule dialect = DummyLanguageDialectModule.getInstance();
+
+        try (LanguageProcessor baseProcessor = lang.createProcessor(lang.newPropertyBundle());
+                LanguageProcessor dialectProcessor = dialect.createProcessor(dialect.newPropertyBundle())) {
+
+            Set<XPathFunctionDefinition> dialectFunctions = dialectProcessor.services().getXPathHandler().getRegisteredExtensionFunctions();
+            for (XPathFunctionDefinition fn : baseProcessor.services().getXPathHandler().getRegisteredExtensionFunctions()) {
+                assertTrue(dialectFunctions.contains(fn), "The function " + fn.getQName() + " is not available in the dialect.");
+            }
+        }
+    }
+
+    @Test
+    void dialectSpecificXPathFunctionAvailable() throws Exception {
+        // TODO
+    }
+}
