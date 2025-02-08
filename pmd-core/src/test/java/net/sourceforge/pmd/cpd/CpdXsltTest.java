67e67c92ff9870ea4d81dc3273f4617a7bfdd2b7
Andreas Dangel <andreas.dangel@pmd-code.org>
2025-02-08 11:17:56 +0100

    [core] CpdXsltTest - use resource from checked out sources
    In CI, we optimize to build PMD only once. This happens
    on a linux machine which uses linux line endings
    on checkout. This means, under Windows, the resource
    files under target/classes/... contains the linux
    line endings. So this test failed with differences
    only in line endings.

--- pmd-core/src/test/java/net/sourceforge/pmd/cpd/CpdXsltTest.java
+++ pmd-core/src/test/java/net/sourceforge/pmd/cpd/CpdXsltTest.java
@@ -10,8 +10,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.io.File;
 import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import javax.xml.transform.ErrorListener;
 import javax.xml.transform.Templates;
 import javax.xml.transform.Transformer;
@@ -35,18 +39,38 @@ class CpdXsltTest {
 </project>
      */
 
+    // note: we use src/test/resources instead of CpdXsltTest.class.getResourceAsStream()
+    // so that we use the platform specific line endings in all cases
+    private Path expectedBasePath = Paths.get("src/test/resources", CpdXsltTest.class.getPackage().getName().replace('.', '/'));
+
+
     @Test
     void cpdhtml() throws Exception {
         String result = runXslt("cpdhtml.xslt");
-        String expected = IOUtil.readToString(CpdXsltTest.class.getResourceAsStream("ExpectedCpdHtmlReport.html"), StandardCharsets.UTF_8);
+        assertPlatformLineEndingFirstLine(result);
+        String expected = IOUtil.readFileToString(expectedBasePath.resolve("ExpectedCpdHtmlReport.html").toFile(), StandardCharsets.UTF_8);
         assertEquals(expected, result);
     }
 
     @Test
     void cpdhtmlv2() throws Exception {
         String result = runXslt("cpdhtml-v2.xslt");
-        String expected = IOUtil.readToString(CpdXsltTest.class.getResourceAsStream("ExpectedCpdHtmlReport-v2.html"), StandardCharsets.UTF_8);
+        assertPlatformLineEndingFirstLine(result);
+        String expected = IOUtil.readFileToString(expectedBasePath.resolve("ExpectedCpdHtmlReport-v2.html").toFile(), StandardCharsets.UTF_8);
         assertEquals(expected, result);
+    }
+
+    private static void assertPlatformLineEndingFirstLine(String text) {
+        String eol = System.lineSeparator();
+        int firstEol = text.indexOf(eol);
+        assertTrue(firstEol > -1, "Could find line separator at all");
+        String actualEol = text.substring(firstEol, firstEol + eol.length());
+
+        Function<String, String> hexDump = s -> s.codePoints()
+                .mapToObj(Integer::toHexString)
+                .map(h -> "0x" + h)
+                .collect(Collectors.joining(" ", "'", "'"));
+        assertEquals(eol, actualEol, "Wrong line separator found: expected=" + hexDump.apply(eol) + " actual=" + hexDump.apply(actualEol));
     }
 
     private String runXslt(String stylesheet) throws Exception {
@@ -65,7 +89,7 @@ class CpdXsltTest {
         transformer.setErrorListener(errorListener);
         transformer.transform(cpdReport, result);
 
-        assertTrue(errorListener.hasNoErrors(), "XSLT errors occured: " + errorListener);
+        assertTrue(errorListener.hasNoErrors(), "XSLT errors occurred: " + errorListener);
         return result.getWriter().toString();
     }
 
