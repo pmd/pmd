c639f081393cc7205e21e5c745d1e5a4ac6ebf5a
Andreas Dangel <andreas.dangel@pmd-code.org>
2025-02-27 09:22:31 +0100

    [apex] Apply fixes from review (#5425)

--- pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/errorprone/AvoidStatefulDatabaseResultRule.java
+++ pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/errorprone/AvoidStatefulDatabaseResultRule.java
@@ -7,26 +7,35 @@ package net.sourceforge.pmd.lang.apex.rule.errorprone;
 import java.util.Locale;
 import java.util.Set;
 
+import org.checkerframework.checker.nullness.qual.NonNull;
+
 import net.sourceforge.pmd.lang.apex.ast.ASTField;
 import net.sourceforge.pmd.lang.apex.ast.ASTUserClass;
 import net.sourceforge.pmd.lang.apex.rule.AbstractApexRule;
+import net.sourceforge.pmd.lang.rule.RuleTargetSelector;
 import net.sourceforge.pmd.util.CollectionUtil;
 
 /**
- * Using stateful `Database.[x]Result` instance variables can cause odd
+ * Using stateful {@code Database.[x]Result} instance variables can cause odd
  * serialization errors between successive batch iterations.
  *
- * https://issues.salesforce.com/issue/a028c00000qPwlqAAC/stateful-batch-job-that-stores-databasesaveresult-failed-after-validation-errors-throws-error-during-deserialization
+ * <p>This rule scans classes which implement the {@code Database.Stateful} interface. If
+ * there are instance variables of type {@code Database.[x]Result} (ex:
+ * {@code Database.SaveResult}), then a violation will be added to those variables.
  *
- * This rule scans classes which implement the `Database.Stateful` interface. If
- * there are instance variables of type `Database.[x]Result` (ex:
- * `Database.SaveResult`), then a violation will be added to those variables.
+ * @see <a href="https://issues.salesforce.com/issue/a028c00000qPwlqAAC/stateful-batch-job-that-stores-databasesaveresult-failed-after-validation-errors-throws-error-during-deserialization">Stateful batch job that stores Database.SaveResult (failed after validation errors) throws error during deserialization</a>
+ * @since 7.11.0
  */
 public final class AvoidStatefulDatabaseResultRule extends AbstractApexRule {
 
     private static final Set<String> DATABASE_RESULT_TYPES = CollectionUtil.immutableSetOf("database.leadconvertresult",
             "database.deleteresult", "database.emptyrecyclebinresult", "database.mergeresult", "database.saveresult",
             "database.undeleteresult", "database.upsertresult");
+
+    @Override
+    protected @NonNull RuleTargetSelector buildTargetSelector() {
+        return RuleTargetSelector.forTypes(ASTUserClass.class);
+    }
 
     @Override
     public Object visit(ASTUserClass theClass, Object data) {
@@ -43,7 +52,7 @@ public final class AvoidStatefulDatabaseResultRule extends AbstractApexRule {
         return data;
     }
 
-    /** Determines if the class implements the `Database.Stateful` interface */
+    /** Determines if the class implements the {@code Database.Stateful} interface. */
     private boolean implementsDatabaseStateful(ASTUserClass theClass) {
         for (String interfaceName : theClass.getInterfaceNames()) {
             if ("database.stateful".equalsIgnoreCase(interfaceName)) {
@@ -56,7 +65,7 @@ public final class AvoidStatefulDatabaseResultRule extends AbstractApexRule {
     /**
      * Determines if a variable's definition may cause issues within batch
      * iteration by being: an instance variable, not transient, and a data type
-     * of `Database.[x]Result` (or collection of them).
+     * of {@code Database.[x]Result} (or collection of them).
      */
     private boolean isNonTransientInstanceDatabaseResultField(ASTField theField) {
         return !theField.getModifiers().isStatic() && !theField.getModifiers().isTransient()
@@ -66,7 +75,7 @@ public final class AvoidStatefulDatabaseResultRule extends AbstractApexRule {
     /**
      * Determines if any of the unsupported types contains the type. We check
      * containment even as a substring to check if the type is a collection of
-     * the result types ex: `List<Database.SaveResult>`.
+     * the result types ex: {@code List<Database.SaveResult>}.
      */
     private boolean isDatabaseResultOrCollection(String type) {
         for (String databaseResultType : DATABASE_RESULT_TYPES) {
