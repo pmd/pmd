3a7ee5d15bd67d07320ee986e531b7196ef1b7c9
Thomas Prouvot <prouvot.t@gmail.com>
2025-04-14 11:18:27 +0200

    Refactor test

--- pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/bestpractices/ApexUnitTestShouldNotUseSeeAllDataTrueRule.java
+++ pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/bestpractices/ApexUnitTestShouldNotUseSeeAllDataTrueRule.java
@@ -45,11 +45,10 @@ public class ApexUnitTestShouldNotUseSeeAllDataTrueRule extends AbstractApexUnit
 
         if (modifierNode != null) {
             for (ASTAnnotationParameter parameter : modifierNode.descendants(ASTAnnotationParameter.class)) {
-                if (parameter.hasName(ASTAnnotationParameter.SEE_ALL_DATA)) {
-                    if (parameter.getBooleanValue() || "true".equalsIgnoreCase(parameter.getValue())) {
-                        asCtx(data).addViolation(node);
-                        return data;
-                    }
+                if (parameter.hasName(ASTAnnotationParameter.SEE_ALL_DATA) &&
+                        (parameter.getBooleanValue() || "true".equalsIgnoreCase(parameter.getValue()))) {
+                    asCtx(data).addViolation(node);
+                    return data;
                 }
             }
         }
