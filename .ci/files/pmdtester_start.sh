53195658642db3498ad3b6741c36ad7e8a7e0a84
Andreas Dangel <andreas.dangel@pmd-code.org>
2025-04-14 20:08:00 +0200

    [ci] build: Add pmdtester_start.sh to run regression tester
    This is now more flexible and supporting to fetch additional commits
    not only from PRs but also from branches.
    This means, a push on a branch other than main will create a report
    diffing the branch against main, just like a PR. This should be
    handy for forks/local branch developments.
    
    And a push on the main branch will create a regression report
    by looking ath the pushed commits only
    (using github.event.push.before as base). This should create a report
    showing the differencs caused by the push. Note: Since we use
    the last created regression tester baseline, this might not be
    accurate, though.
    
    We need to really use "main" as the base branch, as
    pmd-regression-tester will download the baseline using this
    base branch name as the filename.

---
+++ .ci/files/pmdtester_start.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+set -e
+
+case "${GITHUB_EVENT_NAME}" in
+  pull_request)
+    echo "Detected pull request..."
+    PMD_REGRESSION_TESTER_BASE_BRANCH="${GITHUB_BASE_REF}"
+    PMD_REGRESSION_TESTER_HEAD_BRANCH="pull/${GITHUB_REF_NAME%/merge}/head"
+    PMD_REGRESSION_TESTER_2ND_REF="${PMD_REGRESSION_TESTER_HEAD_BRANCH}_HEAD_FETCH"
+  ;;
+  push)
+    echo "Detected push onto branch ${GITHUB_REF_NAME}...."
+    case "${GITHUB_REF_NAME}" in
+      main)
+        PMD_REGRESSION_TESTER_BASE_BRANCH=main
+        PMD_REGRESSION_TESTER_HEAD_BRANCH=main
+        PMD_REGRESSION_TESTER_2ND_REF="${PMD_REGRESSION_TESTER_PUSH_BEFORE}"
+        ;;
+      *)
+        PMD_REGRESSION_TESTER_BASE_BRANCH=main
+        PMD_REGRESSION_TESTER_HEAD_BRANCH="${GITHUB_REF_NAME}"
+        PMD_REGRESSION_TESTER_2ND_REF="${PMD_REGRESSION_TESTER_HEAD_BRANCH}_HEAD_FETCH"
+        ;;
+    esac
+  ;;
+  *)
+  echo "Unsupported event: ${GITHUB_EVENT_NAME}"
+  exit 1
+esac
+
+
+echo "PMD_REGRESSION_TESTER_BASE_BRANCH=${PMD_REGRESSION_TESTER_BASE_BRANCH}"
+echo "PMD_REGRESSION_TESTER_HEAD_BRANCH=${PMD_REGRESSION_TESTER_HEAD_BRANCH}"
+echo "PMD_REGRESSION_TESTER_2ND_REF=${PMD_REGRESSION_TESTER_2ND_REF}"
+
+if [ "${PMD_REGRESSION_TESTER_BASE_BRANCH}" != "main" ]; then
+  echo "Only main is supported as base branch, and not: ${PMD_REGRESSION_TESTER_BASE_BRANCH}"
+  exit 1
+fi
+
+
+echo "::group::Fetching additional commits"
+# actions/checkout (git clone) initially only fetched with depth 2. Regression tester
+# needs more history, so we'll fetch more here
+# we also create local branches, so that we can use them in "merge-base" calls
+# the local branches are:
+# - ${PMD_REGRESSION_TESTER_BASE_BRANCH}_BASE_FETCH
+# - ${PMD_REGRESSION_TESTER_HEAD_BRANCH}_HEAD_FETCH
+
+echo "Fetching 25 commits for ${PMD_REGRESSION_TESTER_BASE_BRANCH} and ${PMD_REGRESSION_TESTER_HEAD_BRANCH}"
+git fetch --no-tags --depth=25 origin \
+    "${PMD_REGRESSION_TESTER_BASE_BRANCH}:${PMD_REGRESSION_TESTER_BASE_BRANCH}_BASE_FETCH" \
+    "${PMD_REGRESSION_TESTER_HEAD_BRANCH}:${PMD_REGRESSION_TESTER_HEAD_BRANCH}_HEAD_FETCH"
+
+# if the PR/branch is older, base might have advanced more than 25 commits... fetch more, up to 150
+# until we find a merge base, so that we are sure, regression tester can find all the changed files on
+# the branch/pull request.
+for i in $(seq 1 3); do
+  if [ -z "$( git merge-base "${PMD_REGRESSION_TESTER_BASE_BRANCH}_BASE_FETCH" "${PMD_REGRESSION_TESTER_2ND_REF}" )" ]; then
+    echo "No merge-base yet - fetching more commits... (try $i)"
+    git fetch --no-tags --deepen=50 origin \
+      "${PMD_REGRESSION_TESTER_BASE_BRANCH}:${PMD_REGRESSION_TESTER_BASE_BRANCH}_BASE_FETCH" \
+      "${PMD_REGRESSION_TESTER_HEAD_BRANCH}:${PMD_REGRESSION_TESTER_HEAD_BRANCH}_HEAD_FETCH"
+  fi
+done
+merge_base="$( git merge-base "${PMD_REGRESSION_TESTER_BASE_BRANCH}_BASE_FETCH" "${PMD_REGRESSION_TESTER_2ND_REF}" )"
+echo "Found merge base: ${merge_base}"
+if [ "$(git symbolic-ref HEAD 2>/dev/null)" = "refs/heads/main" ]; then
+  # rename main branch to free up the name "main"
+  git branch -m "original_main"
+fi
+git branch "main" "${merge_base}"
+echo "::endgroup::"
+
+echo "::group::Running pmdtester on branch ${PMD_CI_BRANCH}"
+export PMD_CI_BRANCH="main"
+if [ "${GITHUB_EVENT_NAME}" = "pull_request" ]; then
+  export PMD_CI_PULL_REQUEST_NUMBER="${GITHUB_REF_NAME%/merge}"
+fi
+bundle exec ruby .ci/files/pmdtester.rb
+echo "::endgroup::"
