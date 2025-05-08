4b330875ca04916d717b0f45d8dcb13e39334547
Clément Fournier <clement.fournier@tu-dresden.de>
2025-05-08 21:01:10 +0200

    Add new rule SimplifyStreamExpression
    Ref #5723

---
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/bestpractices/SimplifiableStreamExpressionRule.java
@@ -0,0 +1,39 @@
+/*
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
+package net.sourceforge.pmd.lang.java.rule.bestpractices;
+
+import net.sourceforge.pmd.lang.LanguageVersion;
+import net.sourceforge.pmd.lang.java.ast.ASTMethodCall;
+import net.sourceforge.pmd.lang.java.rule.AbstractJavaRulechainRule;
+import net.sourceforge.pmd.lang.java.types.InvocationMatcher;
+
+/**
+ *
+ */
+public class SimplifiableStreamExpressionRule extends AbstractJavaRulechainRule {
+
+
+    private static final InvocationMatcher STREAM_COLLECT = InvocationMatcher.parse("java.util.stream.Stream#collect(_)");
+    private static final InvocationMatcher COLLECTORS_TO_LIST = InvocationMatcher.parse("java.util.stream.Collectors#toList()");
+
+    public SimplifiableStreamExpressionRule() {
+        super(ASTMethodCall.class);
+    }
+
+    @Override
+    public Object visit(ASTMethodCall node, Object data) {
+        LanguageVersion javaVersion = node.getAstInfo().getLanguageProcessor().getLanguageVersion();
+
+        boolean hasStreamToList = javaVersion.compareToVersion("16") >= 0;
+        if (hasStreamToList && STREAM_COLLECT.matchesCall(node) && COLLECTORS_TO_LIST.matchesCall(node.getArguments().get(0))) {
+            asCtx(data).addViolationWithMessage(node, "Terminal operation `.collect(Collectors.toList())` can be replaced with `.toList()`");
+            return null;
+        }
+
+        // todo there are tons more
+
+        return null;
+    }
+}
