dbe48dd347aa6223a0e41800a58747784af2feea
Clément Fournier <clement.fournier@tu-dresden.de>
2025-05-08 21:13:20 +0200

    Support matching Arrays.asList().stream()

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/bestpractices/SimplifiableStreamExpressionRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/bestpractices/SimplifiableStreamExpressionRule.java
@@ -18,6 +18,9 @@ public class SimplifiableStreamExpressionRule extends AbstractJavaRulechainRule 
     private static final InvocationMatcher STREAM_COLLECT = InvocationMatcher.parse("java.util.stream.Stream#collect(_)");
     private static final InvocationMatcher COLLECTORS_TO_LIST = InvocationMatcher.parse("java.util.stream.Collectors#toList()");
 
+    private static final InvocationMatcher ARRAYS_ASLIST = InvocationMatcher.parse("java.util.Arrays#asList(_*)");
+    private static final InvocationMatcher COLLECTION_STREAM = InvocationMatcher.parse("java.util.Collection#stream()");
+
     public SimplifiableStreamExpressionRule() {
         super(ASTMethodCall.class);
     }
@@ -28,7 +31,11 @@ public class SimplifiableStreamExpressionRule extends AbstractJavaRulechainRule 
 
         boolean hasStreamToList = javaVersion.compareToVersion("16") >= 0;
         if (hasStreamToList && STREAM_COLLECT.matchesCall(node) && COLLECTORS_TO_LIST.matchesCall(node.getArguments().get(0))) {
-            asCtx(data).addViolationWithMessage(node, "Terminal operation `.collect(Collectors.toList())` can be replaced with `.toList()`");
+            asCtx(data).addViolation(node, "replace `.collect(Collectors.toList())` with `.toList()`");
+            return null;
+        }
+        if (COLLECTION_STREAM.matchesCall(node) && ARRAYS_ASLIST.matchesCall(node.getQualifier())) {
+            asCtx(data).addViolation(node, "replace `Arrays.asList(..).stream()` with `Arrays.stream(..)` or `Stream.of(..)`");
             return null;
         }
 
