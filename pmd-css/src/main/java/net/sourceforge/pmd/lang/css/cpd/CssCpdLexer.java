a09b0827e5b1b755d9ed8bff7fcef845ba6a80d1
Thomas Prouvot <prouvot.t@gmail.com>
2025-07-04 10:10:07 +0200

    Implementing changes requested in PR comments

--- pmd-css/src/main/java/net/sourceforge/pmd/lang/css/cpd/CssCpdLexer.java
+++ pmd-css/src/main/java/net/sourceforge/pmd/lang/css/cpd/CssCpdLexer.java
@@ -4,13 +4,10 @@
 
 package net.sourceforge.pmd.lang.css.cpd;
 
-import java.util.Locale;
-
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.Lexer;
 
 import net.sourceforge.pmd.cpd.impl.AntlrCpdLexer;
-import net.sourceforge.pmd.lang.ast.impl.antlr4.AntlrToken;
 import net.sourceforge.pmd.lang.css.ast.CssLexer;
 
 public class CssCpdLexer extends AntlrCpdLexer {
@@ -18,11 +15,5 @@ public class CssCpdLexer extends AntlrCpdLexer {
     @Override
     protected Lexer getLexerForSource(CharStream charStream) {
         return new CssLexer(charStream);
-    }
-
-    @Override
-    protected String getImage(AntlrToken token) {
-        // normalize case-insensitive tokens
-        return token.getImage().toUpperCase(Locale.ROOT);
     }
 }
