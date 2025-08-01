4ff0702e50881a305bb98d37908f83adef49353a
Andreas Dangel <andreas.dangel@pmd-code.org>
2025-08-01 11:41:57 +0200

    [java] OverrideBothEqualsAndHashcode - share implementations

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/errorprone/OverrideBothEqualsAndHashCodeOnComparableRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/errorprone/OverrideBothEqualsAndHashCodeOnComparableRule.java
@@ -4,74 +4,40 @@
 
 package net.sourceforge.pmd.lang.java.rule.errorprone;
 
-import net.sourceforge.pmd.lang.java.ast.ASTAnonymousClassDeclaration;
-import net.sourceforge.pmd.lang.java.ast.ASTClassDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTMethodDeclaration;
-import net.sourceforge.pmd.lang.java.ast.ASTRecordDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTTypeDeclaration;
-import net.sourceforge.pmd.lang.java.ast.internal.JavaAstUtils;
-import net.sourceforge.pmd.lang.java.rule.AbstractJavaRulechainRule;
 import net.sourceforge.pmd.lang.java.types.TypeTestUtil;
+import net.sourceforge.pmd.reporting.RuleContext;
 
 /**
  * Rule that checks {@link Comparable} classes for proper equals/hashCode implementations.
- * <p>
- * <a href="https://www.jetbrains.com/help/inspectopedia/ComparableImplementedButEqualsNotOverridden.html">ComparableImplementedButEqualsNotOverridden</a>
+ *
+ * <p>This is essentially the same as the rule {@link OverrideBothEqualsAndHashcodeRule} but only for
+ * {@link Comparable} types.
  *
  * @author <a href="mailto:vpotucek@me.com">Vincent Rudolf Potuček</a>
+ * @see <a href="https://www.jetbrains.com/help/inspectopedia/ComparableImplementedButEqualsNotOverridden.html">ComparableImplementedButEqualsNotOverridden</a>
  * @since 7.17.0
  */
-public class OverrideBothEqualsAndHashCodeOnComparableRule extends AbstractJavaRulechainRule {
-
+public class OverrideBothEqualsAndHashCodeOnComparableRule extends OverrideBothEqualsAndHashcodeRule {
     private static final String MESSAGE_PREFIX = "When implementing Comparable ";
     private static final String MISSING_HASH_CODE = MESSAGE_PREFIX + "hashCode() should be overridden";
     private static final String MISSING_EQUALS = MESSAGE_PREFIX + "equals() should be overridden";
     private static final String MISSING_EQUALS_AND_HASH_CODE = MESSAGE_PREFIX + "both equals() and hashCode() should be overridden";
 
-    public OverrideBothEqualsAndHashCodeOnComparableRule() {
-        super(ASTClassDeclaration.class,
-                ASTRecordDeclaration.class,
-                ASTAnonymousClassDeclaration.class);
+    @Override
+    protected boolean skipType(ASTTypeDeclaration node) {
+        return !TypeTestUtil.isA(Comparable.class, node);
     }
 
     @Override
-    public Object visit(ASTAnonymousClassDeclaration node, Object data) {
-        visitTypeDecl(node, data);
-        return null;
-    }
-
-    @Override
-    public Object visit(ASTClassDeclaration node, Object data) {
-        visitTypeDecl(node, data);
-        return null;
-    }
-
-    @Override
-    public Object visit(ASTRecordDeclaration node, Object data) {
-        visitTypeDecl(node, data);
-        return null;
-    }
-
-    private void visitTypeDecl(ASTTypeDeclaration node, Object data) {
-        if (TypeTestUtil.isA(Comparable.class, node) && !node.isInterface() && !node.isAbstract()) {
-            ASTMethodDeclaration equalsMethod = null;
-            ASTMethodDeclaration hashCodeMethod = null;
-
-            for (ASTMethodDeclaration m : node.getDeclarations(ASTMethodDeclaration.class)) {
-                if (JavaAstUtils.isEqualsMethod(m)) {
-                    equalsMethod = m;
-                } else if (JavaAstUtils.isHashCodeMethod(m)) {
-                    hashCodeMethod = m;
-                }
-            }
-
-            if (equalsMethod == null && hashCodeMethod == null) {
-                asCtx(data).addViolationWithMessage(node, MISSING_EQUALS_AND_HASH_CODE);
-            } else if (equalsMethod == null) {
-                asCtx(data).addViolationWithMessage(hashCodeMethod, MISSING_EQUALS);
-            } else if (hashCodeMethod == null) {
-                asCtx(data).addViolationWithMessage(equalsMethod, MISSING_HASH_CODE);
-            }
+    protected void maybeReport(RuleContext ctx, ASTTypeDeclaration node, ASTMethodDeclaration hashCodeMethod, ASTMethodDeclaration equalsMethod) {
+        if (equalsMethod == null && hashCodeMethod == null) {
+            ctx.addViolationWithMessage(node, MISSING_EQUALS_AND_HASH_CODE);
+        } else if (equalsMethod == null) {
+            ctx.addViolationWithMessage(hashCodeMethod, MISSING_EQUALS);
+        } else if (hashCodeMethod == null) {
+            ctx.addViolationWithMessage(equalsMethod, MISSING_HASH_CODE);
         }
     }
 }
