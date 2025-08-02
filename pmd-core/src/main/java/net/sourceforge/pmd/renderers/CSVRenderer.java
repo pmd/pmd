4f425c4b35eb24afe24a2d8184c4b90c85c69544
Jude Pereira <contact@judepereira.com>
2025-08-02 17:06:23 +0200

    Remove wildcard import.

--- pmd-core/src/main/java/net/sourceforge/pmd/renderers/CSVRenderer.java
+++ pmd-core/src/main/java/net/sourceforge/pmd/renderers/CSVRenderer.java
@@ -13,7 +13,14 @@ import org.apache.commons.lang3.StringUtils;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 
 /**
