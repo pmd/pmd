b3bbdfbdf56f8555b764f611eeb11e6a6f1eeb6c
Clément Fournier <clement.fournier@tu-dresden.de>
2025-09-01 02:49:13 +0200

    Fix JClassSymbolTest

--- pmd-java/src/test/kotlin/net/sourceforge/pmd/lang/java/symbols/internal/JClassSymbolTest.java
+++ pmd-java/src/test/kotlin/net/sourceforge/pmd/lang/java/symbols/internal/JClassSymbolTest.java
@@ -4,7 +4,6 @@
 
 package net.sourceforge.pmd.lang.java.symbols.internal;
 
-import static java.lang.annotation.ElementType.TYPE_PARAMETER;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.empty;
@@ -15,14 +14,16 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.RetentionPolicy;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 
+import net.sourceforge.pmd.lang.LanguageRegistry;
+import net.sourceforge.pmd.lang.LanguageVersion;
 import net.sourceforge.pmd.lang.java.symbols.JClassSymbol;
 import net.sourceforge.pmd.lang.java.symbols.internal.SymImplementation.Fixture;
 import net.sourceforge.pmd.lang.java.symbols.testdata.AnnotationWithNoRetention;
 import net.sourceforge.pmd.lang.java.symbols.testdata.TypeAnnotation;
+import net.sourceforge.pmd.util.OptionalBool;
 
 /**
  * Tests that test both AST and ASM symbols.
@@ -48,15 +49,25 @@ class JClassSymbolTest {
         assertEquals(RetentionPolicy.CLASS, sym.getAnnotationRetention());
     }
 
+
     @EnumSource
     @ParameterizedTest
-    @Disabled("tmp")
     void testAnnotWithNoTarget(SymImplementation impl) {
+        // Default retention changed several times
+        // between versions, here we only test with Java 8...
+        // See annotationAppliesToContext.
+        LanguageVersion javaVer =
+            LanguageRegistry.PMD.getLanguageById("java").getVersion("8");
+
+        JClassSymbol sym = impl.getSymbol(AnnotationWithNoRetention.class);
+
         for (ElementType type : ElementType.values()) {
+            boolean supportedOnJava8 =
+                type != ElementType.TYPE_PARAMETER && type != ElementType.TYPE_USE;
             assertEquals(
-                TYPE_PARAMETER != type,
-                impl.getSymbol(AnnotationWithNoRetention.class).annotationAppliesTo(type),
-                "annot supports " + type);
+                OptionalBool.definitely(supportedOnJava8),
+                sym.annotationAppliesToContext(type, javaVer),
+                         "annot supports " + type);
         }
     }
 
