1ec3a4129cea20f5b1ed7366ac93c92025196079
Andreas Dangel <andreas.dangel@pmd-code.org>
2025-09-11 09:39:11 +0200

    [java] DanglingJavadoc - update from review

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/documentation/DanglingJavadocRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/documentation/DanglingJavadocRule.java
@@ -8,10 +8,10 @@ import net.sourceforge.pmd.lang.java.ast.ASTCompilationUnit;
 import net.sourceforge.pmd.lang.java.ast.JavaComment;
 import net.sourceforge.pmd.lang.java.ast.JavadocComment;
 import net.sourceforge.pmd.lang.java.rule.AbstractJavaRulechainRule;
-import net.sourceforge.pmd.reporting.RuleContext;
 
 /**
  * Looks for Javadoc that doesn't belong to any particular class, method or field.
+ * @since 7.17.0
  */
 public class DanglingJavadocRule extends AbstractJavaRulechainRule {
 
@@ -23,7 +23,7 @@ public class DanglingJavadocRule extends AbstractJavaRulechainRule {
     public Object visit(ASTCompilationUnit unit, Object data) {
         for (JavaComment comment: unit.getComments()) {
             if (comment instanceof JavadocComment && ((JavadocComment) comment).getOwner() == null) {
-                ((RuleContext) data).addViolationWithPosition(comment.getToken(),
+                asCtx(data).addViolationWithPosition(comment.getToken(),
                     unit.getAstInfo(),
                     comment.getReportLocation(), getMessage());
             }
