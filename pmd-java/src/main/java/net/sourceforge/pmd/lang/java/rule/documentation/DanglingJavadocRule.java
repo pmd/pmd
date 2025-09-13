0deeae23ac6d71a90cee19c4c46c29aa48b79204
lukasgraef <lukas.graef@st.oth-regensburg.de>
2025-09-13 10:36:03 +0200

    [java] Fix #6058: DanglingJavadoc FP in module-info files

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/documentation/DanglingJavadocRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/documentation/DanglingJavadocRule.java
@@ -21,6 +21,10 @@ public class DanglingJavadocRule extends AbstractJavaRulechainRule {
 
     @Override
     public Object visit(ASTCompilationUnit unit, Object data) {
+        if (unit.getModuleDeclaration() != null) {
+            return null;
+        }
+
         for (JavaComment comment: unit.getComments()) {
             if (comment instanceof JavadocComment && ((JavadocComment) comment).getOwner() == null) {
                 asCtx(data).addViolationWithPosition(comment.getToken(),
