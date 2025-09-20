097592c10fc64ce1ebeeb5d18a8e670d3c631510
Sören Glimm <git@uncleowen.de>
2025-09-20 21:11:56 +0200

    A record without equals/hashCode shouldn't be reported.

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/errorprone/OverrideBothEqualsAndHashCodeOnComparableRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/errorprone/OverrideBothEqualsAndHashCodeOnComparableRule.java
@@ -6,6 +6,7 @@ package net.sourceforge.pmd.lang.java.rule.errorprone;
 
 import net.sourceforge.pmd.lang.java.ast.ASTMethodDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTTypeDeclaration;
+import net.sourceforge.pmd.lang.java.ast.internal.JavaAstUtils;
 import net.sourceforge.pmd.lang.java.types.JPrimitiveType;
 import net.sourceforge.pmd.lang.java.types.TypeTestUtil;
 import net.sourceforge.pmd.reporting.RuleContext;
@@ -37,6 +38,16 @@ public class OverrideBothEqualsAndHashCodeOnComparableRule extends OverrideBothE
                 && !method.isStatic();
     }
 
+    private static boolean hasBrokenEqualsMethod(ASTTypeDeclaration node) {
+        for (ASTMethodDeclaration m : node.getDeclarations(ASTMethodDeclaration.class)) {
+            if ("equals".equals(m.getName()) && !JavaAstUtils.isEqualsMethod(m)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     @Override
     protected void maybeReport(RuleContext ctx, ASTTypeDeclaration node, ASTMethodDeclaration hashCodeMethod, ASTMethodDeclaration equalsMethod) {
         ASTMethodDeclaration compareToMethod = node
@@ -47,7 +58,9 @@ public class OverrideBothEqualsAndHashCodeOnComparableRule extends OverrideBothE
         }
 
         if (equalsMethod == null && hashCodeMethod == null) {
-            ctx.addViolationWithMessage(compareToMethod, MISSING_EQUALS_AND_HASH_CODE);
+            if (!node.isRecord() || hasBrokenEqualsMethod(node)) {
+                ctx.addViolationWithMessage(compareToMethod, MISSING_EQUALS_AND_HASH_CODE);
+            }
         } else if (equalsMethod == null) {
             ctx.addViolationWithMessage(hashCodeMethod, MISSING_EQUALS);
         } else if (hashCodeMethod == null) {
