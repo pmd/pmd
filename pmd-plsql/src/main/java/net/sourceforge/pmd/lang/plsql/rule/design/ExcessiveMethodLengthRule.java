4551753a5bd154b3d16c0bb1c59052720dac3792
Andreas Dangel <andreas.dangel@pmd-code.org>
2025-11-13 15:17:56 +0100

    [plsql] Deprecate rules Excessive*Length

--- pmd-plsql/src/main/java/net/sourceforge/pmd/lang/plsql/rule/design/ExcessiveMethodLengthRule.java
+++ pmd-plsql/src/main/java/net/sourceforge/pmd/lang/plsql/rule/design/ExcessiveMethodLengthRule.java
@@ -9,7 +9,11 @@ import net.sourceforge.pmd.lang.plsql.ast.ExecutableCode;
 /**
  * This rule detects when a method exceeds a certain threshold. i.e. if a method
  * has more than x lines of code.
+ *
+ * <p>Equivalent XPath: {@code //(MethodDeclaration|ProgramUnit|TriggerTimingPointSection|TriggerUnit|TypeMethod)[@EndLine - @BeginLine > 100]}</p>
+ * @deprecated Since 7.19.0. Use the rule {@link NcssCountRule} instead.
  */
+@Deprecated
 public class ExcessiveMethodLengthRule extends AbstractCounterCheckRule.AbstractLineLengthCheckRule<ExecutableCode> {
     public ExcessiveMethodLengthRule() {
         super(ExecutableCode.class
