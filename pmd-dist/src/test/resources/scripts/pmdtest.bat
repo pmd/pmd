ebc3e42df2939247c47c8a7932f4f763721f2c95
Andreas Dangel <andreas.dangel@pmd-code.org>
2025-12-04 18:05:17 +0100

    [cli] Improve start script for designer (windows)

--- pmd-dist/src/test/resources/scripts/pmdtest.bat
+++ pmd-dist/src/test/resources/scripts/pmdtest.bat
@@ -6,63 +6,75 @@ rem
 rem Simple manual test script
 rem - code is copied from pmd.bat to be tested here (so please check, it might be out of sync)
 rem - mostly the function "determine_java_version" is tested here
-rem - just run it with "pmd.bat" and look at the output
+rem - just run it with "pmdtest.bat" and look at the output
 rem - test cases are at the end of this script
 rem
 
+setlocal EnableDelayedExpansion
+rem use unicode codepage to properly support UTF-8
+chcp 65001>nul
+rem make all variables local to not add new global environment variables to the current cmd session
+setlocal
+
 GOTO :main
 
+:mock_show_settings_properties
+rem instead of: java -XshowSettings:properties 2>&1
+
+set nl=^
+
+
+rem two empty lines required
+set "MOCKED_JAVA_PROPERTIES=Property settings:!nl!"
+set "MOCKED_JAVA_PROPERTIES=!MOCKED_JAVA_PROPERTIES!    some.other.property1 = value!nl!"
+set "MOCKED_JAVA_PROPERTIES=!MOCKED_JAVA_PROPERTIES!    java.version = !MOCKED_JAVA_VERSION!!nl!"
+set "MOCKED_JAVA_PROPERTIES=!MOCKED_JAVA_PROPERTIES!    some.other.property2 = value!nl!"
+set "MOCKED_JAVA_PROPERTIES=!MOCKED_JAVA_PROPERTIES!    java.home = !MOCKED_JAVA_HOME!!nl!"
+set "MOCKED_JAVA_PROPERTIES=!MOCKED_JAVA_PROPERTIES!    some.other.property3 = value!nl!"
+EXIT /B
+
 :determine_java_version
-rem sets the jver variable to the java version, eg 90 for 9.0.1+x or 80 for 1.8.0_171-b11 or 110 for 11.0.6.1
-rem sets the jvendor variable to either java (oracle) or openjdk
-for /f tokens^=1^,3^,4^,5^ delims^=.-_+^"^  %%j in (%full_version%) do (
-  set jvendor=%%j
-  if %%l EQU ea (
-    set /A "jver=%%k0"
-  ) else (
-    if %%k EQU 1 (
-      rem for java version 1.7.x, 1.8.x, ignore the first 1.
-      set /A "jver=%%l%%m"
-    ) else (
-      set /A "jver=%%k%%l"
-    )
-  )
+CALL :mock_show_settings_properties
+
+rem sets the java_version_feature variable. This is e.g. "8" for java 1.8, "9" for java 9, "10" for java 10, ...
+rem sets the java_version_update variable. This is the update release counter: e.g. "17" for java 11.0.17
+
+set java_version_prop=
+set java_home_property=
+FOR /F tokens^=1^,3^  %%j IN ("!MOCKED_JAVA_PROPERTIES!") DO (
+    IF "%%j" == "java.version" set java_version_prop=%%k
+    IF "%%j" == "java.home" set java_home_property=%%k
 )
 
-set detection=
-if %jver% GEQ 70 (
-    if %jver% LSS 80 (
-        set detection="detected java 7"
-    )
+set java_version_feature=
+set java_version_update=
+FOR /f tokens^=1^,2^,3^,4^ delims^=.-_+^"^  %%j IN ("%java_version_prop%") DO (
+  IF %%j EQU 1 (
+    set java_version_feature=%%k
+    set java_version_update=%%m
+  ) ELSE (
+    set java_version_feature=%%j
+    set java_version_update=%%l
+  )
 )
-if [%detection%] == [] (
-    if %jver% GEQ 80 (
-        if %jver% LSS 90 (
-            set detection="detected java 8"
-        )
-    )
+rem if there was no 3rd component (eg. -ea or ga version), use "0"
+IF "%java_version_update%" == "" set java_version_update=0
+
+set java_has_javafx=0
+set java_javafx_properties=
+set java_javafx_properties_path=
+IF EXIST "%java_home_property%/lib/javafx.properties" (
+    set "java_javafx_properties=%java_home_property%/lib/javafx.properties"
+    set java_has_javafx=1
 )
-if [%detection%] == [] (
-    if %jver% GEQ 90 (
-        if %jver% LSS 110 (
-          if %jvendor% == java (
-              set detection="detected java 9 or 10 from oracle"
-          )
-        )
-    )
+IF EXIST "%java_home_property%/jre/lib/javafx.properties" (
+    set "java_javafx_properties=%java_home_property%/jre/lib/javafx.properties"
+    set java_has_javafx=1
 )
-if [%detection%] == [] (
-    if %jvendor% == openjdk (
-        set detection="detected java 11 from oracle or any openjdk"
-    )
-)
-if [%detection%] == [] (
-    if %jvendor% == java (
-        if %jver% GEQ 110 (
-            set detection="detected java 11 from oracle or any openjdk"
-        )
-    )
-)
+rem resolve dirname
+IF %java_has_javafx% EQU 1 FOR %%F IN (%java_javafx_properties%) DO set "java_javafx_properties_path=%%~dpF"
+rem remove trailing backslash
+IF %java_has_javafx% EQU 1 set "java_javafx_properties_path=%java_javafx_properties_path:~0,-1%"
 
 EXIT /B
 
@@ -86,18 +98,54 @@ if defined cp (
 endlocal
 EXIT /B
 
-:run_test
-set full_version=%1
-set expected_vendor=%2
-set expected_version=%3
-set expected_detection=%4
+:pass
+echo  [32mOK[0m %~1
+EXIT /B
 
+:fail
+echo  [31mFAILED[0m
+set failed=1
+EXIT /B
+
+:run_test
+IF %failed% EQU 1 EXIT /B
+set MOCKED_JAVA_VERSION=%~1
+set MOCKED_JAVA_HOME=%~2
+set EXPECTED_FEATURE=%3
+set EXPECTED_UPDATE=%4
+set EXPECTED_HAS_JAVAFX=%5
+
+echo Testing: '%MOCKED_JAVA_VERSION%' in '%MOCKED_JAVA_HOME%'
+
+rem use mocked path relative to the test script
+set MOCKED_JAVA_HOME=%~dp0%MOCKED_JAVA_HOME%
 CALL :determine_java_version
 
-echo full_version: %full_version%
-if %jver% == %expected_version% ( echo jver: %jver% [32mOK[0m ) ELSE ( echo jver: %jver% [31mEXPECTED: %expected_version% [0m )
-if %jvendor% == %expected_vendor% ( echo jvendor: %jvendor% [32mOK[0m ) ELSE ( echo jvendor: %jvendor% [31mEXPECTED: %expected_vendor% [0m )
-if [%detection%] == [%expected_detection%] ( echo detection: %detection% [32mOK[0m ) ELSE ( echo detection: %detection% [31mEXPECTED: %expected_detection% [0m )
+echo.|set /p =java_version_feature: %java_version_feature%
+IF %EXPECTED_FEATURE% == %java_version_feature% ( CALL :pass ) ELSE ( CALL :fail )
+echo.|set /p =java_version_update: %java_version_update%
+IF %EXPECTED_UPDATE% == %java_version_update% ( CALL :pass ) ELSE ( CALL :fail )
+echo.|set /p =java_has_javafx: %java_has_javafx%
+set "_temp_javafx_passed=0"
+IF "%EXPECTED_HAS_JAVAFX%" == "yes" (
+    IF %java_has_javafx% EQU 1 (
+        IF NOT "%java_javafx_properties%" == "" (
+            set "_temp_javafx_passed=1"
+            CALL :pass
+        )
+    )
+)
+IF "%EXPECTED_HAS_JAVAFX%" == "no" (
+    IF %java_has_javafx% EQU 0 (
+        IF "%java_javafx_properties%" == "" (
+            set "_temp_javafx_passed=1"
+            CALL :pass
+        )
+    )
+)
+IF %_temp_javafx_passed% EQU 0 ( CALL :fail )
+rem echo javafx_properties: %java_javafx_properties%
+rem echo javafx_properties_path: %java_javafx_properties_path%
 echo.
 
 EXIT /B
@@ -114,18 +162,39 @@ CALL :check_classpath_extension "a\b.jar"
 CALL :check_classpath_extension "a"\b.jar
 CALL :check_classpath_extension "a"\b.jar;"c"\d.jar
 
-CALL :run_test "java version ""1.7.0_80"""                java      70    "detected java 7"
-CALL :run_test "openjdk version ""1.7.0_352"""            openjdk   70    "detected java 7"
-CALL :run_test "java version ""1.8.0_271"""               java      80    "detected java 8"
-CALL :run_test "openjdk version ""1.8.0_345"""            openjdk   80    "detected java 8"
-CALL :run_test "java version ""9.0.4"""                   java      90    "detected java 9 or 10 from oracle"
-CALL :run_test "openjdk version ""9.0.4"""                openjdk   90    "detected java 11 from oracle or any openjdk"
-CALL :run_test "java version ""10.0.2"" 2018-07-17"       java      100   "detected java 9 or 10 from oracle"
-CALL :run_test "openjdk version ""11.0.6"" 2022-08-12"    openjdk   110   "detected java 11 from oracle or any openjdk"
-CALL :run_test "openjdk version ""11.0.6.1"" 2022-08-12"  openjdk   110   "detected java 11 from oracle or any openjdk"
-CALL :run_test "java version ""11.0.13"" 2021-10-19 LTS"  java      110   "detected java 11 from oracle or any openjdk"
-CALL :run_test "openjdk version ""17.0.4"" 2022-08-12"    openjdk   170   "detected java 11 from oracle or any openjdk"
-CALL :run_test "openjdk version ""17.1.4"" 2022-08-12"    openjdk   171   "detected java 11 from oracle or any openjdk"
-CALL :run_test "openjdk version ""17.0.4.1"" 2022-08-12"  openjdk   170   "detected java 11 from oracle or any openjdk"
-CALL :run_test "openjdk version ""18.0.2.1"" 2022-08-18"  openjdk   180   "detected java 11 from oracle or any openjdk"
-CALL :run_test "openjdk version ""19-ea"" 2022-09-20"     openjdk   190   "detected java 11 from oracle or any openjdk"
+rem Some predefined variants. We only check for the existence of a javafx.properties file
+rem at lib/ or jre/lib. java.home sometimes points already to jre (oracle), but openjdk
+rem builds don't have a jre folder anymore. Anyway, both should work.
+set JH_WITH_FX=java-with-fx
+set JH_WITH_FX2=java-with-fx/jre
+set JH_WITHOUT_FX=java-without-fx
+
+set failed=0
+CALL :run_test "1.7.0_80"  "%JH_WITH_FX%"       7     80  yes
+CALL :run_test "1.7.0_352" "%JH_WITH_FX%"       7    352  yes
+CALL :run_test "1.8.0_271" "%JH_WITH_FX%"       8    271  yes
+CALL :run_test "1.8.0_345" "%JH_WITH_FX%"       8    345  yes
+CALL :run_test "1.8.0_441" "%JH_WITH_FX%"       8    441  yes
+CALL :run_test "1.8.0_441" "%JH_WITH_FX2%"      8    441  yes
+rem e.g. oracle
+CALL :run_test "1.8.0_471" "%JH_WITHOUT_FX%"    8    471  no
+rem # e.g. azul
+CALL :run_test "1.8.0_471" "%JH_WITH_FX2%"      8    471  yes
+CALL :run_test "9.0.4"     "%JH_WITHOUT_FX%"    9      4  no
+CALL :run_test "10.0.2"    "%JH_WITHOUT_FX%"   10      2  no
+CALL :run_test "11.0.6"    "%JH_WITHOUT_FX%"   11      6  no
+CALL :run_test "11.0.6.1"  "%JH_WITHOUT_FX%"   11      6  no
+CALL :run_test "11.0.13"   "%JH_WITHOUT_FX%"   11     13  no
+CALL :run_test "11.0.19"   "%JH_WITHOUT_FX%"   11     19  no
+CALL :run_test "17.0.4"    "%JH_WITHOUT_FX%"   17      4  no
+CALL :run_test "17.0.4.1"  "%JH_WITHOUT_FX%"   17      4  no
+CALL :run_test "17.0.17"   "%JH_WITH_FX%"      17     17  yes
+CALL :run_test "18.0.2.1"  "%JH_WITHOUT_FX%"   18      2  no
+CALL :run_test "19-ea"     "%JH_WITHOUT_FX%"   19      0  no
+CALL :run_test "25"        "%JH_WITHOUT_FX%"   25      0  no
+CALL :run_test "25.0.1"    "%JH_WITHOUT_FX%"   25      1  no
+
+
+IF %failed% EQU 0 CALL :pass "All tests passed. ✔️"
+
+EXIT /B
