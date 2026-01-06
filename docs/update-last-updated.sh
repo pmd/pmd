1ac42cf5993b722d9d885dcf82963e0f59b1f2e1
Andreas Dangel <andreas.dangel@pmd-code.org>
2026-01-06 18:53:02 +0100

    [doc] Improve update-last-updated.sh to ignore front matter

--- docs/update-last-updated.sh
+++ docs/update-last-updated.sh
@@ -20,15 +20,21 @@
 function process_file() {
   FILE="$1"
   echo -n "$FILE: "
-  read -ra COMMIT_INFO < <(git log -1 --pretty="%cI %H" "$FILE")
+
+  LINE_NUMBER_FRONT_MATTER_END=$(grep -n -e "---" "$FILE"|head -2|tail -1|cut -d ":" -f 1)
+  LINE_NUMBER_FRONT_MATTER_END=$((LINE_NUMBER_FRONT_MATTER_END + 1))
+  read -ra COMMIT_INFO < <(git annotate -L $LINE_NUMBER_FRONT_MATTER_END -t "$FILE"| cut -f 1,3|sed -e 's/\(.\+\)\t\(.\+\) .\+/\2 \1/'|sort|tail -1)
   #echo "Date: ${COMMIT_INFO[0]}"
   #echo "Commit: ${COMMIT_INFO[1]}"
 
-  DATE=$(date --date="${COMMIT_INFO[0]}" "+%B %Y")
+  DATE=$(date --date="@${COMMIT_INFO[0]}" "+%B %Y")
 
   TAG="$(git describe --contains "${COMMIT_INFO[1]}")"
   TAG="${TAG%%~*}"
   TAG="${TAG#pmd_releases/}"
+  if [[ $TAG = 7.0.0-* ]]; then
+    TAG="7.0.0"
+  fi
   #echo "Version: $TAG"
 
   LAST_UPDATED="last_updated: $DATE ($TAG)"
@@ -45,10 +51,11 @@ if [ -n "$1" ]; then
 else
   # no file given, search for all markdown files in current directory
   # ignore files in directory "vendor"
-  for i in `find . -type d -name vendor -prune -o -type f -name "*.md" -print`; do
+  while IFS= read -r -d '' i
+  do
     # only consider files, that are under version control
     if git ls-files --error-unmatch "$i" >/dev/null 2>&1; then
       process_file "$i"
     fi
-  done
+  done < <(find . -type d -name vendor -prune -o -type f -name "*.md" -print0)
 fi
