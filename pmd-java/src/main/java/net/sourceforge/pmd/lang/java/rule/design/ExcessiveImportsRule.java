16acd4af1a5da0cdd23f16b6d75577055578e4d6
Andreas Dangel <andreas.dangel@pmd-code.org>
2026-01-08 09:25:09 +0100

    ExcessiveImports: improve report location calculation

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/design/ExcessiveImportsRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/design/ExcessiveImportsRule.java
@@ -7,7 +7,7 @@ package net.sourceforge.pmd.lang.java.rule.design;
 import java.util.List;
 
 import net.sourceforge.pmd.lang.document.FileLocation;
-import net.sourceforge.pmd.lang.document.TextRange2d;
+import net.sourceforge.pmd.lang.document.TextRegion;
 import net.sourceforge.pmd.lang.java.ast.ASTCompilationUnit;
 import net.sourceforge.pmd.lang.java.ast.ASTImportDeclaration;
 import net.sourceforge.pmd.lang.java.rule.internal.AbstractJavaCounterCheckRule;
@@ -49,14 +49,11 @@ public class ExcessiveImportsRule extends AbstractJavaCounterCheckRule<ASTCompil
     protected FileLocation getReportLocation(ASTCompilationUnit node) {
         List<ASTImportDeclaration> imports = node.children(ASTImportDeclaration.class).toList();
 
+        // note: reportLevel must be positive (>0), so we have at least one import declaration in the list
         ASTImportDeclaration firstImport = imports.get(0);
         ASTImportDeclaration lastImport = imports.get(imports.size() - 1);
 
-        TextRange2d textRange = TextRange2d.range2d(
-                firstImport.getBeginLine(), firstImport.getBeginColumn(),
-                lastImport.getEndLine(), lastImport.getEndColumn()
-        );
-
-        return FileLocation.range(node.getAstInfo().getTextDocument().getFileId(), textRange);
+        TextRegion importsRegion = TextRegion.union(firstImport.getTextRegion(), lastImport.getTextRegion());
+        return node.getTextDocument().toLocation(importsRegion);
     }
 }
