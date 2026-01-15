bcf296af2a92d85616a17b0fc288bfdd5fd8d83d
Andreas Dangel <andreas.dangel@pmd-code.org>
2026-01-15 17:47:08 +0100

    [java] Move DoNotUseUnsafe to errorprone

--- pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/errorprone/DoNotUseUnsafeTest.java
+++ pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/errorprone/DoNotUseUnsafeTest.java
@@ -2,7 +2,7 @@
  * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
  */
 
-package net.sourceforge.pmd.lang.java.rule.security;
+package net.sourceforge.pmd.lang.java.rule.errorprone;
 
 import net.sourceforge.pmd.test.PmdRuleTst;
 
