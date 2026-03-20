847de47f983c47b69e08673f1ace10f5b93d7474
Andreas Dangel <andreas.dangel@pmd-code.org>
2026-03-20 10:19:39 +0100

    Use summary and conclusion from regression tester

--- .ci/files/pmdtester.rb
+++ .ci/files/pmdtester.rb
@@ -22,6 +22,9 @@ def get_args(base_branch, autogen = true, patch_config = './pmd/.ci/files/all-re
    ]
 end
 
+@summary_file = 'target/reports/diff/summary.txt'
+@conclusion_file = 'target/reports/diff/conclusion.txt'
+
 def run_pmdtester
   Dir.chdir('..') do
     begin
@@ -29,55 +32,30 @@ def run_pmdtester
       @logger.info "Run against PR base #{@base_branch}"
       @summary = PmdTester::Runner.new(get_args(@base_branch)).run
 
-      if Dir.exist?('target/reports/diff')
-        message = create_message
-        conclusion = determine_conclusion
-      else
-        message = "No regression tested rules have been changed."
-        conclusion = "skipped"
+      unless File.exist?(@summary_file) && File.exist?(@conclusion_file)
+        write_error_result
+        @logger.error "Running pmdtester failed: summary or conclusion file not found."
       end
 
     rescue StandardError => e
-      message = "⚠️ Running pmdtester failed, this message is mainly used to remind the maintainers of PMD."
-      conclusion = "failure"
+      write_error_result
       @logger.error "Running pmdtester failed: #{e.inspect}"
     end
-
-    unless Dir.exist?('target/reports/diff')
-      Dir.mkdir('target/reports/diff')
-    end
-    summary_file = 'target/reports/diff/summary.txt'
-    File.write(summary_file, message)
-    @logger.info "Wrote summary file #{summary_file}:\n\n#{message}"
-    conclusion_file = 'target/reports/diff/conclusion.txt'
-    File.write(conclusion_file, conclusion)
-    @logger.info "Wrote conclusion file #{conclusion_file}: #{conclusion}"
   end
 end
 
-def create_message
-  "Compared to #{@base_branch}:\n"\
-  "This changeset " \
-  "changes #{@summary[:violations][:changed]} violations,\n" \
-  "introduces #{@summary[:violations][:new]} new violations, " \
-  "#{@summary[:errors][:new]} new errors and " \
-  "#{@summary[:configerrors][:new]} new configuration errors,\n" \
-  "removes #{@summary[:violations][:removed]} violations, "\
-  "#{@summary[:errors][:removed]} errors and " \
-  "#{@summary[:configerrors][:removed]} configuration errors.\n"
-end
-
-def determine_conclusion
-  total_changes = @summary[:violations][:changed]
-    + @summary[:violations][:new]
-    + @summary[:violations][:removed]
-    + @summary[:errors][:new]
-    + @summary[:configerrors][:new]
-  if total_changes > 0
-    "neutral"
-  else
-    "success"
+def write_error_result
+  unless Dir.exist?('target/reports/diff')
+    Dir.mkdir('target/reports/diff')
   end
+
+  message = "⚠️ Running pmdtester failed, this message is mainly used to remind the maintainers of PMD."
+  File.write(@summary_file, message)
+  @logger.info "Wrote summary file #{@summary_file}:\n\n#{message}"
+
+  conclusion = "failure"
+  File.write(@conclusion_file, conclusion)
+  @logger.info "Wrote conclusion file #{@conclusion_file}: #{conclusion}"
 end
 
 # Perform regression testing
