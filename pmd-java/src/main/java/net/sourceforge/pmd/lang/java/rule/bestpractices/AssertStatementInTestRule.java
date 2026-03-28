b2ca2e30023171139130961fbfcc5910cfb014de
Zbynek Konecny <zbynek@geogebra.org>
2026-03-28 23:51:32 +0100

    [java] new rule AssertStatementInTest

---
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/bestpractices/AssertStatementInTestRule.java
@@ -0,0 +1,27 @@
+/*
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
+package net.sourceforge.pmd.lang.java.rule.bestpractices;
+
+import net.sourceforge.pmd.lang.java.ast.ASTAssertStatement;
+import net.sourceforge.pmd.lang.java.ast.ASTMethodDeclaration;
+import net.sourceforge.pmd.lang.java.rule.AbstractJavaRulechainRule;
+import net.sourceforge.pmd.lang.java.rule.internal.TestFrameworksUtil;
+
+public class AssertStatementInTestRule extends AbstractJavaRulechainRule {
+
+    public AssertStatementInTestRule() {
+        super(ASTMethodDeclaration.class);
+    }
+
+    @Override
+    public Object visit(ASTMethodDeclaration method, Object data) {
+        if (TestFrameworksUtil.isTestMethod(method)
+                || TestFrameworksUtil.isTestConfigurationMethod(method)) {
+            method.descendants(ASTAssertStatement.class).forEach(
+                node -> asCtx(data).addViolation(node));
+        }
+        return null;
+    }
+}
