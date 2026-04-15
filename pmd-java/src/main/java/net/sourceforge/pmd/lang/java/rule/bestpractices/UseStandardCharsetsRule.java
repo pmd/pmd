4af2dbd294f05ae0f89bd4a63577d0acdad7405e
Sören Glimm <git@uncleowen.de>
2026-04-15 13:45:27 +0200

    Update description

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/bestpractices/UseStandardCharsetsRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/bestpractices/UseStandardCharsetsRule.java
@@ -23,7 +23,7 @@ import net.sourceforge.pmd.reporting.RuleContext;
 /**
  * Starting with Java 7, StandardCharsets provides constants for common Charset objects, such as UTF-8.
  * Using the constants is less error prone, and can provide a small performance advantage compared to
- * `Charset.forName(...)` since no scan across the internal `Charset` caches is needed.
+ * `Charset.forName(...)` or `String.getBytes(String)` since no scan across the internal `Charset` caches is needed.
  *
  * @since 6.34.0 (as XPath) / 7.24.0 (as Java)
  */
