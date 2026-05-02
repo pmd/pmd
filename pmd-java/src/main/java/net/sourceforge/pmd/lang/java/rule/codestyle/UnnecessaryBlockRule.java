624ec7595e2a729e5c11e3b261ffa1713c27fdeb
Sören Glimm <git@uncleowen.de>
2026-05-02 21:31:27 +0200

    New rule: UnnecessaryBlock

---
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/codestyle/UnnecessaryBlockRule.java
@@ -0,0 +1,42 @@
+/*
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
+package net.sourceforge.pmd.lang.java.rule.codestyle;
+
+import net.sourceforge.pmd.lang.java.ast.ASTBlock;
+import net.sourceforge.pmd.lang.java.ast.ASTLocalVariableDeclaration;
+import net.sourceforge.pmd.lang.java.rule.AbstractJavaRulechainRule;
+import net.sourceforge.pmd.reporting.RuleContext;
+
+public class UnnecessaryBlockRule extends AbstractJavaRulechainRule {
+
+    public UnnecessaryBlockRule() {
+        super(ASTBlock.class);
+    }
+
+    @Override
+    public RuleContext visit(ASTBlock block, Object data) {
+        RuleContext ctx = (RuleContext) data;
+
+        if (!isInsideAnotherBlock(block)) {
+            return ctx;
+        }
+
+        if (containsVariableDeclaration(block)) {
+            return ctx;
+        }
+
+        ctx.addViolation(block);
+
+        return ctx;
+    }
+
+    private boolean isInsideAnotherBlock(ASTBlock block) {
+        return block.getParent() instanceof ASTBlock;
+    }
+
+    private boolean containsVariableDeclaration(ASTBlock block) {
+        return block.children(ASTLocalVariableDeclaration.class).count() > 0;
+    }
+}
