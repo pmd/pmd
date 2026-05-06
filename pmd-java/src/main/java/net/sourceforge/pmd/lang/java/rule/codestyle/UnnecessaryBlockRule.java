0dc7d60678d830c5026277e321a4dfbbd556bc8a
Sören Glimm <git@uncleowen.de>
2026-05-06 14:23:47 +0200

    Also violate if one ASTBlock is the only child of another ASTBlock.
    This is likely caused by a typo.

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/codestyle/UnnecessaryBlockRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/codestyle/UnnecessaryBlockRule.java
@@ -7,6 +7,7 @@ package net.sourceforge.pmd.lang.java.rule.codestyle;
 import net.sourceforge.pmd.lang.java.ast.ASTBlock;
 import net.sourceforge.pmd.lang.java.ast.ASTLocalClassStatement;
 import net.sourceforge.pmd.lang.java.ast.ASTLocalVariableDeclaration;
+import net.sourceforge.pmd.lang.java.ast.JavaNode;
 import net.sourceforge.pmd.lang.java.rule.AbstractJavaRulechainRule;
 import net.sourceforge.pmd.reporting.RuleContext;
 
@@ -20,7 +21,7 @@ public class UnnecessaryBlockRule extends AbstractJavaRulechainRule {
     public RuleContext visit(ASTBlock block, Object data) {
         RuleContext ctx = (RuleContext) data;
 
-        if (doesntRestrictScope(block)) {
+        if (doesntRestrictScope(block) || blockDirectlyInBlock(block)) {
             ctx.addViolation(block);
         }
 
@@ -43,5 +44,11 @@ public class UnnecessaryBlockRule extends AbstractJavaRulechainRule {
 
     private boolean containsClassDeclaration(ASTBlock block) {
         return block.children(ASTLocalClassStatement.class).nonEmpty();
+    }
+
+    private boolean blockDirectlyInBlock(ASTBlock block) {
+        JavaNode parent = block.getParent();
+
+        return (parent instanceof ASTBlock) && (parent.getNumChildren() == 1);
     }
 }
