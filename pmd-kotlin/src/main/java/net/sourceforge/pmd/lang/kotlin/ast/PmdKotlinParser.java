ff6d2f4c3c4da547a99e86f38f5ec1535d845ed2
Peter Paul Bakker <peter.paul.bakker@stokpop.nl>
2026-05-11 11:11:09 +0000

    Fix #6608: report Kotlin lexer/parser errors with file context
    - Add custom ANTLR error listeners in PmdKotlinParser
    - Lexer errors: log WARN with file location and throw LexException
    - Parser errors: log WARN with file location and throw ParseException
    - Short WARN message (strip verbose expected-token list), full at DEBUG
    - Simplify KotlinParsingHelper: remove duplicate custom parseImpl override
    - Add tests: syntaxErrorThrowsParseException, lexerErrorThrowsLexException

--- pmd-kotlin/src/main/java/net/sourceforge/pmd/lang/kotlin/ast/PmdKotlinParser.java
+++ pmd-kotlin/src/main/java/net/sourceforge/pmd/lang/kotlin/ast/PmdKotlinParser.java
@@ -4,22 +4,84 @@
 
 package net.sourceforge.pmd.lang.kotlin.ast;
 
+import org.antlr.v4.runtime.BaseErrorListener;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Lexer;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import net.sourceforge.pmd.lang.ast.LexException;
+import net.sourceforge.pmd.lang.ast.ParseException;
 import net.sourceforge.pmd.lang.ast.impl.antlr4.AntlrBaseParser;
+import net.sourceforge.pmd.lang.document.FileLocation;
 import net.sourceforge.pmd.lang.kotlin.ast.KotlinParser.KtKotlinFile;
 
 /**
  * Adapter for the KotlinParser.
+ *
+ * <p>Error handling strategy:
+ * <ul>
+ *   <li><b>Lexer errors</b> (e.g. unrecognized tokens): logged as
+ *       {@code [WARN] Lexer error at <file>:<line>:<col>: ...} and a
+ *       {@link net.sourceforge.pmd.lang.ast.LexException} is thrown immediately.</li>
+ *   <li><b>Parser errors</b> (e.g. unexpected token structure): logged as
+ *       {@code [WARN] Parser error at <file>:<line>:<col>: ...} with a short message
+ *       (without the verbose expected-token list), full details at DEBUG.
+ *       A {@link net.sourceforge.pmd.lang.ast.ParseException} is thrown immediately.
+ *       PMD reports the file as a {@code ProcessingError} and skips rule analysis for it.
+ *       All other files continue to be processed. Exit code 5 is returned by the CLI if
+ *       any processing errors occurred (suppressible with {@code --no-fail-on-error}).</li>
+ * </ul>
  */
 public final class PmdKotlinParser extends AntlrBaseParser<KotlinNode, KtKotlinFile> {
 
+    private static final Logger LOG = LoggerFactory.getLogger(PmdKotlinParser.class);
+
     @Override
     protected KtKotlinFile parse(final Lexer lexer, ParserTask task) {
+        lexer.removeErrorListeners();
+        lexer.addErrorListener(buildLexerErrorListener(task));
+
         KotlinParser parser = new KotlinParser(new CommonTokenStream(lexer));
+        parser.removeErrorListeners();
+        parser.addErrorListener(buildParserErrorListener(task));
+
         return parser.kotlinFile().makeAstInfo(task);
+    }
+
+    private static BaseErrorListener buildLexerErrorListener(ParserTask task) {
+        return new BaseErrorListener() {
+            @Override
+            public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol,
+                                    int line, int charPositionInLine,
+                                    String msg, RecognitionException e) {
+                LOG.warn("Lexer error at {}:{}:{}: {}", task.getFileId().getOriginalPath(), line, charPositionInLine, msg);
+                throw new LexException(line, charPositionInLine, task.getFileId(), msg, null);
+            }
+        };
+    }
+
+    private static BaseErrorListener buildParserErrorListener(ParserTask task) {
+        return new BaseErrorListener() {
+            @Override
+            public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol,
+                                    int line, int charPositionInLine,
+                                    String msg, RecognitionException e) {
+                LOG.warn("Parser error at {}:{}:{}: {}", task.getFileId().getOriginalPath(), line, charPositionInLine, shortMessage(msg));
+                LOG.debug("Parser error at {}:{}:{}: {}", task.getFileId().getOriginalPath(), line, charPositionInLine, msg);
+                throw new ParseException(shortMessage(msg))
+                    .withLocation(FileLocation.caret(task.getFileId(), line, charPositionInLine));
+            }
+        };
+    }
+
+    /** Strips the verbose "expecting {...}" token list from ANTLR parser error messages for concise WARN output. */
+    private static String shortMessage(String msg) {
+        int idx = msg.indexOf(" expecting ");
+        return idx >= 0 ? msg.substring(0, idx) : msg;
     }
 
     @Override
