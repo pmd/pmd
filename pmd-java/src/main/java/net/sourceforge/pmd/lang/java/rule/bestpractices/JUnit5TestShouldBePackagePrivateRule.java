d065908d2236500c579c4a40b0825db759dd403a
Sören Glimm <git@uncleowen.de>
2026-05-14 16:59:28 +0200

    Fix #5477:
    * Convert JUnit5TestShouldBePackagePrivateRule from XPath to Java
    * Use new TestFrameworksUtil.isJUnit5Class() to make the rule aware of
    superclasses

---
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/bestpractices/JUnit5TestShouldBePackagePrivateRule.java
@@ -0,0 +1,50 @@
+/*
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
+package net.sourceforge.pmd.lang.java.rule.bestpractices;
+
+import static net.sourceforge.pmd.lang.java.ast.ModifierOwner.Visibility.V_PROTECTED;
+import static net.sourceforge.pmd.lang.java.ast.ModifierOwner.Visibility.V_PUBLIC;
+import static net.sourceforge.pmd.lang.java.rule.internal.TestFrameworksUtil.isJUnit5Class;
+import static net.sourceforge.pmd.lang.java.rule.internal.TestFrameworksUtil.isJUnit5Method;
+
+import net.sourceforge.pmd.lang.java.ast.ASTClassDeclaration;
+import net.sourceforge.pmd.lang.java.ast.ASTMethodDeclaration;
+import net.sourceforge.pmd.lang.java.rule.AbstractJavaRulechainRule;
+import net.sourceforge.pmd.reporting.RuleContext;
+
+/**
+ * @since 6.35.0 (as XPath) / 7.25.0 (as Java)
+ */
+public class JUnit5TestShouldBePackagePrivateRule extends AbstractJavaRulechainRule {
+
+    public JUnit5TestShouldBePackagePrivateRule() {
+        super(ASTClassDeclaration.class, ASTMethodDeclaration.class);
+    }
+
+    @Override
+    public RuleContext visit(ASTClassDeclaration node, Object data) {
+        RuleContext ctx = (RuleContext) data;
+
+        if (isJUnit5Class(node) && (node.hasVisibility(V_PROTECTED) || node.hasVisibility(V_PUBLIC))) {
+            ctx.addViolation(node);
+        }
+
+        return ctx;
+    }
+
+    @Override
+    public RuleContext visit(ASTMethodDeclaration node, Object data) {
+        RuleContext ctx = (RuleContext) data;
+
+        if (isJUnit5Method(node)
+                && (node.hasVisibility(V_PROTECTED) || node.hasVisibility(V_PUBLIC))
+                && !node.getEnclosingType().isInterface()
+        ) {
+            ctx.addViolation(node);
+        }
+
+        return ctx;
+    }
+}
