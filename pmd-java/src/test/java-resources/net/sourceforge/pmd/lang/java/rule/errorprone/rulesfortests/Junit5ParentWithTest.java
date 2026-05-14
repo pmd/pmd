424420111eea230b3f23f5b1b6f35ebd590fa435
Sören Glimm <git@uncleowen.de>
2026-05-14 16:35:21 +0200

    rename package junit5testnoprivatemodifier.ParentWithTest to rulesfortests.Junit5ParentWithTest
    ... because I plan to use it in tests for different rules

--- pmd-java/src/test/java-resources/net/sourceforge/pmd/lang/java/rule/errorprone/rulesfortests/Junit5ParentWithTest.java
+++ pmd-java/src/test/java-resources/net/sourceforge/pmd/lang/java/rule/errorprone/rulesfortests/Junit5ParentWithTest.java
@@ -2,11 +2,11 @@
  * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
  */
 
-package net.sourceforge.pmd.lang.java.rule.errorprone.junit5testnoprivatemodifier;
+package net.sourceforge.pmd.lang.java.rule.errorprone.rulesfortests;
 
 import org.junit.jupiter.api.Test;
 
-public abstract class ParentWithTest {
+public abstract class Junit5ParentWithTest {
     @Test
     void testRegular() {
         // nothing
