12a0436e567b4c125ae1c1318d668b59c578e931
Sören Glimm <git@uncleowen.de>
2026-05-16 19:25:57 +0200

    Fix TestFrameworksUtil.isJUnit4Method() to use type resolution.
    This change removes the visibility check from isJUnit4Method. It was not
    needed (see https://pull-requests.pmd-code.org/pr-6685/cbd30fcbbd68a7b96a9b4eabbbf1c46a46f7a411/regression/).
    This brings isJUnit4Method in line with isJUnit5Method and isTestNgMethod.

--- pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/internal/TestFrameworksUtilTest.java
+++ pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/internal/TestFrameworksUtilTest.java
@@ -4,6 +4,7 @@
 
 package net.sourceforge.pmd.lang.java.rule.internal;
 
+import static net.sourceforge.pmd.lang.java.rule.internal.TestFrameworksUtil.isJUnit4Class;
 import static net.sourceforge.pmd.lang.java.rule.internal.TestFrameworksUtil.isJUnit5Class;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -26,6 +27,67 @@ class TestFrameworksUtilTest {
         ASTCompilationUnit root = java.parse("class A { { assertThat(1); } }");
         ASTMethodCall m = root.descendants(ASTMethodCall.class).toList().get(0);
         assertThat(TestFrameworksUtil.isProbableAssertCall(m)).isTrue();
+    }
+
+    @Nested
+    class IsJUnit4Class {
+        @Test
+        void aBasicClassIsNotAJUnit4Class() {
+            ASTCompilationUnit root = java.parse("public class A {}");
+            ASTClassDeclaration classDecl = root.firstChild(ASTClassDeclaration.class);
+
+            assertFalse(isJUnit4Class(classDecl));
+        }
+
+        @Test
+        void aClassWithATestIsAJUnit4Class() {
+            ASTCompilationUnit root = java.parse(
+                    "import org.junit.Test; public class A { @Test public void foo() {} }"
+            );
+            ASTClassDeclaration classDecl = root.firstChild(ASTClassDeclaration.class);
+
+            assertTrue(isJUnit4Class(classDecl));
+        }
+
+        @Test
+        void aClassWithATestIsAJUnit4ClassEvenIfTheTestIsInAParentClass() {
+            ASTCompilationUnit root = java.parse(
+                    "import net.sourceforge.pmd.lang.java.rule.errorprone.rulesfortests.JUnit4ParentWithTest; public class A extends JUnit4ParentWithTest {}"
+            );
+            ASTClassDeclaration classDecl = root.firstChild(ASTClassDeclaration.class);
+
+            assertTrue(isJUnit4Class(classDecl));
+        }
+
+        @Test
+        void anInterfaceWithATestIsANotJUnit4Class() {
+            ASTCompilationUnit root = java.parse(
+                    "import org.junit.Test; public interface A { @Test public default void foo() {} }"
+            );
+            ASTClassDeclaration classDecl = root.firstChild(ASTClassDeclaration.class);
+
+            assertFalse(isJUnit4Class(classDecl));
+        }
+
+        @Test
+        void anAbstractClassWithATestIsANotJUnit4Class() {
+            ASTCompilationUnit root = java.parse(
+                    "import org.junit.Test; public abstract class A { @Test public void foo() {} }"
+            );
+            ASTClassDeclaration classDecl = root.firstChild(ASTClassDeclaration.class);
+
+            assertFalse(isJUnit4Class(classDecl));
+        }
+
+        @Test
+        void aNestedClassWithATestIsANotJUnit4Class() {
+            ASTCompilationUnit root = java.parse(
+                    "import org.junit.Test; public class A { class B { @Test public void foo() {} } }"
+            );
+            ASTClassDeclaration classDecl = root.descendants(ASTClassDeclaration.class).last();
+
+            assertFalse(isJUnit4Class(classDecl));
+        }
     }
 
     @Nested
