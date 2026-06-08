9d564856c04d156a04575c908b03a604ebb5c2de
Zbynek Konecny <zbynek@geogebra.org>
2026-06-08 02:16:10 +0200

    Add more test frameworks, harden condition

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/errorprone/ConstantInEqualityAssertionRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/errorprone/ConstantInEqualityAssertionRule.java
@@ -7,6 +7,7 @@ package net.sourceforge.pmd.lang.java.rule.errorprone;
 import java.util.Arrays;
 import java.util.List;
 
+import net.sourceforge.pmd.lang.java.ast.ASTExpression;
 import net.sourceforge.pmd.lang.java.ast.ASTLiteral;
 import net.sourceforge.pmd.lang.java.ast.ASTMethodCall;
 import net.sourceforge.pmd.lang.java.rule.AbstractJavaRulechainRule;
@@ -18,22 +19,28 @@ import net.sourceforge.pmd.lang.java.types.InvocationMatcher;
 public class ConstantInEqualityAssertionRule extends AbstractJavaRulechainRule {
 
     private static final class EqualMethod {
-        InvocationMatcher matcher;
-        int actualPosition;
+        private final InvocationMatcher matcher;
+        private final int actualPosition;
+        private final int expectedPosition;
 
-        private EqualMethod(String pattern, int actualPosition) {
+        private EqualMethod(String pattern, int actualPosition, int expectedPosition) {
             this.matcher = InvocationMatcher.parse(pattern);
+            this.expectedPosition = expectedPosition;
             this.actualPosition = actualPosition;
         }
     }
 
     private static final List<EqualMethod> EQUAL_METHODS = Arrays.asList(
-        new EqualMethod("org.junit.Assert#assertEquals(_,_)", 1),
-        new EqualMethod("org.junit.Assert#assertEquals(_,_,_)", 2),
-        new EqualMethod("junit.framework.TestCase#assertEquals(_,_)", 1),
-        new EqualMethod("junit.framework.TestCase#assertEquals(_,_,_)", 2),
-        new EqualMethod("org.junit.jupiter.api.Assertions#assertEquals(_,_)", 1),
-        new EqualMethod("org.junit.jupiter.api.Assertions#assertEquals(_,_,_)", 1)
+        new EqualMethod("org.junit.Assert#assertEquals(_,_)", 1, 0),
+        new EqualMethod("org.junit.Assert#assertEquals(_,_,_)", 2, 1),
+        new EqualMethod("junit.framework.TestCase#assertEquals(_,_)", 1, 0),
+        new EqualMethod("junit.framework.TestCase#assertEquals(_,_,_)", 2, 1),
+        new EqualMethod("org.junit.jupiter.api.Assertions#assertEquals(_,_)", 1, 0),
+        new EqualMethod("org.junit.jupiter.api.Assertions#assertEquals(_,_,_)", 1, 0),
+        new EqualMethod("org.testng.Assert#assertEquals(_*)", 0, 1),
+        new EqualMethod("org.springframework.test.util.AssertionErrors#assertEquals(_,_,_)", 2, 1),
+        new EqualMethod("org.skyscreamer.jsonassert.JSONAssert#assertEquals(_,_,_)", 1, 0),
+        new EqualMethod("org.skyscreamer.jsonassert.JSONAssert#assertEquals(_,_,_,_)", 2, 1)
     );
 
     public ConstantInEqualityAssertionRule() {
@@ -42,10 +49,18 @@ public class ConstantInEqualityAssertionRule extends AbstractJavaRulechainRule {
 
     @Override
     public Object visit(ASTMethodCall node, Object data) {
+        if (!"assertEquals".equals(node.getMethodName())) {
+            return null;
+        }
         EQUAL_METHODS.forEach(method -> {
             if (method.matcher.matchesCall(node)) {
-                if (node.getArguments().get(method.actualPosition) instanceof ASTLiteral) {
-                    asCtx(data).addViolation(node);
+                ASTExpression actual = node.getArguments().get(method.actualPosition);
+                if (actual.getConstFoldingResult().getValue() != null) {
+                    if (actual instanceof ASTLiteral
+                            || node.getArguments().get(method.expectedPosition)
+                                    .getConstFoldingResult().getValue() == null) {
+                        asCtx(data).addViolation(node);
+                    }
                 }
             }
         });
