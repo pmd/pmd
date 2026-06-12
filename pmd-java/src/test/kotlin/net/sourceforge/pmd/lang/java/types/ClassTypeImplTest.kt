651c81f8dd4debf638318c1713c7321cc66b2b47
Sören Glimm <git@uncleowen.de>
2026-06-12 16:33:30 +0200

    Add tests for ClassTypeImpl.streamClasses()

--- pmd-java/src/test/kotlin/net/sourceforge/pmd/lang/java/types/ClassTypeImplTest.kt
+++ pmd-java/src/test/kotlin/net/sourceforge/pmd/lang/java/types/ClassTypeImplTest.kt
@@ -6,10 +6,18 @@
 package net.sourceforge.pmd.lang.java.types
 
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.Matcher
+import io.kotest.matchers.MatcherResult
+import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldHave
+import net.sourceforge.pmd.lang.java.JavaParsingHelper
+import net.sourceforge.pmd.lang.java.ast.ASTClassDeclaration
+import net.sourceforge.pmd.lang.java.ast.ASTCompilationUnit
+import net.sourceforge.pmd.lang.java.symbols.internal.asm.createUnresolvedAsmSymbol
 import net.sourceforge.pmd.lang.test.ast.IntelliMarker
 import net.sourceforge.pmd.lang.test.ast.shouldBe
-import net.sourceforge.pmd.lang.java.symbols.internal.asm.createUnresolvedAsmSymbol
+import java.util.stream.Stream
 
 /**
  * @author Clément Fournier
@@ -42,5 +50,26 @@ class ClassTypeImplTest : IntelliMarker,FunSpec({
         }
     }
 
+    context("streamClasses()") {
+        val parser: JavaParsingHelper = JavaParsingHelper.DEFAULT.withResourceContext(javaClass);
+
+        test("class with superclass") {
+            val source = """
+                class Parent {
+                    class NestedInsideParent {}
+                }
+                class Child extends Parent {
+                    class NestedInsideChild {}
+                }
+            """.trimIndent()
+            val root: ASTCompilationUnit = parser.parse(source)
+            val classDecl: ASTClassDeclaration = root.children(ASTClassDeclaration::class.java).filter{it.canonicalName == "Child"}.first()!!
+            val typeMirror: JTypeMirror = classDecl.typeMirror
+
+            typeMirror.streamClasses().count() shouldBe 2
+            typeMirror.streamClasses().anyMatch { it.symbol?.simpleName == "NestedInsideParent" }.shouldBeTrue()
+            typeMirror.streamClasses().anyMatch { it.symbol?.simpleName == "NestedInsideChild" }.shouldBeTrue()
+        }
+    }
 
 })
