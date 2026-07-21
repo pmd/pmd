835a7b2ee0d14845f858fd19906bbf187a384382
Andreas Dangel <andreas.dangel@pmd-code.org>
2026-07-21 19:02:14 +0200

    [test] Increase cache size for AuxClasspathLoader

--- pmd-test/src/main/java/net/sourceforge/pmd/test/internal/RuleTstLauncherSession.java
+++ pmd-test/src/main/java/net/sourceforge/pmd/test/internal/RuleTstLauncherSession.java
@@ -21,7 +21,13 @@ public class RuleTstLauncherSession implements LauncherSessionListener {
     @Override
     public void launcherSessionOpened(LauncherSession session) {
         LOG.debug("Enabling AuxClasspathLoader reuse");
-        AuxClasspathLoader.enableReuse(1);
+        // Note: in tests in pmd-java, we might create multiple different instances.
+        // Especially, we might create the instances only once and store them in static fields.
+        // If the cache is too small, cache eviction takes place and closes the AuxClasspathLoader,
+        // that might still be used via static fields.
+        // E.g. net.sourceforge.pmd.lang.java.JavaParsingHelper creates one,
+        // and net.sourceforge.pmd.lang.java.symbols.ClassLoadingChildFirstTest creates another one.
+        AuxClasspathLoader.enableReuse(2);
     }
 
     @Override
