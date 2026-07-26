0013d7c711b3deeb76c528ade658cfb027f4d750
Sören Glimm <git@uncleowen.de>
2026-07-26 20:35:19 +0200

    New rule: UnusedReturnValue

---
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/errorprone/UnusedReturnValueRule.java
@@ -0,0 +1,87 @@
+/*
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
+package net.sourceforge.pmd.lang.java.rule.errorprone;
+
+import net.sourceforge.pmd.lang.java.ast.ASTExpressionStatement;
+import net.sourceforge.pmd.lang.java.ast.ASTMethodCall;
+import net.sourceforge.pmd.lang.java.rule.AbstractJavaRulechainRule;
+import net.sourceforge.pmd.lang.java.symbols.AnnotableSymbol;
+import net.sourceforge.pmd.lang.java.symbols.JClassSymbol;
+import net.sourceforge.pmd.lang.java.symbols.JExecutableSymbol;
+import net.sourceforge.pmd.lang.java.symbols.JTypeDeclSymbol;
+import net.sourceforge.pmd.lang.java.symbols.SymbolResolver;
+import net.sourceforge.pmd.lang.java.types.JMethodSig;
+import net.sourceforge.pmd.lang.java.types.Substitution;
+import net.sourceforge.pmd.lang.java.types.TypeSystem;
+import net.sourceforge.pmd.reporting.RuleContext;
+
+/**
+ * @since 7.27.0
+ */
+public class UnusedReturnValueRule extends AbstractJavaRulechainRule {
+
+    private static final String CHECK_RETURN_VALUE_ANNOTATION = "CheckReturnValue";
+    private static final String CAN_IGNORE_RETURN_VALUE_ANNOTATION = "CanIgnoreReturnValue";
+
+    public UnusedReturnValueRule() {
+        super(ASTMethodCall.class);
+    }
+
+    @Override
+    public RuleContext visit(ASTMethodCall call, Object data) {
+        RuleContext ctx = (RuleContext) data;
+
+        if (shouldCheckResult(call) && !isResultUsed(call)) {
+            ctx.addViolation(call, formatCall(call));
+        }
+
+        return ctx;
+    }
+
+    private String formatCall(ASTMethodCall call) {
+        JMethodSig methodSig = call.getMethodType();
+
+        return methodSig.getDeclaringType() + "." + methodSig.getName() + "()";
+    }
+
+    private boolean shouldCheckResult(ASTMethodCall call) {
+        return isCheckReturnValueAnnotated(call);
+    }
+
+    // visible for testing
+    /* private */ static boolean isCheckReturnValueAnnotated(ASTMethodCall call) {
+        JExecutableSymbol methodSymbol = call.getMethodType().getSymbol();
+        if (methodSymbol.getReturnType(Substitution.EMPTY).isVoid()) {
+            return false;
+        }
+
+        if (isAnnotatedWith(methodSymbol, CHECK_RETURN_VALUE_ANNOTATION)) {
+            return true;
+        }
+
+        JTypeDeclSymbol classSymbol = call.getMethodType().getDeclaringType().getSymbol();
+        if (isAnnotatedWith(classSymbol, CHECK_RETURN_VALUE_ANNOTATION)
+                && !isAnnotatedWith(methodSymbol, CAN_IGNORE_RETURN_VALUE_ANNOTATION)
+        ) {
+            return true;
+        }
+
+        TypeSystem typeSystem = classSymbol.getTypeSystem();
+        SymbolResolver resolver = typeSystem.bootstrapResolver();
+        JClassSymbol packageSymbol = resolver.resolveClassFromCanonicalName(classSymbol.getPackageName() + ".package-info");
+        return packageSymbol != null
+                && isAnnotatedWith(packageSymbol, CHECK_RETURN_VALUE_ANNOTATION)
+                && !isAnnotatedWith(classSymbol, CAN_IGNORE_RETURN_VALUE_ANNOTATION)
+                && !isAnnotatedWith(methodSymbol, CAN_IGNORE_RETURN_VALUE_ANNOTATION);
+    }
+
+    private static boolean isAnnotatedWith(AnnotableSymbol symbol, String name) {
+        return symbol.getDeclaredAnnotations().stream().anyMatch(annotation -> name.equals(annotation.getSimpleName()));
+    }
+
+    private boolean isResultUsed(ASTMethodCall call) {
+        return !(call.getParent() instanceof ASTExpressionStatement);
+    }
+}
