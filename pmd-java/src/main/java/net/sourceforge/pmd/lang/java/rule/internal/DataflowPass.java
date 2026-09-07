8bb86c5505ab050ce4dee0b9068ab9a2f4cc3b1d
Sören Glimm <git@uncleowen.de>
2026-09-07 15:36:35 +0200

    Remove remaining usages of Node.getImage() from Java rules in pmd-java

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/internal/DataflowPass.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/internal/DataflowPass.java
@@ -881,12 +881,12 @@ public final class DataflowPass {
 
         @Override
         public SpanInfo visit(ASTContinueStatement node, SpanInfo data) {
-            return data.global.continueTargets.doBreak(data, node.getImage());
+            return data.global.continueTargets.doBreak(data, node.getLabel());
         }
 
         @Override
         public SpanInfo visit(ASTBreakStatement node, SpanInfo data) {
-            return processBreakableStmt(node, data, () -> data.global.breakTargets.doBreak(data, node.getImage()));
+            return processBreakableStmt(node, data, () -> data.global.breakTargets.doBreak(data, node.getLabel()));
         }
 
         @Override
