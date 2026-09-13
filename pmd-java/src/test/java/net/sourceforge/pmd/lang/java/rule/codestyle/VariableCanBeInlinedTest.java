173b24fa43f88bda0f826eac4cfe3fe20d8cbac1
Sören Glimm <git@uncleowen.de>
2026-09-13 20:11:25 +0200

    We really don't need a test for that.
    Only make actually removing the property harder.

--- pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/codestyle/VariableCanBeInlinedTest.java
+++ pmd-java/src/test/java/net/sourceforge/pmd/lang/java/rule/codestyle/VariableCanBeInlinedTest.java
@@ -4,23 +4,8 @@
 
 package net.sourceforge.pmd.lang.java.rule.codestyle;
 
-import static net.sourceforge.pmd.properties.internal.PropertyParsingUtil.DEPRECATED_RULE_PROPERTY_MARKER;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import org.junit.jupiter.api.Test;
-
-import net.sourceforge.pmd.properties.PropertyDescriptor;
 import net.sourceforge.pmd.test.PmdRuleTst;
 
 class VariableCanBeInlinedTest extends PmdRuleTst {
-
-    @Test
-    void statementOrderMattersPropertyIsMarkedDeprecated() {
-        PropertyDescriptor<?> descriptor =
-                new VariableCanBeInlinedRule().getPropertyDescriptor("statementOrderMatters");
-
-        assertNotNull(descriptor);
-        assertTrue(descriptor.description().startsWith(DEPRECATED_RULE_PROPERTY_MARKER));
-    }
+    // no additional unit tests
 }
