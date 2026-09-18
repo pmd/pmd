pmd/pmd

commit cb6e77265e0fe09deaf4183582294839c0987a82

[java] Fix #6010: Add replacement for ClasspathClassloader (#6859)
* [java] Avoid using JvmLanguagePropertyBundle#getAnalysisClassLoader

Instead, use new AuxClasspathLoader, which will replace
ClasspathClassLoader (an internal class).

Might fix #6010

* [java] Adding caching for AuxClasspathLoader

This is especially needed for RuleTst.

* fix: Use setAuxclasspath instead of prependAuxClasspath

So that JvmLanguageProperty.AUX_CLASSPATH is wired...

* [doc] Update release notes (#6010)

* Fix PMD Violations

* Improve performance of AuxClasspathLoader

Cache the information whether the entry is a file or directory,
to avoid asking the filesystem over and over again.

* Fix tests

* [java] AuxClasspathLoader: Use AuxClasspathUtil

* [java] JavaLanguageProcessor: Use AuxClasspathUtil

* [java] PmdAnalysis: Suggest to use new setAuxClasspath

instead of prependAuxClasspath

* Fix typos

* [core] Move AuxClasspathLoader from java to core

* [core] AuxClasspathLoader: Support caching for RuleTst

* [test] Make RuleTstLauncherSession internal

* [core] Deprecated ClasspathClassLoader

* [core] Make AuxClasspathLoader public but experimental

* [java] Deprecate TypeSystem#usingClassLoaderClasspath

* [core] Ignore CPD duplication in deprecated ClasspathClassLoader

* [core] AuxClasspathLoader: Add missing support for module-info.class

Also add test for supporting different Java versions
(loadFromJava)

* [core] Refactor ClasspathClassLoader to use AuxClasspathLoader

* [java] Update tests to use AuxClasspathLoader

* [core] Fix JvmLanguagePropertyBundleTest

* [core] Fix PMD violations

* [core] Correctly close AuxClasspathLoader from ClasspathClassLoader

* [core] Fix AnalysisCache integration with auxClasspath

* [core] Address review comments

* [test] Silent out warning about missing jrt-fs.jar in RuleTst

* [core] AuxClasspathLoader: close the removed eldest entry

* [core] AuxClasspathLoader: refactor to initialize in ctor

pmd-core/src/main/java/net/sourceforge/pmd/util/internal/AuxClasspathUtil.java
@@ -15,12 +15,14 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -71,23 +73,29 @@ public final class AuxClasspathUtil {
      *
      * @see <a href="https://openjdk.org/jeps/220">JEP 220: Modular Run-Time Images</a>
      */
-    public static List<Path> getPlatformClasspath() {
+    public static Path getPlatformClasspath() {
         String javaHome = System.getProperty("java.home");
         Path jrtFsJar = Paths.get(javaHome, "lib", "jrt-fs.jar"); // Java 11+
         Path rtJar = Paths.get(javaHome, "lib", "rt.jar"); // Java 8
         if (Files.isRegularFile(jrtFsJar)) {
             LOG.debug("Found current JVM runtime classes at {}", jrtFsJar);
-            return Collections.singletonList(jrtFsJar);
+            return jrtFsJar;
         } else if (Files.isRegularFile(rtJar)) {
             LOG.debug("Found current JVM runtime classes at {}", rtJar);
-            return Collections.singletonList(rtJar);
+            return rtJar;
         }
         throw new IllegalStateException("Could not determine current jvm classpath");
     }
 
+    public static String toRawClasspath(List<Path> paths, Path... additionalPaths) {
+        List<Path> completePath = new ArrayList<>(paths);
+        completePath.addAll(Arrays.asList(additionalPaths));
+        return StringUtils.join(completePath, File.pathSeparator);
+    }
+
     /**
      * Uses the given configuration to either return the classpath entries from an externally
-     * provided classloader (soon to be deprecated functionality) or from the given auxClasspath
+     * provided classloader (deprecated functionality) or from the given auxClasspath
      * (CLI option {@code --aux-classpath}).
      *
      * @see #expandClasspath(String)
@@ -96,19 +104,23 @@ public final class AuxClasspathUtil {
         List<Path> result = new ArrayList<>();
 
         ClassLoader classLoader = configuration.getClassLoader();
-        try {
-            if (classLoader instanceof URLClassLoader) {
-                @SuppressWarnings("PMD.CloseResource") // we just need to get the URLs, don't close it here. the classloader will be needed later on...
-                URLClassLoader urlClassLoader = (URLClassLoader) classLoader;
-                for (URL url : urlClassLoader.getURLs()) {
-                    result.add(Paths.get(url.toURI()));
+        if (!PMDConfiguration.class.getClassLoader().equals(classLoader)) {
+            try {
+                if (classLoader instanceof URLClassLoader) {
+                    @SuppressWarnings("PMD.CloseResource") // we just need to get the URLs, don't close it here. the classloader will be needed later on...
+                    URLClassLoader urlClassLoader = (URLClassLoader) classLoader;
+                    for (URL url : urlClassLoader.getURLs()) {
+                        result.add(Paths.get(url.toURI()));
+                    }
+                    return result;
                 }
+            } catch (URISyntaxException e) {
+                throw new RuntimeException(e);
             }
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
         }
 
-        return result;
+        String auxClasspath = configuration.getAuxClasspath();
+        return AuxClasspathUtil.expandClasspath(auxClasspath);
     }
 
     /**

pmd-java/src/main/java/net/sourceforge/pmd/lang/java/internal/JavaLanguageProcessor.java
@@ -4,6 +4,9 @@
 
 package net.sourceforge.pmd.lang.java.internal;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Objects;
 
@@ -11,6 +14,7 @@ import org.checkerframework.checker.nullness.qual.NonNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import net.sourceforge.pmd.lang.JvmLanguagePropertyBundle;
 import net.sourceforge.pmd.lang.LanguageVersionHandler;
 import net.sourceforge.pmd.lang.ast.Parser;
 import net.sourceforge.pmd.lang.impl.BatchLanguageProcessor;
@@ -30,7 +34,9 @@ import net.sourceforge.pmd.lang.metrics.LanguageMetricsProvider;
 import net.sourceforge.pmd.lang.rule.xpath.impl.XPathHandler;
 import net.sourceforge.pmd.reporting.ViolationDecorator;
 import net.sourceforge.pmd.reporting.ViolationSuppressor;
+import net.sourceforge.pmd.util.AuxClasspathLoader;
 import net.sourceforge.pmd.util.designerbindings.DesignerBindings;
+import net.sourceforge.pmd.util.internal.AuxClasspathUtil;
 
 /**
  * @author Clément Fournier
@@ -45,10 +51,12 @@ public class JavaLanguageProcessor extends BatchLanguageProcessor<JavaLanguagePr
     private final JavaParser parserWithoutProcessing;
     private final boolean firstClassLombok;
     private TypeSystem typeSystem;
+    private AuxClasspathLoader auxClasspathLoader;
 
-    public JavaLanguageProcessor(JavaLanguageProperties properties, TypeSystem typeSystem) {
+    public JavaLanguageProcessor(JavaLanguageProperties properties) {
         super(properties);
-        this.typeSystem = typeSystem;
+
+        initTypeSystem(properties);
 
         String suppressMarker = properties.getSuppressMarker();
         this.parser = new JavaParser(suppressMarker, this, true);
@@ -56,9 +64,27 @@ public class JavaLanguageProcessor extends BatchLanguageProcessor<JavaLanguagePr
         this.firstClassLombok = properties.getProperty(JavaLanguageProperties.FIRST_CLASS_LOMBOK);
     }
 
-    public JavaLanguageProcessor(JavaLanguageProperties properties) {
-        this(properties, TypeSystem.usingClassLoaderClasspath(properties.getAnalysisClassLoader()));
-        LOG.debug("Using analysis classloader: {}", properties.getAnalysisClassLoader());
+    private void initTypeSystem(JavaLanguageProperties properties) {
+        ClassLoader externallyConfiguredClassLoader = properties.getExternalClassLoader();
+        if (externallyConfiguredClassLoader != null) {
+            LOG.debug("Using externally configured classloader as analysis classloader: {}", externallyConfiguredClassLoader);
+            this.typeSystem = TypeSystem.usingClassLoaderClasspath(externallyConfiguredClassLoader);
+        } else {
+            String auxClasspath = properties.getProperty(JvmLanguagePropertyBundle.AUX_CLASSPATH);
+
+            Path relativeJrtFsJar = Paths.get("lib/jrt-fs.jar");
+            Path relativeRtJar = Paths.get("lib/rt.jar");
+            if (!auxClasspath.contains(relativeJrtFsJar.toString()) && !auxClasspath.contains(relativeRtJar.toString())) {
+                Path platformClasspath = AuxClasspathUtil.getPlatformClasspath();
+                LOG.warn("Adding current platform {} to auxClasspath, which could be the wrong java version. "
+                        + "Please add the correct jrt-fs.jar explicitly to the auxClasspath.",
+                        platformClasspath);
+                auxClasspath += File.pathSeparator + platformClasspath;
+            }
+            LOG.debug("Using auxClasspath as analysis classloader: {}", auxClasspath);
+            this.auxClasspathLoader = AuxClasspathLoader.create(auxClasspath);
+            this.typeSystem = TypeSystem.usingClasspath(name -> auxClasspathLoader.findResource(name));
+        }
     }
 
     @Override
@@ -139,6 +165,9 @@ public class JavaLanguageProcessor extends BatchLanguageProcessor<JavaLanguagePr
     @Override
     public void close() throws Exception {
         this.typeSystem.logStats();
+        if (this.auxClasspathLoader != null) {
+            this.auxClasspathLoader.close();
+        }
         super.close();
     }
 }

pmd-core/src/test/java/net/sourceforge/pmd/util/internal/AuxClasspathUtilTest.java
@@ -127,4 +127,15 @@ class AuxClasspathUtilTest {
                 aJar
         ), AuxClasspathUtil.getAuxClasspath(configuration));
     }
+
+    @Test
+    void fromConfigurationAuxClasspath() throws IOException {
+        Path bJar = Files.createFile(tempDir.resolve("b.jar"));
+        PMDConfiguration configuration = new PMDConfiguration();
+        configuration.setAuxClasspath(bJar.toString());
+
+        assertEquals(CollectionUtil.listOf(
+                bJar
+        ), AuxClasspathUtil.getAuxClasspath(configuration));
+    }
 }

pmd-ant/src/main/java/net/sourceforge/pmd/ant/internal/PMDTaskImpl.java
@@ -231,7 +231,7 @@ public class PMDTaskImpl {
         try {
             if (auxClasspath != null) {
                 project.log("Using auxclasspath: " + auxClasspath, Project.MSG_VERBOSE);
-                configuration.prependAuxClasspath(auxClasspath.toString());
+                configuration.setAuxClasspath(auxClasspath.toString());
             }
         } catch (IllegalArgumentException ioe) {
             throw new BuildException(ioe.getMessage(), ioe);

pmd-cli/src/main/java/net/sourceforge/pmd/cli/commands/internal/PmdCommand.java
@@ -279,7 +279,7 @@ public class PmdCommand extends AbstractAnalysisPmdSubcommand<PMDConfiguration> 
         configuration.setReporter(new SimpleMessageReporter(LoggerFactory.getLogger(PmdCommand.class)));
 
         try {
-            configuration.prependAuxClasspath(auxClasspath);
+            configuration.setAuxClasspath(auxClasspath);
         } catch (IllegalArgumentException e) {
             throw new ParameterException(spec.commandLine(), "Invalid auxiliary classpath: " + e.getMessage(), e);
         }

pmd-core/src/main/java/net/sourceforge/pmd/PmdAnalysis.java
@@ -130,7 +130,7 @@ import net.sourceforge.pmd.util.log.PmdReporter;
  * <h2>Specifying the Java classpath</h2>
  *
  * <p>Java rules work better if you specify the path to the compiled classes
- * of the analysed sources. See {@link PMDConfiguration#prependAuxClasspath(String)}.
+ * of the analysed sources. See {@link PMDConfiguration#setAuxClasspath(String)}.
  *
  * <h2>Customizing message output</h2>
  *
@@ -218,11 +218,11 @@ public final class PmdAnalysis implements AutoCloseable {
             //  CLI syntax is implemented. #2947
             props.setProperty(LanguagePropertyBundle.SUPPRESS_MARKER, config.getSuppressMarker());
             if (props instanceof JvmLanguagePropertyBundle) {
-                ClassLoader externallyConfiguredClassLoader = config.getClassLoader();
-                if (externallyConfiguredClassLoader != null) {
-                    ((JvmLanguagePropertyBundle) props).setClassLoader(externallyConfiguredClassLoader);
-                } else {
+                if (config.getAuxClasspath() != null) {
                     props.setProperty(JvmLanguagePropertyBundle.AUX_CLASSPATH, config.getAuxClasspath());
+                } else {
+                    ClassLoader externallyConfiguredClassLoader = config.getClassLoader();
+                    ((JvmLanguagePropertyBundle) props).setClassLoader(externallyConfiguredClassLoader);
                 }
             }
         }

commit 513bd8afa242bea29664f43310c4646f3ab54302

[java] Fix #7081: Exception when auxClasspath is a file URL
Regression has been introduced with cb6e77265e (#6859).
Solution is to first expand the given classpath (in case
it is a file: URL) and only after that add the platform
classpath, if it is needed.

pmd-core/src/main/java/net/sourceforge/pmd/util/internal/AuxClasspathUtil.java
@@ -87,6 +87,21 @@ public final class AuxClasspathUtil {
         throw new IllegalStateException("Could not determine current jvm classpath");
     }
 
+    /**
+     * Returns whether the given (already expanded) classpath entries contain the platform
+     * classpath, ie a {@code lib/jrt-fs.jar} or {@code lib/rt.jar} entry.
+     *
+     * @since 7.28.0
+     */
+    public static boolean containsPlatformClasspath(List<Path> entries) {
+        Path relativeJrtFsJar = Paths.get("lib/jrt-fs.jar");
+        Path relativeRtJar = Paths.get("lib/rt.jar");
+
+        return entries.stream()
+                .map(Path::toAbsolutePath)
+                .anyMatch(p -> p.endsWith(relativeJrtFsJar) || p.endsWith(relativeRtJar));
+    }
+
     public static String toRawClasspath(List<Path> paths, Path... additionalPaths) {
         List<Path> completePath = new ArrayList<>(paths);
         completePath.addAll(Arrays.asList(additionalPaths));
@@ -153,6 +168,7 @@ public final class AuxClasspathUtil {
                     path = Paths.get(classpath.substring(5));
                 }
 
+                // TODO: PMD 8: Use UTF-8
                 try (Stream<String> lines = Files.lines(path, Charset.defaultCharset())) {
                     entries.addAll(lines
                             .map(String::trim)

pmd-java/src/main/java/net/sourceforge/pmd/lang/java/internal/JavaLanguageProcessor.java
@@ -4,9 +4,8 @@
 
 package net.sourceforge.pmd.lang.java.internal;
 
-import java.io.File;
 import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -70,19 +69,18 @@ public class JavaLanguageProcessor extends BatchLanguageProcessor<JavaLanguagePr
             LOG.debug("Using externally configured classloader as analysis classloader: {}", externallyConfiguredClassLoader);
             this.typeSystem = TypeSystem.usingClassLoaderClasspath(externallyConfiguredClassLoader);
         } else {
-            String auxClasspath = properties.getProperty(JvmLanguagePropertyBundle.AUX_CLASSPATH);
+            String rawAuxClasspath = properties.getProperty(JvmLanguagePropertyBundle.AUX_CLASSPATH);
+            List<Path> auxClasspath = new ArrayList<>(AuxClasspathUtil.expandClasspath(rawAuxClasspath));
 
-            Path relativeJrtFsJar = Paths.get("lib/jrt-fs.jar");
-            Path relativeRtJar = Paths.get("lib/rt.jar");
-            if (!auxClasspath.contains(relativeJrtFsJar.toString()) && !auxClasspath.contains(relativeRtJar.toString())) {
+            if (!AuxClasspathUtil.containsPlatformClasspath(auxClasspath)) {
                 Path platformClasspath = AuxClasspathUtil.getPlatformClasspath();
                 LOG.warn("Adding current platform {} to auxClasspath, which could be the wrong java version. "
-                        + "Please add the correct jrt-fs.jar explicitly to the auxClasspath.",
-                        platformClasspath);
-                auxClasspath += File.pathSeparator + platformClasspath;
+                        + "Please add the correct jrt-fs.jar explicitly to the auxClasspath.", platformClasspath);
+                auxClasspath.add(platformClasspath);
             }
-            LOG.debug("Using auxClasspath as analysis classloader: {}", auxClasspath);
-            this.auxClasspathLoader = AuxClasspathLoader.create(auxClasspath);
+            String expandedAuxClasspath = AuxClasspathUtil.toRawClasspath(auxClasspath);
+            LOG.debug("Using auxClasspath as analysis classloader: {}", expandedAuxClasspath);
+            this.auxClasspathLoader = AuxClasspathLoader.create(expandedAuxClasspath);
             this.typeSystem = TypeSystem.usingClasspath(name -> auxClasspathLoader.findResource(name));
         }
     }

pmd-core/src/test/java/net/sourceforge/pmd/util/internal/AuxClasspathUtilTest.java
@@ -6,6 +6,8 @@ package net.sourceforge.pmd.util.internal;
 
 import static java.util.Collections.emptyList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
@@ -15,6 +17,7 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Collections;
 
 import org.junit.jupiter.api.Test;
@@ -137,5 +140,18 @@ class AuxClasspathUtilTest {
         assertEquals(CollectionUtil.listOf(
                 bJar
         ), AuxClasspathUtil.getAuxClasspath(configuration));
+    }
+
+    @Test
+    void containsPlatform() {
+        Path lib1 = Paths.get("somwhere/lib1.jar");
+        Path rt = Paths.get("somewhere/lib/rt.jar");
+        Path platformClasspath = AuxClasspathUtil.getPlatformClasspath();
+
+        assertFalse(AuxClasspathUtil.containsPlatformClasspath(Collections.singletonList(lib1)));
+        assertTrue(AuxClasspathUtil.containsPlatformClasspath(Arrays.asList(lib1, platformClasspath)));
+        assertTrue(AuxClasspathUtil.containsPlatformClasspath(Arrays.asList(platformClasspath, lib1)));
+        assertTrue(AuxClasspathUtil.containsPlatformClasspath(Arrays.asList(lib1, rt)));
+        assertTrue(AuxClasspathUtil.containsPlatformClasspath(Arrays.asList(rt, lib1)));
     }
 }

pmd-java/src/test/java/net/sourceforge/pmd/lang/java/internal/JavaLanguageProcessorTest.java
@@ -1,0 +1,132 @@
+/*
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
+package net.sourceforge.pmd.lang.java.internal;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import net.sourceforge.pmd.lang.JvmLanguagePropertyBundle;
+import net.sourceforge.pmd.lang.LanguagePropertyBundle;
+import net.sourceforge.pmd.lang.java.JavaLanguageModule;
+import net.sourceforge.pmd.lang.java.symbols.JClassSymbol;
+import net.sourceforge.pmd.util.internal.AuxClasspathUtil;
+
+import com.github.stefanbirkner.systemlambda.SystemLambda;
+
+class JavaLanguageProcessorTest {
+    @TempDir
+    private Path tempDir;
+
+    @Test
+    void classpathListWithJrtFs() throws Exception {
+        String auxClasspath = writeClasspathFile("classpath-with-jrtfs.txt", true);
+        String log = SystemLambda.tapSystemErr(() -> assertPlatformClassesFound(auxClasspath));
+        assertTrue(log.isEmpty(), "unexpected output: " + log);
+    }
+
+    @Test
+    void classpathListWithoutJrtFs() throws Exception {
+        String auxClasspath = writeClasspathFile("classpath-without-jrtfs.txt", false);
+        String log = SystemLambda.tapSystemErr(() -> assertPlatformClassesFound(auxClasspath));
+        assertThat(log, containsString("Adding current platform"));
+    }
+
+    @Test
+    void auxClasspathWithJrtFs() throws Exception {
+        String auxClasspath = createRawClasspath(true);
+        String log = SystemLambda.tapSystemErr(() -> assertPlatformClassesFound(auxClasspath));
+        assertTrue(log.isEmpty(), "unexpected output: " + log);
+    }
+
+    @Test
+    void auxClasspathWithoutJrtFs() throws Exception {
+        String auxClasspath = createRawClasspath(false);
+        String log = SystemLambda.tapSystemErr(() -> assertPlatformClassesFound(auxClasspath));
+        assertThat(log, containsString("Adding current platform"));
+    }
+
+    @Test
+    void emptyClasspathWithoutJrtFs() throws Exception {
+        String log = SystemLambda.tapSystemErr(() -> assertPlatformClassesFound(""));
+        assertThat(log, containsString("Adding current platform"));
+    }
+
+    /**
+     * AuxClasspathLoader uses a static cache. Check that we don't create new
+     * instances the lead to cache eviction and other failing tests.
+     *
+     * @see net.sourceforge.pmd.lang.java.JavaParsingHelper#TEST_AUX_CLASSPATH_LOADER
+     */
+    @Test
+    void ensureSameClasspath() throws IOException {
+        final String javaParsingHelperClasspath = AuxClasspathUtil.toRawClasspath(
+                AuxClasspathUtil.getRuntimeClasspath(),
+                AuxClasspathUtil.getPlatformClasspath());
+
+        // platform classpath already provided
+        String auxClasspath = writeClasspathFile("same-classpath-with-jrtfs.txt", true);
+        assertEquals(javaParsingHelperClasspath,
+                AuxClasspathUtil.toRawClasspath(AuxClasspathUtil.expandClasspath(auxClasspath)));
+
+        // platform classpath added implicitly
+        auxClasspath = writeClasspathFile("same-classpath-test-without-jrtfs.txt", false);
+        assertEquals(javaParsingHelperClasspath,
+                AuxClasspathUtil.toRawClasspath(AuxClasspathUtil.expandClasspath(auxClasspath), AuxClasspathUtil.getPlatformClasspath()));
+
+        // without a classpath file - platform classes already provided
+        auxClasspath = createRawClasspath(true);
+        assertEquals(javaParsingHelperClasspath,
+                AuxClasspathUtil.toRawClasspath(AuxClasspathUtil.expandClasspath(auxClasspath)));
+        // without a classpath file - platform classpath added implicitly
+        auxClasspath = createRawClasspath(false);
+        assertEquals(javaParsingHelperClasspath,
+                AuxClasspathUtil.toRawClasspath(AuxClasspathUtil.expandClasspath(auxClasspath), AuxClasspathUtil.getPlatformClasspath()));
+    }
+
+    private void assertPlatformClassesFound(String auxClasspath) throws Exception {
+        LanguagePropertyBundle properties = JavaLanguageModule.getInstance().newPropertyBundle();
+        properties.setProperty(JvmLanguagePropertyBundle.AUX_CLASSPATH, auxClasspath);
+        try (JavaLanguageProcessor processor = new JavaLanguageProcessor((JavaLanguageProperties) properties)) {
+            JClassSymbol classSymbol = processor.getTypeSystem().getClassSymbol("java.lang.Object");
+            assertNotNull(classSymbol);
+        }
+    }
+
+    private String writeClasspathFile(String name, boolean addPlatformClasspath) throws IOException {
+        List<String> lines = new ArrayList<>();
+        lines.add("# classpath file for " + name);
+        AuxClasspathUtil.getRuntimeClasspath().forEach(p -> lines.add(p.toString()));
+        if (addPlatformClasspath) {
+            lines.add(AuxClasspathUtil.getPlatformClasspath().toString());
+        }
+        Path classpathFile = tempDir.resolve(name);
+        Files.write(classpathFile, lines, Charset.defaultCharset());
+        return classpathFile.toUri().toString(); // -> file:///.../classpath-with-jrtfs.txt
+    }
+
+    private String createRawClasspath(boolean addPlatformClasspath) {
+        String rawClasspath = AuxClasspathUtil.getRuntimeClasspath()
+                .stream().map(Path::toString).collect(Collectors.joining(File.pathSeparator));
+        if (addPlatformClasspath) {
+            rawClasspath += File.pathSeparator + AuxClasspathUtil.getPlatformClasspath().toString();
+        }
+        return rawClasspath;
+    }
+}