5ce92383e49b2ce5baea5f3e253106e20ba63b6f
Yuriy Tumanov <57435526+Eljees@users.noreply.github.com>
2026-09-26 12:45:56 +0300

    [java] ExceptionAsFlowControl: stop at a lambda body (#7105)
    * [java] ExceptionAsFlowControl: stop at a lambda body
    
    An exception thrown in a lambda body propagates to whoever invokes the
    lambda, not to the try statement around the lambda expression.
    
    Fixes #4815
    
    * Move lambda expression check into the loop header.
    
    * formatting
    
    * [docs] Update release notes
    
    ---------
    
    Co-authored-by: Sören Glimm <git@uncleowen.de>

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/design/ExceptionAsFlowControlRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/design/ExceptionAsFlowControlRule.java
@@ -6,6 +6,7 @@ package net.sourceforge.pmd.lang.java.rule.design;
 
 import net.sourceforge.pmd.lang.java.ast.ASTBodyDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTCatchClause;
+import net.sourceforge.pmd.lang.java.ast.ASTLambdaExpression;
 import net.sourceforge.pmd.lang.java.ast.ASTThrowStatement;
 import net.sourceforge.pmd.lang.java.ast.ASTTryStatement;
 import net.sourceforge.pmd.lang.java.ast.JavaNode;
@@ -31,7 +32,12 @@ public class ExceptionAsFlowControlRule extends AbstractJavaRulechainRule {
     public Object visit(ASTThrowStatement node, Object data) {
         JTypeMirror thrownType = node.getExpr().getTypeMirror();
         JavaNode parent = node.getParent();
-        while (!(parent instanceof ASTBodyDeclaration)) {
+        while (!(parent instanceof ASTBodyDeclaration)
+                // An exception thrown in a lambda body leaves the lambda, not
+                // the enclosing method: it reaches whoever invokes the lambda,
+                // which in general is not the try statement around it. See #4815.
+                && !(parent instanceof ASTLambdaExpression)
+        ) {
             if (parent instanceof ASTCatchClause) {
                 // if the exception is thrown in a catch block, then we
                 // have to ignore the try stmt (jump past it).
